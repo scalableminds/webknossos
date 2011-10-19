768d764639a73acf4a60e10f583ea381c3304da7
Thomas Werkmeister <Thomas.Werkmeister@scalableminds.com>
2011-10-19 14:46:17 +0200

    MongoConfig was missing in previous commit

---
+++ src/main/scala/com/scalableminds/brainflight/model/MongoConfig.scala
@@ -0,0 +1,24 @@
+package com.scalableminds.brainflight.model
+
+import net.liftweb._
+import mongodb._
+import com.mongodb.{Mongo}
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lesnail
+ * Date: 19.10.11
+ * Time: 14:29
+ * To change this template use File | Settings | File Templates.
+ */
+
+object UserDb extends MongoIdentifier{
+  val jndiName = "user"
+}
+
+object MongoConfig {
+  def init: Unit = {
+    val myMongo = new Mongo("127.0.0.1", 27017)
+    MongoDB.defineDb(UserDb, myMongo, "user")
+  }
+}

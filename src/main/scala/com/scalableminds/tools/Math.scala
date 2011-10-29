db6e8c3c6386931f4be5fa43840a14e28163792f
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2011-10-29 16:42:15 +0200

    fixed datamodel to rotate independent of axis length + test added

--- src/main/scala/com/scalableminds/tools/Math.scala
+++ src/main/scala/com/scalableminds/tools/Math.scala
@@ -1,4 +1,5 @@
 package com.scalableminds.tools
+import scala.math._
 
 import util.DynamicVariable
 
@@ -12,4 +13,8 @@ import util.DynamicVariable
 object Math {
   def square(x:Int) = x * x
   def square(x:Double) = x * x
+  def normalizeVector(v:Tuple3[Int,Int, Int]):Tuple3[Double, Double, Double] = {
+    var l = sqrt(square(v._1) + square(v._2) + square(v._3))
+    if(l>0) (v._1 / l,  v._2 / l, v._3 / l) else (v._1.toDouble,v._2.toDouble,v._3.toDouble)
+  }
 }

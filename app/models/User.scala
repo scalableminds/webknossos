369c0e70ce99be2e397619dc00bb2c6871e6e4a2
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-01-15 19:28:30 +0100

    implemented pwd hashing for user

--- app/models/User.scala
+++ app/models/User.scala
@@ -5,27 +5,30 @@ import play.api.Play.current
 
 import com.mongodb.casbah.Imports._
 import com.novus.salat.global._
+import com.novus.salat.annotations._
 import com.novus.salat.dao.SalatDAO
+import brainflight.security.SCrypt._
 
 
-case class User(email: String, name: String, password: String, verified: Boolean, _id: ObjectId = new ObjectId)
+case class User(email: String, name: String, verified: Boolean = false, pwdHash: String = "", _id: ObjectId = new ObjectId)
 
 object User extends BasicDAO[User]("users"){
-
+  
   def findByEmail(email: String) = findOne(MongoDBObject(
     "email" -> email
   ))
 
   def findAll = find(MongoDBObject.empty).toList
 
-  def authenticate(email: String, password: String) = findOne(MongoDBObject(
-    "email" -> email,
-    "password" -> password
-  ))
+  def authenticate(email: String, password: String) = 
+    for{
+      user <- findOne(MongoDBObject("email" -> email))
+      if verifyPassword(password,user.pwdHash) 
+    } yield user
 
-  def create(user: User) = {
+  def create(email: String, name: String, password: String) = {
+    val user = User(email,name,false,hashPassword(password))
     insert(user)
     user
   }
-
 }

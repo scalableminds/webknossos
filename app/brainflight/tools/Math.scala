08e1ec04933efabdb651c4ae042e05b4cf519ef7
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-01-26 22:01:29 +0100

    changed round to floor in calculation of surrounding cube

--- app/brainflight/tools/Math.scala
+++ app/brainflight/tools/Math.scala
@@ -19,16 +19,16 @@ object Math {
     if ( l > 0 ) ( v._1 / l, v._2 / l, v._3 / l ) else v
   }
 
-  def surroundingCube( vertices: Seq[Vector3D] ): Seq[Tuple3[Long, Long, Long]] = {
+  def surroundingCube( vertices: Seq[Vector3D] ): Seq[Tuple3[Int, Int, Int]] = {
     val top = vertices.foldLeft( vertices( 0 ) )( ( b, e ) => (
       math.max( b.x, e.x ), math.max( b.y, e.y ), math.max( b.z, e.z ) ) )
     val bottom = vertices.foldLeft( vertices( 0 ) )( ( b, e ) => (
       math.min( b.x, e.x ), math.min( b.y, e.y ), math.min( b.z, e.z ) ) )
 
     for {
-      x <- bottom.x.round to top.x.round
-      y <- bottom.y.round to top.y.round
-      z <- bottom.z.round to top.z.round
+      x <- bottom.x.toInt to top.x.toInt
+      y <- bottom.y.toInt to top.y.toInt
+      z <- bottom.z.toInt to top.z.toInt
       if x >= 0 && y >= 0 && z >= 0
     } yield ( x, y, z )
   }

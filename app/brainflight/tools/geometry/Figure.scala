14f1fba2e9352b598346b5390c51e00d6a02149c
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-01-27 16:29:44 +0100

    fixed bugs

--- app/brainflight/tools/geometry/Figure.scala
+++ app/brainflight/tools/geometry/Figure.scala
@@ -1,13 +1,19 @@
 package brainflight.tools.geometry
 
 case class Figure( polygons: Seq[Polygon]){
-   def isInside( point: Vector3D): Boolean = {
-    for( polygon <- polygons){
+   def isInside( point: Vector3D, p: Polygon): Boolean = {
+    for{ polygon <- polygons
+    	if polygon != p
+    }{
       if( point ° polygon.normalVector - polygon.d > 1e-10){ 
         val dot = point ° polygon.normalVector
         return false
       }
     } 
     return true
+  }
+   
+  override def toString() = {
+    polygons.toString
   }
 }

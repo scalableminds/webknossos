02603715c3f53d77d1441b17eefc605566b7c67a
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-01-28 21:58:35 +0100

    fixed hash println

--- app/controllers/BinaryData.scala
+++ app/controllers/BinaryData.scala
@@ -75,7 +75,7 @@ object BinaryData extends Controller with Secured {
             // rotate the model and generate the requested data
             val result: Array[Byte] =
               coordinates.map( DataStore.load ).toArray
-            println("Calculated %d points. Hash %f".format(coordinates.size, hash))
+            println("Calculated %d points. Hash %d".format(coordinates.size, hash))
             output.push( binHandle ++ binHandle ++ hash.toBinary ++ result )
           case _ =>
             output.push( binHandle )

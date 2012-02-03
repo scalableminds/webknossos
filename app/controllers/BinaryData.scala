4857a580ff4d5712545f1a18dd8ae1d19d067993
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-02-03 16:46:04 +0100

    added backwars compitable ajax request method for non HTTP 1.1 clients

--- app/controllers/BinaryData.scala
+++ app/controllers/BinaryData.scala
@@ -19,6 +19,7 @@ import brainflight.tools.Math._
 import brainflight.tools.geometry.Vector3D._
 import brainflight.tools.geometry.Figure
 import play.api.libs.concurrent._
+import play.api.libs.json.JsValue
 
 /**
  * scalableminds - brainflight
@@ -29,6 +30,49 @@ import play.api.libs.concurrent._
 
 object BinaryData extends Controller with Secured {
 
+  def calculateBinaryData( model: DataModel, matrix: Array[Float], clientCoord: Array[Int] = Array() ) = {
+    if ( matrix.length != 16 ) {
+      println("Size: "+matrix.length)
+      Array[Byte]()
+    } else {
+      val figure = Figure( model.polygons.map( _.rotateAndMove( matrix ) ) )
+      val coordinates = pointsInFigure( figure )
+
+      Akka.future {
+        def f( x: Array[Int], y: Seq[Tuple3[Int, Int, Int]] ) {
+          if ( x.length / 3 != y.length )
+            System.err.println( "Size doesn't match! %d (S) != %d (C)".format( y.length, x.length / 3 ) )
+          val it = x.iterator
+          var failed = 0
+          y.zipWithIndex.foreach {
+            case ( e, i ) => {
+              if ( it.hasNext ) {
+                val client = ( it.next, it.next, it.next )
+                if ( e != client && failed < 20 ) {
+                  failed += 1
+                  System.err.println( "ELEMTNS don't match: %s (S) <-> %s (C)".format( e, client ) )
+                }
+              }
+            }
+          }
+        }
+        f( clientCoord, coordinates )
+      }
+      // rotate the model and generate the requested data
+      coordinates.map( DataStore.load ).toArray
+    }
+  }
+
+  def requestViaAjax( modelType: String ) = Action { implicit request =>
+    ( request.body.asRaw, ModelStore( modelType ) ) match {
+      case ( Some( binRequest ), Some( model ) ) =>
+        val matrix = binRequest.asBytes().getOrElse( Array[Byte]() ).subDivide( 4 ).map( _.reverse.toFloat )
+        val result = calculateBinaryData( model, matrix )
+        Ok( result )
+      case _ =>
+        BadRequest( "Request needs to be binary" )
+    }
+  }
   /**
    * Websocket implementation. Client needs to send a 4 byte handle and a 64
    * byte matrix. This matrix is used to apply a helmert transformation on the
@@ -39,61 +83,37 @@ object BinaryData extends Controller with Secured {
    * @param
    * 	modelType:	id of the model to use
    */
-  def dataWebsocket( modelType: String ) = WebSocket.using[Array[Byte]] { request =>
-    val output = new PushEnumerator[Array[Byte]]
-    val input = Iteratee.foreach[Array[Byte]]( in => {
-      println( "Message arrived! Bytes: %d".format( in.length ) )
-      val start = System.currentTimeMillis()
-      // first 4 bytes are always used as a client handle
-      if ( in.length >= 68 && in.length % 4 == 0 ) {
-        val ( binHandle, inRest ) = in.splitAt( 4 )
-        val ( binMatrix, binClientCoord ) = inRest.splitAt( 64 )
+  def requestViaWebsocket( modelType: String ) =
+    WebSocket.using[Array[Byte]] { request =>
+      val output = new PushEnumerator[Array[Byte]]
+      val input = Iteratee.foreach[Array[Byte]]( in => {
+        //println( "Message arrived! Bytes: %d".format( in.length ) )
+        //val start = System.currentTimeMillis()
+        // first 4 bytes are always used as a client handle
+        if ( in.length >= 68 && in.length % 4 == 0 ) {
+          val ( binHandle, inRest ) = in.splitAt( 4 )
+          val ( binMatrix, binClientCoord ) = inRest.splitAt( 64 )
 
-        // convert the matrix from byte to float representation
-        val matrix = binMatrix.subDivide( 4 ).map( _.reverse.toFloat )
-        val pauseStart = System.currentTimeMillis()
-        val clientCoord = binClientCoord.subDivide( 4 ).map( _.reverse.toFloat ).map( _.toInt )
-        val pauseEnd = System.currentTimeMillis()
+          // convert the matrix from byte to float representation
+          val matrix = binMatrix.subDivide( 4 ).map( _.reverse.toFloat )
+          //val pauseStart = System.currentTimeMillis()
+          val clientCoord = binClientCoord.subDivide( 4 ).map( _.reverse.toFloat ).map( _.toInt )
+          //val pauseEnd = System.currentTimeMillis()
 
-        ModelStore( modelType ) match {
-          case Some( model ) =>
-            val figure = Figure( model.polygons.map( _.rotateAndMove( matrix ) ) )
-            val coordinates = pointsInFigure( figure )
-
-            Akka.future {
-              def f( x: Array[Int], y: Seq[Tuple3[Int, Int, Int]] ) {
-                if ( x.length / 3 != y.length ) System.err.println( "Size doesn't match! %d (S) != %d (C)".format( y.length, x.length / 3 ) )
-                val it = x.iterator
-                var failed = 0
-                y.zipWithIndex.foreach {
-                  case ( e, i ) => {
-                    if ( it.hasNext ) {
-                      val client = ( it.next, it.next, it.next )
-                      if ( e != client && failed < 20 ) {
-                        failed += 1
-                        System.err.println( "ELEMTNS don't match: %s (S) <-> %s (C)".format( e, client ) )
-                      }
-                    }
-                  }
-                }
-                println( "Compare completed! Handle: " + binHandle.toFloat )
-              }
-              f( clientCoord, coordinates )
-            }
-            // rotate the model and generate the requested data
-            val result: Array[Byte] =
-              coordinates.map( DataStore.load ).toArray
-            val end = System.currentTimeMillis()
-            println( "Calculated %d points in %d ms. Handle: %s".format( coordinates.size, end + pauseEnd - pauseStart - start, binHandle.toFloat ) )
-            output.push( binHandle ++ result )
-          case _ =>
-            output.push( binHandle )
+          ModelStore( modelType ) match {
+            case Some( model ) =>
+              val result = calculateBinaryData( model, matrix, clientCoord )
+              val end = System.currentTimeMillis()
+              //println( "Calculated %d points in %d ms. Handle: %s".format( coordinates.size, end + pauseEnd - pauseStart - start, binHandle.toFloat ) )
+              output.push( binHandle ++ result )
+            case _ =>
+              output.push( binHandle )
+          }
         }
-      }
-    } )
+      } )
 
-    ( input, output )
-  }
+      ( input, output )
+    }
 
   def model( modelType: String ) = Action {
     ModelStore( modelType ) match {

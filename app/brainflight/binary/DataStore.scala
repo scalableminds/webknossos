685b5fbca069a0e64417fabdda4f22cdabbbbee8
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-02-08 23:23:46 +0100

    refactoring

--- app/brainflight/binary/DataStore.scala
+++ app/brainflight/binary/DataStore.scala
@@ -19,15 +19,17 @@ import java.io.{ FileNotFoundException, InputStream, FileInputStream, File }
 object DataStore {
   lazy val nullBlock = (for (x <- 0 to 128 * 128 * 128) yield 0.toByte).toArray
   // defines the maximum count of cached file handles
-  val fileBufferLimit = 500
+  val maxCacheSize = 500
   // binary data ID
-  val binaryDataID = Play.configuration.getString("binarydata.id") getOrElse("100527_k0563_mag1")
+  val binaryDataID = 
+    Play.configuration.getString("binarydata.id") getOrElse("100527_k0563_mag1")
   // binary data Path
-  val dataPath = Play.configuration.getString("binarydata.path") getOrElse ("binaryData/")
+  val dataPath = 
+    Play.configuration.getString("binarydata.path") getOrElse ("binaryData/")
   // defines how many file handles are deleted when the limit is reached
   val dropCount = 50
   // try to prevent loading a file multiple times into memory
-  val fileBuffer = new HashMap[Tuple3[Int, Int, Int], Array[Byte]]
+  val fileCache = new HashMap[Tuple3[Int, Int, Int], Array[Byte]]
 
   /**
    * Load the binary data of the given coordinate from file
@@ -40,20 +42,20 @@ object DataStore {
     val y = point._2 / 128
     val z = point._3 / 128
 
-    val byteArray: Array[Byte] = fileBuffer.get((x, y, z)) match {
+    val byteArray: Array[Byte] = fileCache.get((x, y, z)) match {
       case Some(x) =>
         x
       case _ =>
         // pretends to flood memory with to many files
-        if (fileBuffer.size > fileBufferLimit)
-          fileBuffer.drop(dropCount)
+        if (fileCache.size > maxCacheSize)
+          fileCache.drop(dropCount)
         try {
-          val br = new FileInputStream(
+          val binaryStream = new FileInputStream(
             "%sx%04d/y%04d/z%04d/%s_x%04d_y%04d_z%04d.raw".
               format(dataPath, x, y, z, binaryDataID, x, y, z))
-          val b = inputStreamToByteArray(br)
-          fileBuffer += (((x, y, z), b))
-          b
+          val binData = inputStreamToByteArray(binaryStream)
+          fileCache += (((x, y, z), binData))
+          binData
         } catch {
           case e: FileNotFoundException =>
             System.err.println(
@@ -61,7 +63,7 @@ object DataStore {
                 format(dataPath, x, y, z, binaryDataID, x, y, z))
             // if the file block isn't found, a nullBlock is associated with 
             // the coordinates
-            fileBuffer += (((x, y, z), nullBlock))
+            fileCache += (((x, y, z), nullBlock))
             nullBlock
         }
     }
@@ -72,13 +74,13 @@ object DataStore {
    *  Read file contents to a byteArray
    */
   def inputStreamToByteArray(is: InputStream) = {
-    val b = new Array[Byte](2097152)
-    is.read(b, 0, 2097152)
-    b
+    val byteArray = new Array[Byte](2097152)
+    is.read(byteArray, 0, 2097152)
+    byteArray
   }
   
   def cleanUp(){
-    fileBuffer.clear()
+    fileCache.clear()
   }
   
 }

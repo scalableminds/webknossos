9cc6e87a1752d7e89c9cfa9a5b442a35f5da1141
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-02-08 16:08:15 +0100

    refactoring

--- app/brainflight/tools/geometry/NGonalFrustum.scala
+++ app/brainflight/tools/geometry/NGonalFrustum.scala
@@ -14,24 +14,29 @@ package brainflight.tools.geometry
  * @param rBase: of the site-to-center line segment
  *
  */
-class NGonalFrustum(n: Int, h:Int, rBase: Int, rTop: Int) {
+class NGonalFrustum( numberOfVertices: Int, height: Int, radiusBase: Int, radiusTop: Int ) {
   val polygons = {
     // create the polygon which represents the base of the frustrum
-    val baseP = new RegularPolygon(n,rBase)
+    val basePolygon = new RegularPolygon( numberOfVertices, radiusBase )
     // top of the frustum
-    val topP = new RegularPolygon(n,rTop)
+    val topPolygon = new RegularPolygon( numberOfVertices, radiusTop )
     // list of all polygons the frustum contains, top polygon will be the last in the list
     // extend all vectors with a 3rd dimension
-    var polys = new Polygon(topP.to3D(h,2).reverse) :: Nil  // top gets reveresed to make it counter clockwise
-    for(i<-0 until n){
+    var polygons = new Polygon( topPolygon.to3D( height, 2 ).reverse ) :: Nil // top gets reveresed to make it counter clockwise
+    for ( i <- 0 until numberOfVertices ) {
       // create the cladding faces of the frustum
       // because the vertex vectors of the base and top polygon are in 2D they need to be extended to a 3D vector
-      val vertices = baseP.vertex(i).to3D(0,2) :: baseP.vertex(i-1).to3D(0,2) :: topP.vertex(i-1).to3D(h,2) :: topP.vertex(i).to3D(h,2) :: Nil
-      polys ::= new Polygon(vertices)
+      val vertices =
+        basePolygon.vertex( i ).to3D( 0, 2 ) ::
+          basePolygon.vertex( i - 1 ).to3D( 0, 2 ) ::
+          topPolygon.vertex( i - 1 ).to3D( height, 2 ) ::
+          topPolygon.vertex( i ).to3D( height, 2 ) ::
+          Nil
+      polygons ::= new Polygon( vertices )
     }
     // return the list of polygons, but first prepend the base polygon
-    new Polygon(baseP.to3D(0,2)) :: polys
+    new Polygon( basePolygon.to3D( 0, 2 ) ) :: polygons
   }
-  // used to convert polygon to json representation
-  override def toString = polygons.mkString("[",",","]")
+
+  override def toString = polygons.mkString( "[", ",", "]" )
 }

8841d4160bab17389a1dd1f391fb5b2ba9687841
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-02-10 20:28:25 +0100

    initial facebook login

--- app/models/User.scala
+++ app/models/User.scala
@@ -9,26 +9,41 @@ import com.novus.salat.annotations._
 import com.novus.salat.dao.SalatDAO
 import brainflight.security.SCrypt._
 
+@Salat
+abstract class LoginType(@Persist val identifier: String)
 
-case class User(email: String, name: String, verified: Boolean = false, pwdHash: String = "", _id: ObjectId = new ObjectId)
+case class LocalLogin() extends LoginType("local")
 
-object User extends BasicDAO[User]("users"){
-  
-  def findByEmail(email: String) = findOne(MongoDBObject(
-    "email" -> email
-  ))
+case class User( 
+    email: String, 
+    name: String, 
+    verified: Boolean = false, 
+    pwdHash: String = "", 
+    loginType: LoginType = LocalLogin(), 
+    _id: ObjectId = new ObjectId )
 
-  def findAll = find(MongoDBObject.empty).toList
+object User extends BasicDAO[User]( "users" ) {
 
-  def authenticate(email: String, password: String) = 
-    for{
-      user <- findOne(MongoDBObject("email" -> email))
-      if verifyPassword(password,user.pwdHash) 
+  def findByEmail( email: String ) = findOne( MongoDBObject(
+    "email" -> email ) )
+
+  def findAll = find( MongoDBObject.empty ).toList
+
+  def authenticate( email: String, password: String ) =
+    for {
+      user <- findOne( MongoDBObject( "email" -> email , "loginType" -> LocalLogin) )
+      if verifyPassword( password, user.pwdHash )
     } yield user
 
-  def create(email: String, name: String, password: String) = {
-    val user = User(email,name,false,hashPassword(password))
-    insert(user)
+  def create( email: String, name: String, password: String = "" ) = {
+    val user = User( email, name, false, hashPassword( password ) )
+    insert( user )
+    user
+  }
+
+  def createRemote( email: String, name: String, loginType: LoginType ) = {
+    val user = User( email, name, true, "", loginType )
+    insert( user )
     user
   }
 }

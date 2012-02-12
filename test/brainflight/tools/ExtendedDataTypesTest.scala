d26cbff5ef252eb413f6b9cc89ff7e2e9273d69c
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-02-12 13:53:30 +0100

    fixed and implemented tests

---
+++ test/brainflight/tools/ExtendedDataTypesTest.scala
@@ -0,0 +1,79 @@
+package brainflight.tools
+
+import brainflight.tools.ExtendedDataTypes._
+import org.specs2.mutable.Specification
+
+class ExtendedDataTypesTest extends Specification{
+	"Extended byte array" should {
+	  "be able to convert to float" in {
+	    val b = Array[Byte](1,2,3,4)
+	    b.toFloat must be equalTo 2.3879393E-38F
+	  }
+	  "subdivide into parts of length 1" in {
+	    val b = Array[Byte](1,2,3,4,5)
+	    val devided = b.subDivide(1)
+	    
+	    devided.size must be equalTo 5
+	    devided.head.head must be equalTo 1
+	    devided.last.head must be equalTo 5	    
+	  }
+	  "subdivide into parts even if array is to short" in {
+	    val b = Array[Byte](1,2,3,4,5)
+	    val devided = b.subDivide(4)
+	    
+	    devided.size must be equalTo 2
+	    devided.head.size must be equalTo 4
+	    devided.last.size must be equalTo 1   
+	  }
+	  "don't subdivide if array is smaller than one part" in {
+	    val b = Array[Byte](1,2,3,4,5)
+	    val devided = b.subDivide(10)
+	    
+	    devided.size must be equalTo 1
+	    devided.head.size must be equalTo 5
+	  }
+	}
+	
+	"Extended int" should {
+	  "be able to convert to binary" in {
+	    
+	    42.toBinary must be equalTo Array[Byte](0,0,0,42)
+	    
+	    13371337.toBinary must be equalTo Array[Byte](0,-52,7,-55)
+	    
+	    -123456.toBinary must be equalTo Array[Byte](-1,-2,29,-64)
+	  }
+	}
+	
+	"Extended float" should {
+	  "be able to convert to binary" in {
+	    
+	    42.0F.toBinary must be equalTo Array[Byte](66, 40, 0, 0)
+	    
+	    1337.1337F.toBinary must be equalTo Array[Byte](68, -89, 36, 71)
+	    
+	    -123.456F.toBinary must be equalTo Array[Byte](-62, -10, -23, 121)
+	  }
+	}
+	
+	"Extended double" should {
+	  "be able to convert to binary" in {
+	    
+	    42.0.toBinary must be equalTo Array[Byte](64, 69, 0, 0, 0, 0, 0, 0)
+	    
+	    1337.1337.toBinary must be equalTo Array[Byte](64, -108, -28, -120, -24, -89, 29, -25)
+	    
+	    -123.456.toBinary must be equalTo Array[Byte](-64, 94, -35, 47, 26, -97, -66, 119)
+	  }
+	  "decide if a value is near zero" in {
+	    
+	    42.0.isNearZero must be equalTo false
+	    
+	    -42.0.isNearZero must be equalTo false
+	    
+	    0.0000000000000001.isNearZero should be equalTo true
+	    
+	    -0.0000000000000001.isNearZero should be equalTo true
+	  }
+	}
+}
--- test/integration/BinaryTest.scala
+++ test/integration/BinaryTest.scala
@@ -5,14 +5,18 @@ import play.api.test._
 import play.api.test.Helpers._
 import brainflight.binary.CubeModel
 import play.api.libs.json._
+import play.api.libs.json.Json._
 import scala.util.Random
 import play.api.mvc._
+import play.api.mvc.AnyContent
+import brainflight.tools.ExtendedDataTypes._
+import controllers.BinaryData
 
 object BinaryTest extends Specification {
   sequential
 
   "Binary REST interface" should {
-    "return model" in {
+    "return a models vertices" in {
       running( FakeApplication() ) {
         /**
          * URL:
@@ -20,18 +24,17 @@ object BinaryTest extends Specification {
          * Params:
          *  	- modeltype: String , A valid data source model (e.q. cube)
          * Response-type:
-         * 	application/octet-stream
+         * 	application/json
          * Response:
-         * 	Coordinates of all points inside the model in 8 byte format.
-         * 	3 Dimensional in the following order: x,y,z
+         * 	Coordinates of all vertices of the model
          */
-        val Some( result ) = routeAndCall( FakeRequest( "GET", "/binary/model/cube" ) )
+        val Some( result ) = routeAndCall( FakeRequest( GET, "/binary/model/cube" ) )
         status( result ) must be equalTo ( OK )
-        contentType( result ) must equalTo( Some( "application/octet-stream" ) )
-        contentAsBytes( result ).foldLeft( 0 )( ( b, x ) => b + x ) must be equalTo CubeModel.modelInformation.foldLeft( 0 )( ( b, x ) => b + x )
+        contentType( result ) must equalTo( Some( "application/json" ) )
+        contentAsString( result ) must be equalTo Json.stringify( toJson( CubeModel.vertices ) )
       }
     }
-    "return polygons" in {
+    "return a models polygons" in {
       /**
        * URL:
        * 	GET - /route/polygons/:modeltype
@@ -56,9 +59,10 @@ object BinaryTest extends Specification {
       val Some( result ) = routeAndCall( FakeRequest( GET, "/binary/polygons/cube" ) )
       status( result ) must be equalTo ( OK )
       contentType( result ) must equalTo( Some( "application/json" ) )
-      contentAsString(result) must be equalTo Json.stringify(toJson( CubeModel.polygons ))
+      contentAsString( result ) must be equalTo Json.stringify( toJson( CubeModel.polygons ) )
     }
-    "return data" in {
+
+    "return data through GET" in {
       running( FakeApplication() ) {
         /**
          * URL:
@@ -78,19 +82,33 @@ object BinaryTest extends Specification {
          * 	to the given origin. Afterwards the colors of the points inside the
          * 	produced figure are returned as binary data.
          */
-        val Some( result ) = routeAndCall( FakeRequest( GET, "/binary/data/cube?px=0&py=0&pz=0&ax=0&ay=1&az=0" ) )
+        val matrix = Array[Float]( 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 685, 611, 648, 1 )
+        val Some( result ) = routeAndCall( FakeRequest(
+          POST,
+          "/binary/data/cube",
+          FakeHeaders( Map( "Content" -> List( "application/octet-stream" ) ) ),
+          AnyContentAsRaw( raw = RawBuffer( memoryThreshold = 1024, matrix.flatMap( _.toBinary.reverse ) ) ) ) )
+
         status( result ) must be equalTo ( OK )
         contentType( result ) must equalTo( Some( "application/octet-stream" ) )
-        contentAsBytes( result ).foldLeft( 0 )( ( b, x ) => b + x ) must be equalTo 0
+        contentAsBytes( result ).size must be equalTo 302379
       }
     }
+
+    "return data through WebSocket" in {
+      running( FakeApplication() ) {
+        ko
+      }
+    }.pendingUntilFixed( "Testing websockets isn't implemented in play till now" )
+
     "return null block for negative parameters" in {
       running( FakeApplication() ) {
-        val Some( result ) = routeAndCall( FakeRequest( GET, "/binary/data/cube?px=173&py=-26&pz=198&ax=-0.9&ay=0.2&az=-0.3" ) )
-        status( result ) must be equalTo ( OK )
-        contentType( result ) must equalTo( Some( "application/octet-stream" ) )
-        contentAsBytes( result ).foldLeft( 0 )( ( b, x ) => b + x ) must be equalTo 0
+        ko
+        //        val Some( result ) = routeAndCall( FakeRequest( GET, "/binary/data/cube?px=173&py=-26&pz=198&ax=-0.9&ay=0.2&az=-0.3" ) )
+        //        status( result ) must be equalTo ( OK )
+        //        contentType( result ) must equalTo( Some( "application/octet-stream" ) )
+        //        contentAsBytes( result ).foldLeft( 0 )( ( b, x ) => b + x ) must be equalTo 0
       }
-    }
+    }.pendingUntilFixed
   }
 }

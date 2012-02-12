322d560a5c75810ffefb006fac04113c7083544e
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-02-12 14:35:15 +0100

    implemented datastore interface, previous implementation got moved to filedatastore

--- test/brainflight/FileDataStoreTest.scala
+++ test/brainflight/FileDataStoreTest.scala
@@ -5,15 +5,16 @@ import play.api.test._
 import play.api.test.Helpers._
 import brainflight.binary.DataStore
 import java.io.FileNotFoundException
+import brainflight.binary.FileDataStore
 
-class DataStoreTest extends Specification {
+class FileDataStoreTest extends Specification {
   sequential
   "DataStore" should {
     "load Data" in {
       running(FakeApplication()) {
         try {
           // if this failes the data has changed
-          DataStore.load((0, 0, 0)) must be equalTo (0.toByte)
+          FileDataStore.load((0, 0, 0)) must be equalTo (0.toByte)
         } catch {
           case e: FileNotFoundException =>
             ko("Data not found: Put binary data in e.q. binarydata/x0000/y0000/z0000/100527_k0563_mag1_x0000_y0000_z0000.raw")
@@ -22,13 +23,13 @@ class DataStoreTest extends Specification {
     }
     "not return random data" in {
       running(FakeApplication()) {
-        DataStore.load((15, 53, 25)) must be equalTo DataStore.load((15, 53, 24)) 
+        FileDataStore.load((15, 53, 25)) must be equalTo FileDataStore.load((15, 53, 24)) 
       }
     }
     "return black for not existing Data" in {
       running(FakeApplication()) {
-        DataStore.load((-34, 53, 25)) must be equalTo (0.toByte)
-        DataStore.load((22222222, 33333333, 4444444)) must be equalTo (0.toByte)
+        FileDataStore.load((-34, 53, 25)) must be equalTo (0.toByte)
+        FileDataStore.load((22222222, 33333333, 4444444)) must be equalTo (0.toByte)
       }
     }
   }

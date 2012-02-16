f9a9688838e4cf43c8276bb769e46e4d02e84f8b
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-02-16 21:53:43 +0100

    added security roles / permissons and branching

---
+++ app/brainflight/security/Secured.scala
@@ -0,0 +1,99 @@
+package brainflight.security
+
+import models.User
+import play.api.mvc._
+import play.api.mvc.Results._
+import play.api.mvc.Request
+import play.api.Play
+import play.api.Play.current
+import play.api.libs.iteratee.Input
+import controllers.routes
+import play.api.libs.iteratee.Done
+import models.{ Role, Permission }
+
+/**
+ * Provide security features
+ */
+
+trait Secured {
+
+  val DefaultAccessRole: Option[Role] = None
+  val DefaultAccessPermission: Option[Permission] = None
+
+  def user( implicit request: RequestHeader ): Option[models.User] = {
+    for {
+      email <- username( request )
+      user <- User.findByEmail( email )
+    } yield user
+  }
+
+  def userId( implicit request: RequestHeader ) = {
+    user match {
+      case Some( u ) => u._id
+      case _         => throw new Exception( "not logged in" )
+    }
+  }
+
+  /**
+   * Retrieve the connected user email.
+   */
+  private def username( request: RequestHeader ) = {
+    if ( Play.configuration.getBoolean( "application.enableAutoLogin" ).get ) {
+      Some( "scmboy@scalableminds.com" )
+    } else
+      request.session.get( "email" )
+  }
+
+  def Authenticated(
+    role: Option[Role] = DefaultAccessRole,
+    permission: Option[Permission] = DefaultAccessPermission )( f: => User => Request[AnyContent] => Result ): Action[( Action[AnyContent], AnyContent )] = Authenticated( username, role, permission, onUnauthorized ) {
+    user =>
+      Action( request => f( user )( request ) )
+  }
+
+  def Authenticated[A](
+    username: RequestHeader => Option[String],
+    role: Option[Role],
+    permission: Option[Permission],
+    onUnauthorized: RequestHeader => Result )( action: User => Action[A] ): Action[( Action[A], A )] = {
+
+    val authenticatedBodyParser = BodyParser { request =>
+      user( request ).map { user =>
+        if ( ( role.isEmpty || user.hasRole( role.get ) ) &&
+          ( permission.isEmpty || user.hasPermission( permission.get ) ) ) {
+
+          val innerAction = action( user )
+          innerAction.parser( request ).mapDone { body =>
+            body.right.map( innerBody => ( innerAction, innerBody ) )
+          }
+        } else {
+          val input: Input[Array[Byte]] = Input.Empty
+          Done( Left( Forbidden ), input )
+        }
+      }.getOrElse {
+        Done( Left( onUnauthorized( request ) ), Input.Empty )
+      }
+    }
+
+    Action( authenticatedBodyParser ) { request =>
+      val ( innerAction, innerBody ) = request.body
+      innerAction( request.map( _ => innerBody ) )
+    }
+  }
+
+  /**
+   * Redirect to login if the user in not authorized.
+   */
+  private def onUnauthorized( request: RequestHeader ) = Results.Redirect( routes.Application.login )
+
+  // --
+
+  /**
+   * Action for authenticated users.
+   */
+  def IsAuthenticated( f: => String => Request[AnyContent] => Result ) = Security.Authenticated( username, onUnauthorized ) {
+    user =>
+      Action( request => f( user )( request ) )
+  }
+
+}
---
+++ app/controllers/Branch.scala
@@ -0,0 +1,50 @@
+package controllers
+
+import brainflight.security.Secured
+import play.api.mvc.Controller
+import models.User
+import brainflight.tools.geometry.TransformationMatrix
+import models.Role
+import models.Permission
+import brainflight.tools.ExtendedDataTypes._
+import play.api.mvc.Action
+import play.api.mvc.Request
+import play.api.libs.json.Json._
+import play.api.libs.json._
+
+object Branch extends Controller with Secured {
+  override val DefaultAccessRole = Role( "user" )
+
+  def push = Authenticated() { user =>
+    implicit request =>
+      request.body.asRaw match {
+        case Some( binRequest ) =>
+          val binMatrix = binRequest.asBytes().getOrElse( Array[Byte]() )
+          val branchMatrix = binMatrix.subDivide( 4 ).map( _.reverse.toFloat )
+          User.save( user.copy( branchPoints = TransformationMatrix(branchMatrix.toList) :: user.branchPoints ) )
+          Ok
+        case _ =>
+          BadRequest( "Request needs to be binary" )
+      }
+  }
+
+  def pop = Authenticated() { user =>
+    implicit request =>
+      if ( !user.branchPoints.isEmpty ){
+        User.save( user.copy( branchPoints = user.branchPoints.tail ) )
+      }
+      Ok
+  }
+
+  def list = Authenticated() { user =>
+    implicit request =>
+      val points: List[List[Float]] = user.branchPoints.map(_.value)
+      Ok( toJson( points ) )
+  }
+
+  def clear = Authenticated() { user =>
+    implicit request =>
+      User.save( user.copy( branchPoints = Nil ) )
+      Ok
+  }
+}

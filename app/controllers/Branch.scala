db621788a37059f561db1991a9bfec452319270c
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-02-28 00:53:38 +0100

    removed push and pop for branches

--- app/controllers/Branch.scala
+++ app/controllers/Branch.scala
@@ -16,34 +16,6 @@ import models.TrackedRoute
 object Branch extends Controller with Secured {
   override val DefaultAccessRole = Role( "user" )
 
-  def push = Authenticated() { user =>
-    implicit request =>
-      request.body.asRaw match {
-        case Some( binRequest ) =>
-          val binMatrix = binRequest.asBytes().getOrElse( Array[Byte]() )
-          val branchMatrix = binMatrix.subDivide( 4 ).map( _.reverse.toFloat )
-          User.save( user.copy( branchPoints = TransformationMatrix(branchMatrix.toList) :: user.branchPoints ) )
-          TrackedRoute.findOpenBy( user ) foreach { flightRoute =>
-            flightRoute.addBranch()
-          }
-          Ok
-        case _ =>
-          BadRequest( "Request needs to be binary" )
-      }
-  }
-
-  def pop = Authenticated() { user =>
-    implicit request =>
-      if ( !user.branchPoints.isEmpty ){
-        val branchPoint = user.branchPoints.head
-        User.save( user.copy( branchPoints = user.branchPoints.tail ) )
-        TrackedRoute.findOpenBy( user ) foreach { flightRoute =>
-            flightRoute.closeBranch( branchPoint.extractTranslation.get.toVector3I )
-        }
-      }
-      Ok
-  }
-
   def list = Authenticated() { user =>
     implicit request =>
       val points: List[List[Float]] = user.branchPoints.map(_.value)

929c8a44673e142c226e6851e03b22fec2bff855
Thomas Werkmeister <Thomas.Werkmeister@scalableminds.com>
2012-03-10 23:10:30 +0100

    implemented gridfs working like datastore, deleted a lot of old testing stuff

---
+++ app/brainflight/binary/GridFileDataStore.scala
@@ -0,0 +1,100 @@
+package brainflight.binary
+
+import collection.mutable.HashMap
+import play.api.Play.current
+import play.api.Play
+import play.Logger
+import java.io.{ FileNotFoundException, InputStream, FileInputStream, File }
+import com.mongodb.casbah.Imports._
+import scala.collection.JavaConverters._
+import com.mongodb.casbah.gridfs.Imports._
+
+object GridFileDataStore extends DataStore{
+	  //GridFs handle
+  val BinDatabase = GridFS(MongoConnection()(Play.configuration.getString("mongo.dbname").getOrElse("salat-dao")))
+  
+  lazy val nullBlock = (for (x <- 0 to 128 * 128 * 128) yield 0.toByte).toArray
+  // defines the maximum count of cached file handles
+  val maxCacheSize = 500
+  // binary data ID
+  val binaryDataID = 
+    Play.configuration.getString("binarydata.id") getOrElse("100527_k0563_mag1")
+  // binary data Path
+  val dataPath = 
+    Play.configuration.getString("binarydata.path") getOrElse ("binaryData/")
+  // defines how many file handles are deleted when the limit is reached
+  val dropCount = 50
+  // try to prevent loading a file multiple times into memory
+  val fileCache = new HashMap[Tuple3[Int, Int, Int], Array[Byte]]
+
+  /**
+   * Load the binary data of the given coordinate from DB
+   */
+  def load(point: Tuple3[Int, Int, Int]): Byte = {
+    // TODO: Insert upper bound
+    if (point._1 < 0 || point._2 < 0 || point._3 < 0) return 0
+
+    val x = point._1 / 128
+    val y = point._2 / 128
+    val z = point._3 / 256
+
+    val byteArray: Array[Byte] = fileCache.get((x, y, z)) match {
+      case Some(x) =>
+        x
+      case _ =>
+        // pretends to flood memory with to many files
+        if (fileCache.size > maxCacheSize)
+          fileCache.drop(dropCount)
+          
+        BinDatabase.findOne(convertCoordinatesToString(x,y,z)) match {
+          case Some(data) => 
+            val binData = inputStreamToByteArray(data.inputStream)
+            fileCache += (((x,y,z), binData))
+            binData
+          case None => 
+            fileCache += (((x,y,z),nullBlock))
+            nullBlock
+        }
+    }
+    val zB = (point._3 % 256) / 2
+    byteArray((zB * 128 * 128 + (point._2 % 128) * 128 + point._1 % 128))
+  }
+
+  /**
+   *  Read file contents to a byteArray
+   */
+  def inputStreamToByteArray(is: InputStream) = {
+    val byteArray = new Array[Byte](2097152)
+    is.read(byteArray, 0, 2097152)
+    //assert(is.skip(1) == 0, "INPUT STREAM NOT EMPTY")
+    byteArray
+  }
+  
+  def cleanUp(){
+    fileCache.clear()
+  }
+  
+  def createFilename(x: Int, y: Int, z: Int): String = {
+	"%s/x%04d/y%04d/z%04d/%s_x%04d_y%04d_z%04d.raw".format(dataPath,x,y,z,binaryDataID,x,y,z) 
+  }
+  
+  def convertCoordinatesToString(x: Int, y: Int, z: Int):String = {
+    "%04d%04d%04d".format(x,y,z)
+  }
+  
+  private def create(x: Int, y: Int, z: Int):Array[Byte] ={
+    try{
+      val IS = new FileInputStream(createFilename(x,y,z))
+      BinDatabase(IS) { fh=>
+      fh.filename = convertCoordinatesToString(x,y,z)
+      fh.contentType = "application"
+      }
+      FileDataStore.inputStreamToByteArray(IS)
+    }
+    catch {
+      case e: FileNotFoundException =>
+        Logger.warn("%s not found!".format(createFilename(x,y,z)));
+        nullBlock
+    }
+  }
+}
--- app/controllers/Admin.scala
+++ app/controllers/Admin.scala
@@ -4,117 +4,29 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.Play.current
-import models._
-import views._
 import play.mvc.Results.Redirect
+import play.Logger
 import java.text.SimpleDateFormat
 import java.util.TimeZone
-import scala.collection.mutable.HashMap
-import brainflight.binary.DataStore
-import brainflight.binary.FileDataStore
+import brainflight.binary.{FileDataStore,GridFileDataStore}
 
 object Admin extends Controller{
-  val fileDataStore = FileDataStore
   
-  
-  def loadData(x: Int, y: Int, z: Int) = Action { implicit request => 
-    BinData.createOrGet(x,y,z)
-    Ok("saved x:%d y:%d z:%d".format(x,y,z))
-  }
-  
-  def fullTest() = Action{
-    Ok("BinData: %s\n GridFS: %s\n DataStore: %s\n".format(timeBinDoc(),
-        timeGridFS(),timeDataStore()))
-  }
-  
-  def createTestBinData()= Action{
-    val x = 5
-    BinData.collection.dropCollection()
-    for{y <- 0 until 25
-    	z <- 0 until 20}{
-    	  BinData.createOrGet(x,y,z)
+  def testGridFS = Action{
+    //Iterate over Block 5/10/10
+    var differences = 0
+    for{x <- 5*128 until 6*128
+    	y <- 10*128 until 11*128
+    	z <- 10*256 until 11*256 by 2}{
+    	  val FileStoreByte = FileDataStore.load((x,y,z))
+    	  val GridFileStoreByte = GridFileDataStore.load((x,y,z))
+    	  
+    	  if( FileStoreByte != GridFileStoreByte)
+    	    differences += 1
     }
-    Ok("BinData Database created")
-  }
-  
-  def createGridFsData() = Action{
-    val x = 5
-    for{y <- 0 until 25
-    	z <- 0 until 20}{
-    	  Logger.info("processing %d %d %d".format(x,y,z))
-    	  BinDataGridFs.createOrGet(x,y,z)
-    	}
-    Ok("Gridfs Database created")
-  }
-  
-  def timeBinDoc() = {
-    val x = 5
-    var time = -System.currentTimeMillis()
-    val fileBuffer = new HashMap[Tuple3[Int, Int, Int], Array[Byte]]
-
-    for{y <- 0 until 25
-        z <- 0 until 20}{
-          BinData.findByCoordinates(x,y,z) match {
-            case Some(data) => fileBuffer += (((x,y,z),data.buffer))
-            case None => println("this will not happen")
-          }
-        }
-    time += System.currentTimeMillis()
-    TimeZone.setDefault(TimeZone.getTimeZone("GMT"))
-    val sdf = new SimpleDateFormat("HH:mm:ss:SSS")
-    fileBuffer.clear()
-    sdf.format(time)
-  }
-  
-  def testBinDoc() = Action {
-	Ok(timeBinDoc())
-  }
-  
-  def timeGridFS()={
-    val x = 5
-    var fileBuffer = new HashMap[Tuple3[Int, Int, Int], Array[Byte]]
-	
-    var time = -System.currentTimeMillis()
-    for{y <- 0 until 25
-    	z <- 0 until 20}{
-    	  BinDataGridFs.findByCoordinates(x,y,z) match {
-    	    case Some(data) => fileBuffer += (((x,y,z),fileDataStore.inputStreamToByteArray(data.inputStream)))
-            case None => println("this will not happen")
-    	  }
-    	}
-    time += System.currentTimeMillis()
-    TimeZone.setDefault(TimeZone.getTimeZone("GMT"))
-    val sdf = new SimpleDateFormat("HH:mm:ss:SSS")
-    fileBuffer.clear()
-    sdf.format(time)
-  }
-  
-  def testGridFS() = Action {
-    Ok(timeGridFS()) 
-  }
-  
-  def cleanup() = Action {
-    System.runFinalization()
-    System.gc()
-    Ok("cleaned up!")
-  }
-  
-  def timeDataStore() = {
-    val x = 5
-    var time = -System.currentTimeMillis()
-    for{y <- 0 until 25
-    	z <- 0 until 20}{
-    	  fileDataStore.load(Tuple3(x*128,y*128,z*128))
-    	}
-    time += System.currentTimeMillis()
-    TimeZone.setDefault(TimeZone.getTimeZone("GMT"))
-    val sdf = new SimpleDateFormat("HH:mm:ss:SSS")
-    fileDataStore.cleanUp()
-    sdf.format(time)
-  }
-  
-  def testDataStore() = Action {
-	Ok(timeDataStore())
+    Logger.info("%d items in FileDataStore.".format(FileDataStore.fileCache.size))
+    Logger.info("%d items in GridFileDataStore.".format(GridFileDataStore.fileCache.size))
+    Ok("done with %d differences".format(differences))
   }
 }
 

7dc6b8e749caa9cafab5ac20dad925c45a061a05
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-04-02 17:38:44 +0200

    BF-18 refactored & commented

--- app/brainflight/tools/extendedTypes/ExtendedArray.scala
+++ app/brainflight/tools/extendedTypes/ExtendedArray.scala
@@ -2,6 +2,10 @@ package brainflight.tools.extendedTypes
 
 class ExtendedArray[A]( array: Array[A] ) {
 
+  /**
+   * A dynamic sliding window is a sliding window which is moved n steps
+   * according to the return value of the passed function.
+   */
   def dynamicSliding( windowSize: Int )( f: List[A] => Int ) {
     val iterator = array.sliding( windowSize, 1 )
     while ( iterator.hasNext ) {
--- app/brainflight/tools/extendedTypes/ExtendedFloat.scala
+++ app/brainflight/tools/extendedTypes/ExtendedFloat.scala
@@ -3,6 +3,9 @@ package brainflight.tools.extendedTypes
 import java.nio.ByteBuffer
 
 class ExtendedFloat( f: Float ) {
+  /**
+   * Converts this float into an array of bytes
+   */
   def toBinary = {
     val binary = new Array[Byte]( 4 )
     ByteBuffer.wrap( binary ).putFloat( f )

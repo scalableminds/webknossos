a8ef550ef70daa3a27670e3f36b0eaae8a4afdd1
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-04-08 02:20:10 +0200

    BF-30 implemented mailer & user verification

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,8 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     "com.mongodb.casbah" % "casbah_2.9.1" % "2.1.5-1",
     "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT",
-    "com.restfb" % "restfb" % "1.6.9"
+    "com.restfb" % "restfb" % "1.6.9",
+    "org.apache.commons" % "commons-email" % "1.2"
   )
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
     resolvers += "repo.novus rels" at "http://repo.novus.com/releases/",

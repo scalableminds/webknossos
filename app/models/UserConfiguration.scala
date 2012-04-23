5e385a70e3e8959844723910834f2689c208a544
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-04-23 23:34:19 +0200

    BF-46 implemented support for boolean and integer

--- app/models/UserConfiguration.scala
+++ app/models/UserConfiguration.scala
@@ -2,20 +2,25 @@ package models
 
 import play.api.db._
 import play.api.Play.current
-
 import com.mongodb.casbah.Imports._
 import models.context._
 import com.novus.salat.annotations._
 import com.novus.salat.dao.SalatDAO
 import brainflight.security.SCrypt._
 import scala.collection.mutable.Stack
+import play.api.libs.json.JsValue
+import play.api.libs.json.JsBoolean
 
 case class UserConfiguration( 
-  settings: Map[String, String]  
+  settings: Map[String, JsValue]  
 )
 
 object UserConfiguration {
   val defaultConfiguration = UserConfiguration( 
-    Map("mouseActive" -> "true")  
+    Map("mouseActive" -> JsBoolean(true))  
   )
+  def isValidSetting( field: Tuple2[String, JsValue]) = {
+    val (_,value) = field
+    (value.asOpt[String]).isDefined || (value.asOpt[Int]).isDefined || (value.asOpt[Boolean]).isDefined
+  }
 }

fa4f97a41b473fb16db88360ec4451ed52551912
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-10-01 18:00:40 +0200

    OX-53 replaced websocket with REST

--- app/controllers/Task.scala
+++ app/controllers/Task.scala
@@ -32,20 +32,9 @@ import brainflight.tools.geometry.Point3D
  * Date: 19.12.11
  * Time: 11:27
  */
-class TaskHandler(var task: Experiment) {
 
-  def processInput(input: JsValue) {
-    (input.asOpt[Map[String, JsValue]]) map { obj =>
-      obj.collect {
-        case ("log", value) =>
-          (value).asOpt[Experiment].map(Experiment.save)
-        case ("useBranchpoint", value) =>
-        // TODO
-        case ("addBranchpoint", value) =>
-        // TODO
-      }
-    }
-  }
+object Task extends Controller with Secured {
+  override val DefaultAccessRole = Role.User
 
   def createDataSetInformation(dataSetId: ObjectId) =
     DataSet.findOneById(dataSetId) match {
@@ -59,72 +48,20 @@ class TaskHandler(var task: Experiment) {
         Json.obj("error" -> "Couldn't find dataset.")
     }
 
-  def createTaskInformation = {
+  def createTaskInformation(experiment: Experiment) = {
     Json.obj(
-      "task" -> task)
+      "experiment" -> experiment)
   }
 
-  def unicastOnStart(channel: Channel[JsValue]) {
-    channel.push(
-      createTaskInformation ++ createDataSetInformation(task.dataSetId))
+  def info(experimentId: String) = Authenticated { implicit request =>
+    Experiment.findOneById(experimentId).map(exp =>
+      Ok(createTaskInformation(exp) ++ createDataSetInformation(exp.dataSetId))).getOrElse(BadRequest("Task not found."))
   }
 
-  def unicastOnComplete() {
-    Logger.trace("Unicast task socket completed.")
-  }
-
-  def unicastOnError(error: String, input: Input[JsValue]) {
-    Logger.warn("Error on task socket: %s".format(error))
-  }
-
-  def openWebsocket() = {
-    val output = Concurrent.unicast[JsValue](
-      unicastOnStart,
-      unicastOnComplete,
-      unicastOnError)
-
-    val input = Iteratee.foreach[JsValue] { input =>
-      processInput(input)
-    } /*.mapDone { _ =>
-      
-    }*/
-    (input, output)
-  }
-}
-
-object Task extends Controller with Secured {
-  override val DefaultAccessRole = Role.User
-
-  def createEOFPair(error: Option[String] = None) = {
-    val iteratee = Done[JsValue, Unit]((), Input.EOF)
-
-    // Send an error and close the socket    
-
-    val enumerator = Enumerator[JsValue](
-      // send any kind of error message here    
-      Json.toJson(Map(
-        "error" -> (error getOrElse "connection closed")))).andThen(Enumerator.enumInput(Input.EOF))
-    (iteratee, enumerator)
-  }
-
-  def connect(taskId: String) = AuthenticatedWebSocket[JsValue]() { user =>
-    request =>
-      (for {
-        task <- Experiment.findOneById(taskId)
-      } yield (new TaskHandler(task)).openWebsocket()) getOrElse createEOFPair(Some("Couldn't find a task."))
-  }
-
-  def cometRCV(taskId: String, callback: String) = Authenticated { implicit request =>
-    val (_, output) = (for {
-      task <- Experiment.findOneById(taskId)
-    } yield (new TaskHandler(task)).openWebsocket()) getOrElse createEOFPair(Some("Couldn't find a task."))
-    Ok.stream(output &> Comet(callback = "parent." + callback))
-  }
-
-  def cometSND(taskId: String) = Authenticated(parser = parse.json) { implicit request =>
-    Experiment.findOneById(taskId).map { task =>
-      ((new TaskHandler(task)) processInput (request.body))
+  def update(expId: String) = Authenticated(parse.json) { implicit request =>
+    (request.body).asOpt[Experiment].map { exp =>
+      Experiment.save(exp)
       Ok
-    } getOrElse BadRequest("Couldn't update the requested task.")
+    } getOrElse (BadRequest("Update failed."))
   }
 }

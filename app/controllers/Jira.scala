4eb57259473ef3b7112eff840e4c73111b0241c9
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-10-13 16:09:20 +0200

    OX-78 added issue submission

--- app/controllers/Jira.scala
+++ app/controllers/Jira.scala
@@ -14,20 +14,46 @@ import com.sun.jersey.api.client.WebResource
 import com.sun.jersey.api.client.Client
 import com.sun.jersey.api.client.ClientResponse
 import views.html
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+import brainflight.security.InsecureSSLSocketFactory._
+import play.api.Play
+import play.api.Play.current
 
 object Jira extends Controller with Secured {
 
-  def index = Authenticated{ implicit request =>
-    Ok(html.jira.index())
+  val jiraUrl = "https://jira.scm.io"
+  val conf = Play.configuration
+  val branchName = conf.getString( "branchname" ) getOrElse "master"
+
+  def index = Authenticated { implicit request =>
+    Ok(html.jira.index(request.user))
   }
-  
-  def submit(title: String, message: String) = Authenticated { implicit request =>
+
+  def createIssue(user: User, summary: String, description: String) {
     val auth = new String(Base64.encodeBase64("autoreporter:frw378iokl!24".getBytes))
     val client = Client.create();
-    val webResource: WebResource = client.resource("https://jira.scm.io/rest/api/2/project")
-    
-    val response = webResource.header("Authorization", "Basic " + auth).`type`("application/json").accept("application/json").get(classOf[ClientResponse]);
-    println(response)
+
+    val issue = Json.obj(
+      "fields" -> Json.obj(
+        "project" -> Json.obj(
+          "key" -> "OX"),
+        "summary" -> summary,
+        "customfield_10008" -> branchName,
+        "description" -> (description + "\n\n Reported by: %s (%s)".format(user.name, user.email)),
+        "issuetype" -> Json.obj(
+          "name" -> "Bug"))).toString
+
+    usingSelfSignedCert {
+      val webResource: WebResource = client.resource(jiraUrl + "/rest/api/2/issue")
+
+      val response = webResource.header("Authorization", "Basic " + auth).`type`("application/json").accept("application/json").post(classOf[ClientResponse], issue);
+      println(response)
+    }
+  }
+
+  def submit(summary: String, description: String) = Authenticated { implicit request =>
+    createIssue(request.user, summary, description)
     Ok
   }
 }

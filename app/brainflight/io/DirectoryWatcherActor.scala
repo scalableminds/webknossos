a5912cb8a422467cb5d25d0d3943b28f4dee6bc6
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-11-15 18:17:42 +0100

    OX-148 added initial data for models

--- app/brainflight/io/DirectoryWatcherActor.scala
+++ app/brainflight/io/DirectoryWatcherActor.scala
@@ -84,7 +84,7 @@ class DirectoryWatcherActor(changeHandler: DirectoryChangeHandler) extends Actor
 
     registerAll(watchedPath)
     changeHandler.onStart(watchedPath)
-    updateTicker = context.system.scheduler.schedule(TICKER_INTERVAL, TICKER_INTERVAL)(
+    updateTicker = context.system.scheduler.schedule(0 seconds, TICKER_INTERVAL)(
       changeHandler.onTick(watchedPath))
     Akka.future {
       try {

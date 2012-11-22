4e8ad16240a2cc0a968dc4f44b4fe5172f6966e9
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-11-22 14:22:44 +0100

    OX-186 preserving previous review comments

--- app/models/tracing/TracingState.scala
+++ app/models/tracing/TracingState.scala
@@ -4,12 +4,15 @@ case class TracingState(
   isAssigned: Boolean = false,
   isFinished: Boolean = false,
   isInReview: Boolean = false,
+  isReadyForReview: Boolean = false,
   isInProgress: Boolean = false)
 
 object TracingState{
   val Assigned = TracingState(isAssigned = true)
   val Unassigned = TracingState()
 
+  val ReadyForReview = Assigned.copy(isReadyForReview = true)
+  
   val InReview = Assigned.copy(isInReview = true)
 
   val InProgress = Assigned.copy(isInProgress = true)

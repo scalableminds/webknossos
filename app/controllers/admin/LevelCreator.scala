819a38fcd547d390d303e3c455bb776af10c7514
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-12-05 14:38:41 +0100

    OX-229 created level views

--- app/controllers/admin/LevelCreator.scala
+++ app/controllers/admin/LevelCreator.scala
@@ -10,18 +10,35 @@ import play.api.data.Form
 import play.api.data.Forms.mapping
 import play.api.data.Forms.number
 import play.api.data.Forms.text
+import play.api.i18n.Messages
 
 object LevelCreator extends Controller with Secured {
   override def DefaultAccessRole = Role.Admin
 
   val levelForm = Form(
     mapping(
-      "name" -> text.verifying("level.invalidName", Level.isValidLevelName _))(Level.apply)(Level.unapply))
+      "name" -> text.verifying("level.invalidName", Level.isValidLevelName _))(Level.fromForm)(Level.toForm))
 
-  def index = Authenticated { implicit request =>
-    Ok(html.admin.creator.levelCreator())
+  def use(levelName: String) = Authenticated { implicit request =>
+    Level
+      .findOneByName(levelName)
+      .map { level =>
+        Ok(html.admin.creator.levelCreator(level))
+      }
+      .getOrElse(BadRequest("Level not found."))
   }
 
+  def delete(levelName: String) = Authenticated { implicit request =>
+    Level
+      .findOneByName(levelName)
+      .map { level =>
+        Level.remove(level)
+        AjaxOk.success(Messages("level.removed"))
+      }
+      .getOrElse(AjaxBadRequest.error("Level not found."))
+  }
+
+  
   def uploadAsset(levelName: String) = Authenticated(parse.multipartFormData) { implicit request =>
     request.body.file("asset").flatMap { assetFile =>
       Level.findOneByName(levelName).map { level =>
@@ -32,9 +49,9 @@ object LevelCreator extends Controller with Secured {
       }
     } getOrElse BadRequest("Invalid request or level.")
   }
-  
-  def listAssets(levelName: String) = Authenticated{ implicit request =>
-        Level
+
+  def listAssets(levelName: String) = Authenticated { implicit request =>
+    Level
       .findOneByName(levelName)
       .map { level =>
         Ok(level.assets.map(_.getName).mkString(", "))
@@ -55,7 +72,7 @@ object LevelCreator extends Controller with Secured {
       }
       .getOrElse(BadRequest("Level not found."))
   }
-  
+
   def deleteAsset(levelName: String, asset: String) = Authenticated { implicit request =>
     Level
       .findOneByName(levelName)
@@ -72,18 +89,17 @@ object LevelCreator extends Controller with Secured {
 
   def create = Authenticated(parser = parse.urlFormEncoded) { implicit request =>
     levelForm.bindFromRequest.fold(
-      formWithErrors => BadRequest(formWithErrors.toString), //((taskCreateHTML(taskFromTracingForm, formWithErrors)),
+      formWithErrors => BadRequest(html.admin.creator.levelList(Level.findAll, formWithErrors)), //((taskCreateHTML(taskFromTracingForm, formWithErrors)),
       { t =>
         if (Level.isValidLevelName(t.name)) {
           Level.insertOne(t)
-          Ok
+          Ok(html.admin.creator.levelList(Level.findAll, levelForm))
         } else
           BadRequest("Invalid level name")
       })
   }
 
   def list = Authenticated { implicit request =>
-    Level.findAll
-    Ok
+    Ok(html.admin.creator.levelList(Level.findAll, levelForm))
   }
 }

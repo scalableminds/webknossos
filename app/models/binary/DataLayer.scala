f0877ca5b4e23e00e1d0036526fe9d39731bcca6
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2012-12-16 09:07:52 +0100

    BF-278 fixed compilation error

--- app/models/binary/DataLayer.scala
+++ app/models/binary/DataLayer.scala
@@ -5,6 +5,60 @@ import brainflight.tools.geometry.Vector3D
 import brainflight.tools.Interpolator
 import scala.collection.mutable.ArrayBuffer
 
+trait TrilerpInterpolation {
+
+  def getColor(
+    byteLoader: (Point3D, Int, Int, Map[Point3D, Array[Byte]]) => ArrayBuffer[Byte],
+    resolution: Int,
+    blockMap: Map[Point3D, Array[Byte]])(point: Point3D): Double = {
+
+    val color = byteLoader(point, 1, resolution, blockMap)(0)
+    (0xff & color.asInstanceOf[Int])
+  }
+
+  def interpolate(
+    resolution: Int,
+    blockMap: Map[Point3D, Array[Byte]],
+    byteLoader: (Point3D, Int, Int, Map[Point3D, Array[Byte]]) => ArrayBuffer[Byte])(point: Vector3D): ArrayBuffer[Byte] = {
+
+    val colorF = getColor(byteLoader, resolution, blockMap) _
+    val x = point.x.toInt
+    val y = point.y.toInt
+    val z = point.z.toInt
+
+    val floored = Vector3D(x, y, z)
+    if (point == floored) {
+      ArrayBuffer(getColor(byteLoader, resolution, blockMap)(Point3D(x, y, z)).toByte)
+    } else {
+      val q = Array(
+        colorF(Point3D(x, y, z)),
+        colorF(Point3D(x, y, z + 1)),
+        colorF(Point3D(x, y + 1, z)),
+        colorF(Point3D(x, y + 1, z + 1)),
+        colorF(Point3D(x + 1, y, z)),
+        colorF(Point3D(x + 1, y, z + 1)),
+        colorF(Point3D(x + 1, y + 1, z)),
+        colorF(Point3D(x + 1, y + 1, z + 1)))
+
+      ArrayBuffer(Interpolator.triLerp(point - floored, q).round.toByte)
+    }
+  }
+}
+
+trait NearestNeighborInterpolation {
+  def bytesPerElement: Int
+
+  def interpolate(
+    resolution: Int,
+    blockMap: Map[Point3D, Array[Byte]],
+    byteLoader: (Point3D, Int, Int, Map[Point3D, Array[Byte]]) => ArrayBuffer[Byte])(point: Vector3D): ArrayBuffer[Byte] = {
+    
+    val byte = point.x % bytesPerElement
+    val x = (point.x - byte + (if (bytesPerElement - 2 * byte >= 0) 0 else bytesPerElement)).toInt
+    byteLoader(Point3D(x, point.y.round.toInt, point.z.round.toInt), bytesPerElement, resolution, blockMap)
+  }
+}
+
 sealed trait DataLayer {
   val folder: String
   val elementSize: Int

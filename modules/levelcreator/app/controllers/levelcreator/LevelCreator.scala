b1e2b28f1b182d874ad0e1612fc505bf571602c1
Thomas Werkmeister <thomas@scm.io>
2013-02-09 14:22:17 +0100

    OX-286 files are zipped on stackcreation now

--- modules/levelcreator/app/controllers/levelcreator/LevelCreator.scala
+++ modules/levelcreator/app/controllers/levelcreator/LevelCreator.scala
@@ -91,7 +91,8 @@ object LevelCreator extends Controller {
       Ok(Json.toJson(level.assets.map(_.getName)))
     }
   }
-
+  
+  //TODO produce one that has not been created yet
   def produce(levelId: String) = Action { implicit request =>
     Async{
       for {
@@ -107,7 +108,7 @@ object LevelCreator extends Controller {
       }
     }
   }
-  
+  /*
   def download(levelId: String) = Action { implicit request => 
     Async{
       for {
@@ -118,7 +119,7 @@ object LevelCreator extends Controller {
         future.mapTo[File].map{file => Ok.sendFile(content = file)}
       }
     }
-  }
+  }*/
 
   def retrieveAsset(levelId: String, asset: String) = Action { implicit request =>
     for {

ef144b639841b145b294e63ab284eeecbd9641d1
Thomas Werkmeister <thomas@scm.io>
2013-02-23 00:15:52 +0100

    LEV-45 introduced Stacks controller

--- modules/levelcreator/app/controllers/levelcreator/LevelCreator.scala
+++ modules/levelcreator/app/controllers/levelcreator/LevelCreator.scala
@@ -1,38 +1,23 @@
 package controllers.levelcreator
 
 import braingames.mvc.Controller
-import play.api.libs.concurrent._
-import scala.concurrent.duration._
-import scala.concurrent._
-import akka.pattern.ask
-import akka.routing.RoundRobinRouter
 import views._
 import braingames.mvc._
 import models.knowledge._
 import play.api.Play.current
-import akka.actor._
-import akka.util.Timeout
-import akka.pattern.AskTimeoutException
-import braingames.levelcreator._
 import play.api.mvc.Action
 import play.api.data._
 import play.api.data.Forms._
 import play.api.i18n.Messages
 import play.api.libs.json._
 import play.api._
-import ExecutionContext.Implicits.global
+
 import java.io.File
 import scala.util.Failure
 import models.binary.DataSet
 import braingames.util.ExtendedTypes.ExtendedString
 
 object LevelCreator extends LevelCreatorController {
-
-  val levelCreateRouter = Akka.system.actorOf(Props[LevelCreateActor].withRouter(RoundRobinRouter(nrOfInstances = 4)),
-      name = "LevelCreateRouter")
-
-  val conf = Play.current.configuration
-  implicit val timeout = Timeout(60 seconds)
 
   val levelForm = Form(
     mapping(
@@ -80,65 +65,6 @@ object LevelCreator extends LevelCreatorController {
 
   def listAssets(levelId: String) = ActionWithValidLevel(levelId) { implicit request =>
       Ok(Json.toJson(request.level.assets.map(_.getName)))
-  }
-  //TODO: timeout like infinite
-  def createLevels(level: Level, missions: List[Mission]) = {
-    println(missions)
-    implicit val timeout = Timeout((100 * missions.size) seconds)
-    val future = Future.traverse(missions)(m => ask(levelCreateRouter, CreateLevel(level, m))).recover {
-      case e: AskTimeoutException =>
-        Logger.error("stack creation timed out")
-        Messages("level.stack.creationTimeout")
-        List()
-    }
-    future.mapTo[List[Option[Mission]]].map { ms => 
-      val renderedMissions = ms.flatten
-      level.addRenderedMissions(renderedMissions.map(_.id))
-      JsonOk(s"created ${renderedMissions.map(m => (m.id.takeRight(6))).mkString("\n")}") 
-    } 
-  }
-  
-  def stackList(levelId: String) = ActionWithValidLevel(levelId) { implicit request =>
-      val missions = for{missionId <- request.level.renderedMissions
-                          mission <- Mission.findOneById(missionId)
-        } yield mission
-      Ok(html.levelcreator.stackList(request.level, missions))
-  }
-  
-  def stackListJson(levelId: String) = ActionWithValidLevel(levelId) { implicit request =>
-      Ok(Json.toJson(request.level.renderedMissions))
-  }
-  
-  def deleteStack(levelId: String, missionId: String) = ActionWithValidLevel(levelId) { implicit request =>
-      request.level.removeRenderedMission(missionId)
-      JsonOk(Messages("level.stack.removed"))
-  }
-
-  def produce(levelId: String, count: Int) = ActionWithValidLevel(levelId) { implicit request =>
-    Async {
-      val missions = Mission.findByDataSetName(request.level.dataSetName).
-          filterNot(m => request.level.renderedMissions.contains(m.id))
- 
-      createLevels(request.level, missions.take(count))
-    }
-  }
-  
-  def produceAll(levelId: String) = ActionWithValidLevel(levelId) { implicit request =>
-    Async{
-      val missions = Mission.findByDataSetName(request.level.dataSetName).toList
-      createLevels(request.level, missions) 
-    }
-  }
-
-  def produceBulk(levelId: String) = ActionWithValidLevel(levelId, parse.urlFormEncoded) { implicit request =>
-    Async{
-      for {
-        missionIds <- postParameterList("missionId") ?~ Messages("mission.startId.missing")
-        missions = missionIds.flatMap(Mission.findOneById).toList
-      } yield {
-        createLevels(request.level, missions)
-      }
-    }
   }
 
   def retrieveAsset(levelId: String, asset: String) = ActionWithValidLevel(levelId) { implicit request =>
---
+++ modules/levelcreator/app/controllers/levelcreator/Stacks.scala
@@ -0,0 +1,85 @@
+package controllers.levelcreator
+
+import akka.pattern.ask
+import akka.routing.RoundRobinRouter
+import play.api.libs.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import akka.actor._
+import akka.util.Timeout
+import akka.pattern.AskTimeoutException
+import play.api.Play.current
+import play.api.libs.json._
+import play.api.i18n.Messages
+import play.api._
+
+import braingames.levelcreator._
+import views._
+import models.knowledge.{Level, Mission}
+
+object Stacks extends LevelCreatorController{
+  
+  val levelCreateRouter = Akka.system.actorOf(Props[LevelCreateActor].withRouter(RoundRobinRouter(nrOfInstances = 4)),
+      name = "LevelCreateRouter")
+      
+  def list(levelId: String) = ActionWithValidLevel(levelId) { implicit request =>
+      val missions = for{missionId <- request.level.renderedMissions
+                          mission <- Mission.findOneById(missionId)
+        } yield mission
+      Ok(html.levelcreator.stackList(request.level, missions))
+  }
+  
+  def listJson(levelId: String) = ActionWithValidLevel(levelId) { implicit request =>
+      Ok(Json.toJson(request.level.renderedMissions))
+  }
+  
+  def delete(levelId: String, missionId: String) = ActionWithValidLevel(levelId) { implicit request =>
+      request.level.removeRenderedMission(missionId)
+      JsonOk(Messages("level.stack.removed"))
+  }
+  
+  def create(level: Level, missions: List[Mission]) = {
+    println(missions)
+    implicit val timeout = Timeout((100 * missions.size) seconds)
+    val future = Future.traverse(missions)(m => ask(levelCreateRouter, CreateLevel(level, m))).recover {
+      case e: AskTimeoutException =>
+        Logger.error("stack creation timed out")
+        Messages("level.stack.creationTimeout")
+        List()
+    }
+    future.mapTo[List[Option[Mission]]].map { ms => 
+      val renderedMissions = ms.flatten
+      level.addRenderedMissions(renderedMissions.map(_.id))
+      JsonOk(s"created ${renderedMissions.map(m => (m.id.takeRight(6))).mkString("\n")}") 
+    } 
+  }
+
+  def produce(levelId: String, count: Int) = ActionWithValidLevel(levelId) { implicit request =>
+    Async {
+      val missions = Mission.findByDataSetName(request.level.dataSetName).
+          filterNot(m => request.level.renderedMissions.contains(m.id))
+ 
+      create(request.level, missions.take(count))
+    }
+  }
+  
+  def produceAll(levelId: String) = ActionWithValidLevel(levelId) { implicit request =>
+    Async{
+      val missions = Mission.findByDataSetName(request.level.dataSetName).toList
+      create(request.level, missions) 
+    }
+  }
+
+  def produceBulk(levelId: String) = ActionWithValidLevel(levelId, parse.urlFormEncoded) { implicit request =>
+    Async{
+      for {
+        missionIds <- postParameterList("missionId") ?~ Messages("mission.startId.missing")
+        missions = missionIds.flatMap(Mission.findOneById).toList
+      } yield {
+        create(request.level, missions)
+      }
+    }
+  }
+  
+}

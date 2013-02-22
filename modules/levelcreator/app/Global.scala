39e7d6236d36c5260f7f207f4763eaeb69075847
Thomas Werkmeister <thomas@scm.io>
2013-02-23 00:39:22 +0100

    LEV-45 Stopping mission watcher on shutdown

--- modules/levelcreator/app/Global.scala
+++ modules/levelcreator/app/Global.scala
@@ -36,7 +36,7 @@ object Global extends GlobalSettings {
             //BasicEvolution.runDBEvolution()
             // Data insertion needs to be delayed, because the dataSets need to be
             // found by the DirectoryWatcher first
-            MissionWatcher ! StartWatchingForMissions()
+            MissionWatcher ! StartWatchingForMissions
             Logger.info("starting in Dev mode")
           }
       }
@@ -45,6 +45,7 @@ object Global extends GlobalSettings {
   override def onStop(app: Application) {
     ActorSystems.dataRequestSystem.shutdown
     DirectoryWatcher ! StopWatching
+    MissionWatcher ! StopWatchingForMissions
     models.context.BinaryDB.connection.close()
     models.context.db.close()
   }

be6621015e589ef76b7b73e3c827f917bacf35f1
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2013-03-16 18:38:16 +0100

    BF-300 implemented data image creation

--- modules/isoshader/app/controllers/isoshader/BinaryData.scala
+++ modules/isoshader/app/controllers/isoshader/BinaryData.scala
@@ -15,7 +15,7 @@ import models.binary._
 import akka.util.Timeout
 import brainflight.binary._
 import braingames.mvc.Controller
-import play.api.i18n.Messages 
+import play.api.i18n.Messages
 import brainflight.tools.geometry._
 import akka.pattern.ask
 import akka.pattern.AskTimeoutException
@@ -23,42 +23,10 @@ import scala.collection.mutable.ArrayBuffer
 import play.api.libs.concurrent.Execution.Implicits._
 
 object BinaryData extends Controller {
-  val dataRequestActor = Akka.system.actorOf(Props(new DataRequestActor), name = "dataRequestActor") //.withRouter(new RoundRobinRouter(3)))
-
   val conf = Play.current.configuration
   implicit val timeout = Timeout((conf.getInt("actor.defaultTimeout") getOrElse 20) seconds) // needed for `?` bel
-  
-  val cubeSize = 64
-  def cuboidFromPosition(px: Int, py: Int, pz: Int) = {
-    val cubeCorner = Vector3D(px - px % cubeSize, py - py % cubeSize, pz - pz % cubeSize)
-    Cuboid(cubeSize, cubeSize, cubeSize, 1, Some(cubeCorner))
-  }
-  
-  def viaAjax(dataSetName: String, px: Int, py: Int, pz: Int) = 
-    Action { implicit request => 
-    Async {
-      val t = System.currentTimeMillis()
-      for {
-        dataSet <- DataSet.findOneByName(dataSetName) ?~ Messages("dataset.notFound")
-        dataLayer <- dataSet.dataLayers.get("color")
-      } yield {
-        (dataRequestActor ? SingleRequest(DataRequest(
-          dataSet,
-          dataLayer,
-          1,
-          cuboidFromPosition(px, py, pz),
-          useHalfByte = false,
-          skipInterpolation = true)))
-        .recover{
-          case e: AskTimeoutException =>
-            Logger.error("calculateImages: AskTimeoutException")
-            new Array[Byte](cubeSize * cubeSize * cubeSize * dataLayer.bytesPerElement).toBuffer
-        }
-        .mapTo[ArrayBuffer[Byte]].map { data =>
-          Logger.debug("total: %d ms".format(System.currentTimeMillis - t))
-          Ok(data.toArray)
-        }
-      }
-    }
+
+  def requestImage(dataSetName: String, dataLayerName: String, imagesPerRow: Int, x: Int, y: Int, z: Int, resolution: Int) = Action { implicit request =>
+    controllers.BinaryData.respondeWithImage(dataSetName, dataLayerName, imagesPerRow, x, y, z, resolution)
   }
 }

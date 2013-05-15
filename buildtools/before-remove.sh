f552d36d788842595e8aea20a67f131f193e2027
Thomas Werkmeister <thomas@scm.io>
2013-05-15 08:58:19 +0200

    comparing strings and not numbers

--- buildtools/before-remove.sh
+++ buildtools/before-remove.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ $1 = "purge" -o $1 -eq 0 ];then
+if [ "$1" = "purge" -o "$1" = "0" ];then
   <% if init_system == "systemd" %>
   systemctl stop <%= name %>.service
   systemctl disable <%= name %>.service

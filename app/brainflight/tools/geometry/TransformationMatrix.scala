1d7a52424611c884db22b62e8826b9cb057c6833
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2013-07-17 14:49:45 +0200

    fixed rotation matrix

--- app/brainflight/tools/geometry/TransformationMatrix.scala
+++ app/brainflight/tools/geometry/TransformationMatrix.scala
@@ -20,8 +20,8 @@ object TransformationMatrix {
     val direction = Vector3D(directionOld.x * 11, directionOld.y * 11, directionOld.z * 28)
     val nz = direction.normalize
     val y = Vector3D(0, 1, 0)
-    val nx = (y x nz).normalize
-    val ny = (nx x nz).normalize
+    val nx = (nz x y).normalize
+    val ny = (nz x nx).normalize
 
     TransformationMatrix(Array(nx.x, nx.y, nx.z, 0, ny.x, ny.y, ny.z, 0, nz.x, nz.y, nz.z, 0, pos.x, pos.y, pos.z, 1).map(_.toFloat))
   }

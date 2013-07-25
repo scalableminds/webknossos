26ee5f7f0dd9be63f65d982c7ad1c72ed17ab528
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2013-07-25 15:00:06 +0200

    OX-722 implemented interpolation for multiple bytes per data point

--- modules/braingames-binary/src/main/scala/braingames/binary/models/Interpolation.scala
+++ modules/braingames-binary/src/main/scala/braingames/binary/models/Interpolation.scala
@@ -12,23 +12,22 @@ trait Interpolation {
 
 object TrilerpInterpolation extends Interpolation {
 
-  def getColor(byteLoader: (Point3D) => Array[Byte])(point: Point3D): Double = {
-    val color = byteLoader(point)(0)
-    (0xff & color.asInstanceOf[Int])
+  def getColor(byteLoader: (Point3D) => Array[Byte])(point: Point3D): Array[Double] = {
+    byteLoader(point).map(b => (0xff & b.asInstanceOf[Int]).asInstanceOf[Double])
   }
 
   def interpolate(
     bytesPerElement: Int,
     byteLoader: (Point3D) => Array[Byte])(point: Vector3D): Array[Byte] = {
 
-    val colorF = getColor(byteLoader) _
     val x = point.x.toInt
     val y = point.y.toInt
     val z = point.z.toInt
 
     if (point.x == x && point.y == y & point.z == z) {
-      Array(colorF(Point3D(x, y, z)).toByte)
+      byteLoader(Point3D(x,y,z))
     } else {
+      val colorF = getColor(byteLoader) _
       val floored = Vector3D(x, y, z)
       val q = Array(
         colorF(Point3D(x, y, z)),
@@ -40,7 +39,7 @@ object TrilerpInterpolation extends Interpolation {
         colorF(Point3D(x + 1, y + 1, z)),
         colorF(Point3D(x + 1, y + 1, z + 1)))
 
-      Array(Interpolator.triLerp(point - floored, q).round.toByte)
+      Interpolator.triLerp(point - floored, q, bytesPerElement).map(_.round.toByte)
     }
   }
 }

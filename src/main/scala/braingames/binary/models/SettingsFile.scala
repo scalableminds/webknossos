d6255d6f4ef9ed16d02d7c188c7cb3d04f73829f
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2013-10-12 23:35:35 -0700

    made json helper read more fault tolerant

--- src/main/scala/braingames/binary/models/SettingsFile.scala
+++ src/main/scala/braingames/binary/models/SettingsFile.scala
@@ -1,9 +1,11 @@
 package braingames.binary.models
 
 import java.io.File
-import play.api.libs.json.{Writes, Reads}
+import play.api.libs.json._
 import braingames.util.JsonHelper._
 import braingames.util.FileIO
+import net.liftweb.common._
+import net.liftweb.common.Box._
 
 /**
  * Company: scalableminds
@@ -11,13 +13,17 @@ import braingames.util.FileIO
  * Date: 09.06.13
  * Time: 17:44
  */
-trait SettingsFile {
-  def extractSettingsFromFile[T](file: File, settingsReads: Reads[T]): Option[T] = {
+trait SettingsFile{
+  def extractSettingsFromFile[T](file: File, settingsReads: Reads[T]): Box[T] = {
     if (file.isFile) {
-      JsonFromFile(file)
-        .validate(settingsReads)
-        .asOpt
-    } else None
+      JsonFromFile(file).flatMap{
+        _.validate(settingsReads) match{
+          case JsSuccess(e ,_) => Full(e)
+          case e : JsError => Failure(e.toString)
+        }
+      }
+    } else 
+      Empty
   }
 
   def writeSettingsToFile[T](settings: T, file: File)(implicit writer: Writes[T]) = {

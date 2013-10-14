94e2125bf11124bf1c9741cb278f372a3f52a00f
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2013-10-14 16:39:00 -0700

    introduced matrixBase3D

--- src/main/scala/braingames/binary/DataModel.scala
+++ src/main/scala/braingames/binary/DataModel.scala
@@ -14,34 +14,36 @@ abstract class DataModel {
 
   protected def rotateAndMove(
     moveVector: (Double, Double, Double),
-    axis: (Double, Double, Double),
+    rotationBase: Option[MatrixBase3D],
     coordinates: Array[Vector3D]): Array[Vector3D] = {
     def ff(f: (Double, Double, Double) => Array[Vector3D]): Array[Vector3D] = {
       coordinates.map(c => f(c.x, c.y, c.z)(0))
     }
 
-    rotateAndMove(moveVector, axis)(ff)((x, y, z) => Array(Vector3D(x, y, z)))
+    rotateAndMove(moveVector, rotationBase)(ff)((x, y, z) => Array(Vector3D(x, y, z)))
   }
 
   @inline
   protected def rotateAndMove[T](
     moveVector: (Double, Double, Double),
-    axis: (Double, Double, Double))(coordinates: ((Double, Double, Double) => Array[T]) => Array[T])(f: (Double, Double, Double) => Array[T]): Array[T] = {
+    rotationBase: Option[MatrixBase3D])(coordinates: ((Double, Double, Double) => Array[T]) => Array[T])(f: (Double, Double, Double) => Array[T]): Array[T] = {
 
-    if (axis._1 == 0 && axis._2 == 0 && axis._3 == 0) {
-      simpleMove(moveVector, coordinates)(f)
-    } else {
-      // orthogonal vector to (0,1,0) and rotation vector
-      val matrix = TransformationMatrix(Vector3D(moveVector), Vector3D(axis)).value
+    rotationBase match{
+      case Some(rotation) =>
+        // orthogonal vector to (0,1,0) and rotation vector
+        val matrix = TransformationMatrix(Vector3D(moveVector), rotation).value
 
-      @inline
-      def coordinateTransformer(px: Double, py: Double, pz: Double) = {
-        f(matrix(0) * px + matrix(1) * py + matrix(2) * pz + matrix(3),
-          matrix(4) * px + matrix(5) * py + matrix(6) * pz + matrix(7),
-          matrix(8) * px + matrix(9) * py + matrix(10) * pz + matrix(11))
-      }
+        @inline
+        def coordinateTransformer(px: Double, py: Double, pz: Double) = {
+          f(matrix(0) * px + matrix(1) * py + matrix(2) * pz + matrix(3),
+            matrix(4) * px + matrix(5) * py + matrix(6) * pz + matrix(7),
+            matrix(8) * px + matrix(9) * py + matrix(10) * pz + matrix(11))
+        }
 
-      coordinates(coordinateTransformer)
+        coordinates(coordinateTransformer)
+      case None =>
+        simpleMove(moveVector, coordinates)(f)
+
     }
   }
 
@@ -73,7 +75,7 @@ case class Cuboid(
     resolution: Int = 1,
     topLeftOpt: Option[Vector3D] = None,
     moveVector: (Double, Double, Double) = (0, 0, 0),
-    axis: (Double, Double, Double) = (0, 0, 0)) extends DataModel {
+    rotationBase: Option[MatrixBase3D] = None) extends DataModel {
 
   val width = resolution * _width
   val height = resolution * _height
@@ -86,7 +88,7 @@ case class Cuboid(
     Vector3D(-xh, -yh, -zh)
   }
 
-  val corners = rotateAndMove(moveVector, axis, Array(
+  val corners = rotateAndMove(moveVector, rotationBase, Array(
     topLeft,
     topLeft.dx(width - 1),
     topLeft.dy(height - 1),
@@ -95,7 +97,7 @@ case class Cuboid(
     topLeft.dz(depth - 1).dx(width - 1),
     topLeft.dz(depth - 1).dy(height - 1),
     topLeft.dz(depth - 1).dx(width - 1).dy(height - 1)))
-  
+
   val maxCorner = corners.foldLeft((0.0, 0.0, 0.0))((b, e) => (
     math.max(b._1, e.x), math.max(b._2, e.y), math.max(b._3, e.z)))
 
@@ -103,7 +105,7 @@ case class Cuboid(
     math.min(b._1, e.x), math.min(b._2, e.y), math.min(b._3, e.z)))
 
   def isUnrotated =
-    axis == (0,0,0)
+    rotationBase.isEmpty
 
   @inline
   private def looper[T](extendArrayBy: Int, c: ClassTag[T])(f: (Double, Double, Double) => Array[T]) = {
@@ -134,6 +136,6 @@ case class Cuboid(
   }
 
   override def withContainingCoordinates[T](extendArrayBy: Int = 1)(f: (Double, Double, Double) => Array[T])(implicit c: ClassTag[T]): Array[T] = {
-    rotateAndMove(moveVector, axis)(looper[T](extendArrayBy, c))(f)
+    rotateAndMove(moveVector, rotationBase)(looper[T](extendArrayBy, c))(f)
   }
 }

b57d0b96d07bff2528c8a2697187c416fa65411a
Tom Herold <tom.herold@scalableminds.com>
2014-02-08 13:30:52 +0800

    added REST API to get task selection algorithm

--- app/controllers/admin/TaskAlgorithm.scala
+++ app/controllers/admin/TaskAlgorithm.scala
@@ -5,7 +5,9 @@ import play.api.libs.json.Json
 import views.html
 import play.api.i18n.Messages
 import braingames.util.ExtendedTypes.ExtendedBoolean
+import braingames.util.ExtendedTypes.ExtendedString
 import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json._
 
 object TaskAlgorithm extends AdminController {
 
@@ -18,27 +20,36 @@ object TaskAlgorithm extends AdminController {
     }
   }
 
-  def index = Authenticated.async { implicit request =>
-    for {
-      algorithms <- TaskSelectionAlgorithmDAO.findAll
-      current <- TaskSelectionAlgorithmDAO.current
-    } yield {
-      Ok(html.admin.task.taskSelectionAlgorithm(algorithms, current))
-    }
+  def index = Authenticated { implicit request =>
+    Ok(views.html.main()(Html.empty))
   }
 
-  def submitAlgorithm = Authenticated.async(parse.urlFormEncoded) { implicit request =>
-    (for {
-      code <- postParameter("code") ?~> Messages("taskAlgorithm.notSupplied")
-      use <- postParameter("use") ?~> Messages("taskAlgorithm.use.notSupplied")
-      if(TaskSelectionAlgorithm.isValidAlgorithm(code))
-      alg = TaskSelectionAlgorithm(code)
-      _ <- TaskSelectionAlgorithmDAO.insert(alg)
+  // def create = Authenticated.async(parse.json){ implicit request =>
+  //   (for {
+  //     code <- postParameter("code") ?~> Messages("taskAlgorithm.notSupplied")
+  //     use <- postParameter("use") ?~> Messages("taskAlgorithm.use.notSupplied")
+  //     if(TaskSelectionAlgorithm.isValidAlgorithm(code))
+  //     alg = TaskSelectionAlgorithm(code)
+  //     _ <- TaskSelectionAlgorithmDAO.insert(alg)
+  //   } yield {
+  //     if (use == "1")
+  //       TaskSelectionAlgorithmDAO.use(alg)
+  //     Ok
+  //   }) ?~> Messages("taskAlgorithm.invalid") ~> 422
+  // }
+
+  def list = Authenticated.async { implicit request =>
+    for {
+      algorithms <- TaskSelectionAlgorithmDAO.findAll
     } yield {
-      if (use == "1")
-        TaskSelectionAlgorithmDAO.use(alg)
-      Ok
-    }) ?~> Messages("taskAlgorithm.invalid") ~> 422
+      val filtered = request.getQueryString("isActive").flatMap(_.toBooleanOpt) match{
+        case Some(isActive) =>
+          algorithms.filter(_.isActive == isActive)
+        case None =>
+          algorithms
+      }
+      Ok(Writes.list[TaskSelectionAlgorithm].writes(filtered))
+    }
   }
 
   def useAlgorithm(id: String) = Authenticated.async { implicit request =>
--- app/models/task/TaskSelectionAlgorithm.scala
+++ app/models/task/TaskSelectionAlgorithm.scala
@@ -9,7 +9,7 @@ import reactivemongo.bson.BSONObjectID
 import play.modules.reactivemongo.json.BSONFormats._
 import play.api.libs.concurrent.Execution.Implicits._
 
-case class TaskSelectionAlgorithm(js: String, active: Boolean = true, timestamp: Date = new Date, _id: BSONObjectID = BSONObjectID.generate) {
+case class TaskSelectionAlgorithm(js: String, isActive: Boolean = true, timestamp: Date = new Date, _id: BSONObjectID = BSONObjectID.generate) {
   val id = _id.stringify
 
   def isValidAlgorithm = TaskSelectionAlgorithm.isValidAlgorithm(js)
@@ -28,7 +28,7 @@ object TaskSelectionAlgorithmDAO extends SecuredBaseDAO[TaskSelectionAlgorithm] 
   val formatter = TaskSelectionAlgorithm.taskSelectionAlgorithmFormat
 
   def current(implicit ctx: DBAccessContext) = {
-    collectionFind(Json.obj("active" -> true))
+    collectionFind(Json.obj("isActive" -> true))
     .sort(Json.obj("timestamp" -> -1))
     .one[TaskSelectionAlgorithm].map {
       case Some(a) => a
@@ -39,18 +39,18 @@ object TaskSelectionAlgorithmDAO extends SecuredBaseDAO[TaskSelectionAlgorithm] 
   def use(alg: TaskSelectionAlgorithm)(implicit ctx: DBAccessContext) {
     collectionUpdate(
       Json.obj("_id" -> Json.obj("$ne" -> alg._id)),
-      Json.obj("$set" -> Json.obj("active" -> false)),
+      Json.obj("$set" -> Json.obj("isActive" -> false)),
       multi = true)
     collectionUpdate(
       Json.obj("_id" -> alg._id),
-      Json.obj("$set" -> Json.obj("active" -> true)))
+      Json.obj("$set" -> Json.obj("isActive" -> true)))
   }
 
   implicit object TaskSelectionAlgorithmFormat extends Writes[TaskSelectionAlgorithm] {
     def writes(e: TaskSelectionAlgorithm) = Json.obj(
       "id" -> e.id,
       "js" -> e.js,
-      "active" -> e.active)
+      "isActive" -> e.isActive)
   }
 
 }

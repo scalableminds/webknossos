39dee1364534da90e20d3a36117928d6ee843382
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2014-02-14 16:34:40 +0800

    refactored MongoDAO and introduced exception catchers for all db operations

--- src/main/scala/braingames/binary/DataRequestActor.scala
+++ src/main/scala/braingames/binary/DataRequestActor.scala
@@ -26,7 +26,7 @@ import net.liftweb.common.Box
 import net.liftweb.common.{Failure => BoxFailure}
 import net.liftweb.common.Full
 import java.util.NoSuchElementException
-import braingames.util.BlockedArray3D
+import braingames.util.{Fox, BlockedArray3D}
 import braingames.util.ExtendedTypes.ExtendedArraySeq
 import braingames.util.ExtendedTypes.ExtendedDouble
 
@@ -157,7 +157,7 @@ class DataRequestActor(
       section =>
         requestedLayer.section.map( _ == section.sectionId) getOrElse true
     }: _*).append {
-      Await.result(layer.fallback.map(dataSourceRepository.findByName).getOrElse(Future.successful(None)).map(_.flatMap {
+      Await.result(layer.fallback.map(dataSourceRepository.findByName).getOrElse(Fox.empty).futureBox.map(_.flatMap{
         d =>
           d.dataLayer(requestedLayer.typ).map {
             fallbackLayer =>

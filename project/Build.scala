32802cb74073bf9ceb72b12c0d0baf1ae3eafcb8
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2014-03-20 21:00:09 +0100

    updated braingames libs to 4.1.0

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object Dependencies{
   val akkaVersion = "2.2.0"
   val reactiveVersion = "0.10.0"
   val reactivePlayVersion = "0.10.2"
-  val braingamesVersion = "4.1.0-SNAPSHOT"
+  val braingamesVersion = "4.1.0"
 
   val restFb = "com.restfb" % "restfb" % "1.6.11"
   val commonsIo = "commons-io" % "commons-io" % "2.4"

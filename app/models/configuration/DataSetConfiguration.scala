96ee366be64d99473424c8c802c13930ec28554e
Johannes Frohnhofen <johannes@scm.io>
2014-03-26 18:05:41 +0100

    added evolution

--- app/models/configuration/DataSetConfiguration.scala
+++ app/models/configuration/DataSetConfiguration.scala
@@ -2,22 +2,17 @@ package models.configuration
 
 import play.api.libs.json._
 import models.basics.BasicSettings
-/*import play.api.Play.current
-import braingames.security.SCrypt._
-import scala.collection.mutable.Stack
-import play.api.libs.json.JsValue
-import play.api.libs.json.JsBoolean
-*/
 
-case class DataSetConfiguration(
-    configuration: Map[String, JsValue]) {
+case class DataSetConfiguration(configuration: Map[String, JsValue]) {
 
   def configurationOrDefaults = {
     DataSetConfiguration.default.configuration ++ configuration
   }
+
 }
 
 object DataSetConfiguration extends BasicSettings {
+
   implicit val dataSetConfigurationFormat = Json.format[DataSetConfiguration]
 
   val default = DataSetConfiguration(
@@ -30,5 +25,3 @@ object DataSetConfiguration extends BasicSettings {
       "layerColors" -> JsObject(Seq.empty)))
 
 }
-
-
---
+++ conf/evolutions/4.js
@@ -0,0 +1,13 @@
+// Update to change the user configuration 'isWritable'
+//  - rename configuration.settings to userConfiguration.configuration
+//  - remove attributes, that now belong to dataSetConfiguration
+//  - create dataSetConfigurations
+
+// --- !Ups
+db.users.update({}, {$rename: {"configuration.settings": "userConfiguration.configuration"}}, {multi: true})
+db.users.update({}, {$unset: {"configuration": 0, "userConfiguration.configuration.brightnessContrastSettings": 0, "userConfiguration.configuration.fourBit": 0, "userConfiguration.configuration.quality": 0, "userConfiguration.configuration.interpolation": 0}, $set: {dataSetConfigurations: {}}}, {multi: true})
+
+// --- !Downs
+db.users.update({}, {$rename: {"userConfiguration.configuration": "configuration.settings"}}, {multi: true})
+db.users.update({}, {$unset: {dataSetConfigurations: 0, userConfiguration: 0}, $set: {"configuration.settings.brightnessContrastSettings": {default: {brightness: 0, contrast: 1}, "st08x2": {brightness: -90, contrast: 2.4}, "07x2": {brightness: -70, contrast: 2.4}}, "configuration.settings.fourBit": false, "configuration.settings.quality": 0, "configuration.settings.interpolation": false}}, {multi: true})
+

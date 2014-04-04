e9e47ee97e113d074c10f87d4af592976c7ade12
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2014-04-04 12:52:01 +0200

    fixed user activities

--- app/models/user/User.scala
+++ app/models/user/User.scala
@@ -200,8 +200,8 @@ object UserDAO extends SecuredBaseDAO[User] {
     update(findByIdQ(_user), Json.obj("$unset" -> Json.obj(s"experiences.$domain" -> 1)))
   }
 
-  def logActivity(user: User, lastActivity: Long)(implicit ctx: DBAccessContext) = {
-    update(findByIdQ(user._id), Json.obj("$set" -> Json.obj("lastActivity" -> lastActivity)))
+  def logActivity(_user: BSONObjectID, lastActivity: Long)(implicit ctx: DBAccessContext) = {
+    update(findByIdQ(_user), Json.obj("$set" -> Json.obj("lastActivity" -> lastActivity)))
   }
 
   def updateTeams(_user: BSONObjectID, teams: List[TeamMembership])(implicit ctx: DBAccessContext) = {

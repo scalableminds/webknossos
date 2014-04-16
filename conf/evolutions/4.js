a9165f68d70362485d5b966dfe24b8f4def3b9cd
Johannes Frohnhofen <johannes@scm.io>
2014-04-16 16:44:22 +0200

    Changed evolution #292

--- conf/evolutions/4.js
+++ conf/evolutions/4.js
@@ -9,5 +9,4 @@ db.users.update({}, {$unset: {"configuration": 0, "userConfiguration.configurati
 
 // --- !Downs
 db.users.update({}, {$rename: {"userConfiguration.configuration": "configuration.settings"}}, {multi: true})
-db.users.update({}, {$unset: {dataSetConfigurations: 0, userConfiguration: 0}, $set: {"configuration.settings.brightnessContrastSettings": {default: {brightness: 0, contrast: 1}, "st08x2": {brightness: -90, contrast: 2.4}, "07x2": {brightness: -70, contrast: 2.4}}, "configuration.settings.fourBit": false, "configuration.settings.quality": 0, "configuration.settings.interpolation": false}}, {multi: true})
-
+db.users.update({}, {$unset: {dataSetConfigurations: 0, userConfiguration: 0}, $set: {"configuration.settings.brightnessContrastSettings": {"default": {brightness: 0, contrast: 1}, "st08x2": {brightness: 0, contrast: 2.4}, "07x2": {brightness: 0, contrast: 2.4}, "Cortex": {brightness: 55, contrast: 1}}, "configuration.settings.fourBit": false, "configuration.settings.quality": 0, "configuration.settings.interpolation": false}}, {multi: true})

1a10e0ecade60a8b34d198bef2f462f3634b7b92
Johannes Frohnhofen <johannes@frohnhofen.com>
2014-12-31 16:28:25 +0100

    work in progress, #506

--- src/main/scala/com/scalableminds/braingames/binary/repository/KnossosDataSourceType.scala
+++ src/main/scala/com/scalableminds/braingames/binary/repository/KnossosDataSourceType.scala
@@ -8,6 +8,7 @@ import java.nio.file.{Files, Path}
 import com.scalableminds.braingames.binary.models._
 import com.scalableminds.util.geometry.{Scale, BoundingBox}
 import com.scalableminds.util.tools.ProgressTracking.ProgressTracker
+import com.scalableminds.util.tools.JsonHelper
 import com.scalableminds.util.io.PathUtils
 import net.liftweb.common.{Empty, Box, Full}
 import org.apache.commons.io.FilenameUtils
@@ -17,7 +18,7 @@ object KnossosDataSourceType extends DataSourceType with KnossosDataSourceTypeHa
   val name = "knossos"
 
   def mappingsDirectory = "mapping"
-  def mappingFileExtension = "map"
+  def mappingFileExtension = "json"
   def fileExtension = "raw"
 }
 
@@ -71,9 +72,11 @@ trait KnossosDataSourceTypeHandler extends DataSourceTypeHandler {
       .toList
       .filter(_.toString.toLowerCase.endsWith(s".${KnossosDataSourceType.mappingFileExtension}"))
       .map {
-        mapping =>
-          DataLayerMapping(FilenameUtils.getBaseName(mapping.toString), mapping.toString)
-      })
+        mappingFile =>
+          JsonHelper.JsonFromFile(mappingFile).flatMap( _.validate(DataLayerMapping.dataLayerMappingFormat).asOpt)
+      }
+      .flatten
+    )
   }
 
   protected def extractLayers(path: Path, dataSourcePath: String) = {

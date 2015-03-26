cc5d1d7694245375ad302842b40c8617ecd6bf50
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2015-03-26 10:46:14 +0100

    changed email template for changing password. fixed #547

--- app/oxalis/mail/DefaultMails.scala
+++ app/oxalis/mail/DefaultMails.scala
@@ -60,8 +60,8 @@ object DefaultMails {
   def changePasswordMail(name: String, receiver: String) = {
     Mail(
       from = defaultFrom,
-      subject = "Your Oxalis password was changed. If this was not you, please get in contact with your admin immediately.",
-      bodyText = html.mail.validated(name).body,
+      subject = "Your Oxalis password was changed",
+      bodyText = html.mail.passwordChanged(name).body,
       recipients = List(receiver))
   }
 }

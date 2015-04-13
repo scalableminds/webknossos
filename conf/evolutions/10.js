c8abaf64f8996e2116af519c278a6c856b4cb96a
Johannes Frohnhofen <johannes@scm.io>
2015-04-13 19:48:12 +0200

    update userDataLayers in evolution, #506

--- conf/evolutions/10.js
+++ conf/evolutions/10.js
@@ -2,6 +2,8 @@
 
 // --- !Ups
 db.dataSets.update({}, {"$unset" : {"dataSource" : ""}}, {"multi" : true})
+db.userDataLayers.update({}, {"$set": {"dataLayer.mappings": []}}, {"multi" : true})
 
 // --- !Downs
 db.dataSets.update({}, {"$unset" : {"dataSource" : ""}}, {"multi" : true})
+db.userDataLayers.update({}, {"$unset": {"dataLayer.mappings": ""}}, {"multi" : true})

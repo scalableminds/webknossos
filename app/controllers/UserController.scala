078b9d7fd58bb9509257f737e7191c7fea9010b8
Johannes Frohnhofen <johannes@scm.io>
2015-08-11 09:52:48 +0200

    Fixed UserController after merge

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -163,11 +163,9 @@ object UserController extends Controller with Secured with Dashboard with FoxImp
           _ <- ensureProperTeamAdministration(user, assignedTeams.zip(teams))
         } yield {
           val trimmedExperiences = experiences.map{ case (key, value) => key.trim -> value}
-
           val teamsWithoutUpdate = allTeams.filterNot{t =>
             issuingUser.adminTeamNames.contains(t.name) || assignedTeams.find(_.team == t.name).isDefined
           }.map(_.name)
-          val trimmedExperiences = experiences.map{ case (key, value) => key.trim -> value}.toMap
           val updatedTeams = assignedTeams ++ user.teams.filter(tm => teamsWithoutUpdate.contains(tm.team))
           UserService.update(user, firstName, lastName, verified, updatedTeams, trimmedExperiences)
           Ok

e5baa378b282f998493bb3d3ea3b0ad4aa9d7963
Johannes Frohnhofen <johannes@frohnhofen.com>
2015-10-07 02:18:08 +0200

    added html upload to master

--- app/com/scalableminds/datastore/controllers/DataSourceController.scala
+++ app/com/scalableminds/datastore/controllers/DataSourceController.scala
@@ -5,18 +5,32 @@ package com.scalableminds.datastore.controllers
 
 import net.liftweb.common.Failure
 import play.api.Logger
-import play.api.mvc.Action
+import com.scalableminds.util.tools._
 import com.scalableminds.util.mvc.ExtendedController
 import com.scalableminds.util.tools.{NotStarted, Finished, InProgress, ProgressState}
-import play.api.libs.json.Json
+import play.api.libs.json.{JsError, JsSuccess, Json, JsValue, JsString}
 import com.scalableminds.datastore.services.{DataSourceRepository, BinaryDataService}
 import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent.Future
 import play.api.i18n.Messages
 import com.scalableminds.datastore.DataStorePlugin
 import com.scalableminds.datastore.models.DataSourceDAO
+import com.scalableminds.braingames.binary.models.DataSourceUpload
+import java.io.{File, ByteArrayInputStream, FileOutputStream}
+import org.apache.commons.codec.binary.Base64
+import org.apache.commons.io.FileUtils
+import scalax.file.Path
+import java.util.zip._
+import com.scalableminds.util.io.ZipIO
+import play.api.Play
+import com.scalableminds.util.io.PathUtils
+import com.scalableminds.braingames.binary.models._
+import net.liftweb.common.{Box, Empty, Full, Failure}
 
 object DataSourceController extends Controller {
+
+  lazy val config = Play.current.configuration.underlying
+
   def progressToResult(progress: ProgressState) = progress match {
     case InProgress(p) =>
       JsonOk(Json.obj(
@@ -58,6 +72,68 @@ object DataSourceController extends Controller {
             Logger.error("An error occoured: " + f)
         }
         progressToResult(InProgress(0))
+      }
+  } 
+
+  private def unzipDataSource(baseDir: Path, content: String): Box[Unit] = {
+    val zip = new ZipInputStream(new ByteArrayInputStream(Base64.decodeBase64(content)))
+    var entry: ZipEntry = zip.getNextEntry()
+
+    if(entry == null) {
+      return Empty
+    }
+
+    while(entry != null) {
+      val path = baseDir.resolve(Path.fromString(entry.getName()))
+      if(!entry.getName().matches("[._].*")) {
+        if(entry.isDirectory) {
+          PathUtils.ensureDirectory(path)
+        } else {
+          val out = new FileOutputStream(new File(path.path))
+
+          var buffer = Array.fill(4096)(0.toByte)
+          var bytesRead = zip.read(buffer)
+          while(bytesRead != -1) {
+            out.write(buffer, 0, bytesRead)
+            bytesRead = zip.read(buffer)
+          }
+
+          out.close()
+        }
+      }
+
+      zip.closeEntry()
+      entry = zip.getNextEntry()      
+    }
+
+    zip.close()
+    Full()
+  }
+
+  def upload() = Action.async(parse.json(maxLength = 1024 * 1024 * 1024)) {
+    implicit request =>
+      request.body.validate[DataSourceUpload] match {
+        case JsSuccess(upload, _) =>
+          val baseDir = Path.fromString(config.getString("braingames.binary.baseFolder")) / upload.team / upload.name
+          (for {
+            _ <- unzipDataSource(baseDir, upload.content).toFox ?~> Messages("zip.file.invalid")
+            dataSource = DataStorePlugin.binaryDataService.dataSourceInbox.handler.dataSourceFromFolder(baseDir, upload.team)
+            importingDataSource <- DataStorePlugin.binaryDataService.dataSourceInbox.importDataSource(dataSource)
+            usableDataSource <- importingDataSource
+          } yield {
+            DataSourceDAO.updateDataSource(usableDataSource)
+            DataStorePlugin.binaryDataService.oxalisServer.reportDataSouce(usableDataSource)
+            Ok(Json.obj())
+          }).futureBox.map {
+            case Full(r) => r
+            case Empty =>
+              BadRequest(Json.obj("error" -> JsString(Messages("error.unknown"))))
+            case Failure(error,_,_) =>
+              BadRequest(Json.obj("error" -> JsString(error)))
+          }
+
+        case e: JsError =>
+          Future.successful(BadRequest(Json.obj("error" -> JsString("Json could not be parsed: " + e.toString))))
       }
   }
 }

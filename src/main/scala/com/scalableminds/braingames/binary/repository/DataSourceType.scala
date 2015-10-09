738e912d2b4469db0ea22a8babd60942919055b3
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2015-10-09 02:51:36 +0200

    improved automatic datatype detection

--- src/main/scala/com/scalableminds/braingames/binary/repository/DataSourceType.scala
+++ src/main/scala/com/scalableminds/braingames/binary/repository/DataSourceType.scala
@@ -7,6 +7,7 @@ import java.nio.file.Path
 import com.scalableminds.braingames.binary.models.{UnusableDataSource, DataSource}
 import com.scalableminds.util.io.PathUtils
 import com.scalableminds.util.tools.ProgressTracking.ProgressTracker
+import play.api.Logger
 
 trait DataSourceTypeHandler {
   def importDataSource(unusableDataSource: UnusableDataSource, progressTracker: ProgressTracker): Option[DataSource]
@@ -17,18 +18,8 @@ trait DataSourceTypeGuesser {
 
   def fileExtension: String
 
-  private def lazyFileFinder(source: Path, excludeDirs: Seq[String]): Stream[Path] = {
-    def tail = {
-      PathUtils.listDirectories(source).filterNot( path => excludeDirs.contains(path.getFileName.toString)).foldLeft(Stream.empty[Path]){
-        case (stream, path) =>
-          stream ++ PathUtils.listFiles(path) ++ lazyFileFinder(path, excludeDirs)
-      }
-    }
-    PathUtils.listFiles(source).toStream ++ tail
-  }
-
-  def chanceOfInboxType(source: Path) = {
-    lazyFileFinder(source, Seq("target"))
+  def chanceOfInboxType(filelist: Stream[Path]) = {
+    filelist
       .take(MaxNumberOfFilesForGuessing)
       .filter(_.getFileName.toString.endsWith(fileExtension))
       .size.toFloat / MaxNumberOfFilesForGuessing
@@ -36,9 +27,22 @@ trait DataSourceTypeGuesser {
 }
 
 object DataSourceTypeGuessers extends DataSourceTypes{
+  def lazyFileFinder(source: Path, excludeDirs: Seq[String]): Stream[Path] = {
+    Logger.trace(s"accessing files of $source")
+    Option(PathUtils.listFiles(source)).getOrElse(Nil)
+      .toStream #::: {
+      if (source.toFile.isDirectory && !excludeDirs.contains(source.getFileName.toString) && !source.toFile.isHidden) {
+        Logger.trace(s"accessing direc of $source")
+        PathUtils.listDirectories(source).toStream.flatMap(d => lazyFileFinder(d, excludeDirs))
+      }
+      else
+        Stream.empty
+    }
+  }
+  
   def guessRepositoryType(source: Path) = {
-    // HOTFIX: Always return knossos data type
-    KnossosDataSourceType // types.maxBy(_.chanceOfInboxType(source))
+    val paths = lazyFileFinder(source, Seq("target"))
+    types.maxBy(_.chanceOfInboxType(paths))
   }
 }
 

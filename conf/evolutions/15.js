7cdfb17c7c38eb1d4d8af5678214281d32e2bbc1
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2015-12-01 11:38:47 +0100

    added editrotation to tracing json

--- conf/evolutions/15.js
+++ conf/evolutions/15.js
@@ -2,6 +2,8 @@
 
 // --- !Ups
 db.skeletons.update({}, {"$set" : {"editRotation" : [0.0, 0.0, 1.0]}}, {"multi" : true})
+db.volumes.update({}, {"$set" : {"editRotation" : [0.0, 0.0, 1.0]}}, {"multi" : true})
 
 // --- !Downs
 db.skeletons.update({}, {"$unset" : {"editRotation" : ""}}, {"multi" : true})
+db.volumes.update({}, {"$unset" : {"editRotation" : ""}}, {"multi" : true})

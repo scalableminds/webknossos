c9ed3512b8e5677575e909db46a0a4e065ec48bf
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2015-12-07 14:46:16 +0100

    Added rotation to nodes

--- app/oxalis/nml/Node.scala
+++ app/oxalis/nml/Node.scala
@@ -1,21 +1,24 @@
 package oxalis.nml
 
-import com.scalableminds.util.geometry.Point3D
+import com.scalableminds.util.geometry.{Point3D, Vector3D}
 import play.api.libs.json._
 import com.scalableminds.util.xml.{SynchronousXMLWrites, XMLWrites}
 
-case class Node(id: Int, position: Point3D, radius: Float = 120, viewport: Int = 1, resolution: Int = 1, bitDepth: Int = 0, interpolation: Boolean = false, timestamp: Long = System.currentTimeMillis)
+case class Node(id: Int, position: Point3D, rotation: Vector3D = Node.defaultRotation, radius: Float = 120, viewport: Int = 1, resolution: Int = 1, bitDepth: Int = 0, interpolation: Boolean = false, timestamp: Long = System.currentTimeMillis)
 
 object Node {
+  val defaultRotation = Vector3D(0,0,0)
+
   implicit object NodeXMLWrites extends SynchronousXMLWrites[Node] {
     def synchronousWrites(n: Node) =
       <node id={ n.id.toString } radius={ n.radius.toString } x={ n.position.x.toString } y={ n.position.y.toString } z={ (n.position.z).toString } inVp={ n.viewport.toString } inMag={ n.resolution.toString } bitDepth={ n.bitDepth.toString } interpolation={ n.interpolation.toString } time={ n.timestamp.toString }/>
   }
- 
+
   implicit object NodeFormat extends Format[Node] {
     val ID = "id"
     val RADIUS = "radius"
     val POSITION = "position"
+    val ROTATION = "rotation"
     val VIEWPORT = "viewport"
     val RESOLUTION = "resolution"
     val TIMESTAMP = "timestamp"
@@ -27,6 +30,7 @@ object Node {
         ID -> n.id,
         RADIUS -> n.radius,
         POSITION -> n.position,
+        ROTATION -> n.rotation,
         VIEWPORT -> n.viewport,
         RESOLUTION -> n.resolution,
         BITDEPTH -> n.bitDepth,
@@ -38,6 +42,7 @@ object Node {
     def reads(js: JsValue) =
       JsSuccess(Node((js \ ID).as[Int],
         (js \ POSITION).as[Point3D],
+        (js \ ROTATION).as[Vector3D],
         (js \ RADIUS).as[Float],
         (js \ VIEWPORT).as[Int],
         (js \ RESOLUTION).as[Int],
--- conf/evolutions/15.js
+++ conf/evolutions/15.js
@@ -1,8 +1,8 @@
 // Add rotation to skeleton tracings
 
 // --- !Ups
-db.skeletons.update({}, {"$set" : {"editRotation" : [0.0, 0.0, 1.0]}}, {"multi" : true})
-db.volumes.update({}, {"$set" : {"editRotation" : [0.0, 0.0, 1.0]}}, {"multi" : true})
+db.skeletons.update({}, {"$set" : {"editRotation" : [0.0, 0.0, 0.0]}}, {"multi" : true})
+db.volumes.update({}, {"$set" : {"editRotation" : [0.0, 0.0, 0.0]}}, {"multi" : true})
 
 // --- !Downs
 db.skeletons.update({}, {"$unset" : {"editRotation" : ""}}, {"multi" : true})

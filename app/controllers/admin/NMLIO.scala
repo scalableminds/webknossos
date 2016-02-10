087bad1f13de7fe0b2c7dbfa419dcd5d0f9f02d7
Johannes Frohnhofen <johannes@scm.io>
2016-02-11 00:23:52 +0100

    small improvement, #1086

--- app/controllers/admin/NMLIO.scala
+++ app/controllers/admin/NMLIO.scala
@@ -46,12 +46,15 @@ class NMLIO @Inject()(val messagesApi: MessagesApi) extends Controller with Secu
       val nmls = parseSuccess.flatMap(_.nml).toList
 
       AnnotationService
-      .createAnnotationFrom(request.user, nmls, AnnotationType.Explorational, nameForNMLs(fileNames))
-      .map { annotation =>
-        JsonOk(
-          Json.obj("annotation" -> Json.obj("typ" -> annotation.typ, "id" -> annotation.id)),
-          Messages("nml.file.uploadSuccess")
-        )
+      .createAnnotationFrom(request.user, nmls, AnnotationType.Explorational, nameForNMLs(fileNames)).futuerBox.map{
+        case Full(annotation) =>
+          JsonOk(
+            Json.obj("annotation" -> Json.obj("typ" -> annotation.typ, "id" -> annotation.id)),
+            Messages("nml.file.uploadSuccess")
+          )
+
+        case _ =>
+          JsonBadRequest(Messages("nml.file.invalid"))
       }
     }
   }

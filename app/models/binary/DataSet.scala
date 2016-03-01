2f2a5132507c38c2685d1baf729bed13415c5ca6
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2016-03-01 22:38:28 +0100

    changed default DS rotation

--- app/models/binary/DataSet.scala
+++ app/models/binary/DataSet.scala
@@ -35,7 +35,7 @@ case class DataSet(
     dataSource.map(_.boundingBox.center).getOrElse(Point3D(0, 0, 0))
 
   def defaultRotation =
-    Vector3D(0, 0, 1)
+    Vector3D(0, 0, 0)
 }
 
 object DataSet {

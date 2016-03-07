7a298bb413d33c3e4a5d16cd9ed0531f33dcbfe3
Norman Rzepka <code@normanrzepka.de>
2016-03-07 13:56:23 +0100

    Proper mongo connection für docker installation

--- buildtools/cmd.sh
+++ buildtools/cmd.sh
@@ -7,4 +7,15 @@ PORT=${PORT:-9000}
 LOGGER_XML=${LOGGER_XML:-${INSTALL_DIR}/conf/application-logger.xml}
 MONGO_DB=${MONGO_DB:-oxalis}
 
-exec bin/oxalis -Dconfig.file=${APPLICATION_CONFIG} -Dhttp.port=${PORT} -Djava.io.tmpdir=disk -Dmongodb.db=${MONGO_DB} -Dmongodb.url=${MONGO_PORT_27017_TCP_ADDR} -Dmongodb.port=${MONGO_PORT_27017_TCP_PORT}
+echo bin/oxalis \
+  -Dconfig.file=${APPLICATION_CONFIG} -Dhttp.port=${PORT} \
+  -Djava.io.tmpdir=disk \
+  -Dmongodb.db=${MONGO_DB} -Dmongodb.url=${MONGO_PORT_27017_TCP_ADDR} -Dmongodb.port=${MONGO_PORT_27017_TCP_PORT} \
+  -Dmongodb.uri="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/${MONGO_DB}"
+
+exec bin/oxalis \
+  -Dconfig.file=${APPLICATION_CONFIG} -Dhttp.port=${PORT} \
+  -Djava.io.tmpdir=disk \
+  -Dmongodb.db=${MONGO_DB} -Dmongodb.url=${MONGO_PORT_27017_TCP_ADDR} -Dmongodb.port=${MONGO_PORT_27017_TCP_PORT} \
+  -Dmongodb.uri="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/${MONGO_DB}"
+

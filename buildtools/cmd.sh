3691946d7eb2eca5703cc27dcec4d5a622b2007e
Norman Rzepka <code@normanrzepka.de>
2016-03-07 15:10:27 +0100

    Fixed ip binding and config file selection for docker

--- buildtools/cmd.sh
+++ buildtools/cmd.sh
@@ -2,19 +2,19 @@
 set -e
 
 INSTALL_DIR=$(pwd)
-APPLICATION_CONFIG=${INSTALL_DIR}/conf/application.conf
+APPLICATION_CONFIG=${INSTALL_DIR}/conf/application_${MODE:-dev}.conf
 PORT=${PORT:-9000}
 LOGGER_XML=${LOGGER_XML:-${INSTALL_DIR}/conf/application-logger.xml}
 MONGO_DB=${MONGO_DB:-oxalis}
 
 echo bin/oxalis \
-  -Dconfig.file=${APPLICATION_CONFIG} -Dhttp.port=${PORT} \
+  -Dconfig.file=${APPLICATION_CONFIG} -Dhttp.address="0.0.0.0" -Dhttp.port=${PORT} \
   -Djava.io.tmpdir=disk \
   -Dmongodb.db=${MONGO_DB} -Dmongodb.url=${MONGO_PORT_27017_TCP_ADDR} -Dmongodb.port=${MONGO_PORT_27017_TCP_PORT} \
   -Dmongodb.uri="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/${MONGO_DB}"
 
 exec bin/oxalis \
-  -Dconfig.file=${APPLICATION_CONFIG} -Dhttp.port=${PORT} \
+  -Dconfig.file=${APPLICATION_CONFIG} -Dhttp.address="0.0.0.0" -Dhttp.port=${PORT} \
   -Djava.io.tmpdir=disk \
   -Dmongodb.db=${MONGO_DB} -Dmongodb.url=${MONGO_PORT_27017_TCP_ADDR} -Dmongodb.port=${MONGO_PORT_27017_TCP_PORT} \
   -Dmongodb.uri="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/${MONGO_DB}"

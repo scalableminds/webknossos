4b50f796d8dc92c9763306af4dafc846dbbec11f
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2016-05-02 01:40:21 +0200

    updated resolvers to use s3 buckets

--- project/Build.scala
+++ project/Build.scala
@@ -47,9 +47,8 @@ object Resolvers {
   val sonaSnaps = "sonatype snaps" at "https://oss.sonatype.org/content/repositories/snapshots/"
   val sgSnaps = "sgodbillon" at "https://bitbucket.org/sgodbillon/repository/raw/master/snapshots/"
   val typesafeRel = "typesafe" at "http://repo.typesafe.com/typesafe/releases"
-  val scmRel = Resolver.url("Scalableminds REL Repo", url("http://scalableminds.github.com/releases/"))(Resolver.ivyStylePatterns)
-  val scmIntRel = "scm.io intern releases repo" at "http://maven.scm.io/releases/"
-  val scmIntSnaps = "scm.io intern snapshots repo" at "http://maven.scm.io/snapshots/"
+  val scmRel = "scm.io releases S3 bucket" at "https://s3-eu-central-1.amazonaws.com/maven.scm.io/releases/"
+  val scmSnaps = "scm.io snapshots S3 bucket" at "https://s3-eu-central-1.amazonaws.com/maven.scm.io/snapshots/"
   val teamon = "teamon.eu repo" at "http://repo.teamon.eu"
 }
 
@@ -163,8 +162,7 @@ object ApplicationBuild extends Build {
     sgSnaps,
     typesafeRel,
     scmRel,
-    scmIntRel,
-    scmIntSnaps,
+    scmSnaps,
     teamon
   )
 

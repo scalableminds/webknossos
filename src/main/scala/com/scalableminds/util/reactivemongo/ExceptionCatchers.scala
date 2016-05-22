20db6605c0ac8ab7dec2b3e0e9aaaab959579b0e
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2016-05-22 16:37:58 +0200

    improved db error msg creation on failed database result parsing

--- src/main/scala/com/scalableminds/util/reactivemongo/ExceptionCatchers.scala
+++ src/main/scala/com/scalableminds/util/reactivemongo/ExceptionCatchers.scala
@@ -11,16 +11,18 @@ import play.api.libs.concurrent.Execution.Implicits._
 import com.scalableminds.util.tools.{FoxImplicits, Fox}
 
 trait ExceptionCatchers extends DBInteractionLogger with FoxImplicits {
+  def collectionName: String
+
   def dbExceptionHandler: PartialFunction[Throwable, Failure] = {
     case e: Exception =>
-      logger.error("A DB exception occoured.", e)
+      logger.error(s"A DB exception occoured while querying '$collectionName': ", e)
       Failure(e.getMessage, Some(e), Empty)
   }
 
   def withFailureHandler(e: => Future[WriteResult]): Fox[WriteResult] =
     e.map {
       case LastError(false, errMsg, code, _, _, err, _, _, _, _, _, _) =>
-        Failure(s"$errMsg ($err)", Full(GenericDatabaseException(errMsg orElse err getOrElse "", code)), Empty)
+        Failure(s"Error in '$collectionName': $errMsg ($err)", Full(GenericDatabaseException(errMsg orElse err getOrElse "", code)), Empty)
       case lastError =>
         Full(lastError)
     }.recover(dbExceptionHandler)

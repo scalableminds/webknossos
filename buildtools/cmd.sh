29b502cbdfebaa6a1d0f7b8bda1205da49d80ff1
jstriebel <jstriebel@users.noreply.github.com>
2016-06-17 14:26:45 +0200

    oxalis standalone docker image: adapting cmd to compose v2 networkin #1247

--- buildtools/cmd.sh
+++ buildtools/cmd.sh
@@ -10,12 +10,12 @@ MONGO_DB=${MONGO_DB:-oxalis}
 echo bin/oxalis \
   -Dconfig.file=${APPLICATION_CONFIG} -Dhttp.address="0.0.0.0" -Dhttp.port=${PORT} \
   -Djava.io.tmpdir=disk \
-  -Dmongodb.db=${MONGO_DB} -Dmongodb.url=${MONGO_PORT_27017_TCP_ADDR} -Dmongodb.port=${MONGO_PORT_27017_TCP_PORT} \
+  -Dmongodb.db=${MONGO_DB} -Dmongodb.url=mongo -Dmongodb.port=27017 \
   -Dmongodb.uri="mongodb://mongo:27017/${MONGO_DB}"
 
 exec bin/oxalis \
   -Dconfig.file=${APPLICATION_CONFIG} -Dhttp.address="0.0.0.0" -Dhttp.port=${PORT} \
   -Djava.io.tmpdir=disk \
-  -Dmongodb.db=${MONGO_DB} -Dmongodb.url=${MONGO_PORT_27017_TCP_ADDR} -Dmongodb.port=${MONGO_PORT_27017_TCP_PORT} \
+  -Dmongodb.db=${MONGO_DB} -Dmongodb.url=mongo -Dmongodb.port=27017 \
   -Dmongodb.uri="mongodb://mongo:27017/${MONGO_DB}"
 

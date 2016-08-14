9a968f95e06463fe82f411da844eadbd78b02abc
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2016-08-14 13:29:49 +0200

    replaced actors with futures and proper locks

---
+++ src/main/scala/com/scalableminds/util/cache/LRUConcurrentCache.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) Tom Bocklisch <https://github.com/tmbo>
+ */
+package com.scalableminds.util.cache
+
+class LRUConcurrentCache[K, V](maxEntries: Int) {
+  private val cache = new java.util.LinkedHashMap[K, V]() {
+    override def removeEldestEntry(eldest: java.util.Map.Entry[K, V]): Boolean = size > maxEntries
+  }
+
+  def put(key: K, value: V) {
+    cache.synchronized {
+      cache.put(key, value)
+    }
+  }
+
+  def get(key: K): Option[V] = {
+    cache.synchronized {
+      Option(cache.get(key))
+    }
+  }
+
+  def clear(): Unit = {
+    cache.clear()
+  }
+}

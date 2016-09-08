f3c7c93f9f884d259e225c16603d3fc7d0582dad
Norman Rzepka <code@normanrzepka.de>
2016-09-08 13:54:14 +0200

    fixed create_play_root_env.sh

--- buildtools/create_play_root_env.sh
+++ buildtools/create_play_root_env.sh
@@ -9,13 +9,10 @@ BRANCH=$(<${WORKSPACE}/.git/REAL_BRANCH)
 NAME=${PROJECT}-${BRANCH}
 
 APP_DIR=${APP_DIR_PREFIX}/target/universal/stage
-ROOT_ENV="rootenv"
-INSTALL_DIR="/usr/lib/${NAME}"
+export ROOT_ENV="rootenv"
+export INSTALL_DIR="/usr/lib/${NAME}"
 
 chmod +x $APP_DIR/bin/${PROJECT}
 
 mkdir -p ${ROOT_ENV}${INSTALL_DIR}
 cp -r ${APP_DIR}/* ${ROOT_ENV}${INSTALL_DIR}
-
-export ROOT_ENV
-export INSTALL_DIR

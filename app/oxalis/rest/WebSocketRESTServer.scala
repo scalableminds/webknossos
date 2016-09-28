6ce3ce0eedc889b7e59f5cd696b697e6960bedbf
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2016-09-28 13:39:39 +0200

    removed unecessary logging

--- app/oxalis/rest/WebSocketRESTServer.scala
+++ app/oxalis/rest/WebSocketRESTServer.scala
@@ -72,8 +72,10 @@ case class WebSocketRESTServer(out: Channel[Array[Byte]]) extends FoxImplicits{
       val json = Json.parse(rawJson)
       json.validate[RESTResponse] match {
         case JsSuccess(response, _) =>
-          if(response.status != Status.OK.toString)
-            Logger.warn(s"Failed (Code: ${response.status})  REST call to '${response.path}'(${response.uuid}). Result: '${response.body.toString().take(500)}'")
+          if(response.status != Status.OK.toString) {
+            val log = if(response.status != Status.NOT_FOUND.toString) Logger.warn(_) else Logger.debug(_)
+            log(s"Failed (Code: ${response.status})  REST call to '${response.path}'(${response.uuid}). Result: '${response.body.toString().take(500)}'")
+          }
           openCalls().get(response.uuid).foreach {
             promise =>
               promise.trySuccess(Full(response)) match {

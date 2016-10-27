a88ec903d824142df834dfa504267337924bc318
Norman Rzepka <code@normanrzepka.de>
2016-10-27 10:54:56 +0200

    verbose infrastructure test results

--- test/infrastructure/deployment.bash
+++ test/infrastructure/deployment.bash
@@ -2,4 +2,4 @@
 
 source "$(dirname "$0")/helper.bash"
 
-run_test "curl localhost:9000" curl -m 10 --retry 20 --retry-delay 5 http://localhost:9000
+run_test "curl localhost:9000" curl -m 10 --retry 20 --retry-delay 5 -v http://localhost:9000

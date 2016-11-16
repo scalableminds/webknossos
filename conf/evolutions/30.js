076216ef68bc3a5a89dde25e036c65923d6bc9e6
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2016-11-16 16:45:25 +0100

    zipped volume annotations on disc

---
+++ conf/evolutions/30.js
@@ -0,0 +1,15 @@
+// --- !Ups
+db.userDataLayers.update({}, {$set: {"dataLayer._isCompressed": true}}, {"multi": true});
+
+db.dataSets.find().forEach(function (elem) {
+  if(elem.dataSource) {
+    var dataLayers = elem.dataSource.dataLayers;
+    for (var i = 0; i < dataLayers.length; i++) {
+      dataLayers[i]["_isCompressed"] = false;
+    }
+    db.dataSets.update({"_id": elem._id}, {"$set": {"dataSource.dataLayers": dataLayers}});
+  }
+});
+
+// --- !Downs
+

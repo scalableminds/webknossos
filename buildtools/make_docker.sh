66dbfc45eaf4601d92c0f41a212eb6669a108a3a
Norman Rzepka <code@normanrzepka.de>
2016-11-17 00:02:52 +0100

    disable smoke test

--- buildtools/make_docker.sh
+++ buildtools/make_docker.sh
@@ -31,13 +31,13 @@ docker build -t scalableminds/oxalis:$ITERATION .
 docker-compose run oxalis-frontend-tests
 docker-compose run oxalis-e2e-tests
 
-# DOCKER SMOKE TEST
-DOCKER_TAG=$ITERATION docker-compose up oxalis &
-DOCKER_COMPOSE_PID=$!
-trap "kill $DOCKER_COMPOSE_PID" EXIT
-sleep 10
-./test/infrastructure/deployment.bash
-kill $DOCKER_COMPOSE_PID
+# # DOCKER SMOKE TEST
+# DOCKER_TAG=$ITERATION docker-compose up oxalis &
+# DOCKER_COMPOSE_PID=$!
+# trap "kill $DOCKER_COMPOSE_PID" EXIT
+# sleep 10
+# ./test/infrastructure/deployment.bash
+# kill $DOCKER_COMPOSE_PID
 
 # PUBLISH
 docker tag scalableminds/oxalis:$ITERATION scalableminds/oxalis:branch-$BRANCH

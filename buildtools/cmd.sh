a37981e71ffe4fe6f2752ba43a4d08e69d0c4dff
Norman Rzepka <code@normanrzepka.de>
2016-12-07 20:50:57 +0100

    fixed cmd.sh

--- buildtools/cmd.sh
+++ buildtools/cmd.sh
@@ -7,20 +7,20 @@ PORT=${PORT:-9000}
 LOGGER_XML=${LOGGER_XML:-${INSTALL_DIR}/conf/application-logger.xml}
 MONGO_DB=${MONGO_DB:-oxalis}
 
-ARGS="\
-  -Dconfig.file=${APPLICATION_CONFIG} -Dhttp.address=\"0.0.0.0\" -Dhttp.port=${PORT} \
+ARGS=(\
+  -Dconfig.file=${APPLICATION_CONFIG} -Dhttp.address="0.0.0.0" -Dhttp.port=${PORT} \
   -Djava.io.tmpdir=disk \
   -Dmongodb.db=${MONGO_DB} -Dmongodb.url=mongo -Dmongodb.port=27017 \
-  -Dmongodb.uri=\"mongodb://mongo:27017/${MONGO_DB}\""
+  -Dmongodb.uri="mongodb://mongo:27017/${MONGO_DB}")
 
 if [[ "$NEW_RELIC_LICENSE_KEY" ]]; then
   NEWRELIC_AGENT_JAR=$(ls ${INSTALL_DIR}/lib | grep com.newrelic.agent.java.newrelic-agent)
-  ARGS="${ARGS}\
+  ARGS+=(\
     -J-javaagent:${INSTALL_DIR}/lib/${NEWRELIC_AGENT_JAR} \
-    -J-Dnewrelic.config.file=${INSTALL_DIR}/conf/newrelic.yml"
+    -J-Dnewrelic.config.file=${INSTALL_DIR}/conf/newrelic.yml)
 fi
 
-echo bin/oxalis ${ARGS}
+echo bin/oxalis ${ARGS[@]}
 
-exec bin/oxalis ${ARGS}
+exec bin/oxalis ${ARGS[@]}
 

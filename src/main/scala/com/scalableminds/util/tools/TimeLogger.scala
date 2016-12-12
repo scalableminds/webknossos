12bb9cf41cac30f6170f0aec3495c80360f84d75
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2016-12-12 13:51:16 +0100

    changed signature of time logger

--- src/main/scala/com/scalableminds/util/tools/TimeLogger.scala
+++ src/main/scala/com/scalableminds/util/tools/TimeLogger.scala
@@ -6,14 +6,14 @@ package com.scalableminds.util.tools
 import scala.concurrent.{ExecutionContext, Future}
 
 object TimeLogger {
-  def logTime[A](caption: String, log: ( => String) => Unit)(op: => A): A = {
+  def logTime[A](caption: String, log: String => Unit)(op: => A): A = {
     val t = System.currentTimeMillis()
     val result = op
     log(s"TIMELOG | $caption took ${System.currentTimeMillis-t} ms")
     result
   }
 
-  def logTimeF[A](caption: String, log: ( => String) => Unit)(op: => Fox[A])(implicit ec: ExecutionContext): Fox[A] = {
+  def logTimeF[A](caption: String, log: String => Unit)(op: => Fox[A])(implicit ec: ExecutionContext): Fox[A] = {
     val t = System.currentTimeMillis()
     val result = op
     result.futureBox.onComplete(_ => log(s"TIMELOG | $caption took ${System.currentTimeMillis - t} ms"))

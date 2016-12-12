36ceebb5bec69b422ead518654e2961a30754041
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2016-12-12 11:57:20 +0100

    updated libs

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import play.sbt.routes.RoutesKeys._
 
 object Dependencies{
-  val braingamesVersion = "8.17.1"
+  val braingamesVersion = "8.17.2"
 
   val braingamesDataStore = "com.scalableminds" %% "braingames-datastore" % braingamesVersion
 

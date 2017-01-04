990697d21d6a39d9fe4bf198dcb59b0853b64035
Norman Rzepka <code@normanrzepka.de>
2017-01-04 15:30:47 +0100

    webpack2 support WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 /* eslint no-var:0 */
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var fs = require("fs");
+var path = require("path");
 
-var srcPath = __dirname + "/app/assets/javascripts/";
+var srcPath = path.resolve(__dirname, "app/assets/javascripts/");
 var nodePath = __dirname + "/node_modules/";
 var scriptPaths = {
-  "gzip"                  : nodePath + "zlibjs/bin/gzip.min",
   "three"                 : nodePath + "three.js/build/three",
   "three.color"           : nodePath + "three.js/examples/js/math/ColorConverter",
   "three.trackball"       : nodePath + "three.js/examples/js/controls/TrackballControls",
@@ -14,10 +14,9 @@ var scriptPaths = {
   "bootstrap-multiselect" : nodePath + "bootstrap-multiselect/dist/js/bootstrap-multiselect",
 };
 
-
 module.exports = {
   entry: {
-    main: srcPath + "main.js",
+    main: "main.js",
   },
   output: {
     path:              __dirname + "/public/bundle",
@@ -25,11 +24,6 @@ module.exports = {
     sourceMapFilename: "[file].map",
     publicPath:        "/assets/bundle/"
   },
-
-  resolveLoader: {
-    modulesDirectories: [nodePath]
-  },
-
   module: {
     // Reduce compilation time by telling webpack to not parse these libraries.
     // Only add libraries that have no dependencies eg. no require, define or similar calls.
@@ -37,58 +31,78 @@ module.exports = {
       /lodash/,
       /\/jquery\//,
     ],
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react'],
-        }
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [
+            ["env", {
+              "targets": {
+                "browsers": [
+                  "last 2 Chrome versions",
+                  "last 2 Firefox versions",
+                  "last 2 Edge versions",
+                  "last 1 Safari versions",
+                  "last 1 iOS versions",
+                ],
+              },
+            }],
+            "react",
+          ],
+        },
+      }
+    }, {
+      test: scriptPaths["three.color"],
+      use: [{
+        loader: "imports-loader",
+        options: { "THREE": "three" },
+      }, {
+        loader: "exports-loader",
+        options: { "THREE.ColorConverter": "THREE.ColorConverter" },
+      }],
+    }, {
+      test: scriptPaths["three.trackball"],
+      use: { loader: "imports-loader", options: { "THREE": "three" } },
+    }, {
+      test: scriptPaths["three"],
+      use: { loader: "exports-loader", options: { "THREE": "THREE" } },
+    },
+    { test: /\.css$/, use: "css-loader" },
+    { test: /\.less$/, use: ["style-loader", "css-loader", "less-loader"] },
+    {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          mimetype: "application/font-woff",
+        },
       },
-      { test: scriptPaths["three.color"], loader: "imports?THREE=three!exports?THREE.ColorConverter" },
-      { test: scriptPaths["three.trackball"], loader: "imports?THREE=three" },
-      { test: scriptPaths["three"], loader: "exports?THREE" },
-      { test: scriptPaths["gzip"], loader: "exports?this.Zlib" },
-      {
-        test: /\.less$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader'),
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&minetype=application/font-woff',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
-      },
-      { test: /\.png$/, loader: "url-loader?limit=100000" },
-      { test: /\.jpg$/, loader: "file-loader" },
-      // {
-      //   test: /\.jsx?$/,
-      //   exclude: /(node_modules|bower_components)/,
-      //   loader: 'babel'
-      // }
-    ]
-  },
+    }, {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: "file-loader",
+    },
+    { test: /\.png$/, use: { loader: "url-loader", options: { limit: 100000 } } },
+    { test: /\.jpg$/, use: "file-loader" },
+  ]},
   resolve: {
-    root: srcPath,
+    modules: [
+      srcPath,
+      nodePath,
+    ],
     alias: scriptPaths,
-    extensions: ['', '.js', '.json', '.coffee', '.cjsx'],
-    modulesDirectories: [nodePath],
   },
   externals: [
     { "routes": "var jsRoutes" }
   ],
-  // devtool: "source-map",
-  // debug: true,
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
+      "process.env": {
         NODE_ENV: process.env.NODE_ENV ? JSON.stringify(process.env.NODE_ENV) : undefined,
       },
     }),
-    new ExtractTextPlugin('main.css'),
     new webpack.ProvidePlugin({
       $ : "jquery",
       jQuery : "jquery",

90645f714263f32573ece560276b89175fc4be09
Norman Rzepka <norman.rzepka@scalableminds.com>
2017-01-12 12:31:36 +0100

    Added title to GithubMilestone
    As per https://developer.github.com/v3/issues/

--- src/main/scala/com/scalableminds/util/github/models/GithubMilestone.scala
+++ src/main/scala/com/scalableminds/util/github/models/GithubMilestone.scala
@@ -5,7 +5,7 @@ import play.api.libs.json.Json
 /**
   * Created by tmbo on 10.01.17.
   */
-case class GithubMilestone(description: String, id: Int, number: Int)
+case class GithubMilestone(title: String, description: String, id: Int, number: Int)
 
 object GithubMilestone{
   implicit val githubMilestoneFormat = Json.format[GithubMilestone]

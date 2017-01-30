55edb9252fbc1927357c8e3f6ec3dc7e8380cce0
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2017-01-30 23:49:33 +0100

    Improved code style

--- src/main/scala/com/scalableminds/braingames/binary/requester/handlers/KnossosBlockHandler.scala
+++ src/main/scala/com/scalableminds/braingames/binary/requester/handlers/KnossosBlockHandler.scala
@@ -6,12 +6,10 @@ package com.scalableminds.braingames.binary.requester.handlers
 import java.io.RandomAccessFile
 import java.util.concurrent.TimeoutException
 
-import akka.remote.transport.ActorTransportAdapter.AssociateUnderlying
 import com.scalableminds.braingames.binary.models._
-import com.scalableminds.braingames.binary.requester.{CachedBlock, Cube, DataCache, DataCubeCache}
+import com.scalableminds.braingames.binary.requester.{Cube, DataCubeCache}
 import com.scalableminds.braingames.binary.store._
-import com.scalableminds.util.cache.LRUConcurrentCache
-import com.scalableminds.util.geometry.Point3D
+import com.scalableminds.util.tools.ExtendedTypes.ExtendedRandomAccessFile
 import com.scalableminds.util.tools.{Fox, FoxImplicits}
 import com.typesafe.scalalogging.LazyLogging
 import net.liftweb.common.Failure
@@ -20,26 +18,21 @@ import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent._
 import scala.concurrent.duration.FiniteDuration
 
-class KnossosCube(file: RandomAccessFile) extends Cube with LazyLogging{
+class KnossosCube(file: RandomAccessFile) extends Cube with LazyLogging {
   override def copyTo(offset: Long, other: Array[Byte], destPos: Int, length: Int): Boolean = {
     var numReadBytes = 0
     val buffer = new Array[Byte](length)
-    file.synchronized{
+    file.synchronized {
       file.seek(offset)
       numReadBytes = file.read(buffer)
     }
-    if(numReadBytes == length) {
+    if (numReadBytes == length) {
       System.arraycopy(buffer, 0, other, destPos, length)
       true
     } else {
-      val method = file.getClass.getDeclaredField("closed")
-      method.setAccessible(true)
-      val isClosed = method.getBoolean(file)
-
-      val method2 = file.getClass.getDeclaredField("path")
-      method2.setAccessible(true)
-      val path = method2.get(file).asInstanceOf[String]
-      logger.error(s"Failed to read data! Expected length '$length' got '$numReadBytes'. File: ${file.length()} Closed? ${isClosed} Offset: $offset Path: $path") // TODO: add more usefull information
+      logger.error(s"Failed to read data! Expected length '$length' " +
+        s"got '$numReadBytes'. File: ${file.length} " +
+        s"Closed? ${file.isClosed} Offset: $offset Path: ${file.getPath}")
       false
     }
   }
@@ -52,8 +45,8 @@ class KnossosCube(file: RandomAccessFile) extends Cube with LazyLogging{
 
 class KnossosBlockHandler(val cache: DataCubeCache)
   extends BlockHandler
-  with FoxImplicits
-  with LazyLogging {
+    with FoxImplicits
+    with LazyLogging {
 
   lazy val dataStore = new FileDataStore
 
@@ -64,7 +57,7 @@ class KnossosBlockHandler(val cache: DataCubeCache)
           .futureBox
           .map {
             case f: Failure =>
-              f.exception.map( e => logger.warn("Load from store failed: " + f.msg, e))
+              f.exception.map(e => logger.warn("Load from store failed: " + f.msg, e))
               f
             case x =>
               x.map(data => f(new KnossosCube(data)))
@@ -89,7 +82,7 @@ class KnossosBlockHandler(val cache: DataCubeCache)
     }.recover {
       case _: TimeoutException | _: InterruptedException =>
         logger.warn(s"No response in time for block during save: " +
-          s"(${saveBlock.dataSource.id }/${saveBlock.dataLayerSection.baseDir } ${saveBlock.block })")
+          s"(${saveBlock.dataSource.id}/${saveBlock.dataLayerSection.baseDir} ${saveBlock.block})")
         Failure("dataStore.save.timeout")
     }
   }

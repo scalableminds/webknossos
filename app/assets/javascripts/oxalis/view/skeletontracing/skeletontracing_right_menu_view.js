1f6682ef3a118017c3597fcbee231517f1eb86e4
Johannes Frohnhofen <johannes@frohnhofen.com>
2017-02-03 22:49:02 +0100

    flow type checking for list_tree_view.js

--- app/assets/javascripts/oxalis/view/skeletontracing/skeletontracing_right_menu_view.js
+++ app/assets/javascripts/oxalis/view/skeletontracing/skeletontracing_right_menu_view.js
@@ -3,7 +3,7 @@
  * @flow weak
  */
 
- import AbstractTabView from "../abstract_tab_view";
+import AbstractTabView from "../abstract_tab_view";
 import CommentTabView from "./right-menu/comment_tab/comment_tab_view";
 import AbstractTreeView from "./right-menu/abstract_tree_view";
 import ListTreeView from "./right-menu/list_tree_view";

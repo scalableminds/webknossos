5d218ec5ac48084005da39017432838956f5beb9
jonathan.striebel <jstriebel@users.noreply.github.com>
2017-02-04 14:44:45 +0100

    clean db-files (#1666)
    * removed dummy-db
    
    * move testdb
    
    * clean dummy-db usage

--- dummy-db/exportTrees.sh
+++
@@ -1,5 +0,0 @@
-mongoexport --db play-oxalis --collection nodes --out nodes.json
-mongoexport --db play-oxalis --collection edges --out edges.json
-mongoexport --db play-oxalis --collection annotations --out annotations.json
-mongoexport --db play-oxalis --collection skeletons --out skeletons.json
-mongoexport --db play-oxalis --collection trees --out trees.json

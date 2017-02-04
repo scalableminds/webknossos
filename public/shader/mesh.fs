53433a52faef647098b3e90cac792dfaa0806cce
Tom Herold <tom.herold@scalableminds.com>
2017-02-04 16:11:29 +0100

    removed unused shader files

--- public/shader/mesh.fs
+++
@@ -1,9 +0,0 @@
-#ifdef GL_ES                 
-precision mediump float;     
-#endif                       
-                          
-varying vec4 frontColor;   
-varying vec3 lightWeighting;
-void main(void){                
-	gl_FragColor = vec4(frontColor.rgb * lightWeighting, frontColor.a);    
-}

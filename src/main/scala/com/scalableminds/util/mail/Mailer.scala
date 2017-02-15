b8658ff0b2da39256438716aaee20b8969a7e2c1
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2017-02-15 20:08:03 +0100

    don't send email if empty

--- src/main/scala/com/scalableminds/util/mail/Mailer.scala
+++ src/main/scala/com/scalableminds/util/mail/Mailer.scala
@@ -46,7 +46,7 @@ class Mailer(conf: Config) extends Actor {
    * @return
    */
   def send(mail: Mail) = {
-    if (enabled) {
+    if (enabled && mail.recipients.exists(_.trim != "")) {
       // Content type
       val contentType = mail.contentType getOrElse guessContentType(mail)
 

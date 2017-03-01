739a9828ca5339fc802ba4147dbdd7e611a54607
Norman Rzepka <code@normanrzepka.de>
2017-03-01 01:00:28 +0100

    removes clipping distance from volume mode

--- app/assets/javascripts/oxalis/view/settings/user_settings_view.js
+++ app/assets/javascripts/oxalis/view/settings/user_settings_view.js
@@ -94,27 +94,36 @@ class UserSettingsView extends Component {
 
   getViewportOptions = () => {
     const mode = this.props.oldModel.get("mode");
-    if (mode === Constants.MODE_PLANE_TRACING || mode === Constants.MODE_VOLUME) {
-      return (
-        <Panel header="Viewport Options" key="2">
-          <LogSliderSetting label="Zoom" min={0.1} max={this.props.oldModel.get("flycam").getMaxZoomStep()} value={this.props.userConfiguration.zoom} onChange={_.partial(this.props.onChangeUser, "zoom")} />
-          <NumberSliderSetting label="Viewport Scale" min={0.05} max={20} step={0.1} value={this.props.userConfiguration.scale} onChange={_.partial(this.props.onChangeUser, "scale")} />
-          <NumberSliderSetting label="Clipping Distance" max={12000} value={this.props.userConfiguration.clippingDistance} onChange={_.partial(this.props.onChangeUser, "clippingDistance")} />
-          <SwitchSetting label="Show Crosshairs" value={this.props.userConfiguration.displayCrosshair} onChange={_.partial(this.props.onChangeUser, "displayCrosshair")} />
-        </Panel>
-      );
-    } else {
-      return (
-        <Panel header="Flight Options" key="2">
-          <NumberSliderSetting label="Mouse Rotation" min={0.0001} max={0.02} step={0.001} value={this.props.userConfiguration.mouseRotateValue} onChange={_.partial(this.props.onChangeUser, "mouseRotateValue")} />
-          <NumberSliderSetting label="Keyboard Rotation Value" min={0.001} max={0.08} step={0.001} value={this.state.activeNodeId} onChange={this.onChangeActiveNodeId} />
-          <NumberSliderSetting label="Move Value (nm/s)" min={30} max={1500} step={10} value={this.props.userConfiguration.moveValue3d} onChange={_.partial(this.props.onChangeUser, "moveValue3d")} />
-          <NumberSliderSetting label="Crosshair Size" min={0.05} max={0.5} step={0.01} value={this.props.userConfiguration.crosshairSize} onChange={_.partial(this.props.onChangeUser, "crosshairSize")} />
-          <NumberSliderSetting label="Sphere Radius" min={50} max={500} step={1} value={this.props.userConfiguration.sphericalCapRadius} onChange={_.partial(this.props.onChangeUser, "sphericalCapRadius")} />
-          <NumberSliderSetting label="Clipping Distance" max={127} value={this.props.userConfiguration.clippingDistanceArbitrary} onChange={_.partial(this.props.onChangeUser, "clippingDistanceArbitrary")} />
-          <SwitchSetting label="Show Crosshair" value={this.props.userConfiguration.displayCrosshair} onChange={_.partial(this.props.onChangeUser, "displayCrosshair")} />
-        </Panel>
-      );
+    switch (mode) {
+      case Constants.MODE_PLANE_TRACING:
+        return (
+          <Panel header="Viewport Options" key="2">
+            <LogSliderSetting label="Zoom" min={0.1} max={this.props.oldModel.get("flycam").getMaxZoomStep()} value={this.props.userConfiguration.zoom} onChange={_.partial(this.props.onChangeUser, "zoom")} />
+            <NumberSliderSetting label="Viewport Scale" min={0.05} max={20} step={0.1} value={this.props.userConfiguration.scale} onChange={_.partial(this.props.onChangeUser, "scale")} />
+            <NumberSliderSetting label="Clipping Distance" max={12000} value={this.props.userConfiguration.clippingDistance} onChange={_.partial(this.props.onChangeUser, "clippingDistance")} />
+            <SwitchSetting label="Show Crosshairs" value={this.props.userConfiguration.displayCrosshair} onChange={_.partial(this.props.onChangeUser, "displayCrosshair")} />
+          </Panel>
+        );
+      case Constants.MODE_VOLUME:
+        return (
+          <Panel header="Viewport Options" key="2">
+            <LogSliderSetting label="Zoom" min={0.1} max={this.props.oldModel.get("flycam").getMaxZoomStep()} value={this.props.userConfiguration.zoom} onChange={_.partial(this.props.onChangeUser, "zoom")} />
+            <NumberSliderSetting label="Viewport Scale" min={0.05} max={20} step={0.1} value={this.props.userConfiguration.scale} onChange={_.partial(this.props.onChangeUser, "scale")} />
+            <SwitchSetting label="Show Crosshairs" value={this.props.userConfiguration.displayCrosshair} onChange={_.partial(this.props.onChangeUser, "displayCrosshair")} />
+          </Panel>
+        );
+      default:
+        return (
+          <Panel header="Flight Options" key="2">
+            <NumberSliderSetting label="Mouse Rotation" min={0.0001} max={0.02} step={0.001} value={this.props.userConfiguration.mouseRotateValue} onChange={_.partial(this.props.onChangeUser, "mouseRotateValue")} />
+            <NumberSliderSetting label="Keyboard Rotation Value" min={0.001} max={0.08} step={0.001} value={this.state.activeNodeId} onChange={this.onChangeActiveNodeId} />
+            <NumberSliderSetting label="Move Value (nm/s)" min={30} max={1500} step={10} value={this.props.userConfiguration.moveValue3d} onChange={_.partial(this.props.onChangeUser, "moveValue3d")} />
+            <NumberSliderSetting label="Crosshair Size" min={0.05} max={0.5} step={0.01} value={this.props.userConfiguration.crosshairSize} onChange={_.partial(this.props.onChangeUser, "crosshairSize")} />
+            <NumberSliderSetting label="Sphere Radius" min={50} max={500} step={1} value={this.props.userConfiguration.sphericalCapRadius} onChange={_.partial(this.props.onChangeUser, "sphericalCapRadius")} />
+            <NumberSliderSetting label="Clipping Distance" max={127} value={this.props.userConfiguration.clippingDistanceArbitrary} onChange={_.partial(this.props.onChangeUser, "clippingDistanceArbitrary")} />
+            <SwitchSetting label="Show Crosshair" value={this.props.userConfiguration.displayCrosshair} onChange={_.partial(this.props.onChangeUser, "displayCrosshair")} />
+          </Panel>
+        );
     }
   }
 

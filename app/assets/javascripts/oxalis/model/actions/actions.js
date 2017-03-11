4df7aba3647a00cb7094127a54f11b8cdea3797a
Norman Rzepka <code@normanrzepka.de>
2017-03-11 16:06:27 +0100

    renames SettingActionTypes to SettingActionType

--- app/assets/javascripts/oxalis/model/actions/actions.js
+++ app/assets/javascripts/oxalis/model/actions/actions.js
@@ -2,13 +2,13 @@
 
 // @flow
 import type { SkeletonTracingActionTypes } from "oxalis/model/actions/skeletontracing_actions";
-import type { SettingActionTypes } from "oxalis/model/actions/settings_actions";
+import type { SettingActionType } from "oxalis/model/actions/settings_actions";
 import type { TaskActionTypes } from "oxalis/model/actions/task_actions";
 import type { SaveActionTypes } from "oxalis/model/actions/save_actions";
 
 export type ActionType =
   SkeletonTracingActionTypes |
-  SettingActionTypes |
+  SettingActionType |
   TaskActionTypes |
   SaveActionTypes;
 

7ce024c09a0162113048b1a4746474bfdd39b55d
Tom Herold <tom.herold@scalableminds.com>
2017-03-15 17:13:06 +0100

    cache settings on change handler #1709

--- app/assets/javascripts/oxalis/view/settings/user_settings_view.js
+++ app/assets/javascripts/oxalis/view/settings/user_settings_view.js
@@ -93,29 +93,29 @@ class UserSettingsView extends Component {
       case Constants.MODE_PLANE_TRACING:
         return (
           <Panel header="Viewport Options" key="2">
-            <LogSliderSetting label="Zoom" min={0.1} max={getMaxZoomStep(this.props.state)} value={this.props.userConfiguration.zoom} onChange={_.partial(this.props.onChangeUser, "zoom")} />
-            <NumberSliderSetting label="Viewport Scale" min={0.05} max={20} step={0.1} value={this.props.userConfiguration.scale} onChange={_.partial(this.props.onChangeUser, "scale")} />
-            <NumberSliderSetting label="Clipping Distance" max={12000} value={this.props.userConfiguration.clippingDistance} onChange={_.partial(this.props.onChangeUser, "clippingDistance")} />
-            <SwitchSetting label="Show Crosshairs" value={this.props.userConfiguration.displayCrosshair} onChange={_.partial(this.props.onChangeUser, "displayCrosshair")} />
+            <LogSliderSetting label="Zoom" min={0.1} max={getMaxZoomStep(this.props.state)} value={this.props.userConfiguration.zoom} onChange={this.props.onChangeUser["zoom"]} />
+            <NumberSliderSetting label="Viewport Scale" min={0.05} max={20} step={0.1} value={this.props.userConfiguration.scale} onChange={this.props.onChangeUser["scale"]} />
+            <NumberSliderSetting label="Clipping Distance" max={12000} value={this.props.userConfiguration.clippingDistance} onChange={this.props.onChangeUser["clippingDistance"]} />
+            <SwitchSetting label="Show Crosshairs" value={this.props.userConfiguration.displayCrosshair} onChange={this.props.onChangeUser["displayCrosshair"]} />
           </Panel>
         );
       case Constants.MODE_VOLUME:
         return (
           <Panel header="Viewport Options" key="2">
-            <LogSliderSetting label="Zoom" min={0.1} max={getMaxZoomStep(this.props.state)} value={this.props.userConfiguration.zoom} onChange={_.partial(this.props.onChangeUser, "zoom")} />
-            <NumberSliderSetting label="Viewport Scale" min={0.05} max={20} step={0.1} value={this.props.userConfiguration.scale} onChange={_.partial(this.props.onChangeUser, "scale")} />
-            <SwitchSetting label="Show Crosshairs" value={this.props.userConfiguration.displayCrosshair} onChange={_.partial(this.props.onChangeUser, "displayCrosshair")} />
+            <LogSliderSetting label="Zoom" min={0.1} max={getMaxZoomStep(this.props.state)} value={this.props.userConfiguration.zoom} onChange={this.props.onChangeUser["zoom"]} />
+            <NumberSliderSetting label="Viewport Scale" min={0.05} max={20} step={0.1} value={this.props.userConfiguration.scale} onChange={this.props.onChangeUser["scale"]} />
+            <SwitchSetting label="Show Crosshairs" value={this.props.userConfiguration.displayCrosshair} onChange={this.props.onChangeUser["displayCrosshair"]} />
           </Panel>
         );
       default:
         return (
           <Panel header="Flight Options" key="2">
-            <NumberSliderSetting label="Mouse Rotation" min={0.0001} max={0.02} step={0.001} value={this.props.userConfiguration.mouseRotateValue} onChange={_.partial(this.props.onChangeUser, "mouseRotateValue")} />
-            <NumberSliderSetting label="Keyboard Rotation" min={0.001} max={0.08} step={0.001} value={this.props.userConfiguration.rotateValue} onChange={_.partial(this.props.onChangeUser, "rotateValue")} />
-            <NumberSliderSetting label="Crosshair Size" min={0.05} max={0.5} step={0.01} value={this.props.userConfiguration.crosshairSize} onChange={_.partial(this.props.onChangeUser, "crosshairSize")} />
-            <NumberSliderSetting label="Sphere Radius" min={50} max={500} step={1} value={this.props.userConfiguration.sphericalCapRadius} onChange={_.partial(this.props.onChangeUser, "sphericalCapRadius")} />
-            <NumberSliderSetting label="Clipping Distance" max={127} value={this.props.userConfiguration.clippingDistanceArbitrary} onChange={_.partial(this.props.onChangeUser, "clippingDistanceArbitrary")} />
-            <SwitchSetting label="Show Crosshair" value={this.props.userConfiguration.displayCrosshair} onChange={_.partial(this.props.onChangeUser, "displayCrosshair")} />
+            <NumberSliderSetting label="Mouse Rotation" min={0.0001} max={0.02} step={0.001} value={this.props.userConfiguration.mouseRotateValue} onChange={this.props.onChangeUser["mouseRotateValue"]} />
+            <NumberSliderSetting label="Keyboard Rotation" min={0.001} max={0.08} step={0.001} value={this.props.userConfiguration.rotateValue} onChange={this.props.onChangeUser["rotateValue"]} />
+            <NumberSliderSetting label="Crosshair Size" min={0.05} max={0.5} step={0.01} value={this.props.userConfiguration.crosshairSize} onChange={this.props.onChangeUser["crosshairSize"]} />
+            <NumberSliderSetting label="Sphere Radius" min={50} max={500} step={1} value={this.props.userConfiguration.sphericalCapRadius} onChange={this.props.onChangeUser["sphericalCapRadius"]} />
+            <NumberSliderSetting label="Clipping Distance" max={127} value={this.props.userConfiguration.clippingDistanceArbitrary} onChange={this.props.onChangeUser["clippingDistanceArbitrary"]} />
+            <SwitchSetting label="Show Crosshair" value={this.props.userConfiguration.displayCrosshair} onChange={this.props.onChangeUser["displayCrosshair"]} />
           </Panel>
         );
     }
@@ -131,18 +131,18 @@ class UserSettingsView extends Component {
           <NumberInputSetting label="Active Node ID" value={activeNodeId} onChange={this.props.onChangeActiveNodeId} />
           <NumberInputSetting label="Active Tree ID" value={this.props.skeletonTracing.activeTreeId} onChange={this.props.onChangeActiveTreeId} />
           <NumberSliderSetting label="Radius" max={5000} value={this.props.userConfiguration.radius} onChange={this.props.onChangeRadius} />
-          <NumberSliderSetting label="Particle Size" max={20} step={0.1} value={this.props.userConfiguration.particleSize} onChange={_.partial(this.props.onChangeUser, "particleSize")} />
-          <SwitchSetting label="Soma Clicking" value={this.props.userConfiguration.newNodeNewTree} onChange={_.partial(this.props.onChangeUser, "newNodeNewTree")} />
-          <SwitchSetting label="Override Radius" value={this.props.userConfiguration.overrideNodeRadius} onChange={_.partial(this.props.onChangeUser, "overrideNodeRadius")} />
+          <NumberSliderSetting label="Particle Size" max={20} step={0.1} value={this.props.userConfiguration.particleSize} onChange={this.props.onChangeUser["particleSize"]} />
+          <SwitchSetting label="Soma Clicking" value={this.props.userConfiguration.newNodeNewTree} onChange={this.props.onChangeUser["newNodeNewTree"]} />
+          <SwitchSetting label="Override Radius" value={this.props.userConfiguration.overrideNodeRadius} onChange={this.props.onChangeUser["overrideNodeRadius"]} />
         </Panel>
       );
     } else if (mode === Constants.MODE_VOLUME && !this.props.isPublicViewMode) {
       return (
         <Panel header="Volume Options" key="3">
           <NumberInputSetting label="Active Cell ID" value={this.state.activeCellId} onChange={this.onChangeActiveCellId} />
-          <SwitchSetting label="3D Volume Rendering" value={this.props.userConfiguration.isosurfaceDisplay} onChange={_.partial(this.props.onChangeUser, "isosurfaceDisplay")} />
-          <NumberSliderSetting label="3D Rendering Bounding Box Size" min={1} max={10} step={0.1} value={this.props.userConfiguration.isosurfaceBBsize} onChange={_.partial(this.props.onChangeUser, "isosurfaceBBsize")} />
-          <NumberSliderSetting label="3D Rendering Resolution" min={40} max={400} value={this.props.userConfiguration.isosurfaceResolution} onChange={_.partial(this.props.onChangeUser, "isosurfaceResolution")} />
+          <SwitchSetting label="3D Volume Rendering" value={this.props.userConfiguration.isosurfaceDisplay} onChange={this.props.onChangeUser["isosurfaceDisplay"]} />
+          <NumberSliderSetting label="3D Rendering Bounding Box Size" min={1} max={10} step={0.1} value={this.props.userConfiguration.isosurfaceBBsize} onChange={this.props.onChangeUser["isosurfaceBBsize"]} />
+          <NumberSliderSetting label="3D Rendering Resolution" min={40} max={400} value={this.props.userConfiguration.isosurfaceResolution} onChange={this.props.onChangeUser["isosurfaceResolution"]} />
         </Panel>
       );
     }
@@ -152,26 +152,26 @@ class UserSettingsView extends Component {
   render() {
     const mode = this.props.oldModel.get("mode");
     const moveValueSetting = Constants.MODES_ARBITRARY.includes(mode) ?
-      <NumberSliderSetting label="Move Value (nm/s)" min={30} max={1500} step={10} value={this.props.userConfiguration.moveValue3d} onChange={_.partial(this.props.onChangeUser, "moveValue3d")} /> :
-      <NumberSliderSetting label="Move Value (nm/s)" min={30} max={14000} step={10} value={this.props.userConfiguration.moveValue} onChange={_.partial(this.props.onChangeUser, "moveValue")} />;
+      <NumberSliderSetting label="Move Value (nm/s)" min={30} max={1500} step={10} value={this.props.userConfiguration.moveValue3d} onChange={this.props.onChangeUser["moveValue3d"]} /> :
+      <NumberSliderSetting label="Move Value (nm/s)" min={30} max={14000} step={10} value={this.props.userConfiguration.moveValue} onChange={this.props.onChangeUser["moveValue"]} />;
 
     return (
       <Collapse defaultActiveKey={["1", "2", "3", "4", "5"]}>
         <Panel header="Controls" key="1">
-          <NumberSliderSetting label="Keyboard delay (ms)" min={0} max={500} value={this.props.userConfiguration.keyboardDelay} onChange={_.partial(this.props.onChangeUser, "keyboardDelay")} />
+          <NumberSliderSetting label="Keyboard delay (ms)" min={0} max={500} value={this.props.userConfiguration.keyboardDelay} onChange={this.props.onChangeUser["keyboardDelay"]} />
           {moveValueSetting}
-          <SwitchSetting label="Inverse X" value={this.props.userConfiguration.inverseX} onChange={_.partial(this.props.onChangeUser, "inverseX")} />
-          <SwitchSetting label="Inverse Y" value={this.props.userConfiguration.inverseY} onChange={_.partial(this.props.onChangeUser, "inverseY")} />
-          <SwitchSetting label="d/f-Switching" value={this.props.userConfiguration.dynamicSpaceDirection} onChange={_.partial(this.props.onChangeUser, "dynamicSpaceDirection")} />
+          <SwitchSetting label="Inverse X" value={this.props.userConfiguration.inverseX} onChange={this.props.onChangeUser["inverseX"]} />
+          <SwitchSetting label="Inverse Y" value={this.props.userConfiguration.inverseY} onChange={this.props.onChangeUser["inverseY"]} />
+          <SwitchSetting label="d/f-Switching" value={this.props.userConfiguration.dynamicSpaceDirection} onChange={this.props.onChangeUser["dynamicSpaceDirection"]} />
         </Panel>
         { this.getViewportOptions() }
         { this.getSkeletonOrVolumeOptions() }
         <Panel header="Segmentation" key="4">
-          <NumberSliderSetting label="Segmentation Opacity" min={0} max={100} value={this.props.userConfiguration.segmentationOpacity} onChange={_.partial(this.props.onChangeUser, "segmentationOpacity")} />
+          <NumberSliderSetting label="Segmentation Opacity" min={0} max={100} value={this.props.userConfiguration.segmentationOpacity} onChange={this.props.onChangeUser["segmentationOpacity"]} />
         </Panel>
         <Panel header="Other" key="5">
           <Vector6InputSetting label="Bounding Box" tooltipTitle="Format: minX, minY, minZ, width, height, depth" value={this.props.temporaryConfiguration.boundingBox} onChange={this.onChangeBoundingBox} />
-          <SwitchSetting label="Display Planes in 3D View" value={this.props.userConfiguration.tdViewDisplayPlanes} onChange={_.partial(this.props.onChangeUser, "tdViewDisplayPlanes")} />
+          <SwitchSetting label="Display Planes in 3D View" value={this.props.userConfiguration.tdViewDisplayPlanes} onChange={this.props.onChangeUser["tdViewDisplayPlanes"]} />
         </Panel>
       </Collapse>
     );
@@ -186,7 +186,7 @@ const mapStateToProps = (state: OxalisState) => ({
 });
 
 const mapDispatchToProps = (dispatch: Dispatch<*>) => ({
-  onChangeUser(propertyName, value) { dispatch(updateUserSettingAction(propertyName, value)); },
+  dispatch,
   onChangeTemporary(propertyName, value) { dispatch(updateTemporarySettingAction(propertyName, value)); },
   onChangeActiveNodeId(id: number) { dispatch(setActiveNodeAction(id)); },
   onChangeActiveTreeId(id: number) { dispatch(setActiveTreeAction(id)); },
@@ -196,4 +196,16 @@ const mapDispatchToProps = (dispatch: Dispatch<*>) => ({
   },
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserSettingsView);
+const mergeProps = (stateProps, dispatchProps, ownProps) => Object.assign(
+  {},
+  ownProps,
+  stateProps,
+  dispatchProps,
+  {
+    onChangeUser: _.mapValues(stateProps.userConfiguration, (__, propertyName) =>
+      value => dispatchProps.dispatch(updateUserSettingAction(propertyName, value)),
+    ),
+  },
+);
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(UserSettingsView);

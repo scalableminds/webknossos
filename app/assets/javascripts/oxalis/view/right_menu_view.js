9d41c092f23c007ed9d899595ac73d9f91ce5fd8
Tom Herold <tom.herold@scalableminds.com>
2017-03-21 21:37:15 +0100

    switch right side menu tabs based on tracing mode #1709

--- app/assets/javascripts/oxalis/view/right_menu_view.js
+++ app/assets/javascripts/oxalis/view/right_menu_view.js
@@ -11,21 +11,43 @@ import AbstractTreeTabView from "oxalis/view/skeletontracing/right-menu/abstract
 import TreesTabView from "oxalis/view/skeletontracing/right-menu/trees_tab_view";
 import DatasetInfoTabView from "oxalis/view/viewmode/right-menu/dataset_info_tab_view";
 import MappingInfoView from "oxalis/view/volumetracing/right-menu/mapping_info_view";
+import Constants from "oxalis/constants";
 
 const TabPane = Tabs.TabPane;
 
 type RightMenuViewProps = {
   oldModel: Model,
+  isPublicViewMode: boolean,
 };
 
-export default function RightMenuView({ oldModel }: RightMenuViewProps) {
-  return (
-    <Tabs destroyInactiveTabPane defaultActiveKey="1">
-      <TabPane tab="Info" key="1"><DatasetInfoTabView oldModel={oldModel} /></TabPane>
-      <TabPane tab="Tree Viewer" key="2"><AbstractTreeTabView /></TabPane>
-      <TabPane tab="Trees" key="3"><TreesTabView /></TabPane>
-      <TabPane tab="Comments" key="4"><CommentTabView /></TabPane>
-      <TabPane tab="Mappings" key="5"><MappingInfoView oldModel={oldModel} /></TabPane>
-    </Tabs>
-  );
+class RightMenuView extends React.PureComponent {
+
+  props: RightMenuViewProps;
+
+  getTabs() {
+    if (!this.props.isPublicViewMode) {
+      if (this.props.oldModel.get("mode") in Constants.MODES_SKELETON) {
+        return [
+          <TabPane tab="Tree Viewer" key="2"><AbstractTreeTabView /></TabPane>,
+          <TabPane tab="Trees" key="3"><TreesTabView /></TabPane>,
+          <TabPane tab="Comments" key="4"><CommentTabView /></TabPane>,
+        ];
+      } else {
+        return <TabPane tab="Mappings" key="5"><MappingInfoView oldModel={this.props.oldModel} /></TabPane>;
+      }
+    }
+
+    return null;
+  }
+
+  render() {
+    return (
+      <Tabs destroyInactiveTabPane defaultActiveKey="1">
+        <TabPane tab="Info" key="1"><DatasetInfoTabView oldModel={this.props.oldModel} /></TabPane>
+        { this.getTabs() }
+      </Tabs>
+    );
+  }
 }
+
+export default RightMenuView;

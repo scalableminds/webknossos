174cad9c1f5a46f8cff8345c46d397af7536b769
Tom Herold <tom.herold@scalableminds.com>
2017-03-23 22:51:24 +0100

    removed unused comment #1709

--- app/assets/javascripts/oxalis/view/skeletontracing/skeletontracing_view.js
+++ app/assets/javascripts/oxalis/view/skeletontracing/skeletontracing_view.js
@@ -3,53 +3,11 @@
  * @flow weak
  */
 
-import _ from "lodash";
-import Backbone from "backbone";
-// import app from "app";
-// import Toast from "libs/toast";
 import modal from "oxalis/view/modal";
 import View from "oxalis/view";
 
 class SkeletonTracingView extends View {
   // Consider for deletion
-
-  // Copied from backbone events (TODO: handle this better)
-  listenTo: Function;
-
-  constructor(model) {
-    super(model);
-    _.extend(this, Backbone.Events);
-
-//     const autoSaveFailureMessage = "Auto-Save failed!";
-//     this.listenTo(this.model.skeletonTracing.stateLogger, "pushFailed", function () {
-//       if (this.reloadDenied) {
-//         Toast.error(autoSaveFailureMessage, true);
-//       } else {
-//         modal.show(
-//           "Several attempts to reach our server have failed. You should \
-// reload the page to make sure that your work won't be lost.",
-//           "Connection error",
-//           [
-//             {
-//               id: "reload-button",
-//               label: "OK, reload",
-//               callback() {
-//                 app.router.off("beforeunload");
-//                 app.router.reload();
-//               },
-//             },
-//             {
-//               id: "cancel-button",
-//               label: "Cancel",
-//               callback: () => { this.reloadDenied = true; },
-//             },
-//           ],
-//         );
-//       }
-//     });
-//     this.listenTo(this.model.skeletonTracing.stateLogger, "pushDone", () => Toast.delete("danger", autoSaveFailureMessage));
-  }
-
 
   showFirstVisToggle() {
     modal.show("You just toggled the skeleton visibility. To toggle back, just hit the 1-Key.",

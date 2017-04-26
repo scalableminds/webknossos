564afa536e9803d5652bb483e0f296bd160d9618
Daniel Werner <daniel.werner@student.hpi.uni-potsdam.de>
2017-04-26 21:55:37 +0200

    apply PR feedback

--- app/assets/javascripts/oxalis/model/helpers/reduce_reducers.js
+++ app/assets/javascripts/oxalis/model/helpers/reduce_reducers.js
@@ -3,12 +3,13 @@
  * @flow
  */
 
+import _ from "lodash";
 import deepFreezeLib from "deep-freeze";
 
 // Do not use the deep-freeze library in production
 // process.env.NODE_ENV is being substituted by webpack
 let deepFreeze = deepFreezeLib;
-if (process.env.NODE_ENV === "production") deepFreeze = a => a;
+if (process.env.NODE_ENV === "production") deepFreeze = _.identity;
 
 export default function reduceReducers(...reducers: Array<Function>): Function {
   return (previous, current) =>

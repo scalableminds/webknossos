2eb8c1ce64ae77073f6c2886f621c8195074d57b
Daniel Werner <daniel.werner@student.hpi.uni-potsdam.de>
2017-04-26 21:25:58 +0200

    replace seamless-immutable with deep-freeze, store subscribe notifies only if the state changed, cache tracing diffing between saving and rendering

--- app/assets/javascripts/oxalis/model/helpers/reduce_reducers.js
+++ app/assets/javascripts/oxalis/model/helpers/reduce_reducers.js
@@ -3,12 +3,17 @@
  * @flow
  */
 
-import Immutable from "seamless-immutable";
+import deepFreezeLib from "deep-freeze";
+
+// Do not use the deep-freeze library in production
+// process.env.NODE_ENV is being substituted by webpack
+let deepFreeze = deepFreezeLib;
+if (process.env.NODE_ENV === "production") deepFreeze = a => a;
 
 export default function reduceReducers(...reducers: Array<Function>): Function {
   return (previous, current) =>
     reducers.reduce(
-      (p, r) => Immutable(r(p, current)),
-      Immutable(previous),
+      (p, r) => deepFreeze(r(p, current)),
+      deepFreeze(previous),
     );
 }

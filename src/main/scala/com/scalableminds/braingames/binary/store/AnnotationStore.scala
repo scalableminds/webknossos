6268557aca66e9da28f5cbf9defbf2407fdfe9e0
Johannes Frohnhofen <johannes@scm.io>
2017-06-22 19:26:37 +0200

    Removed old stuff to get things to compile

--- src/main/scala/com/scalableminds/braingames/binary/store/AnnotationStore.scala
+++
@@ -1,8 +0,0 @@
-
-package com.scalableminds.braingames.binary.store
-
-import com.scalableminds.braingames.binary.store.kvstore.VersionedKeyValueStore
-
-trait AnnotationStore {
-  def volumeStore: VersionedKeyValueStore
-}

abf7e6535f351e6c6bd64a75b23129d1088d665b
Johannes Frohnhofen <johannes@scm.io>
2017-06-23 13:29:37 +0200

    Created RPC abstraction

--- app/com/scalableminds/braingames/datastore/services/WebKnossosServer.scala
+++ app/com/scalableminds/braingames/datastore/services/WebKnossosServer.scala
@@ -3,27 +3,18 @@
  */
 package com.scalableminds.braingames.datastore.services
 
-import java.util.concurrent.atomic.AtomicInteger
-
 import akka.actor.ActorSystem
 import com.google.inject.Inject
 import com.google.inject.name.Named
-import com.scalableminds.braingames.binary.models.datasource.inbox.InboxDataSourceLike
-import com.scalableminds.braingames.binary.helpers.IntervalScheduler
+import com.scalableminds.braingames.binary.helpers.{IntervalScheduler, RPC}
 import com.scalableminds.braingames.binary.models.datasource.DataSourceId
-import com.scalableminds.util.tools.{Fox, FoxImplicits}
-import com.typesafe.scalalogging.LazyLogging
+import com.scalableminds.braingames.binary.models.datasource.inbox.InboxDataSourceLike
+import com.scalableminds.util.tools.Fox
 import play.api.Configuration
-import play.api.Play.current
-import play.api.http.HeaderNames
 import play.api.i18n.MessagesApi
 import play.api.inject.ApplicationLifecycle
-import play.api.libs.json.{Json, Writes}
-import play.api.libs.ws.{WS, WSResponse}
-import play.api.test.Helpers._
+import play.api.libs.json.Json
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 import scala.concurrent.duration._
 
 case class DataStoreStatus(ok: Boolean, url: String)
@@ -37,65 +28,39 @@ class WebKnossosServer @Inject()(
                                   val lifecycle: ApplicationLifecycle,
                                   @Named("braingames-binary") val system: ActorSystem,
                                   val messagesApi: MessagesApi
-                                ) extends IntervalScheduler with FoxImplicits with LazyLogging {
+                                ) extends IntervalScheduler {
+
+  private val dataStoreKey: String = config.getString("datastore.key").getOrElse("something-secure")
+  private val dataStoreName: String = config.getString("datastore.name").getOrElse("local-datastore")
+  private val dataStoreUrl: String = config.getString("http.uri").getOrElse("http://localhost:9000")
+
+  private val webKnossosUrl = {
+    val url = config.getString("datastore.oxalis.uri").getOrElse("localhost:9000")
+    config.getBoolean("datastore.oxalis.secured") match {
+      case Some(true) => s"https://$url"
+      case _ => s"http://$url"
+    }
+  }
 
   protected lazy val tickerInterval: FiniteDuration = config.getInt("datastore.oxalis.pingIntervalMinutes").getOrElse(10).minutes
 
-  private val url = config.getString("datastore.oxalis.uri").getOrElse("localhost:9000")
-  private val secured = config.getBoolean("datastore.oxalis.secured").getOrElse(false)
-  private val webKnossosUrl = s"${if (secured) "https://" else "http://"}$url"
-  private val dataStoreKey = config.getString("datastore.key").getOrElse("something-secure")
-  private val dataStoreName = config.getString("datastore.name").getOrElse("local-datastore")
-  private val dataStoreUrl = config.getString("http.uri").getOrElse("http://localhost:9000")
-
-  private val requestCounter: AtomicInteger = new AtomicInteger()
-
-  def reportStatus(ok: Boolean): Future[Any] = {
-    webKnossos(s"/api/datastores/$dataStoreName/status", DataStoreStatus(ok, dataStoreUrl))
-  }
-
-  def reportDataSources(dataSources: List[InboxDataSourceLike]): Future[Any] = {
-    webKnossos(s"/api/datastores/$dataStoreName/datasources", dataSources)
-  }
-
-  def reportDataSouce(dataSource: InboxDataSourceLike): Future[Any] = {
-    webKnossos(s"/api/datastores/$dataStoreName/datasource", dataSource)
-  }
-
-  def validateDataSourceUpload(token: String, id: DataSourceId): Fox[Any] = {
-    // TODO
-    Future.successful()
-  }
-
   def tick: Unit = reportStatus(ok = true)
 
-  private def webKnossos[T](path: String, body: T)(implicit wrt: Writes[T]): Future[WSResponse] = {
-    val jsonBody = Json.toJson(body)
-    val requestId = requestCounter.getAndIncrement()
-
-    logger.debug(s"Sending WS request to webKnossos (ID: $requestId). " +
-      s"Path '$path'. Body: '${jsonBody.toString.take(100)}'")
-
-    val request = WS
-      .url(s"$webKnossosUrl$path")
+  def reportStatus(ok: Boolean): Fox[_] = {
+    RPC(s"$webKnossosUrl/api/datastores/$dataStoreName/status")
       .withQueryString("key" -> dataStoreKey)
-      .withHeaders(HeaderNames.CONTENT_TYPE -> "application/json")
-      .post(jsonBody)
-      .map { response =>
-        if (response.status == OK) {
-          logger.debug(s"Successful request (ID: $requestId). " +
-            s"Body: '${response.body.take(100)}'")
-        } else {
-          logger.warn(s"Failed to send WS request to webKnossos (ID: $requestId). " +
-            s"Path: '$path'. RequestBody: '${jsonBody.toString.take(100)}'. " +
-            s"Status ${response.status}. ResponseBody: '${response.body.take(100)}'")
-        }
-        response
-      }
-    request.recover { case er =>
-      logger.error(s"Error sending WS request to webknossos (ID: $requestId): " +
-        s"${er.getMessage}\n${er.getStackTrace.mkString("\n    ")}")
-    }
-    request
+      .post(DataStoreStatus(ok, dataStoreUrl))
+  }
+
+  def reportDataSources(dataSources: List[InboxDataSourceLike]): Fox[_] = {
+    RPC(s"$webKnossosUrl/api/datastores/$dataStoreName/datasources")
+      .withQueryString("key" -> dataStoreKey)
+      .post(dataSources)
+  }
+
+  def validateDataSourceUpload(token: String, id: DataSourceId): Fox[_] = {
+    RPC(s"$webKnossosUrl/api/datastores/$dataStoreName/datasource")
+      .withQueryString("key" -> dataStoreKey)
+      .get
   }
 }

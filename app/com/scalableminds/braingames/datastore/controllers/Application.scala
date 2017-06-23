9061301b736938d9ab3927b5dc8750168b204b77
Johannes Frohnhofen <johannes@scm.io>
2017-06-23 13:28:26 +0200

    Small enhancements

--- app/com/scalableminds/braingames/datastore/controllers/Application.scala
+++ app/com/scalableminds/braingames/datastore/controllers/Application.scala
@@ -10,9 +10,10 @@ import play.api.mvc.Action
 
 class Application @Inject()(val messagesApi: MessagesApi) extends Controller {
 
-  def health = Action { implicit request =>
-    // This is a simple health endpoint that will always return 200 if the server is correctly running
-    // we might do some more advanced checks of the server in the future
-    Ok
+  def health = Action {
+    implicit request =>
+      // This is a simple health endpoint that will always return 200 if the server is correctly running
+      // we might do some more advanced checks of the server in the future
+      Ok
   }
 }

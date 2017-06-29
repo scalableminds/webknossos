55b1f949581502ba3bd32a7f54c059589c2f0520
Johannes Frohnhofen <johannes@scm.io>
2017-06-29 11:07:24 +0200

    Adding route to backup tracingDataStore

--- app/com/scalableminds/braingames/datastore/controllers/Application.scala
+++ app/com/scalableminds/braingames/datastore/controllers/Application.scala
@@ -3,17 +3,39 @@
 */
 package com.scalableminds.braingames.datastore.controllers
 
+import java.nio.file.Paths
 import javax.inject.Inject
 
+import com.google.inject.name.Named
+import com.scalableminds.braingames.binary.store.kvstore.VersionedKeyValueStore
+import play.api.Configuration
 import play.api.i18n.MessagesApi
+import play.api.libs.json.Json
 import play.api.mvc.Action
 
-class Application @Inject()(val messagesApi: MessagesApi) extends Controller {
+class Application @Inject()(
+                             @Named("tracing-data-store") tracingDataStore: VersionedKeyValueStore,
+                             config: Configuration,
+                             val messagesApi: MessagesApi
+                           ) extends Controller {
+
+  val backupFolder = Paths.get(config.getString("braingames.binary.backupFolder").getOrElse("backup"))
 
   def health = Action {
     implicit request =>
       // This is a simple health endpoint that will always return 200 if the server is correctly running
       // we might do some more advanced checks of the server in the future
       Ok
+  }
+
+  def backup = Action {
+    implicit request =>
+      val start = System.currentTimeMillis()
+      logger.info(s"Starting tracing data backup to '${backupFolder.toAbsolutePath}'")
+      tracingDataStore.backup(backupFolder).map { backupInfo =>
+        val duration = System.currentTimeMillis() - start
+        logger.info(s"Finished tracing data backup after $duration ms")
+        Ok(Json.toJson(backupInfo))
+      }
   }
 }

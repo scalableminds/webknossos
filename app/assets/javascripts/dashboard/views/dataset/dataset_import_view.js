71eac3213bd11b61a2bd773c2f2162cabd737b89
Tom Herold <tom.herold@scalableminds.com>
2017-07-12 16:36:53 +0200

    finish dataset_import_view #1896

--- app/assets/javascripts/dashboard/views/dataset/dataset_import_view.js
+++ app/assets/javascripts/dashboard/views/dataset/dataset_import_view.js
@@ -1,6 +1,5 @@
 import React from "react";
-import JsonEdit from 'react-jsoneditor'
-import { Button, Spin } from "antd";
+import { Button, Spin, Input } from "antd";
 import Request from "libs/request";
 import Toast from "libs/toast";
 
@@ -13,6 +12,7 @@ class DatasetImportView extends React.PureComponent {
   state = {
     dataset: null,
     datasetJson: null,
+    isValidJSON: true,
   }
 
   componentDidMount() {
@@ -25,47 +25,74 @@ class DatasetImportView extends React.PureComponent {
 
         this.setState({
           dataset,
-          datasetJson: datasetJson.dataSource,
+          datasetJson: JSON.stringify(datasetJson.dataSource, null, "  "),
         });
       });
     });
   }
 
   importDataset = () => {
-    const url = `${this.state.dataset.dataStore.url}/data/datasets/${this.props.datasetName}`;
-    Request.sendJSONReceiveJSON(url, {
-      data: this.state.datasetJson,
-    }).then(
-      () => Toast.success(`Successfully imported ${this.props.datasetName}`),
-      (error) => {
-        Toast.error(error);
-      },
-    );
+    if (this.state.isValidJSON) {
+      const url = `${this.state.dataset.dataStore.url}/data/datasets/${this.props.datasetName}`;
+      Request.sendJSONReceiveJSON(url, {
+        data: JSON.parse(this.state.datasetJson),
+      }).then(
+        () => {
+          Toast.success(`Successfully imported ${this.props.datasetName}`);
+          window.history.back();
+        },
+        (error) => {
+          Toast.error(error);
+        },
+      );
+    } else {
+      Toast.error("Invalid JSON. Please fix the errors.");
+    }
   }
 
-  handleChangeJson = (newJson) => {
-    debugger
-    this.setState({
-      datasetJson: newJson,
-    });
+  handleChangeJson = (el) => {
+    try {
+      JSON.parse(el.target.value);
+      this.setState({
+        datasetJson: el.target.value,
+        isValidJSON: true,
+      });
+    } catch (e) {
+      this.setState({
+        datasetJson: el.target.value,
+        isValidJSON: false,
+      });
+    }
   }
 
   render() {
     const datasetJson = this.state.datasetJson;
-    // const content = datasetJson ? (<JsonEdit
-    //   json={{"as": 1  }}
-    //   changeVal={this.handleChangeJson}
-    // />) : <Spin size="large" />;
-    console.log(datasetJson)
+    const textAreaStyle = this.state.isValidJSON ? {
+      fontFamily: "monospace",
+    } : {
+      fontFamily: "monospace",
+      border: "1px solid red",
+      boxShadow: "0 0 0 2px rgba(233, 16, 76, 0.28)",
+    };
+
+    const content = datasetJson ? (<Input.TextArea
+      value={datasetJson}
+      onChange={this.handleChangeJson}
+      rows={20}
+      style={textAreaStyle}
+    />) :
+    <Spin size="large" />;
 
     return (
-      <div>
+      <div className="container" id="dataset-import-view">
         <h3>Import Dataset {this.props.datasetName}</h3>
-        <JsonEdit
-
-        />
-        <Button onClick={this.importDataset}>Import</Button>
-        <Button onClick={() => window.history.back()}>Cancel</Button>
+        <div className="content">
+          {content}
+        </div>
+        <div>
+          <Button onClick={this.importDataset} type="primary">Import</Button>
+          <Button onClick={() => window.history.back()}>Cancel</Button>
+        </div>
       </div>
     );
   }

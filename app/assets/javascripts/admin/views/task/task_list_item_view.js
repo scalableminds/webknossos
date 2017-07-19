f5690bc11204077414803aefb4d84076bf0bb282
Daniel Werner <daniel.werner@student.hpi.uni-potsdam.de>
2017-07-19 16:37:32 +0200

    unify admin task actions for task list and user annotation list (#1242)

--- app/assets/javascripts/admin/views/task/task_list_item_view.js
+++ app/assets/javascripts/admin/views/task/task_list_item_view.js
@@ -4,6 +4,7 @@ import Marionette from "backbone.marionette";
 import AnnotationCollection from "admin/models/task/annotation_collection";
 import TemplateHelpers from "libs/template_helpers";
 import TaskAnnotationView from "admin/views/task/task_annotation_view";
+import TaskTransferModalView from "dashboard/views/task_transfer_modal_view";
 
 
 class TaskListItemView extends Marionette.CompositeView {
@@ -64,6 +65,7 @@ class TaskListItemView extends Marionette.CompositeView {
       <tbody>
       </tbody>
     </table>
+    <div class="modal-container"></div>
   </td>
 </tr>\
 `);
@@ -74,11 +76,13 @@ class TaskListItemView extends Marionette.CompositeView {
     this.prototype.events = {
       "click .delete": "deleteTask",
       "click .details-toggle": "toggleDetails",
+      "click #transfer-task": "transferTask",
     };
 
     this.prototype.ui = {
       detailsRow: ".details-row",
       detailsToggle: ".details-toggle",
+      modalContainer: ".modal-container",
     };
 
     this.prototype.templateContext =
@@ -94,10 +98,11 @@ class TaskListItemView extends Marionette.CompositeView {
     this.collection = new AnnotationCollection(this.model.get("id"));
 
     // minimize the toggle view on item deletion
-    this.listenTo(this.collection, "remove", () => this.toggleDetails(),
-    );
-  }
+    this.listenTo(this.collection, "remove", () => this.toggleDetails());
 
+    // refresh the list after transfering a task
+    this.listenTo(app.vent, "modal:destroy", this.refresh);
+  }
 
   deleteTask() {
     if (window.confirm("Do you really want to delete this task?")) {
@@ -105,6 +110,16 @@ class TaskListItemView extends Marionette.CompositeView {
     }
   }
 
+  transferTask(evt) {
+    evt.preventDefault();
+
+    const modalContainer = new Marionette.Region({
+      el: this.ui.modalContainer,
+    });
+    const url = evt.target.href;
+    this.modal = new TaskTransferModalView({ url });
+    modalContainer.show(this.modal);
+  }
 
   toggleDetails() {
     if (this.ui.detailsRow.hasClass("hide")) {
@@ -120,6 +135,10 @@ class TaskListItemView extends Marionette.CompositeView {
       this.ui.detailsRow.addClass("hide");
       this.ui.detailsToggle.removeClass("open");
     }
+  }
+
+  refresh() {
+    this.collection.fetch().then(() => this.render());
   }
 }
 TaskListItemView.initClass();
--- app/assets/javascripts/dashboard/views/dashboard_task_list_item_view.js
+++ app/assets/javascripts/dashboard/views/dashboard_task_list_item_view.js
@@ -7,6 +7,7 @@ import _ from "lodash";
 import Marionette from "backbone.marionette";
 import Request from "libs/request";
 import moment from "moment";
+import Toast from "libs/toast";
 
 class DashboardTaskListItemView extends Marionette.View {
   static initClass() {
@@ -33,27 +34,34 @@ class DashboardTaskListItemView extends Marionette.View {
   <% if (annotation.state.isFinished) { %>
     <i class="fa fa-check"></i><span> Finished</span><br />
   <% } else { %>
-    <a href="/annotations/<%- annotation.typ %>/<%- annotation.id %>">
-      <i class="fa fa-random"></i>
-      <strong>trace</strong>
-    </a>
-    <% if (isAdminView) { %>
-      <br/>
-      <a href="/annotations/<%- annotation.typ %>/<%- annotation.id %>/transfer" id="transfer-task">
-        <i class="fa fa-share"></i>
-        transfer
-      </a>
-      </br>
-      <a href="#" id="cancel-task">
-        <i class="fa fa-trash-o"></i>
-        cancel
-      </a>
-    <% } %>
-    <br/>
-    <a href="#" id="finish-task" class="trace-finish">
-      <i class="fa fa-check-circle-o"></i>
-      finish
-    </a>
+    <ul>
+      <li><a href="/annotations/Task/<%- annotation.id %>">
+        <i class="fa fa-random"></i>
+        <strong>trace</strong>
+      </a></li>
+      <% if (isAdminView) { %>
+        <li><a href="/annotations/Task/<%- annotation.id %>/transfer" id="transfer-task">
+          <i class="fa fa-share"></i>
+          transfer
+        </a></li>
+        <li><a href="/annotations/Task/<%- annotation.id %>/download">
+          <i class="fa fa-download"></i>
+          download
+        </a></li>
+        <li><a href="/annotations/Task/<%- annotation.id %>/reset" class="isAjax">
+          <i class="fa fa-undo"></i>
+          reset
+        </a></li>
+        <li><a href="#" id="cancel-task">
+          <i class="fa fa-trash-o"></i>
+          cancel
+        </a></li>
+      <% } %>
+      <li><a href="#" id="finish-task">
+        <i class="fa fa-check-circle-o"></i>
+        finish
+      </a></li>
+    </ul>
   <% } %>
 </td>\
 `);
@@ -64,6 +72,7 @@ class DashboardTaskListItemView extends Marionette.View {
     this.prototype.events = {
       "click #finish-task": "finish",
       "click #cancel-task": "cancelAnnotation",
+      "click .isAjax": "callAjax",
     };
   }
 
@@ -82,6 +91,17 @@ class DashboardTaskListItemView extends Marionette.View {
   }
 
 
+  // some actions are real links and some need to be send as ajax calls to the server
+  callAjax(evt) {
+    evt.preventDefault();
+
+    Request.receiveJSON(evt.target.href).then((jsonData) => {
+      Toast.message(jsonData.messages);
+    },
+    );
+  }
+
+
   finish() {
     if (confirm("Are you sure you want to permanently finish this tracing?")) {
       this.model.finish();
@@ -92,7 +112,7 @@ class DashboardTaskListItemView extends Marionette.View {
   cancelAnnotation() {
     if (confirm("Do you really want to cancel this annotation?")) {
       const annotation = this.model.get("annotation");
-      Request.triggerRequest(`/annotations/${annotation.typ}/${annotation.id}`, { method: "DELETE" }).then(() =>
+      Request.triggerRequest(`/annotations/Task/${annotation.id}`, { method: "DELETE" }).then(() =>
         this.model.collection.fetch());
     }
   }

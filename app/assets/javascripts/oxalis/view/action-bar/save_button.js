f9f69931d1d83af21abe3ecca9a42d75dbbd8fe9
Tom Herold <tom.herold@scalableminds.com>
2017-08-15 13:08:53 +0200

    enable polling for save button again #1964

--- app/assets/javascripts/oxalis/view/action-bar/save_button.js
+++ app/assets/javascripts/oxalis/view/action-bar/save_button.js
@@ -4,9 +4,25 @@ import Model from "oxalis/model";
 import ButtonComponent from "oxalis/view/components/button_component";
 import type { OxalisState } from "oxalis/store";
 
+const SAVED_POLLING_INTERVAL = 100;
+
 class SaveButton extends React.PureComponent {
   props: {
     onClick: Function,
+  };
+
+  componentDidMount() {
+    // Polling can be removed once VolumeMode saving is reactive
+    this.savedPollingInterval = window.setInterval(this._forceUpdate, SAVED_POLLING_INTERVAL);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.savedPollingInterval);
+  }
+
+  savedPollingInterval: number = 0;
+  _forceUpdate = () => {
+    this.forceUpdate();
   };
 
   getSaveButtonIcon() {

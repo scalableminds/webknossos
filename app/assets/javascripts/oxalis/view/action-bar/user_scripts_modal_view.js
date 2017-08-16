9db5dae9178dc67cf442dd81bbc6bfb8a05ecd25
Daniel Werner <daniel.werner@student.hpi.uni-potsdam.de>
2017-08-16 17:00:31 +0200

    apply PR feedback, move add script button into dropdown, show whether script was changed in the script modal (#1918)

--- app/assets/javascripts/oxalis/view/action-bar/user_scripts_modal_view.js
+++ app/assets/javascripts/oxalis/view/action-bar/user_scripts_modal_view.js
@@ -5,16 +5,24 @@ import React from "react";
 import { Modal, Input, Select, Spin } from "antd";
 import Request from "libs/request";
 import { fetchGistContent } from "libs/gist";
+import messages from "messages";
+import type { ScriptType } from "oxalis/store";
 
 type UserScriptsModalViewPropsType = {
-  onClose: Function,
-  visible: boolean,
+  onOK: Function,
+  isVisible: boolean,
 };
 
 class UserScriptsModalView extends React.PureComponent {
   props: UserScriptsModalViewPropsType;
 
-  state = {
+  state: {
+    code: string,
+    isCodeChanged: boolean,
+    scripts: Array<ScriptType>,
+    selectedScript: ?string,
+    isLoading: boolean,
+  } = {
     code: "",
     isCodeChanged: false,
     scripts: [],
@@ -42,26 +50,32 @@ class UserScriptsModalView extends React.PureComponent {
   handleScriptChange = async (scriptId: string) => {
     const script = this.state.scripts.find(s => s.id === scriptId);
     if (script == null) return;
-    if (
-      !this.state.isCodeChanged ||
-      confirm("This will replace the code you've written. Continue?")
-    ) {
-      this.setState({ isLoading: true });
-      const content = await fetchGistContent(script.gist, script.name);
-      this.setState({
-        isLoading: false,
-        selectedScript: scriptId,
-        code: content,
-        isCodeChanged: false,
+    if (!this.state.isCodeChanged) {
+      this.loadScript(script);
+    } else {
+      Modal.confirm({
+        content: messages["add_script.confirm_change"],
+        onOk: () => this.loadScript(script),
       });
     }
+  };
+
+  loadScript = async (script: ScriptType) => {
+    this.setState({ isLoading: true });
+    const content = await fetchGistContent(script.gist, script.name);
+    this.setState({
+      isLoading: false,
+      selectedScript: script.id,
+      code: content,
+      isCodeChanged: false,
+    });
   };
 
   handleClick = () => {
     try {
       eval(this.state.code);
       // close modal if the script executed successfully
-      return this.props.onClose();
+      return this.props.onOK();
     } catch (error) {
       console.error(error);
       return alert(error);
@@ -71,17 +85,17 @@ class UserScriptsModalView extends React.PureComponent {
   render() {
     return (
       <Modal
-        visible={this.props.visible}
-        title="Add user script"
+        visible={this.props.isVisible}
+        title="Add User Script"
         okText="Add"
         cancelText="Close"
         onOk={this.handleClick}
-        onCancel={this.props.onClose}
+        onCancel={this.props.onOK}
       >
         <Spin spinning={this.state.isLoading}>
           <Select
-            value={this.state.selectedScript}
-            style={{ width: 200, "margin-bottom": 10 }}
+            value={this.state.isCodeChanged ? "Modified Script" : this.state.selectedScript}
+            style={{ width: 200, marginBottom: 10 }}
             onChange={this.handleScriptChange}
             placeholder="Select an existing user script"
           >

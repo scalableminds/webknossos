4beafd5c1097672fb4f6b367d08df0b3f6a6a726
Johannes Frohnhofen <johannes@frohnhofen.com>
2017-08-17 09:49:21 +0200

    update libs

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ import sbtbuildinfo._
 import sbtbuildinfo.BuildInfoKeys._
 
 object Dependencies{
-  val braingamesVersion = "11.1.3"
+  val braingamesVersion = "11.1.4"
 
   val braingamesDataStore = "com.scalableminds" %% "braingames-datastore" % braingamesVersion
 

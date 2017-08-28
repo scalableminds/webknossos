e1fdc6f57caa8523a896ba56e47a376e3b0689eb
Tom Herold <tom.herold@scalableminds.com>
2017-08-28 14:32:01 +0200

    reactifed team list view

--- app/assets/javascripts/admin/views/team/team_list_view.js
+++ app/assets/javascripts/admin/views/team/team_list_view.js
@@ -1,63 +1,155 @@
+// @flow
+/* eslint-disable jsx-a11y/href-no-hash */
+
 import _ from "lodash";
-import Marionette from "backbone.marionette";
-import SelectAllRows from "libs/behaviors/select_all_rows_behavior";
-import app from "app";
-import TeamListItemView from "admin/views/team/team_list_item_view";
-import CreateTeamModalView from "admin/views/team/create_team_modal_view";
+import * as React from "react";
+import { Table, Icon, Spin, Button, Input, Modal } from "antd";
+import Utils from "libs/utils";
+import Request from "libs/request";
+import messages from "messages";
+import CreateTeamModal from "admin/views/team/create_team_modal_view.js";
+import type { APITeamType } from "admin/api_flow_types";
 
-class TeamListView extends Marionette.CompositeView {
-  static initClass() {
-    this.prototype.template = _.template(`\
- <h3>Teams</h3>
- <table class="table table-striped">
-   <thead>
-     <tr>
-       <th>Name</th>
-       <th>Parent</th>
-       <th>Owner</th>
-       <th>Roles</th>
-       <th>Actions</th>
-     </tr>
-   </thead>
-   <tbody></tbody>
- </table>
-<div class="modal-wrapper"></div>\
-`);
+const { Column } = Table;
+const { Search } = Input;
 
-    this.prototype.className = "team-administration container wide";
-    this.prototype.childView = TeamListItemView;
-    this.prototype.childViewContainer = "tbody";
+type State = {
+  isLoading: boolean,
+  teams: Array<APITeamType>,
+  searchQuery: string,
+};
 
-    this.prototype.behaviors = {
-      SelectAllRows: {
-        behaviorClass: SelectAllRows,
-      },
-    };
+class TeamListView extends React.PureComponent<{}, State> {
+  state = {
+    isLoading: true,
+    teams: [],
+    searchQuery: "",
+    isTeamCreationModalVisible: false,
+  };
 
-    this.prototype.ui = { modalWrapper: ".modal-wrapper" };
+  componentDidMount() {
+    this.fetchData();
   }
 
-  initialize() {
-    this.listenTo(app.vent, "paginationView:filter", this.filterBySearch);
-    this.listenTo(app.vent, "modal:destroy", this.render);
-    this.listenTo(app.vent, "paginationView:addElement", this.showModal);
+  async fetchData(): Promise<void> {
+    const url = "/api/teams?isEditable=true";
+    const teams = await Request.receiveJSON(url);
 
-    return this.collection.fetch({
-      data: "isEditable=true",
+    this.setState({
+      isLoading: false,
+      teams: teams.filter(p => p.owner),
     });
   }
 
-  filterBySearch(filterQuery) {
-    return this.collection.setFilter(["name", "owner"], filterQuery);
-  }
+  handleSearch = (event: SyntheticInputEvent<>): void => {
+    this.setState({ searchQuery: event.target.value });
+  };
 
-  showModal(modalView) {
-    modalView = new CreateTeamModalView({ teamCollection: this.collection });
-    this.ui.modalWrapper.html(modalView.render().el);
+  deleteTeam = async (script: APITeamType) => {
+    Modal.confirm({
+      title: messages["team.delete"],
+      onOk: () => {
+        this.setState({
+          isLoading: true,
+        });
 
-    return modalView.show();
+        const url = `/api/teams/${script.id}`;
+        Request.receiveJSON(url, {
+          method: "DELETE",
+        }).then(() => {
+          this.setState({
+            isLoading: false,
+            teams: this.state.teams.filter(p => p.id !== script.id),
+          });
+        });
+      },
+    });
+  };
+
+  createTeam = async (newTeam: APITeamType) => {
+    this.setState({
+      isTeamCreationModalVisible: false,
+      teams: this.state.teams.concat([newTeam]),
+    });
+  };
+
+  render() {
+    const marginRight = { marginRight: 20 };
+
+    return (
+      <div className="container wide">
+        <div style={{ marginTag: 20 }}>
+          <div className="pull-right">
+            <Button
+              icon="plus"
+              style={marginRight}
+              type="primary"
+              onClick={() => this.setState({ isTeamCreationModalVisible: true })}
+            >
+              Add Team
+            </Button>
+            <Search
+              style={{ width: 200 }}
+              onPressEnter={this.handleSearch}
+              onChange={this.handleSearch}
+            />
+          </div>
+          <h3>Teams</h3>
+          <div className="clearfix" style={{ margin: "20px 0px" }} />
+
+          <Spin spinning={this.state.isLoading} size="large">
+            <Table
+              dataSource={Utils.filterWithSearchQueryOR(
+                this.state.teams,
+                ["name", "owner", "parent"],
+                this.state.searchQuery,
+              )}
+              rowKey="id"
+              pagination={{
+                defaultPageSize: 50,
+              }}
+              style={{ marginTop: 30, marginBotton: 30 }}
+            >
+              <Column
+                title="Name"
+                dataIndex="name"
+                key="name"
+                sorter={Utils.localeCompareBy("name")}
+              />
+              <Column
+                title="Parent"
+                dataIndex="parent"
+                key="parent"
+                sorter={Utils.localeCompareBy("parent")}
+              />
+              <Column
+                title="Owner"
+                dataIndex="owner"
+                key="owner"
+                sorter={Utils.localeCompareBy((team: APITeamType) => team.owner.lastName)}
+                render={owner =>
+                  owner.email ? `${owner.firstName} ${owner.lastName} (${owner.email})` : "-"}
+              />
+              <Column
+                title="Action"
+                key="actions"
+                render={(__, script: APITeamType) =>
+                  <a href="#" onClick={_.partial(this.deleteTeam, script)}>
+                    <Icon type="delete" />Delete
+                  </a>}
+              />
+            </Table>
+          </Spin>
+          <CreateTeamModal
+            teams={this.state.teams}
+            visible={this.state.isTeamCreationModalVisible}
+            onOk={this.createTeam}
+            onCancel={() => this.setState({ isTeamCreationModalVisible: false })}
+          />
+        </div>
+      </div>
+    );
   }
 }
-TeamListView.initClass();
 
 export default TeamListView;

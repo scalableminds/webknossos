a4b6cd8b4ba9e7f94ec39496010d6fd66049e479
Florian Meinel <florian@scm.io>
2017-08-28 16:00:36 +0200

    merge JsonUtils into JsonHelper, remove obsolete GeoJson

--- src/main/scala/com/scalableminds/util/tools/JsonHelper.scala
+++ src/main/scala/com/scalableminds/util/tools/JsonHelper.scala
@@ -10,6 +10,7 @@ import com.scalableminds.util.io.FileIO
 import com.scalableminds.util.mvc.BoxImplicits
 import com.typesafe.scalalogging.LazyLogging
 import net.liftweb.common._
+import play.api.i18n.Messages
 import play.api.libs.json._
 
 import scala.io.{BufferedSource, Source}
@@ -53,6 +54,32 @@ object JsonHelper extends BoxImplicits with LazyLogging {
         Failure(s"Failed to parse Json in '${rootPath.relativize(path).toString}'. Cause: ${e.getCause}")
     } finally {
       if(buffer != null) buffer.close()
+    }
+  }
+
+
+  def jsError2HumanReadable(js: JsError)(implicit messages: Messages): String = {
+    js.errors.map {
+      case (path, errors) =>
+        val errorStr = errors.map(m => Messages(m.message)).mkString(", ")
+        s"Error at json path '$path': $errorStr."
+    }.mkString("\n")
+  }
+
+  implicit def boxFormat[T: Format]: Format[Box[T]] = new Format[Box[T]]{
+    override def reads(json: JsValue): JsResult[Box[T]] = {
+      (json \ "status").validate[String].flatMap {
+        case "Full" => (json \ "value").validate[T].map(Full(_))
+        case "Empty" => JsSuccess(Empty)
+        case "Failure" => (json \ "value").validate[String].map(Failure(_))
+        case _ => JsError("invalid status")
+      }
+    }
+
+    override def writes(o: Box[T]): JsValue = o match {
+      case Full(t) => Json.obj("status" -> "Full", "value" -> Json.toJson(t))
+      case Empty => Json.obj("status" -> "Empty")
+      case f: Failure => Json.obj("status" -> "Failure", "value" -> f.msg)
     }
   }
 }

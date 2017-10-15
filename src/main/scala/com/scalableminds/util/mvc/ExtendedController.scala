90bb98f2439cce9c2f09f6f29907911ad8552c82
Florian Meinel <florian@scm.io>
2017-10-15 10:02:17 +0200

    add box2Opt to box implicits

--- src/main/scala/com/scalableminds/util/mvc/ExtendedController.scala
+++ src/main/scala/com/scalableminds/util/mvc/ExtendedController.scala
@@ -79,6 +79,11 @@ trait BoxImplicits {
 
   implicit def option2Box[T](in: Option[T]): Box[T] = Box(in)
 
+  implicit def box2Option[T](in: Box[T]): Option[T] = in match {
+    case Full(t) => Some(t)
+    case _ => None
+  }
+
   implicit def jsResult2Box[T](result: JsResult[T]): Box[T] = result match {
     case JsSuccess(value, _) => Full(value)
     case JsError(e) => Failure(s"Invalid json: $e")

0af82b6f8c3edc0cf0c2f67888cfff6548250e67
Florian Meinel <florian@scm.io>
2017-11-22 19:23:32 +0100

    move json implicits to utils

--- app/oxalis/security/CombinedAuthenticatorDAO.scala
+++ app/oxalis/security/CombinedAuthenticatorDAO.scala
@@ -1,56 +1,25 @@
 package oxalis.security
 
-import javax.inject.Inject
-
-import com.scalableminds.util.tools.{Fox, FoxImplicits}
-import controllers.routes
-import models.user.{MongoUserTokenDao, User, UserService}
-import net.liftweb.common.{Empty, Full}
-import play.api.Play.current
-import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.json.Json._
-import com.mohiva.play.silhouette.api.{Environment, LoginInfo, SecuredErrorHandler, Silhouette}
-import com.mohiva.play.silhouette.impl.authenticators.{BearerTokenAuthenticator, _}
+import com.mohiva.play.silhouette.api.LoginInfo
 import com.mohiva.play.silhouette.impl.daos.AuthenticatorDAO
 import com.scalableminds.util.reactivemongo.{DBAccessContext, GlobalAccessContext}
+import com.scalableminds.util.tools.JsonHelper._
+import com.scalableminds.util.tools.{Fox, JsonHelper}
 import models.basics.SecuredBaseDAO
-import org.joda.time.DateTime
-import play.api.libs.json._
-import play.modules.reactivemongo.ReactiveMongoApi
-import reactivemongo.play.json.collection.JSONCollection
-import play.api.libs.functional.syntax._
-
-import scala.concurrent.{ExecutionContext, Future}
-import scala.concurrent.duration._
-import play.api.libs.json._
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json.Json._
 import play.api.libs.json.Reads._
 import play.api.libs.json.Writes._
-import reactivemongo.api.commands.WriteResult
-import reactivemongo.bson.BSONObjectID
-import net.ceedubs.ficus.Ficus._
-import net.ceedubs.ficus.readers.ArbitraryTypeReader._
-import play.api.libs.json.Json
+import play.api.libs.json.{Json, _}
+
+import scala.concurrent.Future
 /**
   * Created by robert on 14.11.17.
   */
-class CombinedAuthenticatorDAO extends AuthenticatorDAO[CombinedAuthenticator] with SecuredBaseDAO[CombinedAuthenticator]{
+class CombinedAuthenticatorDAO extends AuthenticatorDAO[CombinedAuthenticator] with SecuredBaseDAO[CombinedAuthenticator] {
   override def collectionName: String = "combinedAuthenticators"
 
-  implicit object FiniteDurationFormat extends Format[FiniteDuration] {
-    def reads(json: JsValue): JsResult[FiniteDuration] = LongReads.reads(json).map(_.seconds)
-    def writes(o: FiniteDuration): JsValue = LongWrites.writes(o.toSeconds)
-  }
-
-  implicit def optionFormat[T: Format]: Format[Option[T]] = new Format[Option[T]]{
-    override def reads(json: JsValue): JsResult[Option[T]] = json.validateOpt[T]
-
-    override def writes(o: Option[T]): JsValue = o match {
-      case Some(t) ⇒ implicitly[Writes[T]].writes(t)
-      case None ⇒ JsNull
-    }
-  }
-
-  override protected implicit def formatter: OFormat[CombinedAuthenticator] = JsonUtil.oFormat(Json.format[CombinedAuthenticator])
+  override protected implicit def formatter: OFormat[CombinedAuthenticator] = JsonHelper.oFormat(Json.format[CombinedAuthenticator])
 
   override def findOne[V](attribute: String, value: V)(implicit w: Writes[V], ctx: DBAccessContext): Fox[CombinedAuthenticator] = findOne(Json.obj(attribute -> w.writes(value)))
 
@@ -82,12 +51,3 @@ class CombinedAuthenticatorDAO extends AuthenticatorDAO[CombinedAuthenticator] w
     findOne("loginInfoParameter", loginInfo)(implicitly[Writes[LoginInfo]],GlobalAccessContext).futureBox.map(box => box.toOption)
 }
 
-object JsonUtil {
-  def oFormat[T](format:Format[T]) : OFormat[T] = {
-    val oFormat: OFormat[T] = new OFormat[T](){
-      override def writes(o: T): JsObject = format.writes(o).as[JsObject]
-      override def reads(json: JsValue): JsResult[T] = format.reads(json)
-    }
-    oFormat
-  }
-}

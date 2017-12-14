d4aff72349617b50f28994bbdbbee6b959c3b03a
Jonathan Striebel <jonathan.striebel@gmail.com>
2017-12-14 11:44:59 +0100

    refactoring fossildb download

--- fossildb/run.sh
+++ fossildb/run.sh
@@ -5,9 +5,11 @@ JAR="fossildb.jar"
 VERSION="0.1.1"
 FOSSILDB="$(dirname $(readlink -f $0))/$JAR"
 URL="https://github.com/scalableminds/fossildb/releases/download/$VERSION/$JAR"
+CURRENT_VERSION="$(java -jar "$FOSSILDB" --version || echo "unknown")"
 
-if [ ! -f "$FOSSILDB" ] || [ ! "$(java -jar \"$FOSSILDB\") --version" = "$VERSION" ]; then
-  wget -O "$FOSSILDB" "$URL"
+if [ ! -f "$FOSSILDB" ] || [ ! "$CURRENT_VERSION" == "$VERSION" ]; then
+  echo "Updating FossilDB version from $CURRENT_VERSION to $VERSION"
+  wget -q --show-progress -O "$FOSSILDB" "$URL"
 fi
 
 exec java -jar "$FOSSILDB" -c skeletons,skeletonUpdates,volumes,volumeData

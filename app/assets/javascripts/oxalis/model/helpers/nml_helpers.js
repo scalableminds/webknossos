8f88254491fd519e3b500655a5d9587579ccd084
Daniel <daniel.werner@student.hpi.uni-potsdam.de>
2017-12-15 20:41:58 +0100

    Export visible trees as nml in frontend (#2231)
    * add nml serializer to frontend, add button to download all visible trees as nml (#2094)
    
    * correct indentation using tagged template literals (#2094)
    
    * use arrays instead of multiline strings for nml export (#2094)
    
    * show spinner while exporting nml, set correct nml name (#2094)
    
    * apply PR feedback (#2231)
    
    * uppercase NML (#2231)

---
+++ app/assets/javascripts/oxalis/model/helpers/nml_helpers.js
@@ -0,0 +1,196 @@
+// @flow
+
+import _ from "lodash";
+import { getPosition, getRotation } from "oxalis/model/accessors/flycam_accessor";
+import type {
+  OxalisState,
+  SkeletonTracingType,
+  NodeMapType,
+  EdgeType,
+  TreeType,
+} from "oxalis/store";
+
+function indent(array: Array<string>): Array<string> {
+  // Use forEach instead of map for performance reasons
+  array.forEach((line, index) => {
+    array[index] = `  ${line}`;
+  });
+  return array;
+}
+
+export function getNmlName(state: OxalisState): string {
+  // Use the same naming convention as the backend
+  const { activeUser, dataset, task, tracing } = state;
+  if (tracing.name !== "") return `${tracing.name}.nml`;
+
+  const datasetName = dataset.name;
+  const tracingType = task ? task.id : "explorational";
+  let userName = activeUser
+    ? `${activeUser.firstName.slice(0, 1)}${activeUser.lastName}`.toLowerCase()
+    : "";
+  // Replace spaces in user names
+  userName = userName.replace(/ /g, "_");
+  const shortAnnotationId = tracing.annotationId.slice(-6);
+
+  return `${datasetName}__${tracingType}__${userName}__${shortAnnotationId}.nml`;
+}
+
+export function serializeToNml(state: OxalisState, tracing: SkeletonTracingType): string {
+  // Only visible trees will be serialized!
+  // _.filter throws flow errors here, because the type definitions are wrong and I'm not able to fix them
+  const visibleTrees = Object.keys(tracing.trees)
+    .filter(treeId => tracing.trees[Number(treeId)].isVisible)
+    .map(treeId => tracing.trees[Number(treeId)]);
+  return [
+    "<things>",
+    ...indent(
+      _.concat(
+        serializeParameters(state),
+        serializeTrees(visibleTrees),
+        serializeBranchPoints(visibleTrees),
+        serializeComments(visibleTrees),
+      ),
+    ),
+    "</things>",
+  ].join("\n");
+}
+
+function serializeParameters(state: OxalisState): Array<string> {
+  const editPosition = getPosition(state.flycam).map(Math.round);
+  const editRotation = getRotation(state.flycam);
+  const userBB = state.tracing.userBoundingBox;
+  return [
+    "<parameters>",
+    ...indent([
+      serializeTag("experiment", {
+        name: state.dataset.name,
+        description: state.tracing.description,
+      }),
+      serializeTag("scale", {
+        x: state.dataset.scale[0],
+        y: state.dataset.scale[1],
+        z: state.dataset.scale[2],
+      }),
+      serializeTag("offset", {
+        x: 0,
+        y: 0,
+        z: 0,
+      }),
+      serializeTag("time", { ms: state.tracing.createdTimestamp }),
+      serializeTag("editPosition", {
+        x: editPosition[0],
+        y: editPosition[1],
+        z: editPosition[2],
+      }),
+      serializeTag("editRotation", {
+        xRot: editRotation[0],
+        yRot: editRotation[1],
+        zRot: editRotation[2],
+      }),
+      serializeTag("zoomLevel", { zoom: state.flycam.zoomStep }),
+      userBB != null
+        ? serializeTag("userBoundingBox", {
+            topLeftX: userBB.min[0],
+            topLeftY: userBB.min[1],
+            topLeftZ: userBB.min[2],
+            width: userBB.max[0] - userBB.min[0],
+            height: userBB.max[1] - userBB.min[1],
+            depth: userBB.max[2] - userBB.min[2],
+          })
+        : "",
+    ]),
+    "</parameters>",
+  ];
+}
+
+function serializeTrees(trees: Array<TreeType>): Array<string> {
+  return _.flatten(
+    trees.map(tree => [
+      serializeTag(
+        "thing",
+        {
+          id: tree.treeId,
+          "color.r": tree.color[0],
+          "color.g": tree.color[1],
+          "color.b": tree.color[2],
+          "color.a": 1.0,
+          name: tree.name,
+        },
+        false,
+      ),
+      ...indent([
+        "<nodes>",
+        ...indent(serializeNodes(tree.nodes)),
+        "</nodes>",
+        "<edges>",
+        ...indent(serializeEdges(tree.edges)),
+        "</edges>",
+      ]),
+      "</thing>",
+    ]),
+  );
+}
+
+function serializeNodes(nodes: NodeMapType): Array<string> {
+  return Object.keys(nodes).map(nodeId => {
+    const node = nodes[Number(nodeId)];
+    const position = node.position.map(Math.round);
+    return serializeTag("node", {
+      id: node.id,
+      radius: node.radius,
+      x: position[0],
+      y: position[1],
+      z: position[2],
+      rotX: node.rotation[0],
+      rotY: node.rotation[1],
+      rotZ: node.rotation[2],
+      inVp: node.viewport,
+      inMag: node.resolution,
+      bitDepth: node.bitDepth,
+      interpolation: node.interpolation,
+      time: node.timestamp,
+    });
+  });
+}
+
+function serializeEdges(edges: Array<EdgeType>): Array<string> {
+  return edges.map(edge => serializeTag("edge", { source: edge.source, target: edge.target }));
+}
+
+function serializeBranchPoints(trees: Array<TreeType>): Array<string> {
+  const branchPoints = _.flatten(trees.map(tree => tree.branchPoints));
+  return [
+    "<branchpoints>",
+    ...indent(
+      branchPoints.map(branchPoint =>
+        serializeTag("branchpoint", { id: branchPoint.nodeId, time: branchPoint.timestamp }),
+      ),
+    ),
+    "</branchpoints>",
+  ];
+}
+
+function serializeComments(trees: Array<TreeType>): Array<string> {
+  const comments = _.flatten(trees.map(tree => tree.comments));
+  return [
+    "<comments>",
+    ...indent(
+      comments.map(comment =>
+        serializeTag("comment", { node: comment.nodeId, content: comment.content }),
+      ),
+    ),
+    "</comments>",
+  ];
+}
+
+function serializeTag(
+  name: string,
+  properties: { [string]: ?(string | number | boolean) },
+  closed: boolean = true,
+): string {
+  return `<${name} ${Object.keys(properties)
+    .map(key => `${key}="${properties[key] != null ? properties[key].toString() : ""}"`)
+    .join(" ")}${closed ? " /" : ""}>`;
+}
+
+export function parseNml() {}
--- app/assets/javascripts/oxalis/view/right-menu/trees_tab_view.js
+++ app/assets/javascripts/oxalis/view/right-menu/trees_tab_view.js
@@ -6,7 +6,7 @@
 import _ from "lodash";
 import * as React from "react";
 import { connect } from "react-redux";
-import { Button, Dropdown, Input, Menu } from "antd";
+import { Button, Dropdown, Input, Menu, Icon, Spin, Modal } from "antd";
 import TreesTabItemView from "oxalis/view/right-menu/trees_tab_item_view";
 import InputComponent from "oxalis/view/components/input_component";
 import ButtonComponent from "oxalis/view/components/button_component";
@@ -22,8 +22,11 @@ import {
   toggleAllTreesAction,
   toggleInactiveTreesAction,
 } from "oxalis/model/actions/skeletontracing_actions";
-import type { Dispatch } from "redux";
 import Store from "oxalis/store";
+import { serializeToNml, getNmlName } from "oxalis/model/helpers/nml_helpers";
+import Utils from "libs/utils";
+import { saveAs } from "file-saver";
+import type { Dispatch } from "redux";
 import type { OxalisState, SkeletonTracingType, UserConfigurationType } from "oxalis/store";
 
 const ButtonGroup = Button.Group;
@@ -42,7 +45,15 @@ type Props = {
   userConfiguration: UserConfigurationType,
 };
 
-class TreesTabView extends React.PureComponent<Props> {
+type State = {
+  isDownloading: boolean,
+};
+
+class TreesTabView extends React.PureComponent<Props, State> {
+  state = {
+    isDownloading: false,
+  };
+
   handleChangeTreeName = evt => {
     this.props.onChangeTreeName(evt.target.value);
   };
@@ -68,6 +79,18 @@ class TreesTabView extends React.PureComponent<Props> {
   toggleInactiveTrees() {
     Store.dispatch(toggleInactiveTreesAction());
   }
+
+  downloadAsNml = async () => {
+    await this.setState({ isDownloading: true });
+    // Wait for the Modal to render
+    await Utils.sleep(1000);
+    const state = Store.getState();
+    const nml = serializeToNml(state, this.props.skeletonTracing);
+    this.setState({ isDownloading: false });
+
+    const blob = new Blob([nml], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, getNmlName(state));
+  };
 
   getTreesComponents() {
     const orderAttribute = this.props.userConfiguration.sortTreesByName ? "name" : "timestamp";
@@ -99,6 +122,28 @@ class TreesTabView extends React.PureComponent<Props> {
     );
   }
 
+  getActionsDropdown() {
+    return (
+      <Menu>
+        <Menu.Item key="shuffleTreeColor">
+          <div onClick={this.shuffleTreeColor} title="Change Tree Color">
+            <i className="fa fa-adjust" /> Change Color
+          </div>
+        </Menu.Item>
+        <Menu.Item key="shuffleAllTreeColors">
+          <div onClick={this.shuffleAllTreeColors} title="Shuffle All Tree Colors">
+            <i className="fa fa-random" /> Shuffle All Colors
+          </div>
+        </Menu.Item>
+        <Menu.Item key="downloadAsNml">
+          <div onClick={this.downloadAsNml} title="Download visible trees as NML">
+            <Icon type="export" /> Download as NML
+          </div>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+
   render() {
     const activeTreeName = getActiveTree(this.props.skeletonTracing)
       .map(activeTree => activeTree.name)
@@ -106,18 +151,22 @@ class TreesTabView extends React.PureComponent<Props> {
 
     return (
       <div id="tree-list" className="flex-column">
+        <Modal
+          visible={this.state.isDownloading}
+          title="Preparing NML"
+          closable={false}
+          footer={null}
+          width={200}
+          style={{ textAlign: "center" }}
+        >
+          <Spin />
+        </Modal>
         <ButtonGroup>
           <ButtonComponent onClick={this.props.onCreateTree} title="Create Tree">
             <i className="fa fa-plus" /> Create
           </ButtonComponent>
           <ButtonComponent onClick={this.deleteTree} title="Delete Tree">
             <i className="fa fa-trash-o" /> Delete
-          </ButtonComponent>
-          <ButtonComponent onClick={this.shuffleTreeColor} title="Change Tree Color">
-            <i className="fa fa-adjust" /> Change Color
-          </ButtonComponent>
-          <ButtonComponent onClick={this.shuffleAllTreeColors} title="Shuffle All Tree Colors">
-            <i className="fa fa-random" /> Shuffle All Colors
           </ButtonComponent>
           <ButtonComponent onClick={this.toggleAllTrees} title="Toggle Visibility of All Trees">
             <i className="fa fa-toggle-on" /> Toggle All
@@ -128,6 +177,11 @@ class TreesTabView extends React.PureComponent<Props> {
           >
             <i className="fa fa-toggle-off" /> Toggle Inactive
           </ButtonComponent>
+          <Dropdown overlay={this.getActionsDropdown()}>
+            <ButtonComponent>
+              More<Icon type="down" />
+            </ButtonComponent>
+          </Dropdown>
         </ButtonGroup>
         <InputGroup compact>
           <ButtonComponent onClick={this.props.onSelectNextTreeBackward}>

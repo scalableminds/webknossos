4736a91ddbe1ced48383737cb6177b87edfaea58
Florian Meinel <florian@scm.io>
2017-12-20 16:56:14 +0100

    open assignment database evolution: add allowDiskUse

--- conf/evolutions/36.js
+++ conf/evolutions/36.js
@@ -1,8 +1,9 @@
-// Store instance count with a single OpenAnnotations
+// Store instance count with a single OpenAssignments
+// Note that the Down splits them into OpenAssignments with just 1 instance.
 
 // --- !Ups
-db.openAssignments.renameCollection("openAssignments_singeInstance");
-db.openAssignments_singeInstance.aggregate([
+db.openAssignments.renameCollection("openAssignments_multiplePerTask");
+db.openAssignments_multiplePerTask.aggregate([
   {$group: {
     _id: "$_task",
     _task: {$first: "$_task"},
@@ -13,7 +14,7 @@ db.openAssignments_singeInstance.aggregate([
     created: {$first: "$created"},
     instances: {$sum: 1},
   }}
-]).forEach(function(assignment) {
+], {allowDiskUse:true}).forEach(function(assignment) {
   delete assignment._id;
   db.openAssignments.insert(assignment);
 });

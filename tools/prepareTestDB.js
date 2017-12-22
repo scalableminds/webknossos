48a29d9713f9a38f107cd98ce58c41dc87c140f6
Norman Rzepka <code@normanrz.com>
2017-12-22 19:42:23 +0100

    moves prepareTestDB.js to tools

--- tools/prepareTestDB.js
+++ tools/prepareTestDB.js
@@ -2,7 +2,7 @@ const childProcess = require("child_process");
 
 const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/webknossos-testing";
 
-childProcess.execFileSync("./tools/dropDB.sh", [mongoURI]);
-childProcess.execFileSync("./tools/import_export/import.sh", [mongoURI, "test/db"], {
+childProcess.execFileSync("./dropDB.sh", [mongoURI]);
+childProcess.execFileSync("./import_export/import.sh", [mongoURI, "test/db"], {
   stdio: "ignore",
 });

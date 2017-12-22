d5b79526057a8658c7ec4ee84026a53e7c3452b8
Norman Rzepka <code@normanrz.com>
2017-12-22 20:35:55 +0100

    fixes tools/prepareTestDB.js

--- tools/prepareTestDB.js
+++
@@ -1,8 +0,0 @@
-const childProcess = require("child_process");
-
-const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/webknossos-testing";
-
-childProcess.execFileSync("./dropDB.sh", [mongoURI]);
-childProcess.execFileSync("./import_export/import.sh", [mongoURI, "test/db"], {
-  stdio: "ignore",
-});

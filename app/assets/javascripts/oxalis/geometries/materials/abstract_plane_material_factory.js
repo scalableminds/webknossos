09e119470deef5999048c537a05879a6d816e9c9
Daniel Werner <daniel.werner@student.hpi.uni-potsdam.de>
2017-12-22 17:40:25 +0100

    use uglify-js in production, use better source maps

--- app/assets/javascripts/oxalis/geometries/materials/abstract_plane_material_factory.js
+++ app/assets/javascripts/oxalis/geometries/materials/abstract_plane_material_factory.js
@@ -139,17 +139,12 @@ class AbstractPlaneMaterialFactory {
   }
 
   getVertexShader(): string {
-    return `\
-
-
+    return `
 varying vec2 vUv;
 
 void main() {
   vUv = uv;
-  gl_Position =   projectionMatrix *
-                  modelViewMatrix *
-                  vec4(position,1.0); }\
-`;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0); }`;
   }
 }
 

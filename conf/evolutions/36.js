2ba3a8b6043a9c5ad0da1b0caae0aca8403c8800
Florian Meinel <florian@scm.io>
2018-01-02 13:42:17 +0100

    switch collections only after the long-running aggregate

--- conf/evolutions/36.js
+++ conf/evolutions/36.js
@@ -2,8 +2,7 @@
 // Note that the Down splits them into OpenAssignments with just 1 instance.
 
 // --- !Ups
-db.openAssignments.renameCollection("openAssignments_multiplePerTask");
-db.openAssignments_multiplePerTask.aggregate([
+db.openAssignments.aggregate([
   {$group: {
     _id: "$_task",
     _task: {$first: "$_task"},
@@ -15,9 +14,10 @@ db.openAssignments_multiplePerTask.aggregate([
     instances: {$sum: "$instances"},
   }}
 ], {allowDiskUse:true}).forEach(function(assignment) {
-  delete assignment._id;
-  db.openAssignments.insert(assignment);
+  db.openAssignments_aggregated.insert(assignment);
 });
+db.openAssignments.renameCollection("openAssignments_multiplePerTask");
+db.openAssignments_aggregated.renameCollection("openAssignments");
 
 // --- !Downs
 db.openAssignments.renameCollection("openAssignments_aggregated");

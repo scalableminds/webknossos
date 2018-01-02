45e55435464be5fa12ab2897d2852bbb6d17c0ab
Florian Meinel <florian@scm.io>
2018-01-02 14:40:51 +0100

    fix database evolution that integrates OpenAssignments into Tasks

--- conf/evolutions/39.js
+++ conf/evolutions/39.js
@@ -1,26 +1,31 @@
-// Directly integrate OpenAnnotations into Tasks
+// Directly integrate OpenAnnotations into Tasks, deprecate mturkAssignments
 
 // --- !Ups
 db.openAssignments.find().forEach(function(assignment) {
-  db.tasks.update({"_id": assignment._task}, {$set: {"openInstances": assignment.instances}})
+  db.tasks.update({"_id": assignment._task}, {$set: {"openInstances": assignment.instances}});
 })
-db.projects.find("isPaused": false).forEach(function(project) {
-  db.tasks.update({"_project": project._id}, {$set: {"priority": project.priority}})
+db.projects.find({"paused": false}).forEach(function(project) {
+  db.tasks.update({"_project": project.name}, {$set: {"priority": project.priority}}, {multi: true});
 })
-db.tasks.update("priority": {$exists: false}, {$set: {"priority" : -1})
-db.tasks.update("openInstances": {$exists: false}, {$set: {"openInstances": 0}})
+db.tasks.update({"priority": {$exists: false}}, {$set: {"priority" : -1}}, {multi: true});
+db.tasks.update({"openInstances": {$exists: false}}, {$set: {"openInstances": 0}}, {multi: true});
 
 db.openAssignments.renameCollection("_openAssignments");
 db.mturkAssignments.renameCollection("_mturkAssignments");
 
 
 // --- !Downs
-db.openAssignments.renameCollection("openAssignments_aggregated");
-db.openAssignments_aggregated.find().forEach(function(assignment) {
-  var instanceCount = assignment.instances;
-  delete assignment.instances;
-  delete assignment._id;
-  for (var i=0; i<instanceCount; i++) {
-    db.openAssignments.insert(assignment);
-  }
+db.tasks.find({"openInstances": {$gt: 0}}).forEach(function(task) {
+	db.openAssignments.insert({
+		_task: task._id,
+		team: task.team,
+		_project: task.project,
+		neededExperience: task.neededExperience,
+		priority: task.priority,
+		created: task.created,
+		instances: task.openInstances
+	});
 });
+db.tasks.update({}, {$unset: {"openInstances": "", "priority": ""}});
+
+db.getCollection("_mturkAssignments").renameCollection("mturkAssignments");

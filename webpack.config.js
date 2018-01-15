f6d36a3e68bdbbe096beb284a0d9d19767d3eecf
Tom Herold <tom.herold@scalableminds.com>
2018-01-15 17:42:03 +0100

    removed erronous comma from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -30,9 +30,9 @@ module.exports = function(env = {}) {
         return module.context && module.context.indexOf("node_modules") !== -1;
       },
     }),
-    //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+    // CommonChunksPlugin will now extract all the common modules from vendor and main bundles
     new webpack.optimize.CommonsChunkPlugin({
-      name: "manifest", //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+      name: "manifest", // But since there are no more common modules between them we end up with just the runtime code included in the manifest file
     }),
   ];
 

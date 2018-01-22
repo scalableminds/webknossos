34858ee6c955d1adef20e9ee5279b8005b239dc0
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-01-22 20:34:02 +0100

    updated the evolution #2311

--- conf/evolutions/44.js
+++ conf/evolutions/44.js
@@ -1,12 +1,7 @@
 // Add the flag "tokenType = Authentication" to every token that is currently in the bearerTokenAuthenticator-collection
 
 // --- !Ups
-db.bearerTokenAuthenticators.find().forEach(function(elem) {
-  elem.tokenType = "Authentication";
-  db.bearerTokenAuthenticators.save(elem);
-});
+db.bearerTokenAuthenticators.update({}, { $set: { "tokenType": "Authentication" } }, { multi: true });
+
 // --- !Downs
-db.bearerTokenAuthenticators.find().forEach(function(elem) {
-  delete elem.tokenType;
-  db.bearerTokenAuthenticators.save(elem);
-});
+db.bearerTokenAuthenticators.update({}, { $unset: { "tokenType": "" } }, { multi: true });

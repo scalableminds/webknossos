2a880c8a3e4d68d424aa631705b3bceeef13c889
Norman Rzepka <norman.rzepka@scalableminds.com>
2018-02-20 14:56:15 +0100

    adds discussionBoard feature toggle (#2370)

--- app/assets/javascripts/admin/admin_rest_api.js
+++ app/assets/javascripts/admin/admin_rest_api.js
@@ -26,6 +26,7 @@ import type {
   APIProjectProgressReportType,
   APIOpenTasksReportType,
   APITracingType,
+  APIFeatureToggles,
 } from "admin/api_flow_types";
 import type { QueryObjectType } from "admin/task/task_search_form";
 import type { NewTaskType, TaskCreationResponseType } from "admin/task/task_create_bulk_view";
@@ -575,4 +576,8 @@ export async function getOpenTasksReport(teamId: string): Promise<Array<APIOpenT
   const openTasksData = await Request.receiveJSON(`/api/teams/${teamId}/openTasksOverview`);
   assertResponseLimit(openTasksData);
   return openTasksData;
+}
+
+export async function getFeatureToggles(): Promise<APIFeatureToggles> {
+  return Request.receiveJSON("/api/features");
 }

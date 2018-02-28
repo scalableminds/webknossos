6101f83de873c31e9db55eae57ee6e92081abdf1
Philipp Otto <philipp.4096@googlemail.com>
2018-02-28 09:32:29 +0100

    fix tree sorting in tracing view by changing antd's menu prop from onSelect to onClick

--- app/assets/javascripts/oxalis/view/right-menu/trees_tab_view.js
+++ app/assets/javascripts/oxalis/view/right-menu/trees_tab_view.js
@@ -130,9 +130,9 @@ class TreesTabView extends React.PureComponent<Props, State> {
     const activeMenuKey = this.props.userConfiguration.sortTreesByName
       ? "sortByName"
       : "sortByTime";
-
+    console.log("activeMenuKey", activeMenuKey);
     return (
-      <Menu defaultSelectedKeys={[activeMenuKey]} onSelect={this.handleDropdownClick}>
+      <Menu selectedKeys={[activeMenuKey]} onClick={this.handleDropdownClick}>
         <Menu.Item key="sortByName">by name</Menu.Item>
         <Menu.Item key="sortByTime">by creation time</Menu.Item>
       </Menu>

455a83807f2b6ef995e38cfa508a9114173b7074
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-03-02 16:28:50 +0100

    WIP

--- app/assets/javascripts/test/backend-snapshot-tests/teamstructure.e2e.js
+++ app/assets/javascripts/test/backend-snapshot-tests/teamstructure.e2e.js
@@ -7,7 +7,23 @@ import _ from "lodash";
 import * as api from "admin/admin_rest_api";
 import {fetchWrapper} from "../enzyme/e2e-setup";
 
+let activeUser;
 test.beforeEach(global.fetch = changeXAuthToken);
 
-//...
+test.before("Initialize values", async () => {
+  activeUser = await api.getActiveUser();
+});
+
+//one of these should fail
+test.serial("test whether is test is working", async t => {
+  t.equal(1, 1, "One is not equal to One")
+});
+
+test.serial("test whether is test is working", async t => {
+  t.equal(1, 0, "One is not equal to One")
+});
+
+ test.serial("Test Name", async t => {
+  t.equal(activeUser.lastName, "Boy", "lastName do not match")
+});
 

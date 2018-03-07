3ab7bf9203e8d875601b8f4715c1b07964a3aba6
Tom Herold <tom.herold@scalableminds.com>
2018-03-07 16:02:25 +0100

    updated to webpack v4 WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 module.exports = function(env = {}) {
   /* eslint no-var:0, import/no-extraneous-dependencies:0 */
   var webpack = require("webpack");
-  var ExtractTextPlugin = require("extract-text-webpack-plugin");
+  // var ExtractTextPlugin = require("extract-text-webpack-plugin");
   var fs = require("fs");
   var path = require("path");
-  const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+  // const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
   var srcPath = path.resolve(__dirname, "app/assets/javascripts/");
   var nodePath = path.join(__dirname, "node_modules/");
@@ -15,46 +15,47 @@ module.exports = function(env = {}) {
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": env.production ? '"production"' : '"development"',
     }),
-    new ExtractTextPlugin("main.css"),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery",
-      _: "lodash",
-    }),
+    // new ExtractTextPlugin("main.css"),
+    // new webpack.ProvidePlugin({
+    //   $: "jquery",
+    //   jQuery: "jquery",
+    //   "window.jQuery": "jquery",
+    //   _: "lodash",
+    // }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
-      minChunks: function(module) {
-        // this assumes your vendor imports exist in the node_modules directory
-        return module.context && module.context.indexOf("node_modules") !== -1;
-      },
-    }),
-    // CommonChunksPlugin will now extract all the common modules from vendor and main bundles
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "manifest", // But since there are no more common modules between them we end up with just the runtime code included in the manifest file
-    }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "vendor",
+    //   minChunks: function(module) {
+    //     // this assumes your vendor imports exist in the node_modules directory
+    //     return module.context && module.context.indexOf("node_modules") !== -1;
+    //   },
+    // }),
+    // // CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "manifest", // But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+    // }),
   ];
 
-  if (env.production) {
-    plugins.push(
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true,
-        uglifyOptions: {
-          compress: {
-            inline: 1,
-          },
-        },
-      }),
-    );
-  }
+  // if (env.production) {
+  //   plugins.push(
+  //     new UglifyJsPlugin({
+  //       cache: true,
+  //       parallel: true,
+  //       sourceMap: true,
+  //       uglifyOptions: {
+  //         compress: {
+  //           inline: 1,
+  //         },
+  //       },
+  //     }),
+  //   );
+  // }
 
   return {
     entry: {
       main: "main.js",
     },
+    mode: env.production ? "production" : "development",
     output: {
       path: `${__dirname}/public/bundle`,
       filename: "[name].js",
@@ -63,9 +64,6 @@ module.exports = function(env = {}) {
       chunkFilename: env.production ? "[chunkhash].js" : "[name].js",
     },
     module: {
-      // Reduce compilation time by telling webpack to not parse these libraries.
-      // Only add libraries that have no dependencies eg. no require, define or similar calls.
-      noParse: [/\/jquery\//],
       rules: [
         {
           test: /\.js$/,
@@ -74,21 +72,11 @@ module.exports = function(env = {}) {
         },
         {
           test: /\.less$/,
-          // This needs to be `loader` until `extract-text-webpack-plugin` is fixed
-          // Ref: https://github.com/webpack/extract-text-webpack-plugin/issues/250
-          use: ExtractTextPlugin.extract({
-            fallback: "style-loader",
-            use: "css-loader!less-loader",
-          }),
+          use: ["style-loader", "css-loader", "less-loader"],
         },
         {
           test: /\.css$/,
-          // This needs to be `loader` until `extract-text-webpack-plugin` is fixed
-          // Ref: https://github.com/webpack/extract-text-webpack-plugin/issues/250
-          use: ExtractTextPlugin.extract({
-            fallback: "style-loader",
-            use: "css-loader!less-loader",
-          }),
+          use: ["style-loader", "css-loader", "less-loader"],
         },
         {
           test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -111,7 +99,6 @@ module.exports = function(env = {}) {
     resolve: {
       modules: [srcPath, nodePath],
     },
-    externals: [{ routes: "var jsRoutes" }],
     // See https://webpack.js.org/configuration/devtool/
     devtool: env.production ? "source-map" : "eval-source-map",
     plugins,

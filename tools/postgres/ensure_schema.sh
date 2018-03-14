5d7bf8b13600603c2dd092a90f3976d37b57b193
Norman Rzepka <code@normanrz.com>
2018-03-14 10:38:45 +0100

    moar quotes

--- tools/postgres/ensure_schema.sh
+++ tools/postgres/ensure_schema.sh
@@ -3,8 +3,8 @@ set -Eeuo pipefail
 
 scriptdir="$(dirname "$0")"
 
-dbName="$($scriptdir/db_name.sh)"
-dbHost="$($scriptdir/db_host.sh)"
+dbName="$("$scriptdir"/db_name.sh)"
+dbHost="$("$scriptdir"/db_host.sh)"
 
 schemaPath="$scriptdir/schema.sql"
 
@@ -14,4 +14,4 @@ then
     exit
 fi
 
-PGPASSWORD=postgres psql -U postgres -h  $dbHost --dbname=$dbName -f $schemaPath
+PGPASSWORD=postgres psql -U postgres -h  $dbHost --dbname=$dbName -f "$schemaPath"

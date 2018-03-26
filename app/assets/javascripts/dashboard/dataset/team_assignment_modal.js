70215f8a825fb97e36b024c9e63667f75692bbc2
Tom Herold <tom.herold@scalableminds.com>
2018-03-26 14:08:16 +0200

    updated frontend schema for team assignment #935

--- app/assets/javascripts/dashboard/dataset/team_assignment_modal.js
+++ app/assets/javascripts/dashboard/dataset/team_assignment_modal.js
@@ -18,7 +18,7 @@ type Props = {
 type State = {
   teams: Array<APITeamType>,
   isLoading: boolean,
-  selectedTeams: Array<string>,
+  selectedTeams: Array<APITeamType>,
 };
 
 class TeamAssignmentModal extends React.PureComponent<Props, State> {
@@ -41,9 +41,11 @@ class TeamAssignmentModal extends React.PureComponent<Props, State> {
     });
   }
 
-  selectTeams = (selectedTeams: Array<string>) => {
+  selectTeams = (selectedTeamIds: Array<string>) => {
     // make sure the owningTeam is always selected
-    const allowedTeams = _.uniq([this.props.dataset.owningOrganization, ...selectedTeams]);
+
+    const uniqueIds = _.uniq([...selectedTeamIds]);
+    const allowedTeams = this.state.teams.filter(team => uniqueIds.includes(team.id));
     this.setState({
       selectedTeams: allowedTeams,
     });
@@ -54,7 +56,8 @@ class TeamAssignmentModal extends React.PureComponent<Props, State> {
       allowedTeams: this.state.selectedTeams,
     });
 
-    updateDatasetTeams(updatedDataset.name, updatedDataset.allowedTeams).then(() => {
+    const teamIds = updatedDataset.allowedTeams.map(t => t.id);
+    updateDatasetTeams(updatedDataset.name, teamIds).then(() => {
       this.props.onOk(updatedDataset);
     });
   };
@@ -75,7 +78,7 @@ class TeamAssignmentModal extends React.PureComponent<Props, State> {
             placeholder="Select a Team"
             optionFilterProp="children"
             onChange={this.selectTeams}
-            value={this.state.selectedTeams}
+            value={this.state.selectedTeams.map(t => t.id)}
             filterOption={(input, option) =>
               option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
             }

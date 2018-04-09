5bfaae6830787042cc0209610ad0de130b6dfd36
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-04-09 15:10:20 +0200

    yarn pretty

--- app/assets/javascripts/test/backend-snapshot-tests/teamstructure.e2e.js
+++ app/assets/javascripts/test/backend-snapshot-tests/teamstructure.e2e.js
@@ -62,17 +62,18 @@ test("teams_user_E", async t => {
   t.is(teams.length, 1);
 });
 
-test("teams_delete_user_D", async t => { // the teamManager is not allowed to delete the team
+test("teams_delete_user_D", async t => {
+  // the teamManager is not allowed to delete the team
   await setCurrToken(tokenUser_D);
   t.plan(1);
 
-  await api.deleteTeam("69882b370d889b84020efd4f")
-    .catch(err => {
-      t.is(err.messages[0].error, 'Access denied.');
-    });
+  await api.deleteTeam("69882b370d889b84020efd4f").catch(err => {
+    t.is(err.messages[0].error, "Access denied.");
+  });
 });
 
-test("teams_create_user_D", async t => { // the teamManager is not allowed to create a new team
+test("teams_create_user_D", async t => {
+  // the teamManager is not allowed to create a new team
   await setCurrToken(tokenUser_D);
   t.plan(1);
 
@@ -81,10 +82,9 @@ test("teams_create_user_D", async t => { // the teamManager is not allowed to cr
     name: "test-team-name",
     organization: organizations[0].name,
   };
-  await api.createTeam(newTeam)
-    .catch(err => {
-      t.is(err.messages[0].error, 'Access denied. Only admin users can execute this operation.');
-    });
+  await api.createTeam(newTeam).catch(err => {
+    t.is(err.messages[0].error, "Access denied. Only admin users can execute this operation.");
+  });
 });
 
 // TaskTypes
@@ -112,20 +112,18 @@ test("tasks_user_D", async t => {
   await setCurrToken(tokenUser_D);
   t.plan(1);
 
-  await api.getTask("58135c192faeb34c0081c058")
-    .catch(err => {
-      t.is(err.messages[0].error, 'Task couldn\'t be found');
-    });
+  await api.getTask("58135c192faeb34c0081c058").catch(err => {
+    t.is(err.messages[0].error, "Task couldn't be found");
+  });
 });
 
 test("tasks_user_E", async t => {
   await setCurrToken(tokenUser_E);
   t.plan(1);
 
-  await api.getTask("58135c192faeb34c0081c058")
-    .catch(err => {
-      t.is(err.messages[0].error, 'Task couldn\'t be found');
-    });
+  await api.getTask("58135c192faeb34c0081c058").catch(err => {
+    t.is(err.messages[0].error, "Task couldn't be found");
+  });
 });
 
 test("tasks_user_C", async t => {
@@ -135,7 +133,8 @@ test("tasks_user_C", async t => {
 });
 
 // User
-test("user_user_B", async t => { // teamMng are not allowed to de-/activate a user (if they are not an admin)
+test("user_user_B", async t => {
+  // teamMng are not allowed to de-/activate a user (if they are not an admin)
   await setCurrToken(tokenUser_B);
   t.plan(2);
 
@@ -143,20 +142,19 @@ test("user_user_B", async t => { // teamMng are not allowed to de-/activate a us
   const user = await api.getUser(userId_C);
   t.is(user.firstName, "user_C");
   const newUser = Object.assign({}, user, { isActive: false });
-  await api.updateUser(newUser)
-    .catch(err => {
-      t.is(err.messages[0].error, 'You are not authorized to view or edit this resource');
-    });
+  await api.updateUser(newUser).catch(err => {
+    t.is(err.messages[0].error, "You are not authorized to view or edit this resource");
+  });
 });
 
 // Project
-test("project_user_B", async t => { // teamMng are not allowed to delete a project (if they are not an admin and they are not the owner)
+test("project_user_B", async t => {
+  // teamMng are not allowed to delete a project (if they are not an admin and they are not the owner)
   await setCurrToken(tokenUser_E);
   t.plan(1);
 
   const projectName = "Test_Project";
-  await api.deleteProject(projectName)
-    .catch(err => {
-      t.is(err.messages[0].error, 'Project Test_Project couldn\'t be found');
-    });
+  await api.deleteProject(projectName).catch(err => {
+    t.is(err.messages[0].error, "Project Test_Project couldn't be found");
+  });
 });

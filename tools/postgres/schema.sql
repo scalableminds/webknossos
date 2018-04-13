854110e7afb0291d2ba7b6a1da8c8ac732467ef4
Florian Meinel <florian@scm.io>
2018-04-13 16:02:57 +0200

    add displayname to test db datasets

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -395,6 +395,6 @@ COMMIT TRANSACTION;
 
 START TRANSACTION;
 DROP VIEW webknossos.datasets_;
-ALTER TABLE webknossos.datasets ADD COLUMN displayName CHAR(256);
+ALTER TABLE webknossos.datasets ADD COLUMN displayName VARCHAR(256);
 CREATE VIEW webknossos.dataSets_ AS SELECT * FROM webknossos.dataSets WHERE NOT isDeleted;
 COMMIT TRANSACTION;

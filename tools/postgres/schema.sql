b2362ae81fedeb101664f328d7ada90147470b43
Daniel Werner <daniel.werner@student.hpi.uni-potsdam.de>
2018-04-16 17:16:05 +0200

    remove remainder from merge conflict from schema.sql

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -388,7 +388,6 @@ DROP VIEW webknossos.datasets_;
 ALTER TABLE webknossos.datasets ADD COLUMN sharingToken CHAR(256);
 CREATE VIEW webknossos.dataSets_ AS SELECT * FROM webknossos.dataSets WHERE NOT isDeleted;
 COMMIT TRANSACTION;
-<<<<<<< HEAD
 
 
 

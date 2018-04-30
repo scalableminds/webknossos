adb69fdb634fd3a35a7a284aa6207986553c341f
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-04-30 16:26:37 +0200

    renamed evolution from 004 to 005

--- conf/evolutions_postgres/005-remove-team-assignment-from-task.sql
+++ conf/evolutions_postgres/005-remove-team-assignment-from-task.sql
@@ -16,6 +16,6 @@ COMMIT TRANSACTION;
 START TRANSACTION;
 DROP VIEW webknossos.tasks_;
 ALTER TABLE webknossos.tasks ADD COLUMN _team CHAR(255); -- which data type is 'team'?
-UPDATE webknossos.tasks SET _team = (SELECT _team FROM webknossos.projects WHERE webknossos.tasks._team = webknossos.projects._team)
+UPDATE webknossos.tasks SET _team = (SELECT _team FROM webknossos.projects WHERE webknossos.tasks._project = webknossos.projects._id)
 CREATE VIEW webknossos.tasks_ AS SELECT * FROM webknossos.tasks WHERE NOT isDeleted;
 COMMIT TRANSACTION;

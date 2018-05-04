c57ded78b17b78dc224af938e5d5835c38cb17ee
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-05-04 11:17:59 +0200

    deleted mongo evolutions

--- conf/evolutions/11.js
+++
@@ -1,7 +0,0 @@
-// Update users to add 'md5hash' property
-
-// --- !Ups
-db.users.update({}, {"$set" : {"md5hash" : ""}}, {"multi" : true})
-
-// --- !Downs
-db.users.update({}, {"$unset" : {"md5hash" : ""}}, {"multi" : true})

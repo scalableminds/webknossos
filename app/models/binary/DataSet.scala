4d7adb556da67b1ceebab5da54b84ec153901f37
Florian Meinel <florian@scm.io>
2018-05-11 10:43:59 +0200

    in update allowed teams, make team list unique

--- app/models/binary/DataSet.scala
+++ app/models/binary/DataSet.scala
@@ -482,7 +482,7 @@ object DataSetDAO {
     DataSetSQLDAO.updateDataSourceByName(name, dataStoreInfo.name, source, isActive)
 
   def updateTeams(name: String, teams: List[BSONObjectID])(implicit ctx: DBAccessContext) =
-    DataSetAllowedTeamsSQLDAO.updateAllowedTeamsForDataSetByName(name, teams.map(ObjectId.fromBsonId(_)))
+    DataSetAllowedTeamsSQLDAO.updateAllowedTeamsForDataSetByName(name, teams.map(ObjectId.fromBsonId(_)).toSet.toList)
 
   def update(name: String, description: Option[String], displayName: Option[String], isPublic: Boolean)(implicit ctx: DBAccessContext): Fox[DataSet] = {
     for {

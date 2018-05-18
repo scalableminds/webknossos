f5071f466f488ce1e04d6e349d8490ebedf63e57
Youri K <youri.kaminsky@student.hpi.de>
2018-05-18 16:46:40 +0200

    add evolution for new isOrganizationTeam field

---
+++ conf/evolutions_postgres/009-add-isOrganizationTeam.sql
@@ -0,0 +1,26 @@
+-- https://github.com/scalableminds/webknossos/pull/2613
+
+-- UP:
+
+
+START TRANSACTION;
+DROP VIEW webknossos.teams_;
+DROP VIEW webknossos.organizations_;
+ALTER TABLE webknossos.teams ADD COLUMN isOrganizationTeam BOOLEAN;
+ALTER TABLE webknossos.organizations DROP COLUMN _organizationTeam;
+CREATE VIEW webknossos.teams_ AS SELECT * FROM webknossos.teams WHERE NOT isDeleted;
+CREATE VIEW webknossos.organizations_ AS SELECT * FROM webknossos.organizations WHERE NOT isDeleted;
+COMMIT TRANSACTION;
+
+
+-- DOWN:
+
+
+START TRANSACTION;
+DROP VIEW webknossos.teams_;
+DROP VIEW webknossos.organizations_;
+ALTER TABLE webknossos.teams DROP COLUMN isOrganizationTeam;
+ALTER TABLE webknossos.organizations ADD COLUMN _organizationTeam CHAR(24);
+CREATE VIEW webknossos.teams_ AS SELECT * FROM webknossos.teams WHERE NOT isDeleted;
+CREATE VIEW webknossos.organizations_ AS SELECT * FROM webknossos.organizations WHERE NOT isDeleted;
+COMMIT TRANSACTION;

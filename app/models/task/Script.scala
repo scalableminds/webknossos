eff46ea1edb55a140f8b8c9c8df67c94cc1be80f
Youri K <youri.kaminsky@student.hpi.de>
2018-05-18 13:26:53 +0200

    fix script find all to only show scripts from your organization #2613

--- app/models/task/Script.scala
+++ app/models/task/Script.scala
@@ -46,6 +46,9 @@ object ScriptSQLDAO extends SQLDAO[ScriptSQL, ScriptsRow, Scripts] {
   def idColumn(x: Scripts): Rep[String] = x._Id
   def isDeletedColumn(x: Scripts): Rep[Boolean] = x.isdeleted
 
+  override def readAccessQ(requestingUserId: ObjectId): String =
+    s"(select _organization from webknossos.users_ u where u._id = _owner) = (select _organization from webknossos.users_ u where u._id = '${requestingUserId.id}')"
+
   def parse(r: ScriptsRow): Fox[ScriptSQL] =
     Fox.successful(ScriptSQL(
       ObjectId(r._Id),
@@ -73,6 +76,13 @@ object ScriptSQLDAO extends SQLDAO[ScriptSQL, ScriptsRow, Scripts] {
                             isDeleted = ${s.isDeleted}
                           where _id = ${s._id.id}""")
     } yield ()
+
+  override def findAll(implicit ctx: DBAccessContext): Fox[List[ScriptSQL]] =
+    for {
+      accessQuery <- readAccessQuery
+      r <- run(sql"select #${columns} from webknossos.scripts_ where #${accessQuery}".as[ScriptsRow])
+      parsed <- Fox.combined(r.toList.map(parse))
+    } yield parsed
 }
 
 

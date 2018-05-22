eb2b16fc175d54ac94602469b3ea906611224035
Daniel Werner <daniel.werner@student.hpi.uni-potsdam.de>
2018-05-22 11:23:45 +0200

    adjust parameters of tracing statistics script and add project information

--- tools/statistics/stats.js
+++ tools/statistics/stats.js
@@ -1,17 +1,22 @@
 const { Client } = require("pg");
 
-const MIN_NODES_PER_HOUR = 500;
-const LAST_X_DAYS = 365;
-const MIN_TRACING_MINUTES = 15;
+const MIN_NODES_PER_HOUR = 100;
+const LAST_X_DAYS = 2 * 365;
+const MIN_TRACING_MINUTES = 60;
 
 const ANNOTATIONS_SQL = `
-SELECT annotations._id, tracing_id, _user, _task, firstname, lastname, modified, tracingtime, json_extract_path_text(statistics::json,'nodeCount') AS nodecount
+SELECT annotations._id, tracing_id, _task, firstname, lastname, modified,
+  annotations.tracingtime, json_extract_path_text(statistics::json,'nodeCount') AS nodecount,
+  _project, projects.name
 FROM webknossos.annotations
 JOIN webknossos.users ON webknossos.annotations._user=webknossos.users._id
+JOIN webknossos.tasks ON webknossos.annotations._task=webknossos.tasks._id
+JOIN webknossos.projects ON webknossos.tasks._project=webknossos.projects._id
 WHERE DATE_PART('day', now() - modified) <= ${LAST_X_DAYS}
 AND _task IS NOT NULL
-AND (tracingtime / 1000 / 60) > ${MIN_TRACING_MINUTES}
-AND json_extract_path_text(statistics::json,'nodeCount')::integer / (GREATEST(tracingtime, 1000) / 1000.0 / 3600.0) < ${MIN_NODES_PER_HOUR};`;
+AND (annotations.tracingtime / 1000 / 60) > ${MIN_TRACING_MINUTES}
+AND json_extract_path_text(statistics::json,'nodeCount')::integer / (GREATEST(annotations.tracingtime, 1000) / 1000.0 / 3600.0) < ${MIN_NODES_PER_HOUR}
+ORDER BY modified;`;
 
 async function connect() {
   const client = new Client();
@@ -50,7 +55,9 @@ async function connect() {
         entry.firstname
       } ${entry.lastname}, TracingTime: ${tracingTimeMinutes}m${tracingTimeSeconds}s, TaskId: ${
         entry._task
-      }, AnnotationId: ${entry._id}, TracingId: ${entry.tracing_id}, Modified: ${entry.modified}`,
+      }, AnnotationId: ${entry._id}, TracingId: ${entry.tracing_id}, ProjectId: ${
+        entry._project
+      }, ProjectName: ${entry.name}, Modified: ${entry.modified}`,
     );
   }
 

92bfdac3f1f0b83f151eaf864b1f067d59df692e
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-05-24 15:38:20 +0200

    update index

--- conf/evolutions_postgres/006-remove-team-assignment-from-task.sql
+++ conf/evolutions_postgres/006-remove-team-assignment-from-task.sql
@@ -7,6 +7,9 @@ START TRANSACTION;
 DROP VIEW webknossos.tasks_;
 ALTER TABLE webknossos.tasks DROP COLUMN _team;
 CREATE VIEW webknossos.tasks_ AS SELECT * FROM webknossos.tasks WHERE NOT isDeleted;
+
+DROP INDEX webknossos.tasks__team_neededexperience_domain_neededexperience_value_idx;
+CREATE INDEX tasks_neededexperience_domain_neededexperience_value_idx ON webknossos.tasks(neededExperience_domain, neededExperience_value);
 COMMIT TRANSACTION;
 
 
@@ -18,4 +21,7 @@ DROP VIEW webknossos.tasks_;
 ALTER TABLE webknossos.tasks ADD COLUMN _team CHAR(255); -- which data type is 'team'?
 UPDATE webknossos.tasks SET _team = (SELECT _team FROM webknossos.projects WHERE webknossos.tasks._project = webknossos.projects._id)
 CREATE VIEW webknossos.tasks_ AS SELECT * FROM webknossos.tasks WHERE NOT isDeleted;
+
+DROP INDEX webknossos.tasks_neededexperience_domain_neededexperience_value_idx;
+CREATE INDEX ON webknossos.tasks(neededExperience_domain, neededExperience_value);
 COMMIT TRANSACTION;

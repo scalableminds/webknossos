c64fc306ce1e41a485a15955288526dc3bcacbb1
Philipp Otto <philippotto@users.noreply.github.com>
2018-05-24 09:36:45 +0200

    Make hovered cell easy to copy (#2624)
    * allow easy copying of hovered cell ids; display id's in mappings view in a table; colorize ids according to cell colors
    
    * pretty keyboard.js
    
    * pin pretty version and undo keyboard.js formatting
    
    * debounce rendering of mappings view
    
    * use existing mapId function and only render mapped columns in mapping view if mapping is activated
    
    * fix wrong merge-conflict-resolving
    
    * update yarn.lock

--- app/assets/javascripts/oxalis/view/right-menu/mapping_info_view.js
+++ app/assets/javascripts/oxalis/view/right-menu/mapping_info_view.js
@@ -3,18 +3,38 @@
  * @flow
  */
 import React, { Component } from "react";
+import { Table, Tooltip, Icon } from "antd";
 import { connect } from "react-redux";
 import Cube from "oxalis/model/binary/data_cube";
 import Model from "oxalis/model";
 import { getPosition } from "oxalis/model/accessors/flycam_accessor";
 import { SwitchSetting } from "oxalis/view/settings/setting_input_views";
-import type { Vector3 } from "oxalis/constants";
+import type { OrthoViewType, Vector2, Vector3 } from "oxalis/constants";
 import type { OxalisState } from "oxalis/store";
+import { OrthoViews } from "oxalis/constants";
 import _ from "lodash";
+import { calculateGlobalPos } from "oxalis/controller/viewmodes/plane_controller";
+import message from "messages";
+import debounceRender from "react-debounce-render";
 
 type Props = {
   position: Vector3,
+  mousePosition: ?Vector2,
   isMappingEnabled: boolean,
+  activeViewport: OrthoViewType,
+  activeCellId: number,
+};
+
+// This function mirrors convertCellIdToRGB in the fragment shader of the rendering plane
+const convertCellIdToHSV = id => {
+  if (id === 0) {
+    return "white";
+  }
+  const goldenRatio = 0.618033988749895;
+  const lastEightBits = id & (2 ** 8 - 1);
+  const value = ((lastEightBits * goldenRatio) % 1.0) * 360;
+
+  return `hsla(${value}, 100%, 50%, 0.15)`;
 };
 
 class MappingInfoView extends Component<Props> {
@@ -44,26 +64,85 @@ class MappingInfoView extends Component<Props> {
     this.getCube().setMappingEnabled(isEnabled);
   };
 
+  renderIdTable() {
+    const cube = this.getCube();
+    const hasMapping = cube.hasMapping();
+
+    let globalMousePosition;
+    if (this.props.mousePosition && this.props.activeViewport !== OrthoViews.TDView) {
+      const [x, y] = this.props.mousePosition;
+      globalMousePosition = calculateGlobalPos({ x, y });
+    }
+
+    const getIdForPos = pos => pos && cube.getDataValue(pos, null);
+
+    const tableData = [
+      { name: "Active ID", key: "active", unmapped: this.props.activeCellId },
+      {
+        name: "ID at current position",
+        key: "current",
+        unmapped: getIdForPos(this.props.position),
+      },
+      {
+        name: (
+          <span>
+            ID at mouse position{" "}
+            <Tooltip
+              title={
+                message[hasMapping ? "tracing.copy_maybe_mapped_cell_id" : "tracing.copy_cell_id"]
+              }
+              placement="bottomLeft"
+            >
+              <Icon type="info-circle" />
+            </Tooltip>
+          </span>
+        ),
+        key: "mouse",
+        unmapped: getIdForPos(globalMousePosition),
+      },
+    ]
+      .map(idInfo => ({
+        ...idInfo,
+        mapped: idInfo.unmapped && cube.mapId(idInfo.unmapped),
+      }))
+      .map(idInfo => ({
+        ...idInfo,
+        unmapped: (
+          <span style={{ background: convertCellIdToHSV(idInfo.unmapped) }}>{idInfo.unmapped}</span>
+        ),
+        mapped: (
+          <span style={{ background: convertCellIdToHSV(idInfo.mapped) }}>{idInfo.mapped}</span>
+        ),
+      }));
+
+    const columnHelper = (title, dataIndex) => ({ title, dataIndex });
+    const idColumns =
+      hasMapping && this.props.isMappingEnabled
+        ? // Show an unmapped and mapped id column if there's a mapping
+          [columnHelper("Unmapped", "unmapped"), columnHelper("Mapped", "mapped")]
+        : // Otherwise, only show an ID column
+          [columnHelper("ID", "unmapped")];
+    const columns = [columnHelper("", "name"), ...idColumns];
+    return (
+      <Table
+        style={{ maxWidth: 500 }}
+        size="small"
+        dataSource={tableData}
+        columns={columns}
+        pagination={false}
+        align="right"
+      />
+    );
+  }
+
   render() {
     const cube = this.getCube();
     const hasMapping = cube.hasMapping();
-    const idWithMapping = cube.getDataValue(this.props.position, cube.mapping);
-    const idWithoutMapping = cube.getDataValue(this.props.position, null);
 
     return (
       <div id="volume-mapping-info">
-        <div className="well">
-          {hasMapping ? (
-            <div>
-              <p>ID without mapping: {idWithoutMapping}</p>
-              <p>ID with mapping: {idWithMapping}</p>
-            </div>
-          ) : (
-            <p>ID at current position: {idWithoutMapping}</p>
-          )}
-        </div>
         {hasMapping ? (
-          <div>
+          <div style={{ marginBottom: 12 }}>
             <SwitchSetting
               value={this.props.isMappingEnabled}
               onChange={this.handleChangeMappingEnabled}
@@ -71,6 +150,7 @@ class MappingInfoView extends Component<Props> {
             />
           </div>
         ) : null}
+        {this.renderIdTable()}
       </div>
     );
   }
@@ -80,7 +160,11 @@ function mapStateToProps(state: OxalisState): Props {
   return {
     position: getPosition(state.flycam),
     isMappingEnabled: state.temporaryConfiguration.isMappingEnabled,
+    mousePosition: state.temporaryConfiguration.mousePosition,
+    activeViewport: state.viewModeData.plane.activeViewport,
+    activeCellId: typeof state.tracing.activeCellId === "number" ? state.tracing.activeCellId : 0,
   };
 }
 
-export default connect(mapStateToProps)(MappingInfoView);
+const debounceTime = 100;
+export default connect(mapStateToProps)(debounceRender(MappingInfoView, debounceTime));

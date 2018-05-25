8e1cd5cab8b7610d694624524b80b061bfda3de3
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-05-25 13:55:16 +0200

    changed SQL-type of field 'team' in evolution

--- conf/evolutions_postgres/006-remove-team-assignment-from-task.sql
+++ conf/evolutions_postgres/006-remove-team-assignment-from-task.sql
@@ -18,7 +18,7 @@ COMMIT TRANSACTION;
 
 START TRANSACTION;
 DROP VIEW webknossos.tasks_;
-ALTER TABLE webknossos.tasks ADD COLUMN _team CHAR(255); -- which data type is 'team'?
+ALTER TABLE webknossos.tasks ADD COLUMN _team CHAR(24);
 UPDATE webknossos.tasks SET _team = (SELECT _team FROM webknossos.projects WHERE webknossos.tasks._project = webknossos.projects._id)
 CREATE VIEW webknossos.tasks_ AS SELECT * FROM webknossos.tasks WHERE NOT isDeleted;
 

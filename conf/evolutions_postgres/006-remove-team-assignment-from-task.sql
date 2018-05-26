d8651153069ce5f0e6cc3561de5e35040bac0203
Florian M <florian@scm.io>
2018-05-26 12:12:04 +0200

    fix small error in pg evolution

--- conf/evolutions_postgres/006-remove-team-assignment-from-task.sql
+++ conf/evolutions_postgres/006-remove-team-assignment-from-task.sql
@@ -1,14 +1,14 @@
--- remove the team assignment from the task. You can get the team via 'project'
+-- https://github.com/scalableminds/webknossos/pull/2553
+
+-- removes the team assignment from the task. You can get the team via 'project'
 
 -- UP:
 
 
 START TRANSACTION;
 DROP VIEW webknossos.tasks_;
-ALTER TABLE webknossos.tasks DROP COLUMN _team;
+ALTER TABLE webknossos.tasks DROP COLUMN _team; -- automatically cascades to matching index
 CREATE VIEW webknossos.tasks_ AS SELECT * FROM webknossos.tasks WHERE NOT isDeleted;
-
-DROP INDEX webknossos.tasks__team_neededexperience_domain_neededexperience_value_idx;
 CREATE INDEX tasks_neededexperience_domain_neededexperience_value_idx ON webknossos.tasks(neededExperience_domain, neededExperience_value);
 COMMIT TRANSACTION;
 
@@ -19,9 +19,9 @@ COMMIT TRANSACTION;
 START TRANSACTION;
 DROP VIEW webknossos.tasks_;
 ALTER TABLE webknossos.tasks ADD COLUMN _team CHAR(24);
-UPDATE webknossos.tasks SET _team = (SELECT _team FROM webknossos.projects WHERE webknossos.tasks._project = webknossos.projects._id)
+UPDATE webknossos.tasks SET _team = (SELECT _team FROM webknossos.projects WHERE webknossos.tasks._project = webknossos.projects._id);
 CREATE VIEW webknossos.tasks_ AS SELECT * FROM webknossos.tasks WHERE NOT isDeleted;
 
 DROP INDEX webknossos.tasks_neededexperience_domain_neededexperience_value_idx;
-CREATE INDEX ON webknossos.tasks(neededExperience_domain, neededExperience_value);
+CREATE INDEX ON webknossos.tasks(_team, neededExperience_domain, neededExperience_value);
 COMMIT TRANSACTION;

b4d905f6b223fbd96b40d67429da0a1828ac7276
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-05-28 16:00:01 +0200

    yarn pretty

--- app/assets/javascripts/test/backend-snapshot-tests/projects.e2e.js
+++ app/assets/javascripts/test/backend-snapshot-tests/projects.e2e.js
@@ -1,7 +1,7 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
 /* eslint-disable import/first */
 // @flow
-import { tokenUserA,tokenUserD, setCurrToken } from "../enzyme/e2e-setup";
+import { tokenUserA, tokenUserD, setCurrToken } from "../enzyme/e2e-setup";
 import test from "ava";
 import _ from "lodash";
 import * as api from "admin/admin_rest_api";

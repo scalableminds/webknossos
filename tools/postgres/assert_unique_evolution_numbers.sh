b49590bd68c4f98aaf3d5ad16072a983fbe83d67
Jonathan Striebel <jonathan.striebel@gmail.com>
2018-06-19 15:26:49 +0200

    minor refactoring in assert_unique_evolution_numbers

--- tools/postgres/assert_unique_evolution_numbers.sh
+++ tools/postgres/assert_unique_evolution_numbers.sh
@@ -3,9 +3,7 @@ set -Eeuo pipefail
 
 scriptdir="$(dirname "$0")"
 
-numbers="$(basename -a $(echo "$scriptdir/../../conf/evolutions/"*.sql) | cut -d'-' -f1)"
-
-all="$(echo "$numbers" | sort)"
+all="$(basename -a $(echo "$scriptdir/../../conf/evolutions/"*.sql) | cut -d'-' -f1 | sort)"
 unique="$(echo "$all" | uniq)"
 
 diff <(echo "$all") <(echo "$unique")

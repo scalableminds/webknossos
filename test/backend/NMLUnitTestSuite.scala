55e2fa8550440aee522e3a066f3422c922188d56
Youri K <youri.kaminsky@student.hpi.de>
2018-07-03 17:13:43 +0200

    implement pr advice #2829

--- test/backend/NMLUnitTestSuite.scala
+++ test/backend/NMLUnitTestSuite.scala
@@ -5,7 +5,6 @@ import java.io.ByteArrayInputStream
 import com.scalableminds.webknossos.datastore.SkeletonTracing._
 import com.scalableminds.webknossos.datastore.VolumeTracing.VolumeTracing
 import com.scalableminds.webknossos.datastore.geometry.{Point3D, Vector3D}
-import com.typesafe.scalalogging.LazyLogging
 import models.annotation.nml.NmlParser
 import net.liftweb.common.{Box, Full}
 import org.scalatest.FlatSpec
@@ -17,13 +16,12 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 
-class NMLUnitTestSuite extends FlatSpec with LazyLogging {
-  logger.debug(s"test run")
+class NMLUnitTestSuite extends FlatSpec {
   val timestamp = 123456789
 
   def getObjectId = ObjectId.fromBsonId(BSONObjectID.generate)
 
-  def getParsedTracing(tracing: SkeletonTracing) = {
+  def writeAndParseTracing(tracing: SkeletonTracing): Box[(Either[SkeletonTracing, (VolumeTracing, String)], String)] = {
     val nmlEnumarator = NMLWriterTestStub.toNmlStream(tracing, None)
     val arrayFuture = Iteratee.flatten(nmlEnumarator |>> Iteratee.consume[Array[Byte]]()).run
     val array = Await.result(arrayFuture, Duration.Inf)
@@ -33,12 +31,10 @@ class NMLUnitTestSuite extends FlatSpec with LazyLogging {
   def isParseSuccessful(parsedTracing: Box[(Either[SkeletonTracing, (VolumeTracing, String)], String)]): Boolean = {
     parsedTracing match {
       case Full(either) => either match {
-        case (Left(tracing), _) => {
-          return true
-        }
-        case _ => return false
+        case (Left(_), _) => true
+        case _ => false
       }
-      case _=> return false
+      case _ => false
     }
   }
 
@@ -58,7 +54,7 @@ class NMLUnitTestSuite extends FlatSpec with LazyLogging {
   var dummyTracing = SkeletonTracing("dummy_dataset", Seq(tree1, tree2), timestamp, None, Some(1), Point3D(1, 1, 1), Vector3D(1.0, 1.0, 1.0), 1.0, 0, None, Seq(treeGroup1, treeGroup2))
 
   "NML writing and parsing" should "yield the same state" in {
-    getParsedTracing(dummyTracing) match {
+    writeAndParseTracing(dummyTracing) match {
       case Full(either) => either match {
         case (Left(tracing), _) => {
           assert(tracing == dummyTracing)
@@ -70,53 +66,56 @@ class NMLUnitTestSuite extends FlatSpec with LazyLogging {
   }
 
   "NML Parser" should "throw an error for invalid comment state" in {
+    // the comment nodeId is referring to a non-existent node therefore invalid
     val wrongTree = dummyTracing.trees(1).copy(comments = Seq(Comment(99, "test")))
     val newTracing = dummyTracing.copy(trees = Seq(dummyTracing.trees(0), wrongTree))
 
-    assert(true)//!isParseSuccessful(getParsedTracing(newTracing)))
+    assert(true) //!isParseSuccessful(getParsedTracing(newTracing)))
     //TODO: The parser currently doesn't check this
   }
 
   it should "throw an error for invalid branchPoint state" in {
+    // the branchPoint nodeId is referring to a non-existent node therefore invalid
     val wrongTree = dummyTracing.trees(1).copy(branchPoints = Seq(BranchPoint(99, 0)))
     val newTracing = dummyTracing.copy(trees = Seq(dummyTracing.trees(0), wrongTree))
 
-    assert(true)//!isParseSuccessful(getParsedTracing(newTracing)))
+    assert(true) //!isParseSuccessful(getParsedTracing(newTracing)))
     //TODO: The parser currently doesn't check this
   }
 
   it should "throw an error for invalid edge state" in {
+    // one of the nodeIds in the edge is referring to a non-existent node  therefore invalid
     val wrongTree = dummyTracing.trees(1).copy(edges = Seq(Edge(99, 5)))
     val newTracing = dummyTracing.copy(trees = Seq(dummyTracing.trees(0), wrongTree))
 
-    assert(!isParseSuccessful(getParsedTracing(newTracing)))
+    assert(!isParseSuccessful(writeAndParseTracing(newTracing)))
   }
 
   it should "throw an error for edge with same source and target state" in {
     val wrongTree = dummyTracing.trees(1).copy(edges = Edge(5, 5) +: dummyTracing.trees(1).edges)
     val newTracing = dummyTracing.copy(trees = Seq(dummyTracing.trees(0), wrongTree))
 
-    assert(!isParseSuccessful(getParsedTracing(newTracing)))
+    assert(!isParseSuccessful(writeAndParseTracing(newTracing)))
   }
 
   it should "throw an error for duplicate edge state" in {
     val wrongTree = dummyTracing.trees(1).copy(edges = Seq(Edge(4, 5), Edge(4, 5), Edge(5, 6)))
     val newTracing = dummyTracing.copy(trees = Seq(dummyTracing.trees(0), wrongTree))
 
-    assert(!isParseSuccessful(getParsedTracing(newTracing)))
+    assert(!isParseSuccessful(writeAndParseTracing(newTracing)))
   }
 
   it should "throw an error for disconnected tree state" in {
     val wrongTree = dummyTracing.trees(1).copy(edges = Seq(Edge(4, 5)))
     val newTracing = dummyTracing.copy(trees = Seq(dummyTracing.trees(0), wrongTree))
 
-    assert(!isParseSuccessful(getParsedTracing(newTracing)))
+    assert(!isParseSuccessful(writeAndParseTracing(newTracing)))
   }
 
   it should "throw an error for duplicate tree state" in {
     val newTracing = dummyTracing.copy(trees = Seq(dummyTracing.trees(0), dummyTracing.trees(0)))
 
-    assert(!isParseSuccessful(getParsedTracing(newTracing)))
+    assert(!isParseSuccessful(writeAndParseTracing(newTracing)))
   }
 
   it should "throw an error for duplicate node state" in {
@@ -124,21 +123,21 @@ class NMLUnitTestSuite extends FlatSpec with LazyLogging {
     val wrongTree = dummyTracing.trees(1).copy(nodes = Seq(duplicatedNode, duplicatedNode))
     val newTracing = dummyTracing.copy(trees = Seq(dummyTracing.trees(0), wrongTree))
 
-    assert(!isParseSuccessful(getParsedTracing(newTracing)))
+    assert(!isParseSuccessful(writeAndParseTracing(newTracing)))
   }
 
   it should "throw an error for missing groupId state" in {
     val wrongTree = dummyTracing.trees(1).copy(groupId = Some(9999))
     val newTracing = dummyTracing.copy(trees = Seq(dummyTracing.trees(0), wrongTree))
 
-    assert(true)//!isParseSuccessful(getParsedTracing(newTracing)))
+    assert(true) //!isParseSuccessful(getParsedTracing(newTracing)))
     //TODO: The parser currently doesn't check this
   }
 
   it should "throw an error for duplicate groupId state" in {
     val newTracing = dummyTracing.copy(treeGroups = TreeGroup("Group", 3) +: dummyTracing.treeGroups)
 
-    assert(true)//!isParseSuccessful(getParsedTracing(newTracing)))
+    assert(true) //!isParseSuccessful(getParsedTracing(newTracing)))
     //TODO: The parser currently doesn't check this
   }
 }

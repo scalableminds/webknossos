de96ad8c75aa8ee16b0c618c0a26fb2ba3e85978
Daniel Werner <daniel.werner@student.hpi.uni-potsdam.de>
2018-07-05 13:24:39 +0200

    fix linting (#2822)

--- app/assets/javascripts/test/tracing-view-screenshot-tests/dataset_rendering.screenshot.js
+++ app/assets/javascripts/test/tracing-view-screenshot-tests/dataset_rendering.screenshot.js
@@ -1,3 +1,4 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
 const test = require("ava");
 const puppeteer = require("puppeteer");
 const pixelmatch = require("pixelmatch");

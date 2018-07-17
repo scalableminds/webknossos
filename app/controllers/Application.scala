b2829c86bce1c2e18b66773316fafcee96778014
Florian M <florian@scm.io>
2018-07-17 13:39:58 +0200

    add schema versioning, show in buildinfo

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,7 +1,7 @@
 package controllers
 
+import com.scalableminds.util.tools.{Fox, FoxImplicits}
 import javax.inject.Inject
-
 import com.typesafe.config.ConfigRenderOptions
 import oxalis.security.WebknossosSilhouette.UserAwareAction
 import models.analytics.{AnalyticsDAO, AnalyticsEntry}
@@ -9,18 +9,26 @@ import models.binary.DataStoreHandlingStrategy
 import play.api.i18n.MessagesApi
 import play.api.Play.current
 import play.api.libs.json.Json
+import play.api.libs.concurrent.Execution.Implicits._
+import utils.SimpleSQLDAO
+import slick.jdbc.PostgresProfile.api._
 
 class Application @Inject()(val messagesApi: MessagesApi) extends Controller {
 
-  def buildInfo = UserAwareAction { implicit request =>
+  def buildInfo = UserAwareAction.async { implicit request =>
     val token = request.identity.flatMap { user =>
       if (user.isSuperUser) Some(DataStoreHandlingStrategy.webKnossosToken) else None
     }
-    Ok(Json.obj(
-      "webknossos" -> webknossos.BuildInfo.toMap.mapValues(_.toString),
-      "webknossos-wrap" -> webknossoswrap.BuildInfo.toMap.mapValues(_.toString),
-      "token" -> token
-    ))
+    for {
+      schemaVersion <- ReleaseInformationSQLDAO.getSchemaVersion.futureBox
+    } yield {
+      Ok(Json.obj(
+        "webknossos" -> webknossos.BuildInfo.toMap.mapValues(_.toString),
+        "webknossos-wrap" -> webknossoswrap.BuildInfo.toMap.mapValues(_.toString),
+        "schemaVersion" -> schemaVersion.toOption,
+        "token" -> token
+      ))
+    }
   }
 
   def analytics(namespace: String) = UserAwareAction(parse.json(1024 * 1024)) { implicit request =>
@@ -36,4 +44,14 @@ class Application @Inject()(val messagesApi: MessagesApi) extends Controller {
     Ok(current.configuration.underlying.getConfig("features").resolve.root.render(ConfigRenderOptions.concise()))
   }
 
+}
+
+
+object ReleaseInformationSQLDAO extends SimpleSQLDAO with FoxImplicits {
+  def getSchemaVersion = {
+    for {
+      rList <- run(sql"select schemaVersion from webknossos.releaseInformation".as[Int])
+      r <- rList.headOption.toFox
+    } yield r
+  }
 }

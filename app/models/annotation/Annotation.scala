099116a88931823ba9f029cc01da70d06b10ed79
Florian M <florian@scm.io>
2018-07-23 11:12:43 +0200

    send unformatted timestamps in task+annotation json

--- app/models/annotation/Annotation.scala
+++ app/models/annotation/Annotation.scala
@@ -98,7 +98,7 @@ case class AnnotationSQL(
       annotationRestrictions <- AnnotationRestrictions.writeAsJson(composeRestrictions(restrictions, readOnly), requestingUser)
     } yield {
       Json.obj(
-        "modified" -> DateTimeFormat.forPattern("yyyy-MM-dd HH:mm").print(modified),
+        "modified" -> modified,
         "state" -> state,
         "id" -> id,
         "name" -> name,

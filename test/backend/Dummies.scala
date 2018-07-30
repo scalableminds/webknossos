5b3cdc87635ace47ca9de3fa95640b2ce29fd4ef
Jonathan Striebel <jonathan.striebel@gmail.com>
2018-07-30 16:00:23 +0200

    add tests for update & deletion

---
+++ test/backend/Dummies.scala
@@ -0,0 +1,23 @@
+package backend
+
+import com.scalableminds.webknossos.datastore.SkeletonTracing._
+import com.scalableminds.webknossos.datastore.geometry.{Point3D, Vector3D}
+
+object Dummies {
+  val timestamp = 123456789
+
+  def createDummyNode(id: Int) = Node(id, Point3D(id, id, id), Vector3D(id, id, id), id, 1, 10, 8, id % 2 == 0, timestamp)
+
+  val tree1 = Tree(1, Seq(createDummyNode(0), createDummyNode(1), createDummyNode(2), createDummyNode(7)),
+    Seq(Edge(0, 1), Edge(2, 1), Edge(1, 7)), Some(Color(23, 23, 23, 1)), Seq(BranchPoint(1, 0), BranchPoint(7, 0)), Seq(Comment(0, "comment")),
+    "TestTree-0", timestamp, None)
+
+  val tree2 = Tree(2, Seq(createDummyNode(4), createDummyNode(5), createDummyNode(6)),
+    Seq(Edge(4, 5), Edge(5, 6)), Some(Color(30, 30, 30, 1)), Seq[BranchPoint](), Seq[Comment](),
+    "TestTree-1", timestamp, Some(1))
+
+  val treeGroup1 = TreeGroup("Axon 1", 1, Seq(TreeGroup("Blah", 3), TreeGroup("Blah 2", 4)))
+  val treeGroup2 = TreeGroup("Axon 2", 2)
+
+  val tracing = SkeletonTracing("dummy_dataset", Seq(tree1, tree2), timestamp, None, Some(1), Point3D(1, 1, 1), Vector3D(1.0, 1.0, 1.0), 1.0, 0, None, Seq(treeGroup1, treeGroup2))
+}
--- test/backend/SkeletonUpdateActionsUnitTestSuite.scala
+++ test/backend/SkeletonUpdateActionsUnitTestSuite.scala
@@ -8,48 +8,64 @@ import org.scalatest.FlatSpec
 
 
 class SkeletonUpdateActionsUnitTestSuite extends FlatSpec {
-  val timestamp = 123456789
 
-  object Dummy {
-    def createDummyNode(id: Int) = Node(id, Point3D(id, id, id), Vector3D(id, id, id), id, 1, 10, 8, id % 2 == 0, timestamp)
-
-    val tree1 = Tree(1, Seq(createDummyNode(0), createDummyNode(1), createDummyNode(2), createDummyNode(7)),
-      Seq(Edge(0, 1), Edge(2, 1), Edge(1, 7)), Some(Color(23, 23, 23, 1)), Seq(BranchPoint(1, 0), BranchPoint(7, 0)), Seq(Comment(0, "comment")),
-      "TestTree-0", timestamp, None)
-
-    val tree2 = Tree(2, Seq(createDummyNode(4), createDummyNode(5), createDummyNode(6)),
-      Seq(Edge(4, 5), Edge(5, 6)), Some(Color(30, 30, 30, 1)), Seq[BranchPoint](), Seq[Comment](),
-      "TestTree-1", timestamp, Some(1))
-
-    val treeGroup1 = TreeGroup("Axon 1", 1, Seq(TreeGroup("Blah", 3), TreeGroup("Blah 2", 4)))
-    val treeGroup2 = TreeGroup("Axon 2", 2)
-
-    val tracing = SkeletonTracing("dummy_dataset", Seq(tree1, tree2), timestamp, None, Some(1), Point3D(1, 1, 1), Vector3D(1.0, 1.0, 1.0), 1.0, 0, None, Seq(treeGroup1, treeGroup2))
-
-    def applyUpdateAction(action: UpdateAction.SkeletonUpdateAction) =
-      action.applyOn(tracing)
-  }
+  def applyUpdateAction(action: UpdateAction.SkeletonUpdateAction) =
+    action.applyOn(Dummies.tracing)
 
 
-
-
-  "CreateTreeSkeletonAction" should "add a new tree with the correct id" in {
+  "CreateTreeSkeletonAction" should "add the specified tree" in {
     val createTreeAction = new CreateTreeSkeletonAction(
       id = 1000,
       color = None,
-      name = "some stuff",
-      branchPoints = List(UpdateActionBranchPoint(0, timestamp)),
-      timestamp = timestamp,
+      name = "new tree",
+      branchPoints = List(UpdateActionBranchPoint(0, backend.Dummies.timestamp)),
+      timestamp = backend.Dummies.timestamp,
       comments = List[UpdateActionComment]()
     )
-    val result = Dummy.applyUpdateAction(createTreeAction)
+    val result = applyUpdateAction(createTreeAction)
 
-    assert(result.trees.length == Dummy.tracing.trees.length + 1)
+    assert(result.trees.length == backend.Dummies.tracing.trees.length + 1)
     result.trees.find(_.treeId == createTreeAction.id) match {
       case Some(tree) => {
-        assert(tree.branchPoints == List(BranchPoint(0, timestamp)))
-        assert(tree.createdTimestamp == timestamp)
+        assert(tree.branchPoints == List(BranchPoint(0, backend.Dummies.timestamp)))
+        assert(tree.createdTimestamp == backend.Dummies.timestamp)
         assert(tree.comments == createTreeAction.comments)
+        assert(tree.name === createTreeAction.name)
+      }
+      case _ => throw new Exception
+    }
+  }
+
+  "DeleteTreeSkeletonAction" should "delete the specified tree" in {
+    val deleteTreeAction = new DeleteTreeSkeletonAction(id = 1)
+    val result = applyUpdateAction(deleteTreeAction)
+
+    assert(result.trees.length == backend.Dummies.tracing.trees.length - 1)
+    result.trees.find(_.treeId == deleteTreeAction.id) match {
+      case Some(tree) => throw new Exception
+      case None =>
+    }
+  }
+
+  "UpdateTreeSkeletonAction" should "update the specified tree" in {
+    val updateTreeAction = new UpdateTreeSkeletonAction(
+      id = 1,
+      updatedId = Some(1000),
+      color = None,
+      name = "updated tree",
+      branchPoints = List(UpdateActionBranchPoint(0, backend.Dummies.timestamp)),
+      comments = List[UpdateActionComment](),
+      groupId = None
+    )
+    val result = applyUpdateAction(updateTreeAction)
+
+    assert(result.trees.length == backend.Dummies.tracing.trees.length)
+    result.trees.find(_.treeId == 1000) match {
+      case Some(tree) => {
+        assert(tree.branchPoints == List(BranchPoint(0, backend.Dummies.timestamp)))
+        assert(tree.createdTimestamp == backend.Dummies.timestamp)
+        assert(tree.comments == updateTreeAction.comments)
+        assert(tree.name === updateTreeAction.name)
       }
       case _ => throw new Exception
     }

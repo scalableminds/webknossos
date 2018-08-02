528433953c178facb84a7bf812e18b951e3624ce
Florian M <fm3@users.noreply.github.com>
2018-08-02 13:07:38 +0200

    rename AnnotationTypeSQL to AnnotationType (#2988)

--- app/models/annotation/handler/AnnotationInformationHandler.scala
+++ app/models/annotation/handler/AnnotationInformationHandler.scala
@@ -2,17 +2,17 @@ package models.annotation.handler
 
 import com.scalableminds.util.accesscontext.DBAccessContext
 import com.scalableminds.util.tools.{Fox, FoxImplicits}
-import models.annotation.AnnotationTypeSQL.AnnotationTypeSQL
+import models.annotation.AnnotationType.AnnotationType
 import models.annotation._
 import models.user.User
 import play.api.libs.concurrent.Execution.Implicits._
 import utils.ObjectId
 
 object AnnotationInformationHandler {
-  val informationHandlers: Map[AnnotationTypeSQL, AnnotationInformationHandler] = Map(
-    AnnotationTypeSQL.CompoundProject -> ProjectInformationHandler,
-    AnnotationTypeSQL.CompoundTask     -> TaskInformationHandler,
-    AnnotationTypeSQL.CompoundTaskType -> TaskTypeInformationHandler)
+  val informationHandlers: Map[AnnotationType, AnnotationInformationHandler] = Map(
+    AnnotationType.CompoundProject -> ProjectInformationHandler,
+    AnnotationType.CompoundTask     -> TaskInformationHandler,
+    AnnotationType.CompoundTaskType -> TaskTypeInformationHandler)
       .withDefaultValue(SavedTracingInformationHandler)
 }
 

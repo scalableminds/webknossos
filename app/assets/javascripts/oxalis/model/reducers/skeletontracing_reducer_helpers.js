9e9cb77e3b1331e2bf7af06e02cc94d4aeee2ce3
Philipp Otto <philipp.4096@googlemail.com>
2018-08-13 17:01:06 +0200

    pretty code

--- app/assets/javascripts/oxalis/model/reducers/skeletontracing_reducer_helpers.js
+++ app/assets/javascripts/oxalis/model/reducers/skeletontracing_reducer_helpers.js
@@ -68,7 +68,10 @@ function getMaximumTreeId(trees: TreeMapType): number {
   return _.max(_.map(trees, "treeId"));
 }
 
-export function* mapGroups<R>(groups: Array<TreeGroupType>, callback: TreeGroupType => R): Generator<R, void, void> {
+export function* mapGroups<R>(
+  groups: Array<TreeGroupType>,
+  callback: TreeGroupType => R,
+): Generator<R, void, void> {
   for (const group of groups) {
     yield callback(group);
     if (group.children) {

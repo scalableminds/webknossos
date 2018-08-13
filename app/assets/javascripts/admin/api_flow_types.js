d099fd77b5ec5a49eef73d6a49a2174bdbafcb7f
MichaelBuessemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>
2018-08-13 10:49:20 +0200

    Show active users modal  (#2863)
    * Added usersWithOpenTasks to the admin api. Added a button to the projects-view that displays all users with open tasks of the selected project in a modal
    
    * added method to transfer many tasks at once to admin_rest_api, created a modified copy of the task_transfer_modal to control a bulk task transfer
    
    * adding a fetch function, that fetches all users with open tasks -> onhold, waiting for backend to provide the api request
    
    * creating mock data, ongoing
    
    * added mock data, the modal is now working and displayable but has no real functionallity
    
    * fixed flow errors
    
    * fixed flow errors and added some error handling if no project was selected
    
    * [WIP] transfer annotations to different user for api #2862
    
    * [WIP] route for transferring active tasks of a project to a user #2862
    
    * routes for showing active tasks and transfering them #2862
    
    * added changelog, moved error message to messages, fixed flow error
    
    * added the request for active users to the admin_rest_api and used it to display the table in the modal
    
    * added a type for activeUsers, added an api request to transfer all active tasks of a project, transfer modal only renders if set to visible
    
    * reverted changelog -> entry for this issue still missing
    
    * added changelog entry
    
    * added message for successful transfer, added transfer functionality, and change api call to post-type
    
    * corrected changelog entry
    
    * added error handling to the transfer request, added an error message to messages
    
    * refeshed snapshots
    
    * refactored requested changes
    
    * updated snapshots and resolved auto merging errors
    
    * fixed flow type problems
    
    * fixed linter issues
    
    * added a spinner while fetching data
    
    * disallow transferring tasks to users who cannot access the dataset

--- app/assets/javascripts/admin/api_flow_types.js
+++ app/assets/javascripts/admin/api_flow_types.js
@@ -121,6 +121,11 @@ export type APIUserLoggedTimeType = {
   loggedTime: Array<APITimeIntervalType>,
 };
 
+export type APIActiveUserType = {
+  email: string,
+  activeTasks: number,
+};
+
 export type APIRestrictionsType = {|
   +allowAccess: boolean,
   +allowUpdate: boolean,
--- app/models/annotation/Annotation.scala
+++ app/models/annotation/Annotation.scala
@@ -206,6 +206,20 @@ object AnnotationDAO extends SQLDAO[Annotation, AnnotationsRow, Annotations] {
       parsed <- Fox.combined(r.toList.map(parse))
     } yield parsed
 
+  // hint: does not use access query (because they dont support prefixes yet). use only after separate access check
+  def findAllActiveForProject(projectId: ObjectId)(implicit ctx: DBAccessContext): Fox[List[ObjectId]] =
+    for {
+      accessQuery <- readAccessQuery
+      r <- run(sql""" select a._id from
+                        webknossos.annotations_ a
+                        join webknossos.tasks_ t on a._task = t._id
+                        join webknossos.projects_ p on t._project = p._id
+                        join webknossos.users_ u on a._user = u._id
+                        where p._id = ${projectId}
+                        and a.state = '#${AnnotationState.Active.toString}'
+                        and a.typ = '#${AnnotationType.Task}' """.as[String])
+    } yield r.map(ObjectId(_)).toList
+
   def findAllByTaskIdAndType(taskId: ObjectId, typ: AnnotationType)(implicit ctx: DBAccessContext): Fox[List[Annotation]] =
     for {
       r <- run(Annotations.filter(r => notdel(r) && r._Task === taskId.id && r.typ === typ.toString && r.state =!= AnnotationState.Cancelled.toString).result)

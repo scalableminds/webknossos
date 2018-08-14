f3ce11465a9c0a1cc7fffe360d917b819fee178f
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-08-14 16:40:02 +0200

    edited evolution

--- conf/evolutions/020-add-foreign-datastore.sql
+++ conf/evolutions/020-add-foreign-datastore.sql
@@ -4,7 +4,7 @@
 START TRANSACTION;
 
 DROP VIEW webknossos.datastores_;
-ALTER TABLE webknossos.datastores add isForeign BOOLEAN;
+ALTER TABLE webknossos.datastores add isForeign BOOLEAN NOT NULL DEFAULT false;
 UPDATE webknossos.datastores set isForeign = false;
 CREATE VIEW webknossos.dataStores_ AS SELECT * FROM webknossos.dataStores WHERE NOT isDeleted;
 

e8d18ec45f1e28d7a78757739d53fb2c17281138
Florian M <florian@scm.io>
2018-08-20 10:08:42 +0200

    re-enable dnd nml upload in dashboard

--- app/assets/javascripts/dashboard/dashboard_view.js
+++ app/assets/javascripts/dashboard/dashboard_view.js
@@ -197,7 +197,7 @@ class DashboardView extends React.PureComponent<Props, State> {
     ) : null;
 
     return (
-      <NmlUploadZoneContainer onImport={this.uploadNmls}>
+      <NmlUploadZoneContainer onImport={this.uploadNmls} isAllowed={true}>
         <div className="container">
           {userHeader}
           <Tabs

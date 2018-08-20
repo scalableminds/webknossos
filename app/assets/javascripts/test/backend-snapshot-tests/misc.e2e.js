026dd923333de98e7bd05f87a9a82f2e28c779a8
Daniel <daniel.werner@student.hpi.uni-potsdam.de>
2018-08-20 12:00:30 +0200

    Flow types vs backend types (#3065)
    * write first version of function that writes files to check flow types against backend objects, add function to misc, annotation, task, dataset e2e files
    
    * add flow checks to remainder of e2e files, add timetracking data to test db and update snapshots
    
    * update snapshots, add eslintignore
    
    * run flow after e2e tests to check the generated files as well
    
    * adjust text in gitkeep file for flow-check
    
    * apply PR feedback
    
    * apply PR feedback #2

--- app/assets/javascripts/test/backend-snapshot-tests/misc.e2e.js
+++ app/assets/javascripts/test/backend-snapshot-tests/misc.e2e.js
@@ -1,7 +1,12 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
 // @flow
 import test from "ava";
-import { tokenUserA, setCurrToken, resetDatabase } from "test/enzyme/e2e-setup";
+import {
+  tokenUserA,
+  setCurrToken,
+  resetDatabase,
+  writeFlowCheckingFile,
+} from "test/enzyme/e2e-setup";
 import * as api from "admin/admin_rest_api";
 
 test.before("Reset database and change token", async () => {
@@ -11,15 +16,18 @@ test.before("Reset database and change token", async () => {
 
 test("datastores()", async t => {
   const datastores = await api.getDatastores();
+  writeFlowCheckingFile(datastores, "datastore", "APIDataStoreType", { isArray: true });
   t.snapshot(datastores, { id: "misc-datastores" });
 });
 
 test("activeUser()", async t => {
   const activeUser = await api.getActiveUser();
+  writeFlowCheckingFile(activeUser, "user", "APIUserType");
   t.snapshot(activeUser, { id: "misc-activeUser" });
 });
 
 test("getFeatureToggles()", async t => {
   const features = await api.getFeatureToggles();
+  writeFlowCheckingFile(features, "feature-toggles", "APIFeatureToggles");
   t.snapshot(features, { id: "misc-getFeatureToggles" });
 });

d9079f9dc0e3315f45fe51d3bfbb15eb890366ea
Florian M <florian@scm.io>
2018-08-27 10:24:16 +0200

    fix syntax error in evolution reversion 23

--- conf/evolutions/reversions/023-drop-datastore-type.sql
+++ conf/evolutions/reversions/023-drop-datastore-type.sql
@@ -1,7 +1,7 @@
 START TRANSACTION;
 
 DROP VIEW webknossos.datastores_;
-ALTER TABLE webknossos.datastores add typ webknossos.DATASTORE_TYPE NOT NULL DEFAULT 'webknossos-store',
+ALTER TABLE webknossos.datastores add column typ webknossos.DATASTORE_TYPE NOT NULL DEFAULT 'webknossos-store';
 CREATE VIEW webknossos.dataStores_ AS SELECT * FROM webknossos.dataStores WHERE NOT isDeleted;
 
 UPDATE webknossos.releaseInformation set schemaVersion = 22;

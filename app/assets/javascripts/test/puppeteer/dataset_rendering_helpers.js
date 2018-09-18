7d953789e6d3e8a083e1d98928beef940779387c
Daniel Werner <daniel.werner@student.hpi.uni-potsdam.de>
2018-09-18 17:40:04 +0200

    use babel for screenshot tests, add flow types to screenshot test files, fix diff

--- app/assets/javascripts/test/puppeteer/dataset_rendering_helpers.js
+++ app/assets/javascripts/test/puppeteer/dataset_rendering_helpers.js
@@ -1,29 +1,39 @@
+// @flow
 /* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}], no-await-in-loop: 0 */
-const urljoin = require("url-join");
-const fetch = require("node-fetch");
-const pixelmatch = require("pixelmatch");
-const mergeImg = require("merge-img");
+import urljoin from "url-join";
+import pixelmatch from "pixelmatch";
+import mergeImg from "merge-img";
+import type { Page } from "puppeteer";
+import { createExplorational } from "../../admin/admin_rest_api";
 
-const DEV_AUTH_TOKEN = "secretScmBoyToken";
+export const DEV_AUTH_TOKEN = "secretScmBoyToken";
 
-async function createExplorational(datasetName, typ, withFallback, baseUrl) {
-  const fullUrl = urljoin(baseUrl, `/api/datasets/${datasetName}/createExplorational`);
-  return (await fetch(fullUrl, {
-    body: JSON.stringify({ typ, withFallback }),
-    method: "POST",
+type Screenshot = {
+  screenshot: Buffer,
+  width: number,
+  height: number,
+};
+
+function getDefaultRequestOptions(baseUrl: string) {
+  return {
+    host: baseUrl,
     headers: {
       "X-Auth-Token": DEV_AUTH_TOKEN,
-      "Content-Type": "application/json",
     },
-  })).json();
+  };
 }
 
-async function screenshotDataset(page, baseUrl, datasetName) {
-  const createdExplorational = await createExplorational(datasetName, "skeleton", false, baseUrl);
+export async function screenshotDataset(
+  page: Page,
+  baseUrl: string,
+  datasetName: string,
+): Promise<Screenshot> {
+  const options = getDefaultRequestOptions(baseUrl);
+  const createdExplorational = await createExplorational(datasetName, "skeleton", false, options);
   return openTracingViewAndScreenshot(page, baseUrl, createdExplorational.id);
 }
 
-async function waitForTracingViewLoad(page) {
+async function waitForTracingViewLoad(page: Page) {
   let inputCatchers;
   while (inputCatchers == null || inputCatchers.length < 4) {
     inputCatchers = await page.$(".inputcatcher");
@@ -31,9 +41,14 @@ async function waitForTracingViewLoad(page) {
   }
 }
 
-async function waitForRenderingFinish(page) {
-  const tdView = await page.$("#inputcatcher_TDView");
-  const { width, height } = await tdView.boundingBox();
+async function waitForRenderingFinish(page: Page): Promise<Buffer> {
+  const tdViewSelector = "#inputcatcher_TDView";
+  const tdView = await page.$(tdViewSelector);
+  if (tdView == null)
+    throw new Error(`Element ${tdViewSelector} not present, although page is loaded.`);
+  const boundingBox = await tdView.boundingBox();
+  if (boundingBox == null) throw new Error(`Element ${tdViewSelector} has no bounding box.`);
+  const { width, height } = boundingBox;
 
   let currentShot;
   let lastShot = await tdView.screenshot();
@@ -50,27 +65,29 @@ async function waitForRenderingFinish(page) {
     lastShot = currentShot;
   }
 
-  return { screenshot: currentShot, width, height };
+  return currentShot;
 }
 
-async function openTracingViewAndScreenshot(page, baseUrl, annotationId) {
+async function openTracingViewAndScreenshot(
+  page: Page,
+  baseUrl: string,
+  annotationId: string,
+): Promise<Screenshot> {
   await page.goto(urljoin(baseUrl, `/annotations/Explorational/${annotationId}`), {
     timeout: 0,
   });
 
   await waitForTracingViewLoad(page);
 
-  const {
-    screenshot: tdViewScreenshot,
-    width: tdViewWidth,
-    height: tdViewHeight,
-  } = await waitForRenderingFinish(page);
+  const tdViewScreenshot = await waitForRenderingFinish(page);
 
   // Take screenshots of the other rendered planes
   const PLANE_IDS = ["#inputcatcher_PLANE_XY", "#inputcatcher_PLANE_YZ", "#inputcatcher_PLANE_XZ"];
   const screenshots = [tdViewScreenshot];
   for (const planeId of PLANE_IDS) {
     const element = await page.$(planeId);
+    if (element == null)
+      throw new Error(`Element ${planeId} not present, although page is loaded.`);
     const screenshot = await element.screenshot();
     screenshots.push(screenshot);
   }
@@ -81,14 +98,11 @@ async function openTracingViewAndScreenshot(page, baseUrl, annotationId) {
     img.getBuffer("image/png", (_, buffer) =>
       resolve({
         screenshot: buffer,
-        width: tdViewWidth * 4,
-        height: tdViewHeight,
+        width: img.bitmap.width,
+        height: img.bitmap.height,
       }),
     ),
   );
 }
 
-module.exports = {
-  screenshotDataset,
-  DEV_AUTH_TOKEN,
-};
+export default {};

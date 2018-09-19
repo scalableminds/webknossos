67655352045d547086f3291d43adfb4314d7ff27
Daniel <daniel.werner@student.hpi.uni-potsdam.de>
2018-09-19 14:22:54 +0200

    Repair screenshot tests (#3247)
    * concat screenshots of all planes to fix screenshots tests for custom viewports
    
    * use babel for screenshot tests, add flow types to screenshot test files, fix diff
    
    * update screenshots
    
    * remove fps meter and wait until page no longer changes, then screenshot

--- app/assets/javascripts/test/puppeteer/dataset_rendering_helpers.js
+++ app/assets/javascripts/test/puppeteer/dataset_rendering_helpers.js
@@ -1,58 +1,115 @@
+// @flow
 /* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}], no-await-in-loop: 0 */
-const urljoin = require("url-join");
-const fetch = require("node-fetch");
-const pixelmatch = require("pixelmatch");
+import urljoin from "url-join";
+import pixelmatch from "pixelmatch";
+import mergeImg from "merge-img";
+import type { Page } from "puppeteer";
+import { createExplorational } from "../../admin/admin_rest_api";
 
-const DEV_AUTH_TOKEN = "secretScmBoyToken";
+export const DEV_AUTH_TOKEN = "secretScmBoyToken";
 
-async function createExplorational(datasetName, typ, withFallback, baseUrl) {
-  const fullUrl = urljoin(baseUrl, `/api/datasets/${datasetName}/createExplorational`);
-  return (await fetch(fullUrl, {
-    body: JSON.stringify({ typ, withFallback }),
-    method: "POST",
+type Screenshot = {
+  screenshot: Buffer,
+  width: number,
+  height: number,
+};
+
+function getDefaultRequestOptions(baseUrl: string) {
+  return {
+    host: baseUrl,
     headers: {
       "X-Auth-Token": DEV_AUTH_TOKEN,
-      "Content-Type": "application/json",
     },
-  })).json();
+  };
 }
 
-async function screenshotDataset(page, baseUrl, datasetName) {
-  const createdExplorational = await createExplorational(datasetName, "skeleton", false, baseUrl);
+export async function screenshotDataset(
+  page: Page,
+  baseUrl: string,
+  datasetName: string,
+): Promise<Screenshot> {
+  const options = getDefaultRequestOptions(baseUrl);
+  const createdExplorational = await createExplorational(datasetName, "skeleton", false, options);
   return openTracingViewAndScreenshot(page, baseUrl, createdExplorational.id);
 }
 
-async function openTracingViewAndScreenshot(page, baseUrl, annotationId) {
-  await page.goto(urljoin(baseUrl, `/annotations/Explorational/${annotationId}`), {
-    timeout: 0,
+function removeFpsMeter(page: Page) {
+  return page.evaluate(() => {
+    const fpsMeter = document.querySelector("#stats");
+    if (fpsMeter != null) {
+      const { parentNode } = fpsMeter;
+      if (parentNode != null) parentNode.removeChild(fpsMeter);
+    }
   });
+}
 
-  let canvas;
-  while (canvas == null) {
-    canvas = await page.$("#render-canvas");
+async function waitForTracingViewLoad(page: Page) {
+  let inputCatchers;
+  while (inputCatchers == null || inputCatchers.length < 4) {
+    inputCatchers = await page.$(".inputcatcher");
     await page.waitFor(500);
   }
-  const { width, height } = await canvas.boundingBox();
+}
+
+async function waitForRenderingFinish(page: Page) {
+  // Remove the FPS meter as it will update even after the rendering is finished
+  await removeFpsMeter(page);
 
   let currentShot;
-  let lastShot = await canvas.screenshot();
+  let lastShot = await page.screenshot();
   let changedPixels = Infinity;
-  // If the screenshot didn't change in the last x seconds, we're probably done
+  // If the screenshot of the page didn't change in the last x seconds, rendering should be finished
   while (currentShot == null || changedPixels > 0) {
     await page.waitFor(10000);
-    currentShot = await canvas.screenshot();
+    currentShot = await page.screenshot({ fullPage: true });
     if (lastShot != null) {
-      changedPixels = pixelmatch(lastShot, currentShot, {}, width, height, {
+      changedPixels = pixelmatch(lastShot, currentShot, {}, 1920, 1080, {
         threshold: 0.0,
       });
     }
     lastShot = currentShot;
   }
-
-  return { screenshot: currentShot, width, height };
 }
 
-module.exports = {
-  screenshotDataset,
-  DEV_AUTH_TOKEN,
-};
+async function openTracingViewAndScreenshot(
+  page: Page,
+  baseUrl: string,
+  annotationId: string,
+): Promise<Screenshot> {
+  await page.goto(urljoin(baseUrl, `/annotations/Explorational/${annotationId}`), {
+    timeout: 0,
+  });
+
+  await waitForTracingViewLoad(page);
+  await waitForRenderingFinish(page);
+
+  // Take screenshots of the other rendered planes
+  const PLANE_IDS = [
+    "#inputcatcher_TDView",
+    "#inputcatcher_PLANE_XY",
+    "#inputcatcher_PLANE_YZ",
+    "#inputcatcher_PLANE_XZ",
+  ];
+  const screenshots = [];
+  for (const planeId of PLANE_IDS) {
+    const element = await page.$(planeId);
+    if (element == null)
+      throw new Error(`Element ${planeId} not present, although page is loaded.`);
+    const screenshot = await element.screenshot();
+    screenshots.push(screenshot);
+  }
+
+  // Concatenate all screenshots
+  const img = await mergeImg(screenshots);
+  return new Promise(resolve =>
+    img.getBuffer("image/png", (_, buffer) =>
+      resolve({
+        screenshot: buffer,
+        width: img.bitmap.width,
+        height: img.bitmap.height,
+      }),
+    ),
+  );
+}
+
+export default {};
--- tools/test.sh
+++ tools/test.sh
@@ -17,7 +17,7 @@ function prepare {
   # Webpack with the proto loader isn't used when running the tests, so the proto files need to be prepared manually
   pbjs -t json "webknossos-datastore/proto/SkeletonTracing.proto" > "$testBundlePath/SkeletonTracing.proto.json"
   pbjs -t json "webknossos-datastore/proto/VolumeTracing.proto" > "$testBundlePath/VolumeTracing.proto.json"
-  BABEL_ENV=test babel "$jsPath" --out-dir "$testBundlePath" $additionalParams
+  BABEL_ENV=test babel --quiet "$jsPath" --out-dir "$testBundlePath" $additionalParams
 }
 
 function ensureUpToDateTests {
@@ -43,6 +43,10 @@ elif [ $cmd == "test-e2e" ]
 then
   ensureUpToDateTests
   export NODE_PATH="$testBundlePath" && BABEL_ENV=test nyc --silent --no-clean ava $(find "$testBundlePath" -name "*.e2e.js") --serial "$@"
+elif [ $cmd == "test-screenshot" ]
+then
+  ensureUpToDateTests
+  export NODE_PATH="$testBundlePath" && BABEL_ENV=test ava $(find "$testBundlePath" -name "*.screenshot.js") "$@"
 elif [ $cmd == "prepare" ]
 then
   prepare "$@"

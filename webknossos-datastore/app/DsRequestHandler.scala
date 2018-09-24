78a907e81df142e3960932f1f48d062b7beb31c1
Philipp Otto <philipp.4096@googlemail.com>
2018-09-24 11:02:13 +0200

    allow accessing missing-buckets-header in cors requests (fixes #3269)

--- webknossos-datastore/app/DsRequestHandler.scala
+++ webknossos-datastore/app/DsRequestHandler.scala
@@ -14,7 +14,8 @@ class DsRequestHandler @Inject() (router: Router, errorHandler: HttpErrorHandler
           "Access-Control-Allow-Origin" -> "*",
           "Access-Control-Max-Age" -> "600",
           "Access-Control-Allow-Methods" -> "POST, GET, DELETE, PUT, HEAD, PATCH, OPTIONS",
-          "Access-Control-Allow-Headers" -> request.headers.get("Access-Control-Request-Headers").getOrElse(""))
+          "Access-Control-Allow-Headers" -> request.headers.get("Access-Control-Request-Headers").getOrElse("")),
+          "Access-Control-Expose-Headers" -> "MISSING-BUCKETS"
       })
     } else {
       super.routeRequest(request)

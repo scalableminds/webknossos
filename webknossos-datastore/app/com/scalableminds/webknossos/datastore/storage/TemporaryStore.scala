714f8ee9f66c33dfecedfee75d61947c35b19952
Florian M <florian@scm.io>
2018-09-25 11:32:29 +0200

    use scala synchronized for temporary store

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/storage/TemporaryStore.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/storage/TemporaryStore.scala
@@ -10,58 +10,42 @@ import scala.concurrent.stm.Ref
 
 class TemporaryStore[K, V] @Inject()(system: ActorSystem) {
 
-  lazy val ts = Agent[Map[K, V]](Map())
+  lazy val map: scala.collection.mutable.Map[K, V] = scala.collection.mutable.Map()
 
   def find(id: K) =
-    ts().get(id)
+    map.synchronized {
+      map.get(id)
+    }
 
   def findAll =
-    ts().values.toList
+    map.synchronized {
+      map.values.toList
+    }
 
   def removeAll =
-    ts.send(Map.empty[K, V])
+    map.synchronized {
+      map.clear()
+    }
 
   def removeAllExcept(l: Array[K]) =
-    ts.send(_.filterKeys(l.contains))
+    map.synchronized {
+      map.filterKeys(l.contains)
+    }
 
   def insert(id: K, t: V, to: Option[FiniteDuration] = None) = {
-    ts.send(_ + (id -> t))
+    map.synchronized {
+      map += (id -> t)
+    }
     to.foreach(system.scheduler.scheduleOnce(_)(remove(id)))
   }
 
   def insertAll(els: (K, V)*) =
-    ts.send(_ ++ els)
+    map.synchronized {
+      map ++= els
+    }
 
   def remove(id: K) =
-    ts.send(_ - id)
-}
-
-// New
-
-object MyActor{
-  case class Find[K](id: K)
-  case class FindAll[K]()
-  case class RemoveAll[K]()
-  case class RemoveAllExcept[K](l: Array[K])
-  case class Insert[K, V](id: K, t: V, to: Option[FiniteDuration] = None)
-  case class InsertAll[K, V](els: (K,V)*)
-  case class Remove[K](id: K)
-
-  def apply[K,V](initialMap: Map[K, V]): MyActor[K, V] = new MyActor[K, V](initialMap)
-}
-
-class MyActor[K, V](initialMap: Map[K, V]) extends Actor {
-  private val ref = Ref[Map[K,V]](initialMap)
-
-  private def getMap() = ref.single.get
-
-  def receive[K,V] = {
-    case MyActor.Find(id: K)        => getMap().get(id)
-    case MyActor.FindAll            => getMap().values.toList
-    case MyActor.RemoveAll          => // TODO
-    case MyActor.RemoveAllExcept(l) => // TODO
-    case MyActor.Insert(id, t, to)  => // TODO
-    case MyActor.InsertAll(els)     => // TODO
-    case MyActor.Remove(id)         => // TODO
-  }
+    map.synchronized {
+      map -= id
+    }
 }

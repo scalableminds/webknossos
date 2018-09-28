41de759921c89bea11e17019529a443164e90866
Jonathan Striebel <jonathan.striebel@gmail.com>
2018-09-28 10:22:09 +0200

    cleanup plugins & resolvers

--- project/DependencyResolvers.scala
+++ project/DependencyResolvers.scala
@@ -1,26 +1,18 @@
 import sbt._
 
 object DependencyResolvers {
-  val sonaRels = "sonatype rels" at "https://oss.sonatype.org/content/repositories/releases/"
-  val sonaSnaps = "sonatype snaps" at "https://oss.sonatype.org/content/repositories/snapshots/"
-  val sgSnaps = "sgodbillon" at "https://bitbucket.org/sgodbillon/repository/raw/master/snapshots/"
-  val typesafeRel = "typesafe" at "http://repo.typesafe.com/typesafe/releases"
   val scmRel = "scm.io releases S3 bucket" at "https://s3-eu-central-1.amazonaws.com/maven.scm.io/releases/"
-  val scmRelGithub = Resolver.url("Scalableminds REL Repo", url("http://scalableminds.github.com/releases/"))(Resolver.ivyStylePatterns)
   val scmSnaps = "scm.io snapshots S3 bucket" at "https://s3-eu-central-1.amazonaws.com/maven.scm.io/snapshots/"
   val teamon = "teamon.eu repo" at "http://repo.teamon.eu"
-  val bintray = "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
   val atlassian = "Atlassian Releases" at "https://maven.atlassian.com/public/"
 
   val dependencyResolvers = Seq(
-    sonaRels,
-    sonaSnaps,
-    sgSnaps,
-    typesafeRel,
+    Resolver.sonatypeRepo("releases"),
+    Resolver.sonatypeRepo("snapshots"),
+    Resolver.typesafeRepo("releases"),
     scmRel,
-    scmRelGithub,
     scmSnaps,
-    bintray,
+    Resolver.bintrayRepo("scalaz", "releases"),
     teamon,
     atlassian
   )
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,15 +1,6 @@
-resolvers ++= Seq(
-    DefaultMavenRepository,
-    "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
-)
-
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.18")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.0.3")
-
-addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.7.0")
-
-addSbtPlugin("com.frugalmechanic" % "fm-sbt-s3-resolver" % "0.16.0")
+addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.9.0")
 
 addSbtPlugin("com.thesamet" % "sbt-protoc" % "0.99.18")
 

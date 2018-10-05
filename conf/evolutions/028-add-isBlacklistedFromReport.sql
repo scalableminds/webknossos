4c731fdaadce0fc9d856763bb5016c5a0a9a8c11
Youri K <youri-k@users.noreply.github.com>
2018-10-05 22:54:48 +0200

    Update 028-add-isBlacklistedFromReport.sql

--- conf/evolutions/028-add-isBlacklistedFromReport.sql
+++ conf/evolutions/028-add-isBlacklistedFromReport.sql
@@ -2,7 +2,7 @@
 
 START TRANSACTION;
 DROP VIEW webknossos.projects_;
-ALTER TABLE webknossos.projects ADD COLUMN isBlacklistedFromReport;
+ALTER TABLE webknossos.projects ADD COLUMN isBlacklistedFromReport BOOLEAN NOT NULL DEFAULT false;
 CREATE VIEW webknossos.projects_ AS SELECT * FROM webknossos.projects WHERE NOT isDeleted;
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 28;

c86583b36a2cf71432b7fe1e5cd64acc94550928
Florian M <florian@scm.io>
2018-10-23 16:36:23 +0200

    fix views in evolutions

--- conf/evolutions/032-scratch-datastores.sql
+++ conf/evolutions/032-scratch-datastores.sql
@@ -2,7 +2,10 @@
 
 START TRANSACTION;
 
+DROP VIEW webknossos.dataStores_;
 ALTER TABLE webknossos.dataStores ADD COLUMN isScratch BOOLEAN NOT NULL DEFAULT false;
+
+CREATE VIEW webknossos.dataStores_ AS SELECT * FROM webknossos.dataStores WHERE NOT isDeleted;
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 32;
 
--- conf/evolutions/reversions/32-scratch-datastores.sql
+++ conf/evolutions/reversions/32-scratch-datastores.sql
@@ -1,6 +1,8 @@
 START TRANSACTION;
 
+DROP VIEW webknossos.dataStores_;
 ALTER TABLE webknossos.dataStores DROP COLUMN isScratch;
+CREATE VIEW webknossos.dataStores_ AS SELECT * FROM webknossos.dataStores WHERE NOT isDeleted;
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 31;
 

2e0d1cfa4d224e4265473617eb436391abfd405e
Youri K <youri.kaminsky@student.hpi.de>
2018-11-01 15:10:48 +0100

    try to serve assets from on route requests

--- app/RequestHandler.scala
+++ app/RequestHandler.scala
@@ -1,8 +1,11 @@
+import java.nio.file.{Files, Paths}
+
 import javax.inject.Inject
 import play.api.http.{DefaultHttpRequestHandler, HttpConfiguration, HttpErrorHandler, HttpFilters}
 import play.api.mvc.Results.Ok
 import play.api.mvc.{Action, Handler, InjectedController, RequestHeader}
 import play.api.routing.Router
+import play.api.Play._
 import utils.WkConf
 
 class RequestHandler @Inject() (router: Router,
@@ -13,9 +16,14 @@ class RequestHandler @Inject() (router: Router,
   extends DefaultHttpRequestHandler(router, errorHandler, httpConfiguration, filters) with InjectedController {
 
   override def routeRequest(request: RequestHeader): Option[Handler] = {
-    if (request.uri.matches("^(/api/|/data/|/tracings/|/assets/).*$")) {
+    if (request.uri.matches("^(/api/|/data/|/tracings/).*$")) {
       super.routeRequest(request)
-    } else None
+    } else {
+      if(Files.exists(Paths.get("/public", request.path)))
+        Some(controllers.Assets.at(path="/public", file=request.path))
+      else
+        Some(controllers.Assets.at(path="/public", file="bundle/index.html"))
+    }
   }
 
 }

3e4dd2ead55444d3dc3be11310abde65de06ea54
Philipp Otto <philippotto@users.noreply.github.com>
2018-11-12 13:42:17 +0100

    fix keyboardjs module (#3450)

--- app/assets/javascripts/libs/keyboard.js
+++ app/assets/javascripts/libs/keyboard.js
@@ -1,7 +1,5 @@
 /* eslint-disable */
 
-import window from "libs/window";
-
 !(function(e) {
   if ("object" == typeof exports && "undefined" != typeof module) module.exports = e();
   else if ("function" == typeof define && define.amd) define([], e);

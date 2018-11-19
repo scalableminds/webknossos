53169447576365a4cdc4a3fd9f024e36754fa852
Florian M <florian@scm.io>
2018-11-19 15:37:10 +0100

    add names, dont exit 1 on failure

--- .circleci/slack-notification.sh
+++ .circleci/slack-notification.sh
@@ -3,11 +3,23 @@ set -Eeuo pipefail
 
 if [ "${CIRCLE_BRANCH}" == "master" ] || [ "${CIRCLE_BRANCH}" == "circleci-custom-notification" ]; then
     author=${CIRCLE_USERNAME}
-    author=${author/fm3/florian}
-    author=${author/jstriebel/jonathan}
+    author=${author/fm3/<@florian>}
+    author=${author/jstriebel/<@jonathan>}
+    author=${author/daniel-wer/<@daniel>}
+    author=${author/georgwiese/<@georg>}
+    author=${author/hotzenklotz/<@tom>}
+    author=${author/jfrohnhofen/<@johannes>}
+    author=${author/MichaelBuessemeyer/<@michael>}
+    author=${author/normanrz/<@norman>}
+    author=${author/philippotto/<@philipp>}
+    author=${author/rschwanhold/<@robert>}
+    author=${author/tmbo/<@tmbo>}
+    author=${author/valentin-pinkau/<@valentin>}
     channel="webknossos-bots"
     commitmsg=$(git log --format=%s -n 1)
-    mesg="<@${author}> your ${CIRCLE_BRANCH} build <https://circleci.com/gh/scalableminds/webknossos/${CIRCLE_BUILD_NUM}|${CIRCLE_BUILD_NUM}> (${commitmsg}) is almost finished."
+    buildlink="<https://circleci.com/gh/scalableminds/webknossos/${CIRCLE_BUILD_NUM}|${CIRCLE_BUILD_NUM}>"
+    prlink="<${CIRCLE_PULL_REQUEST}|PR #${CIRCLE_PR_NUMBER}>"
+    mesg="${author} your ${CIRCLE_BRANCH} build ${buildlink} (${commitmsg} ${prlink}) is almost finished."
     echo $mesg
     user="circleci-custom"
     token="${SLACK_NOTIFY_TOKEN:-}"
@@ -21,8 +33,7 @@ if [ "${CIRCLE_BRANCH}" == "master" ] || [ "${CIRCLE_BRANCH}" == "circleci-custo
         https://slack.com/api/chat.postMessage
     )
     if [[ "$(echo ${res} | jq '.ok')" == "false" ]]; then
-        echo "Error sending Slack notification: $(echo ${res} | jq -r '.error')."
-        exit 1
+        echo "[WARN] Error sending Slack notification: $(echo ${res} | jq -r '.error')."
     fi
-    echo "Sent Slack notification to ${channel}."
+    echo "[INFO] Sent Slack notification to ${channel}."
 fi

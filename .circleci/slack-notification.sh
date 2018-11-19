5fac959d5b630eb08fb48deed062131ca9135fdb
Florian M <florian@scm.io>
2018-11-19 15:12:18 +0100

    customize message

--- .circleci/slack-notification.sh
+++ .circleci/slack-notification.sh
@@ -1,20 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -Eeuo pipefail
 
-channel="webknossos-bots"
-mesg="Let’s test sending custom circleci notifications to slack. This is build ${CIRCLE_BUILD_NUM}"
-user="circleci-custom"
-token="${SLACK_NOTIFY_TOKEN:-}"
-res=$(curl -s \
-    -X POST \
-    -d "token=${token}" \
-    -d "channel=${channel}" \
-    -d "text=${mesg}" \
-    -d "username=${user}" \
-    -d "icon_url=https://a.slack-edge.com/41b0a/img/plugins/circleci/service_48.png" \
-    https://slack.com/api/chat.postMessage
-)
-if [[ "$(echo ${res} | jq '.ok')" == "false" ]]; then
-    echo "Error sending Slack notification: $(echo ${res} | jq -r '.error')."
-    exit 1
+if [ "${CIRCLE_BRANCH}" == "master" ] || [ "${CIRCLE_BRANCH}" == "circleci-custom-notification" ]; then
+    author=${CIRCLE_USERNAME}
+    author=${author/fm3/florian}
+    author=${author/jstriebel/jonathan}
+    channel="webknossos-bots"
+    mesg="@${author} your ${CIRCLE_BRANCH} build ${CIRCLE_BUILD_NUM} is almost finished."
+    echo $mesg
+    user="circleci-custom"
+    token="${SLACK_NOTIFY_TOKEN:-}"
+    res=$(curl -s \
+        -X POST \
+        -d "token=${token}" \
+        -d "channel=${channel}" \
+        -d "text=${mesg}" \
+        -d "username=${user}" \
+        -d "icon_url=https://a.slack-edge.com/41b0a/img/plugins/circleci/service_48.png" \
+        https://slack.com/api/chat.postMessage
+    )
+    if [[ "$(echo ${res} | jq '.ok')" == "false" ]]; then
+        echo "Error sending Slack notification: $(echo ${res} | jq -r '.error')."
+        exit 1
+    fi
+    echo "Sent Slack notification to ${channel}."
 fi
-echo "Sent Slack notification to ${channel}."

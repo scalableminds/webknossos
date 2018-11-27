adc42ed9a6ce9727db876f3f5fa9a2253af1d72c
Daniel Werner <daniel.werner@scalableminds.com>
2018-11-27 12:21:18 +0100

    fix schemaVersion and update MIGRATIONS.md

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -21,7 +21,7 @@ START TRANSACTION;
 CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(35);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(36);
 COMMIT TRANSACTION;
 
 CREATE TABLE webknossos.analytics(

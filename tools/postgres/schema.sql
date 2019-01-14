8b010c601cbac322f288c6b01da8ce3d970d51e7
Florian M <florian@scm.io>
2019-01-14 15:59:35 +0100

    update migration guide, schema number

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -21,7 +21,7 @@ START TRANSACTION;
 CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(36);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(37);
 COMMIT TRANSACTION;
 
 CREATE TABLE webknossos.analytics(

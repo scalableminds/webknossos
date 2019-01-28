8ee18c468815705c0ca523d285c9fae84d64510a
Florian M <florian@scm.io>
2019-01-28 10:26:52 +0100

    remove transaction block for evolution 38

--- conf/evolutions/038-more-voxel-types.sql
+++ conf/evolutions/038-more-voxel-types.sql
@@ -1,9 +1,9 @@
--- https://github.com/scalableminds/webknossos/pull/TODO
+-- https://github.com/scalableminds/webknossos/pull/3686
 
 -- note that it is very complex to reverse this (which is why we don't do it), compare also
 -- https://www.postgresql.org/message-id/CANu8FiwwBxZZGX23%3DNa_7bc4DZ-yzd_poKhaoPmN3%2BSHG08MAg%40mail.gmail.com
 
-START TRANSACTION;
+--unfortunately, in postgresql, ALTER TYPE ... ADD cannot run inside a transaction block, so no transaction here
 
 ALTER TYPE webknossos.DATASET_LAYER_ELEMENT_CLASS ADD VALUE 'float';
 ALTER TYPE webknossos.DATASET_LAYER_ELEMENT_CLASS ADD VALUE 'double';
@@ -14,4 +14,3 @@ ALTER TYPE webknossos.DATASET_LAYER_ELEMENT_CLASS ADD VALUE 'int64';
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 38;
 
-COMMIT TRANSACTION;

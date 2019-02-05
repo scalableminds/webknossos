b206bca34bdfccc41deaa84723ce6123f534c53d
Florian M <florian@scm.io>
2019-02-05 13:02:42 +0100

    update evolution numbers

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -21,7 +21,7 @@ START TRANSACTION;
 CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(38);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(39);
 COMMIT TRANSACTION;
 
 CREATE TABLE webknossos.analytics(

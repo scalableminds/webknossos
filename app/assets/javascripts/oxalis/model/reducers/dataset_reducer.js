5651920ac36cd4b070b685cfdb526ec44fc3f20b
Philipp Otto <philipp.4096@googlemail.com>
2019-02-06 17:22:58 +0100

    add missing files

---
+++ app/assets/javascripts/oxalis/model/reducers/dataset_reducer.js
@@ -0,0 +1,35 @@
+// @flow
+import _ from "lodash";
+
+import type { Action } from "oxalis/model/actions/actions";
+import type { OxalisState } from "oxalis/store";
+import { updateKey2 } from "oxalis/model/helpers/deep_update";
+
+function DatasetReducer(state: OxalisState, action: Action): OxalisState {
+  switch (action.type) {
+    case "SET_DATASET": {
+      const { dataset } = action;
+      return {
+        ...state,
+        dataset,
+      };
+    }
+    case "SET_LAYER_MAPPINGS": {
+      const { layerName, mappingNames } = action;
+      const newLayers = state.dataset.dataSource.dataLayers.map(layer => {
+        if (layer.category === "segmentation" && layer.name === layerName) {
+          return { ...layer, mappings: mappingNames };
+        } else {
+          return layer;
+        }
+      });
+      return updateKey2(state, "dataset", "dataSource", { dataLayers: newLayers });
+    }
+    default:
+    // pass;
+  }
+
+  return state;
+}
+
+export default DatasetReducer;

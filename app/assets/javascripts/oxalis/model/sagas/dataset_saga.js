07550e337706840c096eaea344e5daacf311ef41
Philipp Otto <philipp.4096@googlemail.com>
2019-02-06 14:52:46 +0100

    comment code for annoying scratch toast

--- app/assets/javascripts/oxalis/model/sagas/dataset_saga.js
+++ app/assets/javascripts/oxalis/model/sagas/dataset_saga.js
@@ -4,10 +4,9 @@ import Toast from "libs/toast";
 import messages from "messages";
 
 export default function* watchIsScratchSaga(): Saga<void> {
-  yield* take("WK_READY");
-
-  const isScratch = yield* select(state => state.dataset.dataStore.isScratch);
-  if (isScratch) {
-    Toast.error(messages["dataset.is_scratch"], { sticky: true });
-  }
+  // yield* take("WK_READY");
+  // const isScratch = yield* select(state => state.dataset.dataStore.isScratch);
+  // if (isScratch) {
+  //   Toast.error(messages["dataset.is_scratch"], { sticky: true });
+  // }
 }

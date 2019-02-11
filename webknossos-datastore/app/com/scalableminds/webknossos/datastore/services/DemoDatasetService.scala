ab27fcd35c491b41d84f3d755f3b53aa4c43bec8
Florian M <florian@scm.io>
2019-02-11 17:15:28 +0100

    prepare download + guard

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/DemoDatasetService.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/DemoDatasetService.scala
@@ -1,11 +1,34 @@
 package com.scalableminds.webknossos.datastore.services
 
+import java.util.concurrent.atomic.AtomicBoolean
+
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
 import com.google.inject.Inject
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import com.scalableminds.util.tools.Fox
+import com.scalableminds.webknossos.datastore.rpc.RPC
 
-class DemoDatasetService @Inject()() {
-  def initDownload(organizationName: String) = {
-    Fox.successful()
+class DemoDatasetService @Inject()(
+                                    rpc: RPC) {
+  var isDownloadRunning = new AtomicBoolean(false)
+
+  def initDownload(organizationName: String): Fox[Unit] = {
+    if (isDownloadRunning.compareAndSet(false, true)) {
+      download
+      Fox.successful()
+    } else {
+      Fox.failure("alreadyRunning")
+    }
   }
+
+  def download: Fox[Unit] = {
+    for {
+      response <- rpc("https://localhost/cremi.zip").get
+      bytes = response.bodyAsBytes
+      _ = isDownloadRunning.set(false)
+    } yield ()
+  }
+
 }

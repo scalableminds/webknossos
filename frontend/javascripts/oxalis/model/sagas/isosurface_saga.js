ca4474b94ae962eebe32702c192daabae3ce8ead
Philipp Otto <philipp.4096@googlemail.com>
2019-02-21 18:40:54 +0100

    load isosurface using breadth-first instead of depth-first strategy (fixes #3713)

--- frontend/javascripts/oxalis/model/sagas/isosurface_saga.js
+++ frontend/javascripts/oxalis/model/sagas/isosurface_saga.js
@@ -107,7 +107,7 @@ function* ensureSuitableIsosurface(): Saga<void> {
 
   batchCounterPerSegment[segmentId] = 0;
   yield* call(
-    maybeLoadIsosurface,
+    loadIsosurfaceWithNeighbors,
     dataset,
     layer,
     segmentId,
@@ -117,7 +117,7 @@ function* ensureSuitableIsosurface(): Saga<void> {
   );
 }
 
-function* maybeLoadIsosurface(
+function* loadIsosurfaceWithNeighbors(
   dataset: APIDataset,
   layer: DataLayer,
   segmentId: number,
@@ -125,13 +125,38 @@ function* maybeLoadIsosurface(
   zoomStep: number,
   resolutions: Array<Vector3>,
 ): Saga<void> {
+  let positionsToRequest = [clippedPosition];
+
+  while (positionsToRequest.length > 0) {
+    const position = positionsToRequest.shift();
+    const neighbors = yield* call(
+      maybeLoadIsosurface,
+      dataset,
+      layer,
+      segmentId,
+      position,
+      zoomStep,
+      resolutions,
+    );
+    positionsToRequest = positionsToRequest.concat(neighbors);
+  }
+}
+
+function* maybeLoadIsosurface(
+  dataset: APIDataset,
+  layer: DataLayer,
+  segmentId: number,
+  clippedPosition: Vector3,
+  zoomStep: number,
+  resolutions: Array<Vector3>,
+): Saga<Array<Vector3>> {
   const threeDMap = getMapForSegment(segmentId);
 
   if (threeDMap.get(clippedPosition)) {
-    return;
+    return [];
   }
   if (batchCounterPerSegment[segmentId] > (window.__isosurfaceMaxBatchSize || MAXIMUM_BATCH_SIZE)) {
-    return;
+    return [];
   }
   batchCounterPerSegment[segmentId]++;
 
@@ -157,18 +182,9 @@ function* maybeLoadIsosurface(
   const vertices = new Float32Array(responseBuffer);
   getSceneController().addIsosurface(vertices, segmentId);
 
-  for (const neighbor of neighbors) {
-    const neighborPosition = getNeighborPosition(clippedPosition, neighbor, zoomStep, resolutions);
-    yield* call(
-      maybeLoadIsosurface,
-      dataset,
-      layer,
-      segmentId,
-      neighborPosition,
-      zoomStep,
-      resolutions,
-    );
-  }
+  return neighbors.map(neighbor =>
+    getNeighborPosition(clippedPosition, neighbor, zoomStep, resolutions),
+  );
 }
 
 export default function* isosurfaceSaga(): Saga<void> {

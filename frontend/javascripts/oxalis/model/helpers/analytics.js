58a138e706d72d417fdeeed4aa559545f97bbc7f
Philipp Otto <philipp.4096@googlemail.com>
2019-02-25 09:58:02 +0100

    track wk version to analytics

--- frontend/javascripts/oxalis/model/helpers/analytics.js
+++ frontend/javascripts/oxalis/model/helpers/analytics.js
@@ -8,24 +8,37 @@ function getOrganization() {
   return activeUser != null ? activeUser.organization : null;
 }
 
+function gaGuard(...params) {
+  console.log("GA", ...params);
+  if (typeof window.ga !== "undefined" && window.ga !== null) {
+    window.ga(...params);
+  }
+}
+
 // The void return type is needed for flow to check successfully
 export function trackAction(action: string): void {
-  if (typeof window.ga !== "undefined" && window.ga !== null) {
-    window.ga("send", "event", "Action", action, getOrganization());
-  }
+  gaGuard("send", "event", "Action", action, getOrganization());
+}
+
+export function trackVersion(version: string): void {
+  gaGuard("send", {
+    hitType: "event",
+    eventCategory: "pageLoad",
+    eventAction: "version",
+    eventLabel: version,
+  });
 }
 
 export function googleAnalyticsLogClicks(evt: MouseEvent) {
   // This function logs all clicks on elements that contain text to google analytics
-  if (typeof window.ga !== "undefined" && window.ga !== null) {
-    // Flow doesn't allow to check for the textContent property otherwise
-    const target = ((evt.target: any): Node);
-    if (target.textContent != null) {
-      // Restrict the textContent to a maximum length
-      const textContent = target.textContent.trim().slice(0, 50);
-      if (textContent.length > 0) {
-        window.ga("send", "event", "Click", textContent, getOrganization());
-      }
+
+  // Flow doesn't allow to check for the textContent property otherwise
+  const target = ((evt.target: any): Node);
+  if (target.textContent != null) {
+    // Restrict the textContent to a maximum length
+    const textContent = target.textContent.trim().slice(0, 50);
+    if (textContent.length > 0) {
+      gaGuard("send", "event", "Click", textContent, getOrganization());
     }
   }
 }

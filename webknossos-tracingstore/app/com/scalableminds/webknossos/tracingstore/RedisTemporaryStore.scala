5d14628186e0975ac9b11c5690950cdebc33a898
Florian M <florian@scm.io>
2019-03-04 15:37:28 +0100

    prepare redis temporary store

---
+++ webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/RedisTemporaryStore.scala
@@ -0,0 +1,57 @@
+package com.scalableminds.webknossos.tracingstore
+
+import com.redis._
+import javax.inject.Inject
+
+import scala.concurrent.duration.FiniteDuration
+
+class RedisTemporaryStore[V] @Inject()() {
+  val r = new RedisClient("localhost", 6379)
+
+  def find(id: String) =
+    r.synchronized {
+      r.get(id)
+    }
+
+  def removeAllConditional(pattern: String) =
+    r.synchronized {
+      val keysOpt: Option[List[Option[String]]] = r.keys(pattern)
+      keysOpt.map { keys: Seq[Option[String]] =>
+        keys.flatMap { key: Option[String] =>
+          key.flatMap(r.del(_))
+        }
+      }.getOrElse(Seq())
+    }
+
+  def findAllConditional(pattern: String) =
+    r.synchronized {
+      val keysOpt: Option[List[Option[String]]] = r.keys(pattern)
+      keysOpt.map { keys: Seq[Option[String]] =>
+        keys.flatMap { key: Option[String] =>
+          key.flatMap(r.get(_))
+        }
+      }.getOrElse(Seq())
+    }
+
+  def keys(pattern: String) =
+    r.synchronized {
+      r.keys(pattern).map(_.flatten).getOrElse(List())
+    }
+
+  def insert(id: String, t: V, expirationOpt: Option[FiniteDuration] = None) =
+    r.synchronized {
+      expirationOpt
+        .map(
+          expiration => r.setex(id, expiration.toSeconds, t)
+        )
+        .getOrElse(
+          r.set(id, t)
+        )
+    }
+
+  def remove(id: String) =
+    r.synchronized {
+      r.del(id)
+    }
+
+}

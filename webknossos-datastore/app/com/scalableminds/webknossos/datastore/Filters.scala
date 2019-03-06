34fa6fd538e4782075ff74537e4e81ab83a0c23f
Philipp Otto <philipp.4096@googlemail.com>
2019-03-06 16:01:18 +0100

    disable security filter to enable iframe embedding

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/Filters.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/Filters.scala
@@ -6,5 +6,5 @@ import play.api.http.HttpFilters
 import play.filters.headers.SecurityHeadersFilter
 
 class Filters @Inject()(securityHeadersFilter: SecurityHeadersFilter) extends HttpFilters {
-  def filters = Seq(securityHeadersFilter)
+  def filters = Seq()
 }

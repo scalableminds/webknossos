8c8cbad9d7c6ce2dcfdd657983046faa924d0f01
Philipp Otto <philipp.4096@googlemail.com>
2019-03-19 17:30:45 +0100

    only disable frameOptions instead of whole security headers filter

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/Filters.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/Filters.scala
@@ -6,5 +6,5 @@ import play.api.http.HttpFilters
 import play.filters.headers.SecurityHeadersFilter
 
 class Filters @Inject()(securityHeadersFilter: SecurityHeadersFilter) extends HttpFilters {
-  def filters = Seq()
+  def filters = Seq(securityHeadersFilter)
 }

ec1415cb300f1dea7d6e72c4e6a90b153232aae3
Daniel Werner <daniel.werner@scalableminds.com>
2019-04-09 15:58:42 +0200

    refactor automatic brushing code into own module

--- frontend/javascripts/oxalis/model/sagas/volumetracing_saga.js
+++ frontend/javascripts/oxalis/model/sagas/volumetracing_saga.js
@@ -1,18 +1,10 @@
-/**
- * volumetracing_saga.js
- * @flow
- */
+// @flow
 import _ from "lodash";
-import * as tf from "@tensorflow/tfjs";
-import memoizeOne from "memoize-one";
 
-import floodfill from "libs/floodfill";
-import { FlycamActions } from "oxalis/model/actions/flycam_actions";
 import {
   type CopySegmentationLayerAction,
   resetContourAction,
   updateDirectionAction,
-  type InferSegmentationInViewportAction,
 } from "oxalis/model/actions/volumetracing_actions";
 import {
   type Saga,
@@ -27,26 +19,17 @@ import {
   take,
 } from "oxalis/model/sagas/effect-generators";
 import { type UpdateAction, updateVolumeTracing } from "oxalis/model/sagas/update_actions";
-import { V2, V3 } from "libs/mjs";
+import { V3 } from "libs/mjs";
 import type { VolumeTracing, Flycam } from "oxalis/store";
 import {
   enforceVolumeTracing,
   isVolumeTracingDisallowed,
 } from "oxalis/model/accessors/volumetracing_accessor";
-import { getBaseVoxelFactors } from "oxalis/model/scaleinfo";
-import {
-  getPosition,
-  getRotation,
-  getPlaneExtentInVoxelFromStore,
-} from "oxalis/model/accessors/flycam_accessor";
-import { sleep } from "libs/utils";
+import { getPosition, getRotation } from "oxalis/model/accessors/flycam_accessor";
 import {
   type BoundingBoxType,
-  type Vector2,
-  type Vector3,
   type ContourMode,
   ContourModeEnum,
-  OrthoViews,
   type OrthoView,
   type VolumeTool,
   VolumeToolEnum,
@@ -56,17 +39,9 @@ import Model from "oxalis/model";
 import Toast from "libs/toast";
 import VolumeLayer from "oxalis/model/volumetracing/volumelayer";
 import api from "oxalis/api/internal_api";
-import { getMeanAndStdDevFromDataset } from "admin/admin_rest_api";
-import type { APIDataset } from "admin/api_flow_types";
-import { createWorker } from "oxalis/workers/comlink_wrapper";
-import TensorFlowWorker from "oxalis/workers/tensorflow.worker";
-import mainThreadPredict from "oxalis/workers/tensorflow.impl";
-import ThreeDMap from "libs/ThreeDMap";
-
-// Will remove model validation, NaN checks, and other correctness checks in favor of performance
-if (process.env.NODE_ENV === "production") tf.enableProdMode();
-
-const workerPredict = createWorker(TensorFlowWorker);
+import inferSegmentInViewport, {
+  getHalfViewportExtents,
+} from "oxalis/model/sagas/automatic_brush_saga";
 
 export function* watchVolumeTracingAsync(): Saga<void> {
   yield* take("WK_READY");
@@ -89,20 +64,6 @@ function* warnOfTooLowOpacity(): Saga<void> {
       'Your setting for "segmentation opacity" is set very low.<br />Increase it for better visibility while volume tracing.',
     );
   }
-}
-
-function* getHalfViewportExtents(activeViewport): Saga<Vector2> {
-  const zoom = yield* select(state => state.flycam.zoomStep);
-  const baseVoxelFactors = yield* select(state =>
-    Dimensions.transDim(getBaseVoxelFactors(state.dataset.dataSource.scale), activeViewport),
-  );
-  const viewportExtents = yield* select(state =>
-    getPlaneExtentInVoxelFromStore(state, zoom, activeViewport),
-  );
-  const scaledViewportExtents = V2.scale2(viewportExtents, baseVoxelFactors);
-
-  const halfViewportExtents = scaledViewportExtents.map(extent => Math.round(extent / 2));
-  return halfViewportExtents;
 }
 
 export function* editVolumeLayerAsync(): Generator<any, any, any> {
@@ -256,183 +217,6 @@ function* copySegmentationLayer(action: CopySegmentationLayerAction): Saga<void>
       );
     }
   }
-}
-
-const configureTensorFlow = (useWebworker, useGPU) => {
-  window.useWebworker = useWebworker;
-  window.useGPU = useGPU;
-  console.log("useWebworker set to", useWebworker, "and useGPU set to", useGPU);
-};
-
-// $FlowIgnore
-const isOffscreenCanvasSupported = typeof OffscreenCanvas !== "undefined";
-configureTensorFlow(isOffscreenCanvasSupported, true);
-window.configureTensorFlow = configureTensorFlow;
-
-const meanAndStdDevFromDataset = memoizeOne(
-  (dataset: APIDataset, layerName: string): Promise<{ mean: number, stdDev: number }> =>
-    getMeanAndStdDevFromDataset(dataset.dataStore.url, dataset, layerName),
-);
-
-function* inferSegmentInViewport(action: InferSegmentationInViewportAction): Saga<void> {
-  const allowUpdate = yield* select(state => state.tracing.restrictions.allowUpdate);
-  if (!allowUpdate) return;
-
-  const activeViewport = yield* select(state => state.viewModeData.plane.activeViewport);
-  if (activeViewport === "TDView") return;
-
-  const outputExtent = 100;
-  const inputContextExtent = 20;
-  const inputExtent = outputExtent + 2 * inputContextExtent;
-  const NUM_PREDICT_SLICES = 3;
-  const FLOODFILL_THRESHOLD = 0.7;
-
-  let aborted = false;
-  const toastConfig = {
-    onClose: () => {
-      aborted = true;
-    },
-    sticky: true,
-    key: "automatic-brush",
-  };
-  const toastDescription = "Automatic brush is active. Close the toast to stop using it.";
-
-  Toast.info(toastDescription, toastConfig);
-
-  const colorLayers = yield* call([Model, Model.getColorLayers]);
-  const colorLayer = colorLayers[0];
-  if (colorLayers.length > 1) {
-    Toast.warning(
-      `There are multiple color layers. Using ${colorLayer.name} for automatic segmentation.`,
-    );
-  }
-  const [halfViewportExtentX, halfViewportExtentY] = yield* call(
-    getHalfViewportExtents,
-    activeViewport,
-  );
-  const dataset = yield* select(state => state.dataset);
-  const { mean, stdDev } = yield* call(meanAndStdDevFromDataset, dataset, colorLayer.name);
-  const activeCellId = yield* select(state => enforceVolumeTracing(state.tracing).activeCellId);
-
-  let [curX, curY, curZ] = Dimensions.transDim(
-    Dimensions.roundCoordinate(yield* select(state => getPosition(state.flycam))),
-    activeViewport,
-  );
-  const clickPosition = Dimensions.transDim(
-    Dimensions.roundCoordinate(action.position),
-    activeViewport,
-  );
-
-  const predictions = new ThreeDMap();
-  const useWebworker = window.useWebworker != null ? window.useWebworker : false;
-  const useGPU = window.useGPU != null ? window.useGPU : false;
-  console.log("useWebworker", useWebworker);
-  console.log("useGPU", useGPU);
-
-  const getPredictionForTile = async (tileX, tileY, tileZ) => {
-    const x = tileX * outputExtent;
-    const y = tileY * outputExtent;
-    const z = tileZ;
-    const min = Dimensions.transDim(
-      [x - inputContextExtent, y - inputContextExtent, z],
-      activeViewport,
-    );
-    const max = Dimensions.transDim(
-      [x - inputContextExtent + inputExtent, y - inputContextExtent + inputExtent, z + 1],
-      activeViewport,
-    );
-
-    const cuboidData = await api.data.getDataFor2DBoundingBox(colorLayer.name, {
-      min,
-      max,
-    });
-
-    const tensorArray = new Float32Array(new Uint8Array(cuboidData)).map(
-      el => (el - mean) / stdDev,
-    );
-    // When interpreting the 3d data slice as a 2d slice, the x and y axis are flipped only on the YZ plane
-    const isXYflipped = activeViewport === OrthoViews.PLANE_YZ;
-
-    return useWebworker
-      ? workerPredict(useGPU, tensorArray.buffer, inputExtent, isXYflipped)
-      : mainThreadPredict(useGPU, tf, tensorArray.buffer, inputExtent, isXYflipped);
-  };
-
-  function* currentPositionUpdater(): Saga<void> {
-    while (!aborted) {
-      yield* take(FlycamActions);
-      const curPosition = Dimensions.transDim(
-        Dimensions.roundCoordinate(yield* select(state => getPosition(state.flycam))),
-        activeViewport,
-      );
-      [curX, curY, curZ] = curPosition;
-    }
-  }
-
-  const getter = async (x, y, z): Promise<boolean> => {
-    if (
-      x < curX - halfViewportExtentX ||
-      y < curY - halfViewportExtentY ||
-      z < curZ ||
-      x >= curX + halfViewportExtentX ||
-      y >= curY + halfViewportExtentY
-    ) {
-      return false;
-    }
-
-    // If the current z-slice is too far ahead, wait
-    // eslint-disable-next-line no-await-in-loop
-    while (z >= curZ + NUM_PREDICT_SLICES && !aborted) await sleep(500);
-
-    const tileX = Math.floor(x / outputExtent);
-    const tileY = Math.floor(y / outputExtent);
-    const tileZ = z;
-
-    if (predictions.get([tileZ, tileY, tileX]) == null) {
-      // Do not make new predictions if the automatic brush has been aborted
-      if (aborted) return false;
-      console.time("predict");
-      Toast.info(`${toastDescription}\nLabeling slice ${z}.`, toastConfig);
-      predictions.set([tileZ, tileY, tileX], await getPredictionForTile(tileX, tileY, tileZ));
-      console.timeEnd("predict");
-    }
-
-    const relX = x % outputExtent;
-    const relY = y % outputExtent;
-    const prediction = predictions.get([tileZ, tileY, tileX]);
-    if (prediction != null) return prediction[relX * outputExtent + relY] > FLOODFILL_THRESHOLD;
-    throw new Error("This should never happen, prediction was set, but geting it failed.");
-  };
-
-  const onFlood = async (floodedVoxels: Array<Vector3>) => {
-    console.time("label");
-    if (floodedVoxels.length > 0) {
-      // Clean up - If the whole z slice has been flooded, the prediction is no longer needed
-      const [, , tileZ] = floodedVoxels[0];
-      predictions.delete(tileZ);
-    }
-    const voxelAddresses = floodedVoxels.map(addressUVW =>
-      Dimensions.transDim(addressUVW, activeViewport),
-    );
-    api.data.labelVoxels(voxelAddresses, activeCellId);
-    console.timeEnd("label");
-    // Do not block the main thread and allow for some interactivity after labeling a slice
-    await sleep(500);
-  };
-
-  const seed = clickPosition;
-  const seedPrediction = yield* call(getter, ...seed);
-  if (seedPrediction) {
-    // Keep updating the current position
-    yield* fork(currentPositionUpdater);
-
-    // The floodfill will run until aborted or exhausted
-    yield* call(floodfill, { getter, seed, onFlood });
-  } else {
-    Toast.warning("Click position is classified as border, please click inside a segment instead.");
-  }
-
-  Toast.close(toastConfig.key);
 }
 
 export function* finishLayer(

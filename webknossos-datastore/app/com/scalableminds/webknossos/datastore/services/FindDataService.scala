5019f073f4a2dbc8a18dd3bada1c521c1fde2395
Youri K <youri.kaminsky@student.hpi.de>
2019-04-18 16:10:23 +0200

    implement data conversion

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/FindDataService.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/FindDataService.scala
@@ -17,13 +17,6 @@ import scala.reflect.ClassTag
 class FindDataService @Inject()(dataServicesHolder: BinaryDataServiceHolder)(implicit ec: ExecutionContext)
     extends FoxImplicits {
   val binaryDataService: BinaryDataService = dataServicesHolder.binaryDataService
-  var i = 0
-
-  def findPositionWithData(dataSource: DataSource, dataLayer: DataLayer)(
-      implicit m: MessagesProvider): Fox[Option[(Point3D, Point3D)]] =
-    for {
-      positionAndResolutionOpt <- checkAllPositionsForData(dataSource, dataLayer)
-    } yield positionAndResolutionOpt
 
   private def convertData(data: Array[Byte],
                           elementClass: ElementClass.Value): Array[_ >: Byte with Short with Int with Long] =
@@ -48,62 +41,36 @@ class FindDataService @Inject()(dataServicesHolder: BinaryDataServiceHolder)(imp
     dstArray
   }
 
-  private def checkAllPositionsForData(dataSource: DataSource,
-                                       dataLayer: DataLayer): Fox[Option[(Point3D, Point3D)]] = {
-
-    def getExactDataOffset(data: Array[Byte]): Point3D = {
-      val cubeLength = DataLayer.bucketLength / dataLayer.bytesPerElement
-      val convertedData = convertData(data, dataLayer.elementClass)
-      for {
-        z <- 0 until cubeLength
-        y <- 0 until cubeLength
-        x <- 0 until cubeLength
-      } {
-        val voxelOffset = x + y * cubeLength + z * cubeLength * cubeLength
-        if (convertedData(voxelOffset) != 0) return Point3D(x, y, z)
-      }
-      Point3D(0, 0, 0)
-    }
-
-    def searchPositionIter(positions: List[Point3D], resolution: Point3D): Fox[Option[Point3D]] =
-      positions match {
-        case List() => Fox.successful(None)
-        case head :: tail =>
-          checkIfPositionHasData(head, resolution).futureBox.flatMap {
-            case Full(pos) => Fox.successful(Some(pos))
-            case _         => searchPositionIter(tail, resolution)
-          }
-      }
-
-    def checkIfPositionHasData(position: Point3D, resolution: Point3D) = {
-      val request = DataRequest(
-        new VoxelPosition(position.x, position.y, position.z, resolution),
-        DataLayer.bucketLength,
-        DataLayer.bucketLength,
-        DataLayer.bucketLength
-      )
-      for {
-        data <- binaryDataService.handleDataRequest(
-          DataServiceDataRequest(dataSource, dataLayer, None, request.cuboid(dataLayer), request.settings))
-        if data.nonEmpty && data.exists(_ != 0)
-      } yield position.move(getExactDataOffset(data))
-    }
-
-    def resolutionIter(positions: List[Point3D], remainingResolutions: List[Point3D]): Fox[Option[(Point3D, Point3D)]] =
-      remainingResolutions match {
-        case List() => Fox.successful(None)
-        case head :: tail =>
-          (for {
-            foundPosition <- searchPositionIter(positions, head)
-          } yield
-            foundPosition match {
-              case Some(position) => Fox.successful(Some((position, head)))
-              case None           => resolutionIter(positions, tail)
-            }).flatten
-      }
-
-    resolutionIter(createPositions(dataLayer).distinct, dataLayer.resolutions.sortBy(_.maxDim))
+  private def getDataFor(dataSource: DataSource,
+                         dataLayer: DataLayer,
+                         position: Point3D,
+                         resolution: Point3D): Fox[Array[Byte]] = {
+    val request = DataRequest(
+      new VoxelPosition(position.x, position.y, position.z, resolution),
+      DataLayer.bucketLength,
+      DataLayer.bucketLength,
+      DataLayer.bucketLength
+    )
+    binaryDataService.handleDataRequest(
+      DataServiceDataRequest(dataSource, dataLayer, None, request.cuboid(dataLayer), request.settings))
   }
+
+  private def concatenateBuckets(buckets: Seq[Array[Byte]]): Array[Byte] =
+    buckets.foldLeft(Array[Byte]()) { (acc, i) =>
+      {
+        acc ++ i
+      }
+    }
+
+  private def getConcatenatedDataFor(dataSource: DataSource,
+                                     dataLayer: DataLayer,
+                                     positions: List[Point3D],
+                                     resolution: Point3D) =
+    for {
+      dataBucketWise: Seq[Array[Byte]] <- Fox.serialCombined(positions)(pos =>
+        getDataFor(dataSource, dataLayer, pos, resolution))
+      dataConcatenated = concatenateBuckets(dataBucketWise)
+    } yield dataConcatenated
 
   private def createPositions(dataLayer: DataLayer, iterationCount: Int = 4) = {
 
@@ -145,26 +112,63 @@ class FindDataService @Inject()(dataServicesHolder: BinaryDataServiceHolder)(imp
     positionCreationIter((1 to iterationCount).toList, List[Point3D]())
   }
 
-  def meanAndStdDev(dataSource: DataSource, dataLayer: DataLayer)(
-      implicit m: MessagesProvider): Fox[(Double, Double)] = {
+  private def checkAllPositionsForData(dataSource: DataSource,
+                                       dataLayer: DataLayer): Fox[Option[(Point3D, Point3D)]] = {
 
-    def getDataFor(position: Point3D, resolution: Point3D): Fox[Array[Byte]] = {
-      val request = DataRequest(
-        new VoxelPosition(position.x, position.y, position.z, resolution),
-        DataLayer.bucketLength,
-        DataLayer.bucketLength,
-        DataLayer.bucketLength
-      )
-      binaryDataService.handleDataRequest(
-        DataServiceDataRequest(dataSource, dataLayer, None, request.cuboid(dataLayer), request.settings))
+    def getExactDataOffset(data: Array[Byte]): Point3D = {
+      val cubeLength = DataLayer.bucketLength / dataLayer.bytesPerElement
+      val convertedData = convertData(data, dataLayer.elementClass)
+      for {
+        z <- 0 until cubeLength
+        y <- 0 until cubeLength
+        x <- 0 until cubeLength
+      } {
+        val voxelOffset = x + y * cubeLength + z * cubeLength * cubeLength
+        if (convertedData(voxelOffset) != 0) return Point3D(x, y, z)
+      }
+      Point3D(0, 0, 0)
     }
 
-    def concatenateBuckets(buckets: Seq[Array[Byte]]): Array[Byte] =
-      buckets.foldLeft(Array[Byte]()) { (acc, i) =>
-        {
-          acc ++ i
-        }
+    def searchPositionIter(positions: List[Point3D], resolution: Point3D): Fox[Option[Point3D]] =
+      positions match {
+        case List() => Fox.successful(None)
+        case head :: tail =>
+          checkIfPositionHasData(head, resolution).futureBox.flatMap {
+            case Full(pos) => Fox.successful(Some(pos))
+            case _         => searchPositionIter(tail, resolution)
+          }
       }
+
+    def checkIfPositionHasData(position: Point3D, resolution: Point3D) =
+      for {
+        data <- getDataFor(dataSource, dataLayer, position, resolution)
+        if data.nonEmpty && data.exists(_ != 0)
+      } yield position.move(getExactDataOffset(data))
+
+    def resolutionIter(positions: List[Point3D], remainingResolutions: List[Point3D]): Fox[Option[(Point3D, Point3D)]] =
+      remainingResolutions match {
+        case List() => Fox.successful(None)
+        case head :: tail =>
+          (for {
+            foundPosition <- searchPositionIter(positions, head)
+          } yield
+            foundPosition match {
+              case Some(position) => Fox.successful(Some((position, head)))
+              case None           => resolutionIter(positions, tail)
+            }).flatten
+      }
+
+    resolutionIter(createPositions(dataLayer).distinct, dataLayer.resolutions.sortBy(_.maxDim))
+  }
+
+  def findPositionWithData(dataSource: DataSource, dataLayer: DataLayer)(
+      implicit m: MessagesProvider): Fox[Option[(Point3D, Point3D)]] =
+    for {
+      positionAndResolutionOpt <- checkAllPositionsForData(dataSource, dataLayer)
+    } yield positionAndResolutionOpt
+
+  def meanAndStdDev(dataSource: DataSource, dataLayer: DataLayer)(
+      implicit m: MessagesProvider): Fox[(Double, Double)] = {
 
     def convertNonZeroDataToDouble(data: Array[Byte], elementClass: ElementClass.Value): Array[Double] =
       elementClass match {
@@ -192,8 +196,7 @@ class FindDataService @Inject()(dataServicesHolder: BinaryDataServiceHolder)(imp
     def meanAndStdDevForPositions(positions: List[Point3D], resolution: Point3D)(
         implicit m: MessagesProvider): Fox[(Double, Double)] =
       for {
-        dataBucketWise: Seq[Array[Byte]] <- Fox.serialCombined(positions)(pos => getDataFor(pos, resolution))
-        dataConcatenated = concatenateBuckets(dataBucketWise)
+        dataConcatenated <- getConcatenatedDataFor(dataSource, dataLayer, positions, resolution)
         dataAsDoubles = convertNonZeroDataToDouble(dataConcatenated, dataLayer.elementClass)
         _ <- Fox.bool2Fox(dataAsDoubles.nonEmpty) ?~> "dataSet.sampledOnlyBlack"
       } yield (Math.mean(dataAsDoubles), Math.stdDev(dataAsDoubles))
@@ -206,39 +209,25 @@ class FindDataService @Inject()(dataServicesHolder: BinaryDataServiceHolder)(imp
   }
 
   def createHistogram(dataSource: DataSource, dataLayer: DataLayer) = {
-
-    def getDataFor(position: Point3D, resolution: Point3D): Fox[Array[Byte]] = {
-      val request = DataRequest(
-        new VoxelPosition(position.x, position.y, position.z, resolution),
-        DataLayer.bucketLength,
-        DataLayer.bucketLength,
-        DataLayer.bucketLength
-      )
-      binaryDataService.handleDataRequest(
-        DataServiceDataRequest(dataSource, dataLayer, None, request.cuboid(dataLayer), request.settings))
-    }
-
-    def concatenateBuckets(buckets: Seq[Array[Byte]]): Array[Byte] =
-      buckets.foldLeft(Array[Byte]()) { (acc, i) =>
-        {
-          acc ++ i
-        }
-      }
-
-    def createHistogram(data: Array[Int], width: Int = 256) = {
+    def createHistogram(data: Array[_ >: Byte with Short with Int with Long], width: Int = 256) = {
       val counts = Array.ofDim[Long](width)
-      data.foreach(el => counts(el + 128) = counts(el + 128) + 1)
+      data match {
+        case byteData: Array[Byte]   => byteData.foreach(el => counts(el + 128) += 1)
+        case shortData: Array[Short] => shortData.foreach(el => counts(el / math.pow(2, 8).toInt + 128) += 1)
+        case intData: Array[Int]     => intData.foreach(el => counts(el / math.pow(2, 24).toInt + 128) += 1)
+        case longData: Array[Long]   => longData.foreach(el => counts((el / math.pow(2, 56).toInt + 128).toInt) += 1)
+      }
       counts
     }
 
     def histogramForPositions(positions: List[Point3D], resolution: Point3D) =
       for {
-        dataBucketWise: Seq[Array[Byte]] <- Fox.serialCombined(positions)(pos => getDataFor(pos, resolution))
-        dataConcatenated = concatenateBuckets(dataBucketWise)
+        dataConcatenated <- getConcatenatedDataFor(dataSource, dataLayer, positions, resolution)
+        convertedData = convertData(dataConcatenated, dataLayer.elementClass)
 
         // TODO data conversion
-        histogramValues = createHistogram(dataConcatenated.map(_.toInt))
-      } yield (histogramValues, dataConcatenated.length)
+        histogramValues = createHistogram(convertedData)
+      } yield (histogramValues, convertedData.length)
 
     for {
       _ <- bool2Fox(dataLayer.resolutions.nonEmpty)

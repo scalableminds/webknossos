b00b19b183f3aea8406b37a212814899bee40ca1
Philipp Otto <philipp.4096@googlemail.com>
2019-04-18 10:06:34 +0200

    adapt webgl crash message

--- frontend/javascripts/messages.js
+++ frontend/javascripts/messages.js
@@ -113,7 +113,7 @@ In order to restore the current window, a reload is necessary.`,
   "datastore.unknown_type": "Unknown datastore type:",
   "webgl.disabled": "Couldn't initialise WebGL, please make sure WebGL is enabled.",
   "webgl.context_loss":
-    "Unfortunately, WebGL crashed. Please ensure that your graphics card driver is up to date to avoid such crashes. If this message keeps appearing, restarting your browser might also help.",
+    "Unfortunately, WebGL crashed. Please ensure that your graphics card driver is up to date to avoid such crashes. If this message keeps appearing, you can also try to lower the data rendering quality in the settings. Restarting your browser might also help.",
   "task.user_script_retrieval_error": "Unable to retrieve script",
   "task.new_description": "You are now tracing a new task with the following description",
   "task.no_description": "You are now tracing a new task with no description.",

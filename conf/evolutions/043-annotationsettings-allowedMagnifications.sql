3128f5eafc3f17f7d7ec3db3d0a7c82a5fb28663
Youri K <youri.kaminsky@student.hpi.de>
2019-05-16 19:33:08 +0200

    add constraints to sql evolution

--- conf/evolutions/043-annotationsettings-allowedMagnifications.sql
+++ conf/evolutions/043-annotationsettings-allowedMagnifications.sql
@@ -4,7 +4,8 @@ START TRANSACTION;
 
 DROP VIEW webknossos.taskTypes_;
 ALTER TABLE webknossos.taskTypes ADD COLUMN settings_allowedMagnifications JSONB;
-
+ALTER TABLE webknossos.taskTypes
+  ADD CONSTRAINT settings_allowedMagnificationsIsJsonObject CHECK (jsonb_typeof(settings_allowedMagnifications) = 'object');
 
 CREATE VIEW webknossos.taskTypes_ AS SELECT * FROM webknossos.taskTypes WHERE NOT isDeleted;
 
--- conf/evolutions/reversions/043-annotationsettings-allowedMagnifications.sql
+++ conf/evolutions/reversions/043-annotationsettings-allowedMagnifications.sql
@@ -2,8 +2,9 @@ START TRANSACTION;
 
 DROP VIEW webknossos.taskTypes_;
 ALTER TABLE webknossos.taskTypes DROP settings_allowedMagnifications;
-
-
+ALTER TABLE webknossos.taskTypes
+  DROP CONSTRAINT settings_allowedMagnificationsIsJsonObject;
+  
 CREATE VIEW webknossos.taskTypes_ AS SELECT * FROM webknossos.taskTypes WHERE NOT isDeleted;
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 42;

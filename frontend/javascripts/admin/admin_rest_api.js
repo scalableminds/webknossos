0f1009378de37fece7a40fa15ebac1ecf517b49b
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2019-08-12 16:13:50 +0200

    fixed typo

--- frontend/javascripts/admin/admin_rest_api.js
+++ frontend/javascripts/admin/admin_rest_api.js
@@ -646,7 +646,7 @@ export async function downloadNml(
   versions?: Versions = {},
 ) {
   const possibleVersionString = Object.entries(versions)
-    // $FlowFixMe Flow returns val as mixed here die to the use of Object.entries
+    // $FlowFixMe Flow returns val as mixed here due to the use of Object.entries
     .map(([key, val]) => `${key}Version=${val}`)
     .join("&");
   const downloadUrl = `/api/annotations/${annotationType}/${annotationId}/download?${possibleVersionString}`;

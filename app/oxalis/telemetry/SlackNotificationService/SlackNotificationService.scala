82a3210e41c5cc050e8f57d74fb2c1eaac81197f
Florian M <florian@scm.io>
2019-08-14 16:17:35 +0200

    pretty backend

--- app/oxalis/telemetry/SlackNotificationService/SlackNotificationService.scala
+++ app/oxalis/telemetry/SlackNotificationService/SlackNotificationService.scala
@@ -6,25 +6,23 @@ import javax.inject.Inject
 import play.api.libs.json.{JsObject, Json}
 import utils.WkConf
 
-class SlackNotificationService @Inject()(rpc: RPC,
-                      conf: WkConf
-                     ) extends LazyLogging {
+class SlackNotificationService @Inject()(rpc: RPC, conf: WkConf) extends LazyLogging {
 
   lazy val url = conf.SlackNotifications.url
 
-  def noticeError(ex: Throwable, message: String): Unit = {
+  def noticeError(ex: Throwable, message: String): Unit =
     sendNotification(ex.toString + ": " + ex.getLocalizedMessage + "\n" + message)
-  }
 
-  def sendNotification(msg: String): Unit = {
+  def sendNotification(msg: String): Unit =
     if (url != "empty") {
       logger.info(s"Sending Slack notification: $msg")
-      rpc(url).postJson(Json.obj(
-        "attachments" -> Json.arr(Json.obj(
-          "title" -> s"Notification from webKnossos at ${conf.Http.uri}",
-          "text" -> msg,
-          "color" -> "#66d3e4"
-        ))))
+      rpc(url).postJson(
+        Json.obj(
+          "attachments" -> Json.arr(
+            Json.obj(
+              "title" -> s"Notification from webKnossos at ${conf.Http.uri}",
+              "text" -> msg,
+              "color" -> "#66d3e4"
+            ))))
     }
-  }
 }

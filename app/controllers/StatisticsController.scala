6515c6cceda5b14c84376e00b0a55a6bf8d8223d
Youri K <youri-k@users.noreply.github.com>
2019-08-22 13:08:19 +0200

    restrict statistics view to admins and team managers (#4235)
    * ensure user is team manager or admin
    * use fox.assertTrue rather than flatMap(bool2fox)
    * Merge branch 'master' into restrict-statistics-view

--- app/controllers/StatisticsController.scala
+++ app/controllers/StatisticsController.scala
@@ -45,6 +45,8 @@ class StatisticsController @Inject()(timeSpanService: TimeSpanService,
         case Some(handler) =>
           for {
             organizationId <- Fox.successful(request.identity._organization)
+            _ <- Fox
+              .assertTrue(userService.isTeamManagerOrAdminOfOrg(request.identity, organizationId)) ?~> "notAllowed" ~> FORBIDDEN
             times <- timeSpanService.loggedTimePerInterval(handler, start, end, organizationId)
             numberOfUsers <- userDAO.countAllForOrganization(organizationId)
             numberOfDatasets <- dataSetDAO.countAllForOrganization(organizationId)
@@ -69,6 +71,8 @@ class StatisticsController @Inject()(timeSpanService: TimeSpanService,
   def users(interval: String, start: Option[Long], end: Option[Long], limit: Int) = sil.SecuredAction.async {
     implicit request =>
       for {
+        _ <- Fox
+          .assertTrue(userService.isTeamManagerOrAdminOfOrg(request.identity, request.identity._organization)) ?~> "notAllowed" ~> FORBIDDEN
         handler <- intervalHandler.get(interval) ?~> "statistics.interval.invalid"
         users <- userDAO.findAll
         usersWithTimes <- Fox.serialCombined(users)(user =>

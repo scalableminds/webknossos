043dd249647ab4ac8c565bf13c4d730cd0ba8619
Youri K <youri-k@users.noreply.github.com>
2019-10-25 12:41:03 +0200

    Update time logging (#4306)
    * add time logging to request
    
    * use time logging explicitly when sending write requests to fossil
    
    * remove time logging from skeleton tracings

--- util/src/main/scala/com/scalableminds/util/requestlogging/RequestLogging.scala
+++ util/src/main/scala/com/scalableminds/util/requestlogging/RequestLogging.scala
@@ -31,14 +31,14 @@ trait RequestLogging extends AbstractRequestLogging {
 
   def logTime(block: => Future[Result])(implicit request: Request[_], ec: ExecutionContext): Future[Result] = {
     def logTimeFormatted(executionTime: Long, request: Request[_], result: Result) =
-      logger.info(
-        s"Request ${request.method} ${request.uri} took ${executionTime} nano seconds and was${if (result.header.status != 200) " not "
-        else " "}successfull")
+      logger.info(s"Request ${request.method} ${request.uri} took ${BigDecimal(executionTime / 1e9)
+        .setScale(2, BigDecimal.RoundingMode.HALF_UP)} seconds and was${if (result.header.status != 200) " not "
+      else " "}successfull")
 
     val start = System.nanoTime()
     for {
       result: Result <- block
-      _ = logTimeFormatted(System.nanoTime() - start, request, result)
+      _ = if (request.uri.contains("volume")) logTimeFormatted(System.nanoTime() - start, request, result)
     } yield result
   }
 

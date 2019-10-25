d766a49c512228cab4d4dd5bba7b365952168954
Youri K <youri.kaminsky@student.hpi.de>
2019-10-25 15:45:52 +0200

    only log time if request took more than four seconds

--- util/src/main/scala/com/scalableminds/util/requestlogging/RequestLogging.scala
+++ util/src/main/scala/com/scalableminds/util/requestlogging/RequestLogging.scala
@@ -29,16 +29,21 @@ trait RequestLogging extends AbstractRequestLogging {
       _ = logRequestFormatted(request, result)
     } yield result
 
-  def logTime(block: => Future[Result])(implicit request: Request[_], ec: ExecutionContext): Future[Result] = {
-    def logTimeFormatted(executionTime: Long, request: Request[_], result: Result) =
-      logger.info(s"Request ${request.method} ${request.uri} took ${BigDecimal(executionTime / 1e9)
+  def logTime(notifier: String => Unit)(block: => Future[Result])(implicit request: Request[_],
+                                                                  ec: ExecutionContext): Future[Result] = {
+    def logTimeFormatted(executionTime: Long, request: Request[_], result: Result) = {
+      val debugString = s"Request ${request.method} ${request.uri} took ${BigDecimal(executionTime / 1e9)
         .setScale(2, BigDecimal.RoundingMode.HALF_UP)} seconds and was${if (result.header.status != 200) " not "
-      else " "}successfull")
+      else " "}successfull"
+      logger.info(debugString)
+
+    }
 
     val start = System.nanoTime()
     for {
       result: Result <- block
-      _ = if (request.uri.contains("volume")) logTimeFormatted(System.nanoTime() - start, request, result)
+      executionTime = System.nanoTime() - start
+      _ = if (request.uri.contains("volume") && executionTime > 4e9) logTimeFormatted(executionTime, request, result)
     } yield result
   }
 

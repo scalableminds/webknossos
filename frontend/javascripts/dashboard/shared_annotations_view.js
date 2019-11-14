a591734dc77536b548d702812a1f6f8f408392b4
Youri K <youri-k@users.noreply.github.com>
2019-11-14 14:33:24 +0100

    Update frontend/javascripts/dashboard/shared_annotations_view.js
    Co-Authored-By: Philipp Otto <philippotto@users.noreply.github.com>

--- frontend/javascripts/dashboard/shared_annotations_view.js
+++ frontend/javascripts/dashboard/shared_annotations_view.js
@@ -173,7 +173,7 @@ class SharedAnnotationsView extends React.PureComponent<Props, State> {
 
   render = () => (
     <div className="TestExplorativeAnnotationsView">
-      <h3 style={{ display: "inline-block", "margin-right": "10px" }}>Shared Annotations</h3>
+      <h3 style={{ display: "inline-block", "margin-right": "5px" }}>Shared Annotations</h3>
       <Tooltip title="This is the Shared Annotations tab. Annotations that are shared with teams you are a member of are displayed here. You can share your own annotations in the sharing modal in the tracing view.">
         <Icon type="info-circle-o" style={{ color: "gray" }} />
       </Tooltip>

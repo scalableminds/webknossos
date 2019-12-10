120e14000312560a38f3a7894bb7816050549ec2
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2019-12-10 16:08:26 +0100

    disable buttons when predicting

--- frontend/javascripts/oxalis/model/sagas/blackbird_saga.js
+++ frontend/javascripts/oxalis/model/sagas/blackbird_saga.js
@@ -6,7 +6,10 @@ import { type Saga, _takeEvery, call, select, take } from "oxalis/model/sagas/ef
 import * as tf from "@tensorflow/tfjs";
 import api from "oxalis/api/internal_api";
 import Store from "oxalis/store";
-import { setLiveTrainingProgressAction } from "oxalis/model/actions/ui_actions";
+import {
+  setLiveTrainingProgressAction,
+  setIsLiveTrainingPredictingAction,
+} from "oxalis/model/actions/ui_actions";
 import { V3 } from "libs/mjs";
 import * as blackbirdModel from "oxalis/model/blackbird_model";
 
@@ -18,6 +21,7 @@ const bucketWidth = constants.BUCKET_WIDTH;
 
 async function train() {
   try {
+    Store.dispatch(setLiveTrainingProgressAction(0));
     const bbox = {
       min: [0, 0, 0],
       max: [250, 250, 30],
@@ -76,6 +80,7 @@ function getVoxelIndex(voxel: Vector3, channelIndex: number): number {
 
 function* predict(): Saga<void> {
   console.log("predict action");
+  Store.dispatch(setIsLiveTrainingPredictingAction(true));
   const position = yield* select(state => getPosition(state.flycam));
   const bbox = {
     min: [0, 0, Math.floor(position[2])],
@@ -115,11 +120,12 @@ function* predict(): Saga<void> {
       }
     }
   }
+  Store.dispatch(setIsLiveTrainingPredictingAction(false));
   console.log("Finished labeling.");
 }
 
 export default function* isosurfaceSaga(): Saga<void> {
   yield* take("WK_READY");
   yield _takeEvery("TRAIN_CLASSIFIER", trainClassifier);
-  // yield _takeEvery("PREDICT", predict);
+  yield _takeEvery("PREDICT", predict);
 }

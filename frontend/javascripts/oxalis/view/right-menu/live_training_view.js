d8e128151e2177177ae2d86bc75dd87f7a384926
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2019-12-10 18:04:50 +0100

    reworked live training view's appearance

--- frontend/javascripts/oxalis/view/right-menu/live_training_view.js
+++ frontend/javascripts/oxalis/view/right-menu/live_training_view.js
@@ -3,12 +3,10 @@
  * @flow
  */
 import type { Dispatch } from "redux";
-import { Radio, Button, Progress, Spin, Icon } from "antd";
+import { Radio, Button, Icon } from "antd";
 import { connect } from "react-redux";
 import React from "react";
 import _ from "lodash";
-import Store from "oxalis/store";
-import { setLiveTrainingProgressAction } from "oxalis/model/actions/ui_actions";
 import type { OxalisState } from "oxalis/store";
 import { trainClassifierAction, predictAction } from "oxalis/model/actions/blackbird_actions";
 import { getVolumeTracing } from "oxalis/model/accessors/volumetracing_accessor";
@@ -52,56 +50,65 @@ class LiveTrainingView extends React.Component<Props, State> {
     const { activeCellId, liveTrainingProgress, isLiveTrainingPredicting } = this.props;
     const isTraining = liveTrainingProgress > 0 && liveTrainingProgress < 100;
     return (
-      <div id="live-training" className="padded-tab-content" style={{ maxWidth: 500 }}>
-        <div style={{ marginBottom: 6 }}>
-          <label className="setting-label">
-            Brush to label…
-            <Radio.Group
-              value={activeCellId}
-              onChange={this.handleChangeLabel}
-              style={{ marginLeft: 6 }}
-            >
-              <Radio.Button value={1}>Foreground</Radio.Button>
-              <Radio.Button value={2}>Background</Radio.Button>
-            </Radio.Group>
-          </label>
+      <div id="live-training" className="padded-tab-content">
+        <div style={{ margin: 6, position: "absolute", right: 10, top: 10 }}>
+          <Radio.Group value={activeCellId} onChange={this.handleChangeLabel} style={{}}>
+            <Radio.Button value={1}>Foreground</Radio.Button>
+            <Radio.Button value={2}>Background</Radio.Button>
+          </Radio.Group>
         </div>
-
-        <Button
-          type="primary"
-          disabled={isTraining || isLiveTrainingPredicting}
-          onClick={() => {
-            this.props.handleTrain();
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            padding: 50,
           }}
         >
-          Retrain and Predict
-        </Button>
-        <Button
-          type="primary"
-          disabled={isTraining || isLiveTrainingPredicting}
-          style={{ marginLeft: 24 }}
-          onClick={() => {
-            this.props.handlePredict();
-          }}
-        >
-          Predict
-        </Button>
-        <div style={{ display: "block", marginTop: 6 }}>
-          {isTraining ? (
-            <React.Fragment>
-              <div>Training ...</div>
-              <Progress type="circle" percent={liveTrainingProgress} />
-            </React.Fragment>
-          ) : null}
-          {isLiveTrainingPredicting ? (
-            <React.Fragment>
-              <div>Predicting ...</div>
-              <Spin
-                size="large"
-                indicator={<Icon style={{ fontSize: 56 }} type="loading" spin />}
-              />
-            </React.Fragment>
-          ) : null}
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Icon
+              type="experiment"
+              style={{
+                fontSize: 46,
+                height: 80,
+                width: 80,
+                padding: 15,
+                margin: "16px 0px",
+                color: "rgb(241, 248, 253)",
+                backgroundColor: "rgb(24, 144, 255)",
+                borderRadius: "100%",
+              }}
+            />
+          </div>
+          <ol>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+          </ol>
+          <div style={{ marginTop: 16, marginLeft: 6, display: "flex", justifyContent: "center" }}>
+            <Button
+              type="primary"
+              disabled={isTraining || isLiveTrainingPredicting}
+              loading={isTraining}
+              size="large"
+              onClick={() => {
+                this.props.handleTrain();
+              }}
+            >
+              Retrain and Predict {isTraining ? `(${liveTrainingProgress}%)` : null}
+            </Button>
+            <Button
+              disabled={isTraining || isLiveTrainingPredicting}
+              loading={isLiveTrainingPredicting}
+              style={{ marginLeft: 24 }}
+              size="large"
+              onClick={() => {
+                this.props.handlePredict();
+              }}
+            >
+              Predict
+            </Button>
+          </div>
         </div>
       </div>
     );

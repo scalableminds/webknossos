c0107ebe84329ea6dc21fe163988ab5bef68e99f
Youri K <youri.kaminsky@student.hpi.de>
2020-01-24 13:57:12 +0100

    update flow types

--- frontend/javascripts/admin/api_flow_types.js
+++ frontend/javascripts/admin/api_flow_types.js
@@ -515,6 +515,7 @@ export type APIFeatureToggles = {
   +isDemoInstance: boolean,
   +autoBrushReadyDatasets: Array<string>,
   +isDemoInstance: boolean,
+  +taskReopenAllowedFrontend: number,
 };
 
 // Tracing related datatypes

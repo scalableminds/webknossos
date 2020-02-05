4dc3325ffb8f29d75ee22ad05d289a272f5059b6
Daniel Werner <daniel.werner@scalableminds.com>
2020-02-05 18:37:29 +0100

    remove invariant that a tree is always needed

--- frontend/javascripts/messages.js
+++ frontend/javascripts/messages.js
@@ -107,8 +107,7 @@ In order to restore the current window, a reload is necessary.`,
   "tracing.no_viewport_scaling_setting":
     "Scaling the viewports via k/l is not supported anymore. Instead you can increase the viewport size by dragging the borders between the panes. You can also rearrange the panes by dragging the tabs.",
   "tracing.natural_sorting": "Correctly sort numbers in text (word2 < word10). This may be slow!",
-  "tracing.cant_create_node_due_to_active_group":
-    "You cannot create nodes, since no tree is active.",
+  "tracing.cant_create_node": "You cannot create nodes, since no tree is active.",
   "tracing.invalid_state":
     "A corruption in the current skeleton tracing was detected. Please contact your supervisor and/or the maintainers of webKnossos to get help for restoring a working version. Please include as much details as possible about your past user interactions. This will be very helpful to investigate the source of this bug.",
   "layouting.missing_custom_layout_info":

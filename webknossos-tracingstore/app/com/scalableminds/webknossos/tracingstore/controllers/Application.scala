f7a69b991ed127fb1bba8590a762b531d2e01ad3
Youri K <youri.kaminsky@student.hpi.de>
2020-02-11 18:50:15 +0100

    [WIP] play websocket

--- webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/controllers/Application.scala
+++ webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/controllers/Application.scala
@@ -4,11 +4,17 @@ import com.scalableminds.webknossos.datastore.controllers.Controller
 import com.scalableminds.webknossos.tracingstore.RedisTemporaryStore
 import com.scalableminds.webknossos.tracingstore.tracings.TracingDataStore
 import javax.inject.Inject
+import play.api.libs.streams._
+import play.api.mvc._
+import akka.actor._
+import akka.stream.Materializer
 
 import scala.concurrent.ExecutionContext
 
 class Application @Inject()(tracingDataStore: TracingDataStore, redisClient: RedisTemporaryStore)(
-    implicit ec: ExecutionContext)
+    implicit ec: ExecutionContext,
+    system: ActorSystem,
+    mat: Materializer)
     extends Controller {
 
   def health = Action.async { implicit request =>
@@ -22,4 +28,19 @@ class Application @Inject()(tracingDataStore: TracingDataStore, redisClient: Red
     }
   }
 
+  def liveUpdate = WebSocket.accept[String, String] { request =>
+    System.out.println("Connection")
+    ActorFlow.actorRef(out => MyWebSocketActor.props(out))
+  }
+}
+
+object MyWebSocketActor {
+  def props(out: ActorRef) = Props(new MyWebSocketActor(out))
+}
+
+class MyWebSocketActor(out: ActorRef) extends Actor {
+  def receive = {
+    case msg: String =>
+      out ! ("I received your message: " + msg)
+  }
 }

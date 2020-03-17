742f6a9da190610b982f8882b2840506a3a6d232
Youri K <youri.kaminsky@student.hpi.de>
2020-03-17 12:26:59 +0100

    update evolutions

--- conf/evolutions/052-replace-segmentation-opacity.sql
+++ conf/evolutions/052-replace-segmentation-opacity.sql
@@ -6,18 +6,15 @@ START TRANSACTION;
 UPDATE webknossos.user_dataSetConfigurations
 SET configuration = jsonb_set(
         configuration,
-        array['configuration','layers'],
-        (configuration->'configuration'->'layers')::jsonb || jsonb_build_object(dl.name, jsonb_build_object('alpha', configuration->'configuration'->'segmentationOpacity')))
+        array['layers'],
+        (configuration->'layers')::jsonb || jsonb_build_object(dl.name, jsonb_build_object('alpha', configuration->'segmentationOpacity')))
 FROM webknossos.dataSet_layers dl
-WHERE _dataSet = dl._dataset and dl.category = 'segmentation' and configuration->'configuration' ? 'segmentationOpacity';
+WHERE webknossos.user_dataSetConfigurations._dataSet = dl._dataset and dl.category = 'segmentation' and configuration ? 'segmentationOpacity';
 
 -- Remove segmentationOpacityField
 UPDATE webknossos.user_dataSetConfigurations
-SET configuration = jsonb_set(
-        configuration,
-        array['configuration'],
-        (configuration->'configuration')::jsonb - 'segmentationOpacity')
-WHERE configuration->'configuration' ? 'segmentationOpacity';
+SET configuration = configuration - 'segmentationOpacity'
+WHERE configuration ? 'segmentationOpacity';
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 52;
 

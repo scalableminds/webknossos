b8743890556dc75e5b0152f07dd14cb6f11fbdb9
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2020-04-23 14:49:18 +0200

    use hooks for ViewDatasetActionsView

--- frontend/javascripts/oxalis/view/action-bar/view_dataset_actions_view.js
+++ frontend/javascripts/oxalis/view/action-bar/view_dataset_actions_view.js
@@ -1,57 +1,39 @@
 // @flow
-import * as React from "react";
+import React, { useState, type Node } from "react";
 import { Dropdown, Icon, Menu } from "antd";
 import ButtonComponent from "oxalis/view/components/button_component";
 import ShareViewDatasetModalView from "oxalis/view/action-bar/share_view_dataset_modal_view";
 
 type Props = {
-  layoutMenu: React.Node,
-};
-type State = {
-  isShareDatasetModalOpen: boolean,
+  layoutMenu: Node,
 };
 
-class ViewDatasetActionsView extends React.PureComponent<Props, State> {
-  state = {
-    isShareDatasetModalOpen: false,
-  };
+export default function ViewDatasetActionsView(props: Props) {
+  const [shareDatasetModalVisibility, setShareDatasetModalVisibility] = useState(false);
+  const modal = (
+    <ShareViewDatasetModalView
+      isVisible={shareDatasetModalVisibility}
+      onOk={() => setShareDatasetModalVisibility(false)}
+    />
+  );
 
-  handleOpenShareDatasetModal = () => {
-    this.setState({ isShareDatasetModalOpen: true });
-  };
-
-  handleCloseShareDatasetModal = () => {
-    this.setState({ isShareDatasetModalOpen: false });
-  };
-
-  render() {
-    const modal = (
-      <ShareViewDatasetModalView
-        isVisible={this.state.isShareDatasetModalOpen}
-        onOk={this.handleCloseShareDatasetModal}
-      />
-    );
-
-    const overlayMenu = (
-      <Menu>
-        {this.props.layoutMenu}
-        <Menu.Item key="share-button" onClick={this.handleOpenShareDatasetModal}>
-          <Icon type="share-alt" />
-          Share
-        </Menu.Item>
-      </Menu>
-    );
-    return (
-      <div style={{ marginLeft: 10 }}>
-        {modal}
-        <Dropdown overlay={overlayMenu} trigger={["click"]}>
-          <ButtonComponent style={{ padding: "0 10px" }}>
-            <Icon type="down" />
-          </ButtonComponent>
-        </Dropdown>
-      </div>
-    );
-  }
+  const overlayMenu = (
+    <Menu>
+      {props.layoutMenu}
+      <Menu.Item key="share-button" onClick={() => setShareDatasetModalVisibility(true)}>
+        <Icon type="share-alt" />
+        Share
+      </Menu.Item>
+    </Menu>
+  );
+  return (
+    <div style={{ marginLeft: 10 }}>
+      {modal}
+      <Dropdown overlay={overlayMenu} trigger={["click"]}>
+        <ButtonComponent style={{ padding: "0 10px" }}>
+          <Icon type="down" />
+        </ButtonComponent>
+      </Dropdown>
+    </div>
+  );
 }
-
-export default ViewDatasetActionsView;

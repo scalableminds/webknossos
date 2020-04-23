60b55ddeaa6bc6fd9dc4f00bbd970b1291fd8f03
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2020-04-23 13:13:19 +0200

    use hooks for sharing view dataset

--- frontend/javascripts/oxalis/view/action-bar/share_view_dataset_modal_view.js
+++ frontend/javascripts/oxalis/view/action-bar/share_view_dataset_modal_view.js
@@ -1,119 +1,86 @@
 // @flow
 import { Modal, Input, Button, Row, Col } from "antd";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import Clipboard from "clipboard-js";
-import React, { PureComponent } from "react";
+import React, { useState, useEffect } from "react";
 import type { APIDataset } from "admin/api_flow_types";
-import type { OxalisState } from "oxalis/store";
 import { getDatasetSharingToken } from "admin/admin_rest_api";
 import Toast from "libs/toast";
-import window from "libs/window";
-import _ from "lodash";
+import { location } from "libs/window";
 
-type OwnProps = {|
+type Props = {|
   isVisible: boolean,
   onOk: () => void,
 |};
-type StateProps = {|
-  dataset: APIDataset,
-|};
-type Props = {| ...OwnProps, ...StateProps |};
 
-type State = {
-  sharingToken: string,
-};
-
-class ShareViewDatasetModalView extends PureComponent<Props, State> {
-  state = {
-    sharingToken: "",
-  };
-
-  componentDidMount() {
-    this.fetch();
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    if (
-      this.props.dataset.name !== prevProps.dataset.name ||
-      this.props.dataset.isPublic !== prevProps.dataset.isPublic ||
-      this.props.dataset.owningOrganization !== prevProps.dataset.owningOrganization
-    ) {
-      this.fetch();
-    }
-  }
-
-  async fetch() {
-    const { name, owningOrganization, isPublic } = this.props.dataset;
-    if (isPublic) {
-      // We only need to fetch a sharing token if the dataset is not public.
-      return;
-    }
-    const datasetId = { name, owningOrganization };
+export function useDatasetSharingToken(dataset: APIDataset) {
+  const [datasetToken, setDatasetToken] = useState("");
+  const fetchAndSetToken = async () => {
+    const datasetId = { name: dataset.name, owningOrganization: dataset.owningOrganization };
     try {
       const sharingToken = await getDatasetSharingToken(datasetId, { showErrorToast: false });
-      this.setState({ sharingToken });
+      setDatasetToken(sharingToken);
     } catch (error) {
       console.error(error);
     }
-  }
-
-  getUrl() {
-    const { location } = window;
-    const { pathname, origin, hash } = location;
-    const query = this.props.dataset.isPublic ? "" : `?token=${this.state.sharingToken}`;
-    const url = `${origin}${pathname}${query}${hash}`;
-    return url;
-  }
-
-  copyToClipboard = async () => {
-    const url = this.getUrl();
-    await Clipboard.copy(url);
-    Toast.success("URL copied to clipboard.");
   };
-
-  render() {
-    const { isVisible, onOk, dataset } = this.props;
-    return (
-      <Modal
-        title="Share this Tracing"
-        visible={isVisible}
-        width={800}
-        okText="Ok"
-        onOk={onOk}
-        onCancel={onOk}
-      >
-        <Row>
-          <Col span={6} style={{ lineHeight: "30px" }}>
-            Sharing Link
-          </Col>
-          <Col span={18}>
-            <Input.Group compact>
-              <Input style={{ width: "85%" }} value={this.getUrl()} readOnly />
-              <Button style={{ width: "15%" }} onClick={this.copyToClipboard} icon="copy">
-                Copy
-              </Button>
-            </Input.Group>
-            <div
-              style={{
-                marginBottom: 12,
-                margin: "6px 12px",
-              }}
-            >
-              This link includes the current position and zoom value. Consider fine-tuning your
-              current view before copying the URL.{" "}
-              {!dataset.isPublic
-                ? "Additionally, a private token is included in the link, since the dataset is not public."
-                : null}
-            </div>
-          </Col>
-        </Row>
-      </Modal>
-    );
-  }
+  useEffect(() => {
+    fetchAndSetToken();
+  }, [dataset]);
+  return datasetToken;
 }
 
-const mapStateToProps = (state: OxalisState) => ({
-  dataset: state.dataset,
-});
+function getUrl(sharingToken: string, includeToken: boolean) {
+  const { pathname, origin, hash } = location;
+  const query = includeToken ? `?token=${sharingToken}` : "";
+  const url = `${origin}${pathname}${query}${hash}`;
+  return url;
+}
 
-export default connect<Props, OwnProps, _, _, _, _>(mapStateToProps)(ShareViewDatasetModalView);
+async function copyUrlToClipboard(url: string) {
+  await Clipboard.copy(url);
+  Toast.success("URL copied to clipboard.");
+}
+
+export default function ShareViewDatasetModalView(props: Props) {
+  const { isVisible, onOk } = props;
+  const dataset = useSelector(state => state.dataset);
+  const sharingToken = useDatasetSharingToken(dataset);
+  const url = getUrl(sharingToken, !dataset.isPublic);
+  return (
+    <Modal
+      title="Share this Tracing"
+      visible={isVisible}
+      width={800}
+      okText="Ok"
+      onOk={onOk}
+      onCancel={onOk}
+    >
+      <Row>
+        <Col span={6} style={{ lineHeight: "30px" }}>
+          Sharing Link
+        </Col>
+        <Col span={18}>
+          <Input.Group compact>
+            <Input style={{ width: "85%" }} value={url} readOnly />
+            <Button style={{ width: "15%" }} onClick={() => copyUrlToClipboard(url)} icon="copy">
+              Copy
+            </Button>
+          </Input.Group>
+          <div
+            style={{
+              marginBottom: 12,
+              margin: "6px 12px",
+            }}
+          >
+            This link includes the current position and zoom value. Consider fine-tuning your
+            current view before copying the URL.{" "}
+            {!dataset.isPublic
+              ? "Additionally, a private token is included in the link, since the dataset is not public."
+              : null}
+          </div>
+        </Col>
+      </Row>
+    </Modal>
+  );
+}

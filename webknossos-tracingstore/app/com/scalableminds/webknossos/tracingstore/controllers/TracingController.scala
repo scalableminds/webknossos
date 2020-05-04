173f8d859350d2b04f53d09dc29b25e86c6e0ea7
Florian M <florian@scm.io>
2020-05-04 11:47:14 +0200

    add artificial timeout to update request

--- webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/controllers/TracingController.scala
+++ webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/controllers/TracingController.scala
@@ -115,21 +115,24 @@ trait TracingController[T <: GeneratedMessage with Message[T], Ts <: GeneratedMe
   def update(tracingId: String) = Action.async(validateJson[List[UpdateActionGroup[T]]]) { implicit request =>
     log {
       logTime(slackNotificationService.reportUnusalRequest) {
-        accessTokenService.validateAccess(UserAccessRequest.writeTracing(tracingId)) {
-          AllowRemoteOrigin {
-            val updateGroups = request.body
-            val userToken = request.getQueryString("token")
-            if (updateGroups.forall(_.transactionGroupCount.getOrElse(1) == 1)) {
-              commitUpdates(tracingId, updateGroups, userToken).map(_ => Ok)
-            } else {
-              updateGroups
-                .foldLeft(tracingService.currentVersion(tracingId)) { (currentCommittedVersionFox, updateGroup) =>
-                  handleUpdateGroupForTransaction(tracingId, currentCommittedVersionFox, updateGroup, userToken)
-                }
-                .map(_ => Ok)
+        for {
+          _ <- Fox.successful(Thread.sleep(1000 * 60 * 2))
+          result <- accessTokenService.validateAccess(UserAccessRequest.writeTracing(tracingId)) {
+            AllowRemoteOrigin {
+              val updateGroups = request.body
+              val userToken = request.getQueryString("token")
+              if (updateGroups.forall(_.transactionGroupCount.getOrElse(1) == 1)) {
+                commitUpdates(tracingId, updateGroups, userToken).map(_ => Ok)
+              } else {
+                updateGroups
+                  .foldLeft(tracingService.currentVersion(tracingId)) { (currentCommittedVersionFox, updateGroup) =>
+                    handleUpdateGroupForTransaction(tracingId, currentCommittedVersionFox, updateGroup, userToken)
+                  }
+                  .map(_ => Ok)
+              }
             }
           }
-        }
+        } yield result
       }
     }
   }

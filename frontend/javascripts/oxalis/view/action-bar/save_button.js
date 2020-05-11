07eb5197a812c5532462936571a091387d9bc076
Philipp Otto <philippotto@users.noreply.github.com>
2020-05-11 11:37:56 +0200

    Handle slow save requests better (#4593)
    * add artificial timeout to update request
    * warn user when there are unsaved changes older than X minutes; send notice to airbrake when save request took very long or 409 was triggered
    * fix sketchy null check and missing import
    * change unsaved warning threshold to 5 seconds
    * tweak timeout
    * Merge branch 'master' of github.com:scalableminds/webknossos into debug-save-timeout
    * incorporate PR feedback
    * fine-tune logging in proxy.js
    * revert changes in tracingcontroller
    * update changelog
    * perform error reporting in conflict scenario earlier

--- frontend/javascripts/oxalis/view/action-bar/save_button.js
+++ frontend/javascripts/oxalis/view/action-bar/save_button.js
@@ -1,12 +1,15 @@
 // @flow
 import { connect } from "react-redux";
 import React from "react";
+import _ from "lodash";
 
 import type { OxalisState, ProgressInfo, IsBusyInfo } from "oxalis/store";
 import { isBusy } from "oxalis/model/accessors/save_accessor";
 import ButtonComponent from "oxalis/view/components/button_component";
 import Model from "oxalis/model";
 import window from "libs/window";
+import { Tooltip, Icon } from "antd";
+import ErrorHandling from "libs/error_handling";
 
 type OwnProps = {|
   onClick: (SyntheticInputEvent<HTMLButtonElement>) => Promise<*>,
@@ -15,19 +18,34 @@ type OwnProps = {|
 type StateProps = {|
   progressInfo: ProgressInfo,
   isBusyInfo: IsBusyInfo,
+  oldestUnsavedTimestamp: ?number,
 |};
 type Props = {| ...OwnProps, ...StateProps |};
 
 type State = {
   isStateSaved: boolean,
+  currentTimestamp: number,
 };
 
-const SAVE_POLLING_INTERVAL = 1000;
+const SAVE_POLLING_INTERVAL = 1000; // 1s
+const UNSAVED_WARNING_THRESHOLD = 2 * 60 * 1000; // 2 min
+const REPORT_THROTTLE_THRESHOLD = 10 * 60 * 1000; // 10 min
+
+const reportUnsavedDurationThresholdExceeded = _.throttle(() => {
+  ErrorHandling.notify(
+    new Error(
+      `Warning: Saving lag detected. Some changes are unsaved and older than ${Math.ceil(
+        UNSAVED_WARNING_THRESHOLD / 1000 / 60,
+      )} minutes.`,
+    ),
+  );
+}, REPORT_THROTTLE_THRESHOLD);
 
 class SaveButton extends React.PureComponent<Props, State> {
   savedPollingInterval: number = 0;
   state = {
     isStateSaved: false,
+    currentTimestamp: Date.now(),
   };
 
   componentDidMount() {
@@ -43,6 +61,7 @@ class SaveButton extends React.PureComponent<Props, State> {
     const isStateSaved = Model.stateSaved();
     this.setState({
       isStateSaved,
+      currentTimestamp: Date.now(),
     });
   };
 
@@ -62,7 +81,15 @@ class SaveButton extends React.PureComponent<Props, State> {
   }
 
   render() {
-    const { progressInfo } = this.props;
+    const { progressInfo, oldestUnsavedTimestamp } = this.props;
+    const unsavedDuration =
+      oldestUnsavedTimestamp != null ? this.state.currentTimestamp - oldestUnsavedTimestamp : 0;
+    const showUnsavedWarning = unsavedDuration > UNSAVED_WARNING_THRESHOLD;
+    if (showUnsavedWarning) {
+      reportUnsavedDurationThresholdExceeded();
+    }
+    console.log("The oldest unsaved action was", unsavedDuration, "ago");
+
     return (
       <ButtonComponent
         key="save-button"
@@ -70,6 +97,7 @@ class SaveButton extends React.PureComponent<Props, State> {
         onClick={this.props.onClick}
         icon={this.getSaveButtonIcon()}
         className={this.props.className}
+        style={{ background: showUnsavedWarning ? "#e33f36" : null }}
       >
         {this.shouldShowProgress() ? (
           <span style={{ marginLeft: 8 }}>
@@ -79,16 +107,45 @@ class SaveButton extends React.PureComponent<Props, State> {
         ) : (
           <span className="hide-on-small-screen">Save</span>
         )}
+        {showUnsavedWarning ? (
+          <Tooltip
+            visible
+            title={`There are unsaved changes which are older than ${Math.ceil(
+              UNSAVED_WARNING_THRESHOLD / 1000 / 60,
+            )} minutes. Please ensure that your Internet connection works and wait until this warning disappears.`}
+            placement="bottom"
+          >
+            <Icon type="exclamation-circle" />
+          </Tooltip>
+        ) : null}
       </ButtonComponent>
     );
   }
 }
 
+function getOldestUnsavedTimestamp(saveQueue): ?number {
+  let oldestUnsavedTimestamp;
+  if (saveQueue.skeleton.length > 0) {
+    oldestUnsavedTimestamp = saveQueue.skeleton[0].timestamp;
+  }
+  if (saveQueue.volume.length > 0) {
+    const oldestVolumeTimestamp = saveQueue.volume[0].timestamp;
+    oldestUnsavedTimestamp = Math.min(
+      oldestUnsavedTimestamp != null ? oldestUnsavedTimestamp : Infinity,
+      oldestVolumeTimestamp,
+    );
+  }
+  return oldestUnsavedTimestamp;
+}
+
 function mapStateToProps(state: OxalisState): StateProps {
   const { progressInfo, isBusyInfo } = state.save;
+  const oldestUnsavedTimestamp = getOldestUnsavedTimestamp(state.save.queue);
+
   return {
     progressInfo,
     isBusyInfo,
+    oldestUnsavedTimestamp,
   };
 }
 

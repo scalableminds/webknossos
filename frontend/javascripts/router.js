2cd1eebe57f2aaa7e5d622bb65cc9d11535ec143
Florian M <florian@scm.io>
2020-05-14 19:51:11 +0200

    pretty

--- frontend/javascripts/router.js
+++ frontend/javascripts/router.js
@@ -274,10 +274,12 @@ class ReactRouter extends React.Component<Props> {
                 render={this.tracingView}
                 serverAuthenticationCallback={async ({ match }: ContextRouter) => {
                   try {
-                    if (match.params.type === APIAnnotationTypeEnum.CompoundTask ||
+                    if (
+                      match.params.type === APIAnnotationTypeEnum.CompoundTask ||
                       match.params.type === APIAnnotationTypeEnum.CompoundProject ||
-                      match.params.type === APIAnnotationTypeEnum.CompoundTaskType) {
-                      return false
+                      match.params.type === APIAnnotationTypeEnum.CompoundTaskType
+                    ) {
+                      return false;
                     } else {
                       const annotationInformation = await getAnnotationInformation(
                         match.params.id || "",

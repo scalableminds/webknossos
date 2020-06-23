3c4944790a8c3bdde01a26bb56cca1e5dd25ce1d
Florian M <florian@scm.io>
2020-06-23 11:30:11 +0200

    in evolution, make all team-managers dataset manager

--- conf/evolutions/054-add-isDatasetManager.sql
+++ conf/evolutions/054-add-isDatasetManager.sql
@@ -6,6 +6,8 @@ DROP VIEW webknossos.users_;
 
 ALTER TABLE webknossos.users ADD COLUMN isDatasetManager BOOLEAN NOT NULL DEFAULT false;
 
+UPDATE webknossos.users SET isDatasetManager = true WHERE _id IN (SELECT _user FROM webknossos.user_team_roles WHERE isTeamManager);
+
 CREATE VIEW webknossos.users_ AS SELECT * FROM webknossos.users WHERE NOT isDeleted;
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 54;

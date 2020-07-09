a58990efc1144264611a90d7f0237b2ef863253d
Florian M <florian@scm.io>
2020-07-09 13:19:06 +0200

    upon deletion, show toast + redirect to dashboard

--- frontend/javascripts/dashboard/dataset/import_delete_component.js
+++ frontend/javascripts/dashboard/dataset/import_delete_component.js
@@ -1,23 +1,22 @@
 // @flow
 
-import { Button, Input, Checkbox, Tooltip } from "antd";
-import Clipboard from "clipboard-js";
+import { Button } from "antd";
 import React, { useState, useEffect } from "react";
+import * as Utils from "libs/utils";
 
 import type { APIDataset, APIDatasetId } from "admin/api_flow_types";
-import { AsyncButton } from "components/async_clickables";
 import { getDataset, deleteDatasetOnDisk } from "admin/admin_rest_api";
 import Toast from "libs/toast";
-import features from "features";
-import window from "libs/window";
+import messages from "messages";
 
-import { FormItemWithInfo, confirmAsync } from "./helper_components";
+import { confirmAsync } from "./helper_components";
 
 type Props = {
   datasetId: APIDatasetId,
 };
 
 export default function ImportSharingComponent({ datasetId }: Props) {
+  const [isDeleting, setIsDeleting] = useState(false);
   const [dataSet, setDataSet] = useState<?APIDataset>(null);
 
   async function fetch() {
@@ -30,17 +29,31 @@ export default function ImportSharingComponent({ datasetId }: Props) {
   }, []);
 
   async function handleDeleteButtonClicked(): Promise<void> {
+    if (!dataSet) {
+      return;
+    }
     await confirmAsync({
       title: "Deleting a dataset on disk cannot be undone. Are you certain?",
       okText: "Yes, Delete Dataset on Disk now",
     });
+    setIsDeleting(true);
     await deleteDatasetOnDisk(dataSet.dataStore.url, datasetId);
+    Toast.success(
+      messages["dataset.delete_success"]({
+        datasetName: dataSet.name,
+      }),
+    );
+    await Utils.sleep(2000);
+    setIsDeleting(false);
+    location.href = "/dashboard";
   }
 
   return (
     <div>
       <p>Deleting a dataset on disk cannot be undone. Please be certain.</p>
-      <Button type="danger" onClick={handleDeleteButtonClicked}>
+      <p>Note that annotations for the dataset stay downloadable and the name stays reserved.</p>
+      <p>Only admins are allowed to delete datasets.</p>
+      <Button type="danger" loading={isDeleting} onClick={handleDeleteButtonClicked}>
         Delete Dataset on Disk
       </Button>
     </div>

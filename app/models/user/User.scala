205cf46c9b95b8c819f8a6d8ff985ee0071aad7d
Florian M <florian@scm.io>
2020-07-13 15:23:40 +0200

    fix query

--- app/models/user/User.scala
+++ app/models/user/User.scala
@@ -267,9 +267,9 @@ class UserTeamRolesDAO @Inject()(userDAO: UserDAO, sqlClient: SQLClient)(implici
   def findMemberDifference(potentialSubteam: ObjectId, superteams: List[ObjectId]): Fox[List[User]] =
     for {
       r <- run(sql"""select #${userDAO.columnsWithPrefix("u.")} from webknossos.users_
-                     where not u.isAdmin
                      join webknossos.user_team_roles tr on u._id = tr._user
-                     where tr._team = $potentialSubteam)
+                     where not u.isAdmin
+                     and tr._team = $potentialSubteam)
                      and _id not in
                      (select _user from webknossos.user_team_roles
                      where _team in #${writeStructTupleWithQuotes(superteams.map(_.id))})

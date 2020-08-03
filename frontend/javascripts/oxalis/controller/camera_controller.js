14eb2a109029329e1ceeaeec914d63060ce90a70
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2020-08-03 17:09:00 +0200

    refactor 3d rotation method to keep the zoom and simplify it

--- frontend/javascripts/oxalis/controller/camera_controller.js
+++ frontend/javascripts/oxalis/controller/camera_controller.js
@@ -16,7 +16,10 @@ import {
   OrthoViews,
   type Vector3,
 } from "oxalis/constants";
-import { getBoundaries } from "oxalis/model/accessors/dataset_accessor";
+import {
+  getDatasetExtentInLength,
+  getDatasetCenter,
+} from "oxalis/model/accessors/dataset_accessor";
 import { getInputCatcherAspectRatio } from "oxalis/model/accessors/view_mode_accessor";
 import {
   getPlaneExtentInVoxelFromStore,
@@ -25,7 +28,6 @@ import {
 import { listenToStoreProperty } from "oxalis/model/helpers/listener_helpers";
 import { setTDCameraAction } from "oxalis/model/actions/view_mode_actions";
 import { voxelToNm, getBaseVoxel } from "oxalis/model/scaleinfo";
-import Dimensions from "oxalis/model/dimensions";
 import Store, { type CameraData } from "oxalis/store";
 import api from "oxalis/api/internal_api";
 
@@ -168,81 +170,63 @@ class CameraController extends React.PureComponent<Props> {
 }
 
 type TweenState = {
-  upX: number,
-  upY: number,
-  upZ: number,
-  dx: number,
-  dy: number,
-  dz: number,
-  l: number,
-  r: number,
-  t: number,
-  b: number,
+  up: Vector3,
+  position: Vector3,
+  left: number,
+  right: number,
+  top: number,
+  bottom: number,
 };
 
 export function rotate3DViewTo(id: OrthoView, animate: boolean = true): void {
   const state = Store.getState();
   const { dataset } = state;
   const { tdCamera } = state.viewModeData.plane;
-  const b = voxelToNm(dataset.dataSource.scale, getBoundaries(dataset).upperBoundary);
   const flycamPos = voxelToNm(dataset.dataSource.scale, getPosition(state.flycam));
-
-  const aspectRatio = getInputCatcherAspectRatio(state, OrthoViews.TDView);
+  const datasetExtent = getDatasetExtentInLength(dataset);
   // This distance ensures that the 3D camera is so far "in the back" that all elements in the scene
   // are in front of it and thus visible.
   const clippingOffsetFactor = 900000;
+  // Factor to reduce the clipping offset of the z coordinate to get a better angle on the dataset.
+  const zReductionFactor = 0.7;
+  // Use width and height to keep the same zoom.
+  let width = tdCamera.right - tdCamera.left;
+  let height = tdCamera.top - tdCamera.bottom;
 
-  let to: TweenState;
-  if (id === OrthoViews.TDView) {
-    const diagonal = Math.sqrt(b[0] * b[0] + b[1] * b[1]);
-    const padding = 0.05 * diagonal;
-
-    // Calculate the distance from (0, b[1]) in order to center the view
-    const a1 = b[0];
-    const b1 = -b[1];
-    const x1 = 0;
-    const y1 = b[1];
-    const x2 = flycamPos[0];
-    const y2 = flycamPos[1];
-
-    const b2 = 1 / Math.sqrt((b1 * b1) / a1 / a1 + 1);
-    const a2 = (-b2 * b1) / a1;
-    const d2 = ((a1 / b1) * (y1 - y2) - x1 + x2) / (-a2 + (a1 * b2) / b1);
-
-    const intersect = [x2 + d2 * a2, y2 + d2 * b2];
-    const distance = Dimensions.distance([x1, y1], intersect);
-
-    // Approximation to center the view vertically
-    const yOffset = flycamPos[2] - b[2] / 2;
-
-    // Calulate the x coordinate so that the vector from the camera to the cube's middle point is
-    // perpendicular to the vector going from (0, b[1], 0) to (b[0], 0, 0).
-
-    const squareLeft = -distance - padding;
-    const squareRight = diagonal - distance + padding;
-    const squareTop = diagonal / 2 + padding + yOffset;
-    const squareBottom = -diagonal / 2 - padding + yOffset;
-    const squareCenterX = (squareLeft + squareRight) / 2;
-    const squareCenterY = (squareTop + squareBottom) / 2;
-    const squareWidth = Math.abs(squareLeft - squareRight);
-
-    const height = squareWidth / aspectRatio;
-
-    to = {
-      dx: (b[1] / diagonal) * clippingOffsetFactor,
-      dy: (b[0] / diagonal) * clippingOffsetFactor,
-      dz: (-1 / 2) * clippingOffsetFactor,
-      upX: 0,
-      upY: 0,
-      upZ: -1,
-      l: squareCenterX - squareWidth / 2,
-      r: squareCenterX + squareWidth / 2,
-      t: squareCenterY + height / 2,
-      b: squareCenterY - height / 2,
-    };
+  let position: Vector3;
+  let up: Vector3;
+  // Way to calculate the position and rotation of the camera:
+  // First, the camera is either positioned at the current center of the flycam or in the dataset center.
+  // Second, the camera is the moved backwards by a clipping factor into the wanted direction.
+  // Together with matching lookUp (up) vectors and keeping the width and height, the position and rotation updates correctly.
+  if (id === OrthoViews.TDView && (height <= 0 || width <= 0)) {
+    // This should only be the case when initializing the 3D-viewport.
+    const aspectRatio = getInputCatcherAspectRatio(state, OrthoViews.TDView);
+    const datasetCenter = voxelToNm(dataset.dataSource.scale, getDatasetCenter(dataset));
+    // The camera has not width and height which might be due to a bug or the camera has not been initialized.
+    // Thus we zoom out to show the whole dataset.
+    const paddingFactor = 1.1;
+    width =
+      Math.sqrt(
+        datasetExtent.width * datasetExtent.width + datasetExtent.height * datasetExtent.height,
+      ) * paddingFactor;
+    height = width / aspectRatio;
+    up = [0, 0, -1];
+    // For very high datasets that have a very low or hight z starting coordinate, the planes might not be visible.
+    // Thus take the z coordinate of the flycam instead of the z coordinate of the center.
+    position = [
+      datasetCenter[0] - clippingOffsetFactor,
+      datasetCenter[1] - clippingOffsetFactor,
+      flycamPos[2] - clippingOffsetFactor * zReductionFactor,
+    ];
+  } else if (id === OrthoViews.TDView) {
+    position = [
+      flycamPos[0] - clippingOffsetFactor,
+      flycamPos[1] - clippingOffsetFactor,
+      flycamPos[2] - clippingOffsetFactor * zReductionFactor,
+    ];
+    up = [0, 0, -1];
   } else {
-    const width = tdCamera.right - tdCamera.left;
-    const height = tdCamera.top - tdCamera.bottom;
     const positionOffset: OrthoViewMap<Vector3> = {
       [OrthoViews.PLANE_XY]: [0, 0, -clippingOffsetFactor],
       [OrthoViews.PLANE_YZ]: [clippingOffsetFactor, 0, 0],
@@ -253,38 +237,39 @@ export function rotate3DViewTo(id: OrthoView, animate: boolean = true): void {
       [OrthoViews.PLANE_YZ]: [0, -1, 0],
       [OrthoViews.PLANE_XZ]: [0, 0, -1],
     };
-
-    to = {
-      dx: positionOffset[id][0],
-      dy: positionOffset[id][1],
-      dz: positionOffset[id][2],
-      upX: upVector[id][0],
-      upY: upVector[id][1],
-      upZ: upVector[id][2],
-      l: -width / 2,
-      r: width / 2,
-      t: height / 2,
-      b: -height / 2,
-    };
+    up = upVector[id];
+    position = [
+      positionOffset[id][0] + flycamPos[0],
+      positionOffset[id][1] + flycamPos[1],
+      positionOffset[id][2] + flycamPos[2],
+    ];
   }
+  // Ensure a valid width and height if camera ended up with no width and height due to a bug.
+  width = width > 0 ? width : datasetExtent.width;
+  height = height > 0 ? height : datasetExtent.height;
+  const to: TweenState = {
+    position,
+    up,
+    left: -width / 2,
+    right: width / 2,
+    top: height / 2,
+    bottom: -height / 2,
+  };
 
   const updateCameraTDView = (tweenState: TweenState) => {
     const currentFlycamPos = voxelToNm(
       Store.getState().dataset.dataSource.scale,
       getPosition(Store.getState().flycam),
     );
+    const { position: newPosition, up: upDirection, left, right, top, bottom } = tweenState;
     Store.dispatch(
       setTDCameraAction({
-        position: [
-          tweenState.dx + currentFlycamPos[0],
-          tweenState.dy + currentFlycamPos[1],
-          tweenState.dz + currentFlycamPos[2],
-        ],
-        left: tweenState.l,
-        right: tweenState.r,
-        top: tweenState.t,
-        bottom: tweenState.b,
-        up: [tweenState.upX, tweenState.upY, tweenState.upZ],
+        position: newPosition,
+        up: upDirection,
+        left,
+        right,
+        top,
+        bottom,
         lookAt: currentFlycamPos,
       }),
     );
@@ -295,13 +280,13 @@ export function rotate3DViewTo(id: OrthoView, animate: boolean = true): void {
       upX: tdCamera.up[0],
       upY: tdCamera.up[1],
       upZ: tdCamera.up[2],
-      dx: tdCamera.position[0] - flycamPos[0],
-      dy: tdCamera.position[1] - flycamPos[1],
-      dz: tdCamera.position[2] - flycamPos[2],
-      l: tdCamera.left,
-      r: tdCamera.right,
-      t: tdCamera.top,
-      b: tdCamera.bottom,
+      dx: tdCamera.position[0],
+      dy: tdCamera.position[1],
+      dz: tdCamera.position[2],
+      left: tdCamera.left,
+      right: tdCamera.right,
+      top: tdCamera.top,
+      bottom: tdCamera.bottom,
     };
     const tween = new TWEEN.Tween(from);
 

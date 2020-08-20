9c31a5c4bc729dd2df229edbbd403e37afb0e138
Gritta Weisheit <grittaweisheit@gmail.com>
2020-08-20 13:41:19 +0200

    add working 2d layouts

--- frontend/javascripts/oxalis/view/layouting/layout_persistence.js
+++ frontend/javascripts/oxalis/view/layouting/layout_persistence.js
@@ -57,23 +57,23 @@ function readStoredLayoutConfigs() {
         OrthoLayout: {
           "Custom Layout": layouts.OrthoLayout || defaultLayoutConfig.OrthoLayout,
         },
-        OrthoLayout2D: {
-          "Custom Layout": layouts.OrthoLayout2D || defaultLayoutConfig.OrthoLayout2D,
+        OrthoLayout2d: {
+          "Custom Layout": layouts.OrthoLayout2d || defaultLayoutConfig.OrthoLayout2d,
         },
-        OrthoLayoutView2D: {
-          "Custom Layout": layouts.OrthoLayoutView2D || defaultLayoutConfig.OrthoLayoutView2D,
+        OrthoLayoutView2d: {
+          "Custom Layout": layouts.OrthoLayoutView2d || defaultLayoutConfig.OrthoLayoutView2d,
         },
-        VolumeTracingView2D: {
-          "Custom Layout": layouts.VolumeTracingView2D || defaultLayoutConfig.VolumeTracingView2D,
+        VolumeTracingView2d: {
+          "Custom Layout": layouts.VolumeTracingView2d || defaultLayoutConfig.VolumeTracingView2d,
         },
         LastActiveLayouts: {
           OrthoLayoutView: "Custom Layout",
           VolumeTracingView: "Custom Layout",
           ArbitraryLayout: "Custom Layout",
           OrthoLayout: "Custom Layout",
-          OrthoLayout2D: "Custom Layout",
-          OrthoLayoutView2D: "Custom Layout",
-          VolumeTracingView2D: "Custom Layout",
+          OrthoLayout2d: "Custom Layout",
+          OrthoLayoutView2d: "Custom Layout",
+          VolumeTracingView2d: "Custom Layout",
         },
       };
       return withMulipleLayoutsSchema;
@@ -83,9 +83,9 @@ function readStoredLayoutConfigs() {
       layouts.VolumeTracingView &&
       layouts.ArbitraryLayout &&
       layouts.OrthoLayout &&
-      layouts.OrthoLayout2D &&
-      layouts.VolumeTracingView2D &&
-      layouts.OrthoLayoutView2D
+      layouts.OrthoLayout2d &&
+      layouts.VolumeTracingView2d &&
+      layouts.OrthoLayoutView2d
     ) {
       return layouts;
     }

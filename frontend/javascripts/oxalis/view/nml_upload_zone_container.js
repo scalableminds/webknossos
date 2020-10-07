0d11203d4f129ae8656866c59381aa87b80d4d6b
Philipp Otto <philipp.4096@googlemail.com>
2020-10-07 15:58:34 +0200

    adapt headline in import modal

--- frontend/javascripts/oxalis/view/nml_upload_zone_container.js
+++ frontend/javascripts/oxalis/view/nml_upload_zone_container.js
@@ -187,7 +187,7 @@ class NmlUploadZoneContainer extends React.PureComponent<Props, State> {
     const pluralS = this.state.files.length > 1 ? "s" : "";
     return (
       <Modal
-        title={`Import ${this.state.files.length} NML file${pluralS}`}
+        title={`Import ${this.state.files.length} Tracing${pluralS}`}
         visible={this.state.files.length > 0}
         onCancel={() => this.setState({ files: [] })}
         footer={

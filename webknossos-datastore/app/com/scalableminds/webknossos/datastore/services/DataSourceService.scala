dcfc8423a41dd34188c0efc6bd80c0a2f551521a
Florian M <florian@scm.io>
2020-10-15 13:25:08 +0200

    changelog

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/DataSourceService.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/DataSourceService.scala
@@ -47,8 +47,11 @@ class DataSourceService @Inject()(
 
   var inboxCheckVerboseCounter = 0
 
-  def tick: Unit =
-    ()
+  def tick: Unit = {
+    checkInbox(verbose = inboxCheckVerboseCounter == 0)
+    inboxCheckVerboseCounter += 1
+    if (inboxCheckVerboseCounter >= 10) inboxCheckVerboseCounter = 0
+  }
 
   private def skipTrash(path: Path) = !path.toString.contains(".trash")
 

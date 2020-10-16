bfc8f3978fc6353feedba2c4acded8afa99d9570
Youri K <youri.kaminsky@student.hpi.de>
2020-10-16 14:39:23 +0200

    fix naming in reversion of evolution

--- conf/evolutions/reversions/056-add-layer-specific-view-configs.sql
+++ conf/evolutions/reversions/056-add-layer-specific-view-configs.sql
@@ -3,11 +3,11 @@ START TRANSACTION;
 
 -- pre-aggregate all layer jsons into one json object, set json object in configuration
 UPDATE webknossos.user_dataSetConfigurations dsC
-SET configuration = jsonb_set(
-        dsC.configuration,
+SET viewConfiguration = jsonb_set(
+        dsC.viewConfiguration,
         array['layers'],
         subQ.json)
-FROM (select _user, _dataset, jsonb_object_agg(layerName, configuration) AS json FROM webknossos.user_dataSetLayerConfigurations dlC GROUP BY _user, _dataset) AS subQ
+FROM (select _user, _dataset, jsonb_object_agg(layerName, viewConfiguration) AS json FROM webknossos.user_dataSetLayerConfigurations dlC GROUP BY _user, _dataset) AS subQ
 WHERE dsC._user = subQ._user and dsC._dataset = subQ._dataset;
 
 --Rename view configuration back to old naming

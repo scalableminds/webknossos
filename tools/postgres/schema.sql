2e9219a61ae570cc12947f19cb5e9e2e0d570a89
Youri K <youri.kaminsky@student.hpi.de>
2020-10-26 13:31:07 +0100

    bump schema version

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -21,7 +21,7 @@ START TRANSACTION;
 CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(56);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(57);
 COMMIT TRANSACTION;
 
 CREATE TABLE webknossos.analytics(

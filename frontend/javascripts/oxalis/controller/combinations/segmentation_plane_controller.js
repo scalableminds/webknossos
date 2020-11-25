cfdb9a5c841f3722e1c486235cb77a8c0b8cf7eb
Daniel Werner <daniel.werner@scalableminds.com>
2020-11-25 16:41:42 +0100

    add shortcut to load agglomerate skeleton

--- frontend/javascripts/oxalis/controller/combinations/segmentation_plane_controller.js
+++ frontend/javascripts/oxalis/controller/combinations/segmentation_plane_controller.js
@@ -4,12 +4,57 @@ import Model from "oxalis/model";
 import { changeActiveIsosurfaceCellAction } from "oxalis/model/actions/segmentation_actions";
 import { calculateGlobalPos } from "oxalis/controller/viewmodes/plane_controller";
 import { getRequestLogZoomStep } from "oxalis/model/accessors/flycam_accessor";
+import { getAgglomerateSkeleton } from "admin/admin_rest_api";
+import { parseProtoTracing } from "oxalis/model/helpers/proto_helpers";
+import { createMutableTreeMapFromTreeArray } from "oxalis/model/reducers/skeletontracing_reducer_helpers";
+import { addTreesAndGroupsAction } from "oxalis/model/actions/skeletontracing_actions";
 import Store from "oxalis/store";
 
-function isosurfaceLeftClick(pos: Point2, plane: OrthoView, event: MouseEvent) {
+function viewLeftClick(pos: Point2, plane: OrthoView, event: MouseEvent) {
   if (!event.shiftKey) {
     return;
   }
+
+  if (event.ctrlKey) {
+    agglomerateSkeletonLeftClick(pos);
+  }
+
+  isosurfaceLeftClick(pos);
+}
+
+async function agglomerateSkeletonLeftClick(pos: Point2) {
+  const position = calculateGlobalPos(pos);
+
+  const segmentation = Model.getSegmentationLayer();
+  if (!segmentation) {
+    return;
+  }
+  const state = Store.getState();
+  const cellId = segmentation.cube.getMappedDataValue(position, getRequestLogZoomStep(state));
+  if (cellId > 0) {
+    const layerName = segmentation.name;
+    const mappingId = state.temporaryConfiguration.activeMapping.mappingName;
+    const { dataset } = state;
+    const result = await getAgglomerateSkeleton(
+      dataset.dataStore.url,
+      dataset,
+      layerName,
+      mappingId,
+      cellId,
+    );
+
+    const nmlProtoBuffer = result;
+    const parsedTracing = parseProtoTracing(nmlProtoBuffer, "skeleton");
+    Store.dispatch(
+      addTreesAndGroupsAction(
+        createMutableTreeMapFromTreeArray(parsedTracing.trees),
+        parsedTracing.treeGroups,
+      ),
+    );
+  }
+}
+
+function isosurfaceLeftClick(pos: Point2) {
   let cellId = 0;
   const position = calculateGlobalPos(pos);
   const volumeTracingMaybe = Store.getState().tracing.volume;
@@ -30,4 +75,4 @@ function isosurfaceLeftClick(pos: Point2, plane: OrthoView, event: MouseEvent) {
   }
 }
 
-export default isosurfaceLeftClick;
+export default viewLeftClick;

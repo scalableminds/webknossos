bcf656b7aa46c86e193abfcf18aca8f57cbfb5e4
Florian M <florian@scm.io>
2020-12-15 11:49:26 +0100

    create multiuser entries in evolution

--- conf/evolutions/060-multiusers.sql
+++ conf/evolutions/060-multiusers.sql
@@ -2,10 +2,35 @@
 
 START TRANSACTION;
 
+-- Access tokens now have to be linked to user via id instead of email
+UPDATE webknossos.tokens SET logininfo_providerkey = u._id
+FROM (SELECT _id, email FROM webknossos.users) AS u
+WHERE webknossos.tokens.logininfo_providerkey = u.email;
+
 DROP VIEW webknossos.users_;
 
+-- We have to generate fake object ids for the new multiUser objects
+CREATE FUNCTION generate_object_id() RETURNS varchar AS $$
+    DECLARE
+        time_component bigint;
+        machine_id bigint := FLOOR(random() * 16777215);
+        process_id bigint;
+        seq_id bigint := FLOOR(random() * 16777215);
+        result varchar:= '';
+    BEGIN
+        SELECT FLOOR(EXTRACT(EPOCH FROM clock_timestamp())) INTO time_component;
+        SELECT pg_backend_pid() INTO process_id;
+
+        result := result || lpad(to_hex(time_component), 8, '0');
+        result := result || lpad(to_hex(machine_id), 6, '0');
+        result := result || lpad(to_hex(process_id), 4, '0');
+        result := result || lpad(to_hex(seq_id), 6, '0');
+        RETURN result;
+    END;
+$$ LANGUAGE PLPGSQL;
+
 CREATE TABLE webknossos.multiUsers(
-  _id CHAR(24) PRIMARY KEY DEFAULT '',
+  _id CHAR(24) PRIMARY KEY DEFAULT generate_object_id(),
   email VARCHAR(512) NOT NULL UNIQUE CHECK (email ~* '^.+@.+$'),
   passwordInfo_hasher webknossos.USER_PASSWORDINFO_HASHERS NOT NULL DEFAULT 'SCrypt',
   passwordInfo_password VARCHAR(512) NOT NULL,
@@ -15,6 +40,26 @@ CREATE TABLE webknossos.multiUsers(
   isDeleted BOOLEAN NOT NULL DEFAULT false
 );
 
+-- For each user, create one multiUser
+INSERT INTO webknossos.multiUsers(email, passwordInfo_password, isSuperUser, created, isDeleted)
+SELECT email, passwordInfo_password, isSuperUser, created, isDeleted
+FROM webknossos.users;
+
+-- In production, the objectIds should be created by the client, not the db.
+ALTER TABLE webknossos.multiUsers ALTER COLUMN _id SET DEFAULT '';
+DROP FUNCTION generate_object_id;
+
+-- Each user needs to point to its multiUser
+ALTER TABLE webknossos.users ADD COLUMN _multiUser CHAR(24);
+
+UPDATE webknossos.users SET _multiUser = m._id
+FROM (SELECT _id, email FROM webknossos.multiUsers) AS m
+WHERE webknossos.users.email = m.email;
+
+-- Now the constraints can be added
+ALTER TABLE webknossos.users ALTER COLUMN _multiUser SET NOT NULL;
+ALTER TABLE webknossos.users ADD UNIQUE (_multiUser, _organization);
+
 
 ALTER TABLE webknossos.users DROP COLUMN email;
 ALTER TABLE webknossos.users DROP COLUMN loginInfo_providerID;
@@ -22,8 +67,6 @@ ALTER TABLE webknossos.users DROP COLUMN loginInfo_providerKey;
 ALTER TABLE webknossos.users DROP COLUMN passwordInfo_hasher;
 ALTER TABLE webknossos.users DROP COLUMN passwordInfo_password;
 ALTER TABLE webknossos.users DROP COLUMN isSuperUser;
-ALTER TABLE webknossos.users ADD COLUMN _multiUser CHAR(24) NOT NULL;
-ALTER TABLE webknossos.users ADD UNIQUE (_multiUser, _organization);
 
 CREATE TABLE webknossos.invites(
   _id CHAR(24) PRIMARY KEY DEFAULT '',

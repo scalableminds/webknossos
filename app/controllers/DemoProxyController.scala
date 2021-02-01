9aa1b83fc2f0813919d46466e340cb52e5ed8431
Florian M <florian@scm.io>
2021-02-01 14:30:31 +0100

    Force utf-8, allow wildcard

--- app/controllers/DemoProxyController.scala
+++ app/controllers/DemoProxyController.scala
@@ -10,18 +10,24 @@ import play.api.mvc.{Action, AnyContent}
 import utils.WkConf
 
 import scala.concurrent.ExecutionContext
+import scala.util.matching.Regex
 
 class DemoProxyController @Inject()(ws: WSClient, conf: WkConf, sil: Silhouette[WkEnv])(implicit ec: ExecutionContext)
     extends Controller {
 
-  def matchesProxyPage(request: UserAwareRequest[WkEnv, AnyContent]): Boolean =
-    conf.Features.isDemoInstance && conf.Proxy.routes
-      .contains(request.uri) && (request.identity.isEmpty || request.uri != "/")
-
   def proxyPageOrMainView: Action[AnyContent] = sil.UserAwareAction.async { implicit request =>
     if (matchesProxyPage(request)) {
-      ws.url(conf.Proxy.prefix + request.uri).get().map(resp => Ok(resp.body).as("text/html"))
+      ws.url(conf.Proxy.prefix + request.uri).get().map(resp => Ok(resp.bodyAsBytes.utf8String).as("text/html"))
     } else Fox.successful(Ok(views.html.main(conf)))
+  }
+
+  private def matchesProxyPage(request: UserAwareRequest[WkEnv, AnyContent]): Boolean =
+    conf.Features.isDemoInstance && conf.Proxy.routes
+      .exists(route => matchesPageWithWildcard(route, request.uri)) && (request.identity.isEmpty || request.uri != "/")
+
+  private def matchesPageWithWildcard(routeWithWildcard: String, actualRequest: String): Boolean = {
+    val wildcardRegex = "^" + Regex.quote(routeWithWildcard).replace("*", "\\E.*\\Q") + "$"
+    actualRequest.matches(wildcardRegex)
   }
 
 }

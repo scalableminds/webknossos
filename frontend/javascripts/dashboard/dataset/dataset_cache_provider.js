3d87c3b2b2b74d529472e80c6a2f81de2b5bda72
Philipp Otto <philippotto@users.noreply.github.com>
2021-02-03 15:36:53 +0100

    Update frontend/javascripts/dashboard/dataset/dataset_cache_provider.js
    Co-authored-by: Daniel <daniel.werner@scalableminds.com>

--- frontend/javascripts/dashboard/dataset/dataset_cache_provider.js
+++ frontend/javascripts/dashboard/dataset/dataset_cache_provider.js
@@ -159,7 +159,6 @@ export default function DatasetCacheProvider({ children }: { children: Node }) {
     }
   }
 
-  console.log("datasets", datasets);
   return (
     <DatasetCacheContext.Provider
       value={{ datasets, isLoading, checkDatasets, fetchDatasets, updateDataset }}

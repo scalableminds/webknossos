bf66a79156d130080bcd226ec508ff2b2bae3e49
Florian M <florian@scm.io>
2021-03-02 09:45:43 +0100

    format

--- app/models/project/Project.scala
+++ app/models/project/Project.scala
@@ -219,9 +219,12 @@ class ProjectService @Inject()(projectDAO: ProjectDAO, teamDAO: TeamDAO, userSer
       )
     }
 
-  def publicWritesWithStatus(project: Project, openTaskInstances: Int, tracingTime: Int)(implicit ctx: DBAccessContext): Fox[JsObject] =
+  def publicWritesWithStatus(project: Project, openTaskInstances: Int, tracingTime: Int)(
+      implicit ctx: DBAccessContext): Fox[JsObject] =
     for {
       projectJson <- publicWrites(project)
-    } yield projectJson ++ Json.obj("numberOfOpenAssignments" -> JsNumber(openTaskInstances), "tracingTime" -> tracingTime)
+    } yield {
+      projectJson ++ Json.obj("numberOfOpenAssignments" -> JsNumber(openTaskInstances), "tracingTime" -> tracingTime)
+    }
 
 }

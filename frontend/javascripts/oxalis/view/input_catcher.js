f2e387348e5a72c0ca7b7a8daa0c35d929c9c01f
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2021-03-16 15:01:01 +0100

    undo making arbitrary viewport quadratic with css only

--- frontend/javascripts/oxalis/view/input_catcher.js
+++ frontend/javascripts/oxalis/view/input_catcher.js
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 
-import { /* ArbitraryViewport, */ type Rect, type Viewport } from "oxalis/constants";
+import { ArbitraryViewport, type Rect, type Viewport } from "oxalis/constants";
 import { setInputCatcherRects } from "oxalis/model/actions/view_mode_actions";
 import Scalebar from "oxalis/view/scalebar";
 import ViewportStatusIndicator from "oxalis/view/viewport_status_indicator";
@@ -22,32 +22,25 @@ function ignoreContextMenu(event: SyntheticInputEvent<>) {
 
 // Is able to make the input catcher a square (if makeQuadratic is true)
 // and returns its position within the document relative to the rendering canvas
-function adaptInputCatcher(inputCatcherDOM: HTMLElement /* , makeQuadratic: boolean */): Rect {
+function adaptInputCatcher(inputCatcherDOM: HTMLElement, makeQuadratic: boolean): Rect {
   const noneOverflowWrapper = inputCatcherDOM.closest(".flexlayout-dont-overflow");
   if (!noneOverflowWrapper) {
     return { top: 0, left: 0, width: 0, height: 0 };
   }
-
-  // TODO Maybe get rid of this width and height calculation and just use css props (if makeQuadratic) is not set
-
-  /* const getExtent = () => {
-    let { width, height } = noneOverflowWrapper.getBoundingClientRect();
-    // These values should be floored, so that the rendered area does not overlap
-    // with the golden layout containers
-    width = Math.floor(width);
-    height = Math.floor(height);
-
-    if (makeQuadratic) {
+  if (makeQuadratic) {
+    const getQuadraticExtent = () => {
+      let { width, height } = noneOverflowWrapper.getBoundingClientRect();
+      // These values should be floored, so that the rendered area does not overlap
+      // with the containers.
+      width = Math.floor(width);
+      height = Math.floor(height);
       const extent = Math.min(width, height);
       return [extent, extent];
-    } else {
-      return [width, height];
-    }
-  };
-  const [width, height] = getExtent();
-  inputCatcherDOM.style.width = `${width}px`;
-  inputCatcherDOM.style.height = `${height}px`; */
-
+    };
+    const [width, height] = getQuadraticExtent();
+    inputCatcherDOM.style.width = `${width}px`;
+    inputCatcherDOM.style.height = `${height}px`;
+  }
   return makeRectRelativeToCanvas(inputCatcherDOM.getBoundingClientRect());
 }
 
@@ -56,7 +49,8 @@ const renderedInputCatchers = new Map();
 export function recalculateInputCatcherSizes() {
   const viewportRects = {};
   for (const [viewportID, inputCatcher] of renderedInputCatchers.entries()) {
-    const rect = adaptInputCatcher(inputCatcher /* , viewportID === ArbitraryViewport */);
+    const makeQuadratic = viewportID === ArbitraryViewport;
+    const rect = adaptInputCatcher(inputCatcher, makeQuadratic);
     viewportRects[viewportID] = rect;
   }
   Store.dispatch(setInputCatcherRects(viewportRects));

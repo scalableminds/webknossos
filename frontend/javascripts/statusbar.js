3634d38e7fc388f5253918cd5d63c3e46a0d241a
Gritta Weisheit <grittaweisheit@gmail.com>
2021-04-07 16:23:30 +0200

    fix statusbar with missing seg layer

--- frontend/javascripts/statusbar.js
+++ frontend/javascripts/statusbar.js
@@ -8,7 +8,6 @@ import type { OxalisState } from "oxalis/store";
 import { type Vector2, type Vector3, type OrthoView, OrthoViews } from "oxalis/constants";
 import { getCurrentResolution } from "oxalis/model/accessors/flycam_accessor";
 import api from "oxalis/api/internal_api";
-import DataLayer from "oxalis/model/data_layer";
 import { calculateGlobalPos } from "oxalis/controller/viewmodes/plane_controller";
 import Cube from "oxalis/model/bucket_data_handling/data_cube";
 import { V3 } from "libs/mjs";
@@ -19,15 +18,15 @@ type StateProps = {|
   activeResolution: Vector3,
   activeViewport: OrthoView,
   mousePosition: ?Vector2,
-  // activeCellId: ?number,
-  // segmentationLayer: APILayer,
 |};
 type Props = {| ...OwnProps, ...StateProps |};
 type State = {||};
 
 const borderToggleButtonMargin = 40;
+const spaceBetweenItems = 20;
 
 const statusbarStyle: Object = {
+  paddingTop: 2,
   marginLeft: borderToggleButtonMargin,
   marginRight: borderToggleButtonMargin,
 };
@@ -38,22 +37,10 @@ class Statusbar extends React.PureComponent<Props, State> {
   isMounted: boolean = false;
   componentDidMount() {
     this.isMounted = true;
-    if (!hasSegmentation()) {
-      return;
-    }
-    const cube = this.getSegmentationCube();
-    cube.off("bucketLoaded", this._forceUpdate);
-    cube.off("volumeLabeled", this._forceUpdate);
   }
 
   componentWillUnmount() {
     this.isMounted = false;
-    if (!hasSegmentation()) {
-      return;
-    }
-    const cube = this.getSegmentationCube();
-    cube.off("bucketLoaded", this._forceUpdate);
-    cube.off("volumeLabeled", this._forceUpdate);
   }
 
   // eslint-disable-next-line react/sort-comp
@@ -64,23 +51,106 @@ class Statusbar extends React.PureComponent<Props, State> {
     this.forceUpdate();
   }, 100);
 
-  getSegmentationLayer(): DataLayer {
-    const layer = Model.getSegmentationLayer();
-    if (!layer) {
-      throw new Error("No segmentation layer found");
-    }
-    return layer;
-  }
-
   getSegmentationCube(): Cube {
-    const segmentationLayer = this.getSegmentationLayer();
+    const segmentationLayer = Model.getSegmentationLayer();
     return segmentationLayer.cube;
   }
 
-  render() {
-    if (!hasSegmentation()) {
-      return "No segmentation available";
-    }
+  getPosString(pos: Vector3) {
+    return V3.floor(pos).join(",");
+  }
+
+  getZoomShortcut() {
+    return (
+      <span key="zoom">
+        <span
+          key="zoom-i"
+          className="keyboard-key-icon-small"
+          style={{
+            marginLeft: spaceBetweenItems,
+            borderColor: "rgba(255, 255, 255, 0.67)",
+          }}
+        >
+          I
+        </span>{" "}
+        /{" "}
+        <span
+          key="zoom-o"
+          className="keyboard-key-icon-small"
+          style={{
+            borderColor: "rgba(255, 255, 255, 0.67)",
+          }}
+        >
+          O
+        </span>{" "}
+        Zoom in/out
+      </span>
+    );
+  }
+
+  getShortcuts() {
+    const { activeViewport } = this.props;
+    return (
+      <Col span={14} style={{ textAlign: "right" }}>
+        <span>
+          <img
+            className="keyboard-mouse-icon"
+            src="/assets/images/icon-mouse-left-drag.svg"
+            alt="Mouse Left Drag"
+            style={{ height: 12 }}
+          />
+          Move
+        </span>
+        <span style={{ marginLeft: spaceBetweenItems }}>
+          <img
+            className="keyboard-mouse-icon"
+            src="/assets/images/icon-mousewheel.svg"
+            alt="Mouse Wheel"
+            style={{ height: 12 }}
+          />
+          Move along 3rd axis
+        </span>
+        {this.getZoomShortcut()}
+        {activeViewport === OrthoViews.TDView && (
+          <span
+            style={{
+              marginLeft: spaceBetweenItems,
+            }}
+          >
+            <img
+              className="keyboard-mouse-icon"
+              src="/assets/images/icon-mouse-right.svg"
+              alt="Mouse Right"
+              style={{ height: 12 }}
+            />
+            Rotate 3D View
+          </span>
+        )}
+      </Col>
+    );
+  }
+
+  getCellInfo(globalMousePosition: ?Vector3) {
+    const segmentationLayerName = Model.getSegmentationLayer().name;
+    const cube = this.getSegmentationCube();
+    const renderedZoomStepForMousePosition = api.data.getRenderedZoomStepAtPosition(
+      segmentationLayerName,
+      globalMousePosition,
+    );
+    const getIdForPos = (pos, usableZoomStep) =>
+      pos && cube.getDataValue(pos, null, usableZoomStep);
+
+    return (
+      <span style={{ marginLeft: spaceBetweenItems }}>
+        Cell:{" "}
+        {globalMousePosition
+          ? getIdForPos(globalMousePosition, renderedZoomStepForMousePosition)
+          : "-"}
+      </span>
+    );
+  }
+
+  getInfos() {
     const { activeViewport, mousePosition, activeResolution } = this.props;
     let globalMousePosition;
     if (mousePosition && activeViewport !== OrthoViews.TDView) {
@@ -88,97 +158,29 @@ class Statusbar extends React.PureComponent<Props, State> {
       globalMousePosition = calculateGlobalPos({ x, y });
     }
 
-    const spaceBetweenItems = 20;
-
-    const segmentationLayerName = this.getSegmentationLayer().name;
-    const cube = this.getSegmentationCube();
-
-    const renderedZoomStepForMousePosition = api.data.getRenderedZoomStepAtPosition(
-      segmentationLayerName,
-      globalMousePosition,
+    return (
+      <Col span={10} style={{ textAlign: "left" }}>
+        <span>
+          <img
+            src="/assets/images/icon-downsampling.svg"
+            style={{ width: 15, height: 15 }}
+            alt="Resolution"
+          />
+          {activeResolution.join("-")}{" "}
+        </span>
+        <span style={{ marginLeft: spaceBetweenItems }}>
+          Pos: [{globalMousePosition ? this.getPosString(globalMousePosition) : "-,-,-"}]
+        </span>
+        {hasSegmentation() && this.getCellInfo(globalMousePosition)}
+      </Col>
     );
+  }
 
-    const getIdForPos = (pos, usableZoomStep) =>
-      pos && cube.getDataValue(pos, null, usableZoomStep);
-
-    const getPosString = pos => V3.floor(pos).join(",");
+  render() {
     return (
       <Row style={statusbarStyle}>
-        <Col span={10} style={{ textAlign: "left" }}>
-          <span>
-            <img
-              src="/assets/images/icon-downsampling.svg"
-              style={{ width: 15, height: 15 }}
-              alt="Resolution"
-            />
-            {activeResolution.join("-")}{" "}
-          </span>
-          <span style={{ marginLeft: spaceBetweenItems }}>
-            Pos: [{globalMousePosition ? getPosString(globalMousePosition) : "-,-,-"}]
-          </span>
-          <span style={{ marginLeft: spaceBetweenItems }}>
-            Cell:{" "}
-            {globalMousePosition
-              ? getIdForPos(globalMousePosition, renderedZoomStepForMousePosition)
-              : "-"}
-          </span>
-        </Col>
-        <Col span={14} style={{ textAlign: "right" }}>
-          <span>
-            <img
-              key="move-1"
-              className="keyboard-mouse-icon"
-              src="/assets/images/icon-mouse-left-drag.svg"
-              alt="Mouse Left Drag"
-              style={{ height: 12 }}
-            />
-            Move
-          </span>
-          <span style={{ marginLeft: spaceBetweenItems }}>
-            <img
-              key="move-1"
-              className="keyboard-mouse-icon"
-              src="/assets/images/icon-mousewheel.svg"
-              alt="Mouse Wheel"
-              style={{ height: 12 }}
-            />
-            Move along 3rd axis
-          </span>
-          <span
-            key="zoom"
-            className="keyboard-key-icon-small"
-            style={{ marginLeft: spaceBetweenItems, borderColor: "rgba(255, 255, 255, 0.67)" }}
-          >
-            I
-          </span>{" "}
-          /{" "}
-          <span
-            key="zoom"
-            className="keyboard-key-icon-small"
-            style={{
-              borderColor: "rgba(255, 255, 255, 0.67)",
-            }}
-          >
-            O
-          </span>{" "}
-          Zoom in/out
-          {activeViewport === OrthoViews.TDView && (
-            <span
-              style={{
-                marginLeft: spaceBetweenItems,
-              }}
-            >
-              <img
-                key="move-1"
-                className="keyboard-mouse-icon"
-                src="/assets/images/icon-mouse-right.svg"
-                alt="Mouse Right"
-                style={{ height: 12 }}
-              />
-              Rotate 3D View
-            </span>
-          )}
-        </Col>
+        {this.getInfos()}
+        {this.getShortcuts()}
       </Row>
     );
   }

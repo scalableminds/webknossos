0185566cb354800d84bf7a0b82bdf7961658e20e
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2021-04-08 12:04:23 +0200

    increase layout version

--- frontend/javascripts/oxalis/view/layouting/default_layout_configs.js
+++ frontend/javascripts/oxalis/view/layouting/default_layout_configs.js
@@ -33,7 +33,7 @@ import type {
 } from "./flex_layout_types";
 
 // Increment this number to invalidate old layoutConfigs in localStorage
-export const currentLayoutVersion = 11;
+export const currentLayoutVersion = 12;
 const layoutHeaderHeight = 20;
 const dummyExtent = 500;
 export const show3DViewportInArbitrary = false;

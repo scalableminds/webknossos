86945f24c4f9498e44f2926cdef07463ee9e1a2f
Philipp Otto <philippotto@users.noreply.github.com>
2021-04-21 18:58:51 +0200

    Fix flaky sidebar toggles (#5414)
    * fix flaky sidebar toggles
    
    * fix & simplify position calculation of tabsets
    
    Co-authored-by: Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
    Co-authored-by: Norman Rzepka <norman.rzepka@scalableminds.com>

--- frontend/javascripts/oxalis/view/layouting/flex_layout_helper.js
+++ frontend/javascripts/oxalis/view/layouting/flex_layout_helper.js
@@ -1,7 +1,6 @@
 // @flow
 import { Model, Actions, TabSetNode } from "flexlayout-react";
-import Store, { type BorderOpenStatus } from "oxalis/store";
-import { defaultSplitterSize, borderBarSize } from "./default_layout_configs";
+import { type BorderOpenStatus } from "oxalis/store";
 
 export function getMaximizedItemId(model: typeof Model): ?string {
   const maximizedTabset = model.getMaximizedTabset();
@@ -45,41 +44,32 @@ type NodePositionStatus = {
 
 export function getPositionStatusOf(tabSetNode: typeof TabSetNode): NodePositionStatus {
   // We have to determine whether the current tabset is part of the most upper tabsets directly below the header.
-  const rect = tabSetNode.getRect();
-  const isTopMost = rect.y === 0;
+  const tabSetNodeRect = tabSetNode.getRect();
+  const isTopMost = tabSetNodeRect.y === 0;
   let isLeftMost = false;
   let isRightMost = false;
   if (!isTopMost) {
     // In this case we do not need to calculate the other position booleans.
-    return { isTopMost, isLeftMost, isRightMost };
+    return {
+      isTopMost,
+      isLeftMost,
+      isRightMost,
+    };
   }
-  const borders = { left: null, right: null };
-  tabSetNode
-    .getModel()
-    .getBorderSet()
-    .getBorders()
-    .forEach(border => {
-      const side = border.getLocation().getName();
-      borders[side] = border;
-    });
-  const currentBorderOpenStatus = Store.getState().uiInformation.borderOpenStatus;
 
-  if (borders.left != null) {
-    if (currentBorderOpenStatus.left) {
-      const { x: leftBorderX, width: leftBorderWidth } = borders.left.getContentRect();
-      isLeftMost = rect.x === leftBorderX + leftBorderWidth + defaultSplitterSize;
-    } else {
-      isLeftMost = rect.x === 0 + borderBarSize;
-    }
-  }
-  if (borders.right != null) {
-    if (currentBorderOpenStatus.right) {
-      const { x: rightBorderX } = borders.right.getContentRect();
-      isRightMost = rect.x + rect.width + defaultSplitterSize === rightBorderX;
-    } else {
-      // No need to add defaultSplitterSize as the border is hidden and thus there is no splitter.
-      isRightMost = rect.x + rect.width === window.screen.width - borderBarSize;
-    }
-  }
-  return { isTopMost, isLeftMost, isRightMost };
+  const rootContainerRect = tabSetNode
+    .getModel()
+    .getRoot()
+    .getRect();
+
+  // Comparing the left and right side of the tabSetNode with the left and right side
+  // of the root container that contains everything except for the borders.
+  isLeftMost = tabSetNodeRect.x === rootContainerRect.x;
+  isRightMost =
+    tabSetNodeRect.x + tabSetNodeRect.width === rootContainerRect.x + rootContainerRect.width;
+  return {
+    isTopMost,
+    isLeftMost,
+    isRightMost,
+  };
 }

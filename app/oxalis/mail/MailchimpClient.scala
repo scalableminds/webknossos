5380741451273dc72532fb89e936aac2ae9c57f9
Florian M <florian@scm.io>
2021-04-29 09:22:41 +0200

    clean up code

--- app/oxalis/mail/MailchimpClient.scala
+++ app/oxalis/mail/MailchimpClient.scala
@@ -7,6 +7,7 @@ import com.typesafe.scalalogging.LazyLogging
 import javax.inject.Inject
 import models.user.{MultiUser, User}
 import play.api.libs.json.Json
+import play.api.libs.ws.WSResponse
 import utils.WkConf
 
 import scala.concurrent.ExecutionContext
@@ -18,32 +19,32 @@ class MailchimpClient @Inject()(wkConf: WkConf, rpc: RPC)(implicit ec: Execution
   def registerUser(user: User, multiUser: MultiUser, tag: String): Fox[Unit] = {
     if (conf.host.isEmpty) return Fox.successful(())
     val emailMd5 = SCrypt.md5(multiUser.email)
-    val uri = s"${conf.host}/lists/${conf.listId}/members/$emailMd5"
-    val userBody = Json.obj(
-      "email_address" -> multiUser.email,
-      "status" -> "subscribed",
-      "merge_fields" -> Json.obj(
-        "FNAME" -> user.firstName,
-        "LNAME" -> user.lastName,
-      )
-    )
     logger.info(s"Registering user ${user._id} for Mailchimp, tag=$tag")
     for {
-      _ <- rpc(uri).silent.withBasicAuth(conf.user, conf.password).put(userBody)
-      _ <- tagUser(user, multiUser, tag)
+      _ <- registerUser(user.firstName, user.lastName, multiUser.email, emailMd5)
+      _ <- tagUser(emailMd5, tag)
     } yield ()
   }
 
-  private def tagUser(user: User, multiUser: MultiUser, tag: String): Fox[Unit] = {
-    if (conf.host.isEmpty) return Fox.successful(())
-    val emailMd5 = SCrypt.md5(multiUser.email)
+  private def registerUser(firstName: String, lastName: String, email: String, emailMd5: String): Fox[WSResponse] = {
+    val uri = s"${conf.host}/lists/${conf.listId}/members/$emailMd5"
+    val userBody = Json.obj(
+      "email_address" -> email,
+      "status" -> "subscribed",
+      "merge_fields" -> Json.obj(
+        "FNAME" -> firstName,
+        "LNAME" -> lastName,
+      )
+    )
+    rpc(uri).silent.withBasicAuth(conf.user, conf.password).put(userBody)
+  }
+
+  private def tagUser(emailMd5: String, tag: String): Fox[WSResponse] = {
     val uri = s"${conf.host}/lists/${conf.listId}/members/$emailMd5/tags"
     val tagBody = Json.obj(
       "tags" -> List(Json.obj("name" -> tag, "status" -> "active"))
     )
-    for {
-      _ <- rpc(uri).silent.withBasicAuth(conf.user, conf.password).post(tagBody)
-    } yield ()
+    rpc(uri).silent.withBasicAuth(conf.user, conf.password).post(tagBody)
   }
 
 }

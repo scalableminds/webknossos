e45578f74d4edd955417dd5ad9a3ab7cbcabdedb
Philipp Otto <philipp.4096@googlemail.com>
2021-05-27 16:59:07 +0200

    add tests for processTaskWithPool

--- frontend/javascripts/oxalis/view/right-menu/meshes_view_helper.js
+++ frontend/javascripts/oxalis/view/right-menu/meshes_view_helper.js
@@ -19,6 +19,7 @@ import {
 import type { Vector3 } from "oxalis/constants";
 import Toast from "libs/toast";
 import messages from "messages";
+import processTaskWithPool from "libs/task_pool";
 
 const PARALLEL_MESH_LOADING_COUNT = 6;
 
@@ -99,36 +100,4 @@ export async function loadMeshFromFile(
   await processTaskWithPool(tasks, PARALLEL_MESH_LOADING_COUNT);
 
   Store.dispatch(finishedLoadingIsosurfaceAction(id));
-}
-
-function processTaskWithPool(tasks, poolSize) {
-  return new Promise((resolve, reject) => {
-    const promises = [];
-    let isFinalResolveScheduled = false;
-
-    const startNextTask = () => {
-      if (tasks.length === 0) {
-        if (!isFinalResolveScheduled) {
-          isFinalResolveScheduled = true;
-
-          // All tasks were kicked off, which is why all promises can be
-          // awaited now together.
-          Promise.all(promises).then(resolve, reject);
-        }
-        return;
-      }
-
-      const task = tasks.shift();
-      const newPromise = task();
-      promises.push(newPromise);
-
-      // If that promise is done, process a new one (that way,
-      // the pool size stays constant until the queue is almost empty.)
-      newPromise.then(startNextTask);
-    };
-
-    for (let i = 0; i < poolSize; i++) {
-      startNextTask();
-    }
-  });
 }
---
+++ frontend/javascripts/test/libs/task_pool.spec.js
@@ -0,0 +1,98 @@
+// @noflow
+import processTaskWithPool from "libs/task_pool";
+import * as Utils from "libs/utils";
+import test from "ava";
+
+test("processTaskWithPool should run a simple task", async t => {
+  t.plan(1);
+
+  const protocol = [];
+
+  const tasks = [
+    async () => {
+      await Utils.sleep(1);
+      protocol.push(1);
+    },
+  ];
+
+  await processTaskWithPool(tasks, 1);
+
+  t.is(protocol.length, 1);
+});
+
+test("processTaskWithPool should run tasks sequentially", async t => {
+  t.plan(1);
+
+  const protocol = [];
+
+  const tasks = [
+    async () => {
+      await Utils.sleep(1);
+      protocol.push(1);
+    },
+    async () => {
+      await Utils.sleep(1);
+      protocol.push(2);
+    },
+    async () => {
+      await Utils.sleep(1);
+      protocol.push(3);
+    },
+  ];
+
+  await processTaskWithPool(tasks, 1);
+
+  t.deepEqual(protocol, [1, 2, 3]);
+});
+
+test("processTaskWithPool should run tasks in a sliding window manner", async t => {
+  t.plan(1);
+
+  const protocol = [];
+
+  const tasks = [
+    async () => {
+      await Utils.sleep(0.1);
+      protocol.push(2);
+    },
+    async () => {
+      protocol.push(1);
+      await Utils.sleep(5);
+      protocol.push(4);
+    },
+    async () => {
+      await Utils.sleep(1);
+      protocol.push(3);
+    },
+  ];
+
+  await processTaskWithPool(tasks, 2);
+
+  t.deepEqual(protocol, [1, 2, 3, 4]);
+});
+
+test("processTaskWithPool should cope with too large pool size", async t => {
+  t.plan(1);
+
+  const protocol = [];
+
+  const tasks = [
+    async () => {
+      await Utils.sleep(0.1);
+      protocol.push(2);
+    },
+    async () => {
+      protocol.push(1);
+      await Utils.sleep(5);
+      protocol.push(4);
+    },
+    async () => {
+      await Utils.sleep(1);
+      protocol.push(3);
+    },
+  ];
+
+  await processTaskWithPool(tasks, 10);
+
+  t.deepEqual(protocol, [1, 2, 3, 4]);
+});

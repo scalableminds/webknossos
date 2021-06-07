4c0c3e018ddb4c985f7c1a37e8ecaa8a56950d21
Florian M <florian@scm.io>
2021-06-07 09:05:25 +0200

    In evolution, set old failures to manual failure

--- conf/evolutions/072-jobs-manually-repaired.sql
+++ conf/evolutions/072-jobs-manually-repaired.sql
@@ -1,4 +1,4 @@
--- https://github.com/scalableminds/webknossos/pull/TODO
+-- https://github.com/scalableminds/webknossos/pull/5530
 
 START TRANSACTION;
 
@@ -7,6 +7,8 @@ DROP VIEW webknossos.jobs_;
 CREATE TYPE webknossos.JOB_MANUAL_STATE AS ENUM ('SUCCESS', 'FAILURE');
 
 ALTER TABLE webknossos.jobs ADD COLUMN manualState webknossos.JOB_MANUAL_STATE;
+
+UPDATE webknossos.jobs SET manualstate = 'FAILURE' WHERE celeryinfo->'state' = '"FAILURE"'::jsonb;
 
 CREATE VIEW webknossos.jobs_ AS SELECT * FROM webknossos.jobs WHERE NOT isDeleted;
 

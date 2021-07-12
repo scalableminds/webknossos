b741ea55be0f56d4a8d562f002cc5429f8760dcc
Florian M <fm3@users.noreply.github.com>
2021-07-12 13:33:56 +0200

    Add default analytics uri (#5607)
    * [WIP] add default analytics uri
    
    * Enter relayer URI to default config
    
    * Changelog

--- app/models/analytics/AnalyticsService.scala
+++ app/models/analytics/AnalyticsService.scala
@@ -32,9 +32,9 @@ class AnalyticsService @Inject()(rpc: RPC,
   }
 
   private def send(analyticsEventJson: JsObject): Fox[Unit] = {
-    if (wkConf.BackendAnalytics.uri == "" || wkConf.BackendAnalytics.key == "") {
+    if (wkConf.BackendAnalytics.uri == "") {
       if (wkConf.BackendAnalytics.verboseLoggingEnabled) {
-        logger.info(s"Not sending analytics event, since uri/key is not configured. Event was: $analyticsEventJson")
+        logger.info(s"Not sending analytics event, since uri is not configured. Event was: $analyticsEventJson")
       }
     } else {
       if (wkConf.BackendAnalytics.verboseLoggingEnabled) {

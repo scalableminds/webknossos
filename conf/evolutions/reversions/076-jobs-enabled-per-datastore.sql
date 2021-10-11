94c70122d4048ac0fde36e6a4635e0b148812cfa
Florian M <florian@scm.io>
2021-10-11 13:21:13 +0200

    Add sql evolutions

---
+++ conf/evolutions/reversions/076-jobs-enabled-per-datastore.sql
@@ -0,0 +1,11 @@
+START TRANSACTION;
+
+DROP VIEW webknossos.dataStores_;
+
+ALTER TABLE webknossos.dataStores DROP COLUMN jobsEnabled;
+
+CREATE VIEW webknossos.dataStores_ AS SELECT * FROM webknossos.dataStores WHERE NOT isDeleted;
+
+UPDATE webknossos.releaseInformation SET schemaVersion = 76;
+
+COMMIT TRANSACTION;

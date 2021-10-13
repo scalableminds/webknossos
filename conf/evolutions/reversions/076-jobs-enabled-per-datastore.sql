5a9c81ec8415fb5d6dba1f045dcd564c6e5b4192
Florian M <fm3@users.noreply.github.com>
2021-10-13 11:51:32 +0200

    Update conf/evolutions/reversions/076-jobs-enabled-per-datastore.sql
    Co-authored-by: Youri K <youri-k@users.noreply.github.com>

--- conf/evolutions/reversions/076-jobs-enabled-per-datastore.sql
+++ conf/evolutions/reversions/076-jobs-enabled-per-datastore.sql
@@ -6,6 +6,6 @@ ALTER TABLE webknossos.dataStores DROP COLUMN jobsEnabled;
 
 CREATE VIEW webknossos.dataStores_ AS SELECT * FROM webknossos.dataStores WHERE NOT isDeleted;
 
-UPDATE webknossos.releaseInformation SET schemaVersion = 76;
+UPDATE webknossos.releaseInformation SET schemaVersion = 75;
 
 COMMIT TRANSACTION;

c3e83bf21966de6aa2ee6eb1dada80f2efc8b5eb
Florian M <fm3@users.noreply.github.com>
2021-10-13 11:51:45 +0200

    Update tools/postgres/toggle_jobs.sh
    Co-authored-by: Youri K <youri-k@users.noreply.github.com>

--- tools/postgres/toggle_jobs.sh
+++ tools/postgres/toggle_jobs.sh
@@ -6,4 +6,4 @@ scriptdir="$(dirname "$0")"
 dbName="$("$scriptdir"/db_name.sh)"
 dbHost="$("$scriptdir"/db_host.sh)"
 
-PGPASSWORD=postgres psql -U postgres -h $dbHost --dbname=$dbName -c "update webknossos.datastores set jobsEnabled = $1 where name = 'localhost'"
+PGPASSWORD=postgres psql -U postgres -h "$dbHost" --dbname="$dbName" -c "update webknossos.datastores set jobsEnabled = $1 where name = 'localhost'"

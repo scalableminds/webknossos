bb9dc0993ba42f594fe95cd867386b8e7a351526
Philipp Otto <philipp.4096@googlemail.com>
2021-11-03 13:10:41 +0100

    fix collapse/expand sidebars on touchscreens

--- frontend/javascripts/oxalis/view/components/border_toggle_button.js
+++ frontend/javascripts/oxalis/view/components/border_toggle_button.js
@@ -3,6 +3,7 @@ import * as React from "react";
 import { Button, Tooltip } from "antd";
 import { connect } from "react-redux";
 import type { OxalisState, BorderOpenStatus } from "oxalis/store";
+import { V2 } from "libs/mjs";
 
 type OwnProps = {|
   onClick: () => void,
@@ -13,6 +14,8 @@ type StateProps = {|
   borderOpenStatus: BorderOpenStatus,
 |};
 type Props = {| ...OwnProps, ...StateProps |};
+
+const DRAG_THRESHOLD = 5;
 
 function BorderToggleButton(props: Props) {
   const { onClick, side, borderOpenStatus, inFooter } = props;
@@ -27,20 +30,44 @@ function BorderToggleButton(props: Props) {
   const imageClass = `center-item-using-flex icon-sidebar-toggle icon-sidebar-${iconKind}-${side}-${
     inFooter ? "dark" : "bright"
   }`;
+  const [lastTouchPosition, setLastTouchPosition] = React.useState([0, 0]);
 
   return (
     <Tooltip title={tooltipTitle} placement={placement}>
       <Button
         className={className}
+        size="small"
+        /*
+        Normally, a simple onClick handler would be enough for this button
+        to support both desktops as mobile devices with touchscreens.
+        However, since the button is placed in the FlexLayout's panels,
+        its presence interferes with the existing mouse drag / touch drag
+        behavior to move tabs around.
+        For this reason, we have to call stopPropagation and preventDefault.
+        Additionally, we need to detect whether the user has dragged a tap
+        across screen (to move a tab). In that case, onTouchEnd does nothing.
+        */
         onClick={event => {
           if (event != null) {
             event.target.blur();
           }
           onClick();
         }}
-        size="small"
-        onMouseDown={evt => evt.stopPropagation()}
-        onTouchStart={evt => evt.preventDefault()}
+        onMouseDown={evt => {
+          evt.stopPropagation();
+        }}
+        onTouchStart={evt => {
+          evt.preventDefault();
+          setLastTouchPosition([evt.touches[0].pageX, evt.touches[0].pageY]);
+        }}
+        onTouchEnd={evt => {
+          const currentTouchPos = [evt.changedTouches[0].pageX, evt.changedTouches[0].pageY];
+
+          const delta = V2.length(V2.sub(lastTouchPosition, currentTouchPos));
+          if (delta < DRAG_THRESHOLD) {
+            onClick();
+          }
+        }}
       >
         <div className={imageClass} />
       </Button>

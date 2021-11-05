57b42a4b2db088d3fba5b573094edaee807a931a
Florian M <florian@scm.io>
2021-11-05 08:48:58 +0100

    update schema version

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -21,7 +21,7 @@ START TRANSACTION;
 CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(76);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(77);
 COMMIT TRANSACTION;
 
 

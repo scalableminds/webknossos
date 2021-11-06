02496cdf4e674094a25edcd56439005492e0f9f8
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2021-11-06 21:53:02 +0100

    add tests to test annotation tool deselection

--- frontend/javascripts/test/reducers/volumetracing_reducer.spec.js
+++ frontend/javascripts/test/reducers/volumetracing_reducer.spec.js
@@ -2,7 +2,7 @@
 import update from "immutability-helper";
 
 import { getVolumeTracingOrFail } from "test/helpers/apiHelpers";
-import Constants, { AnnotationToolEnum } from "oxalis/constants";
+import { AnnotationToolEnum } from "oxalis/constants";
 import { getRequestLogZoomStep } from "oxalis/model/accessors/flycam_accessor";
 import * as VolumeTracingActions from "oxalis/model/actions/volumetracing_actions";
 import * as UiActions from "oxalis/model/actions/ui_actions";
@@ -10,91 +10,9 @@ import VolumeTracingReducer from "oxalis/model/reducers/volumetracing_reducer";
 import UiReducer from "oxalis/model/reducers/ui_reducer";
 import mockRequire from "mock-require";
 import test from "ava";
-import defaultState from "oxalis/default_state";
+import { initialState } from "test/fixtures/volumetracing_object";
 
 mockRequire("app", { currentUser: { firstName: "SCM", lastName: "Boy" } });
-
-const volumeTracing = {
-  type: "volume",
-  activeCellId: 0,
-  activeTool: AnnotationToolEnum.MOVE,
-  maxCellId: 0,
-  contourList: [],
-  lastCentroid: null,
-};
-
-const notEmptyViewportRect = {
-  top: 0,
-  left: 0,
-  width: Constants.VIEWPORT_WIDTH,
-  height: Constants.VIEWPORT_WIDTH,
-};
-
-const initialState = update(defaultState, {
-  tracing: {
-    annotationType: { $set: "Explorational" },
-    name: { $set: "" },
-    restrictions: {
-      $set: {
-        branchPointsAllowed: true,
-        allowUpdate: true,
-        allowFinish: true,
-        allowAccess: true,
-        allowDownload: true,
-        resolutionRestrictions: { min: null, max: null },
-      },
-    },
-    volume: { $set: volumeTracing },
-  },
-  dataset: {
-    dataSource: {
-      dataLayers: {
-        $set: [
-          {
-            // We need to have some resolutions. Otherwise,
-            // getRequestLogZoomStep will always return 0
-            resolutions: [[1, 1, 1], [2, 2, 2], [4, 4, 4]],
-            category: "segmentation",
-            name: "segmentation",
-            isTracingLayer: true,
-          },
-        ],
-      },
-    },
-  },
-  datasetConfiguration: {
-    layers: {
-      segmentation: {
-        $set: {
-          color: [0, 0, 0],
-          alpha: 100,
-          intensityRange: [0, 255],
-          isDisabled: false,
-          isInverted: false,
-          isInEditMode: false,
-        },
-      },
-    },
-  },
-  // To get a valid calculated current zoomstep, the viewport rects are required not to be empty.
-  viewModeData: {
-    plane: {
-      inputCatcherRects: {
-        $set: {
-          PLANE_XY: notEmptyViewportRect,
-          PLANE_YZ: notEmptyViewportRect,
-          PLANE_XZ: notEmptyViewportRect,
-          TDView: notEmptyViewportRect,
-        },
-      },
-    },
-    arbitrary: {
-      $set: {
-        inputCatcherRect: notEmptyViewportRect,
-      },
-    },
-  },
-});
 
 test("VolumeTracing should set a new active cell", t => {
   const createCellAction = VolumeTracingActions.createCellAction();
---
+++ frontend/javascripts/test/sagas/annotation_tool_saga.spec.js
@@ -0,0 +1,116 @@
+// @flow
+
+import test from "ava";
+import { AnnotationToolEnum } from "oxalis/constants";
+
+import mockRequire from "mock-require";
+import { initialState } from "test/fixtures/volumetracing_object";
+import sinon from "sinon";
+
+const disabledInfoMock: { [any]: any } = {};
+Object.values(AnnotationToolEnum).forEach(annotationTool => {
+  disabledInfoMock[annotationTool] = { isDisabled: false, explanation: "" };
+});
+mockRequire("oxalis/model/accessors/tool_accessor", {
+  getDisabledInfoForTools: () => disabledInfoMock,
+});
+const {
+  MoveTool,
+  SkeletonTool,
+  BoundingBoxTool,
+  DrawTool,
+  EraseTool,
+  FillCellTool,
+  PickCellTool,
+} = mockRequire.reRequire("oxalis/controller/combinations/tool_controls");
+const UiReducer = mockRequire.reRequire("oxalis/model/reducers/ui_reducer").default;
+const { wkReadyAction } = mockRequire.reRequire("oxalis/model/actions/actions");
+const { cycleToolAction, setToolAction } = mockRequire.reRequire("oxalis/model/actions/ui_actions");
+
+const { watchToolDeselection } = mockRequire.reRequire("oxalis/model/sagas/annotation_tool_saga");
+
+const allTools = [
+  MoveTool,
+  SkeletonTool,
+  BoundingBoxTool,
+  DrawTool,
+  EraseTool,
+  FillCellTool,
+  PickCellTool,
+];
+
+const spies = allTools.map(tool => sinon.spy(tool, "onToolDeselected"));
+test.beforeEach(() => {
+  spies.forEach(spy => spy.resetHistory());
+});
+
+test.serial(
+  "Cycling through the annotation tools should trigger a deselection of the previous tool.",
+  t => {
+    let newState = initialState;
+    const saga = watchToolDeselection();
+    saga.next();
+    saga.next(wkReadyAction());
+    const cycleTool = () => {
+      const action = cycleToolAction(newState.uiInformation.activeTool);
+      newState = UiReducer(newState, action);
+      saga.next(action);
+      saga.next(newState);
+    };
+
+    cycleTool();
+    t.true(MoveTool.onToolDeselected.calledOnce);
+    cycleTool();
+    t.true(SkeletonTool.onToolDeselected.calledOnce);
+    cycleTool();
+    t.true(DrawTool.onToolDeselected.calledOnce);
+    cycleTool();
+    t.true(EraseTool.onToolDeselected.calledOnce);
+    cycleTool();
+    t.true(DrawTool.onToolDeselected.calledTwice);
+    cycleTool();
+    t.true(EraseTool.onToolDeselected.calledTwice);
+    cycleTool();
+    t.true(FillCellTool.onToolDeselected.calledOnce);
+    cycleTool();
+    t.true(PickCellTool.onToolDeselected.calledOnce);
+    cycleTool();
+    t.true(BoundingBoxTool.onToolDeselected.calledOnce);
+    cycleTool();
+    t.true(MoveTool.onToolDeselected.calledTwice);
+  },
+);
+
+test.serial("Selecting another tool should trigger a deselection of the previous tool.", t => {
+  let newState = initialState;
+  const saga = watchToolDeselection();
+  saga.next();
+  saga.next(wkReadyAction());
+  const cycleTool = nextTool => {
+    const action = setToolAction(nextTool, newState.uiInformation.activeTool);
+    newState = UiReducer(newState, action);
+    saga.next(action);
+    saga.next(newState);
+  };
+
+  cycleTool(AnnotationToolEnum.SKELETON);
+  t.true(MoveTool.onToolDeselected.calledOnce);
+  cycleTool(AnnotationToolEnum.BRUSH);
+  t.true(SkeletonTool.onToolDeselected.calledOnce);
+  cycleTool(AnnotationToolEnum.ERASE_BRUSH);
+  t.true(DrawTool.onToolDeselected.calledOnce);
+  cycleTool(AnnotationToolEnum.TRACE);
+  t.true(EraseTool.onToolDeselected.calledOnce);
+  cycleTool(AnnotationToolEnum.ERASE_TRACE);
+  t.true(DrawTool.onToolDeselected.calledTwice);
+  cycleTool(AnnotationToolEnum.FILL_CELL);
+  t.true(EraseTool.onToolDeselected.calledTwice);
+  cycleTool(AnnotationToolEnum.PICK_CELL);
+  t.true(FillCellTool.onToolDeselected.calledOnce);
+  cycleTool(AnnotationToolEnum.BOUNDING_BOX);
+  t.true(PickCellTool.onToolDeselected.calledOnce);
+  cycleTool(AnnotationToolEnum.MOVE);
+  t.true(BoundingBoxTool.onToolDeselected.calledOnce);
+  cycleTool(AnnotationToolEnum.SKELETON);
+  t.true(MoveTool.onToolDeselected.calledTwice);
+});

4c91a0a5a5bf9bde138b1fe87af085c9231c9654
Florian M <florian@scm.io>
2021-11-18 08:20:15 +0100

    add worker to db on yarn enable-jobs

--- tools/postgres/toggle_jobs.sh
+++ tools/postgres/toggle_jobs.sh
@@ -6,4 +6,10 @@ scriptdir="$(dirname "$0")"
 dbName="$("$scriptdir"/db_name.sh)"
 dbHost="$("$scriptdir"/db_host.sh)"
 
-PGPASSWORD=postgres psql -U postgres -h "$dbHost" --dbname="$dbName" -c "update webknossos.datastores set jobsEnabled = $1 where name = 'localhost'"
+PGPASSWORD=postgres psql -U postgres -h "$dbHost" --dbname="$dbName" -c "update webknossos.datastores set jobsEnabled = $1 where name = 'localhost'";
+
+if [[ $1 == "true" ]]; then
+  PGPASSWORD=postgres psql -U postgres -h "$dbHost" --dbname="$dbName" -c "insert into webknossos.workers(_id, _dataStore, key) values('6194dc03040200b0027f28a1', 'localhost', 'secretWorkerKey') on conflict do nothing"
+else
+  PGPASSWORD=postgres psql -U postgres -h "$dbHost" --dbname="$dbName" -c "delete from webknossos.workers where _id = '6194dc03040200b0027f28a1'"
+fi

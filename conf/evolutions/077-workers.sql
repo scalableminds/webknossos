66080b296f07e28f2fcba13d6a712dd8b84e0ba7
Florian M <florian@scm.io>
2021-11-18 14:25:35 +0100

    [WIP] more on evolution

--- conf/evolutions/077-workers.sql
+++ conf/evolutions/077-workers.sql
@@ -1,3 +1,6 @@
+-- Note: If your setup contains a webknossos-worker, make sure to change the key below to a secure secret
+-- This evolution assumes that at most one datastore has worker jobs enabled
+
 START TRANSACTION;
 
 DROP VIEW webknossos.jobs_;
@@ -14,22 +17,56 @@ CREATE TABLE webknossos.workers(
 
 CREATE TYPE webknossos.JOB_STATE AS ENUM ('PENDING', 'STARTED', 'SUCCESS', 'FAILURE');
 
-ALTER TABLE webknossos.jobs ADD COLUMN _dataStore CHAR(256) NOT NULL;
-ALTER TABLE webknossos.jobs ADD COLUMN state webknossos.JOB_STATE NOT NULL DEFAULT 'PENDING';
-ALTER TABLE webknossos.jobs ADD COLUMN manualState webknossos.JOB_STATE;
+ALTER TABLE webknossos.jobs ADD COLUMN _dataStore CHAR(256);
+ALTER TABLE webknossos.jobs ADD COLUMN state webknossos.JOB_STATE DEFAULT 'PENDING';
+ALTER TABLE webknossos.jobs ADD COLUMN manualState_NEW webknossos.JOB_STATE;
 ALTER TABLE webknossos.jobs ADD COLUMN _worker CHAR(24);
-ALTER TABLE webknossos.jobs ADD COLUMN latestRunId VARCAR(1024);
-ALTER TABLE webknossos.jobs ADD COLUMN returnValue Text;
+ALTER TABLE webknossos.jobs ADD COLUMN latestRunId VARCHAR(1024);
+ALTER TABLE webknossos.jobs ADD COLUMN returnValue TEXT;
 ALTER TABLE webknossos.jobs ADD COLUMN started TIMESTAMPTZ;
 ALTER TABLE webknossos.jobs ADD COLUMN ended TIMESTAMPTZ;
 
 -- values
 
+insert into webknossos.workers(_id, _dataStore, key)
+select
+    '6149aa5e01000089115af93d', d.name, 'temporaryWorkerKeyCHANGEME'
+from
+    webknossos.dataStores d where jobsEnabled;
+
+update webknossos.jobs set _dataStore = d.name
+   from webknossos.dataStores d where jobsEnabled;
+
+update webknossos.jobs set manualState_NEW = manualState::text::webknossos.job_state;
+
+update webknossos.jobs set _worker = '6149aa5e01000089115af93d';
+
+update webknossos.jobs set latestRunId = celeryJobId;
+
+update webknossos.jobs
+set returnValue = (celeryInfo->>'result')::TEXT
+where celeryInfo ? 'result' and celeryInfo ->> 'result' != 'None';
+
+-- FAILURE should be the state of all that do not have a valid celery state. Differs from new default, hence this set.
+update webknossos.jobs
+set state = 'FAILURE';
+
+update webknossos.jobs
+set state = (celeryInfo->>'state')::TEXT::webknossos.job_state;
+where celeryInfo ? 'state' and celeryInfo ->> 'state' != 'None';
+
+-- values end
+
+-- todo make _datastore + state not null
+-- todo foreign keys
+
+
 ALTER TABLE webknossos.jobs DROP COLUMN manualState;
+ALTER TABLE webknossos.jobs RENAME COLUMN manualState_NEW TO manualState;
 ALTER TABLE webknossos.jobs DROP COLUMN celeryInfo;
 ALTER TABLE webknossos.jobs DROP COLUMN celeryJobId;
 
-DROP TYPE webknossos.JOB_MANUAL_STATE AS ENUM ('SUCCESS', 'FAILURE');
+DROP TYPE webknossos.JOB_MANUAL_STATE;
 
 CREATE VIEW webknossos.jobs_ AS SELECT * FROM webknossos.jobs WHERE NOT isDeleted;
 CREATE VIEW webknossos.workers_ AS SELECT * FROM webknossos.workers WHERE NOT isDeleted;

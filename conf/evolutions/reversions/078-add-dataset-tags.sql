581241d7b8e1a54f6233cc88228056324d17ecfb
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2021-11-29 18:55:36 +0100

    adjust revision of dataset migration

--- conf/evolutions/reversions/078-add-dataset-tags.sql
+++ conf/evolutions/reversions/078-add-dataset-tags.sql
@@ -6,6 +6,6 @@ ALTER TABLE webknossos.dataSets DROP COLUMN tags;
 
 CREATE VIEW webknossos.dataSets_ AS SELECT * FROM webknossos.dataSets WHERE NOT isDeleted;
 
-UPDATE webknossos.releaseInformation SET schemaVersion = 76;
+UPDATE webknossos.releaseInformation SET schemaVersion = 77;
 
 COMMIT TRANSACTION;

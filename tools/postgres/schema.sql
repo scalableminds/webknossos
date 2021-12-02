9eab00e7d7e84b080b003365ab37893a0a7c82b6
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2021-12-02 18:00:58 +0100

    update version in schema.sql manually

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -21,7 +21,7 @@ START TRANSACTION;
 CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(78);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(79);
 COMMIT TRANSACTION;
 
 

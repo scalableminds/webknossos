868d15a5e4797c181539ca732a8299422b450696
Florian M <florian@scm.io>
2022-01-17 11:40:21 +0100

    fix sql comment syntax

--- conf/evolutions/080-job-add-cancelled.sql
+++ conf/evolutions/080-job-add-cancelled.sql
@@ -1,4 +1,4 @@
-# unfortunately, ALTER TYPE ... ADD cannot run inside a transaction block. It can also not be reverted
+-- unfortunately, ALTER TYPE ... ADD cannot run inside a transaction block. It can also not be reverted
 
 ALTER TYPE webknossos.JOB_STATE ADD VALUE 'CANCELLED';
 

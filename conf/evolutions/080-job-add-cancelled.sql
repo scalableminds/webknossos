ce41c000d5973da4c43a6f3d8e1a0f121b64aa15
Florian M <florian@scm.io>
2022-01-17 11:32:46 +0100

    not inside transaction block

--- conf/evolutions/080-job-add-cancelled.sql
+++ conf/evolutions/080-job-add-cancelled.sql
@@ -1,7 +1,5 @@
-START TRANSACTION;
+# unfortunately, ALTER TYPE ... ADD cannot run inside a transaction block. It can also not be reverted
 
-ALTER TYPE webknossos.JOB_STATE ADD VALUE 'CANCELELD';
+ALTER TYPE webknossos.JOB_STATE ADD VALUE 'CANCELLED';
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 80;
-
-COMMIT TRANSACTION;

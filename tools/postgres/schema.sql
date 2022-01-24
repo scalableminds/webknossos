a640224520794e4e680ba338b8d708d5dae5ca71
Florian M <florian@scm.io>
2022-01-24 09:06:19 +0100

    fix schema version after merge

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -21,7 +21,7 @@ START TRANSACTION;
 CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(80);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(81);
 COMMIT TRANSACTION;
 
 

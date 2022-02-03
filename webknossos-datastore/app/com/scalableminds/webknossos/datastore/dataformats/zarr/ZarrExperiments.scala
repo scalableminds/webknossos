c6d4438d1c9d107ee8a1d846eb327f71dd6f2085
Florian M <florian@scm.io>
2022-02-03 11:06:09 +0100

    format

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/dataformats/zarr/ZarrExperiments.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/dataformats/zarr/ZarrExperiments.scala
@@ -2,11 +2,6 @@ package com.scalableminds.webknossos.datastore.dataformats.zarr
 
 import com.bc.zarr.{ZarrArray, ArrayParams, DataType}
 
-
 class ZarrExperiments {
-  ZarrArray.create(new ArrayParams()
-    .shape(10000, 10000)
-    .chunks(1000, 1000)
-    .dataType(DataType.i4)
-  );
+  ZarrArray.create(new ArrayParams().shape(10000, 10000).chunks(1000, 1000).dataType(DataType.i4));
 }

d7537c5bd288dea92bd53e3f8a7c48b38443b6fe
Florian M <fm3@users.noreply.github.com>
2022-02-07 10:04:35 +0100

    More api swagger annotations (#5989)
    * [WIP] more api swagger annotations
    
    * Add swagger annotations for more routes, include project id in task public writes
    
    * format
    
    * change param type from form to body for now, fix nicknames
    
    * add some more swagger annotations
    
    * better aliases
    
    * add nickname

--- app/controllers/ProjectController.scala
+++ app/controllers/ProjectController.scala
@@ -4,6 +4,7 @@ import com.mohiva.play.silhouette.api.actions.SecuredRequest
 import com.scalableminds.util.accesscontext.GlobalAccessContext
 import com.scalableminds.util.tools.DefaultConverters.BoolToOption
 import com.scalableminds.util.tools.{Fox, FoxImplicits}
+import io.swagger.annotations._
 import javax.inject.Inject
 import models.annotation.{AnnotationDAO, AnnotationService, AnnotationType}
 import models.project._
@@ -11,12 +12,13 @@ import models.task._
 import models.user.UserService
 import oxalis.security.WkEnv
 import play.api.i18n.Messages
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{JsObject, JsValue, Json}
 import play.api.mvc.{Action, AnyContent}
 import utils.ObjectId
 
 import scala.concurrent.ExecutionContext
 
+@Api
 class ProjectController @Inject()(projectService: ProjectService,
                                   projectDAO: ProjectDAO,
                                   annotationService: AnnotationService,
@@ -29,6 +31,7 @@ class ProjectController @Inject()(projectService: ProjectService,
     extends Controller
     with FoxImplicits {
 
+  @ApiOperation(hidden = true, value = "")
   def list: Action[AnyContent] = sil.SecuredAction.async { implicit request =>
     for {
       projects <- projectDAO.findAll ?~> "project.list.failed"
@@ -36,6 +39,7 @@ class ProjectController @Inject()(projectService: ProjectService,
     } yield Ok(Json.toJson(js))
   }
 
+  @ApiOperation(hidden = true, value = "")
   def listWithStatus: Action[AnyContent] = sil.SecuredAction.async { implicit request =>
     for {
       projects <- projectDAO.findAll ?~> "project.list.failed"
@@ -49,14 +53,32 @@ class ProjectController @Inject()(projectService: ProjectService,
     } yield Ok(Json.toJson(js))
   }
 
-  def read(id: String): Action[AnyContent] = sil.SecuredAction.async { implicit request =>
-    for {
-      projectIdValidated <- ObjectId.parse(id)
-      project <- projectDAO.findOne(projectIdValidated) ?~> "project.notFound" ~> NOT_FOUND
-      js <- projectService.publicWrites(project)
-    } yield Ok(js)
+  @ApiOperation(value = "Information about a project selected by id", nickname = "projectInfoById")
+  @ApiResponses(
+    Array(new ApiResponse(code = 200, message = "JSON object containing information about this project."),
+          new ApiResponse(code = 400, message = badRequestLabel)))
+  def read(@ApiParam(value = "The id of the project") id: String): Action[AnyContent] = sil.SecuredAction.async {
+    implicit request =>
+      for {
+        projectIdValidated <- ObjectId.parse(id)
+        project <- projectDAO.findOne(projectIdValidated) ?~> "project.notFound" ~> NOT_FOUND
+        js <- projectService.publicWrites(project)
+      } yield Ok(js)
   }
 
+  @ApiOperation(value = "Information about a project selected by name", nickname = "projectInfoByName")
+  @ApiResponses(
+    Array(new ApiResponse(code = 200, message = "JSON object containing information about this project."),
+          new ApiResponse(code = 400, message = badRequestLabel)))
+  def readByName(@ApiParam(value = "The name of the project") name: String): Action[AnyContent] =
+    sil.SecuredAction.async { implicit request =>
+      for {
+        project <- projectDAO.findOneByNameAndOrganization(name, request.identity._organization) ?~> "project.notFound" ~> NOT_FOUND
+        js <- projectService.publicWrites(project)
+      } yield Ok(js)
+    }
+
+  @ApiOperation(hidden = true, value = "")
   def delete(id: String): Action[AnyContent] = sil.SecuredAction.async { implicit request =>
     for {
       projectIdValidated <- ObjectId.parse(id)
@@ -66,6 +88,27 @@ class ProjectController @Inject()(projectService: ProjectService,
     } yield JsonOk(Messages("project.remove.success"))
   }
 
+  @ApiOperation(
+    value =
+      """Create a new Project.
+Expects:
+ - As JSON object body with keys:
+  - name (string) name of the new project
+  - team (string) id of the team this project is for
+  - priority (int) priority of the project’s tasks
+  - paused (bool, default=False) if the project should be paused at time of creation (its tasks are not distributed)
+  - expectedTime (int, optional) time limit
+  - owner (string) id of a user
+  - isBlacklistedFromReport (boolean) if true, the project is skipped on the progress report tables
+""",
+    nickname = "createProject"
+  )
+  @ApiImplicitParams(
+    Array(
+      new ApiImplicitParam(name = "ProjectParameters",
+                           required = true,
+                           dataTypeClass = classOf[JsObject],
+                           paramType = "body")))
   def create: Action[JsValue] = sil.SecuredAction.async(parse.json) { implicit request =>
     withJsonBodyUsing(Project.projectPublicReads) { project =>
       for {
@@ -80,6 +123,7 @@ class ProjectController @Inject()(projectService: ProjectService,
     }
   }
 
+  @ApiOperation(hidden = true, value = "")
   def update(id: String): Action[JsValue] = sil.SecuredAction.async(parse.json) { implicit request =>
     withJsonBodyUsing(Project.projectPublicReads) { updateRequest =>
       for {
@@ -95,14 +139,17 @@ class ProjectController @Inject()(projectService: ProjectService,
     }
   }
 
+  @ApiOperation(hidden = true, value = "")
   def pause(id: String): Action[AnyContent] = sil.SecuredAction.async { implicit request =>
     updatePauseStatus(id, isPaused = true)
   }
 
+  @ApiOperation(hidden = true, value = "")
   def resume(id: String): Action[AnyContent] = sil.SecuredAction.async { implicit request =>
     updatePauseStatus(id, isPaused = false)
   }
 
+  @ApiOperation(hidden = true, value = "")
   private def updatePauseStatus(id: String, isPaused: Boolean)(implicit request: SecuredRequest[WkEnv, _]) =
     for {
       projectIdValidated <- ObjectId.parse(id)
@@ -113,6 +160,7 @@ class ProjectController @Inject()(projectService: ProjectService,
       js <- projectService.publicWrites(updatedProject)
     } yield Ok(js)
 
+  @ApiOperation(hidden = true, value = "")
   def projectsForTaskType(taskTypeId: String): Action[AnyContent] = sil.SecuredAction.async { implicit request =>
     for {
       taskTypeIdValidated <- ObjectId.parse(taskTypeId)
@@ -130,10 +178,20 @@ class ProjectController @Inject()(projectService: ProjectService,
     }
   }
 
-  def tasksForProject(id: String,
-                      limit: Option[Int] = None,
-                      pageNumber: Option[Int] = None,
-                      includeTotalCount: Option[Boolean]): Action[AnyContent] =
+  @ApiOperation(value = "Information about the tasks of a project selected by project id",
+                nickname = "taskInfosByProjectId")
+  @ApiResponses(
+    Array(
+      new ApiResponse(code = 200,
+                      message = "JSON list of objects containing information about the tasks of this project project."),
+      new ApiResponse(code = 400, message = badRequestLabel)
+    ))
+  def tasksForProject(
+      @ApiParam(value = "Id of the project") id: String,
+      @ApiParam(value = "Pagination: limit for the number of tasks to query") limit: Option[Int] = None,
+      @ApiParam(value = "Pagination: page number, skip the first tasks") pageNumber: Option[Int] = None,
+      @ApiParam(value = "Pagination: if true, include total count in response header as X-Total-Count") includeTotalCount: Option[
+        Boolean]): Action[AnyContent] =
     sil.SecuredAction.async { implicit request =>
       for {
         projectIdValidated <- ObjectId.parse(id)
@@ -152,6 +210,7 @@ class ProjectController @Inject()(projectService: ProjectService,
       }
     }
 
+  @ApiOperation(hidden = true, value = "")
   def incrementEachTasksInstances(id: String, delta: Option[Long]): Action[AnyContent] =
     sil.SecuredAction.async { implicit request =>
       for {
@@ -164,6 +223,7 @@ class ProjectController @Inject()(projectService: ProjectService,
       } yield Ok(js)
     }
 
+  @ApiOperation(hidden = true, value = "")
   def usersWithActiveTasks(id: String): Action[AnyContent] = sil.SecuredAction.async { implicit request =>
     for {
       projectIdValidated <- ObjectId.parse(id)
@@ -175,6 +235,7 @@ class ProjectController @Inject()(projectService: ProjectService,
     }
   }
 
+  @ApiOperation(hidden = true, value = "")
   def transferActiveTasks(id: String): Action[JsValue] = sil.SecuredAction.async(parse.json) { implicit request =>
     for {
       projectIdValidated <- ObjectId.parse(id)

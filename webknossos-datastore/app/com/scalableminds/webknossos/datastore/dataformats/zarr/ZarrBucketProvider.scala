2a85a8bebd16860c36a2ee8cdeb3bfae432cdcf3
Florian M <florian@scm.io>
2022-02-09 14:06:02 +0100

    convert to byte array

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/dataformats/zarr/ZarrBucketProvider.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/dataformats/zarr/ZarrBucketProvider.scala
@@ -1,35 +1,52 @@
 package com.scalableminds.webknossos.datastore.dataformats.zarr
 
+import java.nio.file.Paths
+import java.nio.{ByteBuffer, ByteOrder}
+
 import com.bc.zarr.ZarrArray
 import com.scalableminds.webknossos.datastore.dataformats.{BucketProvider, DataCube}
 import com.scalableminds.webknossos.datastore.models.BucketPosition
 import com.scalableminds.webknossos.datastore.models.requests.DataReadInstruction
 import com.typesafe.scalalogging.LazyLogging
-import net.liftweb.common.{Box, Empty}
+import net.liftweb.common.{Box, Empty, Full}
 import net.liftweb.util.Helpers.tryo
 
-class ZarrCube(zarrArray: ZarrArray) extends DataCube {
+class ZarrCube(zarrArray: ZarrArray) extends DataCube with LazyLogging {
 
   def cutOutBucket(bucket: BucketPosition): Box[Array[Byte]] = {
-    val offset = Array(0, 0, bucket.globalX, bucket.globalY, bucket.globalZ)
+    val offset = Array(0, 0, bucket.globalZ, bucket.globalY, bucket.globalX)
     val shape = Array(1, 1, bucket.bucketLength, bucket.bucketLength, bucket.bucketLength)
-    tryo(zarrArray.read(offset, shape).asInstanceOf[Array[Byte]])
+    Full(toByteArray(zarrArray.read(shape, offset).asInstanceOf[Array[Short]]))
+  }
+
+  private def toByteArray(dataShort: Array[Short]): Array[Byte] = {
+    val buffer = ByteBuffer.allocate(dataShort.length * 2).order(ByteOrder.LITTLE_ENDIAN)
+    buffer.asShortBuffer().put(dataShort)
+    buffer.array()
   }
 
   override protected def onFinalize(): Unit = ()
+
 }
 
 class ZarrBucketProvider(layer: ZarrLayer) extends BucketProvider with LazyLogging {
 
   override def loadFromUnderlying(readInstruction: DataReadInstruction): Box[ZarrCube] = {
-    val zarrFilePath = readInstruction.baseDir
-      .resolve(readInstruction.dataSource.id.team)
-      .resolve(readInstruction.dataSource.id.name)
-      .resolve(readInstruction.dataLayer.name)
-    logger.info(s"zarrFilePath: $zarrFilePath")
+    val useS3 = false
 
-    if (zarrFilePath.toFile.exists()) {
-      tryo(ZarrArray.open(zarrFilePath)).map(new ZarrCube(_))
+    val layerPath = if (useS3) {
+      s"https://.../${readInstruction.dataLayer.name}"
+    } else {
+      readInstruction.baseDir
+        .resolve(readInstruction.dataSource.id.team)
+        .resolve(readInstruction.dataSource.id.name)
+        .resolve(readInstruction.dataLayer.name)
+        .toString
+    }
+    logger.info(s"zarrFilePath: $layerPath")
+
+    if (useS3 || Paths.get(layerPath).toFile.exists()) {
+      tryo(ZarrArray.open(layerPath)).map(new ZarrCube(_))
     } else Empty
   }
 

cfa0d09a736f8c8b9c637f52eebfdde3a3a4629e
Philipp Otto <philipp.4096@googlemail.com>
2022-02-18 15:54:49 +0100

    fix mocked transfer

--- frontend/javascripts/oxalis/workers/comlink_wrapper.js
+++ frontend/javascripts/oxalis/workers/comlink_wrapper.js
@@ -17,7 +17,7 @@ function importComlink() {
       wrap: null,
       transferHandlers: new Map(),
       _expose: null,
-      _transfer: null,
+      _transfer: <P>(element: P, transferrables: Array<any>): P => element,
     };
   }
 }

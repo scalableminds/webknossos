ae1e590992ffa2c5f4b3d53e0534543eb01dfa9c
Philipp Otto <philipp.4096@googlemail.com>
2022-02-21 17:14:43 +0100

    fix error when viewing DS

--- frontend/javascripts/oxalis/model/accessors/volumetracing_accessor.js
+++ frontend/javascripts/oxalis/model/accessors/volumetracing_accessor.js
@@ -175,6 +175,9 @@ export function isVolumeAnnotationDisallowedForZoom(tool: AnnotationTool, state:
 const MAX_BRUSH_SIZE_FOR_MAG1 = 300;
 export function getMaximumBrushSize(state: OxalisState) {
   const volumeResolutions = getResolutionInfoOfActiveSegmentationTracingLayer(state);
+  if (volumeResolutions.resolutions.length === 0) {
+    return MAX_BRUSH_SIZE_FOR_MAG1;
+  }
   const lowestExistingResolutionIndex = volumeResolutions.getClosestExistingIndex(0);
 
   // For each leading magnification which does not exist,

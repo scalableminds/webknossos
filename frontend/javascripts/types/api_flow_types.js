ba87806342a7c93a4597b709d7635b4f20cc962f
Tom Herold <tom.herold@scalableminds.com>
2022-04-04 11:59:41 +0200

    Small updates to dataset list (#6131)
    * added filters for dataset access permissions
    
    * added filters for dataset layers
    
    * FIXED LINTING
    
    * fix flow
    
    * updated readme
    
    * updated changelog
    
    * Update CHANGELOG.unreleased.md
    
    Co-authored-by: Philipp Otto <philippotto@users.noreply.github.com>
    
    Co-authored-by: Philipp Otto <philipp.4096@googlemail.com>
    Co-authored-by: Philipp Otto <philippotto@users.noreply.github.com>

--- frontend/javascripts/types/api_flow_types.js
+++ frontend/javascripts/types/api_flow_types.js
@@ -72,22 +72,23 @@ export type APIHistogramData = Array<{
   max: number,
 }>;
 
-type MutableAPIDataSourceBase = {
+type MutableAPIDataSourceBase = {|
   id: {
     name: string,
     team: string,
   },
   status?: string,
-};
+|};
 
 type APIDataSourceBase = $ReadOnly<MutableAPIDataSourceBase>;
 
 type APIUnimportedDatasource = APIDataSourceBase;
 
-export type MutableAPIDataSource = MutableAPIDataSourceBase & {
+export type MutableAPIDataSource = {|
+  ...MutableAPIDataSourceBase,
   dataLayers: Array<APIDataLayer>,
   scale: Vector3,
-};
+|};
 
 export type APIDataSource = $ReadOnly<MutableAPIDataSource>;
 

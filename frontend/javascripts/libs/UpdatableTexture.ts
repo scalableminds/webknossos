24b48b279945095ef02ddce5c96170d02b32618e
Philipp Otto <philipp.4096@googlemail.com>
2022-04-12 15:54:24 +0200

    ignore UpdatableTexture

--- frontend/javascripts/libs/UpdatableTexture.ts
+++ frontend/javascripts/libs/UpdatableTexture.ts
@@ -1,4 +1,4 @@
-// @noflow
+// @ts-nocheck
 import * as THREE from "three";
 import { document } from "libs/window";
 
@@ -6,28 +6,18 @@ function UpdatableTexture(
   this: THREE.Texture,
   width: number,
   height: number,
-  // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'format' implicitly has an 'any' type.
   format,
-  // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'type' implicitly has an 'any' type.
   type,
-  // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'mapping' implicitly has an 'any' type.
   mapping,
-  // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'wrapS' implicitly has an 'any' type.
   wrapS,
-  // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'wrapT' implicitly has an 'any' type.
   wrapT,
-  // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'magFilter' implicitly has an 'any' type... Remove this comment to see the full error message
   magFilter,
-  // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'minFilter' implicitly has an 'any' type... Remove this comment to see the full error message
   minFilter,
-  // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'anisotropy' implicitly has an 'any' typ... Remove this comment to see the full error message
   anisotropy,
-  // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'encoding' implicitly has an 'any' type.
   encoding,
 ) {
   THREE.Texture.call(
     this,
-    // @ts-expect-error ts-migrate(2345) FIXME: Argument of type 'null' is not assignable to param... Remove this comment to see the full error message
     null,
     mapping,
     wrapS,
@@ -39,7 +29,6 @@ function UpdatableTexture(
     anisotropy,
     encoding,
   );
-  // @ts-expect-error ts-migrate(2339) FIXME: Property 'createElement' does not exist on type 'D... Remove this comment to see the full error message
   const canvas = document.createElement("canvas");
   canvas.width = width;
   canvas.height = height;
@@ -58,15 +47,12 @@ UpdatableTexture.prototype = Object.create(THREE.Texture.prototype);
 UpdatableTexture.prototype.constructor = UpdatableTexture;
 UpdatableTexture.prototype.isUpdatableTexture = true;
 
-// @ts-expect-error ts-migrate(7006) FIXME: Parameter 'renderer' implicitly has an 'any' type.
 UpdatableTexture.prototype.setRenderer = function setRenderer(renderer) {
   this.renderer = renderer;
   this.gl = this.renderer.getContext();
-  // @ts-expect-error ts-migrate(2339) FIXME: Property 'WebGLUtils' does not exist on type 'type... Remove this comment to see the full error message
   this.utils = THREE.WebGLUtils(this.gl, this.renderer.extensions, this.renderer.capabilities);
 };
 
-// @ts-expect-error ts-migrate(7006) FIXME: Parameter 'width' implicitly has an 'any' type.
 UpdatableTexture.prototype.setSize = function setSize(width, height) {
   if (width === this.width && height === this.height) return;
   if (!this.isInitialized()) return;
@@ -94,7 +80,6 @@ UpdatableTexture.prototype.isInitialized = function isInitialized() {
   return this.renderer.properties.get(this).__webglTexture != null;
 };
 
-// @ts-expect-error ts-migrate(7006) FIXME: Parameter 'src' implicitly has an 'any' type.
 UpdatableTexture.prototype.update = function update(src, x, y, width, height) {
   if (!this.isInitialized()) {
     this.renderer.initTexture(this);

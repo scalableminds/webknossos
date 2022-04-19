52996a0883a617cc147f5ca0ee9503ec424dda3b
Philipp Otto <philipp.4096@googlemail.com>
2022-04-19 16:39:49 +0200

    fix ts errors in context_menu.tsx

--- frontend/javascripts/oxalis/throttled_store.ts
+++ frontend/javascripts/oxalis/throttled_store.ts
@@ -1,5 +1,4 @@
 /* eslint no-await-in-loop: 0 */
-import type { Action } from "oxalis/model/actions/actions";
 import type { Store as StoreType } from "redux";
 import Deferred from "libs/deferred";
 import type { OxalisState } from "oxalis/store";

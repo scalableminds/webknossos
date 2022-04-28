ab41618d3b04c65260fdb27a221c9e4c334d641e
Philipp Otto <philipp.4096@googlemail.com>
2022-04-28 18:15:04 +0200

    update schema

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -21,7 +21,7 @@ START TRANSACTION;
 CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(81);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(82);
 COMMIT TRANSACTION;
 
 

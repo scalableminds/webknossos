0cf3a1f6c6d2ef10f0c5838a3ea77388480557ec
Arthur Hilbert <arthur.hilbert@t-online.de>
2022-06-01 17:44:01 +0200

    display warning when resolution gets too low during zooming

--- frontend/javascripts/oxalis/model/sagas/dataset_saga.ts
+++ frontend/javascripts/oxalis/model/sagas/dataset_saga.ts
@@ -1,10 +1,11 @@
-/* eslint-disable import/prefer-default-export */
 import type { Saga } from "oxalis/model/sagas/effect-generators";
 import { select } from "oxalis/model/sagas/effect-generators";
-import { takeEvery } from "typed-redux-saga";
+import { call, takeEvery } from "typed-redux-saga";
+import { sleep } from "libs/utils";
 import { getEnabledLayers } from "oxalis/model/accessors/dataset_accessor";
 import Toast from "libs/toast";
 import messages from "messages";
+import { getCurrentResolution } from "../accessors/flycam_accessor";
 export function* watchMaximumRenderableLayers(): Saga<void> {
   function* warnMaybe(): Saga<void> {
     const maximumLayerCountToRender = yield* select(
@@ -30,4 +31,30 @@ export function* watchMaximumRenderableLayers(): Saga<void> {
   }
 
   yield* takeEvery(["WK_READY", "UPDATE_LAYER_SETTING", "UPDATE_DATASET_SETTING"], warnMaybe);
+}
+
+let downsamplingWarningDidShow = false;
+export function* watchZ1Downsampling(): Saga<void> {
+  function* maybeShowWarning(): Saga<void> {
+    const currentRes = yield* select((state) => getCurrentResolution(state));
+    const currentZoomStep = yield* select((state) => state.flycam.zoomStep);
+    const minVoxelPerPixel = 0.5;
+
+    if (!downsamplingWarningDidShow) {
+      // checking the downsampled dimensions x and y
+      for (let i = 0; i < 2; i++) {
+        const voxelPerPixelXY = currentZoomStep / currentRes[i];
+        if (voxelPerPixelXY < minVoxelPerPixel) {
+          Toast.warning(messages["dataset.z1_downsampling_hint"]);
+          downsamplingWarningDidShow = false;
+        }
+      }
+    }
+  }
+  yield* takeEvery("WK_READY", maybeShowWarning);
+  yield* call(sleep, 2000);
+  yield* takeEvery(
+    ["ZOOM_IN", "ZOOM_OUT", "ZOOM_BY_DELTA", "SET_ZOOM_STEP", "SET_STORED_LAYOUTS"],
+    maybeShowWarning,
+  );
 }

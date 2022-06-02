8ae68e4560105ea1fda2025ec83d8ed2498f8ad4
Arthur Hilbert <46814136+Dagobert42@users.noreply.github.com>
2022-06-02 15:17:36 +0200

    Display warning when resolution gets too low during zooming (#6255)
    * display warning when resolution gets too low during zooming
    
    * updated unreleased changelog
    
    * fix crashing dashboard; debounce z1 warnings and tweak quality threshold
    
    Co-authored-by: Philipp Otto <philipp.4096@googlemail.com>

--- frontend/javascripts/oxalis/model/sagas/dataset_saga.ts
+++ frontend/javascripts/oxalis/model/sagas/dataset_saga.ts
@@ -1,10 +1,11 @@
-/* eslint-disable import/prefer-default-export */
 import type { Saga } from "oxalis/model/sagas/effect-generators";
 import { select } from "oxalis/model/sagas/effect-generators";
-import { takeEvery } from "typed-redux-saga";
+import { call, take, takeEvery, takeLatest } from "typed-redux-saga";
+import { sleep } from "libs/utils";
 import { getEnabledLayers } from "oxalis/model/accessors/dataset_accessor";
 import Toast from "libs/toast";
 import messages from "messages";
+import { getCurrentResolution } from "../accessors/flycam_accessor";
 export function* watchMaximumRenderableLayers(): Saga<void> {
   function* warnMaybe(): Saga<void> {
     const maximumLayerCountToRender = yield* select(
@@ -30,4 +31,45 @@ export function* watchMaximumRenderableLayers(): Saga<void> {
   }
 
   yield* takeEvery(["WK_READY", "UPDATE_LAYER_SETTING", "UPDATE_DATASET_SETTING"], warnMaybe);
+}
+
+let userClosedWarning = false;
+export function* watchZ1Downsampling(): Saga<void> {
+  function* maybeShowWarning(): Saga<void> {
+    // In combination with `takeLatest` sleeping here at the beginning of the saga
+    // effectively debounces the saga to avoid that it is executed unnecessarily often.
+    yield* call(sleep, 200);
+    const currentRes = yield* select((state) => getCurrentResolution(state));
+    const currentZoomStep = yield* select((state) => state.flycam.zoomStep);
+    if (currentZoomStep < 1) {
+      // If the user has zoomed into the data,
+      // the rendering quality is expected to be relatively low.
+      // Don't show any warnings in that case.
+      return;
+    }
+    const minVoxelPerPixel = 0.1;
+    if (!userClosedWarning) {
+      // checking only the downsampled dimensions x and y
+      const showWarning =
+        currentZoomStep / currentRes[0] < minVoxelPerPixel ||
+        currentZoomStep / currentRes[1] < minVoxelPerPixel;
+      if (showWarning) {
+        Toast.warning(messages["dataset.z1_downsampling_hint"], {
+          sticky: true,
+          key: "DOWNSAMPLING_CAUSES_BAD_QUALITY",
+          onClose: () => {
+            userClosedWarning = true;
+          },
+        });
+      } else {
+        Toast.close("DOWNSAMPLING_CAUSES_BAD_QUALITY");
+      }
+    }
+  }
+  yield* take("WK_READY");
+  yield* call(maybeShowWarning);
+  yield* takeLatest(
+    ["ZOOM_IN", "ZOOM_OUT", "ZOOM_BY_DELTA", "SET_ZOOM_STEP", "SET_STORED_LAYOUTS"],
+    maybeShowWarning,
+  );
 }

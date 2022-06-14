cba70af92fd0f02d08ee250341461fa985142324
Florian M <fm3@users.noreply.github.com>
2022-06-14 11:49:03 +0200

    Provide clean annotation uri, no more Explorational vs Task (#6208)
    * [WIP] provide clean annotation uri
    
    * don't use annotation type from url when loading an annotation
    
    * fix some @ts-expect-error directives
    
    * remove annotationTyp parameter for accessibleBySwitching route
    
    * change annotationType to maybeCompoundType as it's only needed for compound annotations; add new route without type parameter
    
    * fix linting
    
    * drop more Explorationals/Tasks from urls and adapt url_manager.ts
    
    * update changelog
    
    Co-authored-by: Philipp Otto <philipp.4096@googlemail.com>
    Co-authored-by: Philipp Otto <philippotto@users.noreply.github.com>
    Co-authored-by: Daniel <daniel.werner@scalableminds.com>

--- frontend/javascripts/admin/admin_rest_api.ts
+++ frontend/javascripts/admin/admin_rest_api.ts
@@ -54,6 +54,7 @@ import type {
   ServerTracing,
   TracingType,
   WkConnectDatasetConfig,
+  APICompoundType,
 } from "types/api_flow_types";
 import { APIAnnotationTypeEnum } from "types/api_flow_types";
 import type { Vector3, Vector6 } from "oxalis/constants";
@@ -63,7 +64,6 @@ import type {
   PartialDatasetConfiguration,
   Tracing,
   TraceOrViewCommand,
-  AnnotationType,
   MappingType,
   VolumeTracing,
   UserConfiguration,
@@ -708,11 +708,25 @@ export function copyAnnotationToUserAccount(
 
 export async function getAnnotationInformation(
   annotationId: string,
-  annotationType: APIAnnotationType,
+  options: RequestOptions = {},
+): Promise<APIAnnotation> {
+  const infoUrl = `/api/annotations/${annotationId}/info?timestamp=${Date.now()}`;
+  const annotationWithMessages = await Request.receiveJSON(infoUrl, options);
+
+  // Extract the potential messages property before returning the task to avoid
+  // failing e2e tests in annotations.e2e.ts
+  const { messages: _messages, ...annotation } = annotationWithMessages;
+  return annotation;
+}
+
+export async function getAnnotationCompoundInformation(
+  annotationId: string,
+  annotationType: APICompoundType,
   options: RequestOptions = {},
 ): Promise<APIAnnotation> {
   const infoUrl = `/api/annotations/${annotationType}/${annotationId}/info?timestamp=${Date.now()}`;
   const annotationWithMessages = await Request.receiveJSON(infoUrl, options);
+
   // Extract the potential messages property before returning the task to avoid
   // failing e2e tests in annotations.e2e.ts
   const { messages: _messages, ...annotation } = annotationWithMessages;
@@ -733,8 +747,8 @@ export function getEmptySandboxAnnotationInformation(
 export function createExplorational(
   datasetId: APIDatasetId,
   typ: TracingType,
-  fallbackLayerName: string | null | undefined,
-  resolutionRestrictions: APIResolutionRestrictions | null | undefined,
+  fallbackLayerName?: string | null | undefined,
+  resolutionRestrictions?: APIResolutionRestrictions | null | undefined,
   options: RequestOptions = {},
 ): Promise<APIAnnotation> {
   const url = `/api/datasets/${datasetId.owningOrganization}/${datasetId.name}/createExplorational`;
@@ -1752,12 +1766,11 @@ export async function updateOrganization(
 }
 
 export async function isDatasetAccessibleBySwitching(
-  annotationType: AnnotationType,
   commandType: TraceOrViewCommand,
 ): Promise<APIOrganization | null | undefined> {
   if (commandType.type === ControlModeEnum.TRACE) {
     return Request.receiveJSON(
-      `/api/auth/accessibleBySwitching?annotationTyp=${annotationType}&annotationId=${commandType.annotationId}`,
+      `/api/auth/accessibleBySwitching?annotationId=${commandType.annotationId}`,
     );
   } else {
     return Request.receiveJSON(

69e194cf596ba0dc626f6a5acc0ef1e497be5f27
leowe <13684843+leowe@users.noreply.github.com>
2022-07-14 14:25:54 +0200

    use comma instead of semicolon

--- conf/evolutions/084-add-annotations-publicationforeignkey.sql
+++ conf/evolutions/084-add-annotations-publicationforeignkey.sql
@@ -2,7 +2,7 @@ START TRANSACTION;
 
 DROP VIEW webknossos.annotations_;
 ALTER TABLE webknossos.annotations
-  ADD COLUMN _publication CHAR(24);
+  ADD COLUMN _publication CHAR(24),
   ADD CONSTRAINT publication_ref FOREIGN KEY(_publication) REFERENCES webknossos.publications(_id) DEFERRABLE;
 
 CREATE VIEW webknossos.annotations_ AS SELECT * FROM webknossos.annotations WHERE NOT isDeleted;

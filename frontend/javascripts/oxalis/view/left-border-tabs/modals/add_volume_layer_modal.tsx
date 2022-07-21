a14820d2b85b32336a89493519fbdc3413c7e74a
Arthur Hilbert <46814136+Dagobert42@users.noreply.github.com>
2022-07-21 15:05:04 +0200

    Improve convert to hybrid workflow (#6330)
    * remove old hybrid annotation workflow
    
    * add Add Skeleton button to Layers tab
    
    * add unlock button to segmentation layers which can be made editable
    
    * show Layer Selection as disabled to indicate origin layer

--- frontend/javascripts/oxalis/view/left-border-tabs/modals/add_volume_layer_modal.tsx
+++ frontend/javascripts/oxalis/view/left-border-tabs/modals/add_volume_layer_modal.tsx
@@ -12,6 +12,7 @@ import type { Tracing } from "oxalis/store";
 import { addAnnotationLayer } from "admin/admin_rest_api";
 import {
   getDatasetResolutionInfo,
+  getLayerByName,
   getSegmentationLayers,
 } from "oxalis/model/accessors/dataset_accessor";
 import {
@@ -84,14 +85,18 @@ export default function AddVolumeLayerModal({
   dataset,
   onCancel,
   tracing,
+  preselectedLayerName,
+  disableLayerSelection,
 }: {
   dataset: APIDataset;
   onCancel: () => void;
   tracing: Tracing;
+  preselectedLayerName: string | undefined;
+  disableLayerSelection: boolean | undefined;
 }) {
-  const [selectedSegmentationLayerIndex, setSelectedSegmentationLayerIndex] = useState<
-    number | null | undefined
-  >(null);
+  const [selectedSegmentationLayerName, setSelectedSegmentationLayerName] = useState<
+    string | null | undefined
+  >(preselectedLayerName);
   const allReadableLayerNames = useMemo(
     () => getAllReadableLayerNames(dataset, tracing),
     [dataset, tracing],
@@ -137,7 +142,7 @@ export default function AddVolumeLayerModal({
       ...datasetResolutionInfo.getResolutionByIndexOrThrow(resolutionIndices[1]),
     );
 
-    if (selectedSegmentationLayerIndex == null) {
+    if (selectedSegmentationLayerName == null) {
       await addAnnotationLayer(tracing.annotationId, tracing.annotationType, {
         typ: "Volume",
         name: newLayerName,
@@ -148,7 +153,7 @@ export default function AddVolumeLayerModal({
         },
       });
     } else {
-      selectedSegmentationLayer = availableSegmentationLayers[selectedSegmentationLayerIndex];
+      selectedSegmentationLayer = getLayerByName(dataset, selectedSegmentationLayerName);
       const fallbackLayerName = selectedSegmentationLayer.name;
       await addAnnotationLayer(tracing.annotationId, tracing.annotationType, {
         typ: "Volume",
@@ -188,8 +193,9 @@ export default function AddVolumeLayerModal({
         <NewVolumeLayerSelection
           dataset={dataset}
           segmentationLayers={availableSegmentationLayers}
-          selectedSegmentationLayerIndex={selectedSegmentationLayerIndex}
-          setSelectedSegmentationLayerIndex={setSelectedSegmentationLayerIndex}
+          selectedSegmentationLayerName={selectedSegmentationLayerName}
+          setSelectedSegmentationLayerName={setSelectedSegmentationLayerName}
+          disableLayerSelection={disableLayerSelection ?? false}
         />
       ) : null}
       <RestrictResolutionSlider

81d92f7845fa01f7211a6b0f4056b647e3336930
Philipp Otto <philipp.4096@googlemail.com>
2022-07-25 16:29:52 +0200

    type UpdatableTexture and clean up

--- frontend/javascripts/libs/UpdatableTexture.ts
+++ frontend/javascripts/libs/UpdatableTexture.ts
@@ -1,8 +1,14 @@
-// @ts-nocheck
 import * as THREE from "three";
 import { document } from "libs/window";
 
 class UpdatableTexture extends THREE.Texture {
+  isUpdatableTexture: boolean;
+  renderer!: THREE.WebGLRenderer;
+  gl: any;
+  utils!: THREE.WebGLUtils;
+  width: number | undefined;
+  height: number | undefined;
+
   constructor(
     width: number,
     height: number,
@@ -19,9 +25,6 @@ class UpdatableTexture extends THREE.Texture {
     const canvas = document.createElement("canvas");
     canvas.width = width;
     canvas.height = height;
-    const ctx = canvas.getContext("2d");
-    // const imageData = ctx.createImageData(width, height);
-    // this.image = imageData;
     super(canvas, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy, encoding);
 
     this.magFilter = magFilter !== undefined ? magFilter : THREE.LinearFilter;
@@ -33,13 +36,17 @@ class UpdatableTexture extends THREE.Texture {
     this.isUpdatableTexture = true;
   }
 
-  setRenderer(renderer) {
+  setRenderer(renderer: THREE.WebGLRenderer) {
     this.renderer = renderer;
     this.gl = this.renderer.getContext();
-    this.utils = THREE.WebGLUtils(this.gl, this.renderer.extensions, this.renderer.capabilities);
+    this.utils = new THREE.WebGLUtils(
+      this.gl,
+      this.renderer.extensions,
+      this.renderer.capabilities,
+    );
   }
 
-  setSize(width, height) {
+  setSize(width: number, height: number) {
     if (width === this.width && height === this.height) return;
     if (!this.isInitialized()) return;
     this.width = width;
@@ -47,24 +54,11 @@ class UpdatableTexture extends THREE.Texture {
     const activeTexture = this.gl.getParameter(this.gl.TEXTURE_BINDING_2D);
     const textureProperties = this.renderer.properties.get(this);
     this.gl.bindTexture(this.gl.TEXTURE_2D, textureProperties.__webglTexture);
-    if (!this.isInitialized()) this.width = null;
-
-    console.log("this.format", this.format);
-    console.log("this.utils.convert(this.format)", this.utils.convert(this.format));
-
-    // const convertToInternalFormat = (format) => {
-    //   if (format != THREE.RGFormat) {
-    //     return this.utils.convert(format);
-    //   } else {
-    //     return 0x8230;
-    //   }
-    // };
+    if (!this.isInitialized()) this.width = undefined;
 
     this.gl.texImage2D(
       this.gl.TEXTURE_2D,
       0,
-      // todo
-      // convertToInternalFormat(this.format),
       this.utils.convert(this.format),
       width,
       height,
@@ -80,7 +74,7 @@ class UpdatableTexture extends THREE.Texture {
     return this.renderer.properties.get(this).__webglTexture != null;
   }
 
-  update(src, x, y, width, height) {
+  update(src: Float32Array | Uint8Array, x: number, y: number, width: number, height: number) {
     if (!this.isInitialized()) {
       this.renderer.initTexture(this);
     }

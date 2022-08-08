934fb44a88cd7001944872e4ebb3614fe4bb99a9
Philipp Otto <philipp.4096@googlemail.com>
2022-08-08 10:29:36 +0200

    reuse 64 bit look up code for 32 look ups

--- frontend/javascripts/oxalis/shaders/texture_access.glsl.ts
+++ frontend/javascripts/oxalis/shaders/texture_access.glsl.ts
@@ -102,170 +102,6 @@ export const getColorForCoords: ShaderModule = {
     getResolutionFactors,
   ],
   code: `
-    vec4 getColorForCoords(
-      sampler2D lookUpTexture,
-      float layerIndex,
-      float d_texture_width,
-      float packingDegree,
-      vec3 worldPositionUVW
-    ) {
-      // This method looks up the color data at the given position.
-      // The data will be clamped to be non-negative, since negative data
-      // is reserved for missing buckets.
-
-      vec3 coords = floor(getRelativeCoords(worldPositionUVW, zoomStep));
-      vec3 relativeBucketPosition = div(coords, bucketWidth);
-      vec3 offsetInBucket = mod(coords, bucketWidth);
-
-      if (relativeBucketPosition.x > addressSpaceDimensions.x ||
-          relativeBucketPosition.y > addressSpaceDimensions.y ||
-          relativeBucketPosition.z > addressSpaceDimensions.z ||
-          relativeBucketPosition.x < 0.0 ||
-          relativeBucketPosition.y < 0.0 ||
-          relativeBucketPosition.z < 0.0) {
-        // In theory, the current magnification should always be selected
-        // so that we won't have to address data outside of the addresSpaceDimensions.
-        // Nevertheless, we explicitly guard against this situation here to avoid
-        // rendering wrong data.
-        return vec4(1.0, 1.0, 0.0, 1.0);
-      }
-
-      float bucketIdx = linearizeVec3ToIndex(relativeBucketPosition, addressSpaceDimensions);
-
-      vec2 bucketAddressWithZoomStep = getRgbaAtIndex(
-        lookUpTexture,
-        l_texture_width,
-        bucketIdx
-      ).rg;
-
-      float bucketAddress = bucketAddressWithZoomStep.x;
-      float renderedZoomStep = bucketAddressWithZoomStep.y;
-
-      if (bucketAddress == -2.0) {
-        // The bucket is out of bounds. Render black
-        // In flight mode, it can happen that buckets were not passed to the GPU
-        // since the approximate implementation of the bucket picker missed the bucket.
-        // We simply handle this case as if the bucket was not yet loaded which means
-        // that fallback data is loaded.
-        // The downside is that data which does not exist, will be rendered gray instead of black.
-        // Issue to track progress: #3446
-        float alpha = isFlightMode() ? -1.0 : 0.0;
-        return vec4(0.0, 0.0, 0.0, alpha);
-      }
-
-      if (bucketAddress < 0. ||
-          isNan(bucketAddress)) {
-        // Not-yet-existing data is encoded with a = -1.0
-        return vec4(0.0, 0.0, 0.0, -1.0);
-      }
-
-      if (renderedZoomStep != zoomStep) {
-        /* We already know which fallback bucket we have to look into. However,
-         * for 8 mag-1 buckets, there is usually one fallback bucket in mag-2.
-         * Therefore, depending on the actual mag-1 bucket, we have to look into
-         * different sub-volumes of the one fallback bucket. This is calculated as
-         * the subVolumeIndex.
-         * Then, we adapt the look up position *within* the bucket.
-         *
-         * Example Scenario (let's consider only the x axis):
-         * If we are in the [4, _, _, 0]-bucket, we have to look into the **first** half
-         * of the [2, _, _, 1]-bucket.
-         * If we are in the [5, _, _, 0]-bucket, we have to look into the **second** half
-         * of the [2, _, _, 1]-bucket.
-         * We can determine which "half" (subVolumeIndex) is relevant by doing a modulo operation
-         * with the resolution factor. A typical resolution factor is 2.
-         */
-
-        vec3 magnificationFactors = getResolutionFactors(renderedZoomStep, zoomStep);
-        vec3 worldBucketPosition = relativeBucketPosition + anchorPoint;
-        vec3 subVolumeIndex = mod(worldBucketPosition, magnificationFactors);
-        offsetInBucket = floor(
-          (offsetInBucket + vec3(bucketWidth) * subVolumeIndex)
-          / magnificationFactors
-        );
-      }
-
-      // bucketAddress can span multiple data textures. If the address is higher
-      // than the capacity of one texture, we mod the value and use the div (floored division) as the
-      // texture index
-      float packedBucketSize = bucketSize / packingDegree;
-      float bucketCapacityPerTexture = d_texture_width * d_texture_width / packedBucketSize;
-      float textureIndex = floor(bucketAddress / bucketCapacityPerTexture);
-      bucketAddress = mod(bucketAddress, bucketCapacityPerTexture);
-
-      float x =
-        // Mod while linearizing to avoid imprecisions for large numbers
-        linearizeVec3ToIndexWithMod(offsetInBucket / packingDegree, bucketWidth, d_texture_width);
-
-      float pixelIdxInBucket =
-        // Don't mod since we have to calculate pixelIdxInBucket / d_texture_width
-        linearizeVec3ToIndex(offsetInBucket / packingDegree, bucketWidth);
-      float y =
-        div(pixelIdxInBucket, d_texture_width) +
-        div(packedBucketSize * bucketAddress, d_texture_width);
-
-      vec4 bucketColor = getRgbaAtXYIndex(
-        layerIndex,
-        textureIndex,
-        d_texture_width,
-        x,
-        y
-      );
-
-      if (packingDegree == 0.5) {
-        vec4 bucketColor2 = getRgbaAtXYIndex(
-          layerIndex,
-          textureIndex,
-          d_texture_width,
-          x + 1.0,
-          // todo: test that this case works correctly
-          y + ((x + 1.0) == d_texture_width ? 1.0 : 0.0)
-        );
-
-        return bucketColor + bucketColor2;
-      }
-
-      if (packingDegree == 1.0) {
-        return max(bucketColor, 0.0);
-      }
-
-      float rgbaIndex = linearizeVec3ToIndexWithMod(offsetInBucket, bucketWidth, packingDegree);
-
-      if (packingDegree == 2.0) {
-        // It's essentially irrelevant what we return as the 3rd and 4th value here as we only have 2 byte of information.
-        // The caller needs to unpack this vec4 according to the packingDegree, see getSegmentationId for an example.
-        // The same goes for the following code where the packingDegree is 4 and we only have 1 byte of information.
-        if (rgbaIndex == 0.0) {
-          return vec4(
-            max(bucketColor.r, 0.0),
-            max(bucketColor.g, 0.0),
-            max(bucketColor.r, 0.0),
-            max(bucketColor.g, 0.0)
-          );
-        } else if (rgbaIndex == 1.0) {
-          return vec4(
-            max(bucketColor.b, 0.0),
-            max(bucketColor.a, 0.0),
-            max(bucketColor.b, 0.0),
-            max(bucketColor.a, 0.0)
-          );
-        }
-      }
-
-      // The following code deals with packingDegree == 4.0
-      if (rgbaIndex == 0.0) {
-        return vec4(max(bucketColor.r, 0.0));
-      } else if (rgbaIndex == 1.0) {
-        return vec4(max(bucketColor.g, 0.0));
-      } else if (rgbaIndex == 2.0) {
-        return vec4(max(bucketColor.b, 0.0));
-      } else if (rgbaIndex == 3.0) {
-        return vec4(max(bucketColor.a, 0.0));
-      }
-
-      return vec4(0.0);
-    }
-
     vec4[2] getColorForCoords64(
       sampler2D lookUpTexture,
       float layerIndex,
@@ -443,6 +279,17 @@ export const getColorForCoords: ShaderModule = {
 
       returnValue[1] = vec4(0.0);
       return returnValue;
+    }
+
+    vec4 getColorForCoords(
+      sampler2D lookUpTexture,
+      float layerIndex,
+      float d_texture_width,
+      float packingDegree,
+      vec3 worldPositionUVW
+    ) {
+      vec4[2] retVal = getColorForCoords64(lookUpTexture, layerIndex, d_texture_width, packingDegree, worldPositionUVW);
+      return retVal[1];
     }
   `,
 };

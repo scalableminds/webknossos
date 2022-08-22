75458f3830315f746c979919e1560e87619ecb58
leowe <leo@Leos-MacBook-Pro.local>
2022-08-22 17:27:20 +0200

    rename reversion

--- conf/evolutions/reversions/087-zarr-private-links.sql
+++ conf/evolutions/reversions/087-zarr-private-links.sql
@@ -3,6 +3,6 @@ START TRANSACTION;
 DROP VIEW webknossos.annotation_privateLinks_;
 DROP TABLE webknossos.annotation_privateLinks;
 
-UPDATE webknossos.releaseInformation SET schemaVersion = 85;
+UPDATE webknossos.releaseInformation SET schemaVersion = 86;
 
 COMMIT TRANSACTION;

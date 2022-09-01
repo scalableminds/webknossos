4d985b87b6409aaa6488d7be891d0b9858b3ff13
Florian M <fm3@users.noreply.github.com>
2022-09-01 16:44:51 +0200

    extensive error logging for mailchimp tagging (#6442)

--- app/oxalis/mail/MailchimpTicker.scala
+++ app/oxalis/mail/MailchimpTicker.scala
@@ -5,8 +5,10 @@ import com.scalableminds.util.accesscontext.{DBAccessContext, GlobalAccessContex
 import com.scalableminds.util.tools.Fox
 import com.scalableminds.webknossos.datastore.helpers.IntervalScheduler
 import com.typesafe.scalalogging.LazyLogging
+
 import javax.inject.Inject
 import models.user.{MultiUser, MultiUserDAO}
+import net.liftweb.common.{Empty, Failure, Full}
 import play.api.inject.ApplicationLifecycle
 
 import scala.concurrent.ExecutionContext
@@ -28,27 +30,33 @@ class MailchimpTicker @Inject()(val lifecycle: ApplicationLifecycle,
     logger.info("Checking if any users need mailchimp tagging...")
     for {
       multiUsers: List[MultiUser] <- multiUserDAO.findAll
-      _ <- Fox.serialCombined(multiUsers)(tagUserByActivity)
+      _ = multiUsers.foreach(withErrorLogging(_, tagUserByActivity))
     } yield ()
     ()
   }
 
+  private def withErrorLogging(multiUser: MultiUser, block: MultiUser => Fox[Unit]): Unit =
+    block(multiUser).futureBox.map {
+      case Full(_)    => ()
+      case f: Failure => logger.debug(s"Failed to tag multiuser ${multiUser._id} by activity: $f")
+      case Empty      => logger.debug("Failed to tag multiuser ${multiUser._id} by activity: Empty")
+    }
+
   private def tagUserByActivity(multiUser: MultiUser): Fox[Unit] =
     for {
-      isActivated <- multiUserDAO.hasAtLeastOneActiveUser(multiUser._id)
-      lastActivity <- multiUserDAO.lastActivity(multiUser._id)
+      isActivated <- multiUserDAO.hasAtLeastOneActiveUser(multiUser._id) ?~> "Could not determine isActivated"
+      lastActivity <- multiUserDAO.lastActivity(multiUser._id) ?~> "Could not determine lastActivity"
       now = System.currentTimeMillis()
       registeredAtLeast21DaysAgo = multiUser.created < now - (21 days).toMillis
       registeredAtMost22DaysAgo = multiUser.created > now - (22 days).toMillis
       shouldBeTaggedNow = isActivated && registeredAtLeast21DaysAgo && registeredAtMost22DaysAgo
       _ <- if (shouldBeTaggedNow) {
         for {
-          previousTags <- mailchimpClient.tagsForMultiUser(multiUser)
+          previousTags <- mailchimpClient.tagsForMultiUser(multiUser) ?~> "Could not fetch previous tags"
           alreadyTagged = previousTags.contains(MailchimpTag.WasInactiveInWeeksTwoAndThree) || previousTags.contains(
             MailchimpTag.WasActiveInWeeksTwoOrThree)
         } yield
-          if (alreadyTagged) ()
-          else {
+          if (!alreadyTagged) {
             if (lastActivity < now - (14 days).toMillis) {
               logger.info(s"Tagging user ${multiUser._id} as ${MailchimpTag.WasInactiveInWeeksTwoAndThree}...")
               mailchimpClient.tagMultiUser(multiUser, MailchimpTag.WasInactiveInWeeksTwoAndThree)

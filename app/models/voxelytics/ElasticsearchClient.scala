384b6d1336582f9c5bde0d512fef03d83ba8bc95
Norman Rzepka <code@normanrz.com>
2022-09-05 09:06:59 +0200

    fixes Elasticsearch polling and setup

--- app/models/voxelytics/ElasticsearchClient.scala
+++ app/models/voxelytics/ElasticsearchClient.scala
@@ -1,29 +1,126 @@
 package models.voxelytics
 
+import akka.actor.ActorSystem
 import com.scalableminds.util.mvc.MimeTypes
 import com.scalableminds.util.tools.Fox
+import com.scalableminds.util.tools.Fox.{bool2Fox, box2Fox}
 import com.scalableminds.webknossos.datastore.rpc.RPC
 import com.typesafe.scalalogging.LazyLogging
 import models.voxelytics.VoxelyticsLogLevel.VoxelyticsLogLevel
-import play.api.http.HeaderNames
+import net.liftweb.common.Full
+import play.api.http.{HeaderNames, Status}
+import play.api.inject.ApplicationLifecycle
 import play.api.libs.json.{JsArray, JsNumber, JsValue, Json}
 import utils.WkConf
 
+import java.time.{Duration, LocalDateTime}
 import java.util.UUID
 import javax.inject.Inject
 import scala.collection.mutable.ListBuffer
-import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.{FiniteDuration, SECONDS}
+import scala.concurrent.{ExecutionContext, Future}
 
-class ElasticsearchClient @Inject()(wkConf: WkConf, rpc: RPC)(implicit ec: ExecutionContext)
+class ElasticsearchClient @Inject()(wkConf: WkConf,
+                                    rpc: RPC,
+                                    val lifecycle: ApplicationLifecycle,
+                                    val system: ActorSystem)(implicit ec: ExecutionContext)
     extends LazyLogging
     with MimeTypes {
 
   private lazy val conf = wkConf.Voxelytics.Elasticsearch
   val SCROLL_SIZE = 10000
+  val POLLING_INTERVAL = FiniteDuration(1, SECONDS)
 
-  private def pollUntilServerIsAvailable: Fox[Unit] =
-    // TODO: Implement polling with conf.startupTimeout
-    Fox.successful(())
+  private lazy val elasticsearchSchema = Json.obj(
+    "settings" -> Json.obj(),
+    "mappings" -> Json.obj(
+      "properties" -> Json.obj(
+        "@timestamp" -> Json.obj("type" -> "date"),
+        "@version" -> Json.obj("type" -> "text",
+                               "fields" -> Json.obj("keyword" -> Json.obj("type" -> "keyword", "ignore_above" -> 256))),
+        "level" -> Json.obj("type" -> "text",
+                            "fields" -> Json.obj("keyword" -> Json.obj("type" -> "keyword", "ignore_above" -> 256))),
+        "message" -> Json.obj("type" -> "text",
+                              "fields" -> Json.obj("keyword" -> Json.obj("type" -> "keyword", "ignore_above" -> 256))),
+        "vx" -> Json.obj("properties" -> Json.obj(
+          "hostname" -> Json.obj(
+            "type" -> "text",
+            "fields" -> Json.obj("keyword" -> Json.obj("type" -> "keyword", "ignore_above" -> 256))),
+          "logger_name" -> Json.obj(
+            "type" -> "text",
+            "fields" -> Json.obj("keyword" -> Json.obj("type" -> "keyword", "ignore_above" -> 256))),
+          "run_name" -> Json.obj(
+            "type" -> "text",
+            "fields" -> Json.obj("keyword" -> Json.obj("type" -> "keyword", "ignore_above" -> 256))),
+          "task_name" -> Json.obj(
+            "type" -> "text",
+            "fields" -> Json.obj("keyword" -> Json.obj("type" -> "keyword", "ignore_above" -> 256))),
+          "wk_org" -> Json.obj("type" -> "text",
+                               "fields" -> Json.obj("keyword" -> Json.obj("type" -> "keyword", "ignore_above" -> 256))),
+          "wk_url" -> Json.obj("type" -> "text",
+                               "fields" -> Json.obj("keyword" -> Json.obj("type" -> "keyword", "ignore_above" -> 256))),
+          "workflow_hash" -> Json.obj(
+            "type" -> "text",
+            "fields" -> Json.obj("keyword" -> Json.obj("type" -> "keyword", "ignore_above" -> 256)))
+        ))
+      ))
+  )
+
+  private lazy val serverStartupFuture: Fox[Unit] = {
+    logger.info("Waiting for Elasticsearch to become available.")
+    for {
+      _ <- pollUntilServerStartedUp(LocalDateTime.now.plus(Duration.ofMillis(conf.startupTimeout.toMillis))) ~> 500
+      _ <- bootstrapIndexOnServer
+    } yield ()
+  }
+
+  private def pollUntilServerStartedUp(until: LocalDateTime): Fox[Unit] = {
+    def waitAndRecurse(until: LocalDateTime): Fox[Unit] =
+      for {
+        _ <- akka.pattern.after(POLLING_INTERVAL, using = system.scheduler)(Future.successful(()))
+        _ <- bool2Fox(!LocalDateTime.now().isAfter(until)) ?~> s"Elasticsearch did not become ready within ${conf.startupTimeout}."
+        _ <- pollUntilServerStartedUp(until)
+      } yield ()
+
+    for {
+      isServerAvailableBox <- rpc(s"${conf.uri}/_cluster/health?wait_for_status=yellow&timeout=10s").request
+        .withMethod("GET")
+        .execute()
+        .flatMap(result =>
+          if (Status.isSuccessful(result.status)) {
+            Fox.successful(true)
+          } else {
+            Fox.failure(s"Unexpected error code from Elasticsearch ${result.status}.")
+        })
+        .recoverWith({
+          case e: java.net.ConnectException => {
+            logger.debug(s"Elasticsearch connection exception: $e")
+            Fox.successful(false)
+          }
+          case e =>
+            logger.error(s"Unexpected error $e")
+            Fox.failure(s"Unexpected error while trying to connect to Elasticsearch.", Full(e))
+        })
+      isServerAvailable <- isServerAvailableBox.toFox
+      _ <- if (!isServerAvailable) {
+        waitAndRecurse(until)
+      } else {
+        logger.info("Elasticsearch is available.")
+        Fox.successful(())
+      }
+    } yield ()
+  }
+
+  private def bootstrapIndexOnServer: Fox[Unit] =
+    for {
+      indexExists <- rpc(s"${conf.uri}/${conf.index}").head.map(_ => true).getOrElse(false)
+      _ <- if (!indexExists) {
+        logger.info("Bootstrapping Elasticsearch index for Voxelytics.")
+        rpc(s"${conf.uri}/${conf.index}").put(elasticsearchSchema)
+      } else {
+        Fox.successful(())
+      }
+    } yield ()
 
   def queryLogs(runName: String,
                 organizationName: String,
@@ -47,7 +144,7 @@ class ElasticsearchClient @Inject()(wkConf: WkConf, rpc: RPC)(implicit ec: Execu
     val buffer = ListBuffer[JsValue]()
 
     for {
-      _ <- pollUntilServerIsAvailable
+      _ <- serverStartupFuture
       scroll <- rpc(s"${conf.uri}/${conf.index}/_search?scroll=1m")
         .postJsonWithJsonResponse[JsValue, JsValue](scrollBody) ~> "Could not fetch logs"
       scrollId = (scroll \ "_scroll_id").as[String]
@@ -83,7 +180,7 @@ class ElasticsearchClient @Inject()(wkConf: WkConf, rpc: RPC)(implicit ec: Execu
         .fold(Array.emptyByteArray)((rest, entry) => rest ++ entry ++ "\n".getBytes)
 
       for {
-        _ <- pollUntilServerIsAvailable
+        _ <- serverStartupFuture
         res <- rpc(uri)
           .addHttpHeaders(HeaderNames.CONTENT_TYPE -> jsonMimeType)
           .postBytesWithJsonResponse[JsValue](bytes)

9599105d56af92a3294da83136cb4002e8bc5fe6
Philipp Otto <philipp.4096@googlemail.com>
2022-09-21 15:31:34 +0200

    refactor add-zarr-layer UI into own react component

--- frontend/javascripts/admin/dataset/dataset_add_zarr_view.tsx
+++ frontend/javascripts/admin/dataset/dataset_add_zarr_view.tsx
@@ -1,4 +1,4 @@
-import { Form, Input, Button, Col, Radio, Row, Collapse } from "antd";
+import { Form, Input, Button, Col, Radio, Row, Collapse, FormInstance } from "antd";
 import { connect } from "react-redux";
 import React, { useState } from "react";
 import type { APIDataStore, APIUser } from "types/api_flow_types";
@@ -81,15 +81,9 @@ function mergeNewLayers(
 function DatasetAddZarrView(props: Props) {
   const { activeUser, onAdded } = props;
 
-  const [exploreLog, setExploreLog] = useState<string>("");
-  const [showCredentialsFields, setShowCredentialsFields] = useState<boolean>(false);
-  const [usernameOrAccessKey, setUsernameOrAccessKey] = useState<string>("");
-  const [passwordOrSecretKey, setPasswordOrSecretKey] = useState<string>("");
-  const [selectedProtocol, setSelectedProtocol] = useState<"s3" | "https">("https");
   const [dataSourceEditMode, setDataSourceEditMode] = useState<"simple" | "advanced">("simple");
   const [form] = Form.useForm();
   const datasourceConfigStr: string | null = Form.useWatch("dataSourceJson", form);
-  const datasourceUrl: string | null = Form.useWatch("url", form);
   const maybeDataLayers = Form.useWatch(["dataSource", "dataLayers"], form);
 
   const setDatasourceConfigStr = (dataSourceJson: string) => {
@@ -98,6 +92,111 @@ function DatasetAddZarrView(props: Props) {
     // data which is rendered by the "simple" page.
     syncDataSourceFields(form, "simple");
   };
+
+  async function handleStoreDataset() {
+    const uploadableDatastores = props.datastores.filter((datastore) => datastore.allowsUpload);
+    const datastoreToUse = uploadableDatastores[0];
+    if (!datastoreToUse) {
+      Toast.error("Could not find datastore that allows uploading.");
+      return;
+    }
+
+    if (datasourceConfigStr && activeUser) {
+      let configJSON;
+      try {
+        configJSON = JSON.parse(datasourceConfigStr);
+        const nameValidationResult = await isDatasetNameValid({
+          name: configJSON.id.name,
+          owningOrganization: activeUser.organization,
+        });
+        if (nameValidationResult) {
+          throw new Error(nameValidationResult);
+        }
+        const response = await storeRemoteDataset(
+          datastoreToUse.url,
+          configJSON.id.name,
+          activeUser.organization,
+          datasourceConfigStr,
+        );
+        if (response.status !== 200) {
+          const errorJSONString = JSON.stringify(await response.json());
+          throw new Error(`${response.status} ${response.statusText} ${errorJSONString}`);
+        }
+      } catch (e) {
+        Toast.error(`The datasource config could not be stored. ${e}`);
+        return;
+      }
+      onAdded(activeUser.organization, configJSON.id.name);
+    }
+  }
+
+  return (
+    // Using Forms here only to validate fields and for easy layout
+    <div style={{ padding: 5 }}>
+      <CardContainer title="Add Remote Zarr Dataset">
+        <Form form={form} style={{ marginTop: 20 }} layout="vertical">
+          <AddZarrLayer form={form} setDatasourceConfigStr={setDatasourceConfigStr} />
+          <Hideable hidden={maybeDataLayers == null || maybeDataLayers.length == 0}>
+            {/* Only the component's visibility is changed, so that the form is always rendered.
+                This is necessary so that the form's structure is always populated. */}
+            <DatasetSettingsDataTab
+              isReadOnlyDataset={false}
+              form={form}
+              activeDataSourceEditMode={dataSourceEditMode}
+              onChange={(activeEditMode) => {
+                syncDataSourceFields(form, activeEditMode);
+                form.validateFields();
+                setDataSourceEditMode(activeEditMode);
+              }}
+            />
+          </Hideable>
+          <Row gutter={8}>
+            <Col span={6} />
+            <Col span={6} />
+            <Col span={6}>
+              <FormItem>
+                <Button
+                  size="large"
+                  type="default"
+                  style={{ width: "100%" }}
+                  onClick={() => setDatasourceConfigStr("")}
+                >
+                  Reset
+                </Button>
+              </FormItem>
+            </Col>
+            <Col span={6}>
+              <Button
+                size="large"
+                type="primary"
+                style={{ width: "100%" }}
+                onClick={handleStoreDataset}
+                disabled={!datasourceConfigStr}
+              >
+                Import
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </CardContainer>
+    </div>
+  );
+}
+
+function AddZarrLayer({
+  form,
+  setDatasourceConfigStr,
+}: {
+  form: FormInstance;
+  setDatasourceConfigStr: (dataSourceJson: string) => void;
+}) {
+  const datasourceConfigStr: string | null = Form.useWatch("dataSourceJson", form);
+  const datasourceUrl: string | null = Form.useWatch("url", form);
+  const [exploreLog, setExploreLog] = useState<string>("");
+  const [showCredentialsFields, setShowCredentialsFields] = useState<boolean>(false);
+  const [usernameOrAccessKey, setUsernameOrAccessKey] = useState<string>("");
+  const [passwordOrSecretKey, setPasswordOrSecretKey] = useState<string>("");
+  const [selectedProtocol, setSelectedProtocol] = useState<"s3" | "https">("https");
 
   function validateUrls(userInput: string) {
     if (
@@ -151,184 +250,101 @@ function DatasetAddZarrView(props: Props) {
     setDatasourceConfigStr(jsonStringify(mergeNewLayers(loadedDatasource, dataSource)));
   }
 
-  async function handleStoreDataset() {
-    const uploadableDatastores = props.datastores.filter((datastore) => datastore.allowsUpload);
-    const datastoreToUse = uploadableDatastores[0];
-    if (!datastoreToUse) {
-      Toast.error("Could not find datastore that allows uploading.");
-      return;
-    }
-
-    if (datasourceConfigStr && activeUser) {
-      let configJSON;
-      try {
-        configJSON = JSON.parse(datasourceConfigStr);
-        const nameValidationResult = await isDatasetNameValid({
-          name: configJSON.id.name,
-          owningOrganization: activeUser.organization,
-        });
-        if (nameValidationResult) {
-          throw new Error(nameValidationResult);
-        }
-        const response = await storeRemoteDataset(
-          datastoreToUse.url,
-          configJSON.id.name,
-          activeUser.organization,
-          datasourceConfigStr,
-        );
-        if (response.status !== 200) {
-          const errorJSONString = JSON.stringify(await response.json());
-          throw new Error(`${response.status} ${response.statusText} ${errorJSONString}`);
-        }
-      } catch (e) {
-        Toast.error(`The datasource config could not be stored. ${e}`);
-        return;
-      }
-      onAdded(activeUser.organization, configJSON.id.name);
-    }
-  }
-
   return (
-    // Using Forms here only to validate fields and for easy layout
-    <div style={{ padding: 5 }}>
-      <CardContainer title="Add Remote Zarr Dataset">
-        Please enter a URL that points to the Zarr data you would like to import. If necessary,
-        specify the credentials for the dataset. More layers can be added to the datasource
-        specification below using the Add button. Once you have approved of the resulting datasource
-        you can import it.
-        <Form form={form} style={{ marginTop: 20 }} layout="vertical">
-          <FormItem
-            name="url"
-            label="Dataset URL"
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: messages["dataset.import.required.url"],
-              },
-              {
-                validator: (_rule, value) => {
-                  try {
-                    validateUrls(value);
-                    return Promise.resolve();
-                  } catch (e) {
-                    return Promise.reject(e);
-                  }
-                },
-              },
-            ]}
-            validateFirst
-          >
-            <Input />
-          </FormItem>
-          <FormItem label="Authentication">
-            <RadioGroup
-              defaultValue="hide"
-              value={showCredentialsFields ? "show" : "hide"}
-              onChange={(e) => setShowCredentialsFields(e.target.value === "show")}
+    <>
+      Please enter a URL that points to the Zarr data you would like to import. If necessary,
+      specify the credentials for the dataset. More layers can be added to the datasource
+      specification below using the Add button. Once you have approved of the resulting datasource
+      you can import it.
+      <FormItem
+        name="url"
+        label="Dataset URL"
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: messages["dataset.import.required.url"],
+          },
+          {
+            validator: (_rule, value) => {
+              try {
+                validateUrls(value);
+                return Promise.resolve();
+              } catch (e) {
+                return Promise.reject(e);
+              }
+            },
+          },
+        ]}
+        validateFirst
+      >
+        <Input />
+      </FormItem>
+      <FormItem label="Authentication">
+        <RadioGroup
+          defaultValue="hide"
+          value={showCredentialsFields ? "show" : "hide"}
+          onChange={(e) => setShowCredentialsFields(e.target.value === "show")}
+        >
+          <Radio value="hide">{selectedProtocol === "https" ? "None" : "Anonymous"}</Radio>
+          <Radio value="show">
+            {selectedProtocol === "https" ? "Basic authentication" : "With credentials"}
+          </Radio>
+        </RadioGroup>
+      </FormItem>
+      {showCredentialsFields ? (
+        <Row gutter={8}>
+          <Col span={12}>
+            <FormItem
+              label={selectedProtocol === "https" ? "Username" : "Access Key ID"}
+              hasFeedback
+              rules={[{ required: true }]}
+              validateFirst
             >
-              <Radio value="hide">{selectedProtocol === "https" ? "None" : "Anonymous"}</Radio>
-              <Radio value="show">
-                {selectedProtocol === "https" ? "Basic authentication" : "With credentials"}
-              </Radio>
-            </RadioGroup>
-          </FormItem>
-          {showCredentialsFields ? (
-            <Row gutter={8}>
-              <Col span={12}>
-                <FormItem
-                  label={selectedProtocol === "https" ? "Username" : "Access Key ID"}
-                  hasFeedback
-                  rules={[{ required: true }]}
-                  validateFirst
-                >
-                  <Input
-                    value={usernameOrAccessKey}
-                    onChange={(e) => setUsernameOrAccessKey(e.target.value)}
-                  />
-                </FormItem>
-              </Col>
-              <Col span={12}>
-                <FormItem
-                  label={selectedProtocol === "https" ? "Password" : "Secret Access Key"}
-                  hasFeedback
-                  rules={[{ required: true }]}
-                  validateFirst
-                >
-                  <Password
-                    value={passwordOrSecretKey}
-                    onChange={(e) => setPasswordOrSecretKey(e.target.value)}
-                  />
-                </FormItem>
-              </Col>
-            </Row>
-          ) : null}
-          <FormItem style={{ marginBottom: 0 }}>
-            <Row gutter={8}>
-              <Col span={18} />
-              <Col span={6}>
-                <AsyncButton
-                  size="large"
-                  type={datasourceConfigStr ? "default" : "primary"}
-                  style={{ width: "100%" }}
-                  onClick={handleExplore}
-                >
-                  Add Layer
-                </AsyncButton>
-              </Col>
-            </Row>
-          </FormItem>
-          <Collapse bordered={false} collapsible={exploreLog ? "header" : "disabled"}>
-            <Panel header="Log" key="1">
-              <Hint style={{ width: "90%" }}>
-                <pre style={{ whiteSpace: "pre-wrap" }}>{exploreLog}</pre>
-              </Hint>
-            </Panel>
-          </Collapse>
-          <Hideable hidden={maybeDataLayers == null || maybeDataLayers.length == 0}>
-            {/* Only the component's visibility is changed, so that the form is always rendered.
-                This is necessary so that the form's structure is always populated. */}
-            <DatasetSettingsDataTab
-              isReadOnlyDataset={false}
-              form={form}
-              activeDataSourceEditMode={dataSourceEditMode}
-              onChange={(activeEditMode) => {
-                syncDataSourceFields(form, activeEditMode);
-                form.validateFields();
-                setDataSourceEditMode(activeEditMode);
-              }}
-            />
-          </Hideable>
-          <Row gutter={8}>
-            <Col span={6} />
-            <Col span={6} />
-            <Col span={6}>
-              <FormItem>
-                <Button
-                  size="large"
-                  type="default"
-                  style={{ width: "100%" }}
-                  onClick={() => setDatasourceConfigStr("")}
-                >
-                  Reset
-                </Button>
-              </FormItem>
-            </Col>
-            <Col span={6}>
-              <Button
-                size="large"
-                type="primary"
-                style={{ width: "100%" }}
-                onClick={handleStoreDataset}
-                disabled={!datasourceConfigStr}
-              >
-                Import
-              </Button>
-            </Col>
-          </Row>
-        </Form>
-      </CardContainer>
-    </div>
+              <Input
+                value={usernameOrAccessKey}
+                onChange={(e) => setUsernameOrAccessKey(e.target.value)}
+              />
+            </FormItem>
+          </Col>
+          <Col span={12}>
+            <FormItem
+              label={selectedProtocol === "https" ? "Password" : "Secret Access Key"}
+              hasFeedback
+              rules={[{ required: true }]}
+              validateFirst
+            >
+              <Password
+                value={passwordOrSecretKey}
+                onChange={(e) => setPasswordOrSecretKey(e.target.value)}
+              />
+            </FormItem>
+          </Col>
+        </Row>
+      ) : null}
+      <FormItem style={{ marginBottom: 0 }}>
+        <Row gutter={8}>
+          <Col span={18} />
+          <Col span={6}>
+            <AsyncButton
+              size="large"
+              type={datasourceConfigStr ? "default" : "primary"}
+              style={{ width: "100%" }}
+              onClick={handleExplore}
+            >
+              Add Layer
+            </AsyncButton>
+          </Col>
+        </Row>
+      </FormItem>
+      <Collapse bordered={false} collapsible={exploreLog ? "header" : "disabled"}>
+        <Panel header="Log" key="1">
+          <Hint style={{ width: "90%" }}>
+            <pre style={{ whiteSpace: "pre-wrap" }}>{exploreLog}</pre>
+          </Hint>
+        </Panel>
+      </Collapse>
+    </>
   );
 }
 

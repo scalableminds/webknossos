8839007757ccc1affb9a7c8638d6f1fe9e30520a
leowe <leowndt@gmail.com>
2022-09-29 16:45:21 +0200

    fix addition

--- util/src/main/scala/com/scalableminds/util/geometry/Vec3Float.scala
+++ util/src/main/scala/com/scalableminds/util/geometry/Vec3Float.scala
@@ -9,7 +9,7 @@ case class Vec3Float(x: Float, y: Float, z: Float) {
   def *(s: Float): Vec3Float = scale(s)
   def *(s: Double): Vec3Float = scale(s.toFloat)
   def *(that: Vec3Float): Vec3Float = Vec3Float(x * that.x, y * that.y, z * that.z)
-  def +(that: Vec3Float): Vec3Float = Vec3Float(x + that.x, y + that.y, z + that.y)
+  def +(that: Vec3Float): Vec3Float = Vec3Float(x + that.x, y + that.y, z + that.z)
 
   def toList: List[Float] = List(x, y, z)
 }

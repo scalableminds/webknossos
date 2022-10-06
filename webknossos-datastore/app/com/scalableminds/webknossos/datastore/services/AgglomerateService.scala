938d5240de4d9559feed688aba887ffad3eb7e6b
Florian M <florian@scm.io>
2022-10-06 15:49:11 +0200

    add explaining comment for data conversion

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/AgglomerateService.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/AgglomerateService.scala
@@ -65,6 +65,13 @@ class AgglomerateService @Inject()(config: DataStoreConfig) extends DataConverte
     }
 
     val bytesPerElement = ElementClass.bytesPerElement(request.dataLayer.elementClass)
+    /* Every value of the segmentation data needs to be converted to Long to then look up the
+       agglomerate id in the segment-to-agglomerate array.
+       The value is first converted to the primitive signed number types, and then converted
+       to Long via uByteToLong, uShortToLong etc, which perform bitwise and to take care of
+       the unsigned semantics. Using functions avoids allocating intermediate UnsignedInteger objects.
+       Allocating a fixed-length LongBuffer first is a further performance optimization.
+     */
     convertData(data, request.dataLayer.elementClass) match {
       case data: Array[Byte] =>
         val longBuffer = LongBuffer.allocate(data.length)

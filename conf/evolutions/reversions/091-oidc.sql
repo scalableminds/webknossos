4676667d53ef670eec4dbd2f71006292dbeb72e8
frcroth <frcroth@gmail.com>
2022-10-20 11:45:38 +0200

    Remove complicated reversion

--- conf/evolutions/reversions/091-oidc.sql
+++ conf/evolutions/reversions/091-oidc.sql
@@ -1,20 +1,9 @@
 BEGIN transaction;
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 90;
-DROP VIEW webknossos.multiUsers_;
 
 -- Delete OIDC users
 DELETE FROM webknossos.multiUsers WHERE passwordInfo_hasher = 'Empty';
+-- Enum is not altered (See evolution)
 
--- Remove option from enum (requires creating a new type)
-CREATE TYPE webknossos.USER_PASSWORDINFO_HASHERS_INITIAL AS ENUM ('Scrypt');
-
-ALTER TABLE webknossos.multiUsers
-  ALTER COLUMN passwordInfo_hasher TYPE webknossos.USER_PASSWORDINFO_HASHERS_INITIAL
-    USING (passwordInfo_hasher::text::webknossos.USER_PASSWORDINFO_HASHERS_INITIAL);
-
-DROP TYPE IF EXISTS webknossos.USER_PASSWORDINFO_HASHERS;
-
-ALTER TYPE webknossos.USER_PASSWORDINFO_HASHERS_INITIAL RENAME TO "webknossos.USER_PASSWORDINFO_HASHERS";
-CREATE VIEW webknossos.multiUsers_ AS SELECT * FROM webknossos.multiUsers WHERE NOT isDeleted;
 COMMIT;

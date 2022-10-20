f56cd5bc2b9f2c8ac7f199a4f77773c2310748e7
frcroth <frcroth@gmail.com>
2022-10-20 11:06:58 +0200

    Fix cannot alter enum in transaction block

--- conf/evolutions/091-oidc.sql
+++ conf/evolutions/091-oidc.sql
@@ -1,6 +1,17 @@
 BEGIN transaction;
 
-ALTER TYPE webknossos.USER_PASSWORDINFO_HASHERS ADD VALUE 'Empty';
+-- Cannot alter enum in transaction block, workaround required
+
+ALTER TABLE webknossos.multiusers ALTER COLUMN passwordInfo_hasher TYPE VARCHAR(255);
+DROP TYPE IF EXISTS webknossos.USER_PASSWORDINFO_HASHERS;
+CREATE TYPE webknossos.USER_PASSWORDINFO_HASHERS AS ENUM (
+  'SCrypt',
+  'Empty'
+  );
+ALTER TABLE webknossos.multiusers
+  ALTER COLUMN passwordInfo_hasher TYPE webknossos.USER_PASSWORDINFO_HASHERS
+    USING (passwordInfo_hasher::webknossos.USER_PASSWORDINFO_HASHERS);
+
 UPDATE webknossos.releaseInformation SET schemaVersion = 91;
 
 COMMIT;

8e5e38e65dafa7b836e20a65e4e913c3f1675ce8
frcroth <frcroth@gmail.com>
2022-10-25 13:12:39 +0200

    Refactor bounding box merge deduplication

--- webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/tracings/BoundingBoxMerger.scala
+++ webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/tracings/BoundingBoxMerger.scala
@@ -24,11 +24,8 @@ trait BoundingBoxMerger extends ProtoGeometryImplicits {
     // note that the singleBoundingBox field is deprecated but still supported here to avoid database evolutions
     val singleBoundingBoxes =
       (singleBoundingBoxAOpt ++ singleBoundingBoxBOpt).map(bb => ProtoNamedBoundingBox(0, boundingBox = bb))
-
-    var boundingBoxMap: Map[Any, ProtoNamedBoundingBox] = Map()
-    (userBoundingBoxesA ++ userBoundingBoxesB ++ singleBoundingBoxes).foreach(uBB =>
-      boundingBoxMap += ((uBB.boundingBox, uBB.name, uBB.color, uBB.isVisible) -> uBB))
-    boundingBoxMap.values.zipWithIndex.map(uBB => uBB._1.copy(id = uBB._2)).toSeq
+    val allBoundingBoxes = userBoundingBoxesA ++ userBoundingBoxesB ++ singleBoundingBoxes
+    allBoundingBoxes.map(_.copy(id = 0)).distinct.zipWithIndex.map(uBB => uBB._1.copy(id = uBB._2))
   }
 
 }

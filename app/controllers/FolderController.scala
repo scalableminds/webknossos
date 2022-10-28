177a652a8adb285acd3e8592a920e2f458a8183a
Florian M <florian@scm.io>
2022-10-28 11:28:13 +0200

    create folder route, list datasets by folder

--- app/controllers/FolderController.scala
+++ app/controllers/FolderController.scala
@@ -2,11 +2,11 @@ package controllers
 
 import com.mohiva.play.silhouette.api.Silhouette
 import com.scalableminds.util.tools.FoxImplicits
-import models.folder.{FolderDAO, FolderService}
+import models.folder.{Folder, FolderDAO, FolderService}
 import models.organization.OrganizationDAO
 import oxalis.security.WkEnv
-import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent}
+import utils.ObjectId
 
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
@@ -23,7 +23,16 @@ class FolderController @Inject()(folderDAO: FolderDAO,
       organization <- organizationDAO.findOne(request.identity._organization)
       rootFolder <- folderDAO.findOne(organization._rootFolder)
       rootFolderJson <- folderService.publicWrites(rootFolder)
-    } yield Ok(Json.toJson(rootFolderJson))
+    } yield Ok(rootFolderJson)
+  }
+
+  def create(parentId: String, name: String): Action[AnyContent] = sil.SecuredAction.async { implicit request =>
+    for {
+      parentFolderIdValidated <- ObjectId.fromString(parentId)
+      newFolder = Folder(ObjectId.generate, name)
+      _ <- folderDAO.insertAsChild(parentFolderIdValidated, newFolder)
+      folderJson <- folderService.publicWrites(newFolder)
+    } yield Ok(folderJson)
   }
 
 }

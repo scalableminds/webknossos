9ddcaf569b5d0090159a6dcc4450488633e4bbeb
frcroth <frcroth@gmail.com>
2022-11-03 11:22:54 +0100

    Pass incudedStorage in MB

--- app/models/organization/OrganizationService.scala
+++ app/models/organization/OrganizationService.scala
@@ -32,7 +32,7 @@ class OrganizationService @Inject()(organizationDAO: OrganizationDAO,
         "pricingPlan" -> organization.pricingPlan,
         "paidUntil" -> organization.paidUntil,
         "includedUsers" -> organization.includedUsers,
-        "includedStorage" -> organization.includedStorage
+        "includedStorage" -> organization.includedStorage.map(bytes => bytes / 1000000)
       )
     } else Json.obj()
     Fox.successful(

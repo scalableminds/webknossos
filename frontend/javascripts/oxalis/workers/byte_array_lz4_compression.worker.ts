738d29ff51c4fd1d0a2ed0284d28d76edc29e88c
Philipp Otto <philipp.4096@googlemail.com>
2022-11-23 10:52:01 +0100

    Revert "re-add slow lz4js library to conduct performance comparison on dev instance"
    This reverts commit c86c6479c345efaa4ab16caec149b5c0f2e6770b.

--- frontend/javascripts/oxalis/workers/byte_array_lz4_compression.worker.ts
+++ frontend/javascripts/oxalis/workers/byte_array_lz4_compression.worker.ts
@@ -1,23 +1,12 @@
 import { expose } from "./comlink_wrapper";
-// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'lz4j... Remove this comment to see the full error message
-import lz4Slow from "lz4js";
 import * as lz4 from "lz4-wasm";
 
-const useSlowCompressor = Math.random() > 0.5;
-console.log("useSlowCompressor", useSlowCompressor);
 function compressLz4Block(data: Uint8Array, compress: boolean): Uint8Array {
-  const compressor = useSlowCompressor ? lz4Slow : lz4;
-
-  const then = performance.now();
   if (compress) {
-    const retval = compressor.compress(data);
-    console.log(`${useSlowCompressor ? "slow" : "fast"} compress: ${performance.now() - then}`);
-    return retval;
+    return lz4.compress(data);
   }
 
-  const retval = compressor.decompress(data);
-  console.log(`${useSlowCompressor ? "slow" : "fast"} decompress: ${performance.now() - then}`);
-  return retval;
+  return lz4.decompress(data);
 }
 
 // This function is only exposed for slow_byte_array_lz4_compression.worker.js

7d8ca61016bc61f957e691017c023c03acc27348
Philipp Otto <philippotto@users.noreply.github.com>
2022-11-23 14:23:19 +0100

    Use LZ4 with WASM for volume saving/undo/redo (#6652)
    * fix conversion of NaN to big int
    
    * use lz4-wasm for compression/decompression of buckets (for undo/redo)
    
    * also use lz4-wasm for compression of buckets (for saving)
    
    * clean up lz-wasm usage
    
    * fix tests by using different lz4 library there via mocking
    
    * more mocks
    
    * update changelog
    
    * more mocks
    
    * add missing file
    
    * fix accidental mock of non-spec files
    
    * fix accidental mock of non-spec files
    
    * also mock in e2e tests
    
    * add wasm mime type
    
    * extract slick conf
    
    * extract lz4 mock in e2e specs away using e2e-setup
    
    * re-add slow lz4js library to conduct performance comparison on dev instance
    
    * Revert "re-add slow lz4js library to conduct performance comparison on dev instance"
    
    This reverts commit c86c6479c345efaa4ab16caec149b5c0f2e6770b.
    
    * tune down changelog entry
    
    * bump pako version (used for gzip compression when sending buckets to server)
    
    Co-authored-by: Florian M <florian@scm.io>

--- frontend/javascripts/oxalis/workers/byte_array_to_lz4_base64.worker.ts
+++ frontend/javascripts/oxalis/workers/byte_array_to_lz4_base64.worker.ts
@@ -1,16 +1,13 @@
 import Base64 from "base64-js";
-// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'lz4j... Remove this comment to see the full error message
-import lz4 from "lz4js";
+import * as lz4wasm from "lz4-wasm";
 import { expose } from "./comlink_wrapper";
 
 function compressLz4Block(data: Uint8Array): Uint8Array {
-  // Backend expects the frame-less version of lz4,
-  // so we need to call lz4.compressBlock rather than compress
-  const hashSize = 1 << 16;
-  const hashTable = new Uint32Array(hashSize);
-  const compressedBuffer = new Uint8Array(data.length);
-  const compressedSize = lz4.compressBlock(data, compressedBuffer, 0, data.length, hashTable);
-  return compressedBuffer.slice(0, compressedSize);
+  // The backend expects the block (frame-less) version of lz4.
+  // lz4-wasm uses the block version, but prepends the size of the
+  // compressed data. Therefore, we strip the first 4 bytes.
+  const newCompressed = lz4wasm.compress(data);
+  return newCompressed.slice(4);
 }
 
 export function byteArrayToLz4Base64(byteArray: Uint8Array): string {

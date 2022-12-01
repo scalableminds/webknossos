c0d0e6e1a5bbb289564fffbebc03db9a161177c5
Philipp Otto <philipp.4096@googlemail.com>
2022-12-01 10:28:54 +0100

    dont create folder when user hit escape in new-name-prompt

--- frontend/javascripts/dashboard/folders/folder_tree.tsx
+++ frontend/javascripts/dashboard/folders/folder_tree.tsx
@@ -241,8 +241,12 @@ function generateTitle(
   const { key: id, title, isEditable } = folder;
 
   function createFolder(): void {
-    const folderName = prompt("Please input a name for the new folder");
-    context.queries.createFolderMutation.mutateAsync([id, folderName || "New folder"]);
+    const folderName = prompt("Please input a name for the new folder", "New folder");
+    if (!folderName) {
+      // The user hit escape/cancel
+      return;
+    }
+    context.queries.createFolderMutation.mutateAsync([id, folderName]);
   }
   function deleteFolder(): void {
     context.queries.deleteFolderMutation.mutateAsync(id);

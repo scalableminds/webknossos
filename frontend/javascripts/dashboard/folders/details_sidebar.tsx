27d999713adf6c7c67f914bafc0b8478e4675afb
Philipp Otto <philipp.4096@googlemail.com>
2022-12-06 14:36:34 +0100

    refactor sidebar sub components and display 'n datasets are selected' in multi selection case

--- frontend/javascripts/dashboard/folders/details_sidebar.tsx
+++ frontend/javascripts/dashboard/folders/details_sidebar.tsx
@@ -33,131 +33,172 @@ export function DetailsSidebar({
   const context = useDatasetCollectionContext();
   const { data: folder, error } = useFolderQuery(activeFolderId);
 
-  const selectedDataset = selectedDatasets.length > 0 ? selectedDatasets[0] : null;
-
   useEffect(() => {
-    if (selectedDataset == null || !("folderId" in selectedDataset)) {
-      return;
-    }
     if (
-      selectedDataset.folderId !== context.activeFolderId &&
+      selectedDatasets.some((ds) => ds.folderId !== context.activeFolderId) &&
       context.activeFolderId != null &&
       context.globalSearchQuery == null
     ) {
-      // Ensure that the selected dataset is in the active folder. If not,
-      // clear the sidebar. Don't do this when search results are shown (since
+      // Ensure that the selected dataset(s) are in the active folder. If not,
+      // clear the selection. Don't do this when search results are shown (since
       // these can cover multiple folders).
       // Typically, this is triggered when navigating to another folder.
       setSelectedDataset(null);
     }
-  }, [selectedDataset, context.activeFolderId]);
-
-  const maybeSelectMsg = datasetCount > 0 ? "Select one to see details." : "";
+  }, [selectedDatasets, context.activeFolderId]);
 
   return (
     <div style={{ width: 300, padding: 16 }}>
-      {selectedDataset != null ? (
-        <>
-          <h4 style={{ wordBreak: "break-all" }}>
-            <FileOutlined style={{ marginRight: 4 }} />{" "}
-            {selectedDataset.displayName || selectedDataset.name}
-          </h4>
-          {selectedDataset.isActive && (
-            <div>
-              <span className="sidebar-label">Voxel Size & Extent</span>
-              <div className="info-tab-block" style={{ marginTop: -6 }}>
-                <table
-                  style={{
-                    fontSize: 14,
-                  }}
-                >
-                  <tbody>
-                    <DatasetExtentRow dataset={selectedDataset} />
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          )}
-          {selectedDataset.description && (
-            <div style={{ marginBottom: 4 }}>
-              <span className="sidebar-label">Description</span>
-              <div>{selectedDataset.description}</div>
-            </div>
-          )}
-          <div style={{ marginBottom: 4 }}>
-            <span className="sidebar-label">Access Permissions</span>
-            <br />
-            <TeamTags dataset={selectedDataset} emptyValue="Administrators & Dataset Managers" />
-          </div>
-          <div style={{ marginBottom: 4 }}>
-            <span className="sidebar-label">Layers</span>
-            <br /> <DatasetLayerTags dataset={selectedDataset} />
-          </div>
-          {selectedDataset.isActive ? (
-            <div style={{ marginBottom: 4 }}>
-              <span className="sidebar-label">Tags</span>
-              <DatasetTags dataset={selectedDataset} updateDataset={context.updateCachedDataset} />
-            </div>
-          ) : null}
-        </>
+      {selectedDatasets.length === 1 ? (
+        <DatasetDetails selectedDataset={selectedDatasets[0]} />
+      ) : selectedDatasets.length > 1 ? (
+        <DatasetsDetails selectedDatasets={selectedDatasets} />
+      ) : searchQuery ? (
+        <SearchDetails datasetCount={datasetCount} />
       ) : (
-        <div style={{ textAlign: "center" }}>
-          {searchQuery ? (
-            <Result
-              icon={<SearchOutlined style={{ fontSize: 50 }} />}
-              subTitle={
-                datasetCount !== SEARCH_RESULTS_LIMIT ? (
-                  <>
-                    {datasetCount} {pluralize("dataset", datasetCount)} were found. {maybeSelectMsg}
-                  </>
-                ) : (
-                  <>
-                    At least {SEARCH_RESULTS_LIMIT} datasets match your search criteria.{" "}
-                    {maybeSelectMsg}
-                  </>
-                )
-              }
-            />
-          ) : (
-            <>
-              {folder ? (
-                <div style={{ textAlign: "left" }}>
-                  <h4 style={{ wordBreak: "break-all" }}>
-                    <span
-                      style={{
-                        float: "right",
-                        fontSize: 16,
-                        marginTop: 2,
-                        marginLeft: 2,
-                        color: "var(--ant-text-secondary)",
-                      }}
-                    >
-                      <SettingOutlined onClick={() => setFolderIdForEditModal(folder.id)} />
-                    </span>
-                    <FolderOpenOutlined style={{ marginRight: 8 }} />
-                    {folder.name}
-                  </h4>
-                  <p>
-                    This folder contains{" "}
-                    <Tooltip title="This number is independent of any filters that might be applied to the current view (e.g., only showing available datasets)">
-                      {datasetCount} {pluralize("dataset", datasetCount)}*
-                    </Tooltip>
-                    . {maybeSelectMsg}
-                  </p>
-                  <span className="sidebar-label">Access Permissions</span>
-                  <br />
-                  <FolderTeamTags folder={folder} />
-                </div>
-              ) : error ? (
-                "Could not load folder."
-              ) : activeFolderId != null ? (
-                <Spin spinning />
-              ) : null}
-            </>
-          )}
-        </div>
+        <FolderDetails
+          activeFolderId={activeFolderId}
+          folder={folder}
+          datasetCount={datasetCount}
+          setFolderIdForEditModal={setFolderIdForEditModal}
+          error={error}
+        />
       )}
     </div>
+  );
+}
+
+function getMaybeSelectMessage(datasetCount: number) {
+  return datasetCount > 0 ? "Select one to see details." : "";
+}
+
+function DatasetDetails({ selectedDataset }: { selectedDataset: APIMaybeUnimportedDataset }) {
+  const context = useDatasetCollectionContext();
+  return (
+    <>
+      <h4 style={{ wordBreak: "break-all" }}>
+        <FileOutlined style={{ marginRight: 4 }} />{" "}
+        {selectedDataset.displayName || selectedDataset.name}
+      </h4>
+      {selectedDataset.isActive && (
+        <div>
+          <span className="sidebar-label">Voxel Size & Extent</span>
+          <div className="info-tab-block" style={{ marginTop: -6 }}>
+            <table
+              style={{
+                fontSize: 14,
+              }}
+            >
+              <tbody>
+                <DatasetExtentRow dataset={selectedDataset} />
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+      {selectedDataset.description && (
+        <div style={{ marginBottom: 4 }}>
+          <span className="sidebar-label">Description</span>
+          <div>{selectedDataset.description}</div>
+        </div>
+      )}
+      <div style={{ marginBottom: 4 }}>
+        <span className="sidebar-label">Access Permissions</span>
+        <br />
+        <TeamTags dataset={selectedDataset} emptyValue="Administrators & Dataset Managers" />
+      </div>
+      <div style={{ marginBottom: 4 }}>
+        <span className="sidebar-label">Layers</span>
+        <br /> <DatasetLayerTags dataset={selectedDataset} />
+      </div>
+      {selectedDataset.isActive ? (
+        <div style={{ marginBottom: 4 }}>
+          <span className="sidebar-label">Tags</span>
+          <DatasetTags dataset={selectedDataset} updateDataset={context.updateCachedDataset} />
+        </div>
+      ) : null}
+    </>
+  );
+}
+
+function DatasetsDetails({ selectedDatasets }: { selectedDatasets: APIMaybeUnimportedDataset[] }) {
+  return (
+    <div style={{ textAlign: "center" }}>
+      Selected {selectedDatasets.length} datasets. Move them to another folder with drag and drop.
+    </div>
+  );
+}
+
+function SearchDetails({ datasetCount }: { datasetCount: number }) {
+  const maybeSelectMsg = getMaybeSelectMessage(datasetCount);
+  return (
+    <Result
+      icon={<SearchOutlined style={{ fontSize: 50 }} />}
+      subTitle={
+        datasetCount !== SEARCH_RESULTS_LIMIT ? (
+          <>
+            {datasetCount} {pluralize("dataset", datasetCount)} were found. {maybeSelectMsg}
+          </>
+        ) : (
+          <>
+            At least {SEARCH_RESULTS_LIMIT} datasets match your search criteria. {maybeSelectMsg}
+          </>
+        )
+      }
+    />
+  );
+}
+
+function FolderDetails({
+  activeFolderId,
+  folder,
+  datasetCount,
+  setFolderIdForEditModal,
+  error,
+}: {
+  activeFolderId: string | null;
+  folder: Folder | undefined;
+  datasetCount: number;
+  setFolderIdForEditModal: (id: string | null) => void;
+  error: unknown;
+}) {
+  const maybeSelectMsg = getMaybeSelectMessage(datasetCount);
+  return (
+    <>
+      {folder ? (
+        <div style={{ textAlign: "left" }}>
+          <h4 style={{ wordBreak: "break-all" }}>
+            <span
+              style={{
+                float: "right",
+                fontSize: 16,
+                marginTop: 2,
+                marginLeft: 2,
+                color: "var(--ant-text-secondary)",
+              }}
+            >
+              <SettingOutlined onClick={() => setFolderIdForEditModal(folder.id)} />
+            </span>
+            <FolderOpenOutlined style={{ marginRight: 8 }} />
+            {folder.name}
+          </h4>
+          <p>
+            This folder contains{" "}
+            <Tooltip title="This number is independent of any filters that might be applied to the current view (e.g., only showing available datasets)">
+              {datasetCount} {pluralize("dataset", datasetCount)}*
+            </Tooltip>
+            . {maybeSelectMsg}
+          </p>
+          <span className="sidebar-label">Access Permissions</span>
+          <br />
+          <FolderTeamTags folder={folder} />
+        </div>
+      ) : error ? (
+        "Could not load folder."
+      ) : activeFolderId != null ? (
+        <Spin spinning />
+      ) : null}
+    </>
   );
 }
 

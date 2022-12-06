9c70cca89ed84a08cfc5b0a6ee0ea17d1e19893f
Florian M <florian@scm.io>
2022-12-06 12:05:29 +0100

    bump schema version number

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -19,7 +19,7 @@ START TRANSACTION;
 CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(92);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(93);
 COMMIT TRANSACTION;
 
 

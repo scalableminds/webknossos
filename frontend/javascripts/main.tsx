ac9c306e549ee85487212b6011699d399bc37b4f
Philipp Otto <philipp.4096@googlemail.com>
2022-12-20 13:53:13 +0100

    remove dependency from store to root saga to remove 81 cyclic dependencies

--- frontend/javascripts/main.tsx
+++ frontend/javascripts/main.tsx
@@ -2,6 +2,9 @@ import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
 import { document } from "libs/window";
+import rootSaga from "oxalis/model/sagas/root_saga";
+import { startSagas } from "oxalis/store";
+
 import { getActiveUser, checkAnyOrganizationExists } from "admin/admin_rest_api";
 import { googleAnalyticsLogClicks } from "oxalis/model/helpers/analytics";
 import { load as loadFeatureToggles } from "features";
@@ -18,6 +21,8 @@ import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persist
 import UserLocalStorage from "libs/user_local_storage";
 import { compress, decompress } from "lz-string";
 import ErrorBoundary from "components/error_boundary";
+
+startSagas(rootSaga);
 
 const reactQueryClient = new QueryClient({
   defaultOptions: {

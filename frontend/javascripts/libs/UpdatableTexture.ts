49afeba51de9186a0e8028962ffc1f6b8d46b270
Philipp Otto <philipp.4096@googlemail.com>
2022-12-20 11:41:34 +0100

    add comment

--- frontend/javascripts/libs/UpdatableTexture.ts
+++ frontend/javascripts/libs/UpdatableTexture.ts
@@ -1,6 +1,20 @@
 import * as THREE from "three";
 import _ from "lodash";
 
+/* The UpdatableTexture class exposes a way to partially update a texture.
+ * Since we use this class for data which is usually only available in chunks,
+ * the default ThreeJS way of initializing the texture with an appropriately
+ * sized array buffer is inefficient (both allocation of array and upload to GPU).
+ * Therefore, we only allocate a dummy typed array of size 1 which mismatches
+ * the actual texture size. To avoid (benign) WebGL errors in the console,
+ * the WebGL function texSubImage2D is overridden to do nothing so that ThreeJS
+ * effectively doesn't try to upload the dummy data.
+ * This is a hacky workaround and can hopefully be removed, when/if this issue
+ * is done in ThreeJS: https://github.com/mrdoob/three.js/issues/25133
+ * In WebGL 1, we used to simply resize the texture after initialization, but
+ * this is not possible anymore, since ThreeJS uses texStorage2D now (which is
+ * also recommended).
+ */
 let originalTexSubImage2D: WebGL2RenderingContext["texSubImage2D"] | null = null;
 
 class UpdatableTexture extends THREE.Texture {
@@ -70,6 +84,7 @@ class UpdatableTexture extends THREE.Texture {
     height: number,
   ) {
     if (originalTexSubImage2D == null) {
+      // See explanation at declaration of originalTexSubImage2D.
       originalTexSubImage2D = this.gl.texSubImage2D.bind(this.gl);
       this.gl.texSubImage2D = _.noop;
     }

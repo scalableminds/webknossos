fb1ff2a1ef5a8aeaf5b22dd5c0d017beb475bc86
Tom Herold <tom.herold@scalableminds.com>
2022-12-20 10:27:50 +0100

    update schema version to 94

--- conf/evolutions/094-pricing-plans.sql
+++ conf/evolutions/094-pricing-plans.sql
@@ -1,27 +1,32 @@
-BEGIN transaction;
+
+START TRANSACTION;
+
+ALTER TABLE webknossos.organizations
+ADD paidUntil TIMESTAMPTZ DEFAULT NULL,
+ADD includedUsers INTEGER DEFAULT NULL,
+ADD includedStorage BIGINT DEFAULT NULL;
 
 -- Drop dependent views
 DROP VIEW webknossos.userinfos;
 DROP VIEW webknossos.organizations_;
 
-ALTER TABLE webknossos.organizations
-  DROP COLUMN paidUntil,
-  DROP COLUMN includedUsers,
-  DROP COLUMN includedStorage;
-
 -- Edit pricing plans enum
 ALTER TYPE webknossos.PRICING_PLANS RENAME TO prizing_plans_old;
-CREATE TYPE webknossos.PRICING_PLANS AS ENUM ('Basic', 'Premium', 'Pilot', 'Custom');
+CREATE TYPE webknossos.PRICING_PLANS AS ENUM ('Basic', 'Team', 'Power', 'Team_Trial', 'Power_Trial', 'Custom');
 ALTER TABLE webknossos.organizations
   ALTER COLUMN pricingPLan DROP DEFAULT,
   ALTER COLUMN pricingPlan TYPE webknossos.PRICING_PLANS USING
     CASE pricingPlan
       WHEN 'Basic'::webknossos.prizing_plans_old THEN 'Basic'::webknossos.PRICING_PLANS
-      WHEN 'Team'::webknossos.prizing_plans_old THEN 'Premium'::webknossos.PRICING_PLANS
+      WHEN 'Premium'::webknossos.prizing_plans_old THEN 'Team'::webknossos.PRICING_PLANS
+      WHEN 'Pilot'::webknossos.prizing_plans_old THEN 'Team'::webknossos.PRICING_PLANS
       ELSE 'Custom'::webknossos.PRICING_PLANS
-      END,
+    END,
   ALTER COLUMN pricingPlan SET DEFAULT 'Custom'::webknossos.PRICING_PLANS;
 DROP TYPE webknossos.prizing_plans_old;
+
+UPDATE webknossos.organizations SET includedUsers = 3, includedStorage = 5e10 WHERE pricingplan = 'Basic'::webknossos.PRICING_PLANS;
+UPDATE webknossos.organizations SET includedUsers = 5, includedStorage = 1e12 WHERE pricingplan = 'Team'::webknossos.PRICING_PLANS;
 
 -- Recreate views
 CREATE VIEW webknossos.organizations_ AS SELECT * FROM webknossos.organizations WHERE NOT isDeleted;
@@ -35,6 +40,6 @@ FROM webknossos.users_ u
        JOIN webknossos.organizations_ o ON u._organization = o._id
        JOIN webknossos.multiUsers_ m on u._multiUser = m._id;
 
-UPDATE webknossos.releaseInformation SET schemaVersion = 92;
+UPDATE webknossos.releaseInformation SET schemaVersion = 94;
 
-COMMIT;
+COMMIT TRANSACTION;
--- conf/evolutions/reversions/094-pricing-plans.sql
+++ conf/evolutions/reversions/094-pricing-plans.sql
@@ -1,32 +1,27 @@
-
-START TRANSACTION;
-
-ALTER TABLE webknossos.organizations
-ADD paidUntil TIMESTAMPTZ DEFAULT NULL,
-ADD includedUsers INTEGER DEFAULT NULL,
-ADD includedStorage BIGINT DEFAULT NULL;
+BEGIN transaction;
 
 -- Drop dependent views
 DROP VIEW webknossos.userinfos;
 DROP VIEW webknossos.organizations_;
 
+ALTER TABLE webknossos.organizations
+  DROP COLUMN paidUntil,
+  DROP COLUMN includedUsers,
+  DROP COLUMN includedStorage;
+
 -- Edit pricing plans enum
 ALTER TYPE webknossos.PRICING_PLANS RENAME TO prizing_plans_old;
-CREATE TYPE webknossos.PRICING_PLANS AS ENUM ('Basic', 'Team', 'Power', 'Team_Trial', 'Power_Trial', 'Custom');
+CREATE TYPE webknossos.PRICING_PLANS AS ENUM ('Basic', 'Premium', 'Pilot', 'Custom');
 ALTER TABLE webknossos.organizations
   ALTER COLUMN pricingPLan DROP DEFAULT,
   ALTER COLUMN pricingPlan TYPE webknossos.PRICING_PLANS USING
     CASE pricingPlan
       WHEN 'Basic'::webknossos.prizing_plans_old THEN 'Basic'::webknossos.PRICING_PLANS
-      WHEN 'Premium'::webknossos.prizing_plans_old THEN 'Team'::webknossos.PRICING_PLANS
-      WHEN 'Pilot'::webknossos.prizing_plans_old THEN 'Team'::webknossos.PRICING_PLANS
+      WHEN 'Team'::webknossos.prizing_plans_old THEN 'Premium'::webknossos.PRICING_PLANS
       ELSE 'Custom'::webknossos.PRICING_PLANS
-    END,
+      END,
   ALTER COLUMN pricingPlan SET DEFAULT 'Custom'::webknossos.PRICING_PLANS;
 DROP TYPE webknossos.prizing_plans_old;
-
-UPDATE webknossos.organizations SET includedUsers = 3, includedStorage = 5e10 WHERE pricingplan = 'Basic'::webknossos.PRICING_PLANS;
-UPDATE webknossos.organizations SET includedUsers = 5, includedStorage = 1e12 WHERE pricingplan = 'Team'::webknossos.PRICING_PLANS;
 
 -- Recreate views
 CREATE VIEW webknossos.organizations_ AS SELECT * FROM webknossos.organizations WHERE NOT isDeleted;
@@ -42,4 +37,4 @@ FROM webknossos.users_ u
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 93;
 
-COMMIT TRANSACTION;
+COMMIT;

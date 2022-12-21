e4380639365456a08208572b806803533c008c7f
Philipp Otto <philipp.4096@googlemail.com>
2022-12-21 15:47:27 +0100

    more type import

--- frontend/javascripts/oxalis/singletons.ts
+++ frontend/javascripts/oxalis/singletons.ts
@@ -4,18 +4,16 @@ import { type OxalisModel } from "./model";
 import { type OxalisState } from "./store";
 
 export let Store: ReduxStore<OxalisState>;
+export let Model: OxalisModel;
+export let api: ApiInterface;
 
 export const setStore = (_store: ReduxStore<OxalisState>) => {
   Store = _store;
 };
 
-export let Model: OxalisModel;
-
 export const setModel = (_model: OxalisModel) => {
   Model = _model;
 };
-
-export let api: ApiInterface;
 
 export const setApi = (_api: ApiInterface) => {
   api = _api;

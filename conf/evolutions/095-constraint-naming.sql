47ca44f9ec2b5b1c016b8501edd693f98f317889
Florian M <florian@scm.io>
2023-01-09 10:04:07 +0100

    fix typo

--- conf/evolutions/095-constraint-naming.sql
+++ conf/evolutions/095-constraint-naming.sql
@@ -1,7 +1,7 @@
 START TRANSACTION;
 
-ALTER TABLE webknossos.webknossos.voxelytics_runs DROP CONSTRAINT IF EXISTS voxelytics_runs__organization_workflow_hash_fkey;
-ALTER TABLE webknossos.webknossos.voxelytics_runs DROP CONSTRAINT IF EXISTS voxelytics_runs__organization_fkey1;
+ALTER TABLE webknossos.voxelytics_runs DROP CONSTRAINT IF EXISTS voxelytics_runs__organization_workflow_hash_fkey;
+ALTER TABLE webknossos.voxelytics_runs DROP CONSTRAINT IF EXISTS voxelytics_runs__organization_fkey1;
 
 ALTER TABLE webknossos.voxelytics_runs ADD CONSTRAINT voxelytics_runs__organization_workflow_hash_fkey FOREIGN KEY (_organization, workflow_hash) REFERENCES webknossos.voxelytics_workflows(_organization, hash) ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE;
 

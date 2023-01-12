808b51f61792fa3a4297654ad2560a0749d2f875
Florian M <florian@scm.io>
2023-01-12 09:26:36 +0100

    Verbose error logging for storage scan failures

--- app/models/storage/UsedStorageService.scala
+++ app/models/storage/UsedStorageService.scala
@@ -10,8 +10,9 @@ import com.scalableminds.webknossos.datastore.services.DirectoryStorageReport
 import com.typesafe.scalalogging.LazyLogging
 import models.binary.{DataSet, DataSetService, DataStore, DataStoreDAO, WKRemoteDataStoreClient}
 import models.organization.{Organization, OrganizationDAO}
+import net.liftweb.common.{Failure, Full}
 import play.api.inject.ApplicationLifecycle
-import utils.WkConf
+import utils.{ObjectId, WkConf}
 
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
@@ -28,16 +29,17 @@ class UsedStorageService @Inject()(val system: ActorSystem,
     with IntervalScheduler {
 
   /* Note that not every tick here will scan something, there is additional logic below:
-     Every tick, et most 10 organizations are scanned. But only if their last full scan is was sufficiently long ago
-     The 10 organizations with the most outdated scan are selected each tick. This is to distribute the load.
+     Every tick, et most scansPerTick organizations are scanned.
+     But only if their last full scan is was sufficiently long ago
+     The organizations with the most outdated scan are selected each tick. This is to distribute the load.
    */
-  override protected def tickerInterval: FiniteDuration = 10 minutes
+  override protected def tickerInterval: FiniteDuration = config.WebKnossos.FetchUsedStorage.tickerInterval
   override protected def tickerInitialDelay: FiniteDuration = 1 minute
 
   private val isRunning = new java.util.concurrent.atomic.AtomicBoolean(false)
 
   private val pauseAfterEachOrganization = 5 seconds
-  private val organizationCountToScanPerTick = 10
+  private val organizationCountToScanPerTick = config.WebKnossos.FetchUsedStorage.scansPerTick
 
   implicit private val ctx: DBAccessContext = GlobalAccessContext
 
@@ -50,21 +52,33 @@ class UsedStorageService @Inject()(val system: ActorSystem,
 
   private def tickAsync(): Fox[Unit] =
     for {
-      organizations <- organizationDAO.findNotRecentlyScanned(config.WebKnossos.FetchUsedStorage.interval,
+      organizations <- organizationDAO.findNotRecentlyScanned(config.WebKnossos.FetchUsedStorage.rescanInterval,
                                                               organizationCountToScanPerTick)
       dataStores <- dataStoreDAO.findAllWithStorageReporting
-      _ = logger.info(s"Scanning ${organizations.length} organizations in ${dataStores.length} datastores...")
-      _ <- Fox.serialCombined(organizations)(organization => refreshStorageReports(organization, dataStores))
+      _ = logger.info(s"Scanning used storage for ${organizations.length} organizations (${organizations
+        .map(_._id)}) in ${dataStores.length} datastores (${dataStores.map(_.name)})...")
+      _ <- Fox.serialCombined(organizations)(organization =>
+        tryAndLog(organization._id, refreshStorageReports(organization, dataStores)))
+    } yield ()
+
+  private def tryAndLog(organizationId: ObjectId, result: Fox[Unit]): Fox[Unit] =
+    for {
+      box <- result.futureBox
+      _ = box match {
+        case Full(_)    => ()
+        case f: Failure => logger.error(f"Error during storage scan for organization with id $organizationId: $f")
+        case _          => logger.error(f"Error during storage scan for organization with id $organizationId: Empty")
+      }
     } yield ()
 
   private def refreshStorageReports(organization: Organization, dataStores: List[DataStore]): Fox[Unit] =
     for {
       storageReportsByDataStore <- Fox.serialCombined(dataStores)(dataStore =>
-        refreshStorageReports(dataStore, organization))
-      _ <- organizationDAO.deleteUsedStorage(organization._id)
+        refreshStorageReports(dataStore, organization)) ?~> "Failed to fetch used storage reports"
+      _ <- organizationDAO.deleteUsedStorage(organization._id) ?~> "Failed to delete outdated used storage entries"
       _ <- Fox.serialCombined(storageReportsByDataStore.zip(dataStores))(storageForDatastore =>
-        upsertUsedStorage(organization, storageForDatastore))
-      _ <- organizationDAO.updateLastStorageScanTime(organization._id, Instant.now)
+        upsertUsedStorage(organization, storageForDatastore)) ?~> "Failed to upsert used storage reports into db"
+      _ <- organizationDAO.updateLastStorageScanTime(organization._id, Instant.now) ?~> "Failed to update last storage scan time in db"
       _ = Thread.sleep(pauseAfterEachOrganization.toMillis)
     } yield ()
 

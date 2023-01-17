31fc6515ebfaf300274e79f6689fd4017b27e1d2
Norman Rzepka <code@normanrz.com>
2023-01-17 14:01:46 +0100

    schema

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -19,7 +19,7 @@ START TRANSACTION;
 CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(97);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(98);
 COMMIT TRANSACTION;
 
 

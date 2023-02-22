f45413fbe3f05bc6d661f95874c6cf43aaceedf6
Philipp Otto <philipp.4096@gmail.com>
2023-02-22 15:18:48 +0100

    update glsl-parser and remove hex workaround

--- frontend/javascripts/oxalis/shaders/hashing.glsl.ts
+++ frontend/javascripts/oxalis/shaders/hashing.glsl.ts
@@ -4,20 +4,12 @@ export const hashCombine: ShaderModule = {
   requirements: [],
   code: `
     highp uint hashCombine(highp uint state, highp uint value) {
-      // The used constants are written in decimal, because
-      // the parser tests don't support unsigned int hex notation
-      // (yet).
-      // See this issue: https://github.com/ShaderFrog/glsl-parser/issues/1
-      // 3432918353u == 0xcc9e2d51u
-      //  461845907u == 0x1b873593u
-      // 3864292196u == 0xe6546b64u
-
-      value *= 3432918353u;
+      value *= 0xcc9e2d51u;
       value = (value << 15u) | (value >> 17u);
-      value *= 461845907u;
+      value *= 0x1b873593u;
       state ^= value;
       state = (state << 13u) | (state >> 19u);
-      state = (state * 5u) + 3864292196u;
+      state = (state * 5u) + 0xe6546b64u;
       return state;
     }
 `,

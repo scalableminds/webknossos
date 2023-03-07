ccec35a85c9d0ed48d0d09acb0ae3c7e114def31
frcroth <frcroth@gmail.com>
2023-03-07 10:26:45 +0100

    Fix and expand test for data vault

---
+++ test/backend/DataVaultTestSuite.scala
@@ -0,0 +1,70 @@
+package backend
+
+import akka.actor.ProviderSelection.Remote
+import com.google.cloud.storage.StorageOptions
+import com.google.cloud.storage.contrib.nio.{CloudStorageConfiguration, CloudStorageFileSystem}
+import com.scalableminds.webknossos.datastore.storage.httpsfilesystem.{HttpsFileSystem, HttpsPath}
+import org.scalatestplus.play.PlaySpec
+
+import java.net.URI
+import com.scalableminds.webknossos.datastore.datareaders.FileSystemStore
+import com.scalableminds.webknossos.datastore.datavault.{GoogleCloudDataVault, HttpsDataVault}
+import com.scalableminds.webknossos.datastore.storage.RemoteSourceDescriptor
+
+class DataVaultTestSuite extends PlaySpec {
+
+  "Data vault" when {
+    "using Range requests" when {
+      val range = 0 to 1023
+      val dataKey = "32_32_40/15360-15424_8384-8448_3520-3584" // when accessed via range request, the response body is 1024 bytes long, otherwise 124.8 KB
+
+      "with HTTP Vault" should {
+        "return correct response" in {
+          val uri = new URI("http://storage.googleapis.com/")
+          val vaultPath = HttpsDataVault.create(RemoteSourceDescriptor(uri, None))
+          val bytes = vaultPath.get(s"/neuroglancer-fafb-data/fafb_v14/fafb_v14_orig/$dataKey", Some(range))
+
+          assert(bytes.length == range.length)
+          assert(bytes.take(10).sameElements(Array(-1, -40, -1, -32, 0, 16, 74, 70, 73, 70)))
+        }
+      }
+
+      "with Google Cloud Storage Vault" should {
+        "return correct response" in {
+          val uri = new URI("gs://neuroglancer-fafb-data/fafb_v14/fafb_v14_orig")
+          val vaultPath = GoogleCloudDataVault.create(RemoteSourceDescriptor(uri, None))
+          val bytes = vaultPath.get(s"fafb_v14_orig/$dataKey", Some(range))
+
+          assert(bytes.length == range.length)
+          assert(bytes.take(10).sameElements(Array(-1, -40, -1, -32, 0, 16, 74, 70, 73, 70)))
+        }
+      }
+    }
+    "using regular requests" when {
+      val dataKey = "32_32_40/15360-15424_8384-8448_3520-3584"
+      val dataLength = 127808
+
+      "with HTTP Vault" should {
+        "return correct response" in {
+          val uri = new URI("http://storage.googleapis.com/")
+          val vaultPath = HttpsDataVault.create(RemoteSourceDescriptor(uri, None))
+          val bytes = vaultPath.get(s"/neuroglancer-fafb-data/fafb_v14/fafb_v14_orig/$dataKey")
+
+          assert(bytes.length == dataLength)
+          assert(bytes.take(10).sameElements(Array(-1, -40, -1, -32, 0, 16, 74, 70, 73, 70)))
+        }
+      }
+
+      "with Google Cloud Storage Vault" should {
+        "return correct response" in {
+          val uri = new URI("gs://neuroglancer-fafb-data/fafb_v14/fafb_v14_orig")
+          val vaultPath = GoogleCloudDataVault.create(RemoteSourceDescriptor(uri, None))
+          val bytes = vaultPath.get(s"fafb_v14_orig/$dataKey")
+
+          assert(bytes.length == dataLength)
+          assert(bytes.take(10).sameElements(Array(-1, -40, -1, -32, 0, 16, 74, 70, 73, 70)))
+        }
+      }
+    }
+  }
+}
--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/datavault/GoogleCloudDataVault.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/datavault/GoogleCloudDataVault.scala
@@ -8,8 +8,7 @@ import java.io.ByteArrayInputStream
 import java.net.URI
 import java.nio.ByteBuffer
 
-class GoogleCloudDataVault(uri: URI, credential: Option[GoogleServiceAccountCredential])
-    extends DataVault {
+class GoogleCloudDataVault(uri: URI, credential: Option[GoogleServiceAccountCredential]) extends DataVault {
 
   lazy val storageOptions: StorageOptions = credential match {
     case Some(credential: GoogleServiceAccountCredential) =>
@@ -41,6 +40,7 @@ class GoogleCloudDataVault(uri: URI, credential: Option[GoogleServiceAccountCred
         val bb = ByteBuffer.allocateDirect(r.length)
         blobReader.read(bb)
         val arr = new Array[Byte](r.length)
+        bb.position(0)
         bb.get(arr)
         arr
       }
@@ -54,7 +54,7 @@ object GoogleCloudDataVault {
   def create(remoteSourceDescriptor: RemoteSourceDescriptor): VaultPath = {
     val credential = remoteSourceDescriptor.credential.map(f => f.asInstanceOf[GoogleServiceAccountCredential])
     new VaultPath(remoteSourceDescriptor.uri,
-                   new GoogleCloudDataVault(remoteSourceDescriptor.uri, credential),
-                   credential)
+                  new GoogleCloudDataVault(remoteSourceDescriptor.uri, credential),
+                  credential)
   }
 }

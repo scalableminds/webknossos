39bd0781fc6250face86cbcf773d5277b9d134a1
frcroth <frcroth@gmail.com>
2023-03-20 17:56:49 +0100

    Add comments

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/precomputed/PrecomputedArray.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/precomputed/PrecomputedArray.scala
@@ -78,12 +78,14 @@ class PrecomputedArray(relativePath: DatasetPath,
   }
 
   // SHARDING
+  // Implemented according to https://github.com/google/neuroglancer/blob/master/src/neuroglancer/datasource/precomputed/sharded.md,
+  // directly adapted from https://github.com/scalableminds/webknossos-connect/blob/master/wkconnect/backends/neuroglancer/sharding.py.
 
   private val shardIndexCache: AlfuFoxCache[VaultPath, Array[Byte]] =
-    AlfuFoxCache(maxEntries = 100)
+    AlfuFoxCache()
 
   private val minishardIndexCache: AlfuFoxCache[(VaultPath, Int), Seq[(Long, Long, Long)]] =
-    AlfuFoxCache(maxEntries = 100)
+    AlfuFoxCache()
 
   private def getHashForChunk(chunkIndex: Array[Int]): Long =
     CompressedMortonCode.encode(chunkIndex, header.gridSize)
@@ -123,7 +125,7 @@ class PrecomputedArray(relativePath: DatasetPath,
     Range.Long(0, end, 1)
   }
 
-  private def getShardIndex(shardPath: VaultPath)(implicit ec: ExecutionContext) =
+  private def getShardIndex(shardPath: VaultPath)(implicit ec: ExecutionContext): Fox[Array[Byte]] =
     shardIndexCache.getOrLoad(shardPath, readShardIndex)
 
   private def readShardIndex(shardPath: VaultPath)(implicit ec: ExecutionContext): Fox[Array[Byte]] =
@@ -131,9 +133,10 @@ class PrecomputedArray(relativePath: DatasetPath,
       bytes <- Fox.option2Fox(shardPath.readBytes(Some(shardIndexRange)))
     } yield bytes
 
-  private def parseShardIndex(index: Array[Byte]) =
+  private def parseShardIndex(index: Array[Byte]): Seq[(Long, Long)] =
+    // See https://github.com/google/neuroglancer/blob/master/src/neuroglancer/datasource/precomputed/sharded.md#shard-index-format
     index
-      .grouped(16)
+      .grouped(16) // 16 Bytes: 2 uint64 numbers: start_offset, end_offset
       .map((bytes: Array[Byte]) => {
         (BigInt(bytes.take(8).reverse).toLong, BigInt(bytes.slice(8, 16).reverse).toLong) // bytes reversed because they are stored little endian
       })
@@ -153,10 +156,10 @@ class PrecomputedArray(relativePath: DatasetPath,
   private def getPathForShard(shardNumber: Long): VaultPath = {
     val shardBits = header.precomputedScale.sharding.map(_.shard_bits.toFloat).getOrElse(0f)
     if (shardBits == 0) {
-      vaultPath / s"${relativePath.storeKey}/" / "0.shard"
+      vaultPath / relativePath.storeKey / "0.shard"
     } else {
       val shardString = String.format(s"%1$$${(shardBits / 4).ceil.toInt}s", shardNumber.toHexString).replace(' ', '0')
-      vaultPath / s"${relativePath.storeKey}/" / s"$shardString.shard"
+      vaultPath / relativePath.storeKey / s"$shardString.shard"
     }
 
   }
@@ -170,19 +173,39 @@ class PrecomputedArray(relativePath: DatasetPath,
 
   private def parseMinishardIndex(bytes: Array[Byte]): Seq[(Long, Long, Long)] = {
     // Because readBytes already decodes gzip, we don't need to decompress here
+    /*
+     From: https://github.com/google/neuroglancer/blob/master/src/neuroglancer/datasource/precomputed/sharded.md#minishard-index-format
+     The decoded "minishard index" is a binary string of 24*n bytes, specifying a contiguous C-order array of [3, n]
+      uint64le values.
+     */
     val n = bytes.length / 24
     val buf = ByteBuffer.allocate(bytes.length)
     buf.put(bytes)
+
     val longArray = new Array[Long](n * 3)
     buf.position(0)
     buf.order(ByteOrder.LITTLE_ENDIAN)
     buf.asLongBuffer().get(longArray)
     // longArray is row major / C-order
+    /*
+     From: https://github.com/google/neuroglancer/blob/master/src/neuroglancer/datasource/precomputed/sharded.md#minishard-index-format
+     Values array[0, 0], ..., array[0, n-1] specify the chunk IDs in the minishard, and are delta encoded, such that
+     array[0, 0] is equal to the ID of the first chunk, and the ID of chunk i is equal to the sum
+     of array[0, 0], ..., array[0, i].
+     */
     val chunkIds = new Array[Long](n)
     chunkIds(0) = longArray(0)
     for (i <- 1 until n) {
       chunkIds(i) = longArray(i) + chunkIds(i - 1)
     }
+    /*
+     From: https://github.com/google/neuroglancer/blob/master/src/neuroglancer/datasource/precomputed/sharded.md#minishard-index-format
+     The size of the data for chunk i is stored as array[2, i].
+     Values array[1, 0], ..., array[1, n-1] specify the starting offsets in the shard file of the data corresponding to
+     each chunk, and are also delta encoded relative to the end of the prior chunk, such that the starting offset of the
+     first chunk is equal to shard_index_end + array[1, 0], and the starting offset of chunk i is the sum of
+     shard_index_end + array[1, 0], ..., array[1, i] and array[2, 0], ..., array[2, i-1].
+     */
     val chunkSizes = longArray.slice(2 * n, 3 * n)
     val chunkStartOffsets = new Array[Long](n)
     chunkStartOffsets(0) = longArray(n)
@@ -193,7 +216,8 @@ class PrecomputedArray(relativePath: DatasetPath,
     (chunkIds, chunkStartOffsets, chunkSizes).zipped.map((a, b, c) => (a, b, c))
   }
 
-  private def getMinishardIndex(shardPath: VaultPath, minishardNumber: Int)(implicit ec: ExecutionContext) =
+  private def getMinishardIndex(shardPath: VaultPath, minishardNumber: Int)(
+      implicit ec: ExecutionContext): Fox[Seq[(Long, Long, Long)]] =
     minishardIndexCache.getOrLoad((shardPath, minishardNumber), readMinishardIndex)
 
   private def readMinishardIndex(vaultPathAndMinishardNumber: (VaultPath, Int))(

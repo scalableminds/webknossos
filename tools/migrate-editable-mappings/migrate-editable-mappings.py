42b07991f4e0abb5e7d78c5f84003a4537a9aa07
Florian M <florian@scm.io>
2023-04-12 16:43:57 +0200

    cleanup logging

--- tools/migrate-editable-mappings/migrate-editable-mappings.py
+++ tools/migrate-editable-mappings/migrate-editable-mappings.py
@@ -33,7 +33,10 @@ list_keys_batch_size = 100
 segment_to_agglomerate_chunk_size = 64 * 1024 # max 1MB chunks (two 8-byte numbers per element) # must match the value in new wk code
 persisted_version_interval = 20
 
+put_count = 0
+
 def main():
+    global put_count
     parser = argparse.ArgumentParser()
     parser.add_argument("fossil_host", help="example: localhost:7155")
     parser.add_argument("-v", "--verbose", action="store_true")
@@ -49,8 +52,6 @@ def main():
 
     test_health(stub, f"fossildb at {args.fossil_host}")
 
-    put_count = 0
-
     previous_listed_key = None
     while True:
         list_keys_reply = stub.ListKeys(proto.ListKeysRequest(collection=collection_editable_mappings, limit=list_keys_batch_size, startAfterKey=previous_listed_key))
@@ -62,7 +63,7 @@ def main():
             get_reply = stub.Get(proto.GetRequest(collection=collection_editable_mappings, key=key, version=None))
             assert_success(get_reply)
 
-            convert_and_save(key, get_reply.actualVersion, get_reply.value, stub, put_count, args)
+            convert_and_save(key, get_reply.actualVersion, get_reply.value, stub, args)
 
             if migrate_history:
                 next_version = get_reply.actualVersion - persisted_version_interval
@@ -71,16 +72,17 @@ def main():
                     print(f"Getting {key} v{next_version}...")
                     get_reply = stub.Get(proto.GetRequest(collection=collection_editable_mappings, key=key, version=next_version))
                     assert_success(get_reply)
-                    convert_and_save(key, get_reply.actualVersion, get_reply.value, stub, put_count, args)
+                    convert_and_save(key, get_reply.actualVersion, get_reply.value, stub, args)
                     next_version = get_reply.actualVersion - persisted_version_interval
 
         previous_listed_key = list_keys_reply.keys[-1]
 
-    print(f"Done after {timer() - start_time}. Total put count:", put_count)
+    print(f"Done after {ms(timer() - start_time)}. Total put count:", put_count)
 
 
-def convert_and_save(key, version, editable_mapping_bytes, stub, put_count, args):
-    print(f"For {key} got version {version} ({len(editable_mapping_bytes)} bytes) at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}...")
+def convert_and_save(key, version, editable_mapping_bytes, stub, args):
+    global put_count
+    print(f"  For {key} got v{version} ({len(editable_mapping_bytes)} bytes) at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}...")
     editable_mapping = EditableMapping_pb2.EditableMappingProto()
     editable_mapping.ParseFromString(editable_mapping_bytes)
     segmentToAgglomerate = editable_mapping.segmentToAgglomerate
@@ -88,7 +90,7 @@ def convert_and_save(key, version, editable_mapping_bytes, stub, put_count, args
 
     t5 = timer()
     chunks = {}
-    for pair in editableMapping.segmentToAgglomerate:
+    for pair in editable_mapping.segmentToAgglomerate:
         pair_converted = SegmentToAgglomerateProto_pb2.SegmentAgglomeratePair()
         pair_converted.segmentId = pair.segmentId
         pair_converted.agglomerateId = pair.agglomerateId
@@ -98,12 +100,12 @@ def convert_and_save(key, version, editable_mapping_bytes, stub, put_count, args
         chunks[chunk_id].segmentToAgglomerate.append(pair_converted)
     t6 = timer()
     if args.verbose:
-        print(f"grouping chunks {t6 - t5}")
+        print(f"  grouping chunks took {ms(t6 - t5)}")
     for chunk_id, chunk in chunks.items():
         chunk_key = f"{key}/{chunk_id}"
         chunkBytes = chunk.SerializeToString()
         if args.verbose:
-            print(f"segment to agglomerate chunk with {len(chunk.segmentToAgglomerate)} pairs, to be saved at {chunk_key} v{version}")
+            print(f"    segment to agglomerate chunk with {len(chunk.segmentToAgglomerate)} pairs, to be saved at {chunk_key} v{version}")
         if args.do_write:
             put_reply = stub.Put(proto.PutRequest(collection=collection_segment_to_agglomerate, key=chunk_key, version=version, value=chunkBytes))
             assert_success(put_reply)
@@ -117,14 +119,14 @@ def convert_and_save(key, version, editable_mapping_bytes, stub, put_count, args
         agglomerateToGraphKey = f"{key}/{agglomerateId}"
         graphBytes = graph.SerializeToString()
         if args.verbose:
-            print(f"agglomerate {agglomerateId} has graph with {len(graph.edges)} edges, to be saved at {agglomerateToGraphKey} v{version}")
+            print(f"    agglomerate {agglomerateId} has graph with {len(graph.edges)} edges, to be saved at {agglomerateToGraphKey} v{version}")
         if args.do_write:
             put_reply = stub.Put(proto.PutRequest(collection=collection_agglomerate_to_graph, key=agglomerateToGraphKey, version=version, value=graphBytes))
             assert_success(put_reply)
             put_count += 1
 
     if args.verbose:
-        print(f"converting + putting graphs: {timer() - t7}")
+        print(f"  converting + putting graphs took {ms(timer() - t7)}")
 
     editable_mapping_info = EditableMappingInfo_pb2.EditableMappingInfo()
     editable_mapping_info.baseMappingName = editable_mapping.baseMappingName
@@ -132,21 +134,23 @@ def convert_and_save(key, version, editable_mapping_bytes, stub, put_count, args
     editable_mapping_info.largestAgglomerateId = largest_agglomerate_id
 
     if args.verbose:
-        print(f"EditableMappingInfo with largest agglomerate id {editable_mapping_info.largestAgglomerateId}, to be saved at {key} v{version}")
+        print(f"  EditableMappingInfo with largest agglomerate id {editable_mapping_info.largestAgglomerateId}, to be saved at {key} v{version}")
     info_bytes = editable_mapping_info.SerializeToString()
     if args.do_write:
         put_reply = stub.Put(proto.PutRequest(collection=collection_editable_mappings_info, key=key, version=version, value=info_bytes))
         assert_success(put_reply)
         put_count += 1
 
+def ms(time_diff):
+    return f"{int(time_diff*1000)} ms"
 
 def test_health(stub, label):
     try:
         reply = stub.Health(proto.HealthRequest())
         assert_success(reply)
-        print('successfully connected to ' + label)
+        print('Successfully connected to ' + label)
     except Exception as e:
-        print('failed to connect to ' + label + ': ' + str(e))
+        print('Failed to connect to ' + label + ': ' + str(e))
         sys.exit(1)
 
 def assert_success(reply):

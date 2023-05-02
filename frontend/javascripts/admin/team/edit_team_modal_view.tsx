6aea6f4ce70874ca2f6cb1221fefee709aeee296
Charlie <charliemeister@proton.me>
2023-05-02 13:20:06 +0200

    add and remove functionality

--- frontend/javascripts/admin/team/edit_team_modal_view.tsx
+++ frontend/javascripts/admin/team/edit_team_modal_view.tsx
@@ -1,48 +1,52 @@
-// import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
 import { getEditableUsers, updateUser } from "admin/admin_rest_api";
-import { Modal, Form, AutoComplete, Input, Button } from "antd";
+import { Modal, AutoComplete, Input } from "antd";
 import { DefaultOptionType } from "antd/lib/select";
-import { useFetch } from "libs/react_helpers";
 import * as React from "react";
-import { APITeam, APIUser } from "types/api_flow_types";
+import { useEffect, useState } from "react";
+import { APITeam, APITeamMembership, APIUser } from "types/api_flow_types";
 
 type Props = {
-  //   onOk: (...args: Array<any>) => any;
   onCancel: (...args: Array<any>) => any;
   isOpen: boolean;
   team: APITeam | null;
 };
 
 function EditTeamModalForm({ onCancel, isOpen, team }: Props) {
-  //onOk: onOkCallback, onCancel,
-  const [form] = Form.useForm();
-  const users = useFetch(getEditableUsers, [], []);
+  const [autoCompleteValue, setAutoCompleteValue] = useState("");
+  const onChange = (newValue: string) => setAutoCompleteValue(newValue);
+  const [users, setUsers] = useState<APIUser[]>([]); //encode that its loading TODO
+  const fetchUsers = async () => setUsers(await getEditableUsers());
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   if (team === null) return null;
-  const filterFunc = (user: APIUser) => {
+  const filterUsersInCurrentTeam = (user: APIUser) => {
     return user.teams.map((t) => t.id).includes(team.id);
-  }; //rename me
-  const addTo = (user: APIUser, team: APITeam | null) => {
-    const newTeams = [...user.teams, team];
+  };
+  const updateTeamMembership = async (user: APIUser, newTeams: APITeamMembership[]) => {
     const newUser = Object.assign({}, user, {
       teams: newTeams,
     });
-    updateUser(newUser).then(
-      (serverUser) => Promise.resolve(serverUser),
-      () => Promise.reject(user),
-    );
+    const serverUser = await updateUser(newUser);
+    setUsers(users.map((oldUser) => (oldUser.id === serverUser.id ? serverUser : oldUser)));
   };
-  const removeFrom = (user: APIUser, team: APITeam | null) => {
+
+  const addTo = async (user: APIUser, team: APITeam | null) => {
+    if (team === null) return;
+    const newTeam: APITeamMembership = { id: team.id, name: team.name, isTeamManager: false }; //never make manager?
+    updateTeamMembership(user, [...user.teams, newTeam]);
+  };
+
+  const removeFrom = async (user: APIUser, team: APITeam | null) => {
     const newTeams = user.teams.filter((userteam) => team?.id !== userteam.id);
-    const newUser = Object.assign({}, user, {
-      teams: newTeams,
-    });
-    updateUser(newUser).then(
-      (serverUser) => Promise.resolve(serverUser),
-      () => Promise.reject(user),
-    );
+    updateTeamMembership(user, newTeams);
   };
+
   const renderTeamMember = (user: APIUser, team: APITeam | null): DefaultOptionType => ({
-    value: `${user.firstName} ${user.lastName}.${user.id}`,
+    // TODO make whole span clickable
+    value: `${user.firstName} ${user.lastName} ${user.email}`,
     label: (
       <div
         style={{
@@ -52,14 +56,14 @@ function EditTeamModalForm({ onCancel, isOpen, team }: Props) {
       >
         {user.firstName} {user.lastName}
         <span>
-          <Button onClick={() => removeFrom(user, team)} /> Remove from {team?.name}
+          <MinusOutlined onClick={() => removeFrom(user, team)} /> Remove from {team?.name}
         </span>
       </div>
     ),
   });
 
   const renderUserNotInTeam = (user: APIUser, team: APITeam | null): DefaultOptionType => ({
-    value: `${user.firstName} ${user.lastName}.${user.id}`,
+    value: `${user.firstName} ${user.lastName} ${user.email}`,
     label: (
       <div
         style={{
@@ -69,7 +73,7 @@ function EditTeamModalForm({ onCancel, isOpen, team }: Props) {
       >
         {user.firstName} {user.lastName}
         <span>
-          <Button onClick={() => addTo(user, team)} /> Add to {team?.name}
+          <PlusOutlined onClick={() => addTo(user, team)} /> Add to {team?.name}
         </span>
       </div>
     ),
@@ -78,42 +82,35 @@ function EditTeamModalForm({ onCancel, isOpen, team }: Props) {
   const options = [
     {
       label: "In team",
-      options: users.filter(filterFunc).map((user) => renderTeamMember(user, team)),
+      options: users.filter(filterUsersInCurrentTeam).map((user) => renderTeamMember(user, team)),
     },
     {
       label: "Not in team",
       options: users
-        .filter((user) => !filterFunc(user))
+        .filter((user) => !filterUsersInCurrentTeam(user))
         .map((user) => renderUserNotInTeam(user, team)),
     },
   ];
   return (
-    <>
-      <Modal
-        open={isOpen}
-        onCancel={onCancel}
-        title="Add / Remove Users"
-        okText="Ok"
-        className="edit-team-modal"
+    <Modal open={isOpen} onCancel={onCancel} title="Add / Remove Users" className="edit-team-modal">
+      <AutoComplete
+        style={{ width: "100%" }}
+        options={options}
+        filterOption={(inputValue, option) => {
+          return (
+            inputValue === "" ||
+            (typeof option?.value === "string" &&
+              option!.value?.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1)
+          );
+        }}
+        autoFocus
+        onSelect={() => setAutoCompleteValue("")}
+        value={autoCompleteValue}
+        onChange={onChange}
       >
-        <Form layout="vertical" form={form}>
-          <AutoComplete
-            style={{ width: "100%" }}
-            options={options}
-            filterOption={(inputValue, option) => {
-              //debugger;
-              return (
-                typeof option?.value === "string" &&
-                option!.value?.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
-              );
-            }}
-            autoFocus
-          >
-            <Input.Search size="large" placeholder="Search users" />
-          </AutoComplete>
-        </Form>
-      </Modal>
-    </>
+        <Input.Search size="large" placeholder="Search users" />
+      </AutoComplete>
+    </Modal>
   );
 }
 

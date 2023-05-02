327fcd96292efd464c36780d8a1dbe9c4e3899c9
Philipp Otto <philipp.4096@gmail.com>
2023-05-02 18:07:27 +0200

    clean up

--- frontend/javascripts/libs/draco.ts
+++ frontend/javascripts/libs/draco.ts
@@ -5,7 +5,7 @@ let _dracoLoader: CustomDRACOLoader | null;
 
 class CustomDRACOLoader extends DRACOLoader {
   // Subclass to create a promise-based API and add typing
-  decodeDracoFileAsync = (buffer: ArrayBuffer, ...args: any[]): Promise<BufferGeometry> =>
+  decodeDracoFileAsync = (buffer: ArrayBuffer): Promise<BufferGeometry> =>
     new Promise((resolve, reject) => {
       if (_dracoLoader == null) {
         throw new Error("DracoLoader not instantiated.");

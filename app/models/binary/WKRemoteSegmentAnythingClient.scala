fd8594792568cd7557e5bc5d0288a8f236dce5a8
Florian M <florian@scm.io>
2023-05-16 14:00:00 +0200

    use 4 bytes each for min/max of intensity range, not 8

--- app/models/binary/WKRemoteSegmentAnythingClient.scala
+++ app/models/binary/WKRemoteSegmentAnythingClient.scala
@@ -11,14 +11,14 @@ import javax.inject.Inject
 class WKRemoteSegmentAnythingClient @Inject()(rpc: RPC, conf: WkConf) {
   def getEmbedding(imageData: Array[Byte],
                    elementClass: ElementClass.Value,
-                   intensityMin: Option[Double],
-                   intensityMax: Option[Double]): Fox[Array[Byte]] = {
-    val metadataLengthInBytes = 1 + 1 + 8 + 8
+                   intensityMin: Option[Float],
+                   intensityMax: Option[Float]): Fox[Array[Byte]] = {
+    val metadataLengthInBytes = 1 + 1 + 4 + 4
     val buffer = ByteBuffer.allocate(metadataLengthInBytes + imageData.length)
     buffer.put(ElementClass.encodeAsByte(elementClass))
     buffer.put(if (intensityMin.isDefined && intensityMax.isDefined) 1.toByte else 0.toByte)
-    buffer.putDouble(intensityMin.getOrElse(0.0))
-    buffer.putDouble(intensityMax.getOrElse(0.0))
+    buffer.putFloat(intensityMin.getOrElse(0.0f))
+    buffer.putFloat(intensityMax.getOrElse(0.0f))
     val array = buffer.array()
     System.arraycopy(imageData, 0, array, metadataLengthInBytes, imageData.length)
     rpc(s"${conf.SegmentAnything.uri}/predictions/sam_vit_l")

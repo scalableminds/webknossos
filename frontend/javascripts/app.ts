4a6742636b4063c6933abf221dd2b802973aab0b
Philipp Otto <philipp.4096@gmail.com>
2023-06-01 16:04:36 +0200

    add comment for events in app.vent

--- frontend/javascripts/app.ts
+++ frontend/javascripts/app.ts
@@ -2,6 +2,13 @@ import window from "libs/window";
 import { createNanoEvents } from "nanoevents";
 
 class OxalisApplication {
+  // This event emitter is currently only used for two types of events:
+  // 1) webknossos:ready
+  //    When WK is done with initialization, the front-end API can be constructed.
+  // 2) rerender
+  //    Most of the time, rendering happens when something in the Store changes. However,
+  //    sometimes the store is not updated, but something in the ThreeJS scene is. In these
+  //    cases an explicit rerender can be triggered with this event.
   vent = createNanoEvents();
 }
 

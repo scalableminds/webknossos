a5cf58d34d5d3d9d2c490cf7c9c9660c396a8961
Philipp Otto <philipp.4096@gmail.com>
2023-06-06 16:08:51 +0200

    add spec for tps code

--- frontend/javascripts/main.tsx
+++ frontend/javascripts/main.tsx
@@ -25,9 +25,6 @@ import { setStore, setModel } from "oxalis/singletons";
 import Model from "oxalis/model";
 import { setupApi } from "oxalis/api/internal_api";
 import { setActiveOrganizationAction } from "oxalis/model/actions/organization_actions";
-import { tps } from "libs/thin_plate_spline";
-
-tps();
 
 setModel(Model);
 setStore(UnthrottledStore);

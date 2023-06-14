53f54c208fd463870e0271ddcbad9096256bdfda
Tom Herold <tom.herold@scalableminds.com>
2023-06-14 13:05:08 +0200

    fix linting

--- frontend/javascripts/oxalis/view/right-border-tabs/dataset_info_tab_view.tsx
+++ frontend/javascripts/oxalis/view/right-border-tabs/dataset_info_tab_view.tsx
@@ -1,5 +1,5 @@
 import type { Dispatch } from "redux";
-import { Tooltip, Button, Typography, Tag } from "antd";
+import { Tooltip, Typography, Tag } from "antd";
 import { SettingOutlined, InfoCircleOutlined, EditOutlined } from "@ant-design/icons";
 import { connect } from "react-redux";
 // @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'reac... Remove this comment to see the full error message

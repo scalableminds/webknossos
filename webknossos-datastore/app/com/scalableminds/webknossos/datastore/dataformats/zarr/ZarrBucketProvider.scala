bdc2828c7cdfe33ed36eb30df64df6fb0f741237
Florian M <fm3@users.noreply.github.com>
2023-06-19 14:18:38 +0200

    Second try for “Async IO for HttpsDataVault, Fox Error Handling” (#7155)
    * Revert "Revert "Async IO for HttpsDataVault, Fox Error Handling (#7137)" (#7154)"
    
    This reverts commit 723e207654cfac76ce46f6000236c7fe80205176.
    
    * separate RemoteSourceDescriptorService and DataVaultService
    
    * remove unused imports

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/dataformats/zarr/ZarrBucketProvider.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/dataformats/zarr/ZarrBucketProvider.scala
@@ -2,17 +2,16 @@ package com.scalableminds.webknossos.datastore.dataformats.zarr
 
 import com.scalableminds.util.cache.AlfuCache
 import com.scalableminds.util.geometry.Vec3Int
-import com.scalableminds.util.tools.{Fox, TextUtils}
+import com.scalableminds.util.tools.Fox
 import com.scalableminds.webknossos.datastore.dataformats.{BucketProvider, DataCubeHandle, MagLocator}
 import com.scalableminds.webknossos.datastore.datareaders.zarr.ZarrArray
 import com.scalableminds.webknossos.datastore.datavault.VaultPath
 import com.scalableminds.webknossos.datastore.models.BucketPosition
 import com.scalableminds.webknossos.datastore.models.datasource.DataSourceId
 import com.scalableminds.webknossos.datastore.models.requests.DataReadInstruction
-import com.scalableminds.webknossos.datastore.storage.DataVaultService
+import com.scalableminds.webknossos.datastore.storage.RemoteSourceDescriptorService
 import com.typesafe.scalalogging.LazyLogging
 import net.liftweb.common.Empty
-import net.liftweb.util.Helpers.tryo
 import ucar.ma2.{Array => MultiArray}
 
 import scala.concurrent.ExecutionContext
@@ -31,7 +30,7 @@ class ZarrCubeHandle(zarrArray: ZarrArray) extends DataCubeHandle with LazyLoggi
 
 class ZarrBucketProvider(layer: ZarrLayer,
                          dataSourceId: DataSourceId,
-                         val dataVaultServiceOpt: Option[DataVaultService],
+                         val remoteSourceDescriptorServiceOpt: Option[RemoteSourceDescriptorService],
                          sharedChunkContentsCache: Option[AlfuCache[String, MultiArray]])
     extends BucketProvider
     with LazyLogging {
@@ -44,15 +43,15 @@ class ZarrBucketProvider(layer: ZarrLayer,
     zarrMagOpt match {
       case None => Fox.empty
       case Some(zarrMag) =>
-        dataVaultServiceOpt match {
-          case Some(dataVaultService: DataVaultService) =>
+        remoteSourceDescriptorServiceOpt match {
+          case Some(remoteSourceDescriptorService: RemoteSourceDescriptorService) =>
             for {
               magPath: VaultPath <- if (zarrMag.isRemote) {
-                dataVaultService.vaultPathFor(zarrMag)
+                remoteSourceDescriptorService.vaultPathFor(zarrMag)
               } else localPathFrom(readInstruction, zarrMag.pathWithFallback)
-              chunkContentsCache <- sharedChunkContentsCache
-              cubeHandle <- tryo(onError = (e: Throwable) => logger.error(TextUtils.stackTraceAsString(e)))(ZarrArray
-                .open(magPath, dataSourceId, layer.name, zarrMag.axisOrder, zarrMag.channelIndex, chunkContentsCache))
+              chunkContentsCache <- sharedChunkContentsCache.toFox
+              cubeHandle <- ZarrArray
+                .open(magPath, dataSourceId, layer.name, zarrMag.axisOrder, zarrMag.channelIndex, chunkContentsCache)
                 .map(new ZarrCubeHandle(_))
             } yield cubeHandle
           case None => Empty

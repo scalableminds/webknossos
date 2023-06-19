b5fbf0d6b86b7aa2f0f91439ce5970e054daca75
Florian M <fm3@users.noreply.github.com>
2023-06-19 13:45:34 +0200

    Revert "Revert "Async IO for HttpsDataVault, Fox Error Handling (#7137)" (#7154)"
    This reverts commit 723e207654cfac76ce46f6000236c7fe80205176.

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/n5/N5ChunkReader.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/n5/N5ChunkReader.scala
@@ -30,10 +30,7 @@ class N5ChunkReader(header: DatasetHeader) extends ChunkReader(header) with Lazy
     }
 
     for {
-      bytes <- path.readBytes(range) match {
-        case Some(bytes) => Fox.successful(bytes)
-        case None        => Fox.empty
-      }
+      bytes <- path.readBytes(range)
       (blockHeader, data) <- tryo(dataExtractor.readBytesAndHeader(bytes)).toFox
       paddedChunkBytes <- tryo(processBytes(data, blockHeader.blockSize.product)).toFox ?~> "chunk.decompress.failed"
     } yield (paddedChunkBytes, Some(blockHeader.blockSize))

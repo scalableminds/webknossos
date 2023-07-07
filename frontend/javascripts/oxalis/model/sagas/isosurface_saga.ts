6c7bedd589a76a4f1f24e5d63c8dc227da6cf214
laura <charliemeister@proton.me>
2023-07-07 13:49:40 +0200

    WIP: trying to fix tests

--- frontend/javascripts/oxalis/model/sagas/isosurface_saga.ts
+++ frontend/javascripts/oxalis/model/sagas/isosurface_saga.ts
@@ -981,7 +981,7 @@ function* downloadIsosurfaceCellsAsZIP(
     });
     yield all(promises);
     const result = yield* call([zipWriter, zipWriter.close]);
-    yield* call(Zip.saveAs, result, "mesh-export.zip");
+    yield* call(saveAs, result, "mesh-export.zip");
   } catch (exception) {
     ErrorHandling.notify(exception as Error);
     console.error(exception);

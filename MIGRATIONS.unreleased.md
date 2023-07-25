2439d7f541976753bdc4c11a4f468f22c9e6e78f
frcroth <frcroth@gmail.com>
2023-07-25 11:18:11 +0200

    Update migration to set all emails as verified

--- MIGRATIONS.unreleased.md
+++ MIGRATIONS.unreleased.md
@@ -8,5 +8,11 @@ User-facing changes are documented in the [changelog](CHANGELOG.released.md).
 ## Unreleased
 [Commits](https://github.com/scalableminds/webknossos/compare/23.08.0...HEAD)
 
+- Postgres Evolution 105 (see below) adds email verification and sets the emails of all existing users as verified.
+To set all email addresses as unverified, execute this query:
+```sql
+UPDATE webknossos.multiUsers SET isEmailVerified = false;
+```
+
 ### Postgres Evolutions:
 - [105-verify-email.sql](conf/evolutions/105-verify-email.sql)
--- conf/evolutions/105-verify-email.sql
+++ conf/evolutions/105-verify-email.sql
@@ -15,6 +15,8 @@ CREATE TABLE webknossos.emailVerificationKeys(
                                                isUsed BOOLEAN NOT NULL DEFAULT false
 );
 
+-- Set email verified for all currently registered users (only require email verification for new users)
+UPDATE webknossos.multiUsers SET isEmailVerified = true;
 
 -- Recreate views
 CREATE VIEW webknossos.multiUsers_ AS SELECT * FROM webknossos.multiUsers WHERE NOT isDeleted;
@@ -23,7 +25,7 @@ SELECT
   u._id AS _user, m.email, u.firstName, u.lastname, o.displayName AS organization_displayName,
   u.isDeactivated, u.isDatasetManager, u.isAdmin, m.isSuperUser,
   u._organization, o.name AS organization_name, u.created AS user_created,
-  m.created AS multiuser_created, u._multiUser, m._lastLoggedInIdentity, u.lastActivity
+  m.created AS multiuser_created, u._multiUser, m._lastLoggedInIdentity, u.lastActivity, m.isEmailVerified
 FROM webknossos.users_ u
        JOIN webknossos.organizations_ o ON u._organization = o._id
        JOIN webknossos.multiUsers_ m on u._multiUser = m._id;

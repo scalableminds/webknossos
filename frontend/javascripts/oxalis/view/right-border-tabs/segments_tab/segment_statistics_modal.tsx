2bfb7266bcaeb749e79d2388bc46f1394d94e07b
laura <charliemeister@proton.me>
2023-08-11 18:21:57 +0200

    export segment sizes to CSV

--- frontend/javascripts/oxalis/view/right-border-tabs/segments_tab/segment_statistics_modal.tsx
+++ frontend/javascripts/oxalis/view/right-border-tabs/segments_tab/segment_statistics_modal.tsx
@@ -1,10 +1,17 @@
 import { getSegmentVolume } from "admin/admin_rest_api";
-import { Button, Modal, Table } from "antd";
-import { formatNumberToVolume } from "libs/format_utils";
+import { Modal, Table } from "antd";
+import { formatDateInLocalTimeZone } from "components/formatted_date";
+import saveAs from "file-saver";
+import { formatNumberToUnit, formatNumberToVolume } from "libs/format_utils";
 import { useFetch } from "libs/react_helpers";
+import { Unicode } from "oxalis/constants";
 import { getResolutionInfo } from "oxalis/model/accessors/dataset_accessor";
 import { Segment } from "oxalis/store";
 import React from "react";
+
+const SEGMENT_STATISTICS_CSV_HEADER = "groupId,segmendId,segmentName,volumeInVoxel,volumeInNm3";
+
+const { ThinSpace } = Unicode;
 
 type Props = {
   onCancel: (...args: Array<any>) => any;
@@ -17,16 +24,31 @@ type Props = {
 };
 
 type SegmentInfo = {
-  //TODO remove?
   segmentId: number;
   segmentName: string;
   groupId: number;
-  groupName: string;
-  sizeInNm: number;
+  //groupName: string; TODO
+  volumeInNm3: number;
+  formattedSize: string;
+  volumeInVoxel: number;
 };
 
-const exportStatisticsToCSV = () => {
-  console.log();
+const exportStatisticsToCSV = (segmentInformation: Array<SegmentInfo>) => {
+  if (segmentInformation.length < 0) {
+    return;
+  }
+  const segmentStatisticsAsString = segmentInformation
+    .map(
+      (segmentInfo) =>
+        `${segmentInfo.groupId},${segmentInfo.segmentId},${segmentInfo.segmentName},${segmentInfo.volumeInVoxel},${segmentInfo.volumeInNm3}`,
+    )
+    .join("\n");
+  const csv = [SEGMENT_STATISTICS_CSV_HEADER, segmentStatisticsAsString].join("\n");
+  const filename = `segmentStatistics-${new Date().toLocaleString().replace(/s/, "-")}.csv`; // TODO useful file naming
+  const blob = new Blob([csv], {
+    type: "text/plain;charset=utf-8",
+  });
+  saveAs(blob, filename);
 };
 
 export function SegmentStatisticsModal({
@@ -40,6 +62,7 @@ export function SegmentStatisticsModal({
 }: Props) {
   console.log(tracingId);
   const mag = getResolutionInfo(visibleSegmentationLayer.resolutions);
+  const nmFactorToUnit = new Map([[1, "nm³"]]);
   const dataSource = useFetch(
     async () => {
       const volumeStrings = await segments.map(async (segment: Segment) => {
@@ -49,12 +72,14 @@ export function SegmentStatisticsModal({
           mag.getHighestResolution(),
           segment.id,
         ).then((vol) => {
+          const formattedSize = formatNumberToVolume(vol);
           return {
             segmentId: segment.id,
-            segmentName: segment.name == null ? `Segment ${segment.id}` : segment.id,
+            segmentName: segment.name == null ? `Segment ${segment.id}` : segment.name,
             groupId: group,
-            sizeInNm: vol,
-            formattedSize: formatNumberToVolume(vol),
+            volumeInVoxel: vol,
+            volumeInNm3: parseInt(formatNumberToUnit(vol, nmFactorToUnit).split(ThinSpace)[0]),
+            formattedSize: formattedSize,
           };
         });
       });
@@ -74,7 +99,7 @@ export function SegmentStatisticsModal({
       open={isOpen}
       onCancel={onCancel}
       style={{ marginRight: 10 }}
-      onOk={() => exportStatisticsToCSV()}
+      onOk={() => exportStatisticsToCSV(dataSource)}
       okText="Export to CSV"
     >
       <Table dataSource={dataSource} columns={columns} />

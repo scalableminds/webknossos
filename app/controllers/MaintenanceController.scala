7268a9db1f7caa21104aab57cb93c1923d7b3056
Florian M <florian@scm.io>
2023-08-17 11:56:54 +0200

    WIP: new routes

--- app/controllers/MaintenanceController.scala
+++ app/controllers/MaintenanceController.scala
@@ -5,48 +5,97 @@ import com.scalableminds.util.time.Instant
 import com.scalableminds.util.tools.{Fox, FoxImplicits}
 import models.user.MultiUserDAO
 import oxalis.security.WkEnv
-import play.api.libs.json.Json
+import play.api.libs.json.{JsObject, Json}
 import play.api.mvc.{Action, AnyContent}
-import utils.sql.{SimpleSQLDAO, SqlClient}
+import slick.lifted.Rep
+import utils.ObjectId
+import utils.sql.{SQLDAO, SqlClient}
 
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
+import com.scalableminds.webknossos.schema.Tables._
+import slick.jdbc.PostgresProfile.api._
 
 class MaintenanceController @Inject()(sil: Silhouette[WkEnv],
                                       maintenanceDAO: MaintenanceDAO,
+                                      maintenenaceService: MaintenanceService,
                                       multiUserDAO: MultiUserDAO)(implicit ec: ExecutionContext)
     extends Controller
     with FoxImplicits {
 
-  private val maintenanceDuration: FiniteDuration = 10 minutes
+  private val adHocMaintenanceDuration: FiniteDuration = 5 minutes
 
-  def info: Action[AnyContent] = sil.UserAwareAction.async { implicit request =>
+  def readOne(id: String): Action[AnyContent] = ???
+  def update(id: String): Action[AnyContent] = ???
+  def delete(id: String): Action[AnyContent] = ???
+
+  def listCurrentAndUpcoming: Action[AnyContent] = sil.UserAwareAction.async { implicit request =>
     for {
-      expirationTime <- maintenanceDAO.getExpirationTime
-      isMaintenance = !expirationTime.isPast
-    } yield Ok(Json.obj("isMaintenance" -> Json.toJson(isMaintenance)))
+      currentAndUpcomingMaintenances <- maintenanceDAO.findCurrentAndUpcoming
+      js = currentAndUpcomingMaintenances.map(maintenenaceService.publicWrites)
+    } yield Ok(Json.toJson(js))
   }
 
-  def initMaintenance: Action[AnyContent] = sil.SecuredAction.async { implicit request =>
+  def listAll: Action[AnyContent] = sil.UserAwareAction.async { implicit request =>
     for {
-      multiUser <- multiUserDAO.findOne(request.identity._multiUser)
-      _ <- bool2Fox(multiUser.isSuperUser)
-      _ <- maintenanceDAO.updateExpirationTime(Instant.in(maintenanceDuration))
-    } yield Ok("maintenance.init.success")
+      currentAndUpcomingMaintenances <- maintenanceDAO.findAll
+      js = currentAndUpcomingMaintenances.map(maintenenaceService.publicWrites)
+    } yield Ok(Json.toJson(js))
   }
 
-  def closeMaintenance: Action[AnyContent] = sil.SecuredAction.async { implicit request =>
-    for {
-      multiUser <- multiUserDAO.findOne(request.identity._multiUser)
-      _ <- bool2Fox(multiUser.isSuperUser)
-      _ <- maintenanceDAO.updateExpirationTime(Instant.zero)
-    } yield Ok("maintenance.close.success")
-  }
+  def create: Action[AnyContent] = ???
+
+  def createAdHocMaintenance: Action[AnyContent] = ???
 
 }
 
-class MaintenanceDAO @Inject()(sqlClient: SqlClient)(implicit ec: ExecutionContext) extends SimpleSQLDAO(sqlClient) {
+case class Maintenance(_id: ObjectId,
+                       _user: ObjectId,
+                       startTime: Instant,
+                       endTime: Instant,
+                       message: String,
+                       created: Instant = Instant.now,
+                       isDeleted: Boolean = false)
+
+class MaintenanceService @Inject()(maintenanceDAO: MaintenanceDAO) {
+  def publicWrites(m: Maintenance): JsObject =
+    Json.obj(
+      "id" -> m._id,
+      "startTime" -> m.startTime,
+      "endTime" -> m.endTime,
+      "message" -> m.message
+    )
+}
+
+class MaintenanceDAO @Inject()(sqlClient: SqlClient)(implicit ec: ExecutionContext)
+    extends SQLDAO[Maintenance, MaintenancesRow, Maintenances](sqlClient) {
+  protected val collection = Maintenances
+
+  protected def idColumn(x: Maintenances): Rep[String] = x._Id
+
+  protected def isDeletedColumn(x: Maintenances): Rep[Boolean] = x.isdeleted
+
+  protected def parse(r: MaintenancesRow): Fox[Maintenance] =
+    Fox.successful(
+      Maintenance(ObjectId(r._Id),
+                  ObjectId(r._User),
+                  Instant.fromSql(r.starttime),
+                  Instant.fromSql(r.endtime),
+                  r.message,
+                  Instant.fromSql(r.created),
+                  r.isdeleted)
+    )
+
+  def findCurrentAndUpcoming: Fox[List[Maintenance]] =
+    for {
+      rows <- run(q"""SELECT $columns
+            FROM $existingCollectionName
+            WHERE startTime """.as[MaintenancesRow])
+      parsed <- parseAll(rows)
+    } yield parsed
+
+  def findAll: Fox[List[Maintenance]] = ???
 
   def getExpirationTime: Fox[Instant] =
     for {

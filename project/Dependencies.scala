9c5cbd7d3c04cbb45745bd7c81cde8b37b8e4187
frcroth <frcroth@gmail.com>
2023-09-18 11:16:34 +0200

    Use bcrypt library for pw hashing

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -59,6 +59,7 @@ object Dependencies {
   private val brotli4cOsXX86 = brotli4j.withName("native-osx-x86_64")
   private val brotli4cOsXArm = brotli4j.withName("native-osx-aarch64")
   private val zstdJni = "com.github.luben" % "zstd-jni" % "1.5.5-5"
+  private val bcrypt = "at.favre.lib" % "bcrypt" % "0.10.2"
 
   private val sql = Seq(
     "com.typesafe.slick" %% "slick" % "3.3.3",
@@ -81,7 +82,8 @@ object Dependencies {
     reactiveBson,
     scalapbRuntime,
     scalaLogging,
-    akkaCaching
+    akkaCaching,
+    bcrypt
   )
 
   val webknossosDatastoreDependencies: Seq[ModuleID] = Seq(

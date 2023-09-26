bffcfcba89f128026bb60033c7c6f5016c9232d5
Charlie Meister <charliemeister@proton.me>
2023-09-26 14:06:16 +0200

    fix ad-hoc mesh rendering for visible but ineditable segmentation layers

--- frontend/javascripts/oxalis/model/sagas/isosurface_saga.ts
+++ frontend/javascripts/oxalis/model/sagas/isosurface_saga.ts
@@ -306,11 +306,13 @@ function* loadFullAdHocIsosurface(
 
   const cubeSize = getZoomedCubeSize(zoomStep, resolutionInfo);
   const tracingStoreHost = yield* select((state) => state.tracing.tracingStore.url);
+  debugger
   const mag = resolutionInfo.getResolutionByIndexOrThrow(zoomStep);
 
   const volumeTracing = yield* select((state) => getActiveSegmentationTracing(state));
+  const visibleSegmentationLayer = yield* select((state) => getVisibleSegmentationLayer(state));
   // Fetch from datastore if no volumetracing ...
-  let useDataStore = volumeTracing == null;
+  let useDataStore = volumeTracing == null || visibleSegmentationLayer?.tracingId == null;
   if (isosurfaceExtraInfo.useDataStore != null) {
     // ... except if the caller specified whether to use the data store ...
     useDataStore = isosurfaceExtraInfo.useDataStore;
@@ -322,7 +324,7 @@ function* loadFullAdHocIsosurface(
   // Segment stats can only be used for volume tracings that have a segment index
   // and that don't have editable mappings.
   const usePositionsFromSegmentStats =
-    volumeTracing?.hasSegmentIndex && !volumeTracing.mappingIsEditable;
+    volumeTracing?.hasSegmentIndex && !volumeTracing.mappingIsEditable && visibleSegmentationLayer?.tracingId != null;
   let positionsToRequest = usePositionsFromSegmentStats
     ? yield* getChunkPositionsFromSegmentStats(
         tracingStoreHost,

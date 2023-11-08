5ec15a568736dc98c0e395c64ebf013bfd9dfa62
frcroth <frcroth@gmail.com>
2023-11-08 18:08:41 +0100

    Fix missing view in sql evolutions

--- conf/evolutions/110-mark-datasets-as-deleted-on-disk.sql
+++ conf/evolutions/110-mark-datasets-as-deleted-on-disk.sql
@@ -4,6 +4,10 @@ do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 1
 
 ALTER TABLE webknossos.datasets ADD COLUMN
   isRemovedOnDisk boolean NOT NULL DEFAULT false;
+
+DROP VIEW webknossos.datasets_;
+CREATE VIEW webknossos.dataSets_ AS SELECT * FROM webknossos.dataSets WHERE NOT isDeleted;
+
 UPDATE webknossos.releaseInformation SET schemaVersion = 110;
 
 COMMIT TRANSACTION;

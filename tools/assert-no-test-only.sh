4e529f1eaab51ec7cf6da70e28fa95ceefe5b0fd
Philipp Otto <philipp.4096@gmail.com>
2023-11-14 18:01:59 +0100

    fix exit code in assert-no-test-only.sh

--- tools/assert-no-test-only.sh
+++ tools/assert-no-test-only.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+exit_code=0
 echo "Checking for test.only() in test files."
-! grep -r "test\.only(" frontend/javascripts/test || echo "Found test files with test.only() which disables other tests. Please remove the only modifier."
-! grep -r "test\.serial\.only(" frontend/javascripts/test || echo "Found test files with test.only() which disables other tests. Please remove the only modifier."
+! grep -r "test\.only(" frontend/javascripts/test || { echo "Found test files with test.only() which disables other tests. Please remove the only modifier."; exit_code=1; }
+! grep -r "test\.serial\.only(" frontend/javascripts/test || { echo "Found test files with test.only() which disables other tests. Please remove the only modifier."; exit_code=1; }
 echo "Done"
+exit $exit_code

ed16fbccb94b0e9d8b4bc350fde463a0b66d2242
Florian M <florian@scm.io>
2023-11-28 13:03:08 +0100

    fix query, revert temporary changes

--- app/models/job/Job.scala
+++ app/models/job/Job.scala
@@ -126,23 +126,24 @@ class JobDAO @Inject()(sqlClient: SqlClient)(implicit ec: ExecutionContext)
     }
 
   override protected def readAccessQ(requestingUserId: ObjectId) =
-    q"""_owner = $requestingUserId"""
+    q"""
+      _owner = $requestingUserId
+      OR
+      (_owner in (select _user from webknossos.user_team_roles where _team in (select _team from webknossos.user_team_roles where _user = $requestingUserId and isTeamManager)))
+      OR
+      ((SELECT u._organization FROM webknossos.users_ u WHERE u._id = _owner) in (select _organization from webknossos.users_ where _id = $requestingUserId and isAdmin))
+      OR
+      ($requestingUserId in
+        (
+          select u._id
+          from webknossos.users_ u join webknossos.multiUsers_ m on u._multiUser = m._id
+          where m.isSuperUser
+        )
+      )
+     """
 
   private def listAccessQ(requestingUserId: ObjectId) =
-    q"""_owner = $requestingUserId
-        OR
-        (_owner in (select _user from webknossos.user_team_roles where _team in (select _team from webknossos.user_team_roles where _user = $requestingUserId and isTeamManager)))
-        OR
-        ((SELECT u._organization FROM webknossos.users_ u WHERE u._id = _owner) in (select _organization from webknossos.users_ where _id = $requestingUserId and isAdmin))
-        OR
-        ($requestingUserId in
-          (
-            select u._id
-            from webknossos.users_ u join webknossos.multiUsers_ m on u._multiUser = m._id
-            where m.isSuperUser
-          )
-        )
-       """
+    q"""_owner = $requestingUserId"""
 
   override def findAll(implicit ctx: DBAccessContext): Fox[List[Job]] =
     for {

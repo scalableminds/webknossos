c70e3a5e99da06d9870374d433a96762fa672960
Florian M <florian@scm.io>
2024-01-16 11:29:50 +0100

    migration

--- tools/migrate-axis-bounds/migration.py
+++ tools/migrate-axis-bounds/migration.py
@@ -1,21 +1,36 @@
 from pathlib import Path
 import json
 import shutil
+import time
+import argparse
+
+# Traverses a binaryData directory and changes all datasource-properties.jsons
+# that include additionalAxes, increasing the upper bound by 1
+# This follows a change in the upper bound semantic to be exclusive
 
 def main():
-    binary_data_dir = Path("../../binaryData")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dry",
+        action="store_true",
+    )
+    parser.add_argument("binary_data_dir", type=Path, help="WEBKNOSSOS binary data dir")
+    args = parser.parse_args()
+    dry = args.dry
+    binary_data_dir = args.binary_data_dir
+
+    count = 0
+    print(f"Traversing datasets at {binary_data_dir.resolve()} ...")
     for orga_dir in [item for item in binary_data_dir.iterdir() if item.is_dir()]:
         for dataset_dir in [item for item in orga_dir.iterdir() if item.is_dir()]:
             json_path = dataset_dir / "datasource-properties.json"
             if json_path.exists():
                 changed = False
-                print(f"opening {json_path}...")
                 with open(json_path, 'r') as json_file:
                     content = json.load(json_file)
                     if "dataLayers" in content:
                         for layer in content["dataLayers"]:
                             if "additionalAxes" in layer:
-                                print(layer["additionalAxes"])
                                 for axis in layer["additionalAxes"]:
                                     if "bounds" in axis:
                                         bounds = axis["bounds"]
@@ -23,13 +38,16 @@ def main():
                                             bounds[1] = bounds[1] + 1
                                             changed = True
                 if changed:
-                    # TODO copy old file to .bak (add digit if that already exists)
-                    # TODO write new file
+                    print(f"Updating {json_path} (dry={dry})...")
+                    count += 1
+                    backup_path = Path(f"{json_path}.{round(time.time() * 1000)}.bak")
 
-                    with open(json_path, 'w') as json_outfile:
-                        json.dump(content, json_outfile, indent=4)
+                    if not dry:
+                        shutil.copyfile(json_path, backup_path)
+                        with open(json_path, 'w') as json_outfile:
+                            json.dump(content, json_outfile, indent=4)
 
-
+    print(f"Updated {count} datasets (dry={dry})")
 
 if __name__ == '__main__':
     main()

655b4e996d7b21949f1f45d92e3a70267bd82460
Philipp Otto <philipp.4096@gmail.com>
2024-01-16 13:35:48 +0100

    inline radio buttons in first step of compose-dataset-wizard

--- frontend/javascripts/admin/dataset/composition_wizard/01_select_import_type.tsx
+++ frontend/javascripts/admin/dataset/composition_wizard/01_select_import_type.tsx
@@ -26,26 +26,18 @@ export default function SelectImportType({
       <div style={{ marginBottom: 8 }}>
         You can create a new dataset by composing existing datasets together. There are three
         different ways to accomplish this:
-        <ul>
-          <li>Select existing datasets which should be combined without any transforms</li>
-          <li>
-            Create landmarks nodes using the skeleton tool in two datasets. Download the annotations
-            as NML and upload these here again.
-          </li>
-          <li>Import a landmark CSV as it can be exported by Big Warp.</li>
-        </ul>
+        <div style={{ margin: 12 }}>
+          <Radio.Group onChange={onChange} value={composeMode}>
+            <Space direction="vertical">
+              <Radio value={"WITHOUT_TRANSFORMS"}>Combine datasets without any transforms</Radio>
+              <Radio value={"WK_ANNOTATIONS"}>
+                Combine datasets by using skeleton annotations (NML)
+              </Radio>
+              <Radio value={"BIG_WARP"}>Combine datasets by using BigWarp landmarks (CSV)</Radio>
+            </Space>
+          </Radio.Group>
+        </div>
         In all three cases, you can tweak which layers should be used later.
-      </div>
-      <div>
-        <Radio.Group onChange={onChange} value={composeMode}>
-          <Space direction="vertical">
-            <Radio value={"WITHOUT_TRANSFORMS"}>Combine datasets without any transforms</Radio>
-            <Radio value={"WK_ANNOTATIONS"}>
-              Combine datasets by using skeleton annotations (NML)
-            </Radio>
-            <Radio value={"BIG_WARP"}>Combine datasets by using BigWarp landmarks (CSV)</Radio>
-          </Space>
-        </Radio.Group>
       </div>
       <Button type="primary" style={{ marginTop: 16 }} onClick={onNext}>
         Next

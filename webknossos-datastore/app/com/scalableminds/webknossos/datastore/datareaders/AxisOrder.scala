c7121565c383a78e6f7f89de5e0c01064a66a7c2
Florian M <florian@scm.io>
2024-01-30 12:01:42 +0100

    FullAxisOrder from AxisOrder and AdditionalCoordinates

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/AxisOrder.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/AxisOrder.scala
@@ -1,14 +1,7 @@
 package com.scalableminds.webknossos.datastore.datareaders
 
+import com.scalableminds.webknossos.datastore.models.datasource.AdditionalAxis
 import play.api.libs.json.{Json, OFormat}
-
-case class Axis(name: String, index: Int)
-
-class FullAxisOrder(axes: Seq[Axis])
-
-object FullAxisOrder {
-  def fromAxisOrderAndAdditionalCoordinates: FullAxisOrder = ???
-}
 
 // Defines the axis order of a DatasetArray. Note that this ignores transpose codecs/ArrayOrder.F/C.
 // Those will have to be applied on individual chunk’s contents.
@@ -45,6 +38,11 @@ case class AxisOrder(x: Int, y: Int, z: Option[Int], c: Option[Int] = None) {
   def permuteIndicesArrayToWk(indices: Array[Int]): Array[Int] =
     arrayToWkPermutation(indices.length).map(indices(_))
 
+  def length: Int = {
+    val lengthOfZ = if (z.isDefined) 1 else 0
+    val lengthOfC = if (c.isDefined) 1 else 0
+    lengthOfC + 2 + lengthOfZ
+  }
 }
 
 object AxisOrder {
@@ -65,4 +63,27 @@ object AxisOrder {
 
   def cxyz: AxisOrder = asCxyzFromRank(rank = 4)
   implicit val jsonFormat: OFormat[AxisOrder] = Json.format[AxisOrder]
+}
+
+case class Axis(name: String)
+
+case class FullAxisOrder(axes: Seq[Axis])
+
+object FullAxisOrder {
+  def fromAxisOrderAndAdditionalAxes(axisOrder: AxisOrder,
+                                     additionalAxes: Option[Seq[AdditionalAxis]]): FullAxisOrder = {
+    val asArray: Array[Axis] = Array.fill(additionalAxes.map(_.length).getOrElse(0) + axisOrder.length)(Axis(""))
+    asArray(axisOrder.x) = Axis("x")
+    asArray(axisOrder.y) = Axis("y")
+    axisOrder.c.foreach { c =>
+      asArray(c) = Axis("c")
+    }
+    axisOrder.z.foreach { z =>
+      asArray(z) = Axis("z")
+    }
+    for (additionalAxis <- additionalAxes.getOrElse(Seq.empty)) {
+      asArray(additionalAxis.index) = Axis(additionalAxis.name)
+    }
+    FullAxisOrder(asArray.toVector)
+  }
 }

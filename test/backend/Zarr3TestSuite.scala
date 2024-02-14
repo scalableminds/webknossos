1562d3850cd3adb9a1b5d0c9e0ecaec82ca32efb
frcroth <frcroth@gmail.com>
2024-02-14 14:18:55 +0100

    Add test to parse some codecs

--- test/backend/Zarr3TestSuite.scala
+++ test/backend/Zarr3TestSuite.scala
@@ -1,6 +1,12 @@
 package backend
 
-import com.scalableminds.webknossos.datastore.datareaders.zarr3.Zarr3ArrayHeader
+import com.scalableminds.webknossos.datastore.datareaders.zarr3.{
+  BloscCodecConfiguration,
+  BytesCodec,
+  BytesCodecConfiguration,
+  Zarr3Array,
+  Zarr3ArrayHeader
+}
 import com.scalableminds.webknossos.datastore.datareaders.zarr3.Zarr3ArrayHeader.Zarr3ArrayHeaderFormat
 import org.scalatestplus.play.PlaySpec
 import play.api.libs.json.{JsObject, JsValue, Json}
@@ -10,7 +16,8 @@ class Zarr3TestSuite extends PlaySpec {
   "Zarr 3" when {
     "importing zarr.json" should {
 
-      val zarr3json = Json.parse("""
+      val zarr3json = Json.parse(
+        """
         { "shape": [64,64,64],
           "data_type":"uint8",
           "zarr_format":3,
@@ -19,7 +26,7 @@ class Zarr3TestSuite extends PlaySpec {
        		"name": "regular" },
           "chunk_key_encoding": { "configuration":{"separator": "/"}, "name":"default"},
           "fill_value": 0,
-          "codecs":[],
+          "codecs":[{"configuration": {"endian": "little"}, "name": "bytes"}, {"configuration": {"typesize": 4, "cname": "zstd", "clevel": 5, "shuffle": "noshuffle", "blocksize": 0}, "name": "blosc"}],
           "attributes": { "att1":"test"},
           "dimension_names": ["x","y","z"],
           "node_type":"array"}""".stripMargin)
@@ -27,10 +34,19 @@ class Zarr3TestSuite extends PlaySpec {
       "read correct basic header data" in {
         val header = Zarr3ArrayHeaderFormat.reads(zarr3json).get
         assert(header.shape.sameElements(Seq(64, 64, 64)))
-        assert(header.dataType == "uint8")
+        assert(header.data_type.left.get == "uint8")
         assert(header.zarr_format == 3)
         assert(header.fill_value.right.get.intValue() == 0)
         assert(header.dimension_names.get.sameElements(Seq("x", "y", "z")))
+      }
+
+      "parse basic codecs" in {
+        val header = Zarr3ArrayHeaderFormat.reads(zarr3json).get
+        assert(header.codecs.length == 2)
+        assert(header.codecs(0).isInstanceOf[BytesCodecConfiguration])
+        assert(header.codecs(1).isInstanceOf[BloscCodecConfiguration])
+        val array = new Zarr3Array(null, null, null, header, null, null, null, null)
+        assert(array.codecs.length == 2)
       }
     }
   }

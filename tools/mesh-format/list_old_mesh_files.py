ffc74de6addb056a871323008abbe9b30db061f5
Florian M <florian@scm.io>
2024-02-20 11:47:14 +0100

    add exception handling, deduplicate realpaths, print count

--- tools/mesh-format/list_old_mesh_files.py
+++ tools/mesh-format/list_old_mesh_files.py
@@ -22,30 +22,43 @@ def main():
     args = parser.parse_args()
     binary_data_dir = args.binary_data_dir
 
-    print(f"Scanning {binary_data_dir} for mesh files", file=sys.stderr)
+    if not args.plain:
+        print(f"Scanning {binary_data_dir} for mesh files")
 
-    for orga_dir in [item for item in binary_data_dir.iterdir() if item.is_dir()]:
-        for dataset_dir in [item for item in orga_dir.iterdir() if item.is_dir()]:
-            for layer_dir in [item for item in dataset_dir.iterdir() if item.is_dir()]:
-                meshes_dir = layer_dir.joinpath("meshes")
-                if meshes_dir.exists():
-                    for mesh_file in [
-                        item
-                        for item in meshes_dir.iterdir()
-                        if item.name.endswith(".hdf5")
-                    ]:
-                        with h5py.File(mesh_file, "r") as f:
-                            try:
-                                version = f.attrs["artifact_schema_version"]
-                            except KeyError:
-                                version = 0
-                            if version < 3 or args.all:
-                                if args.plain:
-                                    print(mesh_file)
-                                else:
-                                    print(f"{mesh_file} has version {version}")
+    seen = []
 
-    print(f"Done scanning {binary_data_dir}", file=sys.stderr)
+    for orga_dir in [item for item in binary_data_dir.iterdir() if item.exists() and item.is_dir()]:
+        for dataset_dir in orga_dir.iterdir():
+            try:
+                if dataset_dir.exists() and dataset_dir.is_dir():
+                    for layer_dir in [item for item in dataset_dir.iterdir() if item.exists() and item.is_dir()]:
+                        meshes_dir = layer_dir.joinpath("meshes")
+                        if meshes_dir.exists():
+                            for mesh_file in [
+                                item
+                                for item in meshes_dir.iterdir()
+                                if item.name.endswith(".hdf5")
+                            ]:
+                                if mesh_file.exists():
+                                    with h5py.File(mesh_file, "r") as f:
+                                        try:
+                                            version = f.attrs["artifact_schema_version"]
+                                        except KeyError:
+                                            version = 0
+                                        if version < 3 or args.all:
+                                            realpath = mesh_file.resolve()
+                                            if realpath not in seen:
+                                                seen.append(realpath)
+                                                if args.plain:
+                                                    print(mesh_file)
+                                                else:
+                                                    print(f"{mesh_file} has version {version}")
+            except Exception as e:
+                if not args.plain:
+                    print(f"Exception while scanning dataset dir at {dataset_dir}: {e}")
+
+    if not args.plain:
+        print(f"Done scanning {binary_data_dir}, listed {len(seen)} meshfiles.")
 
 
 if __name__ == "__main__":

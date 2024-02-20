f9cc9ca34bfca66527ea989583601003e2c2ca47
Florian M <florian@scm.io>
2024-02-20 14:56:27 +0100

    connect prototype to api

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/controllers/DSMeshController.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/controllers/DSMeshController.scala
@@ -1,44 +1,27 @@
 package com.scalableminds.webknossos.datastore.controllers
 
 import com.google.inject.Inject
-import com.scalableminds.util.geometry.{Vec3Float, Vec3Int}
-import com.scalableminds.util.time.Instant
+import com.scalableminds.util.geometry.Vec3Int
 import com.scalableminds.util.tools.{Fox, FoxImplicits}
-import com.scalableminds.webknossos.datastore.{DataStoreConfig, NativeDracoToStlConverter}
-import com.scalableminds.webknossos.datastore.models.VoxelPosition
-import com.scalableminds.webknossos.datastore.models.datasource.{DataSource, DataSourceId, SegmentationLayer}
-import com.scalableminds.webknossos.datastore.models.requests.Cuboid
+import com.scalableminds.webknossos.datastore.models.datasource.DataSourceId
 import com.scalableminds.webknossos.datastore.services._
 import com.scalableminds.webknossos.datastore.storage.AgglomerateFileKey
-import net.liftweb.common.Box.tryo
 import play.api.i18n.Messages
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, PlayBodyParsers}
 
-import java.nio.{ByteBuffer, ByteOrder}
 import scala.concurrent.ExecutionContext
 
 class DSMeshController @Inject()(
     accessTokenService: DataStoreAccessTokenService,
     meshFileService: MeshFileService,
     fullMeshService: FullMeshService,
-    dataSourceRepository: DataSourceRepository,
-    adHocMeshServiceHolder: AdHocMeshServiceHolder,
     dsRemoteWebKnossosClient: DSRemoteWebKnossosClient,
-    mappingService: MappingService,
-    config: DataStoreConfig,
     dsRemoteTracingstoreClient: DSRemoteTracingstoreClient,
     binaryDataServiceHolder: BinaryDataServiceHolder
 )(implicit bodyParsers: PlayBodyParsers, ec: ExecutionContext)
     extends Controller
     with FoxImplicits {
-
-  val binaryDataService: BinaryDataService = binaryDataServiceHolder.binaryDataService
-  adHocMeshServiceHolder.dataStoreAdHocMeshConfig =
-    (binaryDataService, mappingService, config.Datastore.AdHocMesh.timeout, config.Datastore.AdHocMesh.actorPoolSize)
-
-  private lazy val converter = new NativeDracoToStlConverter()
-  val adHocMeshService: AdHocMeshService = adHocMeshServiceHolder.dataStoreAdHocMeshService
 
   override def allowRemoteOrigin: Boolean = true
 
@@ -48,101 +31,22 @@ class DSMeshController @Inject()(
     val layerName = "segmentation"
     val segmentId = 1997
     val seedPosition = Vec3Int(3455, 3455, 1023)
-    val chunkSize = Vec3Int.full(101)
 
-    for {
-      (dataSource, dataLayer) <- dataSourceRepository.getDataSourceAndDataLayer(organizationName,
-                                                                                datasetName,
-                                                                                layerName) ~> NOT_FOUND
-      segmentationLayer <- tryo(dataLayer.asInstanceOf[SegmentationLayer]).toFox ?~> "dataLayer.mustBeSegmentation"
-      before = Instant.now
-      verticesNested <- getAllAdHocChunks(
-        dataSource,
-        segmentationLayer,
-        segmentId,
-        VoxelPosition(seedPosition.x, seedPosition.y, seedPosition.z, Vec3Int(4, 4, 1)),
-        chunkSize)
-      _ = Instant.logSince(before, "adHocMeshing")
-      beforeEncoding = Instant.now
-      encoded = verticesNested.map(adHocMeshToStl)
-      array = combineEncodedChunksToStl(encoded)
-      _ = Instant.logSince(beforeEncoding, "transCoding")
-    } yield Ok(array)
-  }
-
-  private def getAllAdHocChunks(
-      dataSource: DataSource,
-      segmentationLayer: SegmentationLayer,
-      segmentId: Long,
-      topLeft: VoxelPosition,
-      chunkSize: Vec3Int,
-      visited: collection.mutable.Set[VoxelPosition] = collection.mutable.Set[VoxelPosition]())
-    : Fox[List[Array[Float]]] = {
-    val adHocMeshRequest = AdHocMeshRequest(
-      Some(dataSource),
-      segmentationLayer,
-      Cuboid(topLeft, chunkSize.x + 1, chunkSize.y + 1, chunkSize.z + 1),
+    val stlRequest = FullMeshRequest(
+      None,
+      None,
       segmentId,
-      dataSource.scale,
       None,
       None,
+      None,
+      Some(Vec3Int(4, 4, 1)),
+      Some(seedPosition),
       None
     )
-    visited += topLeft
     for {
-      (vertices: Array[Float], neighbors) <- adHocMeshService.requestAdHocMeshViaActor(adHocMeshRequest)
-      nextPositions: List[VoxelPosition] = generateNextTopLeftsFromNeighbors(topLeft, neighbors, chunkSize, visited)
-      _ = visited ++= nextPositions
-      neighborVerticesNested <- Fox.serialCombined(nextPositions) { position: VoxelPosition =>
-        getAllAdHocChunks(dataSource, segmentationLayer, segmentId, position, chunkSize, visited)
-      }
-      allVertices: List[Array[Float]] = vertices +: neighborVerticesNested.flatten
-    } yield allVertices
-  }
-
-  private def generateNextTopLeftsFromNeighbors(oldTopLeft: VoxelPosition,
-                                                neighborIds: List[Int],
-                                                chunkSize: Vec3Int,
-                                                visited: collection.mutable.Set[VoxelPosition]): List[VoxelPosition] = {
-    // front_xy, front_xz, front_yz, back_xy, back_xz, back_yz
-    val neighborLookup = Seq(
-      Vec3Int(0, 0, -1),
-      Vec3Int(0, -1, 0),
-      Vec3Int(-1, 0, 0),
-      Vec3Int(0, 0, 1),
-      Vec3Int(0, 1, 0),
-      Vec3Int(1, 0, 0),
-    )
-    val neighborPositions = neighborIds.map { neighborId =>
-      val neighborMultiplier = neighborLookup(neighborId)
-      oldTopLeft.move(neighborMultiplier.x * chunkSize.x * oldTopLeft.mag.x,
-                      neighborMultiplier.y * chunkSize.y * oldTopLeft.mag.y,
-                      neighborMultiplier.z * chunkSize.z * oldTopLeft.mag.z)
-    }
-    neighborPositions.filterNot(visited.contains)
-  }
-
-  private def adHocMeshToStl(vertexBuffer: Array[Float]): Array[Byte] = {
-    val numFaces = vertexBuffer.length / (3 * 3) // a face has three vertices, a vertex has three floats.
-    val outputNumBytes = numFaces * 50
-    val output = ByteBuffer.allocate(outputNumBytes).order(ByteOrder.LITTLE_ENDIAN)
-    val unused = Array.fill[Byte](2)(0)
-    for (faceIndex <- 0 until numFaces) {
-      val v1 = Vec3Float(vertexBuffer(faceIndex), vertexBuffer(faceIndex + 1), vertexBuffer(faceIndex + 2))
-      val v2 = Vec3Float(vertexBuffer(faceIndex + 3), vertexBuffer(faceIndex + 4), vertexBuffer(faceIndex + 5))
-      val v3 = Vec3Float(vertexBuffer(faceIndex + 6), vertexBuffer(faceIndex + 7), vertexBuffer(faceIndex + 8))
-      val norm = Vec3Float.crossProduct(v2 - v1, v3 - v1).normalize
-      output.putFloat(norm.x)
-      output.putFloat(norm.y)
-      output.putFloat(norm.z)
-      for (vertexIndex <- 0 until 3) {
-        for (dimIndex <- 0 until 3) {
-          output.putFloat(vertexBuffer(9 * faceIndex + 3 * vertexIndex + dimIndex))
-        }
-      }
-      output.put(unused)
-    }
-    byteBufferToArray(output, outputNumBytes)
+      data: Array[Byte] <- fullMeshService
+        .loadFor(None, organizationName, datasetName, layerName, stlRequest) ?~> "mesh.file.loadChunk.failed"
+    } yield Ok(data)
   }
 
   def testStl: Action[AnyContent] = Action.async { implicit request =>
@@ -150,57 +54,22 @@ class DSMeshController @Inject()(
     val datasetName = "l4dense_mesh_test"
     val layerName = "segmentation"
     val meshfileName = "meshfile_4-4-2"
-    val listMeshChunksRequest = ListMeshChunksRequest(
-      meshfileName,
-      55834
+
+    val stlRequest = FullMeshRequest(
+      Some(meshfileName),
+      None,
+      55834,
+      None,
+      None,
+      None,
+      None,
+      None,
+      None
     )
-    val before = Instant.now
     for {
-      chunkInfos: WebknossosSegmentInfo <- meshFileService
-        .listMeshChunksForSegmentV3(organizationName, datasetName, layerName, listMeshChunksRequest)
-        .toFox
-      allChunkRanges: List[MeshChunk] = chunkInfos.chunks.lods.head.chunks
-      stlEncodedChunks: Seq[Array[Byte]] <- Fox.serialCombined(allChunkRanges) { chunkRange: MeshChunk =>
-        readMeshChunkAsStl(organizationName, datasetName, layerName, meshfileName, chunkRange)
-      }
-      stlOutput = combineEncodedChunksToStl(stlEncodedChunks)
-      _ = logger.info(s"output size: ${stlOutput.length}. took ${Instant.since(before)}")
-    } yield Ok(stlOutput)
-  }
-
-  private def combineEncodedChunksToStl(stlEncodedChunks: Seq[Array[Byte]]): Array[Byte] = {
-    val numFaces = stlEncodedChunks.map(_.length / 50).sum // our stl implementation writes exactly 50 bytes per face
-    val constantStlHeader = Array.fill[Byte](80)(0)
-    val outputNumBytes = 80 + 4 + stlEncodedChunks.map(_.length).sum
-    val output = ByteBuffer.allocate(outputNumBytes).order(ByteOrder.LITTLE_ENDIAN)
-    output.put(constantStlHeader)
-    output.putInt(numFaces)
-    stlEncodedChunks.foreach(output.put)
-    byteBufferToArray(output, outputNumBytes)
-  }
-
-  private def readMeshChunkAsStl(organizationName: String,
-                                 datasetName: String,
-                                 layerName: String,
-                                 meshfileName: String,
-                                 chunkInfo: MeshChunk): Fox[Array[Byte]] =
-    for {
-      (dracoMeshChunkBytes, encoding) <- meshFileService.readMeshChunkV3(
-        organizationName,
-        datasetName,
-        layerName,
-        MeshChunkDataRequestV3List(meshfileName, List(MeshChunkDataRequestV3(chunkInfo.byteOffset, chunkInfo.byteSize)))
-      )
-      _ <- bool2Fox(encoding == "draco") ?~> s"meshfile encoding is $encoding, only draco is supported"
-      stlEncodedChunk <- tryo(
-        converter.dracoToStl(dracoMeshChunkBytes, chunkInfo.position.x, chunkInfo.position.y, chunkInfo.position.z))
-    } yield stlEncodedChunk
-
-  private def byteBufferToArray(buf: ByteBuffer, numBytes: Int): Array[Byte] = {
-    val arr = new Array[Byte](numBytes)
-    buf.rewind()
-    buf.get(arr)
-    arr
+      data: Array[Byte] <- fullMeshService
+        .loadFor(None, organizationName, datasetName, layerName, stlRequest) ?~> "mesh.file.loadChunk.failed"
+    } yield Ok(data)
   }
 
   def listMeshFiles(token: Option[String],
@@ -372,10 +241,10 @@ class DSMeshController @Inject()(
       }
     }
 
-  def loadFullMesh(token: Option[String],
-                   organizationName: String,
-                   datasetName: String,
-                   dataLayerName: String): Action[FullMeshRequest] =
+  def loadFullMeshStl(token: Option[String],
+                      organizationName: String,
+                      datasetName: String,
+                      dataLayerName: String): Action[FullMeshRequest] =
     Action.async(validateJson[FullMeshRequest]) { implicit request =>
       accessTokenService.validateAccess(UserAccessRequest.readDataSources(DataSourceId(datasetName, organizationName)),
                                         urlOrHeaderToken(token, request)) {
--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/FullMeshService.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/FullMeshService.scala
@@ -1,13 +1,20 @@
 package com.scalableminds.webknossos.datastore.services
 
 import com.google.inject.Inject
-import com.scalableminds.util.geometry.{Vec3Double, Vec3Int}
+import com.scalableminds.util.geometry.{Vec3Float, Vec3Int}
+import com.scalableminds.util.time.Instant
 import com.scalableminds.util.tools.Fox
-import com.scalableminds.util.tools.Fox.option2Fox
-import com.scalableminds.webknossos.datastore.models.AdditionalCoordinate
+import com.scalableminds.util.tools.Fox.{bool2Fox, box2Fox, option2Fox}
+import com.scalableminds.webknossos.datastore.{DataStoreConfig, NativeDracoToStlConverter}
+import com.scalableminds.webknossos.datastore.models.{AdditionalCoordinate, VoxelPosition}
+import com.scalableminds.webknossos.datastore.models.datasource.{DataSource, SegmentationLayer}
+import com.scalableminds.webknossos.datastore.models.requests.Cuboid
+import com.typesafe.scalalogging.LazyLogging
+import net.liftweb.common.Box.tryo
 import play.api.i18n.MessagesProvider
 import play.api.libs.json.{Json, OFormat}
 
+import java.nio.{ByteBuffer, ByteOrder}
 import scala.concurrent.ExecutionContext
 
 case class FullMeshRequest(
@@ -16,16 +23,33 @@ case class FullMeshRequest(
     segmentId: Long, // if mappingName is set, this is an agglomerate id
     mappingName: Option[String],
     mappingType: Option[String], // json, agglomerate, editableMapping
-    mag: Option[Vec3Int],
-    seedPosition: Option[Vec3Double], // required for ad-hoc meshing
-    additionalCoordinates: Option[Seq[AdditionalCoordinate]] = None,
+    editableMappingTracingId: Option[String],
+    mag: Option[Vec3Int], // required for ad-hoc meshing
+    seedPosition: Option[Vec3Int], // required for ad-hoc meshing
+    additionalCoordinates: Option[Seq[AdditionalCoordinate]]
 )
 
 object FullMeshRequest {
   implicit val jsonFormat: OFormat[FullMeshRequest] = Json.format[FullMeshRequest]
 }
 
-class FullMeshService @Inject()(dataSourceRepository: DataSourceRepository) {
+class FullMeshService @Inject()(dataSourceRepository: DataSourceRepository,
+                                meshFileService: MeshFileService,
+                                binaryDataServiceHolder: BinaryDataServiceHolder,
+                                mappingService: MappingService,
+                                config: DataStoreConfig,
+                                adHocMeshServiceHolder: AdHocMeshServiceHolder)
+    extends LazyLogging {
+
+  val binaryDataService: BinaryDataService = binaryDataServiceHolder.binaryDataService
+  adHocMeshServiceHolder.dataStoreAdHocMeshConfig =
+    (binaryDataService, mappingService, config.Datastore.AdHocMesh.timeout, config.Datastore.AdHocMesh.actorPoolSize)
+  val adHocMeshService: AdHocMeshService = adHocMeshServiceHolder.dataStoreAdHocMeshService
+
+  private lazy val dracoToStlConverter = new NativeDracoToStlConverter()
+
+  private lazy val adHocChunkSize = Vec3Int(100, 100, 100)
+
   def loadFor(token: Option[String],
               organizationName: String,
               datasetName: String,
@@ -44,27 +68,157 @@ class FullMeshService @Inject()(dataSourceRepository: DataSourceRepository) {
       dataLayerName: String,
       fullMeshRequest: FullMeshRequest)(implicit ec: ExecutionContext, m: MessagesProvider): Fox[Array[Byte]] =
     for {
-      mag <- fullMeshRequest.mag.toFox ?~> "mag.needeForAdHoc"
+      mag <- fullMeshRequest.mag.toFox ?~> "mag.neededForAdHoc"
+      seedPosition <- fullMeshRequest.seedPosition.toFox ?~> "seedPosition.neededForAdHoc"
       (dataSource, dataLayer) <- dataSourceRepository.getDataSourceAndDataLayer(organizationName,
                                                                                 datasetName,
                                                                                 dataLayerName)
-      // TODO seed position
-      // TODO gather chunks from adHocMeshActor (pass the mapping there if exists)
-      // TODO use neighbors logic
-      // TODO: puzzle them together
-      // TODO: simplify
-    } yield Array[Byte](0)
+      segmentationLayer <- tryo(dataLayer.asInstanceOf[SegmentationLayer]).toFox ?~> "dataLayer.mustBeSegmentation"
+      before = Instant.now
+      verticesForChunks <- getAllAdHocChunks(dataSource,
+                                             segmentationLayer,
+                                             fullMeshRequest.segmentId,
+                                             VoxelPosition(seedPosition.x, seedPosition.y, seedPosition.z, mag),
+                                             adHocChunkSize)
+      _ = Instant.logSince(before, "adHocMeshing")
+      beforeEncoding = Instant.now
+      encoded = verticesForChunks.map(adHocMeshToStl)
+      array = combineEncodedChunksToStl(encoded)
+      _ = Instant.logSince(beforeEncoding, "transCoding")
+    } yield array
+
+  private def getAllAdHocChunks(
+      dataSource: DataSource,
+      segmentationLayer: SegmentationLayer,
+      segmentId: Long,
+      topLeft: VoxelPosition,
+      chunkSize: Vec3Int,
+      visited: collection.mutable.Set[VoxelPosition] = collection.mutable.Set[VoxelPosition]())(
+      implicit ec: ExecutionContext): Fox[List[Array[Float]]] = {
+    val adHocMeshRequest = AdHocMeshRequest(
+      Some(dataSource),
+      segmentationLayer,
+      Cuboid(topLeft, chunkSize.x + 1, chunkSize.y + 1, chunkSize.z + 1),
+      segmentId,
+      dataSource.scale,
+      None,
+      None,
+      None
+    )
+    visited += topLeft
+    for {
+      (vertices: Array[Float], neighbors) <- adHocMeshService.requestAdHocMeshViaActor(adHocMeshRequest)
+      nextPositions: List[VoxelPosition] = generateNextTopLeftsFromNeighbors(topLeft, neighbors, chunkSize, visited)
+      _ = visited ++= nextPositions
+      neighborVerticesNested <- Fox.serialCombined(nextPositions) { position: VoxelPosition =>
+        getAllAdHocChunks(dataSource, segmentationLayer, segmentId, position, chunkSize, visited)
+      }
+      allVertices: List[Array[Float]] = vertices +: neighborVerticesNested.flatten
+    } yield allVertices
+  }
+
+  private def generateNextTopLeftsFromNeighbors(oldTopLeft: VoxelPosition,
+                                                neighborIds: List[Int],
+                                                chunkSize: Vec3Int,
+                                                visited: collection.mutable.Set[VoxelPosition]): List[VoxelPosition] = {
+    // front_xy, front_xz, front_yz, back_xy, back_xz, back_yz
+    val neighborLookup = Seq(
+      Vec3Int(0, 0, -1),
+      Vec3Int(0, -1, 0),
+      Vec3Int(-1, 0, 0),
+      Vec3Int(0, 0, 1),
+      Vec3Int(0, 1, 0),
+      Vec3Int(1, 0, 0),
+    )
+    val neighborPositions = neighborIds.map { neighborId =>
+      val neighborMultiplier = neighborLookup(neighborId)
+      oldTopLeft.move(neighborMultiplier.x * chunkSize.x * oldTopLeft.mag.x,
+                      neighborMultiplier.y * chunkSize.y * oldTopLeft.mag.y,
+                      neighborMultiplier.z * chunkSize.z * oldTopLeft.mag.z)
+    }
+    neighborPositions.filterNot(visited.contains)
+  }
+
+  private def adHocMeshToStl(vertexBuffer: Array[Float]): Array[Byte] = {
+    val numFaces = vertexBuffer.length / (3 * 3) // a face has three vertices, a vertex has three floats.
+    val outputNumBytes = numFaces * 50
+    val output = ByteBuffer.allocate(outputNumBytes).order(ByteOrder.LITTLE_ENDIAN)
+    val unused = Array.fill[Byte](2)(0)
+    for (faceIndex <- 0 until numFaces) {
+      val v1 = Vec3Float(vertexBuffer(faceIndex), vertexBuffer(faceIndex + 1), vertexBuffer(faceIndex + 2))
+      val v2 = Vec3Float(vertexBuffer(faceIndex + 3), vertexBuffer(faceIndex + 4), vertexBuffer(faceIndex + 5))
+      val v3 = Vec3Float(vertexBuffer(faceIndex + 6), vertexBuffer(faceIndex + 7), vertexBuffer(faceIndex + 8))
+      val norm = Vec3Float.crossProduct(v2 - v1, v3 - v1).normalize
+      output.putFloat(norm.x)
+      output.putFloat(norm.y)
+      output.putFloat(norm.z)
+      for (vertexIndex <- 0 until 3) {
+        for (dimIndex <- 0 until 3) {
+          output.putFloat(vertexBuffer(9 * faceIndex + 3 * vertexIndex + dimIndex))
+        }
+      }
+      output.put(unused)
+    }
+    byteBufferToArray(output, outputNumBytes)
+  }
 
   private def loadFullMeshFromMeshfile(
       token: Option[String],
       organizationName: String,
       datasetName: String,
-      dataLayerName: String,
-      fullMeshRequest: FullMeshRequest)(implicit ec: ExecutionContext): Fox[Array[Byte]] = ???
+      layerName: String,
+      fullMeshRequest: FullMeshRequest)(implicit ec: ExecutionContext): Fox[Array[Byte]] =
+    for {
+      meshFileName <- fullMeshRequest.meshFileName.toFox ?~> "meshFileName.needed"
+      listMeshChunksRequest = ListMeshChunksRequest(
+        meshFileName,
+        fullMeshRequest.segmentId
+      )
+      before = Instant.now
+      chunkInfos: WebknossosSegmentInfo <- meshFileService
+        .listMeshChunksForSegmentV3(organizationName, datasetName, layerName, listMeshChunksRequest)
+        .toFox
+      allChunkRanges: List[MeshChunk] = chunkInfos.chunks.lods.head.chunks
+      stlEncodedChunks: Seq[Array[Byte]] <- Fox.serialCombined(allChunkRanges) { chunkRange: MeshChunk =>
+        readMeshChunkAsStl(organizationName, datasetName, layerName, meshFileName, chunkRange)
+      }
+      stlOutput = combineEncodedChunksToStl(stlEncodedChunks)
+      _ = logger.info(s"output size: ${stlOutput.length}. took ${Instant.since(before)}")
+    } yield stlOutput
 
-  // TODO: find out if meshfile exists, has right format, and whether it was computed for the selected mapping
-  // TODO: unmap segment ids if needed
-  // TODO: fetch all required chunks for segment ids from meshfile
-  // TODO: puzzle them together
-  // TODO: simplify
+  private def readMeshChunkAsStl(organizationName: String,
+                                 datasetName: String,
+                                 layerName: String,
+                                 meshfileName: String,
+                                 chunkInfo: MeshChunk)(implicit ec: ExecutionContext): Fox[Array[Byte]] =
+    for {
+      (dracoMeshChunkBytes, encoding) <- meshFileService.readMeshChunkV3(
+        organizationName,
+        datasetName,
+        layerName,
+        MeshChunkDataRequestV3List(meshfileName, List(MeshChunkDataRequestV3(chunkInfo.byteOffset, chunkInfo.byteSize)))
+      )
+      _ <- bool2Fox(encoding == "draco") ?~> s"meshfile encoding is $encoding, only draco is supported"
+      stlEncodedChunk <- tryo(
+        dracoToStlConverter
+          .dracoToStl(dracoMeshChunkBytes, chunkInfo.position.x, chunkInfo.position.y, chunkInfo.position.z))
+    } yield stlEncodedChunk
+
+  private def byteBufferToArray(buf: ByteBuffer, numBytes: Int): Array[Byte] = {
+    val arr = new Array[Byte](numBytes)
+    buf.rewind()
+    buf.get(arr)
+    arr
+  }
+
+  private def combineEncodedChunksToStl(stlEncodedChunks: Seq[Array[Byte]]): Array[Byte] = {
+    val numFaces = stlEncodedChunks.map(_.length / 50).sum // our stl implementation writes exactly 50 bytes per face
+    val constantStlHeader = Array.fill[Byte](80)(0)
+    val outputNumBytes = 80 + 4 + stlEncodedChunks.map(_.length).sum
+    val output = ByteBuffer.allocate(outputNumBytes).order(ByteOrder.LITTLE_ENDIAN)
+    output.put(constantStlHeader)
+    output.putInt(numFaces)
+    stlEncodedChunks.foreach(output.put)
+    byteBufferToArray(output, outputNumBytes)
+  }
 }

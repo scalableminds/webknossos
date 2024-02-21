11f01567dbb7e44c3826c433d9198991031d42a8
frcroth <frcroth@gmail.com>
2024-02-21 09:13:53 +0100

    Add stderr again

--- tools/mesh-format/list_old_mesh_files.py
+++ tools/mesh-format/list_old_mesh_files.py
@@ -23,15 +23,21 @@ def main():
     binary_data_dir = args.binary_data_dir
 
     if not args.plain:
-        print(f"Scanning {binary_data_dir} for mesh files")
+        print(f"Scanning {binary_data_dir} for mesh files", file=sys.stderr)
 
     seen = []
 
-    for orga_dir in [item for item in binary_data_dir.iterdir() if item.exists() and item.is_dir()]:
+    for orga_dir in [
+        item for item in binary_data_dir.iterdir() if item.exists() and item.is_dir()
+    ]:
         for dataset_dir in orga_dir.iterdir():
             try:
                 if dataset_dir.exists() and dataset_dir.is_dir():
-                    for layer_dir in [item for item in dataset_dir.iterdir() if item.exists() and item.is_dir()]:
+                    for layer_dir in [
+                        item
+                        for item in dataset_dir.iterdir()
+                        if item.exists() and item.is_dir()
+                    ]:
                         meshes_dir = layer_dir.joinpath("meshes")
                         if meshes_dir.exists():
                             for mesh_file in [
@@ -52,13 +58,21 @@ def main():
                                                 if args.plain:
                                                     print(mesh_file)
                                                 else:
-                                                    print(f"{mesh_file} has version {version}")
+                                                    print(
+                                                        f"{mesh_file} has version {version}"
+                                                    )
             except Exception as e:
                 if not args.plain:
-                    print(f"Exception while scanning dataset dir at {dataset_dir}: {e}")
+                    print(
+                        f"Exception while scanning dataset dir at {dataset_dir}: {e}",
+                        file=sys.stderr,
+                    )
 
     if not args.plain:
-        print(f"Done scanning {binary_data_dir}, listed {len(seen)} meshfiles.")
+        print(
+            f"Done scanning {binary_data_dir}, listed {len(seen)} meshfiles.",
+            file=sys.stderr,
+        )
 
 
 if __name__ == "__main__":

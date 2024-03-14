819510f9acb35410543c10b577c4dcbea7d53fc2
Florian M <florian@scm.io>
2024-03-14 14:28:57 +0100

    store training annotations, add AiModel publicWrites

--- app/models/aimodels/AiModel.scala
+++ app/models/aimodels/AiModel.scala
@@ -1,10 +1,17 @@
 package models.aimodels
 
+import com.scalableminds.util.accesscontext.DBAccessContext
 import com.scalableminds.util.time.Instant
 import com.scalableminds.util.tools.Fox
 import com.scalableminds.webknossos.schema.Tables.{Aimodels, AimodelsRow}
+import models.dataset.{DataStoreDAO, DataStoreService}
+import models.job.{JobDAO, JobService}
+import models.user.{UserDAO, UserService}
 import play.api.libs.json.{JsObject, Json}
+import slick.dbio.{DBIO, Effect, NoStream}
+import slick.jdbc.PostgresProfile.api._
 import slick.lifted.Rep
+import slick.sql.SqlAction
 import utils.ObjectId
 import utils.sql.{SQLDAO, SqlClient}
 
@@ -16,15 +23,37 @@ case class AiModel(_id: ObjectId,
                    _dataStore: String,
                    _user: ObjectId,
                    _trainingJob: Option[ObjectId],
+                   _trainingAnnotations: List[ObjectId],
                    name: String,
                    comment: String,
                    created: Instant,
                    modified: Instant,
                    isDeleted: Boolean)
 
-class AiModelService @Inject()() {
-  def publicWrites(aiModel: AiModel)(implicit ec: ExecutionContext): Fox[JsObject] =
-    Fox.successful(Json.obj("id" -> aiModel._id))
+class AiModelService @Inject()(dataStoreDAO: DataStoreDAO,
+                               dataStoreService: DataStoreService,
+                               userDAO: UserDAO,
+                               userService: UserService,
+                               jobDAO: JobDAO,
+                               jobService: JobService) {
+  def publicWrites(aiModel: AiModel)(implicit ec: ExecutionContext, ctx: DBAccessContext): Fox[JsObject] =
+    for {
+      dataStore <- dataStoreDAO.findOneByName(aiModel._dataStore)
+      user <- userDAO.findOne(aiModel._user)
+      userJs <- userService.compactWrites(user)
+      dataStoreJs <- dataStoreService.publicWrites(dataStore)
+      trainingJobOpt <- Fox.runOptional(aiModel._trainingJob)(jobDAO.findOne)
+      trainingJobJsOpt <- Fox.runOptional(trainingJobOpt)(jobService.publicWrites)
+    } yield
+      Json.obj(
+        "id" -> aiModel._id,
+        "name" -> aiModel.name,
+        "dataStore" -> dataStoreJs,
+        "user" -> userJs,
+        "comment" -> aiModel.comment,
+        "created" -> aiModel.created,
+        "trainingJob" -> trainingJobJsOpt
+      )
 }
 
 class AiModelDAO @Inject()(sqlClient: SqlClient)(implicit ec: ExecutionContext)
@@ -36,31 +65,55 @@ class AiModelDAO @Inject()(sqlClient: SqlClient)(implicit ec: ExecutionContext)
 
   protected def isDeletedColumn(x: Aimodels): Rep[Boolean] = x.isdeleted
 
-  protected def parse(r: AimodelsRow): Fox[AiModel] = Fox.successful(
-    AiModel(
-      ObjectId(r._Id),
-      ObjectId(r._Organization),
-      r._Datastore.trim,
-      ObjectId(r._User),
-      r._Trainingjob.map(ObjectId(_)),
-      r.name,
-      r.comment,
-      Instant.fromSql(r.created),
-      Instant.fromSql(r.modified),
-      r.isdeleted
-    )
-  )
-
-  def insertOne(a: AiModel): Fox[Unit] =
+  protected def parse(r: AimodelsRow): Fox[AiModel] =
     for {
-      _ <- run(q"""INSERT INTO webknossos.aiModels(
-                  _id, _organization, _dataStore, _user, _trainingJob, name,
-                   comment, created, modified, isDeleted
-                ) VALUES(
-                  ${a._id}, ${a._organization}, ${a._dataStore}, ${a._user}, ${a._trainingJob}, ${a.name},
-                  ${a.comment}, ${a.created}, ${a.modified}, ${a.isDeleted}
-                )
-       """.asUpdate)
-    } yield ()
+      trainingAnnotationIds <- findTrainingAnnotationIdsFor(ObjectId(r._Id))
+    } yield
+      AiModel(
+        ObjectId(r._Id),
+        ObjectId(r._Organization),
+        r._Datastore.trim,
+        ObjectId(r._User),
+        r._Trainingjob.map(ObjectId(_)),
+        trainingAnnotationIds,
+        r.name,
+        r.comment,
+        Instant.fromSql(r.created),
+        Instant.fromSql(r.modified),
+        r.isdeleted
+      )
 
+  def insertOne(a: AiModel): Fox[Unit] = {
+    val insertModelQuery =
+      q"""INSERT INTO webknossos.aiModels(
+                      _id, _organization, _dataStore, _user, _trainingJob, name,
+                       comment, created, modified, isDeleted
+                    ) VALUES(
+                      ${a._id}, ${a._organization}, ${a._dataStore}, ${a._user}, ${a._trainingJob}, ${a.name},
+                      ${a.comment}, ${a.created}, ${a.modified}, ${a.isDeleted}
+                    )
+           """.asUpdate
+    val insertTrainingAnnotationQueries = insertTrainingAnnotationIdQueries(a._id, a._trainingAnnotations)
+    for {
+      _ <- run(DBIO.sequence(insertModelQuery +: insertTrainingAnnotationQueries).transactionally)
+    } yield ()
+  }
+
+  private def insertTrainingAnnotationIdQueries(aiModelId: ObjectId,
+                                                annotationIds: List[ObjectId]): List[SqlAction[Int, NoStream, Effect]] =
+    annotationIds.map { annotationId =>
+      insertTrainingAnnotationIdQuery(aiModelId, annotationId)
+    }
+
+  private def insertTrainingAnnotationIdQuery(aiModelId: ObjectId,
+                                              annotationId: ObjectId): SqlAction[Int, NoStream, Effect] =
+    q"""INSERT INTO webknossos.aiModel_trainingAnnotations(_aiModel, _annotation)
+            VALUES($aiModelId, $annotationId)""".asUpdate
+
+  private def findTrainingAnnotationIdsFor(aiModelId: ObjectId): Fox[List[ObjectId]] =
+    for {
+      rows <- run(
+        q"SELECT _annotation FROM webknossos.aiModel_trainingAnnotations WHERE _aiModel = $aiModelId ORDER BY _annotation"
+          .as[ObjectId])
+    } yield rows.toList
 }

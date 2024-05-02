d6618ee77b5d5308be5fc4badf8fc9b8f7763893
Florian M <florian@scm.io>
2024-05-02 17:21:10 +0200

    minor upgrades for backend dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,21 +3,21 @@ import sbt._
 
 object Dependencies {
   private val silhouetteVersion = "10.0.0"
-  private val brotliVersion = "1.11.0"
+  private val brotliVersion = "1.16.0"
   private val scalapbVersion = scalapb.compiler.Version.scalapbVersion
   private val grpcVersion = scalapb.compiler.Version.grpcJavaVersion
 
   val utilDependencies: Seq[ModuleID] = Seq(
     // Play Web Framework. import play
-    "org.playframework" %% "play" % "3.0.1",
+    "org.playframework" %% "play" % "3.0.2",
     // Play’s JSON serialization. import play.api.libs.json
     "com.typesafe.play" %% "play-json" % "2.10.1",
     // Sending emails. import org.apache.commons.mail
     "org.apache.commons" % "commons-email" % "1.5",
     // File utils. import org.apache.commons.io
-    "commons-io" % "commons-io" % "2.9.0",
+    "commons-io" % "commons-io" % "2.16.1",
     // HashCodeBuilder. import org.apache.commons.lang3
-    "org.apache.commons" % "commons-lang3" % "3.12.0",
+    "org.apache.commons" % "commons-lang3" % "3.14.0",
     // Box/Tryo. import net.liftweb
     "net.liftweb" %% "lift-common" % "3.5.0",
     // ObjectIds. import reactivemongo.api.bson
@@ -48,7 +48,7 @@ object Dependencies {
     // Dependency Injection. import javax.inject.Inject
     guice,
     // Handling of unsigned integer types. import spire
-    "org.typelevel" %% "spire" % "0.17.0",
+    "org.typelevel" %% "spire" % "0.18.0",
     // Redis database client. import com.redis
     "net.debasishg" %% "redisclient" % "3.42",
     // Read hdf5 files. import ch.systemsx.cisd.hdf5
@@ -56,13 +56,13 @@ object Dependencies {
     // MultiArray (ndarray) handles. import ucar
     "edu.ucar" % "cdm-core" % "5.4.2",
     // Amazon S3 cloud storage client. import com.amazonaws
-    "com.amazonaws" % "aws-java-sdk-s3" % "1.12.470",
+    "com.amazonaws" % "aws-java-sdk-s3" % "1.12.584",
     // Google cloud storage client. import com.google.cloud.storage, import com.google.auth.oauth2
-    "com.google.cloud" % "google-cloud-storage" % "2.13.1",
+    "com.google.cloud" % "google-cloud-storage" % "2.36.1",
     // Blosc compression. import org.blosc
     "org.lasersonlab" % "jblosc" % "1.0.1",
     // Zstd compression. import org.apache.commons.compress
-    "org.apache.commons" % "commons-compress" % "1.21",
+    "org.apache.commons" % "commons-compress" % "1.26.1",
     // Zstd compression native bindings. not imported
     "com.github.luben" % "zstd-jni" % "1.5.5-5",
     // Brotli compression. import com.aayushatharva.brotli4j
@@ -77,7 +77,7 @@ object Dependencies {
 
   val webknossosTracingstoreDependencies: Seq[ModuleID] = Seq(
     // Graph algorithms. import org.jgrapht
-    "org.jgrapht" % "jgrapht-core" % "1.5.1"
+    "org.jgrapht" % "jgrapht-core" % "1.5.2"
   )
 
   val webknossosDependencies: Seq[ModuleID] = Seq(
@@ -92,19 +92,19 @@ object Dependencies {
     // End-to-end test specs
     specs2 % Test,
     // Writing XML. import com.sun.xml.txw2
-    "org.glassfish.jaxb" % "txw2" % "4.0.2",
+    "org.glassfish.jaxb" % "txw2" % "4.0.5",
     // Makes txw2 write self-closing tags in xml (which we want). Not imported.
     "org.codehaus.woodstox" % "wstx-asl" % "4.0.6",
     // Json Web Tokens (used for OIDC Auth). import pdi.jwt
-    "com.github.jwt-scala" %% "jwt-play-json" % "9.2.0",
+    "com.github.jwt-scala" %% "jwt-play-json" % "10.0.0",
     // SQL Queries. import slick
-    "com.typesafe.slick" %% "slick" % "3.4.1",
+    "com.typesafe.slick" %% "slick" % "3.5.0",
     // SQL Queries connection pool. not imported.
-    "com.typesafe.slick" %% "slick-hikaricp" % "3.4.1",
+    "com.typesafe.slick" %% "slick-hikaricp" % "3.5.1",
     // SQL Queries class generation. Started with runner as slick.codegen.SourceCodeGenerator
-    "com.typesafe.slick" %% "slick-codegen" % "3.4.1",
+    "com.typesafe.slick" %% "slick-codegen" % "3.5.1",
     // SQL Queries postgres specifics. not imported.
-    "org.postgresql" % "postgresql" % "42.5.4"
+    "org.postgresql" % "postgresql" % "42.7.3"
   )
 
   val dependencyOverrides: Seq[ModuleID] = Seq(

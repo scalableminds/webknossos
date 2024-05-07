f66d549fe8418c4ea9814534e288fc3ae059b703
Florian M <florian@scm.io>
2024-05-07 14:08:08 +0200

    remove outdated test

--- test/backend/DataVaultTestSuite.scala
+++ test/backend/DataVaultTestSuite.scala
@@ -66,12 +66,6 @@ class DataVaultTestSuite extends PlaySpec {
           }
         }
         "returns failure" when {
-          "requesting invalid range" in {
-            val result = (vaultPath / dataKey)
-              .readBytes(Some(Range.Long(-10, 10, 1)))(globalExecutionContext)
-              .await(handleFoxJustification)
-            assertBoxFailure(result)
-          }
           "using invalid credentials" in {
             val vaultPath =
               new VaultPath(uri,

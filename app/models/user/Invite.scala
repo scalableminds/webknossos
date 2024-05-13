ff65c34a6611ee58903d0a456941d35928c1f3d9
Florian M <fm3@users.noreply.github.com>
2024-05-13 09:41:42 +0200

    Unify SQL Query Style (#7770)
    * WIP: Unify SQL Query Style
    
    * format more daos
    
    * report, publication, credential, project, team, emailVerificationKey
    
    * orga, token
    
    * project, task
    
    * dataset
    
    * spelling for the rest
    
    * unused import
    
    * first typo fix
    
    * implement pr feedback
    
    * restore test
    
    ---------
    
    Co-authored-by: MichaelBuessemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>

--- app/models/user/Invite.scala
+++ app/models/user/Invite.scala
@@ -113,8 +113,8 @@ class InviteDAO @Inject()(sqlClient: SqlClient)(implicit ec: ExecutionContext)
   def insertOne(i: Invite): Fox[Unit] =
     for {
       _ <- run(
-        q"""insert into webknossos.invites(_id, tokenValue, _organization, autoActivate, expirationDateTime, created, isDeleted)
-            values(${i._id}, ${i.tokenValue}, ${i._organization}, ${i.autoActivate},
+        q"""INSERT INTO webknossos.invites(_id, tokenValue, _organization, autoActivate, expirationDateTime, created, isDeleted)
+            VALUES(${i._id}, ${i.tokenValue}, ${i._organization}, ${i.autoActivate},
             ${i.expirationDateTime}, ${i.created}, ${i.isDeleted})""".asUpdate)
     } yield ()
 

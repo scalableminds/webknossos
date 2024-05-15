e1141cba12cc99810afa759efa512de92c29f9cb
Philipp Otto <philipp.4096@gmail.com>
2024-05-15 14:12:19 +0200

    tweak hovering

--- frontend/javascripts/oxalis/shaders/main_data_shaders.glsl.ts
+++ frontend/javascripts/oxalis/shaders/main_data_shaders.glsl.ts
@@ -293,14 +293,14 @@ void main() {
       /// float proofreadingAlphaIncrement = isActiveCell && isProofreading && <%= segmentationName %>_alpha > 0.0 ? 0.4 : -1.0;
       float alphaIncrement = isProofreading
         ? (isActiveCell
-            ? (isHoveredUnmappedSegment ? 0.3 : 0.0)
+            ? (isHoveredUnmappedSegment ? 0.3 : (isHoveredSegment ? 0.2 : 0.1))
             : (isHoveredSegment
                 ? 0.2
                 // We are in proofreading mode, but the current voxel neither belongs
                 // to the active segment nor is it hovered. When selective visibility
                 // is enabled, lower the opacity.
                 : (selectiveVisibilityInProofreading ? -0.2 : 0.0)
-            )
+          )
         ) : (isHoveredSegment ? 0.2 : 0.0);
       gl_FragColor = vec4(mix(
         data_color.rgb,

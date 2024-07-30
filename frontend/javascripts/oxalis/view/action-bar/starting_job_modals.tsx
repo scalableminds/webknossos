9ecdc92e1a98b556a3064e63021b19b7aaacbd75
Charlie Meister <charlie.meister@student.hpi.de>
2024-07-30 15:30:06 +0200

    disable start job button if dataformat is not supported

--- frontend/javascripts/oxalis/view/action-bar/starting_job_modals.tsx
+++ frontend/javascripts/oxalis/view/action-bar/starting_job_modals.tsx
@@ -514,7 +514,8 @@ function StartJobForm(props: StartJobFormProps) {
   const dataset = useSelector((state: OxalisState) => state.dataset);
   const tracing = useSelector((state: OxalisState) => state.tracing);
   const activeUser = useSelector((state: OxalisState) => state.activeUser);
-  const layers = chooseSegmentationLayer ? getSegmentationLayers(dataset) : getColorLayers(dataset);
+  const colorLayers = getColorLayers(dataset);
+  const layers = chooseSegmentationLayer ? getSegmentationLayers(dataset) : colorLayers;
   const allLayers = getDataLayers(dataset);
   const defaultBBForLayers: UserBoundingBox[] = layers.map((layer, index) => {
     return {
@@ -526,6 +527,7 @@ function StartJobForm(props: StartJobFormProps) {
     };
   });
   const userBoundingBoxes = defaultBBForLayers.concat(rawUserBoundingBoxes);
+  const isDTypeSupported = colorLayers[0].elementClass !== "uint24";
 
   const startJob = async ({
     layerName,
@@ -579,6 +581,25 @@ function StartJobForm(props: StartJobFormProps) {
       console.error(error);
       handleClose();
     }
+  };
+
+  const getButton = () => {
+    if (!isDTypeSupported) {
+      return (
+        <Button type="primary" size="large" htmlType="submit">
+          {props.buttonLabel ? props.buttonLabel : title}
+        </Button>
+      );
+    }
+    return (
+      <div>
+        AI Jobs are not supported for for uInt24 color layers.
+        <br />
+        <Button type="primary" size="large" htmlType="submit" disabled>
+          {props.buttonLabel ? props.buttonLabel : title}
+        </Button>
+      </div>
+    );
   };
 
   let initialLayerName = layers.length === 1 ? layers[0].name : null;
@@ -642,11 +663,7 @@ function StartJobForm(props: StartJobFormProps) {
         onChangeSelectedBoundingBox={(bBoxId) => form.setFieldsValue({ boundingBoxId: bBoxId })}
         value={form.getFieldValue("boundingBoxId")}
       />
-      <div style={{ textAlign: "center" }}>
-        <Button type="primary" size="large" htmlType="submit">
-          {props.buttonLabel ? props.buttonLabel : title}
-        </Button>
-      </div>
+      <div style={{ textAlign: "center" }}>{getButton()}</div>
     </Form>
   );
 }

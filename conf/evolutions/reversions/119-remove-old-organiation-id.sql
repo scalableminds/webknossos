fa119374781c700e116809664ca56580d89611e0
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2024-08-21 13:28:02 +0200

    readd unique contraint in reversion

--- conf/evolutions/reversions/119-remove-old-organiation-id.sql
+++ conf/evolutions/reversions/119-remove-old-organiation-id.sql
@@ -84,6 +84,7 @@ ALTER TABLE webknossos.organizations RENAME COLUMN _id_old TO _id;
 
 ALTER TABLE webknossos.organizations ALTER COLUMN _id TYPE CHAR(24);
 ALTER TABLE webknossos.organizations ALTER COLUMN _id SET NOT NULL;
+ALTER TABLE webknossos.organizations ADD CONSTRAINT organizations_name_key UNIQUE (name);
 -- Drop newly added constraint for naming restrictions on the now called name column.
 ALTER TABLE webknossos.organizations DROP CONSTRAINT validOrganizationId;
 

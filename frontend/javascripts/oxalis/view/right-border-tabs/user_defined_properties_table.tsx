f666426f46468e6f30113ce97888bd32b9eb5900
Philipp Otto <philipp.4096@gmail.com>
2024-09-06 15:32:07 +0200

    fix type error

--- frontend/javascripts/oxalis/view/right-border-tabs/user_defined_properties_table.tsx
+++ frontend/javascripts/oxalis/view/right-border-tabs/user_defined_properties_table.tsx
@@ -14,172 +14,173 @@ import { memo } from "react";
 
 const getKeyInputIdForIndex = (index: number) => `metadata-key-input-id-${index}`;
 
-export const UserDefinedPropertyTableRows = memo(
-  <ItemType extends { userDefinedProperties: UserDefinedProperty[] }>({
-    item,
-    setUserDefinedProperties,
-    readOnly,
-  }: {
-    item: ItemType;
-    setUserDefinedProperties: (item: ItemType, newProperties: UserDefinedProperty[]) => void;
-    readOnly: boolean;
-  }) => {
-    const updateUserDefinedPropertyByIndex = (
-      item: ItemType,
-      index: number,
-      newPropPartial: Partial<UserDefinedProperty>,
-    ) => {
-      if (readOnly) {
-        return;
-      }
-      const newProps = item.userDefinedProperties.map((element, idx) =>
-        idx === index
-          ? {
-              ...element,
-              ...newPropPartial,
-            }
-          : element,
-      );
-
-      if (
-        newProps.some(
-          (el) =>
-            (el.stringValue != null && el.stringListValue != null) ||
-            (el.stringListValue as any) === "" ||
-            Array.isArray(el.stringValue),
-        )
-      ) {
-        console.error("invalid newprops?");
-        debugger;
-      }
-
-      setUserDefinedProperties(item, newProps);
-    };
-
-    const removeUserDefinedPropertyByIndex = (item: ItemType, index: number) => {
-      const newProps = item.userDefinedProperties.filter((_element, idx) => idx !== index);
-      setUserDefinedProperties(item, newProps);
-    };
-
-    const addUserDefinedProperty = (item: ItemType, newProp: UserDefinedProperty) => {
-      const newProps = item.userDefinedProperties.concat([newProp]);
-      setUserDefinedProperties(item, newProps);
-    };
-
-    const getDeleteEntryButton = (_: APIMetadataWithError, index: number) => (
-      <div className="flex-center-child">
-        <Button
-          type="text"
-          disabled={readOnly}
-          style={{ width: 16, height: 19 }}
-          icon={
-            <CloseOutlined
-              style={{
-                color: "var(--ant-color-text-tertiary)",
-                width: 16,
-              }}
-            />
+function _UserDefinedPropertyTableRows<
+  ItemType extends { userDefinedProperties: UserDefinedProperty[] },
+>({
+  item,
+  setUserDefinedProperties,
+  readOnly,
+}: {
+  item: ItemType;
+  setUserDefinedProperties: (item: ItemType, newProperties: UserDefinedProperty[]) => void;
+  readOnly: boolean;
+}) {
+  const updateUserDefinedPropertyByIndex = (
+    item: ItemType,
+    index: number,
+    newPropPartial: Partial<UserDefinedProperty>,
+  ) => {
+    if (readOnly) {
+      return;
+    }
+    const newProps = item.userDefinedProperties.map((element, idx) =>
+      idx === index
+        ? {
+            ...element,
+            ...newPropPartial,
           }
-          onClick={() => {
-            removeUserDefinedPropertyByIndex(item, index);
+        : element,
+    );
+
+    if (
+      newProps.some(
+        (el) =>
+          (el.stringValue != null && el.stringListValue != null) ||
+          (el.stringListValue as any) === "" ||
+          Array.isArray(el.stringValue),
+      )
+    ) {
+      console.error("invalid newprops?");
+      debugger;
+    }
+
+    setUserDefinedProperties(item, newProps);
+  };
+
+  const removeUserDefinedPropertyByIndex = (item: ItemType, index: number) => {
+    const newProps = item.userDefinedProperties.filter((_element, idx) => idx !== index);
+    setUserDefinedProperties(item, newProps);
+  };
+
+  const addUserDefinedProperty = (item: ItemType, newProp: UserDefinedProperty) => {
+    const newProps = item.userDefinedProperties.concat([newProp]);
+    setUserDefinedProperties(item, newProps);
+  };
+
+  const getDeleteEntryButton = (_: APIMetadataWithError, index: number) => (
+    <div className="flex-center-child">
+      <Button
+        type="text"
+        disabled={readOnly}
+        style={{ width: 16, height: 19 }}
+        icon={
+          <CloseOutlined
+            style={{
+              color: "var(--ant-color-text-tertiary)",
+              width: 16,
+            }}
+          />
+        }
+        onClick={() => {
+          removeUserDefinedPropertyByIndex(item, index);
+        }}
+      />
+    </div>
+  );
+
+  const getKeyInput = (record: APIMetadataWithError, index: number) => {
+    return (
+      <div style={{ display: "flex" }}>
+        <InputWithUpdateOnBlur
+          prefix={<TagsOutlined />}
+          className="transparent-input"
+          value={record.key}
+          disabled={readOnly}
+          onChange={(value) => updateUserDefinedPropertyByIndex(item, index, { key: value })}
+          placeholder="Property"
+          size="small"
+          validate={(value: string) => {
+            if (
+              // If all items (except for the current one) have another key,
+              // everything is fine.
+              item.userDefinedProperties.every(
+                (otherItem, idx) => idx === index || otherItem.key !== value,
+              )
+            ) {
+              return null;
+            }
+            return "Each key must be unique and can only be used once.";
           }}
+          id={getKeyInputIdForIndex(index)}
         />
       </div>
     );
+  };
 
-    const getKeyInput = (record: APIMetadataWithError, index: number) => {
-      return (
-        <div style={{ display: "flex" }}>
-          <InputWithUpdateOnBlur
-            prefix={<TagsOutlined />}
-            className="transparent-input"
-            value={record.key}
-            disabled={readOnly}
-            onChange={(value) => updateUserDefinedPropertyByIndex(item, index, { key: value })}
-            placeholder="Property"
-            size="small"
-            validate={(value: string) => {
-              if (
-                // If all items (except for the current one) have another key,
-                // everything is fine.
-                item.userDefinedProperties.every(
-                  (otherItem, idx) => idx === index || otherItem.key !== value,
-                )
-              ) {
-                return null;
-              }
-              return "Each key must be unique and can only be used once.";
-            }}
-            id={getKeyInputIdForIndex(index)}
-          />
-        </div>
-      );
-    };
+  const itemMetadata = item.userDefinedProperties.map((prop) => ({
+    key: prop.key,
+    type:
+      prop.stringValue != null
+        ? APIMetadataEnum.STRING
+        : prop.numberValue != null
+          ? APIMetadataEnum.NUMBER
+          : APIMetadataEnum.STRING_ARRAY,
+    value: prop.stringValue || prop.numberValue || prop.stringListValue || "",
+  }));
 
-    const itemMetadata = item.userDefinedProperties.map((prop) => ({
-      key: prop.key,
-      type:
-        prop.stringValue != null
-          ? APIMetadataEnum.STRING
-          : prop.numberValue != null
-            ? APIMetadataEnum.NUMBER
-            : APIMetadataEnum.STRING_ARRAY,
-      value: prop.stringValue || prop.numberValue || prop.stringListValue || "",
-    }));
-
-    const getValueInput = (record: APIMetadataWithError, index: number) => {
-      return (
-        <MetadataValueInput
-          record={record}
-          index={index}
-          readOnly={readOnly}
-          updateMetadataValue={(
-            indexToUpdate: number,
-            newValue: number | string | string[],
-            type: APIMetadataEnum,
-          ) => {
-            updateUserDefinedPropertyByIndex(item, indexToUpdate, {
-              stringValue: type === APIMetadataEnum.STRING ? (newValue as string) : undefined,
-              stringListValue:
-                type === APIMetadataEnum.STRING_ARRAY ? (newValue as string[]) : undefined,
-              numberValue: type === APIMetadataEnum.NUMBER ? (newValue as number) : undefined,
-              // todop: support bool?
-            });
-          }}
-          availableStrArrayTagOptions={getUsedTagsWithinMetadata(itemMetadata)}
-        />
-      );
-    };
-
-    const addNewEntryWithType = (type: APIMetadata["type"]) => {
-      const indexOfNewEntry = item.userDefinedProperties.length;
-      // Auto focus the key input of the new entry.
-      setTimeout(
-        () => document.getElementById(getKeyInputIdForIndex(indexOfNewEntry))?.focus(),
-        50,
-      );
-      addUserDefinedProperty(item, {
-        key: "",
-        stringValue: type === APIMetadataEnum.STRING ? "" : undefined,
-        numberValue: type === APIMetadataEnum.NUMBER ? 0 : undefined,
-        stringListValue: type === APIMetadataEnum.STRING_ARRAY ? [] : undefined,
-      });
-    };
-
-    const addNewEntryMenuItems = getTypeSelectDropdownMenu(addNewEntryWithType);
-
+  const getValueInput = (record: APIMetadataWithError, index: number) => {
     return (
-      <>
-        <InnerMetadataTable
-          onlyReturnRows
-          metadata={itemMetadata}
-          getKeyInput={getKeyInput}
-          getValueInput={getValueInput}
-          getDeleteEntryButton={getDeleteEntryButton}
-          addNewEntryMenuItems={addNewEntryMenuItems}
-          readOnly={readOnly}
-        />
-      </>
+      <MetadataValueInput
+        record={record}
+        index={index}
+        readOnly={readOnly}
+        updateMetadataValue={(
+          indexToUpdate: number,
+          newValue: number | string | string[],
+          type: APIMetadataEnum,
+        ) => {
+          updateUserDefinedPropertyByIndex(item, indexToUpdate, {
+            stringValue: type === APIMetadataEnum.STRING ? (newValue as string) : undefined,
+            stringListValue:
+              type === APIMetadataEnum.STRING_ARRAY ? (newValue as string[]) : undefined,
+            numberValue: type === APIMetadataEnum.NUMBER ? (newValue as number) : undefined,
+            // todop: support bool?
+          });
+        }}
+        availableStrArrayTagOptions={getUsedTagsWithinMetadata(itemMetadata)}
+      />
     );
-  },
-);
+  };
+
+  const addNewEntryWithType = (type: APIMetadata["type"]) => {
+    const indexOfNewEntry = item.userDefinedProperties.length;
+    // Auto focus the key input of the new entry.
+    setTimeout(() => document.getElementById(getKeyInputIdForIndex(indexOfNewEntry))?.focus(), 50);
+    addUserDefinedProperty(item, {
+      key: "",
+      stringValue: type === APIMetadataEnum.STRING ? "" : undefined,
+      numberValue: type === APIMetadataEnum.NUMBER ? 0 : undefined,
+      stringListValue: type === APIMetadataEnum.STRING_ARRAY ? [] : undefined,
+    });
+  };
+
+  const addNewEntryMenuItems = getTypeSelectDropdownMenu(addNewEntryWithType);
+
+  return (
+    <>
+      <InnerMetadataTable
+        onlyReturnRows
+        metadata={itemMetadata}
+        getKeyInput={getKeyInput}
+        getValueInput={getValueInput}
+        getDeleteEntryButton={getDeleteEntryButton}
+        addNewEntryMenuItems={addNewEntryMenuItems}
+        readOnly={readOnly}
+      />
+    </>
+  );
+}
+
+export const UserDefinedPropertyTableRows = memo(
+  _UserDefinedPropertyTableRows,
+) as typeof _UserDefinedPropertyTableRows;

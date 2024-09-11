1d97356a4975504f2d7f45579b7a9a8f1e5139c6
Florian M <florian@scm.io>
2024-09-11 16:32:32 +0200

    add explaining comment

--- tools/mapping-format/find-json-mappings.py
+++ tools/mapping-format/find-json-mappings.py
@@ -3,30 +3,22 @@ import argparse
 import sys
 import os
 
-
-def format_bytes(num):
-    for unit in ("", "K", "M", "G", "T"):
-        if abs(num) < 1000.0:
-            return f"{int(num)}{unit}"
-        num /= 1000.0
-    return f"{int(num)}P"
-
+# This script searches a binaryData folder for json mapping files.
+# We had hoped to convert all json files to hdf5, but this got deprioritized
+# because it turned out each oversegment group gets mapped to one of the ids of the group
+# agglomerate files on the other hand, are optimized for continuous agglomerate ids
+# This means to preserve the ids of some json mappings, huge and hugely inefficient
+# agglomerate files would be needed.
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("binary_data_dir", type=Path, help="WEBKNOSSOS binary data dir")
     parser.add_argument(
-        "--all",
-        "-a",
-        action="store_true",
-        help="Print all files, not just old versions",
-    )
-    parser.add_argument(
         "--plain",
         "-p",
         action="store_true",
-        help="Print only the file names, not the version",
+        help="Print only the file names, not info output",
     )
     args = parser.parse_args()
     binary_data_dir = args.binary_data_dir
@@ -73,6 +65,14 @@ def main():
             f"\nDone scanning {binary_data_dir}, listed {len(seen)} json mappings.",
             file=sys.stderr,
         )
+
+
+def format_bytes(num):
+    for unit in ("", "K", "M", "G", "T"):
+        if abs(num) < 1000.0:
+            return f"{int(num)}{unit}"
+        num /= 1000.0
+    return f"{int(num)}P"
 
 
 if __name__ == "__main__":

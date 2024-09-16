77f5a514d2e0c2478dfa1c709641bc3ff69c3470
Philipp Otto <philippotto@users.noreply.github.com>
2024-09-16 09:47:57 +0200

    Open experimental alignment services to public (#8026)
    * various style fixes and improvements; WIP advertise alignment in dataset upload [ci skip]
    
    * extend docs; fix links; remove super-user requirement
    
    * temporarily disable some CI checks
    
    * re-enable ci
    
    * temporarily disable ci
    
    * tmp: set iswkorginstance=true
    
    * re-enable ci
    
    * revert tmp changes

--- frontend/javascripts/admin/dataset/dataset_add_view.tsx
+++ frontend/javascripts/admin/dataset/dataset_add_view.tsx
@@ -181,18 +181,23 @@ const alignBanner = (
         width: "100%",
       }}
     />
-    <Button
-      href="https://webknossos.org/services/alignment"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{
-        display: "block",
-        margin: "10px auto",
-        width: "50%",
-      }}
-    >
-      Learn More
-    </Button>
+    <p>
+      <a
+        href="https://docs.webknossos.org/webknossos/automated_analysis.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Single-tile alignment
+      </a>{" "}
+      of image stacks can be done directly in WEBKNOSSOS.
+    </p>
+    <p>
+      For multi-tile stacks, learn about our{" "}
+      <a href="https://webknossos.org/services/alignment" target="_blank" rel="noopener noreferrer">
+        alignment service
+      </a>
+      .
+    </p>
   </div>
 );
 const manualAnnotationBanner = (
@@ -303,7 +308,6 @@ const getPostUploadModal = (
         <div
           style={{
             display: "flex",
-            justifyContent: "center",
           }}
         >
           <div

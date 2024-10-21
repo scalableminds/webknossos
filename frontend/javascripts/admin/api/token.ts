ffda9f174349674926d87559c3665463675605c8
Michael Büßemeyer <frameworklinux+MichaelBuessemeyer@users.noreply.github.com>
2024-10-21 19:10:54 +0200

    WIP auto use user token when sharing token is not sufficient for a request

--- frontend/javascripts/admin/api/token.ts
+++ frontend/javascripts/admin/api/token.ts
@@ -1,6 +1,7 @@
 import { location } from "libs/window";
 import Request from "libs/request";
 import * as Utils from "libs/utils";
+import Toast from "libs/toast";
 
 let tokenPromise: Promise<string>;
 
@@ -33,22 +34,42 @@ export function getSharingTokenFromUrlParameters(): string | null | undefined {
   return null;
 }
 
-export function doWithToken<T>(fn: (token: string) => Promise<T>, tries: number = 1): Promise<any> {
-  const sharingToken = getSharingTokenFromUrlParameters();
+function removeSharingTokenFromURLParameters() {
+  const urlObj = new URL(window.location.href);
+  if (urlObj.searchParams.has("token")) {
+    urlObj.searchParams.delete("token");
+    window.history.replaceState({}, document.title, urlObj.toString()); // somehow it is replaced back to the state where the token exists :///
+    Toast.info("Token URL token was outdated and therefore removed.");
+  }
+}
 
-  if (sharingToken != null) {
-    return fn(sharingToken);
+export async function doWithToken<T>(
+  fn: (token: string) => Promise<T>,
+  tries: number = 1,
+  useURLTokenIfAvailable: boolean = true,
+): Promise<any> {
+  let token = useURLTokenIfAvailable ? getSharingTokenFromUrlParameters() : null;
+
+  if (token == null) {
+    tokenPromise = tokenPromise == null ? requestUserToken() : tokenPromise;
+  } else {
+    tokenPromise = Promise.resolve(token);
   }
 
-  if (!tokenPromise) tokenPromise = requestUserToken();
-  return tokenPromise.then(fn).catch((error) => {
+  return tokenPromise.then(fn).catch(async (error) => {
     if (error.status === 403) {
       console.warn("Token expired. Requesting new token...");
       tokenPromise = requestUserToken();
 
       // If three new tokens did not fix the 403, abort, otherwise we'll get into an endless loop here
       if (tries < 3) {
-        return doWithToken(fn, tries + 1);
+        // If using the url sharing token failed, we try the user specific token instead.
+        const result = await doWithToken(fn, tries + 1, false);
+        // Upon successful retry with own token, discard the url token.
+        if (useURLTokenIfAvailable) {
+          removeSharingTokenFromURLParameters();
+        }
+        return result;
       }
     }
 

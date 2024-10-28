d29f0d621c7981e1830e8d93cee146fedccfae5a
Florian M <florian@scm.io>
2024-10-28 14:30:14 +0100

    delete incompatible models in reversion

--- conf/evolutions/reversions/123-more-model-categories.sql
+++ conf/evolutions/reversions/123-more-model-categories.sql
@@ -4,6 +4,8 @@ do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 1
 
 -- removing enum values is not supported in postgres, see https://www.postgresql.org/docs/current/datatype-enum.html#DATATYPE-ENUM-IMPLEMENTATION-DETAILS
 
+UPDATE webknossos.aiModels SET isDeleted = TRUE WHERE category IN ('em_synapses', 'em_neuron_types', 'em_cell_organelles');
+
 UPDATE webknossos.releaseInformation SET schemaVersion = 122;
 
 COMMIT TRANSACTION;

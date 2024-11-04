da374adaeaf8f6e26c095eba6ab4e9b6c58fd8c5
Michael Büßemeyer <frameworklinux+MichaelBuessemeyer@users.noreply.github.com>
2024-11-04 10:41:01 +0100

    extract magic numbers into constants

--- frontend/javascripts/oxalis/view/right-border-tabs/scrollable_virtualized_tree.tsx
+++ frontend/javascripts/oxalis/view/right-border-tabs/scrollable_virtualized_tree.tsx
@@ -5,6 +5,10 @@ import Constants from "oxalis/constants";
 import { useCallback, useRef } from "react";
 import type RcTree from "rc-tree";
 
+const SCROLL_SPEED_PX = 32;
+const MIN_SCROLL_AREA_HEIGHT = 48;
+const SCROLL_AREA_RATIO = 10; // 1/10th of the container height
+
 function ScrollableVirtualizedTree<T extends BasicDataNode>(
   props: TreeProps<T> & { ref: React.RefObject<RcTree> },
 ) {
@@ -19,13 +23,16 @@ function ScrollableVirtualizedTree<T extends BasicDataNode>(
       const { bottom: currentBottom, top: currentTop } = target.getBoundingClientRect();
       const { bottom: boxBottom, top: boxTop } = wrapperRef.current.getBoundingClientRect();
       const scrollableList = wrapperRef.current.getElementsByClassName("ant-tree-list-holder")[0];
-      const scrollAreaHeight = Math.max(48, Math.round((boxBottom - boxTop) / 10));
+      const scrollAreaHeight = Math.max(
+        MIN_SCROLL_AREA_HEIGHT,
+        Math.round((boxBottom - boxTop) / SCROLL_AREA_RATIO),
+      );
 
       if (currentTop > boxBottom - scrollAreaHeight && scrollableList) {
-        scrollableList.scrollTop += +32;
+        scrollableList.scrollTop += SCROLL_SPEED_PX;
       }
       if (boxTop + scrollAreaHeight > currentBottom && scrollableList) {
-        scrollableList.scrollTop -= 32;
+        scrollableList.scrollTop -= SCROLL_SPEED_PX;
       }
     }, Constants.RESIZE_THROTTLE_TIME),
     [wrapperRef],

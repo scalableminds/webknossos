10c90ceaf4da4e9f5a3b9dc4a96e866f23cc19b9
frcroth <frcroth@gmail.com>
2024-11-11 11:28:53 +0100

    Add dataset upload test without using frontend

--- frontend/javascripts/test/backend-snapshot-tests/datasets.e2e.ts
+++ frontend/javascripts/test/backend-snapshot-tests/datasets.e2e.ts
@@ -9,6 +9,7 @@ import {
 import type { APIDataset } from "types/api_flow_types";
 import * as api from "admin/admin_rest_api";
 import test from "ava";
+import fs from "node:fs";
 
 async function getFirstDataset(): Promise<APIDataset> {
   const datasets = await api.getActiveDatasetsOfMyOrganization();
@@ -107,4 +108,108 @@ test("Zarr 3 streaming", async (t) => {
   const bytes = await rawDataResponse.arrayBuffer();
   const base64 = btoa(String.fromCharCode(...new Uint8Array(bytes.slice(-128))));
   t.snapshot(base64);
+});
+
+test("Dataset upload", async (t) => {
+
+  const uploadId = "test-dataset-upload-" + Date.now();
+
+  await fetch("/data/datasets/reserveUpload",
+    {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify({
+        filePaths: ["test-dataset-upload.zip"],
+        folderId: "570b9f4e4bb848d0885ea917",
+        initialTeams: [],
+        layersToLink: [],
+        name: "test-dataset-upload",
+        organization: "Organization_X",
+        totalFileCount: 1,
+        uploadId: uploadId,
+      }),
+    })
+
+  const filePath = "test/dataset/test-dataset.zip";
+  const testDataset = fs.readFileSync(filePath);
+
+  let formData = new FormData();
+  formData.append("resumableChunkNumber", "1");
+  formData.append("resumableChunkSize", "10485760");
+  formData.append("resumableCurrentChunkSize", "71988");
+  formData.append("resumableTotalSize", "71988");
+  formData.append("resumableType", "application/zip");
+  formData.append("resumableIdentifier", uploadId + "/test-dataset.zip");
+  formData.append("resumableFilename", "test-dataset.zip");
+  formData.append("resumableRelativePath", "test-dataset.zip");
+  formData.append("resumableTotalChunks", "1");
+
+  // Setting the correct content type header automatically does not work (the boundary is not included)
+  // We can not extract the boundary from the FormData object
+  // Thus we have to set the content type header ourselves and create the body manually
+
+  const boundary = "----WebKitFormBoundaryAqTsFa4N9FW7zF7I";
+  let bodyString = `--${boundary}\r\n`;
+  for (const [key, value] of formData.entries()) {
+    bodyString += `Content-Disposition: form-data; name="${key}"\r\n\r\n${value}\r\n`;
+    bodyString += `--${boundary}\r\n`;
+  }
+  bodyString += `Content-Disposition: form-data; name="file"; filename="test-dataset.zip"\r\n`;
+  bodyString += `Content-Type: application/octet-stream\r\n\r\n`;
+
+  // We have to send the file as bytes, otherwise JS does some encoding, resulting in erroneous bytes
+
+  const formBytes = new TextEncoder().encode(bodyString);
+  const fileBytes =new Uint8Array(testDataset);
+  const endBytes = new TextEncoder().encode(`\r\n--${boundary}--`);
+  const body = new Uint8Array(formBytes.length + fileBytes.length + endBytes.length);
+  body.set(formBytes, 0);
+  body.set(fileBytes, formBytes.length);
+  body.set(endBytes, formBytes.length + fileBytes.length);
+
+  let content_type = `multipart/form-data; boundary=${boundary}`;
+
+  const uploadResult = await fetch("/data/datasets",
+    {
+      method: "POST",
+      headers: new Headers(
+        {
+          "Content-Type": content_type,
+        }
+      ),
+      body: body
+    })
+
+  if (uploadResult.status !== 200) {
+    t.fail("Dataset upload failed");
+  }
+
+  const finishResult = await fetch("/data/datasets/finishUpload",
+    {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify({
+        uploadId: uploadId,
+        needsConversion: false
+      })
+    });
+
+  if (finishResult.status !== 200) {
+    t.fail("Dataset upload failed at finish");
+  }
+
+  const result = await fetch("/api/datasets/Organization_X/test-dataset-upload/health",
+    {
+      headers: new Headers(),
+    })
+
+  if (result.status !== 200) {
+    t.fail("Dataset upload failed");
+  }
+  t.pass();
+
 });

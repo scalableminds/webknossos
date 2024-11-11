078fc20a776a84735b43c55f756553b2e0ea0451
Charlie Meister <charliemeister@proton.me>
2024-11-11 11:22:27 +0100

    Remove unnecessary scrollbars in skeleton tab (#8148)
    * make button bar flexbox
    
    * remove unnessecary scrollbars
    
    * line break group names
    
    * style buttons
    
    * remove stand-alone style
    
    * add changelog
    
    * adress review

--- frontend/javascripts/oxalis/view/right-border-tabs/trees_tab/tree_hierarchy_renderers.tsx
+++ frontend/javascripts/oxalis/view/right-border-tabs/trees_tab/tree_hierarchy_renderers.tsx
@@ -87,10 +87,10 @@ export function renderTreeNode(
 
   return (
     <div
-      className="nowrap"
       onContextMenu={(evt) =>
         onOpenContextMenu(createMenuForTree(tree, props, hideContextMenu), evt)
       }
+      style={{ wordBreak: "break-word" }}
     >
       <ColoredDotIcon colorRGBA={[...tree.color, 1.0]} />
       {`(${tree.nodes.size()}) `} {maybeProofreadingIcon} {tree.name}
@@ -208,18 +208,16 @@ export function renderGroupNode(
   const displayableName = name.trim() || "<Unnamed Group>";
   return (
     <div
-      className="nowrap"
       onContextMenu={(evt) =>
         onOpenContextMenu(
           createMenuForTreeGroup(props, hideContextMenu, node, expandedNodeKeys),
           evt,
         )
       }
+      style={{ wordBreak: "break-word" }}
     >
-      <span>
-        <FolderOutlined className="icon-margin-right" />
-        {displayableName}
-      </span>
+      <FolderOutlined className="icon-margin-right" />
+      {displayableName}
     </div>
   );
 }

8f30c3fd38739f0c2c36ceec9e44ff4255ba1046
Florian M <florian@scm.io>
2024-11-13 11:37:47 +0100

    adapt parsing bucket keys (no more morton index)

--- webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/tracings/volume/VolumeTracingBucketHelper.scala
+++ webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/tracings/volume/VolumeTracingBucketHelper.scala
@@ -104,7 +104,7 @@ trait BucketKeys extends WKWDataFormatHelper with AdditionalCoordinateKey {
     }
 
   private def parseBucketKeyXYZ(key: String) = {
-    val keyRx = "([0-9a-z-]+)/(\\d+|\\d+-\\d+-\\d+)/-?\\d+-\\[(\\d+),(\\d+),(\\d+)]".r
+    val keyRx = "([0-9a-z-]+)/(\\d+|\\d+-\\d+-\\d+)/\\[(\\d+),(\\d+),(\\d+)]".r
     key match {
       case keyRx(name, magStr, xStr, yStr, zStr) =>
         getBucketPosition(xStr, yStr, zStr, magStr, None).map(bucketPosition => (name, bucketPosition))
@@ -117,7 +117,7 @@ trait BucketKeys extends WKWDataFormatHelper with AdditionalCoordinateKey {
       key: String,
       additionalAxes: Seq[AdditionalAxis]): Option[(String, BucketPosition)] = {
     val additionalCoordinateCapture = Array.fill(additionalAxes.length)("(\\d+)").mkString(",")
-    val keyRx = s"([0-9a-z-]+)/(\\d+|\\d+-\\d+-\\d+)/-?\\d+-\\[$additionalCoordinateCapture]\\[(\\d+),(\\d+),(\\d+)]".r
+    val keyRx = s"([0-9a-z-]+)/(\\d+|\\d+-\\d+-\\d+)/\\[$additionalCoordinateCapture]\\[(\\d+),(\\d+),(\\d+)]".r
     val matchOpt = keyRx.findFirstMatchIn(key)
     matchOpt match {
       case Some(aMatch) =>

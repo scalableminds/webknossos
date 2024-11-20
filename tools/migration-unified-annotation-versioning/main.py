247fe9c732bfbcc34587d54602dd0d10923e9e5a
Florian M <florian@scm.io>
2024-11-20 11:48:54 +0100

    wip migration: read annotations from postgres

--- tools/migration-unified-annotation-versioning/main.py
+++ tools/migration-unified-annotation-versioning/main.py
@@ -5,21 +5,21 @@ import grpc
 import sys
 import logging
 import datetime
+import psycopg2
 
 import fossildbapi_pb2 as proto
 import fossildbapi_pb2_grpc as proto_rpc
+
 
 def main():
 
     listKeysBatchSize = 300
 
-    src_stub = connect("localhost:2000")
-    dst_stub = connect("localhost:7199")
-
-    test_health(src_stub, f"source fossildb at {src_host}")
-    test_health(dst_stub, f"destination fossildb at {dst_host}")
+    # src_stub = connect("localhost:2000")
+    # dst_stub = connect("localhost:7199")
 
     annotations = read_annotation_list()
+    print(annotations)
 
     for annotation in annotations:
         migrate_annotation(annotation)
@@ -34,18 +34,30 @@ def migrate_annotation(annotation):
 def migrate_updates(annotation):
     layers = annotation.layers
 
-def migrate_materialized_layers(annotation):
+def migrate_materialized_layers(annotation, layer_version_mapping):
     for layer in annotation.layers:
         migrate_materialized_layer(layer, layer_version_mapping)
 
 def migrate_materialized_layer(layer, layer_version_mapping):
     if layer.type == "Skeleton":
         migrate_skeleton_proto(layer, layer_version_mapping)
-    if layer_type == "Volume"
+    if layer.type == "Volume":
         migrate_volume_proto(layer, layer_version_mapping)
         migrate_volume_buckets(layer, layer_version_mapping)
         migrate_segment_index(layer, layer_version_mapping)
         migrate_editable_mapping(layer, layer_version_mapping)
+
+def migrate_skeleton_proto(layer, layer_version_mapping):
+    pass
+
+def migrate_volume_proto(layer, layer_version_mapping):
+   pass
+
+def migrate_volume_buckets(layer, layer_version_mapping):
+   pass
+
+def migrate_segment_index(layer, layer_version_mapping):
+   pass
 
 def migrate_editable_mapping(layer, layer_version_mapping):
     migrate_editable_mapping_info(layer, layer_version_mapping)
@@ -64,9 +76,14 @@ def migrate_editable_mapping_segment_to_agglomerate(layer, layer_version_mapping
 def insert_annotation_protos(annotation, layer_version_mapping):
     pass
 
-def read_annotation_list():
-    return []
 
+def read_annotation_list():
+    connection = psycopg2.connect(host="localhost", port=5432, database="webknossos", user='postgres', password='postgres')
+    cursor = connection.cursor()
+    query = "SELECT _id FROM webknossos.annotations"
+    cursor.execute(query)
+    records = cursor.fetchall()
+    return records
 
 def connect(host):
     MAX_MESSAGE_LENGTH = 2147483647
@@ -79,7 +96,7 @@ def connect(host):
 def test_health(stub, label):
     try:
         reply = stub.Health(proto.HealthRequest())
-        assertSuccess(reply)
+        assert_success(reply)
         print('successfully connected to ' + label)
     except Exception as e:
         print('failed to connect to ' + label + ': ' + str(e))

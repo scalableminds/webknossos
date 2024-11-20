44bf6fc27fda9f9747f6c6c371ef03a11f3392a0
Florian M <florian@scm.io>
2024-11-20 14:17:37 +0100

    fetch annotation info from postgres

--- tools/migration-unified-annotation-versioning/main.py
+++ tools/migration-unified-annotation-versioning/main.py
@@ -6,46 +6,74 @@ import sys
 import logging
 import datetime
 import psycopg2
+import psycopg2.extras
+import math
+import logging
+import datetime
 
 import fossildbapi_pb2 as proto
 import fossildbapi_pb2_grpc as proto_rpc
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 
 def main():
+    setup_logging()
+    logger.info("Hello from Unified Annotation Versioning Migration!")
 
     listKeysBatchSize = 300
 
     # src_stub = connect("localhost:2000")
     # dst_stub = connect("localhost:7199")
 
-    annotations = read_annotation_list()
-    print(annotations)
+    start_time = datetime.datetime.now()
+
+    logger.info(f"Using start time {start_time}")
+
+    annotations = read_annotation_list(start_time)
 
     for annotation in annotations:
         migrate_annotation(annotation)
 
 
+def setup_logging():
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+
 def migrate_annotation(annotation):
-    print(f"Migrating annotation {annotation}")
+    print(f"Migrating annotation {annotation['_id']}...")
     # layerId → {version_before → version_after}
     layer_version_mapping = migrate_updates(annotation)
     migrate_materialized_layers(annotation, layer_version_mapping)
 
+
 def migrate_updates(annotation):
-    layers = annotation.layers
+    print(annotation)
+    layers = annotation["layers"]
+
 
 def migrate_materialized_layers(annotation, layer_version_mapping):
-    for layer in annotation.layers:
-        migrate_materialized_layer(layer, layer_version_mapping)
+    for tracing_id in annotation["layers"]:
+        migrate_materialized_layer(tracing_id, annotation["layers"][tracing_id], layer_version_mapping)
 
-def migrate_materialized_layer(layer, layer_version_mapping):
-    if layer.type == "Skeleton":
-        migrate_skeleton_proto(layer, layer_version_mapping)
-    if layer.type == "Volume":
-        migrate_volume_proto(layer, layer_version_mapping)
-        migrate_volume_buckets(layer, layer_version_mapping)
-        migrate_segment_index(layer, layer_version_mapping)
-        migrate_editable_mapping(layer, layer_version_mapping)
+
+def migrate_materialized_layer(tracing_id, layer_type, layer_version_mapping):
+    if layer_type == "Skeleton":
+        migrate_skeleton_proto(tracing_id, layer_version_mapping)
+    if layer_type == "Volume":
+        migrate_volume_proto(tracing_id, layer_version_mapping)
+        migrate_volume_buckets(tracing_id, layer_version_mapping)
+        migrate_segment_index(tracing_id, layer_version_mapping)
+        migrate_editable_mapping(tracing_id, layer_version_mapping)
+
 
 def migrate_skeleton_proto(layer, layer_version_mapping):
     pass
@@ -77,13 +105,31 @@ def insert_annotation_protos(annotation, layer_version_mapping):
     pass
 
 
-def read_annotation_list():
+def read_annotation_list(start_time: datetime):
+    logger.info("Determining annotation count from postgres...")
+    page_size = 1
     connection = psycopg2.connect(host="localhost", port=5432, database="webknossos", user='postgres', password='postgres')
-    cursor = connection.cursor()
-    query = "SELECT _id FROM webknossos.annotations"
-    cursor.execute(query)
-    records = cursor.fetchall()
-    return records
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    modified_str = start_time.strftime("'%Y-%m-%d %H:%M:%S'")
+    cursor.execute(f"SELECT COUNT(*) FROM webknossos.annotations WHERE modified < {modified_str}")
+    annotation_count = cursor.fetchone()['count']
+    logger.info(f"Loading infos of {annotation_count} annotations from postgres...")
+    annotations = []
+    for page_num in range(math.ceil(annotation_count / page_size)):
+        query = f"""
+            SELECT a._id, a.created, a.modified, JSON_OBJECT_AGG(al.tracingId, al.typ) AS layers
+            FROM webknossos.annotation_layers al
+            JOIN webknossos.annotations a on al._annotation = a._id
+            WHERE a.modified < {modified_str}
+            GROUP BY a._id
+            ORDER BY a._id
+            LIMIT {page_size}
+            OFFSET {page_size * page_num}
+            """
+        cursor.execute(query)
+        annotations += cursor.fetchall()
+    return annotations
+
 
 def connect(host):
     MAX_MESSAGE_LENGTH = 2147483647

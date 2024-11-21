4cb9b75047d1c6db2d679496bb5bd3747838e1ff
Florian M <florian@scm.io>
2024-11-21 14:31:02 +0100

    wip migrate updates

--- tools/migration-unified-annotation-versioning/main.py
+++ tools/migration-unified-annotation-versioning/main.py
@@ -3,8 +3,6 @@
 import argparse
 import grpc
 import sys
-import logging
-import datetime
 import psycopg2
 import psycopg2.extras
 from psycopg2.extras import RealDictRow
@@ -19,27 +17,14 @@ import fossildbapi_pb2 as proto
 import fossildbapi_pb2_grpc as proto_rpc
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 
 def main():
     setup_logging()
     logger.info("Hello from Unified Annotation Versioning Migration!")
-
-    listKeysBatchSize = 300
-
-    # src_stub = connect_to_fossildb("localhost:2000")
-    # dst_stub = connect_to_fossildb("localhost:7199")
-
-    start_time = datetime.datetime.now()
-
-    logger.info(f"Using start time {start_time}")
-
-    annotations = read_annotation_list(start_time)
-
-    for annotation in annotations:
-        migrate_annotation(annotation)
-
+    args = {}
+    migration = Migration(args)
+    migration.run()
 
 def setup_logging():
     root = logging.getLogger()
@@ -51,100 +36,134 @@ def setup_logging():
     handler.setFormatter(formatter)
     root.addHandler(handler)
 
+class Migration:
 
-def migrate_annotation(annotation):
-    print(f"Migrating annotation {annotation['_id']} ...")
-    # layerId → {version_before → version_after}
-    layer_version_mapping = migrate_updates(annotation)
-    migrate_materialized_layers(annotation, layer_version_mapping)
+    def __init__(self, args):
+        self.args = args
+        self.src_stub = connect_to_fossildb("localhost:7155")
+        # self.dst_stub = connect_to_fossildb("localhost:7199")
 
+    def run(self):
+        start_time = datetime.datetime.now()
 
-def migrate_updates(annotation) -> Dict[str, Dict[int, int]]:
-    layers = annotation["layers"]
-    # TODO
-    return {}
+        logger.info(f"Using start time {start_time}")
 
+        annotations = self.read_annotation_list(start_time)
 
-def migrate_materialized_layers(annotation: RealDictRow, layer_version_mapping):
-    for tracing_id in annotation["layers"]:
-        migrate_materialized_layer(tracing_id, annotation["layers"][tracing_id], layer_version_mapping)
-
-
-def migrate_materialized_layer(tracing_id, layer_type, layer_version_mapping):
-    if layer_type == "Skeleton":
-        migrate_skeleton_proto(tracing_id, layer_version_mapping)
-    if layer_type == "Volume":
-        migrate_volume_proto(tracing_id, layer_version_mapping)
-        migrate_volume_buckets(tracing_id, layer_version_mapping)
-        migrate_segment_index(tracing_id, layer_version_mapping)
-        migrate_editable_mapping(tracing_id, layer_version_mapping)
+        for annotation in annotations:
+            self.migrate_annotation(annotation)
 
 
-def migrate_skeleton_proto(layer, layer_version_mapping):
-    pass
 
 
-def migrate_volume_proto(layer, layer_version_mapping):
-   pass
+    def migrate_annotation(self, annotation):
+        print(f"Migrating annotation {annotation['_id']} ...")
+        # layerId → {version_before → version_after}
+        layer_version_mapping = self.migrate_updates(annotation)
+        self.migrate_materialized_layers(annotation, layer_version_mapping)
 
 
-def migrate_volume_buckets(layer, layer_version_mapping):
-   pass
+    def migrate_updates(self, annotation) -> Dict[str, Dict[int, int]]:
+        layers = annotation["layers"]
+        global_version = 0
+        for tracing_id, tracing_type in annotation["layers"].items():
+            old_newest_version = self.get_newest_version(tracing_id, tracing_type)
+        # TODO
+        return {}
+
+    def get_newest_version(self, tracing_id: str, layer_type: str) -> int:
+        collection = self.update_collection_for_layer_type(layer_type)
+        getReply = self.src_stub.Get(
+            proto.GetRequest(collection=collection, key=tracing_id, mayBeEmpty=True)
+        )
+        assert_success(getReply)
+        return getReply.actualVersion
+
+    def update_collection_for_layer_type(self, layer_type):
+        if layer_type == "Skeleton":
+            return "skeletonUpdates"
+        return "volumeUpdates"
+
+    def migrate_materialized_layers(self, annotation: RealDictRow, layer_version_mapping):
+        for tracing_id, tracing_type in annotation["layers"].items():
+            self.migrate_materialized_layer(tracing_id, tracing_type, layer_version_mapping)
 
 
-def migrate_segment_index(layer, layer_version_mapping):
-   pass
+    def migrate_materialized_layer(self, tracing_id, layer_type, layer_version_mapping):
+        if layer_type == "Skeleton":
+            self.migrate_skeleton_proto(tracing_id, layer_version_mapping)
+        if layer_type == "Volume":
+            self.migrate_volume_proto(tracing_id, layer_version_mapping)
+            self.migrate_volume_buckets(tracing_id, layer_version_mapping)
+            self.migrate_segment_index(tracing_id, layer_version_mapping)
+            self.migrate_editable_mapping(tracing_id, layer_version_mapping)
 
 
-def migrate_editable_mapping(layer, layer_version_mapping):
-    migrate_editable_mapping_info(layer, layer_version_mapping)
-    migrate_editable_mapping_agglomerate_to_graph(layer, layer_version_mapping)
-    migrate_editable_mapping_segment_to_agglomerate(layer, layer_version_mapping)
+    def migrate_skeleton_proto(self, layer, layer_version_mapping):
+        pass
 
 
-def migrate_editable_mapping_info(layer, layer_version_mapping):
-    pass
+    def migrate_volume_proto(self, layer, layer_version_mapping):
+       pass
 
 
-def migrate_editable_mapping_agglomerate_to_graph(layer, layer_version_mapping):
-    pass
+    def migrate_volume_buckets(self, layer, layer_version_mapping):
+       pass
 
 
-def migrate_editable_mapping_segment_to_agglomerate(layer, layer_version_mapping):
-    pass
+    def migrate_segment_index(self, layer, layer_version_mapping):
+       pass
 
 
-def insert_annotation_protos(annotation, layer_version_mapping):
-    pass
+    def migrate_editable_mapping(self, layer, layer_version_mapping):
+        self.migrate_editable_mapping_info(layer, layer_version_mapping)
+        self.migrate_editable_mapping_agglomerate_to_graph(layer, layer_version_mapping)
+        self.migrate_editable_mapping_segment_to_agglomerate(layer, layer_version_mapping)
 
 
-def read_annotation_list(start_time: datetime):
-    before = time.time()
-    logger.info("Determining annotation count from postgres...")
-    page_size = 100
-    connection = psycopg2.connect(host="localhost", port=5432, database="webknossos", user='postgres', password='postgres')
-    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    modified_str = start_time.strftime("'%Y-%m-%d %H:%M:%S'")
-    cursor.execute(f"SELECT COUNT(*) FROM webknossos.annotations WHERE modified < {modified_str}")
-    annotation_count = cursor.fetchone()['count']
-    logger.info(f"Loading infos of {annotation_count} annotations from postgres ...")
-    annotations = []
-    page_count = math.ceil(annotation_count / page_size)
-    for page_num in track(range(page_count), total=page_count, description=f"Loading annotation infos ..."):
-        query = f"""
-            SELECT a._id, a.created, a.modified, JSON_OBJECT_AGG(al.tracingId, al.typ) AS layers
-            FROM webknossos.annotation_layers al
-            JOIN webknossos.annotations a on al._annotation = a._id
-            WHERE a.modified < {modified_str}
-            GROUP BY a._id
-            ORDER BY a._id
-            LIMIT {page_size}
-            OFFSET {page_size * page_num}
-            """
-        cursor.execute(query)
-        annotations += cursor.fetchall()
-    logger.info(f"Loading annotations took {time.time() - before} s")
-    return annotations
+    def migrate_editable_mapping_info(self, layer, layer_version_mapping):
+        pass
+
+
+    def migrate_editable_mapping_agglomerate_to_graph(self, layer, layer_version_mapping):
+        pass
+
+
+    def migrate_editable_mapping_segment_to_agglomerate(self, layer, layer_version_mapping):
+        pass
+
+
+    def insert_annotation_protos(self, annotation, layer_version_mapping):
+        pass
+
+
+    def read_annotation_list(self, start_time: datetime):
+        before = time.time()
+        logger.info("Determining annotation count from postgres...")
+        page_size = 10000
+        connection = psycopg2.connect(host="localhost", port=5432, database="webknossos", user='postgres', password='postgres')
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        modified_str = start_time.strftime("'%Y-%m-%d %H:%M:%S'")
+        cursor.execute(f"SELECT COUNT(*) FROM webknossos.annotations WHERE modified < {modified_str}")
+        annotation_count = cursor.fetchone()['count']
+        logger.info(f"Loading infos of {annotation_count} annotations from postgres ...")
+        annotations = []
+        page_count = math.ceil(annotation_count / page_size)
+        for page_num in track(range(page_count), total=page_count, description=f"Loading annotation infos ..."):
+            query = f"""
+                SELECT a._id, a.created, a.modified, JSON_OBJECT_AGG(al.tracingId, al.typ) AS layers
+                FROM webknossos.annotation_layers al
+                JOIN webknossos.annotations a on al._annotation = a._id
+                WHERE a.modified < {modified_str}
+                GROUP BY a._id
+                ORDER BY a._id
+                LIMIT {page_size}
+                OFFSET {page_size * page_num}
+                """
+            cursor.execute(query)
+            annotations += cursor.fetchall()
+        logger.info(f"Loading annotations took {time.time() - before} s")
+        return annotations
 
 
 def connect_to_fossildb(host):

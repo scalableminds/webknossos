scalableminds/webknossos

commit cba70af92fd0f02d08ee250341461fa985142324

Provide clean annotation uri, no more Explorational vs Task (#6208)
* [WIP] provide clean annotation uri

* don't use annotation type from url when loading an annotation

* fix some @ts-expect-error directives

* remove annotationTyp parameter for accessibleBySwitching route

* change annotationType to maybeCompoundType as it's only needed for compound annotations; add new route without type parameter

* fix linting

* drop more Explorationals/Tasks from urls and adapt url_manager.ts

* update changelog

Co-authored-by: Philipp Otto <philipp.4096@googlemail.com>
Co-authored-by: Philipp Otto <philippotto@users.noreply.github.com>
Co-authored-by: Daniel <daniel.werner@scalableminds.com>

frontend/javascripts/admin/task/task_create_form_view.tsx
@@ -429,12 +429,12 @@ class TaskCreateFormView extends React.PureComponent<Props, State> {
 
                     const annotationResponse =
                       (await tryToAwaitPromise(
-                        getAnnotationInformation(value, "Task", {
+                        getAnnotationInformation(value, {
                           showErrorToast: false,
                         }),
                       )) ||
                       (await tryToAwaitPromise(
-                        getAnnotationInformation(value, "Explorational", {
+                        getAnnotationInformation(value, {
                           showErrorToast: false,
                         }),
                       ));

app/controllers/AnnotationController.scala
@@ -98,12 +98,31 @@ class AnnotationController @Inject()(
           } else Fox.successful(())
         }
         _ = Fox.runOptional(request.identity)(user => userDAO.updateLastActivity(user._id))
-        _ = request.identity.map { user =>
+        _ = request.identity.foreach { user =>
           analyticsService.track(OpenAnnotationEvent(user, annotation))
         }
       } yield Ok(js)
     }
   }
+
+  @ApiOperation(hidden = true, value = "")
+  def infoWithoutType(annotationId: String, timestamp: Long): Action[AnyContent] =
+    sil.UserAwareAction.async { implicit request =>
+      for {
+        annotationIdValidated <- ObjectId.parse(annotationId)
+        annotation <- annotationDAO.findOne(annotationIdValidated) ?~> "annotation.notFound"
+        typ = if (annotation._task.isEmpty) AnnotationType.Explorational else AnnotationType.Task
+        restrictions <- provider.restrictionsFor(typ.toString, annotationId) ?~> "restrictions.notFound" ~> NOT_FOUND
+        _ <- restrictions.allowAccess(request.identity) ?~> "notAllowed" ~> FORBIDDEN
+        js <- annotationService.publicWrites(annotation, request.identity, Some(restrictions)) ?~> "annotation.write.failed"
+        _ <- Fox.runOptional(request.identity) { user =>
+          timeSpanService.logUserInteraction(timestamp, user, annotation) // log time when a user starts working
+        }
+        _ = request.identity.foreach { user =>
+          analyticsService.track(OpenAnnotationEvent(user, annotation))
+        }
+      } yield Ok(js)
+    }
 
   @ApiOperation(hidden = true, value = "")
   def merge(typ: String, id: String, mergedTyp: String, mergedId: String): Action[AnyContent] =

app/controllers/AuthenticationController.scala
@@ -14,7 +14,7 @@ import com.scalableminds.util.tools.{Fox, FoxImplicits, TextUtils}
 import javax.inject.Inject
 import models.analytics.{AnalyticsService, InviteEvent, JoinOrganizationEvent, SignupEvent}
 import models.annotation.AnnotationState.Cancelled
-import models.annotation.{AnnotationDAO, AnnotationInformationProvider}
+import models.annotation.{AnnotationDAO, AnnotationIdentifier, AnnotationInformationProvider}
 import models.binary.DataSetDAO
 import models.organization.{Organization, OrganizationDAO, OrganizationService}
 import models.user._
@@ -199,19 +199,14 @@ class AuthenticationController @Inject()(
 
   def accessibleBySwitching(organizationName: Option[String],
                             dataSetName: Option[String],
-                            annotationTyp: Option[String],
                             annotationId: Option[String]): Action[AnyContent] = sil.SecuredAction.async {
     implicit request =>
       for {
         isSuperuser <- multiUserDAO.findOne(request.identity._multiUser).map(_.isSuperUser)
         selectedOrganization <- if (isSuperuser)
-          accessibleBySwitchingForSuperUser(organizationName, dataSetName, annotationTyp, annotationId)
+          accessibleBySwitchingForSuperUser(organizationName, dataSetName, annotationId)
         else
-          accessibleBySwitchingForMultiUser(request.identity._multiUser,
-                                            organizationName,
-                                            dataSetName,
-                                            annotationTyp,
-                                            annotationId)
+          accessibleBySwitchingForMultiUser(request.identity._multiUser, organizationName, dataSetName, annotationId)
         _ <- bool2Fox(selectedOrganization._id != request.identity._organization) // User is already in correct orga, but still could not see dataset. Assume this had a reason.
         selectedOrganizationJs <- organizationService.publicWrites(selectedOrganization)
       } yield Ok(selectedOrganizationJs)
@@ -219,19 +214,18 @@ class AuthenticationController @Inject()(
 
   private def accessibleBySwitchingForSuperUser(organizationNameOpt: Option[String],
                                                 dataSetNameOpt: Option[String],
-                                                annotationTypOpt: Option[String],
                                                 annotationIdOpt: Option[String]): Fox[Organization] = {
     implicit val ctx: DBAccessContext = GlobalAccessContext
-    (organizationNameOpt, dataSetNameOpt, annotationTypOpt, annotationIdOpt) match {
-      case (Some(organizationName), Some(dataSetName), None, None) =>
+    (organizationNameOpt, dataSetNameOpt, annotationIdOpt) match {
+      case (Some(organizationName), Some(dataSetName), None) =>
         for {
           organization <- organizationDAO.findOneByName(organizationName)
           _ <- dataSetDAO.findOneByNameAndOrganization(dataSetName, organization._id)
         } yield organization
-      case (None, None, Some(_), Some(annotationId)) =>
+      case (None, None, Some(annotationId)) =>
         for {
           annotationObjectId <- ObjectId.parse(annotationId)
-          annotation <- annotationDAO.findOne(annotationObjectId) // Note: this does not work for compound annotations. Future work.
+          annotation <- annotationDAO.findOne(annotationObjectId) // Note: this does not work for compound annotations.
           user <- userDAO.findOne(annotation._user)
           organization <- organizationDAO.findOne(user._organization)
         } yield organization
@@ -242,26 +236,24 @@ class AuthenticationController @Inject()(
   private def accessibleBySwitchingForMultiUser(multiUserId: ObjectId,
                                                 organizationName: Option[String],
                                                 dataSetName: Option[String],
-                                                annotationTyp: Option[String],
                                                 annotationId: Option[String]): Fox[Organization] =
     for {
       identities <- userDAO.findAllByMultiUser(multiUserId)
       selectedIdentity <- Fox.find(identities)(identity =>
-        canAccessDatasetOrAnnotation(identity, organizationName, dataSetName, annotationTyp, annotationId))
+        canAccessDatasetOrAnnotation(identity, organizationName, dataSetName, annotationId))
       selectedOrganization <- organizationDAO.findOne(selectedIdentity._organization)(GlobalAccessContext)
     } yield selectedOrganization
 
   private def canAccessDatasetOrAnnotation(user: User,
                                            organizationNameOpt: Option[String],
                                            dataSetNameOpt: Option[String],
-                                           annotationTypOpt: Option[String],
                                            annotationIdOpt: Option[String]): Fox[Boolean] = {
     val ctx = AuthorizedAccessContext(user)
-    (organizationNameOpt, dataSetNameOpt, annotationTypOpt, annotationIdOpt) match {
-      case (Some(organizationName), Some(dataSetName), None, None) =>
+    (organizationNameOpt, dataSetNameOpt, annotationIdOpt) match {
+      case (Some(organizationName), Some(dataSetName), None) =>
         canAccessDataset(ctx, organizationName, dataSetName)
-      case (None, None, Some(annotationTyp), Some(annotationId)) =>
-        canAccessAnnotation(user, ctx, annotationTyp, annotationId)
+      case (None, None, Some(annotationId)) =>
+        canAccessAnnotation(user, ctx, annotationId)
       case _ => Fox.failure("Can either test access for dataset or annotation, not both")
     }
   }
@@ -274,14 +266,12 @@ class AuthenticationController @Inject()(
     foundFox.futureBox.map(_.isDefined)
   }
 
-  private def canAccessAnnotation(user: User,
-                                  ctx: DBAccessContext,
-                                  annotationTyp: String,
-                                  annotationId: String): Fox[Boolean] = {
+  private def canAccessAnnotation(user: User, ctx: DBAccessContext, annotationId: String): Fox[Boolean] = {
     val foundFox = for {
-      annotation <- annotationProvider.provideAnnotation(annotationTyp, annotationId, user)(ctx)
+      annotationIdParsed <- ObjectId.parse(annotationId)
+      annotation <- annotationDAO.findOne(annotationIdParsed)(GlobalAccessContext)
       _ <- bool2Fox(annotation.state != Cancelled)
-      restrictions <- annotationProvider.restrictionsFor(annotationTyp, annotationId)(ctx)
+      restrictions <- annotationProvider.restrictionsFor(AnnotationIdentifier(annotation.typ, annotationIdParsed))(ctx)
       _ <- restrictions.allowAccess(user)
     } yield ()
     foundFox.futureBox.map(_.isDefined)

conf/webknossos.latest.routes
@@ -19,7 +19,7 @@ GET           /auth/token                                                       
 DELETE        /auth/token                                                               controllers.AuthenticationController.deleteToken
 GET           /auth/switch                                                              controllers.AuthenticationController.switchMultiUser(to: String)
 POST          /auth/switchOrganization/:organizationName                                controllers.AuthenticationController.switchOrganization(organizationName: String)
-GET           /auth/accessibleBySwitching                                               controllers.AuthenticationController.accessibleBySwitching(organizationName: Option[String], dataSetName: Option[String], annotationTyp: Option[String], annotationId: Option[String])
+GET           /auth/accessibleBySwitching                                               controllers.AuthenticationController.accessibleBySwitching(organizationName: Option[String], dataSetName: Option[String], annotationId: Option[String])
 POST          /auth/sendInvites                                                         controllers.AuthenticationController.sendInvites
 POST          /auth/startResetPassword                                                  controllers.AuthenticationController.handleStartResetPassword
 POST          /auth/changePassword                                                      controllers.AuthenticationController.changePassword
@@ -117,6 +117,7 @@ PATCH         /annotations/:typ/:id/reopen                                      
 PUT           /annotations/:typ/:id/reset                                               controllers.AnnotationController.reset(typ: String, id: String)
 PATCH         /annotations/:typ/:id/transfer                                            controllers.AnnotationController.transfer(typ: String, id: String)
 
+GET           /annotations/:id/info                                                     controllers.AnnotationController.infoWithoutType(id: String, timestamp: Long)
 GET           /annotations/:typ/:id/info                                                controllers.AnnotationController.info(typ: String, id: String, timestamp: Long)
 PATCH         /annotations/:typ/:id/makeHybrid                                          controllers.AnnotationController.makeHybrid(typ: String, id: String, fallbackLayerName: Option[String])
 PATCH         /annotations/:typ/:id/downsample                                          controllers.AnnotationController.downsample(typ: String, id: String, tracingId: String)

frontend/javascripts/admin/admin_rest_api.ts
@@ -54,6 +54,7 @@ import type {
   ServerTracing,
   TracingType,
   WkConnectDatasetConfig,
+  APICompoundType,
 } from "types/api_flow_types";
 import { APIAnnotationTypeEnum } from "types/api_flow_types";
 import type { Vector3, Vector6 } from "oxalis/constants";
@@ -63,7 +64,6 @@ import type {
   PartialDatasetConfiguration,
   Tracing,
   TraceOrViewCommand,
-  AnnotationType,
   MappingType,
   VolumeTracing,
   UserConfiguration,
@@ -708,11 +708,25 @@ export function copyAnnotationToUserAccount(
 
 export async function getAnnotationInformation(
   annotationId: string,
-  annotationType: APIAnnotationType,
+  options: RequestOptions = {},
+): Promise<APIAnnotation> {
+  const infoUrl = `/api/annotations/${annotationId}/info?timestamp=${Date.now()}`;
+  const annotationWithMessages = await Request.receiveJSON(infoUrl, options);
+
+  // Extract the potential messages property before returning the task to avoid
+  // failing e2e tests in annotations.e2e.ts
+  const { messages: _messages, ...annotation } = annotationWithMessages;
+  return annotation;
+}
+
+export async function getAnnotationCompoundInformation(
+  annotationId: string,
+  annotationType: APICompoundType,
   options: RequestOptions = {},
 ): Promise<APIAnnotation> {
   const infoUrl = `/api/annotations/${annotationType}/${annotationId}/info?timestamp=${Date.now()}`;
   const annotationWithMessages = await Request.receiveJSON(infoUrl, options);
+
   // Extract the potential messages property before returning the task to avoid
   // failing e2e tests in annotations.e2e.ts
   const { messages: _messages, ...annotation } = annotationWithMessages;
@@ -733,8 +747,8 @@ export function getEmptySandboxAnnotationInformation(
 export function createExplorational(
   datasetId: APIDatasetId,
   typ: TracingType,
-  fallbackLayerName: string | null | undefined,
-  resolutionRestrictions: APIResolutionRestrictions | null | undefined,
+  fallbackLayerName?: string | null | undefined,
+  resolutionRestrictions?: APIResolutionRestrictions | null | undefined,
   options: RequestOptions = {},
 ): Promise<APIAnnotation> {
   const url = `/api/datasets/${datasetId.owningOrganization}/${datasetId.name}/createExplorational`;
@@ -1752,12 +1766,11 @@ export async function updateOrganization(
 }
 
 export async function isDatasetAccessibleBySwitching(
-  annotationType: AnnotationType,
   commandType: TraceOrViewCommand,
 ): Promise<APIOrganization | null | undefined> {
   if (commandType.type === ControlModeEnum.TRACE) {
     return Request.receiveJSON(
-      `/api/auth/accessibleBySwitching?annotationTyp=${annotationType}&annotationId=${commandType.annotationId}`,
+      `/api/auth/accessibleBySwitching?annotationId=${commandType.annotationId}`,
     );
   } else {
     return Request.receiveJSON(

frontend/javascripts/dashboard/dashboard_task_list_view.tsx
@@ -255,7 +255,7 @@ class DashboardTaskListView extends React.PureComponent<PropsWithRouter, State> 
       </div>
     ) : (
       <div>
-        <Link to={`/annotations/Task/${annotation.id}`}>{label}</Link>
+        <Link to={`/annotations/${annotation.id}`}>{label}</Link>
         <br />
         {isAdmin || this.props.isAdminView ? (
           <div>

frontend/javascripts/dashboard/dashboard_view.tsx
@@ -104,7 +104,7 @@ class DashboardView extends PureComponent<PropsWithRouter, State> {
         createGroupForEachFile,
       },
     });
-    this.props.history.push(`/annotations/${response.annotation.typ}/${response.annotation.id}`);
+    this.props.history.push(`/annotations/${response.annotation.id}`);
   };
 
   getValidTabKeys() {

frontend/javascripts/dashboard/dataset/helper_components.tsx
@@ -54,17 +54,18 @@ export const FormItemWithInfo = ({
   </FormItem>
 );
 
+type Props = {
+  children: React.ReactNode;
+};
+
 export class RetryingErrorBoundary extends React.Component<
-  {
-    children: React.ReactNode;
-  },
+  Props,
   {
     error: Error | null | undefined;
   }
 > {
-  constructor() {
-    // @ts-expect-error ts-migrate(2554) FIXME: Expected 1-2 arguments, but got 0.
-    super();
+  constructor(props: Props) {
+    super(props);
     this.state = {
       error: null,
     };

commit 35a0e48091d756fc4aca6bf912b08e0555221750

avoid redundant calls in task creation (regression introduced in cba70af92fd0f02d08ee250341461fa985142324)

frontend/javascripts/admin/task/task_create_form_view.tsx
@@ -479,17 +479,11 @@ function TaskCreateFormView({ taskId, history }: Props) {
                     return Promise.resolve();
                   }
 
-                  const annotationResponse =
-                    (await tryToAwaitPromise(
-                      getMaybeOutdatedAnnotationInformation(value, {
-                        showErrorToast: false,
-                      }),
-                    )) ||
-                    (await tryToAwaitPromise(
-                      getMaybeOutdatedAnnotationInformation(value, {
-                        showErrorToast: false,
-                      }),
-                    ));
+                  const annotationResponse = await tryToAwaitPromise(
+                    getMaybeOutdatedAnnotationInformation(value, {
+                      showErrorToast: false,
+                    }),
+                  );
 
                   if (annotationResponse?.dataSetName != null) {
                     form.setFieldsValue({
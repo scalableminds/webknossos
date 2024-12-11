c18c5e34a380d753484178d9810e2e08875b8606
Florian M <fm3@users.noreply.github.com>
2024-12-11 10:18:32 +0100

    Data loading: save mem + cpu by re-using allocated fill-value chunks (#8271)
    * Data loading: save mem + cpu by re-using allocated fill-value chunks
    
    * changelog

--- CHANGELOG.unreleased.md
+++ CHANGELOG.unreleased.md
@@ -12,6 +12,7 @@ For upgrade instructions, please check the [migration guide](MIGRATIONS.released
 
 ### Added
 - Added the total volume of a dataset to a tooltip in the dataset info tab. [#8229](https://github.com/scalableminds/webknossos/pull/8229)
+- Optimized performance of data loading with “fill value“ chunks. [#8271](https://github.com/scalableminds/webknossos/pull/8271)
 
 ### Changed
 - Renamed "resolution" to "magnification" in more places within the codebase, including local variables. [#8168](https://github.com/scalableminds/webknossos/pull/8168)
--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/ChunkTyper.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/ChunkTyper.scala
@@ -1,5 +1,8 @@
 package com.scalableminds.webknossos.datastore.datareaders
 
+import com.scalableminds.util.cache.AlfuCache
+import com.scalableminds.util.tools.Fox
+import com.scalableminds.util.tools.Fox.box2Fox
 import net.liftweb.common.Box
 import net.liftweb.common.Box.tryo
 
@@ -7,6 +10,8 @@ import java.io.ByteArrayInputStream
 import javax.imageio.stream.MemoryCacheImageInputStream
 import scala.util.Using
 import ucar.ma2.{Array => MultiArray, DataType => MADataType}
+
+import scala.concurrent.ExecutionContext
 
 object ChunkTyper {
   def createFromHeader(header: DatasetHeader): ChunkTyper = header.resolvedDataType match {
@@ -25,7 +30,14 @@ abstract class ChunkTyper {
   def ma2DataType: MADataType
   def wrapAndType(bytes: Array[Byte], chunkShape: Array[Int]): Box[MultiArray]
 
-  def createFromFillValue(chunkShape: Array[Int]): Box[MultiArray] =
+  // If large areas of the array use the fill value, the same chunk shape will be requested often.
+  // This cache implements a flyweight pattern by returning the same instance of the fill-valued array.
+  private val fillValueChunkCache: AlfuCache[String, MultiArray] = AlfuCache(maxCapacity = 1)
+
+  def createFromFillValueCached(chunkShape: Array[Int])(implicit ec: ExecutionContext): Fox[MultiArray] =
+    fillValueChunkCache.getOrLoad(chunkShape.mkString(","), _ => createFromFillValue(chunkShape).toFox)
+
+  protected def createFromFillValue(chunkShape: Array[Int]): Box[MultiArray] =
     MultiArrayUtils.createFilledArray(ma2DataType, chunkShapeOrdered(chunkShape), header.fillValueNumber)
 
   // Chunk shape in header is in C-Order (XYZ), but data may be in F-Order (ZYX), so the chunk shape
@@ -127,7 +139,7 @@ class ShortcutChunkTyper(val header: DatasetHeader) extends ChunkTyper {
     MultiArray.factory(ma2DataType, flatShape, bytes)
   }
 
-  override def createFromFillValue(chunkShape: Array[Int]): Box[MultiArray] = {
+  override protected def createFromFillValue(chunkShape: Array[Int]): Box[MultiArray] = {
     val flatShape = Array(chunkShape.product * header.bytesPerElement)
     MultiArrayUtils.createFilledArray(ma2DataType, flatShape, header.fillValueNumber)
   }

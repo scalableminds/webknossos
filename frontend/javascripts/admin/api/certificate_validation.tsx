c6891dfbe9d75be192e709487f26bdc0c9949194
Michael Büßemeyer <frameworklinux+MichaelBuessemeyer@users.noreply.github.com>
2025-01-16 15:31:57 +0100

    do not block if server is down

--- frontend/javascripts/admin/api/certificate_validation.tsx
+++ frontend/javascripts/admin/api/certificate_validation.tsx
@@ -9,11 +9,16 @@ export async function isCertificateValid(): Promise<CertificateValidationResult>
   try {
     return await Request.receiveJSON("/api/checkCertificate", { showErrorToast: false });
   } catch (errorResponse: any) {
+    if (errorResponse.status !== 400) {
+      // In case the server is not available or some other kind of error occurred, we assume the certificate is valid.
+      return { isValid: true, expiresAt: 0 };
+    }
     try {
       const { isValid, expiresAt } = JSON.parse(errorResponse.errors[0]);
       return { isValid, expiresAt };
     } catch (_e) {
-      return { isValid: false, expiresAt: -1 };
+      // If parsing the error message fails, we assume the certificate is valid.
+      return { isValid: true, expiresAt: 0 };
     }
   }
 }

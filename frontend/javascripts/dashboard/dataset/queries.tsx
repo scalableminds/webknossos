0cf02d0ac22738c1e617cbc21520f3b8d5a3e5aa
Michael Büßemeyer <frameworklinux+MichaelBuessemeyer@users.noreply.github.com>
2025-02-03 17:58:28 +0100

    fix bug in changed datasets detection in frontend

--- frontend/javascripts/dashboard/dataset/queries.tsx
+++ frontend/javascripts/dashboard/dataset/queries.tsx
@@ -497,7 +497,7 @@ function diffDatasets(
     .filter((newDataset) => {
       const oldDataset = oldDatasetsDict[newDataset.id];
       return !_.isEqualWith(oldDataset, newDataset, (oldValue, newValue, key) => {
-        const didUpgradeToRenamableDS =
+        const didUpgradeToRenamableDS = key === undefined && // only check for didUpgradeToRenamableDS with both high level objects are compared and not their properties.
           !("directoryName" in oldValue) && "directoryName" in newValue; // TODO: Can be remove after a few weeks / months.
         if (key === "lastUsedByUser" || didUpgradeToRenamableDS) {
           // Ignore the lastUsedByUser timestamp when diffing datasets and migrating datasets to new renamable version.

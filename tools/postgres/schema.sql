dc53bd072527744bcdfc3a6654acdda93ba9c2ff
Robert Oleynik <robert.oleynik@scalableminds.com>
2025-02-20 13:28:50 +0100

    increment schema version

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
 
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(125);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(126);
 COMMIT TRANSACTION;
 
 

ae279b0dcadbb7ce7dc01528d4eed2ddba9f11d0
frcroth <frcroth@gmail.com>
2025-02-24 13:45:45 +0100

    Fix evolutions

--- conf/evolutions/126-job-resumed-by-super-user.sql
+++ conf/evolutions/126-job-resumed-by-super-user.sql
@@ -2,7 +2,11 @@ START TRANSACTION;
 
 do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 125, 'Previous schema version mismatch'; end; $$ LANGUAGE plpgsql;
 
+DROP VIEW webknossos.jobs_;
+
 ALTER TABLE webknossos.jobs ADD column resumedBySuperUser boolean NOT NULL DEFAULT false;
 UPDATE webknossos.releaseInformation SET schemaVersion = 126;
+
+CREATE VIEW webknossos.jobs_ AS SELECT * FROM webknossos.jobs WHERE NOT isDeleted;
 
 COMMIT TRANSACTION;
--- conf/evolutions/reversions/126-job-resumed-by-super-user.sql
+++ conf/evolutions/reversions/126-job-resumed-by-super-user.sql
@@ -2,7 +2,11 @@ START TRANSACTION;
 
 do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 126, 'Previous schema version mismatch'; end; $$ LANGUAGE plpgsql;
 
+DROP VIEW webknossos.jobs_;
+
 ALTER TABLE webknossos.jobs DROP column resumedBySuperUser;
 UPDATE webknossos.releaseInformation SET schemaVersion = 125;
+
+CREATE VIEW webknossos.jobs_ AS SELECT * FROM webknossos.jobs WHERE NOT isDeleted;
 
 COMMIT TRANSACTION;

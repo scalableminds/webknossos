6062520e9f0d0263d5a2df9d71dbb8ea9bf81184
Michael Büßemeyer <frameworklinux+MichaelBuessemeyer@users.noreply.github.com>
2025-03-03 15:02:50 +0100

    remove dataset id button from into tab

--- frontend/javascripts/oxalis/view/right-border-tabs/dataset_info_tab_view.tsx
+++ frontend/javascripts/oxalis/view/right-border-tabs/dataset_info_tab_view.tsx
@@ -1,4 +1,4 @@
-import { CopyOutlined, EditOutlined, InfoCircleOutlined, SettingOutlined } from "@ant-design/icons";
+import { EditOutlined, InfoCircleOutlined, SettingOutlined } from "@ant-design/icons";
 import { Tag, Typography } from "antd";
 import { formatNumberToVolume, formatScale, formatVoxels } from "libs/format_utils";
 import Markdown from "libs/markdown_adapter";
@@ -38,7 +38,6 @@ import { formatUserName } from "oxalis/model/accessors/user_accessor";
 import { getReadableNameForLayerName } from "oxalis/model/accessors/volumetracing_accessor";
 import type { EmptyObject } from "types/globals";
 import { MarkdownModal } from "../components/markdown_modal";
-import Toast from "libs/toast";
 
 type StateProps = {
   annotation: Tracing;
@@ -341,12 +340,6 @@ export class DatasetInfoTabView extends React.PureComponent<Props, State> {
     ) : null;
   }
 
-  copyDatasetIDToClipboard = (evt: React.MouseEvent<HTMLAnchorElement>) => {
-    evt.preventDefault();
-    navigator.clipboard.writeText(this.props.dataset.id);
-    Toast.success("Dataset ID copied to clipboard");
-  };
-
   getDatasetName() {
     const { name: datasetName, description: datasetDescription } = this.props.dataset;
     const { activeUser } = this.props;
@@ -402,12 +395,7 @@ export class DatasetInfoTabView extends React.PureComponent<Props, State> {
           }}
         >
           {datasetName}
-        </Link>{" "}
-        <FastTooltip title="Copy Dataset ID to clipboard">
-          <Link onClick={this.copyDatasetIDToClipboard} to="#">
-            <CopyOutlined />
-          </Link>
-        </FastTooltip>
+        </Link>
       </div>
     );
   }

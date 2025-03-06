a60c90ff9f927dd38c4325ac50a986fa4cda777d
Philipp Otto <philipp.4096@gmail.com>
2025-03-06 16:14:17 +0100

    fix typo

--- frontend/javascripts/messages.tsx
+++ frontend/javascripts/messages.tsx
@@ -475,7 +475,7 @@ instead. Only enable this option if you understand its effect. All layers will n
     "<%- userName %> is about to become a dataset manager and will be able to access and edit all datasets within this organization.",
   ),
   "users.set_admin": _.template(
-    "<%- userName %> is about to become an admin for this organization with full read/write access to all datasets and management capbilities for all users, projects, and tasks.",
+    "<%- userName %> is about to become an admin for this organization with full read/write access to all datasets and management capabilities for all users, projects, and tasks.",
   ),
   "users.change_email_title": "Do you really want to change the email?",
   "users.change_email": _.template(

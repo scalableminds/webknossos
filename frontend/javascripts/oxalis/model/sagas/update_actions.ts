716dbc534047aa63d6166ec80549ce888559710e
Philipp Otto <philipp.4096@gmail.com>
2025-03-25 15:41:22 +0100

    wording

--- frontend/javascripts/oxalis/model/sagas/update_actions.ts
+++ frontend/javascripts/oxalis/model/sagas/update_actions.ts
@@ -594,8 +594,8 @@ export function mergeAgglomerate(
   name: "mergeAgglomerate";
   value: {
     actionTracingId: string;
-    agglomerateId1: number; // unused
-    agglomerateId2: number; // unused
+    agglomerateId1: number; // unused in backend
+    agglomerateId2: number; // unused in backend
     segmentId1: number | undefined;
     segmentId2: number | undefined;
     // For backwards compatibility reasons,

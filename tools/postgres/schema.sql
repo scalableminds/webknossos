aae16475f1dbf22096864c602edafed998a1133b
frcroth <frcroth@gmail.com>
2025-03-31 11:16:17 +0200

    Make changes in evolution, not new evolution

--- tools/postgres/schema.sql
+++ tools/postgres/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
 
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(131);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(130);
 COMMIT TRANSACTION;
 
 

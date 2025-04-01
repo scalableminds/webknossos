d80da1bd6cfbc81109f784c09dbf7fc9a29c57a2
Florian M <florian@scm.io>
2025-04-01 14:26:37 +0200

    clang-format

--- webknossos-jni/src/bucketScanner.cpp
+++ webknossos-jni/src/bucketScanner.cpp
@@ -1,44 +1,40 @@
 #include "com_scalableminds_webknossos_datastore_helpers_NativeBucketScanner.h"
 
 #include "jniutils.h"
-#include <unordered_set>
-#include <stdexcept>
 #include <iostream>
+#include <stdexcept>
+#include <unordered_set>
 
-uint64_t segmentIdAtIndex(jbyte* bucketBytes, int index, const int bytesPerElement, const bool isSigned) {
-    jbyte* currentPos = bucketBytes + (index * bytesPerElement);
+uint64_t segmentIdAtIndex(jbyte *bucketBytes, int index, const int bytesPerElement, const bool isSigned) {
+    jbyte *currentPos = bucketBytes + (index * bytesPerElement);
     long currentValue;
     switch (bytesPerElement) {
-        case 1:
-            currentValue = isSigned ?
-                static_cast<int64_t>(*reinterpret_cast<int8_t*>(currentPos)) :
-                static_cast<int64_t>(*reinterpret_cast<uint8_t*>(currentPos));
-            break;
-        case 2:
-            currentValue = isSigned ?
-                static_cast<int64_t>(*reinterpret_cast<int16_t*>(currentPos)) :
-                static_cast<int64_t>(*reinterpret_cast<uint16_t*>(currentPos));
-            break;
-        case 4:
-            currentValue = isSigned ?
-                static_cast<int64_t>(*reinterpret_cast<int32_t*>(currentPos)) :
-                static_cast<int64_t>(*reinterpret_cast<uint32_t*>(currentPos));
-            break;
-        case 8:
-            currentValue = isSigned ?
-                static_cast<int64_t>(*reinterpret_cast<int64_t*>(currentPos)) :
-                static_cast<int64_t>(*reinterpret_cast<uint64_t*>(currentPos));
-            break;
-        default:
-            throw std::invalid_argument("Cannot read segment value, unsupported bytesPerElement value");
+    case 1:
+        currentValue = isSigned ? static_cast<int64_t>(*reinterpret_cast<int8_t *>(currentPos))
+                                : static_cast<int64_t>(*reinterpret_cast<uint8_t *>(currentPos));
+        break;
+    case 2:
+        currentValue = isSigned ? static_cast<int64_t>(*reinterpret_cast<int16_t *>(currentPos))
+                                : static_cast<int64_t>(*reinterpret_cast<uint16_t *>(currentPos));
+        break;
+    case 4:
+        currentValue = isSigned ? static_cast<int64_t>(*reinterpret_cast<int32_t *>(currentPos))
+                                : static_cast<int64_t>(*reinterpret_cast<uint32_t *>(currentPos));
+        break;
+    case 8:
+        currentValue = isSigned ? static_cast<int64_t>(*reinterpret_cast<int64_t *>(currentPos))
+                                : static_cast<int64_t>(*reinterpret_cast<uint64_t *>(currentPos));
+        break;
+    default:
+        throw std::invalid_argument("Cannot read segment value, unsupported bytesPerElement value");
     }
     return currentValue;
 }
 
-jlongArray copyToJLongArray(JNIEnv * env, const std::unordered_set<int64_t> &source) {
+jlongArray copyToJLongArray(JNIEnv *env, const std::unordered_set<int64_t> &source) {
     const size_t size = source.size();
-    jlongArray target = env -> NewLongArray(size);
-    jlong* targetElements = env->GetLongArrayElements(target, nullptr);
+    jlongArray target = env->NewLongArray(size);
+    jlong *targetElements = env->GetLongArrayElements(target, nullptr);
 
     auto it = source.begin();
     for (size_t i = 0; i < source.size(); ++i) {
@@ -57,11 +53,11 @@ size_t getElementCount(jsize inputLengthBytes, jint bytesPerElement) {
     return inputLengthBytes / bytesPerElement;
 }
 
-JNIEXPORT jlongArray JNICALL Java_com_scalableminds_webknossos_datastore_helpers_NativeBucketScanner_collectSegmentIds
-    (JNIEnv * env, jobject instance, jbyteArray bucketBytesJavaArray, jint bytesPerElement, jboolean isSigned) {
+JNIEXPORT jlongArray JNICALL Java_com_scalableminds_webknossos_datastore_helpers_NativeBucketScanner_collectSegmentIds(
+    JNIEnv *env, jobject instance, jbyteArray bucketBytesJavaArray, jint bytesPerElement, jboolean isSigned) {
 
-    const jsize inputLengthBytes = env -> GetArrayLength(bucketBytesJavaArray);
-    jbyte * bucketBytes = env -> GetByteArrayElements(bucketBytesJavaArray, nullptr);
+    const jsize inputLengthBytes = env->GetArrayLength(bucketBytesJavaArray);
+    jbyte *bucketBytes = env->GetByteArrayElements(bucketBytesJavaArray, nullptr);
     try {
         const size_t elementCount = getElementCount(inputLengthBytes, bytesPerElement);
 
@@ -70,18 +66,18 @@ JNIEXPORT jlongArray JNICALL Java_com_scalableminds_webknossos_datastore_helpers
         for (size_t i = 0; i < elementCount; ++i) {
             const int64_t currentValue = segmentIdAtIndex(bucketBytes, i, bytesPerElement, isSigned);
             if (currentValue != 0) {
-              uniqueSegmentIds.insert(currentValue);
+                uniqueSegmentIds.insert(currentValue);
             }
         }
 
-        env -> ReleaseByteArrayElements(bucketBytesJavaArray, bucketBytes, 0);
+        env->ReleaseByteArrayElements(bucketBytesJavaArray, bucketBytes, 0);
         return copyToJLongArray(env, uniqueSegmentIds);
-    } catch (const std::exception & e) {
-        env -> ReleaseByteArrayElements(bucketBytesJavaArray, bucketBytes, 0);
+    } catch (const std::exception &e) {
+        env->ReleaseByteArrayElements(bucketBytesJavaArray, bucketBytes, 0);
         throwRuntimeException(env, "Native Exception in BucketScanner: " + std::string(e.what()));
         return nullptr;
     } catch (...) {
-        env -> ReleaseByteArrayElements(bucketBytesJavaArray, bucketBytes, 0);
+        env->ReleaseByteArrayElements(bucketBytesJavaArray, bucketBytes, 0);
         throwRuntimeException(env, "Native Exception in BucketScanner");
         return nullptr;
     }
--- webknossos-jni/src/jniutils.cpp
+++ webknossos-jni/src/jniutils.cpp
@@ -1,9 +1,9 @@
 #include "jniutils.h"
 
-void throwRuntimeException(JNIEnv * env, const std::string msg) {
-    jclass exceptionClass = env -> FindClass("java/lang/RuntimeException");
+void throwRuntimeException(JNIEnv *env, const std::string msg) {
+    jclass exceptionClass = env->FindClass("java/lang/RuntimeException");
 
     if (exceptionClass != nullptr) {
-        env -> ThrowNew(exceptionClass, ("An error occurred in native code: " + msg).c_str());
+        env->ThrowNew(exceptionClass, ("An error occurred in native code: " + msg).c_str());
     }
 }

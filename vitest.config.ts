3578cbd3f8bfc2148fde2dbfac0210d3def9e387
Philipp Otto <philipp.4096@gmail.com>
2025-04-07 15:08:55 +0200

    bump timeout from 5s to 10s

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from 'vite-tsconfig-paths'
-
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   test: {
     globals: true,
-    setupFiles: ['./frontend/javascripts/test/global_mocks.ts'],
-    include: ['./frontend/javascripts/test/**/*.spec.ts'],
-    exclude: ['./frontend/javascripts/test/**/*.e2e.ts'],
-    env: {IS_TESTING: "true"}
+    setupFiles: ["./frontend/javascripts/test/global_mocks.ts"],
+    include: ["./frontend/javascripts/test/**/*.spec.ts"],
+    exclude: ["./frontend/javascripts/test/**/*.e2e.ts"],
+    env: { IS_TESTING: "true" },
+    testTimeout: 10000,
   },
   plugins: [tsconfigPaths()],
-}); 
+});

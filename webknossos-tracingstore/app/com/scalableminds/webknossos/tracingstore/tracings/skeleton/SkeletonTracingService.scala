1512528f43dcdee7e82f4867598a99b3f67abc38
Florian M <florian@scm.io>
2025-04-09 13:30:58 +0200

    Fix concurrency bug in skeleton saving

--- webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/tracings/skeleton/SkeletonTracingService.scala
+++ webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/tracings/skeleton/SkeletonTracingService.scala
@@ -44,7 +44,7 @@ class SkeletonTracingService @Inject()(
       for {
         _ <- Fox.serialCombined(treeIdsToFlush) { treeId =>
           for {
-            treeBody <- extractTreeBody(tracing, treeId)
+            treeBody <- extractTreeBody(tracing, treeId).toFox
             _ <- skeletonTreeBodiesPutBuffer.put(f"$tracingId/$treeId", treeBody)
           } yield ()
         }

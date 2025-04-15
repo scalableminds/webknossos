53837565cbb11380198ffa1401dac27e587fb956
Florian M <florian@scm.io>
2025-04-15 10:59:35 +0200

    run e2e tests with failOnWarning too, to avoid recompilation

--- util/src/main/scala/com/scalableminds/util/tools/Fox.scala
+++ util/src/main/scala/com/scalableminds/util/tools/Fox.scala
@@ -356,18 +356,16 @@ class Fox[+A](val futureBox: Future[Box[A]])(implicit ec: ExecutionContext) {
     }).flatMap(identity)
 
   /**
-    *  Awaits the future and opens the box.
+    *  Awaits the future and opens the box. Do not use in production code!
     */
-  @deprecated(message = "Do not use this in production code", since = "forever")
   def get(justification: String, awaitTimeout: FiniteDuration = 10 seconds): A = {
     val box = await(justification, awaitTimeout)
     box.openOrThrowException(justification)
   }
 
   /**
-    * Awaits the future and returns the box.
+    * Awaits the future and returns the box. Do not use in production code!
     */
-  @deprecated(message = "Do not use this in production code", since = "forever")
   def await(justification: String, awaitTimeout: FiniteDuration = 10 seconds): Box[A] =
     Await.result(futureBox, awaitTimeout)
 

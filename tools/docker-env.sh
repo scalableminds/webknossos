024e8f57cfcd89c2c604c9d063c7ad1411fc792f
Robert Oleynik <robert.oleynik@scalableminds.com>
2025-04-16 17:04:11 +0200

    replace docker compose script in yarn

---
+++ tools/docker-env.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Start Docker
+export POSTGRES_URL=jdbc:postgresql://localhost/webknossos
+export POSTGRES_USER="webknossos"
+export POSTGRES_PASSWORD="secret"
+
+cd .github || exit
+docker compose down -v
+docker compose up -d postgres redis fossildb
+cd .. || exit
+
+"$@"
+RESULT=$?
+
+cd .github || exit
+docker compose down -v
+cd .. || exit
+
+exit "$RESULT"

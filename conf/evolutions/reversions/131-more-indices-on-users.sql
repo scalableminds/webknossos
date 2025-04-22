eeac61e4371ec9103cc170b5483f78db6277c6af
Michael Büßemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>
2025-04-22 17:18:36 +0200

    include stored functions in evolution where case spelling of false was adjusted

--- conf/evolutions/reversions/131-more-indices-on-users.sql
+++ conf/evolutions/reversions/131-more-indices-on-users.sql
@@ -7,6 +7,31 @@ DROP INDEX IF EXISTS ON webknossos.users(_organization);
 DROP INDEX IF EXISTS ON webknossos.users(isDeactivated);
 DROP INDEX IF EXISTS ON webknossos.users(isUnlisted);
 
+-- Make spelling of false to be written in lower case.
+CREATE OR REPLACE FUNCTION webknossos.countsAsTaskInstance(a webknossos.annotations) RETURNS BOOLEAN AS $$
+  BEGIN
+    RETURN (a.state != 'Cancelled' AND a.isDeleted = false AND a.typ = 'Task');
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION webknossos.onInsertAnnotation() RETURNS trigger AS $$
+  BEGIN
+    IF (NEW.typ = 'Task') AND (NEW.isDeleted = false) AND (NEW.state != 'Cancelled') THEN
+      UPDATE webknossos.tasks SET pendingInstances = pendingInstances - 1 WHERE _id = NEW._task;
+    END IF;
+    RETURN NULL;
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION webknossos.onDeleteAnnotation() RETURNS TRIGGER AS $$
+  BEGIN
+    IF (OLD.typ = 'Task') AND (OLD.isDeleted = false) AND (OLD.state != 'Cancelled') THEN
+      UPDATE webknossos.tasks SET pendingInstances = pendingInstances + 1 WHERE _id = OLD._task;
+    END IF;
+    RETURN NULL;
+  END;
+$$ LANGUAGE plpgsql;
+
 UPDATE webknossos.releaseInformation SET schemaVersion = 130;
 
 COMMIT TRANSACTION;

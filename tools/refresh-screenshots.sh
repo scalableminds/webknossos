5dd8e5493424adaf52365bb5f518a166e70b0ab6
Robert Oleynik <robert.oleynik@scalableminds.com>
2025-04-23 13:54:22 +0200

    add script to refresh screenshots

---
+++ tools/refresh-screenshots.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -xe
+
+rm -rf frontend/javascripts/test/screenshots/**
+
+docker run --rm -t \
+  -e "URL=$URL" \
+  -e "WK_AUTH_TOKEN=$WK_AUTH_TOKEN" \
+  -w "/home/pptruser/webknossos" \
+  -v ".:/home/pptruser/webknossos" \
+  -u "$(id -u):$(id -g)" \
+  scalableminds/puppeteer:master bash -c 'for i in {1..3}; do yarn test-screenshot && break; done'

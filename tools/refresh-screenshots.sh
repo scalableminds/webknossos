43530a83d619fbd383f530af6c0c4d3866282392
Robert Oleynik <robert.oleynik@scalableminds.com>
2025-04-29 16:13:03 +0200

    add missing env variables

--- tools/refresh-screenshots.sh
+++ tools/refresh-screenshots.sh
@@ -5,9 +5,12 @@ set -xe
 rm -rf frontend/javascripts/test/screenshots/**
 
 docker run --rm \
-  -e "URL=$URL" \
-  -e "WK_AUTH_TOKEN=$WK_AUTH_TOKEN" \
-  -w "/home/pptruser/webknossos" \
-  -v ".:/home/pptruser/webknossos" \
-  -u "$(id -u):$(id -g)" \
+  --pull "always" \
+  --env "URL=$URL" \
+  --env "WK_AUTH_TOKEN=$WK_AUTH_TOKEN" \
+  --env "BROWSERSTACK_USERNAME=$BROWSERSTACK_USERNAME" \
+  --env "BROWSERSTACK_ACCESS_KEY=$BROWSERSTACK_ACCESS_KEY" \
+  --workdir "/home/pptruser/webknossos" \
+  --volume ".:/home/pptruser/webknossos" \
+  --user "$(id -u):$(id -g)" \
   scalableminds/puppeteer:master bash -c 'for i in {1..3}; do yarn test-screenshot && break; done'

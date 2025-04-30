4578a36854e4ccb5ba1129502fb6c33961f20f82
Tom Herold <tom.herold@scalableminds.com>
2025-04-30 12:57:08 +0200

    add module namespace defintion

--- frontend/javascripts/types/deep-freeze.d.ts
+++ frontend/javascripts/types/deep-freeze.d.ts
@@ -1,18 +1,20 @@
 /**
- * Type definitions for package javascript-natural-sort
- * https://github.com/Bill4Time/javascript-natural-sort
+ * Type definitions for package deep-freeze
+ * https://github.com/Bill4Time/deep-freeze
  */
 
-/**
- * Recursively Object.freeze() on objects and functions
- * 
- * Freezes an object and all its properties deeply making the entire
- * object hierarchy immutable. Once frozen, you can no longer add,
- * remove or change any properties of the object.
- * 
- * @param o - The object to freeze
- * @returns The same object, but frozen
- */
-declare function deepFreeze<T>(o: T): Readonly<T>;
+declare module 'deep-freeze' {
+    /**
+     * Recursively Object.freeze() on objects and functions
+     * 
+     * Freezes an object and all its properties deeply making the entire
+     * object hierarchy immutable. Once frozen, you can no longer add,
+     * remove or change any properties of the object.
+     * 
+     * @param o - The object to freeze
+     * @returns The same object, but frozen
+     */
+    declare function deepFreeze<T>(o: T): Readonly<T>;
 
-export = deepFreeze; 
+    export = deepFreeze;  
+}

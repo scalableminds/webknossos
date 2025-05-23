47e1ea252abc7e70eb467a8cd04028fcc795cecb
Philipp Otto <philipp.4096@gmail.com>
2025-05-23 20:43:44 +0200

    avoid ts-expect error

--- frontend/javascripts/test/helpers/chainReducer.ts
+++ frontend/javascripts/test/helpers/chainReducer.ts
@@ -2,8 +2,9 @@ export function chainReduce<S, A>(reducer: (arg0: S, arg1: A) => S) {
   return (state: S, actionGetters: Array<A | ((arg0: S) => A)>) => {
     return actionGetters.reduce((currentState, actionGetter) => {
       const action: A =
-        // @ts-expect-error ts-migrate(2349) FIXME: This expression is not callable.
-        typeof actionGetter === "function" ? actionGetter(currentState) : actionGetter;
+        typeof actionGetter === "function"
+          ? (actionGetter as (state: S) => A)(currentState)
+          : actionGetter;
       return reducer(currentState, action);
     }, state);
   };

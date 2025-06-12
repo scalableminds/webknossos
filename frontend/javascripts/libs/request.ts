0ec812df044d453b750186f0344162bd5404514d
Philipp Otto <philipp.4096@gmail.com>
2025-06-12 14:36:06 +0200

    fix missing await for dynamic import

--- frontend/javascripts/libs/request.ts
+++ frontend/javascripts/libs/request.ts
@@ -285,7 +285,7 @@ class Request {
       setTimeout(() => resolve("timeout"), timeout);
     });
 
-  handleError = (
+  handleError = async (
     requestedUrl: string,
     showErrorToast: boolean,
     doInvestigate: boolean,
@@ -293,58 +293,56 @@ class Request {
   ): Promise<void> => {
     if (doInvestigate) {
       // Avoid circular imports via dynamic import
-      import("admin/datastore_health_check").then(({ pingMentionedDataStores }) => {
-        // Check whether this request failed due to a problematic datastore
-        pingMentionedDataStores(requestedUrl);
+      const { pingMentionedDataStores } = await import("admin/datastore_health_check");
+      // Check whether this request failed due to a problematic datastore
+      pingMentionedDataStores(requestedUrl);
+      if (error instanceof Response) {
+        return error.text().then(
+          (text) => {
+            try {
+              const json = JSON.parse(text);
 
-        if (error instanceof Response) {
-          return error.text().then(
-            (text) => {
-              try {
-                const json = JSON.parse(text);
-
-                // Propagate HTTP status code for further processing down the road
-                if (error.status != null) {
-                  json.status = error.status;
-                }
-
-                const messages = json.messages.map((message: ServerErrorMessage[]) => ({
-                  ...message,
-                  key: json.status.toString(),
-                }));
-                if (showErrorToast) {
-                  Toast.messages(messages); // Note: Toast.error internally logs to console
-                } else {
-                  console.error(messages);
-                }
-                // Check whether the error chain mentions an url which belongs
-                // to a datastore. Then, ping the datastore
-                pingMentionedDataStores(text);
-
-                /* eslint-disable-next-line prefer-promise-reject-errors */
-                return Promise.reject({ ...json, url: requestedUrl });
-              } catch (_jsonError) {
-                if (showErrorToast) {
-                  Toast.error(text); // Note: Toast.error internally logs to console
-                } else {
-                  console.error(`Request failed for ${requestedUrl}:`, text);
-                }
-
-                /* eslint-disable-next-line prefer-promise-reject-errors */
-                return Promise.reject({
-                  errors: [text],
-                  status: error.status != null ? error.status : -1,
-                  url: requestedUrl,
-                });
+              // Propagate HTTP status code for further processing down the road
+              if (error.status != null) {
+                json.status = error.status;
               }
-            },
-            (textError) => {
-              Toast.error(textError.toString());
-              return Promise.reject(textError);
-            },
-          );
-        }
-      });
+
+              const messages = json.messages.map((message: ServerErrorMessage[]) => ({
+                ...message,
+                key: json.status.toString(),
+              }));
+              if (showErrorToast) {
+                Toast.messages(messages); // Note: Toast.error internally logs to console
+              } else {
+                console.error(messages);
+              }
+              // Check whether the error chain mentions an url which belongs
+              // to a datastore. Then, ping the datastore
+              pingMentionedDataStores(text);
+
+              /* eslint-disable-next-line prefer-promise-reject-errors */
+              return Promise.reject({ ...json, url: requestedUrl });
+            } catch (_jsonError) {
+              if (showErrorToast) {
+                Toast.error(text); // Note: Toast.error internally logs to console
+              } else {
+                console.error(`Request failed for ${requestedUrl}:`, text);
+              }
+
+              /* eslint-disable-next-line prefer-promise-reject-errors */
+              return Promise.reject({
+                errors: [text],
+                status: error.status != null ? error.status : -1,
+                url: requestedUrl,
+              });
+            }
+          },
+          (textError) => {
+            Toast.error(textError.toString());
+            return Promise.reject(textError);
+          },
+        );
+      }
     }
 
     // If doInvestigate is false or the error is not instanceof Response,

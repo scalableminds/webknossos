scalableminds/webknossos

commit f850a8e3a80f5db698672c6a41d932f417448930

Refactor TreeMap to DiffableMap (#8626)
* allow to toggle visibility of each segment via custom segment colors

* allow to toggle segments and groups

* linting

* backend: add UpdateSegmentVisibilityVolumeAction, UpdateSegmentGroupVisibilityVolumeAction

* integrate updateSegmentVisibility update action

* implement toggle compaction and integrate updateSegmentGroupVisibilityVolumeAction

* warn user when too many custom segment colors exist; don't show checkboxes if selective visibility is active

* move titleRender to own method

* implement hideUnregisteredSegments and only-show-this-segment etc in context menu

* fix saving of hideUnregisteredSegments

* fix typing

* remove unlisted segments group prototype again

* expose hide unlisted segments in left sidebar and add show-all-segments in context menu of data viewports

* remove -2 handling for unlisted group stuff

* fix visibility when removing segment from list; refactor toggling

* hide checkboxes when proofreading and selectiveVisibilityInProofreading is active; fix screenshot tests

* write and parse tree and segment isVisible in NMLs

* fix support for view only

* add missing volumetracing_action_helpers.ts module

* fix warning because of non-string value in radio button component for tool bar

* use fast tooltip to avoid findDOMNode warning

* fmt backend

* fix warning when opening context menu by restructuring hooks

* refactor context info in context menu

* lint

* add some position when a segment is registered by toggling its visibility

* update screenshots (the selective rendering is done via hovering where
the opacity is a bit different now)

* implement benchmark for segment list scrolling; improve perf a little bit

* clean up unnecessary isChecked property

* use extracted vars

* add comments

* linting

* integrate some code rabbit feedback

* format

* added docstrings to DiffableMap

* extended unit tests for diffable map

* add missing type to merge

frontend/javascripts/viewer/model/accessors/skeletontracing_accessor.ts
@@ -1,4 +1,3 @@
-import _ from "lodash";
 import {
   type APIAnnotation,
   type AnnotationLayerDescriptor,
@@ -7,22 +6,20 @@ import {
   type ServerTracing,
 } from "types/api_types";
 import { IdentityTransform, type TreeType, type Vector3 } from "viewer/constants";
-import type {
-  BranchPoint,
-  Node,
-  NumberLike,
-  SkeletonTracing,
-  StoreAnnotation,
-  Tree,
-  TreeGroup,
-  TreeGroupTypeFlat,
+import {
+  type BranchPoint,
+  type Node,
+  type Tree,
+  type TreeGroup,
+  type TreeGroupTypeFlat,
   TreeMap,
-  WebknossosState,
-} from "viewer/store";
+} from "viewer/model/types/tree_types";
+import type { NumberLike, SkeletonTracing, StoreAnnotation, WebknossosState } from "viewer/store";
 import {
   MISSING_GROUP_ID,
   findGroup,
 } from "viewer/view/right-border-tabs/trees_tab/tree_hierarchy_view_helpers";
+import { max } from "../helpers/iterator_utils";
 import { invertTransform, transformPointUnscaled } from "../helpers/transformation_helpers";
 import {
   getTransformsForLayerThatDoesNotSupportTransformationConfigOrNull,
@@ -76,7 +73,7 @@ export function getActiveNode(skeletonTracing: SkeletonTracing): Node | null {
   const { activeTreeId, activeNodeId } = skeletonTracing;
 
   if (activeTreeId != null && activeNodeId != null) {
-    return skeletonTracing.trees[activeTreeId].nodes.getOrThrow(activeNodeId);
+    return skeletonTracing.trees.getOrThrow(activeTreeId).nodes.getOrThrow(activeNodeId);
   }
 
   return null;
@@ -89,7 +86,7 @@ export function getActiveTree(skeletonTracing: SkeletonTracing | null | undefine
   const { activeTreeId } = skeletonTracing;
 
   if (activeTreeId != null) {
-    return skeletonTracing.trees[activeTreeId];
+    return skeletonTracing.trees.getNullable(activeTreeId) || null;
   }
 
   return null;
@@ -117,11 +114,11 @@ export function getActiveNodeFromTree(skeletonTracing: SkeletonTracing, tree: Tr
 }
 
 export function findTreeByNodeId(trees: TreeMap, nodeId: number): Tree | undefined {
-  return _.values(trees).find((tree) => tree.nodes.has(nodeId));
+  return trees.values().find((tree) => tree.nodes.has(nodeId));
 }
 
 export function findTreeByName(trees: TreeMap, treeName: string): Tree | undefined {
-  return _.values(trees).find((tree: Tree) => tree.name === treeName);
+  return trees.values().find((tree: Tree) => tree.name === treeName);
 }
 
 export function getTreesWithType(
@@ -132,7 +129,12 @@ export function getTreesWithType(
    * Returns trees of a specific type or all trees if no type is provided.
    */
   return type != null
-    ? _.pickBy(skeletonTracing.trees, (tree) => tree.type === type)
+    ? new TreeMap(
+        skeletonTracing.trees
+          .entries()
+          .filter(([_, tree]) => tree.type === type)
+          .toArray(),
+      )
     : skeletonTracing.trees;
 }
 
@@ -147,13 +149,13 @@ export function getTree(
   const trees = getTreesWithType(skeletonTracing, type);
 
   if (treeId != null) {
-    return trees[treeId] || null;
+    return trees.getNullable(treeId) || null;
   }
 
   const { activeTreeId } = skeletonTracing;
 
   if (activeTreeId != null) {
-    return trees[activeTreeId] || null;
+    return trees.getNullable(activeTreeId) || null;
   }
 
   return null;
@@ -169,19 +171,19 @@ export function getTreeAndNode(
    * Returns a tuple of [tree, node] if the node and tree can be found, otherwise null.
    * If no nodeId is provided, the active node is used. If no treeId is provided, the active tree is used.
    */
-  let tree;
+  let tree: Tree | undefined;
 
   const trees = getTreesWithType(skeletonTracing, type);
 
   if (treeId != null) {
-    tree = trees[treeId];
+    tree = trees.getNullable(treeId);
   } else if (nodeId != null) {
-    tree = _.values(trees).find((__) => __.nodes.has(nodeId));
+    tree = trees.values().find((__) => __.nodes.has(nodeId));
   } else {
     const { activeTreeId } = skeletonTracing;
 
     if (activeTreeId != null) {
-      tree = trees[activeTreeId];
+      tree = trees.getNullable(activeTreeId);
     }
   }
 
@@ -263,52 +265,44 @@ export function untransformNodePosition(position: Vector3, state: WebknossosStat
 }
 
 export function getMaxNodeIdInTree(tree: Tree): number | null {
-  const maxNodeId = _.reduce(
-    Array.from(tree.nodes.keys()),
-    (r, nodeId) => Math.max(r, nodeId),
-    Number.NEGATIVE_INFINITY,
-  );
-
-  return maxNodeId === Number.NEGATIVE_INFINITY ? null : maxNodeId;
+  return max(tree.nodes.values().map((node) => node.id));
 }
 
 export function getMaxNodeId(skeletonTracing: SkeletonTracing): number | null {
-  const maxNodeId = _.reduce(
-    skeletonTracing.trees,
-    (r, tree) => {
-      const treeMaxId = getMaxNodeIdInTree(tree);
-      return Math.max(r, treeMaxId ?? Number.NEGATIVE_INFINITY);
-    },
-    Number.NEGATIVE_INFINITY,
+  return max(
+    skeletonTracing.trees
+      .values()
+      .map((tree) => getMaxNodeIdInTree(tree) ?? Number.NEGATIVE_INFINITY),
   );
-
-  return maxNodeId === Number.NEGATIVE_INFINITY ? null : maxNodeId;
 }
 
-export function getBranchPoints(annotation: StoreAnnotation): BranchPoint[] | null {
+export function getBranchPoints(annotation: StoreAnnotation): IteratorObject<BranchPoint[]> | null {
   const skeletonTracing = getSkeletonTracing(annotation);
   if (skeletonTracing == null) {
     return null;
   }
 
-  return _.flatMap(skeletonTracing.trees, (tree) => tree.branchPoints);
+  return skeletonTracing.trees.values().map((tree) => tree.branchPoints);
 }
 
-export function getFlatTreeGroups(skeletonTracing: SkeletonTracing): Array<TreeGroupTypeFlat> {
-  return Array.from(
-    mapGroupsToGenerator(
-      skeletonTracing.treeGroups,
-      ({ children: _children, ...bareTreeGroup }) => ({
-        ...bareTreeGroup,
-      }),
-    ),
+export function getFlatTreeGroups(
+  skeletonTracing: SkeletonTracing,
+): IteratorObject<TreeGroupTypeFlat> {
+  return mapGroupsToGenerator(
+    skeletonTracing.treeGroups,
+    ({ children: _children, ...bareTreeGroup }) => ({
+      ...bareTreeGroup,
+    }),
   );
 }
+
 export function getTreeGroupsMap(
   skeletonTracing: SkeletonTracing,
 ): Record<number, TreeGroupTypeFlat> {
-  return _.keyBy(getFlatTreeGroups(skeletonTracing), "groupId");
+  const flatGroups = getFlatTreeGroups(skeletonTracing);
+  return Object.fromEntries(flatGroups.map((group) => [group.groupId, group]));
 }
+
 // This is the pattern for the automatically assigned names for agglomerate skeletons
 export const getTreeNameForAgglomerateSkeleton = (
   agglomerateId: NumberLike,

frontend/javascripts/viewer/model/sagas/skeletontracing_saga.ts
@@ -6,7 +6,6 @@ import { V3 } from "libs/mjs";
 import createProgressCallback from "libs/progress_callback";
 import type { Message } from "libs/toast";
 import Toast from "libs/toast";
-import * as Utils from "libs/utils";
 import _ from "lodash";
 import memoizeOne from "memoize-one";
 import messages from "messages";
@@ -75,16 +74,9 @@ import {
   updateUserBoundingBoxesInSkeletonTracing,
 } from "viewer/model/sagas/update_actions";
 import { api } from "viewer/singletons";
-import type {
-  Flycam,
-  Node,
-  NodeMap,
-  SkeletonTracing,
-  Tree,
-  TreeMap,
-  WebknossosState,
-} from "viewer/store";
+import type { Flycam, SkeletonTracing, WebknossosState } from "viewer/store";
 import Store from "viewer/store";
+import type { Node, NodeMap, Tree, TreeMap } from "../types/tree_types";
 import { ensureWkReady } from "./ready_sagas";
 import { takeWithBatchActionSupport } from "./saga_helpers";
 
@@ -134,7 +126,7 @@ function* watchBranchPointDeletion(): Saga<void> {
 
     if (deleteBranchpointAction) {
       const hasBranchPoints = yield* select(
-        (state: WebknossosState) => (getBranchPoints(state.annotation) ?? []).length > 0,
+        (state: WebknossosState) => (getBranchPoints(state.annotation)?.toArray() ?? []).length > 0,
       );
 
       if (hasBranchPoints) {
@@ -176,9 +168,9 @@ function* watchFailedNodeCreations(): Saga<void> {
 
 function* watchTracingConsistency(): Saga<void> {
   const state = yield* select((_state) => _state);
-  const invalidTreeDetails = [];
+  const invalidTreeDetails: Array<Record<string, any>> = [];
 
-  for (const tree of _.values(enforceSkeletonTracing(state.annotation).trees)) {
+  for (const tree of enforceSkeletonTracing(state.annotation).trees.values()) {
     const edgeCount = tree.edges.size();
     const nodeCount = tree.nodes.size();
 
@@ -209,7 +201,7 @@ export function* watchTreeNames(): Saga<void> {
   const state = yield* select((_state) => _state);
 
   // rename trees with an empty/default tree name
-  for (const tree of _.values(enforceSkeletonTracing(state.annotation).trees)) {
+  for (const tree of enforceSkeletonTracing(state.annotation).trees.values()) {
     if (tree.name === "") {
       const newName = generateTreeName(state, tree.timestamp, tree.treeId);
       yield* put(setTreeNameAction(newName, tree.treeId));
@@ -559,31 +551,28 @@ export function* diffTrees(
 ): Generator<UpdateActionWithoutIsolationRequirement, void, void> {
   if (prevTrees === trees) return;
   const {
+    changed: bothTreeIds,
     onlyA: deletedTreeIds,
     onlyB: addedTreeIds,
-    both: bothTreeIds,
-  } = Utils.diffArrays(
-    _.map(prevTrees, (tree) => tree.treeId),
-    _.map(trees, (tree) => tree.treeId),
-  );
+  } = diffDiffableMaps(prevTrees, trees);
 
   for (const treeId of deletedTreeIds) {
-    const prevTree = prevTrees[treeId];
+    const prevTree = prevTrees.getOrThrow(treeId);
     yield* diffNodes(tracingId, prevTree.nodes, new DiffableMap(), treeId);
     yield* diffEdges(tracingId, prevTree.edges, new EdgeCollection(), treeId);
     yield deleteTree(treeId, tracingId);
   }
 
   for (const treeId of addedTreeIds) {
-    const tree = trees[treeId];
+    const tree = trees.getOrThrow(treeId);
     yield createTree(tree, tracingId);
     yield* diffNodes(tracingId, new DiffableMap(), tree.nodes, treeId);
     yield* diffEdges(tracingId, new EdgeCollection(), tree.edges, treeId);
   }
 
   for (const treeId of bothTreeIds) {
-    const tree = trees[treeId];
-    const prevTree: Tree = prevTrees[treeId];
+    const tree = trees.getOrThrow(treeId);
+    const prevTree: Tree = prevTrees.getOrThrow(treeId);
 
     if (tree !== prevTree) {
       yield* diffNodes(tracingId, prevTree.nodes, tree.nodes, treeId);

commit 54bee0c09d3d4c5d4a8b59eeb6a9d16e3d417ed2

Fix getBranchPoints accessor (#8695)
Regression from #8626. The branchpoints need to be a flat list so that
the length can be compared to 0.

### URL of deployed dev instance (used for testing):
- https://___.webknossos.xyz

### Steps to test:
- create a new tree with node
- press J

### Issues:
- fixes
https://discuss.webknossos.org/t/absence-of-jumps-in-annotations-is-not-detected/1930/2

------
(Please delete unneeded items, merge only when none are left open)
- [x] Updated
[changelog](../blob/master/CHANGELOG.unreleased.md#unreleased)

frontend/javascripts/viewer/model/accessors/skeletontracing_accessor.ts
@@ -276,13 +276,16 @@ export function getMaxNodeId(skeletonTracing: SkeletonTracing): number | null {
   );
 }
 
-export function getBranchPoints(annotation: StoreAnnotation): IteratorObject<BranchPoint[]> | null {
+export function getBranchPoints(annotation: StoreAnnotation): BranchPoint[] | null {
   const skeletonTracing = getSkeletonTracing(annotation);
   if (skeletonTracing == null) {
     return null;
   }
 
-  return skeletonTracing.trees.values().map((tree) => tree.branchPoints);
+  return skeletonTracing.trees
+    .values()
+    .flatMap((tree) => tree.branchPoints)
+    .toArray();
 }
 
 export function getFlatTreeGroups(

frontend/javascripts/viewer/model/sagas/skeletontracing_saga.ts
@@ -125,7 +125,7 @@ function* watchBranchPointDeletion(): Saga<void> {
 
     if (deleteBranchpointAction) {
       const hasBranchPoints = yield* select(
-        (state: WebknossosState) => (getBranchPoints(state.annotation)?.toArray() ?? []).length > 0,
+        (state: WebknossosState) => (getBranchPoints(state.annotation) ?? []).length > 0,
       );
 
       if (hasBranchPoints) {
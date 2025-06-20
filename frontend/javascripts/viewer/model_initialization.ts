8b0d9b26c69271e301b60b0a31ee638e9f60ce1d
Philipp Otto <philipp.4096@gmail.com>
2025-06-20 21:54:53 +0200

    add css class to brainspinnerwitherror and add hardcoded error in model initialization

--- frontend/javascripts/viewer/model_initialization.ts
+++ frontend/javascripts/viewer/model_initialization.ts
@@ -206,6 +206,8 @@ export async function initialize(
     datasetId = initialCommandType.datasetId;
   }
 
+  throw new Error("bla");
+
   const [dataset, initialUserSettings, serverTracings] = await fetchParallel(
     annotation,
     datasetId,

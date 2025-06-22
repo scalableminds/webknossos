f84692bbf11c6b758ee0768695f84bc0781ff029
Mark Kittisopikul <mkitti@users.noreply.github.com>
2025-06-22 01:57:43 -0400

    Add Zstd to N5CompressorFactory.scala
    N5 supports Zstandard compression via https://github.com/JaneliaSciComp/n5-zstandard

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/n5/N5CompressorFactory.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/n5/N5CompressorFactory.scala
@@ -8,7 +8,8 @@ import com.scalableminds.webknossos.datastore.datareaders.{
   GzipCompressor,
   NullCompressor,
   StringCompressionSetting,
-  ZlibCompressor
+  ZlibCompressor,
+  ZstdCompressor
 }
 
 object N5CompressorFactory {
@@ -28,6 +29,13 @@ object N5CompressorFactory {
         new ZlibCompressor(properties)
       case "gzip"  => new GzipCompressor(properties)
       case "blosc" => new BloscCompressor(properties)
+      case "zstd"  => {
+        val level = properties.get("level") match {
+          case Some(IntCompressionSetting(l)) => l
+          case _                              => throw new IllegalArgumentException("Zstd level must be int")
+        }
+        new ZstdCompressor(level, checksum = false)
+      }
       case _       => throw new IllegalArgumentException("Compressor id:'" + id + "' not supported.")
     }
 }

scalableminds/webknossos

commit d6c24bde00af468bc281121a79ccbb4684a9932b

Don't fetch mesh files for segmentation layers without fallback layers (#8726)
The fetching of the meshes was not guarded against segmentation layer
without a fallback. Only segmentation layer with fallback can have
meshes.

### URL of deployed dev instance (used for testing):
- https://___.webknossos.xyz

### Steps to test:
- On master: Create a skeleton only annotaiton
- add a empty volume layer without fallback
- open segmentation tab -> watch it crash
- On this branch: Do the same
- No crash  

### Issues:
- fixes report
https://scm.slack.com/archives/C02H5T8Q08P/p1751282276200889

------
(Please delete unneeded items, merge only when none are left open)
- [x] Added changelog entry (create a `$PR_NUMBER.md` file in
`unreleased_changes` or use `./tools/create-changelog-entry.py`)

frontend/javascripts/viewer/model/sagas/meshes/precomputed_mesh_saga.ts
@@ -58,7 +58,12 @@ let fetchDeferredsPerLayer: Record<string, Deferred<Array<APIMeshFileInfo>, unkn
 function* maybeFetchMeshFiles(action: MaybeFetchMeshFilesAction): Saga<void> {
   const { segmentationLayer, dataset, mustRequest, autoActivate, callback } = action;
 
-  if (!segmentationLayer) {
+  // Only an segmentation layer with an existing fallback layer can have meshfiles.
+  if (
+    !segmentationLayer ||
+    !("fallbackLayer" in segmentationLayer) ||
+    segmentationLayer.fallbackLayer == null
+  ) {
     callback([]);
     return;
   }

commit b2ccbaaa7bb4113e93a1303298f3995d6842d9b7

Fix meshfile fetching for view only mode (#8728)
### URL of deployed dev instance (used for testing):
- https://___.webknossos.xyz

### Steps to test:
- Open a dataset with meshfile & volume layer in view only mode and load
a precomputed mesh. Should work

### TODOs:
- [ ] ...

### Issues:
- fixes bug introduced by #8726 and noticed by
https://scm.slack.com/archives/C02H5T8Q08P/p1751285529575259?thread_ts=1751282276.200889&cid=C02H5T8Q08P

------
(Please delete unneeded items, merge only when none are left open)
- [ ] Added changelog entry (create a `$PR_NUMBER.md` file in
`unreleased_changes` or use `./tools/create-changelog-entry.py`)

frontend/javascripts/viewer/model/accessors/volumetracing_accessor.ts
@@ -79,6 +79,13 @@ export function getVolumeTracingById(
   return volumeTracing;
 }
 
+export function isTracingLayerWithoutFallback(segmentationLayer: APIDataLayer): boolean {
+  const isTracingLayer = "tracingId" in segmentationLayer && segmentationLayer.tracingId != null;
+  const hasFallbackLayer =
+    "fallbackLayer" in segmentationLayer && segmentationLayer.fallbackLayer != null;
+  return isTracingLayer && !hasFallbackLayer;
+}
+
 export function getVolumeTracingLayers(dataset: APIDataset): Array<APISegmentationLayer> {
   const layers = getSegmentationLayers(dataset);
   return layers.filter((layer) => layer.tracingId != null);

frontend/javascripts/viewer/model/sagas/meshes/precomputed_mesh_saga.ts
@@ -29,6 +29,7 @@ import {
 import {
   getEditableMappingForVolumeTracingId,
   getTracingForSegmentationLayer,
+  isTracingLayerWithoutFallback,
 } from "viewer/model/accessors/volumetracing_accessor";
 import type { Action } from "viewer/model/actions/actions";
 import {
@@ -58,12 +59,8 @@ let fetchDeferredsPerLayer: Record<string, Deferred<Array<APIMeshFileInfo>, unkn
 function* maybeFetchMeshFiles(action: MaybeFetchMeshFilesAction): Saga<void> {
   const { segmentationLayer, dataset, mustRequest, autoActivate, callback } = action;
 
-  // Only an segmentation layer with an existing fallback layer can have meshfiles.
-  if (
-    !segmentationLayer ||
-    !("fallbackLayer" in segmentationLayer) ||
-    segmentationLayer.fallbackLayer == null
-  ) {
+  // Only an segmentation | tracing layer with an existing fallback layer can have meshFiles.
+  if (!segmentationLayer || isTracingLayerWithoutFallback(segmentationLayer)) {
     callback([]);
     return;
   }
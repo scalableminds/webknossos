ade03c622df6a4d3aa431ee6bfc0973872effb3c
Michael Büßemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>
2025-07-03 13:42:23 +0200

    sort imports

--- frontend/javascripts/viewer/model/sagas/skeletontracing_saga.ts
+++ frontend/javascripts/viewer/model/sagas/skeletontracing_saga.ts
@@ -83,13 +83,13 @@ import { api } from "viewer/singletons";
 import type { SkeletonTracing, WebknossosState } from "viewer/store";
 import Store from "viewer/store";
 import { diffBoundingBoxes, diffGroups } from "../helpers/diff_helpers";
-import type { MutableNode, Node, NodeMap, Tree, TreeMap } from "../types/tree_types";
-import { ensureWkReady } from "./ready_sagas";
-import { takeWithBatchActionSupport } from "./saga_helpers";
 import {
   eulerAngleToReducerInternalMatrix,
   reducerInternalMatrixToEulerAngle,
 } from "../helpers/rotation_helpers";
+import type { MutableNode, Node, NodeMap, Tree, TreeMap } from "../types/tree_types";
+import { ensureWkReady } from "./ready_sagas";
+import { takeWithBatchActionSupport } from "./saga_helpers";
 
 function getNodeRotationWithoutPlaneRotation(activeNode: Readonly<MutableNode>): Vector3 {
   // In orthogonal view mode, we need to subtract the

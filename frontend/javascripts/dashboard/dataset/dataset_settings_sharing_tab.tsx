fd6f247dce11a97722f17bc05fcf1f6922fe00f1
Tom Herold <tom.herold@scalableminds.com>
2025-07-11 13:41:10 +0200

    add useQuery to fro dataset sharing token

--- frontend/javascripts/dashboard/dataset/dataset_settings_sharing_tab.tsx
+++ frontend/javascripts/dashboard/dataset/dataset_settings_sharing_tab.tsx
@@ -9,42 +9,41 @@ import TeamSelectionComponent from "dashboard/dataset/team_selection_component";
 import { useEffectOnlyOnce, useWkSelector } from "libs/react_hooks";
 import { isUserAdminOrDatasetManager, isUserAdminOrTeamManager } from "libs/utils";
 import window from "libs/window";
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { getReadableURLPart } from "viewer/model/accessors/dataset_accessor";
 import { useDatasetSettingsContext } from "./dataset_settings_context";
 import { SettingsTitle } from "admin/account/helpers/settings_title";
 import { SettingsCard, type SettingsCardProps } from "admin/account/helpers/settings_card";
 import { useWatch } from "antd/es/form/Form";
+import { useQuery } from "@tanstack/react-query";
 
 export default function DatasetSettingsSharingTab() {
   const { form, datasetId, dataset } = useDatasetSettingsContext();
-  const [sharingToken, setSharingToken] = useState("");
   const activeUser = useWkSelector((state) => state.activeUser);
   const isDatasetManagerOrAdmin = isUserAdminOrDatasetManager(activeUser);
 
   const isDatasetPublic = useWatch(["dataset", "isPublic"], form);
-
-  async function fetch() {
-    const newSharingToken = await getDatasetSharingToken(datasetId);
-    setSharingToken(newSharingToken);
-  }
-
-  useEffectOnlyOnce(() => {
-    fetch();
+  const { data: sharingToken, refetch } = useQuery({
+    queryKey: ["datasetSharingToken", datasetId],
+    queryFn: () => getDatasetSharingToken(datasetId),
   });
 
-  async function handleRevokeSharingLink(): Promise<void> {
+  const handleRevokeSharingLink = useCallback(async () => {
     await revokeDatasetSharingToken(datasetId);
-    const newSharingToken = await getDatasetSharingToken(datasetId);
-    setSharingToken(newSharingToken);
-  }
+    refetch();
+  }, [datasetId, refetch]);
 
-  function getSharingLink() {
+  const sharingLink = useMemo(() => {
     const tokenSuffix = `?token=${sharingToken}`;
-    return `${window.location.origin}/datasets/${dataset ? getReadableURLPart(dataset) : datasetId}/view${isDatasetPublic ? "" : tokenSuffix}`;
-  }
+    const sharingLink = `${window.location.origin}/datasets/${dataset ? getReadableURLPart(dataset) : datasetId}/view${isDatasetPublic ? "" : tokenSuffix}`;
+    return (
+      <Typography.Paragraph copyable ellipsis={{ expandable: true }}>
+        {sharingLink}
+      </Typography.Paragraph>
+    );
+  }, [dataset, datasetId, isDatasetPublic, sharingToken]);
 
-  function getUserAccessList() {
+  const userAccessList = useMemo(() => {
     if (!activeUser || !dataset) return undefined;
     if (!isUserAdminOrTeamManager(activeUser)) return undefined;
 
@@ -61,69 +60,77 @@ export default function DatasetSettingsSharingTab() {
         ]}
       />
     );
-  }
+  }, [activeUser, dataset]);
 
-  const sharingItems: SettingsCardProps[] = [
-    {
-      title: "Make dataset publicly accessible",
-      tooltip: "Make your dataset public, for anonymous/unregistered users to access your dataset.",
-      content: (
-        <Form.Item name={["dataset", "isPublic"]} valuePropName="checked">
-          <Switch />
-        </Form.Item>
-      ),
-    },
-    {
-      title: "Additional team access permissions for this dataset",
-      tooltip:
-        "The dataset can be seen by administrators, dataset managers and by teams that have access to the folder in which the dataset is located. If you want to grant additional teams access, define these teams here.",
-      content: (
-        <Form.Item name={["dataset", "allowedTeams"]} validateStatus="success">
-          <PricingEnforcedBlur requiredPricingPlan={PricingPlanEnum.Team}>
-            <TeamSelectionComponent
-              mode="multiple"
-              allowNonEditableTeams={isDatasetManagerOrAdmin}
-            />
-          </PricingEnforcedBlur>
-        </Form.Item>
-      ),
-    },
-    {
-      title: "Sharing Link",
-      content: (
-        <>
-          <Typography.Paragraph copyable ellipsis={{ expandable: true }}>
-            {getSharingLink()}
-          </Typography.Paragraph>
-          <Space.Compact>
-            {!isDatasetPublic && (
-              <Tooltip
-                title={
-                  <span>
-                    The URL contains a secret token which enables anybody with this link to view the
-                    dataset. Renew the token to make the old link invalid.
-                  </span>
-                }
-              >
-                <AsyncButton onClick={handleRevokeSharingLink} icon={<RetweetOutlined />}>
-                  Renew Authorization Token
-                </AsyncButton>
-              </Tooltip>
-            )}
-          </Space.Compact>
-        </>
-      ),
-      tooltip:
-        "The sharing link can be used to allow unregistered users to view this dataset. If the dataset itself is not public, the link contains a secret token which ensures that the dataset can be opened if you know the special link.",
-    },
+  const sharingItems: SettingsCardProps[] = useMemo(
+    () => [
+      {
+        title: "Make dataset publicly accessible",
+        tooltip:
+          "Make your dataset public, for anonymous/unregistered users to access your dataset.",
+        content: (
+          <Form.Item name={["dataset", "isPublic"]} valuePropName="checked">
+            <Switch />
+          </Form.Item>
+        ),
+      },
+      {
+        title: "Additional team access permissions for this dataset",
+        tooltip:
+          "The dataset can be seen by administrators, dataset managers and by teams that have access to the folder in which the dataset is located. If you want to grant additional teams access, define these teams here.",
+        content: (
+          <Form.Item name={["dataset", "allowedTeams"]} validateStatus="success">
+            <PricingEnforcedBlur requiredPricingPlan={PricingPlanEnum.Team}>
+              <TeamSelectionComponent
+                mode="multiple"
+                allowNonEditableTeams={isDatasetManagerOrAdmin}
+              />
+            </PricingEnforcedBlur>
+          </Form.Item>
+        ),
+      },
+      {
+        title: "Sharing Link",
+        content: (
+          <>
+            {sharingLink}
+            <Space.Compact>
+              {!isDatasetPublic && (
+                <Tooltip
+                  title={
+                    <span>
+                      The URL contains a secret token which enables anybody with this link to view
+                      the dataset. Renew the token to make the old link invalid.
+                    </span>
+                  }
+                >
+                  <AsyncButton onClick={handleRevokeSharingLink} icon={<RetweetOutlined />}>
+                    Renew Authorization Token
+                  </AsyncButton>
+                </Tooltip>
+              )}
+            </Space.Compact>
+          </>
+        ),
+        tooltip:
+          "The sharing link can be used to allow unregistered users to view this dataset. If the dataset itself is not public, the link contains a secret token which ensures that the dataset can be opened if you know the special link.",
+      },
 
-    {
-      title: "Users with access permission to work with this dataset",
-      tooltip:
-        "Dataset access is based on the specified team permissions and individual user roles. Any changes will only appear after pressing the Save button.",
-      content: getUserAccessList(),
-    },
-  ];
+      {
+        title: "Users with access permission to work with this dataset",
+        tooltip:
+          "Dataset access is based on the specified team permissions and individual user roles. Any changes will only appear after pressing the Save button.",
+        content: userAccessList,
+      },
+    ],
+    [
+      sharingLink,
+      userAccessList,
+      handleRevokeSharingLink,
+      isDatasetPublic,
+      isDatasetManagerOrAdmin,
+    ],
+  );
 
   return (
     <div>

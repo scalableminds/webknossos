scalableminds/webknossos

commit e489f9d8979751189ce569bf3c08549baf09ad37

Rework organization page (#8679)
This PR refreshes the layout and styling of the organization page to be
in inline with the account setting page.

<img width="1777" alt="Screenshot 2025-06-16 at 20 21 10"
src="https://github.com/user-attachments/assets/bdb4e795-213b-440e-ad45-ffb599569ffb"
/>
<img width="1777" alt="Screenshot 2025-06-16 at 20 21 16"
src="https://github.com/user-attachments/assets/d0cc6991-b581-4877-846b-7a2e23299c76"
/>
<img width="1777" alt="Screenshot 2025-06-16 at 20 21 25"
src="https://github.com/user-attachments/assets/ce8fa335-ab7b-4d7a-b639-672c376b7af1"
/>



### URL of deployed dev instance (used for testing):
- https://___.webknossos.xyz

### Steps to test:
- Make sure to test various orga configs: Basic, Team, Power and
variations on too many users, too much storage space, subscription
exceeded etc.
- Check light/dark mode


### Issues:
- related to #8672 

------
(Please delete unneeded items, merge only when none are left open)
- [x] Updated
[changelog](../blob/master/CHANGELOG.unreleased.md#unreleased)
- [ ] Updated [migration
guide](../blob/master/MIGRATIONS.unreleased.md#unreleased) if applicable
- [ ] Updated [documentation](../blob/master/docs) if applicable
- [ ] Adapted [wk-libs python
client](https://github.com/scalableminds/webknossos-libs/tree/master/webknossos/webknossos/client)
if relevant API parts change
- [ ] Removed dev-only changes like prints and application.conf edits
- [x] Considered [common edge
cases](../blob/master/.github/common_edge_cases.md)
- [ ] Needs datastore update after deployment

---------

Co-authored-by: Philipp Otto <philippotto@users.noreply.github.com>

frontend/javascripts/admin/account/account_auth_token_view.tsx
@@ -3,7 +3,7 @@ import { getAuthToken, revokeAuthToken } from "admin/rest_api";
 import { Button, Descriptions, Popover, Spin, Typography } from "antd";
 import { useWkSelector } from "libs/react_hooks";
 import { useEffect, useState } from "react";
-import { AccountSettingsTitle } from "./account_profile_view";
+import { SettingsTitle } from "./helpers/settings_title";
 
 const { Text } = Typography;
 
@@ -80,7 +80,7 @@ function AccountAuthTokenView() {
 
   return (
     <div>
-      <AccountSettingsTitle
+      <SettingsTitle
         title="API Authorization"
         description="Access the WEBKNOSSO Python API with your API token"
       />

frontend/javascripts/admin/organization/organization_overview_view.tsx
@@ -1,0 +1,202 @@
+import { PlusOutlined } from "@ant-design/icons";
+import { SettingsTitle } from "admin/account/helpers/settings_title";
+import { getPricingPlanStatus, getUsers, updateOrganization } from "admin/rest_api";
+import { Button, Col, Row, Spin, Tooltip, Typography } from "antd";
+import { formatCountToDataAmountUnit } from "libs/format_utils";
+import Toast from "libs/toast";
+import { useEffect, useState } from "react";
+import type { APIOrganization, APIPricingPlanStatus } from "types/api_types";
+import { setActiveOrganizationAction } from "viewer/model/actions/organization_actions";
+import { Store } from "viewer/singletons";
+import { SettingsCard } from "../account/helpers/settings_card";
+import {
+  PlanAboutToExceedAlert,
+  PlanExceededAlert,
+  PlanExpirationCard,
+  PlanUpgradeCard,
+} from "./organization_cards";
+import { PricingPlanEnum, getActiveUserCount } from "./pricing_plan_utils";
+import UpgradePricingPlanModal from "./upgrade_plan_modal";
+
+const ORGA_NAME_REGEX_PATTERN = /^[A-Za-z0-9\\-_\\. ß]+$/;
+
+export function OrganizationOverviewView({ organization }: { organization: APIOrganization }) {
+  const [isFetchingData, setIsFetchingData] = useState(true);
+  const [activeUsersCount, setActiveUsersCount] = useState(1);
+  const [pricingPlanStatus, setPricingPlanStatus] = useState<APIPricingPlanStatus | null>(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    setIsFetchingData(true);
+    const [users, pricingPlanStatus] = await Promise.all([getUsers(), getPricingPlanStatus()]);
+
+    setPricingPlanStatus(pricingPlanStatus);
+    setActiveUsersCount(getActiveUserCount(users));
+    setIsFetchingData(false);
+  }
+
+  async function setOrganizationName(newOrgaName: string) {
+    if (!ORGA_NAME_REGEX_PATTERN.test(newOrgaName)) {
+      Toast.error(
+        "Organization name can only contain letters, numbers, spaces, and the following special characters: - _ . ß",
+      );
+      return;
+    }
+
+    const updatedOrganization = await updateOrganization(
+      organization.id,
+      newOrgaName,
+      organization.newUserMailingList,
+    );
+    Store.dispatch(setActiveOrganizationAction(updatedOrganization));
+  }
+
+  const maxUsersCountLabel =
+    organization.includedUsers === Number.POSITIVE_INFINITY ? "∞" : organization.includedUsers;
+
+  const includedStorageLabel =
+    organization.includedStorageBytes === Number.POSITIVE_INFINITY
+      ? "∞"
+      : formatCountToDataAmountUnit(organization.includedStorageBytes, true);
+
+  const usedStorageLabel = formatCountToDataAmountUnit(organization.usedStorageBytes, true);
+
+  let upgradeUsersAction: React.ReactNode = null;
+  let upgradeStorageAction: React.ReactNode = null;
+
+  if (
+    organization.pricingPlan === PricingPlanEnum.Basic ||
+    organization.pricingPlan === PricingPlanEnum.Team ||
+    organization.pricingPlan === PricingPlanEnum.TeamTrial
+  ) {
+    upgradeUsersAction = (
+      <Button
+        shape="circle"
+        size="small"
+        key="upgradeUsersAction"
+        icon={<PlusOutlined />}
+        onClick={
+          organization.pricingPlan === PricingPlanEnum.Basic
+            ? () => UpgradePricingPlanModal.upgradePricingPlan(organization)
+            : UpgradePricingPlanModal.upgradeUserQuota
+        }
+      />
+    );
+
+    upgradeStorageAction = (
+      <Button
+        shape="circle"
+        size="small"
+        key="upgradeStorageAction"
+        icon={<PlusOutlined />}
+        onClick={
+          organization.pricingPlan === PricingPlanEnum.Basic
+            ? () => UpgradePricingPlanModal.upgradePricingPlan(organization)
+            : UpgradePricingPlanModal.upgradeStorageQuota
+        }
+      />
+    );
+  }
+  const buyMoreCreditsAction = (
+    <Tooltip title="Disabled during testing phase" key="buyMoreCreditsAction">
+      <Button
+        type="default"
+        shape="circle"
+        icon={<PlusOutlined />}
+        size="small"
+        key="buyMoreCreditsAction"
+        onClick={UpgradePricingPlanModal.orderWebknossosCredits}
+        disabled
+      />
+    </Tooltip>
+  );
+
+  const orgaStats = [
+    {
+      key: "name",
+      title: "Name",
+      value: (
+        <Typography.Text
+          editable={{
+            onChange: setOrganizationName,
+          }}
+        >
+          {organization.name}
+        </Typography.Text>
+      ),
+    },
+    {
+      key: "owner",
+      title: "Owner",
+      value: organization.ownerName,
+    },
+    {
+      key: "plan",
+      title: "Current Plan",
+      value: organization.pricingPlan,
+      explanation: (
+        <a href="https://webknossos.org/pricing" target="_blank" rel="noopener noreferrer">
+          Compare all plans
+        </a>
+      ),
+    },
+    {
+      key: "users",
+      title: "Users",
+      value: `${activeUsersCount} / ${maxUsersCountLabel}`,
+      action: upgradeUsersAction,
+    },
+    {
+      key: "storage",
+      title: "Storage",
+      value: `${usedStorageLabel} / ${includedStorageLabel}`,
+      action: upgradeStorageAction,
+    },
+
+    {
+      key: "credits",
+      title: "WEBKNOSSOS Credits",
+      value: "2",
+      action: buyMoreCreditsAction,
+    },
+  ];
+
+  return (
+    <>
+      <SettingsTitle title={organization.name} description="Manage your organization." />
+      {pricingPlanStatus?.isExceeded ? <PlanExceededAlert organization={organization} /> : null}
+      {pricingPlanStatus?.isAlmostExceeded && !pricingPlanStatus.isExceeded ? (
+        <PlanAboutToExceedAlert organization={organization} />
+      ) : null}
+      <Spin spinning={isFetchingData}>
+        <Row gutter={[24, 24]} style={{ marginBottom: 24 }}>
+          {orgaStats.map((stat) => (
+            <Col span={8} key={stat.key}>
+              <SettingsCard
+                title={stat.title}
+                description={stat.value}
+                action={stat.action}
+                explanation={stat.explanation}
+              />
+            </Col>
+          ))}
+        </Row>
+      </Spin>
+      <PlanExpirationCard organization={organization} />
+      {organization.pricingPlan === PricingPlanEnum.Basic ||
+      organization.pricingPlan === PricingPlanEnum.Team ||
+      organization.pricingPlan === PricingPlanEnum.TeamTrial ? (
+        <>
+          <SettingsTitle
+            title="Unlock more features"
+            description="Upgrade your organization to unlock more collaboration and proofreading features for your team."
+          />
+          <PlanUpgradeCard organization={organization} />
+        </>
+      ) : null}
+    </>
+  );
+}

app/controllers/OrganizationController.scala
@@ -154,7 +154,7 @@ class OrganizationController @Inject()(
           _ <- Fox.fromBool(request.identity.isAdminOf(organization._id)) ?~> "notAllowed" ~> FORBIDDEN
           _ <- organizationDAO.updateFields(organization._id, name, newUserMailingList)
           updated <- organizationDAO.findOne(organization._id)
-          organizationJson <- organizationService.publicWrites(updated)
+          organizationJson <- organizationService.publicWrites(updated, Some(request.identity))
         } yield Ok(organizationJson)
     }
   }

frontend/javascripts/admin/account/account_password_view.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 import { type RouteComponentProps, withRouter } from "react-router-dom";
 import { logoutUserAction } from "viewer/model/actions/user_actions";
 import Store from "viewer/store";
-import { AccountSettingsTitle } from "./account_profile_view";
+import { SettingsTitle } from "./helpers/settings_title";
 const FormItem = Form.Item;
 const { Password } = Input;
 
@@ -186,7 +186,7 @@ function AccountPasswordView({ history }: Props) {
 
   return (
     <div>
-      <AccountSettingsTitle title="Password" description="Manage and update your password" />
+      <SettingsTitle title="Password" description="Manage and update your password" />
       <Descriptions
         column={2}
         layout="vertical"
@@ -195,7 +195,7 @@ function AccountPasswordView({ history }: Props) {
         style={{ marginBottom: "3rem" }}
       />
 
-      <AccountSettingsTitle title="Passkeys" description="Login passwordless with Passkeys" />
+      <SettingsTitle title="Passkeys" description="Login passwordless with Passkeys" />
       <List
         className="demo-loadmore-list"
         itemLayout="horizontal"

frontend/javascripts/admin/account/account_profile_view.tsx
@@ -1,6 +1,6 @@
 import { CheckOutlined, DownOutlined } from "@ant-design/icons";
 import { updateSelectedThemeOfUser } from "admin/rest_api";
-import { Descriptions, Divider, Dropdown, Typography } from "antd";
+import { Descriptions, Dropdown } from "antd";
 import { useWkSelector } from "libs/react_hooks";
 import * as Utils from "libs/utils";
 import { getSystemColorTheme } from "theme";
@@ -9,23 +9,7 @@ import { formatUserName } from "viewer/model/accessors/user_accessor";
 import { setThemeAction } from "viewer/model/actions/ui_actions";
 import { setActiveUserAction } from "viewer/model/actions/user_actions";
 import Store from "viewer/store";
-
-const { Text } = Typography;
-
-export function AccountSettingsTitle({
-  title,
-  description,
-}: { title: string; description: string }) {
-  return (
-    <div>
-      <h2 style={{ marginBottom: 0 }}>{title}</h2>
-      <Text type="secondary" style={{ display: "block" }}>
-        {description}
-      </Text>
-      <Divider style={{ margin: "12px 0 32px 0" }} />
-    </div>
-  );
-}
+import { SettingsTitle } from "./helpers/settings_title";
 
 function AccountProfileView() {
   const activeUser = useWkSelector((state) => state.activeUser);
@@ -100,7 +84,7 @@ function AccountProfileView() {
 
   return (
     <div>
-      <AccountSettingsTitle
+      <SettingsTitle
         title="Profile"
         description="Manage your personal information and preferences"
       />

frontend/javascripts/admin/account/helpers/settings_card.tsx
@@ -1,0 +1,31 @@
+import { InfoCircleOutlined } from "@ant-design/icons";
+import { Card, Flex, Popover, Typography } from "antd";
+
+interface SettingsCardProps {
+  title: string;
+  description: React.ReactNode;
+  explanation?: React.ReactNode;
+  action?: React.ReactNode;
+}
+
+export function SettingsCard({ title, description, explanation, action }: SettingsCardProps) {
+  return (
+    <Card style={{ minHeight: 105 }}>
+      <Typography.Text type="secondary" style={{ fontSize: 14 }}>
+        <Flex justify="space-between">
+          <div>
+            {title}
+
+            {explanation != null ? (
+              <Popover content={explanation}>
+                <InfoCircleOutlined style={{ marginLeft: 4 }} />
+              </Popover>
+            ) : null}
+          </div>
+          {action}
+        </Flex>
+      </Typography.Text>
+      <div style={{ fontSize: 16, marginTop: 4 }}>{description}</div>
+    </Card>
+  );
+}

frontend/javascripts/admin/account/helpers/settings_title.tsx
@@ -1,0 +1,21 @@
+import { Divider, Typography } from "antd";
+
+const { Text } = Typography;
+
+export function SettingsTitle({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
+  return (
+    <div>
+      <h2 style={{ marginBottom: 0 }}>{title}</h2>
+      <Text type="secondary" style={{ display: "block" }}>
+        {description}
+      </Text>
+      <Divider style={{ margin: "12px 0 32px 0" }} />
+    </div>
+  );
+}

frontend/javascripts/admin/organization/organization_cards.tsx
@@ -1,21 +1,12 @@
-import {
-  FieldTimeOutlined,
-  PlusCircleOutlined,
-  RocketOutlined,
-  SafetyOutlined,
-} from "@ant-design/icons";
-import { Alert, Button, Card, Col, Progress, Row, Tooltip } from "antd";
+import { FieldTimeOutlined, PlusCircleOutlined } from "@ant-design/icons";
+import { Alert, Button, Card, Col, Row } from "antd";
 import { formatDateInLocalTimeZone } from "components/formatted_date";
 import dayjs from "dayjs";
-import { formatCountToDataAmountUnit, formatCreditsString } from "libs/format_utils";
 import { useWkSelector } from "libs/react_hooks";
-import type React from "react";
 import type { APIOrganization } from "types/api_types";
 import Constants from "viewer/constants";
 import {
   PricingPlanEnum,
-  hasPricingPlanExceededStorage,
-  hasPricingPlanExceededUsers,
   hasPricingPlanExpired,
   isUserAllowedToRequestUpgrades,
   powerPlanFeatures,
@@ -23,126 +14,89 @@ import {
 } from "./pricing_plan_utils";
 import UpgradePricingPlanModal from "./upgrade_plan_modal";
 
-export function TeamAndPowerPlanUpgradeCards({
-  teamUpgradeCallback,
+export function TeamPlanUpgradeCard({ teamUpgradeCallback }: { teamUpgradeCallback: () => void }) {
+  return (
+    <Card
+      title={`${PricingPlanEnum.Team} Plan`}
+      styles={{ body: { minHeight: 220 } }}
+      actions={[
+        <Button type="primary" onClick={teamUpgradeCallback} key="buy-teamupgrade-button">
+          <PlusCircleOutlined /> Buy Upgrade
+        </Button>,
+      ]}
+    >
+      <ul>
+        {teamPlanFeatures.map((feature) => (
+          <li key={feature.slice(0, 10)}>{feature}</li>
+        ))}
+      </ul>
+    </Card>
+  );
+}
+
+export function PowerPlanUpgradeCard({
   powerUpgradeCallback,
+  description,
 }: {
-  teamUpgradeCallback: () => void;
   powerUpgradeCallback: () => void;
+  description?: string;
 }) {
   return (
-    <Row gutter={24}>
-      <Col span={12}>
-        <Card
-          title={`${PricingPlanEnum.Team} Plan`}
-          styles={{ body: { minHeight: 220, opacity: 0.8 } }}
-          actions={[
-            <Button type="primary" onClick={teamUpgradeCallback} key="buy-teamupgrade-button">
-              <PlusCircleOutlined /> Buy Upgrade
-            </Button>,
-          ]}
-        >
-          <ul>
-            {teamPlanFeatures.map((feature) => (
-              <li key={feature.slice(0, 10)}>{feature}</li>
-            ))}
-          </ul>
-        </Card>
-      </Col>
-      <Col span={12}>
-        <Card
-          title={`${PricingPlanEnum.Power} Plan`}
-          styles={{ body: { minHeight: 220, opacity: 0.8 } }}
-          actions={[
-            <Button type="primary" onClick={powerUpgradeCallback} key="buy-power-upgrade-button">
-              <PlusCircleOutlined /> Buy Upgrade
-            </Button>,
-          ]}
-        >
-          <ul>
-            {powerPlanFeatures.map((feature) => (
-              <li key={feature.slice(0, 10)}>{feature}</li>
-            ))}
-          </ul>
-        </Card>
-      </Col>
-    </Row>
+    <Card
+      title={`${PricingPlanEnum.Power} Plan`}
+      styles={{ body: { minHeight: 220 } }}
+      actions={[
+        <Button type="primary" onClick={powerUpgradeCallback} key="buy-power-upgrade-button">
+          <PlusCircleOutlined /> Buy Upgrade
+        </Button>,
+      ]}
+    >
+      {description ? <p>{description}</p> : null}
+      <ul>
+        {powerPlanFeatures.map((feature) => (
+          <li key={feature.slice(0, 10)}>{feature}</li>
+        ))}
+      </ul>
+    </Card>
   );
 }
 
 export function PlanUpgradeCard({ organization }: { organization: APIOrganization }) {
   if (
-    organization.pricingPlan === PricingPlanEnum.Power ||
-    organization.pricingPlan === PricingPlanEnum.PowerTrial ||
-    organization.pricingPlan === PricingPlanEnum.Custom
-  )
-    return null;
-
-  let title = "Upgrade to unlock more features";
-  let cardBody = (
-    <TeamAndPowerPlanUpgradeCards
-      teamUpgradeCallback={() =>
-        UpgradePricingPlanModal.upgradePricingPlan(organization, PricingPlanEnum.Team)
-      }
-      powerUpgradeCallback={() =>
-        UpgradePricingPlanModal.upgradePricingPlan(organization, PricingPlanEnum.Power)
-      }
-    />
-  );
-
-  if (
     organization.pricingPlan === PricingPlanEnum.Team ||
     organization.pricingPlan === PricingPlanEnum.TeamTrial
   ) {
-    title = `Upgrade to ${PricingPlanEnum.Power} Plan`;
-    cardBody = (
+    return (
       <Row gutter={24}>
-        <Col span={18}>
-          <p>
-            Upgrading your WEBKNOSSOS plan will unlock more advanced features and increase your user
-            and storage quotas.
-          </p>
-          <span>
-            <ul>
-              {powerPlanFeatures.map((feature) => (
-                <li key={feature.slice(0, 10)}>{feature}</li>
-              ))}
-            </ul>
-          </span>
-        </Col>
-        <Col span={6}>
-          <Button
-            type="primary"
-            icon={<RocketOutlined />}
-            onClick={() => UpgradePricingPlanModal.upgradePricingPlan(organization)}
-            style={{ borderColor: "white" }}
-          >
-            Upgrade Now
-          </Button>
+        <Col span={24}>
+          <PowerPlanUpgradeCard
+            description="Upgrade your organization to unlock more collaboration and proofreading features for your team."
+            powerUpgradeCallback={() =>
+              UpgradePricingPlanModal.upgradePricingPlan(organization, PricingPlanEnum.Power)
+            }
+          />
         </Col>
       </Row>
     );
   }
 
   return (
-    <Card
-      title={title}
-      style={{
-        marginBottom: 20,
-      }}
-      styles={{
-        body: {
-          background: "var(--color-wk-blue)",
-          color: "white",
-        },
-      }}
-    >
-      <p>
-        Upgrading your WEBKNOSSOS plan will unlock more advanced features and increase your user and
-        storage quotas.
-      </p>
-      {cardBody}
-    </Card>
+    <Row gutter={24}>
+      <Col span={12}>
+        <TeamPlanUpgradeCard
+          teamUpgradeCallback={() =>
+            UpgradePricingPlanModal.upgradePricingPlan(organization, PricingPlanEnum.Team)
+          }
+        />
+      </Col>
+      <Col span={12}>
+        <PowerPlanUpgradeCard
+          powerUpgradeCallback={() =>
+            UpgradePricingPlanModal.upgradePricingPlan(organization, PricingPlanEnum.Power)
+          }
+        />
+      </Col>
+    </Row>
   );
 }
 
@@ -150,7 +104,7 @@ export function PlanExpirationCard({ organization }: { organization: APIOrganiza
   if (organization.paidUntil === Constants.MAXIMUM_DATE_TIMESTAMP) return null;
 
   return (
-    <Card style={{ marginBottom: 20 }}>
+    <Card style={{ marginBottom: 36 }}>
       <Row gutter={24}>
         <Col flex="auto">
           Your current plan is paid until{" "}
@@ -167,158 +121,6 @@ export function PlanExpirationCard({ organization }: { organization: APIOrganiza
         </Col>
       </Row>
     </Card>
-  );
-}
-
-export function PlanDashboardCard({
-  organization,
-  activeUsersCount,
-}: {
-  organization: APIOrganization;
-  activeUsersCount: number;
-}) {
-  const usedUsersPercentage = (activeUsersCount / organization.includedUsers) * 100;
-  const usedStoragePercentage =
-    (organization.usedStorageBytes / organization.includedStorageBytes) * 100;
-
-  const hasExceededUserLimit = hasPricingPlanExceededUsers(organization, activeUsersCount);
-  const hasExceededStorageLimit = hasPricingPlanExceededStorage(organization);
-
-  const maxUsersCountLabel =
-    organization.includedUsers === Number.POSITIVE_INFINITY ? "∞" : organization.includedUsers;
-
-  const includedStorageLabel =
-    organization.includedStorageBytes === Number.POSITIVE_INFINITY
-      ? "∞"
-      : formatCountToDataAmountUnit(organization.includedStorageBytes, true);
-
-  const usedStorageLabel = formatCountToDataAmountUnit(organization.usedStorageBytes, true);
-
-  const storageLabel = (
-    <span style={{ display: "inline-block", wordBreak: "break-word", width: 100 }}>
-      {usedStorageLabel} /
-      <wbr />
-      {includedStorageLabel}
-    </span>
-  );
-
-  const redStrokeColor = "#ff4d4f";
-  const greenStrokeColor = "#52c41a";
-
-  let upgradeUsersAction: React.ReactNode[] = [];
-  let upgradeStorageAction: React.ReactNode[] = [];
-  let upgradePlanAction: React.ReactNode[] = [];
-
-  if (
-    organization.pricingPlan === PricingPlanEnum.Basic ||
-    organization.pricingPlan === PricingPlanEnum.Team ||
-    organization.pricingPlan === PricingPlanEnum.TeamTrial
-  ) {
-    upgradeUsersAction = [
-      <span
-        key="upgradeUsersAction"
-        onClick={
-          organization.pricingPlan === PricingPlanEnum.Basic
-            ? () => UpgradePricingPlanModal.upgradePricingPlan(organization)
-            : UpgradePricingPlanModal.upgradeUserQuota
-        }
-      >
-        <PlusCircleOutlined /> Buy Upgrade
-      </span>,
-    ];
-    upgradeStorageAction = [
-      <span
-        key="upgradeStorageAction"
-        onClick={
-          organization.pricingPlan === PricingPlanEnum.Basic
-            ? () => UpgradePricingPlanModal.upgradePricingPlan(organization)
-            : UpgradePricingPlanModal.upgradeStorageQuota
-        }
-      >
-        <PlusCircleOutlined /> Buy Upgrade
-      </span>,
-    ];
-    upgradePlanAction = [
-      [
-        <a
-          href="https://webknossos.org/pricing"
-          target="_blank"
-          rel="noreferrer"
-          key="comparePlanAction"
-        >
-          <SafetyOutlined /> Compare Plans
-        </a>,
-      ],
-    ];
-  }
-  const buyMoreCreditsAction = [
-    <Tooltip title="Disabled during testing phase" key="buyMoreCreditsAction">
-      <Button
-        type="text"
-        key="buyMoreCreditsAction"
-        onClick={UpgradePricingPlanModal.orderWebknossosCredits}
-        disabled
-      >
-        Buy more credits
-      </Button>
-    </Tooltip>,
-  ];
-
-  return (
-    <>
-      <Row gutter={24} justify="space-between" align="stretch" style={{ marginBottom: 20 }}>
-        <Col span={12}>
-          <Card actions={upgradeUsersAction}>
-            <Row style={{ padding: "20px 35px" }} justify="center">
-              <Progress
-                type="dashboard"
-                percent={usedUsersPercentage}
-                format={() => `${activeUsersCount}/${maxUsersCountLabel}`}
-                strokeColor={hasExceededUserLimit ? redStrokeColor : greenStrokeColor}
-                status={hasExceededUserLimit ? "exception" : "active"}
-              />
-            </Row>
-            <Row justify="center">Users</Row>
-          </Card>
-        </Col>
-        <Col span={12}>
-          <Card actions={upgradeStorageAction}>
-            <Row style={{ padding: "20px 35px" }} justify="center">
-              <Progress
-                type="dashboard"
-                percent={usedStoragePercentage}
-                format={() => storageLabel}
-                strokeColor={hasExceededStorageLimit ? redStrokeColor : greenStrokeColor}
-                status={hasExceededStorageLimit ? "exception" : "active"}
-              />
-            </Row>
-            <Row justify="center">Storage</Row>
-          </Card>
-        </Col>
-      </Row>
-      <Row gutter={24} justify="space-between" align="stretch" style={{ marginBottom: 20 }}>
-        <Col span={12}>
-          <Card actions={upgradePlanAction}>
-            <Row justify="center" align="middle" style={{ minHeight: 160 }}>
-              <h3>{organization.pricingPlan}</h3>
-            </Row>
-            <Row justify="center">Current Plan</Row>
-          </Card>
-        </Col>
-        <Col span={12}>
-          <Card actions={buyMoreCreditsAction}>
-            <Row justify="center" align="middle" style={{ minHeight: 160 }}>
-              <h3>
-                {organization.creditBalance != null
-                  ? formatCreditsString(organization.creditBalance)
-                  : "No information access"}
-              </h3>
-            </Row>
-            <Row justify="center">WEBKNOSSOS Credits</Row>
-          </Card>
-        </Col>
-      </Row>
-    </>
   );
 }
 
@@ -379,21 +181,21 @@ export function PlanAboutToExceedAlert({ organization }: { organization: APIOrga
         </Button>
       ),
     });
-  // else {
-  //   alerts.push({
-  //     message:
-  //       "Your organization is about to exceed the storage space included in your current plan. Upgrade now to avoid your account from being blocked.",
-  //     actionButton: (
-  //       <Button
-  //         size="small"
-  //         type="primary"
-  //         onClick={() => UpgradePricingPlanModal.upgradePricingPlan(organization)}
-  //       >
-  //         Upgrade Now
-  //       </Button>
-  //     ),
-  //   });
-  // }
+  else {
+    alerts.push({
+      message:
+        "Your organization is about to exceed the storage space included in your current plan. Upgrade now to avoid your account from being blocked.",
+      actionButton: (
+        <Button
+          size="small"
+          type="primary"
+          onClick={() => UpgradePricingPlanModal.upgradePricingPlan(organization)}
+        >
+          Upgrade Now
+        </Button>
+      ),
+    });
+  }
 
   return (
     <>

commit 0967358f65fd34621bfe21f9f5c71616bf3368b4

format the credits in orga view (#8780)
This PR add formatting for the displayed credits in the organization
page again.

### Issues:
- Fixes a regression from #8679

------
(Please delete unneeded items, merge only when none are left open)
- [ ] Added changelog entry (create a `$PR_NUMBER.md` file in
`unreleased_changes` or use `./tools/create-changelog-entry.py`)
- [ ] Added migration guide entry if applicable (edit the same file as
for the changelog)
- [ ] Updated [documentation](../blob/master/docs) if applicable
- [ ] Adapted [wk-libs python
client](https://github.com/scalableminds/webknossos-libs/tree/master/webknossos/webknossos/client)
if relevant API parts change
- [ ] Removed dev-only changes like prints and application.conf edits
- [ ] Considered [common edge
cases](../blob/master/.github/common_edge_cases.md)
- [ ] Needs datastore update after deployment

---------

Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

frontend/javascripts/admin/account/account_auth_token_view.tsx
@@ -86,7 +86,7 @@ function AccountAuthTokenView() {
     <div>
       <SettingsTitle
         title="API Authorization"
-        description="Access the WEBKNOSSO Python API with your API token"
+        description="Access the WEBKNOSSOS Python API with your API token"
       />
       <Spin size="large" spinning={isLoading}>
         <Row gutter={[24, 24]} style={{ marginBottom: 24 }}>

frontend/javascripts/admin/organization/organization_overview_view.tsx
@@ -2,7 +2,7 @@ import { PlusOutlined } from "@ant-design/icons";
 import { SettingsTitle } from "admin/account/helpers/settings_title";
 import { getPricingPlanStatus, getUsers, updateOrganization } from "admin/rest_api";
 import { Button, Col, Row, Spin, Tooltip, Typography } from "antd";
-import { formatCountToDataAmountUnit } from "libs/format_utils";
+import { formatCountToDataAmountUnit, formatCreditsString } from "libs/format_utils";
 import Toast from "libs/toast";
 import { useEffect, useState } from "react";
 import type { APIOrganization, APIPricingPlanStatus } from "types/api_types";
@@ -159,7 +159,10 @@ export function OrganizationOverviewView({ organization }: { organization: APIOr
     {
       key: "credits",
       title: "WEBKNOSSOS Credits",
-      value: organization.creditBalance || "N/A",
+      value:
+        organization.creditBalance != null
+          ? formatCreditsString(organization.creditBalance)
+          : "N/A",
       action: buyMoreCreditsAction,
     },
   ];
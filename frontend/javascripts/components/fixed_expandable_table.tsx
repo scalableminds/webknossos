76e388271ee4b83797a0014f8aa1802940fd1632
Tom Herold <tom.herold@scalableminds.com>
2025-07-15 09:32:13 +0200

    Refactored several React class components as functional components (#8750)
    In this PR I converted several class components to functional components
    using modern React hooks. Most of the components are located in the
    general `frontend/javascripts/components` directory.
    
    ### Steps to test:
    Check that the view are still showing
    
    
    ### Issues:
    Contributes to https://github.com/scalableminds/webknossos/issues/8747
    
    ------
    (Please delete unneeded items, merge only when none are left open)
    - [ ] Added changelog entry (create a `$PR_NUMBER.md` file in
    `unreleased_changes` or use `./tools/create-changelog-entry.py`)
    - [ ] Added migration guide entry if applicable (edit the same file as
    for the changelog)
    - [ ] Updated [documentation](../blob/master/docs) if applicable
    - [ ] Adapted [wk-libs python
    client](https://github.com/scalableminds/webknossos-libs/tree/master/webknossos/webknossos/client)
    if relevant API parts change
    - [ ] Removed dev-only changes like prints and application.conf edits
    - [ ] Considered [common edge
    cases](../blob/master/.github/common_edge_cases.md)
    - [ ] Needs datastore update after deployment
    
    ---------
    
    Co-authored-by: MichaelBuessemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>

--- frontend/javascripts/components/fixed_expandable_table.tsx
+++ frontend/javascripts/components/fixed_expandable_table.tsx
@@ -1,11 +1,8 @@
 import { Button, Table, type TableProps } from "antd";
-import type { ColumnsType, GetRowKey } from "antd/lib/table/interface";
-import React from "react";
+import type { ColumnsType, ExpandableConfig, GetRowKey } from "antd/lib/table/interface";
+import type React from "react";
+import { useEffect, useState } from "react";
 
-type State = {
-  expandedRows: Array<string>;
-  className?: string;
-};
 /** This is a wrapper for large tables that have fixed columns and support expanded rows.
  *  This wrapper ensures that when rows are expanded no column is fixed as this creates rendering bugs.
  *  If you are using this wrapper, you do not need to set the class "large-table"
@@ -17,72 +14,73 @@ type OwnTableProps<RecordType = any> = TableProps<RecordType> & {
   columns: ColumnsType<RecordType>;
 };
 
-const EMPTY_ARRAY: string[] = [] as const;
+const EMPTY_ARRAY: React.Key[] = [] as const;
 
-export default class FixedExpandableTable extends React.PureComponent<OwnTableProps, State> {
-  state: State = {
-    expandedRows: EMPTY_ARRAY,
+const getAllRowIds = (
+  dataSource: readonly any[] | undefined,
+  rowKey: string | number | symbol | GetRowKey<any> | undefined,
+) => {
+  const canUseRowKey = typeof rowKey === "string";
+  return dataSource != null && canUseRowKey ? dataSource.map((row) => row[rowKey]) : [];
+};
+
+export default function FixedExpandableTable<RecordType>({
+  className,
+  expandable,
+  dataSource,
+  rowKey,
+  columns,
+  ...restProps
+}: OwnTableProps<RecordType>) {
+  const [expandedRows, setExpandedRows] = useState<React.Key[]>(EMPTY_ARRAY);
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: Collapse all rows when source changes
+  useEffect(() => {
+    setExpandedRows(EMPTY_ARRAY);
+  }, [dataSource]);
+
+  const areAllRowsExpanded = dataSource != null && expandedRows.length === dataSource?.length;
+
+  const columnTitleCollapsed = (
+    <Button
+      className="ant-table-row-expand-icon ant-table-row-expand-icon-collapsed"
+      title="Expand all rows"
+      onClick={() => setExpandedRows(getAllRowIds(dataSource, rowKey))}
+    />
+  );
+  const columnTitleExpanded = (
+    <Button
+      className="ant-table-row-expand-icon ant-table-row-expand-icon-expanded"
+      title="Collapse all rows"
+      onClick={() => setExpandedRows(EMPTY_ARRAY)}
+    />
+  );
+
+  const columnsWithAdjustedFixedProp = columns.map((column) => {
+    const columnFixed = expandedRows.length > 0 ? false : column.fixed;
+    return { ...column, fixed: columnFixed };
+  });
+
+  const expandableProp: ExpandableConfig<RecordType> = {
+    ...expandable,
+    expandedRowKeys: expandedRows,
+    onExpandedRowsChange: (selectedRows: readonly React.Key[]) => {
+      setExpandedRows(selectedRows as React.Key[]);
+    },
+    columnTitle: areAllRowsExpanded ? columnTitleExpanded : columnTitleCollapsed,
   };
 
-  getAllRowIds(
-    dataSource: readonly any[] | undefined,
-    rowKey: string | number | symbol | GetRowKey<any> | undefined,
-  ) {
-    const canUseRowKey = typeof rowKey === "string";
-    return dataSource != null && canUseRowKey ? dataSource.map((row) => row[rowKey]) : [];
-  }
-
-  componentDidUpdate(prevProps: Readonly<TableProps<any>>): void {
-    if (prevProps.dataSource !== this.props.dataSource) {
-      this.setState({ expandedRows: EMPTY_ARRAY });
-    }
-  }
-
-  render() {
-    const { expandedRows } = this.state;
-    const { className, expandable, ...restProps } = this.props;
-    const { dataSource, rowKey } = this.props;
-    const areAllRowsExpanded =
-      dataSource != null && this.state.expandedRows.length === dataSource?.length;
-
-    const columnTitleCollapsed = (
-      <Button
-        className="ant-table-row-expand-icon ant-table-row-expand-icon-collapsed"
-        title="Expand all rows"
-        onClick={() => this.setState({ expandedRows: this.getAllRowIds(dataSource, rowKey) })}
-      />
-    );
-    const columnTitleExpanded = (
-      <Button
-        className="ant-table-row-expand-icon ant-table-row-expand-icon-expanded"
-        title="Collapse all rows"
-        onClick={() => this.setState({ expandedRows: EMPTY_ARRAY })}
-      />
-    );
-    const columnsWithAdjustedFixedProp: TableProps["columns"] = this.props.columns.map((column) => {
-      const columnFixed = expandedRows.length > 0 ? false : column.fixed;
-      return { ...column, fixed: columnFixed };
-    });
-    const expandableProp = {
-      ...expandable,
-      expandedRowKeys: expandedRows,
-      onExpandedRowsChange: (selectedRows: readonly React.Key[]) => {
-        this.setState({
-          expandedRows: selectedRows as string[],
-        });
-      },
-      columnTitle: areAllRowsExpanded ? columnTitleExpanded : columnTitleCollapsed,
-    };
-    return (
-      <Table
-        {...restProps}
-        expandable={expandableProp}
-        scroll={{
-          x: "max-content",
-        }}
-        className={`large-table ${className}`}
-        columns={columnsWithAdjustedFixedProp}
-      />
-    );
-  }
+  return (
+    <Table
+      {...restProps}
+      dataSource={dataSource}
+      rowKey={rowKey}
+      expandable={expandableProp}
+      scroll={{
+        x: "max-content",
+      }}
+      className={`large-table ${className}`}
+      columns={columnsWithAdjustedFixedProp}
+    />
+  );
 }

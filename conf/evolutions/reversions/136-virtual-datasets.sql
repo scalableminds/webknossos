82d4796a7a6aabfbcd8dfdef3f7faf18483e5036
frcroth <frcroth@gmail.com>
2025-07-16 14:02:46 +0200

    Update view in migration

--- conf/evolutions/reversions/136-virtual-datasets.sql
+++ conf/evolutions/reversions/136-virtual-datasets.sql
@@ -2,7 +2,9 @@ START TRANSACTION;
 
 do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 136, 'Previous schema version mismatch'; end; $$ LANGUAGE plpgsql;
 
+DROP VIEW IF EXISTS webknossos.datasets_;
 ALTER TABLE webknossos.datasets DROP COLUMN IF EXISTS isVirtual;
+CREATE VIEW webknossos.datasets_ AS SELECT * FROM webknossos.datasets WHERE NOT isDeleted;
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 135;
 

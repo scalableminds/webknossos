446695e8c8dd43446408011212f02ebb23a3c36e
Tom Herold <tom.herold@scalableminds.com>
2025-07-16 11:34:54 +0200

    extract dataset_add_remote_view sub components into separate files

---
+++ frontend/javascripts/admin/dataset/remote/google_auth_form_item.tsx
@@ -0,0 +1,61 @@
+import { UnlockOutlined } from "@ant-design/icons";
+import { Form, Upload } from "antd";
+import { Fragment } from "react";
+import { Unicode } from "viewer/constants";
+
+import type { UploadChangeParam, UploadFile } from "antd/lib/upload";
+
+const FormItem = Form.Item;
+type FileList = UploadFile<any>[];
+
+export function GoogleAuthFormItem({
+  fileList,
+  handleChange,
+  showOptionalHint,
+}: {
+  fileList: FileList;
+  handleChange: (arg: UploadChangeParam<UploadFile<any>>) => void;
+  showOptionalHint?: boolean;
+}) {
+  return (
+    <FormItem
+      name="authFile"
+      label={
+        <Fragment>
+          Google{Unicode.NonBreakingSpace}
+          <a
+            href="https://cloud.google.com/iam/docs/creating-managing-service-account-keys"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Service Account
+          </a>
+          {Unicode.NonBreakingSpace}Key {showOptionalHint && "(Optional)"}
+        </Fragment>
+      }
+      hasFeedback
+    >
+      <Upload.Dragger
+        name="files"
+        fileList={fileList}
+        onChange={handleChange}
+        beforeUpload={() => false}
+      >
+        <p className="ant-upload-drag-icon">
+          <UnlockOutlined
+            style={{
+              margin: 0,
+              fontSize: 35,
+            }}
+          />
+        </p>
+        <p className="ant-upload-text">
+          Click or Drag your Google Cloud Authentication File to this Area to Upload
+        </p>
+        <p className="ant-upload-text-hint">
+          This is only needed if the dataset is located in a non-public Google Cloud Storage bucket
+        </p>
+      </Upload.Dragger>
+    </FormItem>
+  );
+}

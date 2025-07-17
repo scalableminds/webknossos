72dd22824a33bdf3c1f17109351022a24129ecfc
Tom Herold <tom.herold@scalableminds.com>
2025-07-17 17:31:42 +0200

    use three.js src files instead of prebuild binaries

--- webpack.config.js
+++ webpack.config.js
@@ -141,6 +141,7 @@ module.exports = function (env = {}) {
       modules: [srcPath, nodePath, protoPath],
       alias: {
         react: path.resolve("./node_modules/react"),
+        three: path.resolve(__dirname, "node_modules/three/src/Three.js") // build three js from source instead of using their prebuild "binaries"
       },
       extensions: [".ts", ".tsx", ".js", ".json"],
       fallback: {
@@ -210,3 +211,5 @@ module.exports = function (env = {}) {
     ],
   };
 };
+
+

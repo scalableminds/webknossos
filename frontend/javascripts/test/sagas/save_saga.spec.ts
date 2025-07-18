b22737e8ab57792cac113d6619ee609f264ec7b3
Philipp Otto <philipp.4096@gmail.com>
2025-07-18 10:19:06 +0200

    fix one test in save saga spec (but the generator style tests are annoying)

--- frontend/javascripts/test/sagas/save_saga.spec.ts
+++ frontend/javascripts/test/sagas/save_saga.spec.ts
@@ -19,6 +19,7 @@ import {
 } from "viewer/model/sagas/saving/save_queue_draining";
 import { TIMESTAMP } from "test/global_mocks";
 import { sendSaveRequestWithToken } from "admin/rest_api";
+import "test/helpers/apiHelpers"; // ensures Store is available
 
 vi.mock("viewer/model/sagas/root_saga", () => {
   return {
@@ -286,6 +287,10 @@ describe("Save Saga", () => {
     saga.next({
       timeout: "a placeholder",
     }); // put setSaveBusyAction
+
+    saga.next(); // EnsureMaySaveNowAsync
+    saga.next(); // EnsureHasNewestVersionAsync
+    saga.next(1); // select itemCountToSave
 
     saga.next(saveQueue); // call sendSaveRequestToServer
 

3f94638cecba623167903caf3826c6f6e4d32e1e
Tom Herold <tom.herold@scalableminds.com>
2025-07-22 16:54:48 +0200

    fix typing

--- frontend/javascripts/viewer/workers/comlink_wrapper.ts
+++ frontend/javascripts/viewer/workers/comlink_wrapper.ts
@@ -47,18 +47,18 @@ type UseCreateWorkerToUseMe<T> = {
   readonly _wrapped: T;
 };
 export function createWorker<T extends (...args: any) => any>(
-  workerUrl: string,
+  workerFunction: UseCreateWorkerToUseMe<T>,
 ): (...params: Parameters<T>) => Promise<ReturnType<T>> {
   if (wrap == null) {
     // In a node context (e.g., when executing tests), we don't create web workers which is why
     // we can simply return the input function here.
-    // @ts-ignore
-    return WorkerClass;
+    // @ts-expect-error
+    return workerFunction;
   }
 
   return wrap(
     // @ts-ignore
-    new Worker(workerUrl),
+    new Worker(workerFunction),
   );
 }
 export function expose<T>(fn: T): UseCreateWorkerToUseMe<T> {

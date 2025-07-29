scalableminds/webknossos

commit 9ad330ee27bd6a83d199dc7f53f73bd36db592d1

Readd zarr3 streaming v0 (#7995)
* WIP add zarr3 streaming
- Added mag/zarr.json & mag/coords route for datasets (not annotations)

* manage minimal zarr3 support for viewing a dataset and viewing an annotation

* add basic zarr 3 ngff v2 group header route

* add zarr 3 ngff v2 group header route for annotations

* merge Zarr3StreamController into ZarrStreamingController

* ensure full match when parsing zarr coordinates to avoid parsing any non-numerical characters

* make NgffMetadataV2 nd compatible

* refactor code

* remove Zarr3StreamingController.scala

* minor code fixes; mostly remove unused imports

* remove fixed full length match of zarr coordinate regex parsing;
- wk seems to send c.0.x.y.z requests and the c at the start does not match with the regex resulting in invalid coordinate parsing

* add c. as a prefix to the coordinate parsing regex as defined by the zarr spec

* exclude leading c from the zarr3 coordinate parsing

* apply pr feedback

* remove unused imports and format code

* add datasource-properties.json and dir listing routes to zarr 3 streaming

* format code

* add changelog entry

* fix Zarr3GroupHeader json serialization format

* fix backend format

* Update webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/zarr/NgffMetadataV0_5.scala

* add comments about why manual json serializer is needed

* use chunk key encoding version 2 as zarr 2 stream also has no prefix of "c." when addressing an array

* do not include a prefixed "c." when parsing coordinates -> works with zarr 2 and 3 streaming

* remove outdated comment

* update error message (remove hint to include a prepending "c."

---------

Co-authored-by: Norman Rzepka <code@normanrz.com>

webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/zarr3/Zarr3ArrayHeader.scala
@@ -13,6 +13,7 @@ import com.scalableminds.webknossos.datastore.datareaders.{
   NullCompressor
 }
 import com.scalableminds.webknossos.datastore.helpers.JsonImplicits
+import com.scalableminds.webknossos.datastore.models.datasource.{AdditionalAxis, DataLayer}
 import net.liftweb.common.Box.tryo
 import net.liftweb.common.{Box, Full}
 import play.api.libs.json.{Format, JsArray, JsResult, JsString, JsSuccess, JsValue, Json, OFormat}
@@ -240,7 +241,7 @@ object Zarr3ArrayHeader extends JsonImplicits {
             ChunkGridConfiguration(Array(1, 1, 1))))), // Extension not supported for now
         "chunk_key_encoding" -> zarrArrayHeader.chunk_key_encoding,
         "fill_value" -> zarrArrayHeader.fill_value,
-        "attributes" -> Json.toJsFieldJsValueWrapper(zarrArrayHeader.attributes.getOrElse(Map("" -> ""))),
+        "attributes" -> Json.toJsFieldJsValueWrapper(zarrArrayHeader.attributes.getOrElse(Map.empty)),
         "codecs" -> zarrArrayHeader.codecs.map { codec: CodecConfiguration =>
           val configurationJson = if (codec.includeConfiguration) Json.obj("configuration" -> codec) else Json.obj()
           Json.obj("name" -> codec.name) ++ configurationJson
@@ -248,5 +249,41 @@ object Zarr3ArrayHeader extends JsonImplicits {
         "storage_transformers" -> zarrArrayHeader.storage_transformers,
         "dimension_names" -> zarrArrayHeader.dimension_names
       )
+
+  }
+  def fromDataLayer(dataLayer: DataLayer): Zarr3ArrayHeader = {
+    val additionalAxes = reorderAdditionalAxes(dataLayer.additionalAxes.getOrElse(Seq.empty))
+    Zarr3ArrayHeader(
+      zarr_format = 3,
+      node_type = "array",
+      // channel, additional axes, XYZ
+      shape = Array(1) ++ additionalAxes.map(_.highestValue).toArray ++ dataLayer.boundingBox.bottomRight.toArray,
+      data_type = Left(dataLayer.elementClass.toString),
+      chunk_grid = Left(
+        ChunkGridSpecification(
+          "regular",
+          ChunkGridConfiguration(
+            chunk_shape = Array.fill(1 + additionalAxes.length)(1) ++ Array(DataLayer.bucketLength,
+                                                                            DataLayer.bucketLength,
+                                                                            DataLayer.bucketLength))
+        )),
+      chunk_key_encoding =
+        ChunkKeyEncoding("v2", configuration = Some(ChunkKeyEncodingConfiguration(separator = Some(".")))),
+      fill_value = Right(0),
+      attributes = None,
+      codecs = Seq(
+        TransposeCodecConfiguration(TransposeSetting.fOrderFromRank(additionalAxes.length + 4)),
+        BytesCodecConfiguration(Some("little")),
+      ),
+      storage_transformers = None,
+      dimension_names = Some(Array("c") ++ additionalAxes.map(_.name).toArray ++ Seq("x", "y", "z"))
+    )
+  }
+  private def reorderAdditionalAxes(additionalAxes: Seq[AdditionalAxis]): Seq[AdditionalAxis] = {
+    val additionalAxesStartIndex = 1 // channel comes first
+    val sorted = additionalAxes.sortBy(_.index)
+    sorted.zipWithIndex.map {
+      case (axis, index) => axis.copy(index = index + additionalAxesStartIndex)
+    }
   }
 }

webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/tracings/volume/Zarr3BucketStreamSink.scala
@@ -39,39 +39,7 @@ class Zarr3BucketStreamSink(val layer: VolumeTracingLayer, tracingHasFallbackLay
   def apply(bucketStream: Iterator[(BucketPosition, Array[Byte])], mags: Seq[Vec3Int], voxelSize: Option[VoxelSize])(
       implicit ec: ExecutionContext): Iterator[NamedStream] = {
 
-    val header = Zarr3ArrayHeader(
-      zarr_format = 3,
-      node_type = "array",
-      // channel, additional axes, XYZ
-      shape = Array(1) ++ additionalAxesSorted.map(_.highestValue).toArray ++ layer.boundingBox.bottomRight.toArray,
-      data_type = Left(layer.elementClass.toString),
-      chunk_grid = Left(
-        ChunkGridSpecification(
-          "regular",
-          ChunkGridConfiguration(
-            chunk_shape = Array.fill(1 + additionalAxesSorted.length)(1) ++ Array(DataLayer.bucketLength,
-                                                                                  DataLayer.bucketLength,
-                                                                                  DataLayer.bucketLength))
-        )),
-      chunk_key_encoding =
-        ChunkKeyEncoding("default",
-                         configuration = Some(ChunkKeyEncodingConfiguration(separator = Some(dimensionSeparator)))),
-      fill_value = Right(0),
-      attributes = None,
-      codecs = Seq(
-        TransposeCodecConfiguration(TransposeSetting.fOrderFromRank(rank)),
-        BytesCodecConfiguration(Some("little")),
-        BloscCodecConfiguration(
-          BloscCompressor.defaultCname,
-          BloscCompressor.defaultCLevel,
-          StringCompressionSetting(BloscCodecConfiguration.shuffleSettingFromInt(BloscCompressor.defaultShuffle)),
-          Some(BloscCompressor.defaultTypesize),
-          BloscCompressor.defaultBlocksize
-        )
-      ),
-      storage_transformers = None,
-      dimension_names = Some(Array("c") ++ additionalAxesSorted.map(_.name).toArray ++ Seq("x", "y", "z"))
-    )
+    val header = Zarr3ArrayHeader.fromDataLayer(layer)
     bucketStream.flatMap {
       case (bucket, data) =>
         val skipBucket = if (tracingHasFallbackLayer) isAllZero(data) else isRevertedBucket(data)

conf/messages
@@ -138,9 +138,10 @@ dataLayer.mustBeSegmentation=DataLayer “{0}” is not a segmentation layer
 dataLayer.wrongMag=DataLayer “{0}” does not have mag “{1}”
 dataLayer.invalidMag=Supplied “{0}” is not a valid mag format. Please use “x-y-z”
 
-zarr.invalidChunkCoordinates=The requested chunk coordinates are in an invalid format. Expected c.x.y.z
+zarr.invalidChunkCoordinates=Invalid chunk coordinates. Expected dot separated coordinates like c.<additional_axes.>x.y.z
 zarr.invalidFirstChunkCoord="First Channel must be 0"
 zarr.chunkNotFound=Could not find the requested chunk
+zarr.notEnoughCoordinates=Invalid number of chunk coordinates. Expected to get at least 3 dimensions and channel 0.
 
 nml.file.uploadSuccess=Successfully uploaded file
 nml.file.notFound=Could not extract NML file

webknossos-datastore/app/com/scalableminds/webknossos/datastore/dataformats/layers/ZarrDataLayers.scala
@@ -4,14 +4,12 @@ import com.scalableminds.util.cache.AlfuCache
 import com.scalableminds.util.geometry.{BoundingBox, Vec3Int}
 import com.scalableminds.webknossos.datastore.dataformats.{DatasetArrayBucketProvider, MagLocator}
 import com.scalableminds.webknossos.datastore.models.datasource.LayerViewConfiguration.LayerViewConfiguration
-import com.scalableminds.webknossos.datastore.models.datasource._
+import com.scalableminds.webknossos.datastore.models.datasource.{DataFormat, _}
 import com.scalableminds.webknossos.datastore.storage.RemoteSourceDescriptorService
 import play.api.libs.json.{Json, OFormat}
 import ucar.ma2.{Array => MultiArray}
 
 trait ZarrLayer extends DataLayerWithMagLocators {
-
-  val dataFormat: DataFormat.Value = DataFormat.zarr
 
   def bucketProvider(remoteSourceDescriptorServiceOpt: Option[RemoteSourceDescriptorService],
                      dataSourceId: DataSourceId,
@@ -36,7 +34,8 @@ case class ZarrDataLayer(
     adminViewConfiguration: Option[LayerViewConfiguration] = None,
     coordinateTransformations: Option[List[CoordinateTransformation]] = None,
     override val numChannels: Option[Int] = Some(1),
-    override val additionalAxes: Option[Seq[AdditionalAxis]]
+    override val additionalAxes: Option[Seq[AdditionalAxis]],
+    override val dataFormat: DataFormat.Value,
 ) extends ZarrLayer
 
 object ZarrDataLayer {
@@ -54,7 +53,8 @@ case class ZarrSegmentationLayer(
     adminViewConfiguration: Option[LayerViewConfiguration] = None,
     coordinateTransformations: Option[List[CoordinateTransformation]] = None,
     override val numChannels: Option[Int] = Some(1),
-    additionalAxes: Option[Seq[AdditionalAxis]] = None
+    additionalAxes: Option[Seq[AdditionalAxis]] = None,
+    override val dataFormat: DataFormat.Value,
 ) extends SegmentationLayer
     with ZarrLayer
 

webknossos-datastore/app/com/scalableminds/webknossos/datastore/dataformats/zarr/ZarrCoordinatesParser.scala
@@ -1,15 +1,39 @@
 package com.scalableminds.webknossos.datastore.dataformats.zarr
 
-object ZarrCoordinatesParser {
-  def parseDotCoordinates(
-      cxyz: String,
-  ): Option[(Int, Int, Int, Int)] = {
-    val singleRx = "\\s*([0-9]+).([0-9]+).([0-9]+).([0-9]+)\\s*".r
+import com.scalableminds.util.tools.Fox
+import com.scalableminds.util.tools.Fox.{bool2Fox, option2Fox}
+import com.scalableminds.webknossos.datastore.models.AdditionalCoordinate
+import play.api.http.Status.NOT_FOUND
+import play.api.i18n.{Messages, MessagesProvider}
 
-    cxyz match {
-      case singleRx(c, x, y, z) =>
-        Some(Integer.parseInt(c), Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(z))
-      case _ => None
-    }
+import scala.concurrent.ExecutionContext
+
+object ZarrCoordinatesParser {
+
+  def parseNDimensionalDotCoordinates(
+      coordinates: String,
+  )(implicit ec: ExecutionContext, m: MessagesProvider): Fox[(Int, Int, Int, Option[List[AdditionalCoordinate]])] = {
+    val ndCoordinatesRx = "^\\s*([0-9]+)\\.([0-9]+)\\.([0-9]+)(\\.([0-9]+))+\\s*$".r
+
+    for {
+      parsedCoordinates <- ndCoordinatesRx
+        .findFirstIn(coordinates)
+        .map(m => m.split('.').map(coord => Integer.parseInt(coord))) ?~>
+        Messages("zarr.invalidChunkCoordinates") ~> NOT_FOUND
+      channelCoordinate <- parsedCoordinates.headOption ~> NOT_FOUND
+      _ <- bool2Fox(channelCoordinate == 0) ?~> "zarr.invalidFirstChunkCoord" ~> NOT_FOUND
+      _ <- bool2Fox(parsedCoordinates.length >= 4) ?~> "zarr.notEnoughCoordinates" ~> NOT_FOUND
+      (x, y, z) = (parsedCoordinates(parsedCoordinates.length - 3),
+                   parsedCoordinates(parsedCoordinates.length - 2),
+                   parsedCoordinates(parsedCoordinates.length - 1))
+      additionalCoordinates = if (parsedCoordinates.length > 4)
+        Some(
+          parsedCoordinates
+            .slice(1, parsedCoordinates.length - 3)
+            .zipWithIndex
+            .map(coordWithIndex => new AdditionalCoordinate(name = s"t${coordWithIndex._2}", value = coordWithIndex._1))
+            .toList)
+      else None
+    } yield (x, y, z, additionalCoordinates)
   }
 }

webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/zarr/NgffMetadata.scala
@@ -1,44 +1,13 @@
-package com.scalableminds.webknossos.datastore.datareaders.zarr;
+package com.scalableminds.webknossos.datastore.datareaders.zarr
 
 import com.scalableminds.util.geometry.{Vec3Double, Vec3Int}
-import com.scalableminds.webknossos.datastore.models
-import com.scalableminds.webknossos.datastore.models.{LengthUnit, VoxelSize}
-import net.liftweb.common.{Box, Failure, Full}
+import com.scalableminds.webknossos.datastore.models.VoxelSize
 import play.api.libs.json.{Json, OFormat}
-
-case class NgffCoordinateTransformation(`type`: String = "scale", scale: Option[List[Double]])
-
-object NgffCoordinateTransformation {
-  implicit val jsonFormat: OFormat[NgffCoordinateTransformation] = Json.format[NgffCoordinateTransformation]
-}
-
-case class NgffDataset(path: String, coordinateTransformations: List[NgffCoordinateTransformation])
-
-object NgffDataset {
-  implicit val jsonFormat: OFormat[NgffDataset] = Json.format[NgffDataset]
-}
 
 case class NgffGroupHeader(zarr_format: Int)
 object NgffGroupHeader {
   implicit val jsonFormat: OFormat[NgffGroupHeader] = Json.format[NgffGroupHeader]
   val FILENAME_DOT_ZGROUP = ".zgroup"
-}
-
-case class NgffAxis(name: String, `type`: String, unit: Option[String] = None) {
-
-  def lengthUnit: Box[models.LengthUnit.Value] =
-    if (`type` != "space")
-      Failure(f"Could not convert NGFF unit $name of type ${`type`} to LengthUnit")
-    else {
-      unit match {
-        case None | Some("") => Full(VoxelSize.DEFAULT_UNIT)
-        case Some(someUnit)  => LengthUnit.fromString(someUnit)
-      }
-    }
-}
-
-object NgffAxis {
-  implicit val jsonFormat: OFormat[NgffAxis] = Json.format[NgffAxis]
 }
 
 case class NgffMultiscalesItem(
@@ -91,23 +60,4 @@ case class NgffLabelsGroup(labels: List[String])
 object NgffLabelsGroup {
   implicit val jsonFormat: OFormat[NgffLabelsGroup] = Json.format[NgffLabelsGroup]
   val LABEL_PATH = "labels/.zattrs"
-}
-
-case class NgffOmeroMetadata(channels: List[NgffChannelAttributes])
-object NgffOmeroMetadata {
-  implicit val jsonFormat: OFormat[NgffOmeroMetadata] = Json.format[NgffOmeroMetadata]
-}
-
-case class NgffChannelWindow(min: Double, max: Double, start: Double, end: Double)
-object NgffChannelWindow {
-  implicit val jsonFormat: OFormat[NgffChannelWindow] = Json.format[NgffChannelWindow]
-}
-
-case class NgffChannelAttributes(color: Option[String],
-                                 label: Option[String],
-                                 window: Option[NgffChannelWindow],
-                                 inverted: Option[Boolean],
-                                 active: Option[Boolean])
-object NgffChannelAttributes {
-  implicit val jsonFormat: OFormat[NgffChannelAttributes] = Json.format[NgffChannelAttributes]
 }

webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/zarr/NgffMetadataV0_5.scala
@@ -1,0 +1,58 @@
+package com.scalableminds.webknossos.datastore.datareaders.zarr
+
+import com.scalableminds.util.geometry.{Vec3Double, Vec3Int}
+import com.scalableminds.webknossos.datastore.models.VoxelSize
+import com.scalableminds.webknossos.datastore.models.datasource.AdditionalAxis
+import play.api.libs.json.{Json, OFormat}
+
+// See suggested changes to version v0.5 here together with an example: https://ngff.openmicroscopy.org/rfc/2/index.html#examples
+case class NgffMultiscalesItemV0_5(
+    // Ngff V0.5 no longer has the version inside the multiscale field.
+    name: Option[String],
+    axes: List[NgffAxis] = List(
+      NgffAxis(name = "c", `type` = "channel"),
+      NgffAxis(name = "x", `type` = "space", unit = Some("nanometer")),
+      NgffAxis(name = "y", `type` = "space", unit = Some("nanometer")),
+      NgffAxis(name = "z", `type` = "space", unit = Some("nanometer")),
+    ),
+    datasets: List[NgffDataset]
+)
+
+object NgffMultiscalesItemV0_5 {
+  implicit val jsonFormat: OFormat[NgffMultiscalesItemV0_5] = Json.format[NgffMultiscalesItemV0_5]
+}
+
+case class NgffMetadataV0_5(version: String,
+                            multiscales: List[NgffMultiscalesItemV0_5],
+                            omero: Option[NgffOmeroMetadata])
+
+object NgffMetadataV0_5 {
+  def fromNameVoxelSizeAndMags(dataLayerName: String,
+                               dataSourceVoxelSize: VoxelSize,
+                               mags: List[Vec3Int],
+                               additionalAxes: Option[Seq[AdditionalAxis]],
+                               version: String = "0.5"): NgffMetadataV0_5 = {
+    val datasets = mags.map(
+      mag =>
+        NgffDataset(
+          path = mag.toMagLiteral(allowScalar = true),
+          List(NgffCoordinateTransformation(
+            scale = Some(List[Double](1.0) ++ (dataSourceVoxelSize.factor * Vec3Double(mag)).toList)))
+      ))
+    val lengthUnitStr = dataSourceVoxelSize.unit.toString
+    val axes = List(NgffAxis(name = "c", `type` = "channel")) ++ additionalAxes
+      .getOrElse(List.empty)
+      .zipWithIndex
+      .map(axisAndIndex => NgffAxis(name = s"t${axisAndIndex._2}", `type` = "space", unit = Some(lengthUnitStr))) ++ List(
+      NgffAxis(name = "x", `type` = "space", unit = Some(lengthUnitStr)),
+      NgffAxis(name = "y", `type` = "space", unit = Some(lengthUnitStr)),
+      NgffAxis(name = "z", `type` = "space", unit = Some(lengthUnitStr)),
+    )
+    NgffMetadataV0_5(version,
+                     multiscales =
+                       List(NgffMultiscalesItemV0_5(name = Some(dataLayerName), datasets = datasets, axes = axes)),
+                     None)
+  }
+
+  implicit val jsonFormat: OFormat[NgffMetadataV0_5] = Json.format[NgffMetadataV0_5]
+}

commit ab6be1e30b785ab70420bf3cf21fad6605006043

Fix format in volume annotation download zarr.json (#8800)
Fixes a regression introduced in #7995 – the volume buckets are blosc
compressed, but that was no longer mentioned in the zarr header.

Also, the chunks had the `c.` name prefix, but `chunk_key_encoding` was
already `v2` in the zarr header. Now I actually remove the `c.` name
prefix so that it is actually `v2` compliant.

Also, fixes passing the voxel size to the volume zip download so that it
is correct in the included datasource-properties.json

### Steps to test:
- Create volume annotation, download it in zarr format
- Unpack the zip and the contained data_Volume.zip
- place the result in the binaryData folder, should be readable as
standalone dataset
- reupload the downloaded annotation, should still be uploadable as an
annotation
- Also, upload volume annotation downloaded on the *master* code, should
also still be uploadable as annotation

### Issues:
- fixes #7961 

------
- [x] Added changelog entry (create a `$PR_NUMBER.md` file in
`unreleased_changes` or use `./tools/create-changelog-entry.py`)
- [x] Considered [common edge
cases](../blob/master/.github/common_edge_cases.md)

---------

Co-authored-by: MichaelBuessemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>

webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/zarr3/Zarr3ArrayHeader.scala
@@ -258,7 +258,9 @@ object Zarr3ArrayHeader extends JsonImplicits {
       )
 
   }
-  def fromDataLayer(dataLayer: DataLayer, mag: Vec3Int): Zarr3ArrayHeader = {
+  def fromDataLayer(dataLayer: DataLayer,
+                    mag: Vec3Int,
+                    additionalCodecs: Seq[CodecConfiguration] = Seq.empty): Zarr3ArrayHeader = {
     val additionalAxes = reorderAdditionalAxes(dataLayer.additionalAxes.getOrElse(Seq.empty))
     val xyzBBounds = Array(
       // Zarr can't handle data sets that don't start at 0, so we extend the shape to include "true" coords
@@ -287,7 +289,7 @@ object Zarr3ArrayHeader extends JsonImplicits {
       codecs = Seq(
         TransposeCodecConfiguration(TransposeSetting.fOrderFromRank(additionalAxes.length + 4)),
         BytesCodecConfiguration(Some("little")),
-      ),
+      ) ++ additionalCodecs,
       storage_transformers = None,
       dimension_names = Some(Array("c") ++ additionalAxes.map(_.name).toArray ++ Seq("x", "y", "z"))
     )

webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/tracings/volume/VolumeDataZipHelper.scala
@@ -95,10 +95,12 @@ trait VolumeDataZipHelper extends WKWDataFormatHelper with ReversionHelper with 
     val additionalAxesNames: Seq[String] = dimensionNames.toSeq.drop(1).dropRight(3) // drop channel left, and xyz right
 
     // assume additionalAxes,x,y,z
-    val chunkPathRegex = s"(|.*/)(\\d+|\\d+-\\d+-\\d+)/c\\.(.+)".r
+    // the c. at the beginning of chunk names is optional
+    // (used in "default" chunk encoding, which was used for volume downloads previously)
+    val chunkPathRegex = s"(|.*/)(\\d+|\\d+-\\d+-\\d+)/(c\\.)?(.+)".r
 
     path match {
-      case chunkPathRegex(_, magStr, dimsStr) =>
+      case chunkPathRegex(_, magStr, _, dimsStr) =>
         val dims: Seq[String] = dimsStr.split("\\.").toSeq
         val additionalAxesDims = dims.drop(1).dropRight(3) // drop channel left, and xyz right
         val additionalCoordinates: Seq[AdditionalCoordinate] = additionalAxesNames.zip(additionalAxesDims).map {

webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/tracings/volume/Zarr3BucketStreamSink.scala
@@ -41,7 +41,9 @@ class Zarr3BucketStreamSink(val layer: VolumeTracingLayer, tracingHasFallbackLay
   def apply(bucketStream: Iterator[(BucketPosition, Array[Byte])], mags: Seq[Vec3Int], voxelSize: Option[VoxelSize])(
       implicit ec: ExecutionContext): Iterator[NamedStream] = {
 
-    val header = Zarr3ArrayHeader.fromDataLayer(layer, mags.headOption.getOrElse(Vec3Int.ones))
+    val header = Zarr3ArrayHeader.fromDataLayer(layer,
+                                                mags.headOption.getOrElse(Vec3Int.ones),
+                                                additionalCodecs = Seq(compressorConfiguration))
     bucketStream.flatMap {
       case (bucket, data) =>
         val skipBucket = if (tracingHasFallbackLayer) isAllZero(data) else isRevertedElement(data)
@@ -100,7 +102,7 @@ class Zarr3BucketStreamSink(val layer: VolumeTracingLayer, tracingHasFallbackLay
     val additionalCoordinatesPart =
       additionalCoordinatesFilePath(bucketPosition.additionalCoordinates, additionalAxesSorted)
     val channelPart = 0
-    s"$layerName/${bucketPosition.mag.toMagLiteral(allowScalar = true)}/c$dimensionSeparator$channelPart$dimensionSeparator$additionalCoordinatesPart${bucketPosition.bucketX}$dimensionSeparator${bucketPosition.bucketY}$dimensionSeparator${bucketPosition.bucketZ}"
+    s"$layerName/${bucketPosition.mag.toMagLiteral(allowScalar = true)}/$channelPart$dimensionSeparator$additionalCoordinatesPart${bucketPosition.bucketX}$dimensionSeparator${bucketPosition.bucketY}$dimensionSeparator${bucketPosition.bucketZ}"
   }
 
   private def additionalCoordinatesFilePath(additionalCoordinatesOpt: Option[Seq[AdditionalCoordinate]],
@@ -115,6 +117,15 @@ class Zarr3BucketStreamSink(val layer: VolumeTracingLayer, tracingHasFallbackLay
 
   private def zarrHeaderFilePath(layerName: String, mag: Vec3Int): String =
     s"$layerName/${mag.toMagLiteral(allowScalar = true)}/${Zarr3ArrayHeader.FILENAME_ZARR_JSON}"
+
+  private lazy val compressorConfiguration =
+    BloscCodecConfiguration(
+      BloscCompressor.defaultCname,
+      BloscCompressor.defaultCLevel,
+      StringCompressionSetting(BloscCodecConfiguration.shuffleSettingFromInt(BloscCompressor.defaultShuffle)),
+      Some(BloscCompressor.defaultTypesize),
+      BloscCompressor.defaultBlocksize
+    )
 
   private lazy val compressor =
     new BloscCompressor(

webknossos-tracingstore/conf/tracingstore.latest.routes
@@ -18,7 +18,7 @@ POST        /volume/save                                                        
 POST        /volume/:tracingId/initialData                                          @com.scalableminds.webknossos.tracingstore.controllers.VolumeTracingController.initialData(annotationId: ObjectId, tracingId: String, minMag: Option[Int], maxMag: Option[Int])
 POST        /volume/:tracingId/initialDataMultiple                                  @com.scalableminds.webknossos.tracingstore.controllers.VolumeTracingController.initialDataMultiple(annotationId: ObjectId, tracingId: String)
 GET         /volume/:tracingId                                                      @com.scalableminds.webknossos.tracingstore.controllers.VolumeTracingController.get(tracingId: String, annotationId: ObjectId, version: Option[Long])
-GET         /volume/:tracingId/allDataZip                                           @com.scalableminds.webknossos.tracingstore.controllers.VolumeTracingController.allDataZip(tracingId: String, annotationId: Option[ObjectId], version: Option[Long], volumeDataZipFormat: String, voxelSize: Option[String], voxelSizeUnit: Option[String])
+GET         /volume/:tracingId/allDataZip                                           @com.scalableminds.webknossos.tracingstore.controllers.VolumeTracingController.allDataZip(tracingId: String, annotationId: Option[ObjectId], version: Option[Long], volumeDataZipFormat: String, voxelSizeFactor: Option[String], voxelSizeUnit: Option[String])
 POST        /volume/:tracingId/data                                                 @com.scalableminds.webknossos.tracingstore.controllers.VolumeTracingController.data(tracingId: String, annotationId: ObjectId)
 POST        /volume/:tracingId/adHocMesh                                            @com.scalableminds.webknossos.tracingstore.controllers.VolumeTracingController.requestAdHocMesh(tracingId: String)
 POST        /volume/:tracingId/fullMesh.stl                                         @com.scalableminds.webknossos.tracingstore.controllers.VolumeTracingController.loadFullMeshStl(tracingId: String)
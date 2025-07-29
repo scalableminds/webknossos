624affed03a1267d13393dab9be4b79e9f2db048
Charlie Meister <charlie.meister@student.hpi.de>
2025-07-29 11:20:28 +0200

    improve typing of middleware

--- frontend/javascripts/viewer/model/helpers/event_emitter_middleware.ts
+++ frontend/javascripts/viewer/model/helpers/event_emitter_middleware.ts
@@ -1,7 +1,11 @@
-import type { Middleware } from "redux";
+import type { Dispatch } from "redux";
+import type { Action } from "viewer/model/actions/actions";
 import { eventBus } from "./event_bus";
 
-export const eventEmitterMiddleware: Middleware = () => (next) => (action) => {
-  eventBus.emit(action.type, action);
-  return next(action);
-};
+export const eventEmitterMiddleware =
+  <A extends Action>() =>
+  (next: Dispatch<A>) =>
+  (action: A) => {
+    eventBus.emit(action.type, action);
+    return next(action);
+  };

812d35162a5fe3c38483061b49014efca63ec7c5
Michael Büßemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>
2025-08-04 12:33:03 +0200

    fix setting uniform after iterating over all layers

--- frontend/javascripts/viewer/geometries/materials/plane_material_factory.ts
+++ frontend/javascripts/viewer/geometries/materials/plane_material_factory.ts
@@ -874,10 +874,10 @@ class PlaneMaterialFactory {
             if (hasTransform) {
               countOfLayersWithTransforms++;
             }
-            this.uniforms.doAllLayersHaveTransforms = {
-              value: countOfLayersWithTransforms === layers.length,
-            };
           }
+          this.uniforms.doAllLayersHaveTransforms = {
+            value: countOfLayersWithTransforms === layers.length,
+          };
           this.recomputeShaders();
         },
         true,

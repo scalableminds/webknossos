c6c6e58878f03cdb9bb0b285644e8f2a44e3d33d
Florian M <florian@scm.io>
2025-08-12 13:13:47 +0200

    bump schema version

--- conf/evolutions/139-allows-manual-upload.sql
+++ conf/evolutions/139-allows-manual-upload.sql
@@ -1,11 +1,11 @@
 START TRANSACTION;
 
-do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 137, 'Previous schema version mismatch'; end; $$ LANGUAGE plpgsql;
+do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 138, 'Previous schema version mismatch'; end; $$ LANGUAGE plpgsql;
 
 DROP VIEW webknossos.dataStores_;
 ALTER TABLE webknossos.dataStores ADD COLUMN allowsManualUpload BOOLEAN NOT NULL DEFAULT TRUE;
 CREATE VIEW webknossos.dataStores_ AS SELECT * FROM webknossos.dataStores WHERE NOT isDeleted;
 
-UPDATE webknossos.releaseInformation SET schemaVersion = 138;
+UPDATE webknossos.releaseInformation SET schemaVersion = 139;
 
 COMMIT TRANSACTION;
--- conf/evolutions/reversions/139-allows-manual-upload.sql
+++ conf/evolutions/reversions/139-allows-manual-upload.sql
@@ -1,11 +1,11 @@
 START TRANSACTION;
 
-do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 138, 'Previous schema version mismatch'; end; $$ LANGUAGE plpgsql;
+do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 139, 'Previous schema version mismatch'; end; $$ LANGUAGE plpgsql;
 
 DROP VIEW webknossos.dataStores_;
 ALTER TABLE webknossos.dataStores DROP COLUMN allowsManualUpload;
 CREATE VIEW webknossos.dataStores_ AS SELECT * FROM webknossos.dataStores WHERE NOT isDeleted;
 
-UPDATE webknossos.releaseInformation SET schemaVersion = 137;
+UPDATE webknossos.releaseInformation SET schemaVersion = 138;
 
 COMMIT TRANSACTION;

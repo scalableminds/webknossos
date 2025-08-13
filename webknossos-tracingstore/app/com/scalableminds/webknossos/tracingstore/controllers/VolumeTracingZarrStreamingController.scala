afd2c43e0f2346d11b352dd4699a8ccd17f51f64
frcroth <frcroth@gmail.com>
2025-08-13 11:15:45 +0200

    Return 500 on missing buckets in fallback layer

--- webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/controllers/VolumeTracingZarrStreamingController.scala
+++ webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/controllers/VolumeTracingZarrStreamingController.scala
@@ -352,7 +352,7 @@ class VolumeTracingZarrStreamingController @Inject()(
         (fallbackData, fallbackMissingBucketIndices) <- remoteDataStoreClient.getData(
           remoteFallbackLayer,
           List(request)) ~> SERVICE_UNAVAILABLE // return 503 if the request fails, assuming the datastore is still initializing
-        _ <- Fox.fromBool(fallbackMissingBucketIndices.isEmpty) ?~> "No data at coordinations in fallback layer" ~> NOT_FOUND // return 404 if the request worked but data is empty
+        _ <- Fox.fromBool(fallbackMissingBucketIndices.isEmpty) ?~> "Could not retrieve data from fallback layer" ~> INTERNAL_SERVER_ERROR
       } yield fallbackData
     } else Fox.successful(data)
 }

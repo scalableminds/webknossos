822bae040cbbdae8f148a7e06d2cf60f35fe3f46
Florian M <florian@scm.io>
2025-09-08 11:58:06 +0200

    lint

--- app/models/dataset/DatasetService.scala
+++ app/models/dataset/DatasetService.scala
@@ -489,6 +489,8 @@ class DatasetService @Inject()(organizationDAO: OrganizationDAO,
       dataset.flatMap {
         case Full(dataset) if !dataset.isVirtual =>
           datasetMagsDAO.updateMagPathsForDataset(dataset._id, pathInfo.magPathInfos)
+        case Full(_) => // Dataset is virtual, no updates from datastore are accepted.
+          Fox.successful()
         case Empty => // Dataset reported but ignored (non-existing/forbidden org)
           Fox.successful(())
         case e: EmptyBox =>

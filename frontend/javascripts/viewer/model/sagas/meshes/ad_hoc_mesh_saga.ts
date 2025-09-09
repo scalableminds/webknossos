f70f968e09e1cded2a40eb4f5f341b9f5b5e1fc9
Florian M <florian@scm.io>
2025-09-09 10:18:35 +0200

    Fix loading adHocMeshes from dataset view mode

--- frontend/javascripts/viewer/model/sagas/meshes/ad_hoc_mesh_saga.ts
+++ frontend/javascripts/viewer/model/sagas/meshes/ad_hoc_mesh_saga.ts
@@ -442,10 +442,9 @@ function* maybeLoadMeshChunk(
   threeDMap.set(clippedPosition, true);
   const scaleFactor = yield* select((state) => state.dataset.dataSource.scale.factor);
   const dataStoreHost = yield* select((state) => state.dataset.dataStore.url);
-  const owningOrganization = yield* select((state) => state.dataset.owningOrganization);
-  const datasetDirectoryName = yield* select((state) => state.dataset.directoryName);
+  const datasetId = yield* select((state) => state.dataset.id);
   const tracingStoreHost = yield* select((state) => state.annotation.tracingStore.url);
-  const dataStoreUrl = `${dataStoreHost}/data/datasets/${owningOrganization}/${datasetDirectoryName}/layers/${
+  const dataStoreUrl = `${dataStoreHost}/data/datasets/${datasetId}/layers/${
     layer.fallbackLayer != null ? layer.fallbackLayer : layer.name
   }`;
   const tracingStoreUrl = `${tracingStoreHost}/tracings/volume/${layer.name}`;

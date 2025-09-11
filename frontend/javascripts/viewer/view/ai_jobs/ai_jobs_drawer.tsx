b3025e2249206ddcd5f7fa2d527de375c3d77a64
Tom Herold <tom.herold@scalableminds.com>
2025-09-11 14:43:56 +0200

    correctly connect tabs

--- frontend/javascripts/viewer/view/ai_jobs/ai_jobs_drawer.tsx
+++ frontend/javascripts/viewer/view/ai_jobs/ai_jobs_drawer.tsx
@@ -6,10 +6,18 @@ import { setAIJobDrawerStateAction } from "viewer/model/actions/ui_actions";
 import { AiImageAlignmentJob } from "./alignment/ai_image_alignment_job";
 import { AiImageSegmentationJob } from "./run_ai_model/ai_image_segmentation_job";
 import { AiModelTrainingJob } from "./train_ai_model/ai_training_job";
+import type { StartAiJobDrawerState } from "../action-bar/ai_job_modals/constants";
 
 export const AiJobsDrawer = ({ isOpen }: { isOpen: boolean }) => {
   const dispatch = useDispatch();
   const ai_job_drawer_state = useWkSelector((state) => state.uiInformation.aIJobDrawerState);
+
+  const handleChange = useCallback(
+    (key: string) => {
+      dispatch(setAIJobDrawerStateAction(key as StartAiJobDrawerState));
+    },
+    [dispatch],
+  );
 
   const handleClose = useCallback(() => {
     dispatch(setAIJobDrawerStateAction("invisible"));
@@ -35,7 +43,7 @@ export const AiJobsDrawer = ({ isOpen }: { isOpen: boolean }) => {
 
   return (
     <Drawer title="AI Jobs" placement="right" width={1200} open={isOpen} onClose={handleClose}>
-      <Tabs defaultActiveKey={ai_job_drawer_state} items={items} />
+      <Tabs activeKey={ai_job_drawer_state} items={items} onChange={handleChange} />
     </Drawer>
   );
 };

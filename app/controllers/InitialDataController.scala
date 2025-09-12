0b2b6fcf4111f40b1fb0ee8960ff87bea3b21971
Michael Büßemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>
2025-09-12 15:40:55 +0200

    undo testing change

--- app/controllers/InitialDataController.scala
+++ app/controllers/InitialDataController.scala
@@ -143,11 +143,7 @@ Samplecountry
       "This is a wonderful dummy publication, it has authors, it has a link, it has a doi number, those could go here.\nLorem [ipsum](https://github.com/scalableminds/webknossos) dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.")
   )
   private val defaultDataStore =
-    DataStore(conf.Datastore.name,
-              conf.Http.uri,
-              conf.Datastore.publicUri.getOrElse(conf.Http.uri),
-              conf.Datastore.key,
-              reportUsedStorageEnabled = true) // TODOM: Undo this
+    DataStore(conf.Datastore.name, conf.Http.uri, conf.Datastore.publicUri.getOrElse(conf.Http.uri), conf.Datastore.key)
   private val defaultAiModel = AiModel(
     ObjectId("66544a56d20000af0e42ba0f"),
     defaultOrganization._id,

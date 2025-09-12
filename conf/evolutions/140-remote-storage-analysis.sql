6e2350bae265510fe6c0b11bb0ef075041af2faa
Michael Büßemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>
2025-09-12 15:31:35 +0200

    fix evolution & reversion

---
+++ conf/evolutions/140-remote-storage-analysis.sql
@@ -0,0 +1,40 @@
+START TRANSACTION;
+
+do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 139, 'Previous schema version mismatch'; end; $$ LANGUAGE plpgsql;
+
+DROP TABLE IF EXISTS webknossos.organization_usedStorage;
+
+CREATE TABLE webknossos.organization_usedStorage_mags (
+    _dataset         TEXT CONSTRAINT _dataset_objectId
+                     CHECK (_dataset ~ '^[0-9a-f]{24}$') NOT NULL,
+    layerName        TEXT        NOT NULL,
+    mag              TEXT        NOT NULL, -- Could also go for webknossos.VECTOR3, but would make things maybe a little more complicated
+    path             TEXT        NOT NULL,
+  _organization TEXT NOT NULL,
+    usedStorageBytes BIGINT      NOT NULL,
+    lastUpdated      TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (_dataset, layerName, mag)
+);
+
+CREATE TABLE webknossos.organization_usedStorage_attachments (
+    _dataset           TEXT CONSTRAINT _dataset_objectId
+                       CHECK (_dataset ~ '^[0-9a-f]{24}$') NOT NULL,
+  layerName TEXT NOT NULL,
+    name               TEXT                                NOT NULL,
+    path               TEXT                                NOT NULL,
+    type               webknossos.LAYER_ATTACHMENT_TYPE    NOT NULL,
+    _organization      TEXT                                NOT NULL,
+  usedStorageBytes BIGINT NOT NULL,
+    lastUpdated        TIMESTAMPTZ                         NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (_dataset, layerName, name, type)
+);
+
+-- Reset all storage scan timestamps to fill the new webknossos.organization_usedStorage table
+UPDATE webknossos.organizations
+SET lastStorageScanTime = '1970-01-01T00:00:00.000Z'
+WHERE _id IS NULL;
+
+
+UPDATE webknossos.releaseInformation SET schemaVersion = 140;
+
+COMMIT TRANSACTION;
---
+++ conf/evolutions/reversions/140-remote-storage-analysis.sql
@@ -0,0 +1,29 @@
+START TRANSACTION;
+
+do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 140, 'Previous schema version mismatch'; end; $$ LANGUAGE plpgsql;
+
+-- 1. Drop the new organization_usedStorage tables
+DROP TABLE IF EXISTS webknossos.organization_usedStorage_mags;
+DROP TABLE IF EXISTS webknossos.organization_usedStorage_attachments;
+
+-- 2. Recreate the old organization_usedStorage
+CREATE TABLE webknossos.organization_usedStorage(
+  _organization TEXT NOT NULL,
+  _dataStore TEXT NOT NULL,
+  _dataset TEXT CONSTRAINT _dataset_objectId CHECK (_dataset ~ '^[0-9a-f]{24}$') NOT NULL,
+  layerName TEXT NOT NULL,
+  magOrDirectoryName TEXT NOT NULL,
+  usedStorageBytes BIGINT NOT NULL,
+  lastUpdated TIMESTAMPTZ,
+  PRIMARY KEY(_organization, _dataStore, _dataset, layerName, magOrDirectoryName)
+);
+
+-- 3. Reset all storage scan timestamps to fill the new webknossos.organization_usedStorage table
+UPDATE webknossos.organizations
+SET lastStorageScanTime = '1970-01-01T00:00:00.000Z'
+WHERE _id IS NULL;
+
+-- 4. Revert schema version
+UPDATE webknossos.releaseInformation SET schemaVersion = 139;
+
+COMMIT TRANSACTION;

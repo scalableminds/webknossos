26d73282918c0886f7973282fa3ed90ff1e1ae4e
Florian M <florian@scm.io>
2025-09-15 13:55:43 +0200

    Revert "bump schema version to 141"
    This reverts commit b721d24a17be7fe9ef00c4dc6f09eeb15e533480.

--- conf/evolutions/reversions/140-annotation-layer-name-check-deferrable.sql
+++ conf/evolutions/reversions/140-annotation-layer-name-check-deferrable.sql
@@ -1,10 +1,10 @@
 START TRANSACTION;
 
-do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 141, 'Previous schema version mismatch'; end; $$ LANGUAGE plpgsql;
+do $$ begin ASSERT (select schemaVersion from webknossos.releaseInformation) = 140, 'Previous schema version mismatch'; end; $$ LANGUAGE plpgsql;
 
 ALTER TABLE webknossos.annotation_layers DROP CONSTRAINT annotation_layers_name__annotation_key;
 ALTER TABLE webknossos.annotation_layers ADD CONSTRAINT annotation_layers_name__annotation_key UNIQUE (name, _annotation);
 
-UPDATE webknossos.releaseInformation SET schemaVersion = 140;
+UPDATE webknossos.releaseInformation SET schemaVersion = 139;
 
 COMMIT TRANSACTION;

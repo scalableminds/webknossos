6ac57c60660f9126d5bd6e5821fef1e9c836e926
Florian M <florian@scm.io>
2025-09-16 15:44:44 +0200

    WIP calculate surface area of segments

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/controllers/DSMeshController.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/controllers/DSMeshController.scala
@@ -14,6 +14,7 @@ import com.scalableminds.webknossos.datastore.services.mesh.{
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, PlayBodyParsers}
 
+import java.nio.{ByteBuffer, ByteOrder}
 import scala.concurrent.ExecutionContext
 
 class DSMeshController @Inject()(
@@ -95,4 +96,52 @@ class DSMeshController @Inject()(
         } yield Ok(data)
       }
     }
+
+  def fullMeshSurfaceArea(datasetId: ObjectId, dataLayerName: String): Action[FullMeshRequest] =
+    Action.async(validateJson[FullMeshRequest]) { implicit request =>
+      accessTokenService.validateAccessFromTokenContext(UserAccessRequest.readDataset(datasetId)) {
+        for {
+          (dataSource, dataLayer) <- datasetCache.getWithLayer(datasetId, dataLayerName) ~> NOT_FOUND
+          data: Array[Byte] <- fullMeshService.loadFor(dataSource, dataLayer, request.body) ?~> "mesh.file.loadChunk.failed"
+          dataBuffer = ByteBuffer.wrap(data)
+          _ = dataBuffer.order(ByteOrder.LITTLE_ENDIAN)
+          numberOfTriangles = dataBuffer.getInt(80)
+          surface = surfaceFromStlBuffer(dataBuffer, numberOfTriangles)
+        } yield Ok(s"$numberOfTriangles triangles, surface $surface")
+      }
+    }
+
+  private def surfaceFromStlBuffer(dataBuffer: ByteBuffer, numberOfTriangles: Int) = {
+    val normalOffset = 12
+    var surfaceSum = 0.0f
+    val headerOffset = 84
+    for (triangleIndex <- 0 until numberOfTriangles) {
+      val v1x = dataBuffer.getFloat(headerOffset + triangleIndex + normalOffset)
+      val v1y = dataBuffer.getFloat(headerOffset + triangleIndex + normalOffset + 4 * 0)
+      val v1z = dataBuffer.getFloat(headerOffset + triangleIndex + normalOffset + 4 * 1)
+      val v2x = dataBuffer.getFloat(headerOffset + triangleIndex + normalOffset + 4 * 2)
+      val v2y = dataBuffer.getFloat(headerOffset + triangleIndex + normalOffset + 4 * 3)
+      val v2z = dataBuffer.getFloat(headerOffset + triangleIndex + normalOffset + 4 * 4)
+      val v3x = dataBuffer.getFloat(headerOffset + triangleIndex + normalOffset + 4 * 5)
+      val v3y = dataBuffer.getFloat(headerOffset + triangleIndex + normalOffset + 4 * 6)
+      val v3z = dataBuffer.getFloat(headerOffset + triangleIndex + normalOffset + 4 * 7)
+
+      val vec1x = v2x - v1x
+      val vec1y = v2y - v1y
+      val vec1z = v2z - v1z
+      val vec2x = v3x - v1x
+      val vec2y = v3y - v1y
+      val vec2z = v3z - v1z
+
+      val crossx = vec1y * vec2z - vec1z * vec2y
+      val crossy = vec1z * vec2x - vec1x * vec2z
+      val crossz = vec1x * vec2y - vec1y * vec2x
+
+      val magnitude = Math.sqrt(crossx * crossx + crossy * crossy + crossz * crossz).toFloat
+
+      surfaceSum = surfaceSum + (magnitude / 2.0f)
+    }
+    surfaceSum
+  }
+
 }

34d724889b0c449a4ea57f0220c92a08936f88bc
Florian M <florian@scm.io>
2025-10-15 09:44:19 +0200

    add comment explaining about TSChunkCacheService

--- webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/TSChunkCacheService.scala
+++ webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/TSChunkCacheService.scala
@@ -3,6 +3,11 @@ package com.scalableminds.webknossos.tracingstore
 import com.scalableminds.webknossos.datastore.services.ChunkCacheService
 import jakarta.inject.Inject
 
+/* Currently the only usage of this cache is in editablemapping upload. There, the full arrays are read, so
+ * the cache is not really useful and just added to satisfy the DatasetArray parameters. Because of that it is
+ * configured to be pretty small to not waste RAM. When more usages are added, remove this comment and increase the
+ * size in application.conf
+ */
 class TSChunkCacheService @Inject()(config: TracingStoreConfig) extends ChunkCacheService {
   protected val maxSizeBytes: Long = config.Tracingstore.Cache.chunkCacheMaxSizeBytes
 }

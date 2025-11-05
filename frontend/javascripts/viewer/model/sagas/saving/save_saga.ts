9044a3ac1ddb7d1bf1cd9b5a4a66e3ebd5c8cd96
Michael Büßemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>
2025-11-05 18:52:26 +0100

    move functions

--- frontend/javascripts/viewer/model/sagas/saving/save_saga.ts
+++ frontend/javascripts/viewer/model/sagas/saving/save_saga.ts
@@ -1,7 +1,7 @@
-import { getAgglomeratesForSegmentsFromTracingstore, getUpdateActionLog } from "admin/rest_api";
+import { getUpdateActionLog } from "admin/rest_api";
 import ErrorHandling from "libs/error_handling";
 import Toast from "libs/toast";
-import { getAdaptToTypeFunction, sleep } from "libs/utils";
+import { sleep } from "libs/utils";
 import _ from "lodash";
 import { type Channel, buffers } from "redux-saga";
 import { actionChannel, call, delay, flush, fork, put, race, takeEvery } from "typed-redux-saga";
@@ -15,7 +15,6 @@ import {
   finishedApplyingMissingUpdatesAction,
   finishedRebaseAction,
   prepareRebaseAction,
-  replaceSaveQueueAction,
   setVersionNumberAction,
 } from "viewer/model/actions/save_actions";
 import { setMappingAction } from "viewer/model/actions/settings_actions";
@@ -26,24 +25,16 @@ import type { Saga } from "viewer/model/sagas/effect-generators";
 import { select, take } from "viewer/model/sagas/effect-generators";
 import { ensureWkInitialized } from "viewer/model/sagas/ready_sagas";
 import { Model, Store } from "viewer/singletons";
-import type {
-  Mapping,
-  NumberLike,
-  NumberLikeMap,
-  SaveQueueEntry,
-  SkeletonTracing,
-  VolumeTracing,
-} from "viewer/store";
+import type { NumberLike, SkeletonTracing, VolumeTracing } from "viewer/store";
 import {
   enforceExecutionAsBusyBlockingUnlessAllowed,
   takeEveryWithBatchActionSupport,
 } from "../saga_helpers";
 import { splitAgglomerateInMapping, updateMappingWithMerge } from "../volume/proofread_saga";
-import type {
-  MergeAgglomerateUpdateAction,
-  ServerUpdateAction,
-  SplitAgglomerateUpdateAction,
-} from "../volume/update_actions";
+import {
+  saveQueueEntriesToServerUpdateActionBatches,
+  updateSaveQueueEntriesToStateAfterRebase,
+} from "./rebasing_helpers";
 import { pushSaveQueueAsync } from "./save_queue_draining";
 import { setupSavingForAnnotation, setupSavingForTracingType } from "./save_queue_filling";
 
@@ -75,25 +66,6 @@ function* getPollInterval(): Saga<number> {
 
   // The current user is the only one who can edit the annotation.
   return VERSION_POLL_INTERVAL_SINGLE_EDITOR;
-}
-
-export function saveQueueEntriesToServerUpdateActionBatches(
-  data: Array<SaveQueueEntry>,
-  version: number,
-) {
-  return data.map((entry) => ({
-    version,
-    value: entry.actions.map(
-      (action) =>
-        ({
-          ...action,
-          value: {
-            actionTimestamp: 0,
-            ...action.value,
-          },
-        }) as ServerUpdateAction,
-    ),
-  }));
 }
 
 function* shouldCheckForNewerAnnotationVersions(): Saga<boolean> {
@@ -597,190 +569,4 @@ export function* tryToIncorporateActions(
   yield* call(finalize);
   return { success: true };
 }
-
-type IdsToReloadPerMappingId = Record<string, number[]>;
-
-// Gathers mapped agglomerate ids for unknown but relevant segments to apply the passed save queue entries correctly.
-// This is needed in case proofreading was done via mesh interactions whose mapping info is present in the meshes
-// but not in the activeMappingByLayer.mapping. Due to incorporating backend updates the agglomerate ids of the
-// meshes might be outdated, thus we reload this info and store it in the local mapping to perform the correct merge.
-// Returns a list of segment ids to reload for each needed volume / editable tracing id.
-function* getAllUnknownSegmentIdsInPendingUpdates(
-  saveQueue: SaveQueueEntry[],
-): Saga<IdsToReloadPerMappingId> {
-  const activeMappingByLayer = yield* select(
-    (store) => store.temporaryConfiguration.activeMappingByLayer,
-  );
-  const idsToReloadByMappingId = {} as IdsToReloadPerMappingId;
-  for (const saveQueueEntry of saveQueue) {
-    for (const action of saveQueueEntry.actions) {
-      switch (action.name) {
-        case "mergeAgglomerate":
-        case "splitAgglomerate": {
-          const { segmentId1, segmentId2, actionTracingId } = action.value;
-          const mappingSyncedWithBackend = activeMappingByLayer[actionTracingId]?.mapping;
-          if (!mappingSyncedWithBackend || segmentId1 == null || segmentId2 == null) {
-            continue;
-          }
-
-          const adaptToType = getAdaptToTypeFunction(mappingSyncedWithBackend);
-          const updatedAgglomerateId1 = (mappingSyncedWithBackend as NumberLikeMap).get(
-            adaptToType(segmentId1),
-          );
-          const updatedAgglomerateId2 = (mappingSyncedWithBackend as NumberLikeMap).get(
-            adaptToType(segmentId2),
-          );
-          if (!updatedAgglomerateId1) {
-            if (!(actionTracingId in idsToReloadByMappingId)) {
-              idsToReloadByMappingId[actionTracingId] = [];
-            }
-            idsToReloadByMappingId[actionTracingId].push(segmentId1);
-          }
-          if (!updatedAgglomerateId2) {
-            if (!(actionTracingId in idsToReloadByMappingId)) {
-              idsToReloadByMappingId[actionTracingId] = [];
-            }
-            idsToReloadByMappingId[actionTracingId].push(segmentId2);
-          }
-        }
-      }
-    }
-  }
-  return idsToReloadByMappingId;
-}
-
-// For each passed mapping, reload the segment ids' mapping information and store it in the local mapping.
-// Needed after getAllUnknownSegmentIdsInPendingUpdates to load updated mapping info for segment ids of
-// mesh interaction proofreading actions to ensure reapplying these actions is done with up-to-date mapping info.
-function* addMissingSegmentsToLoadedMappings(idsToReload: IdsToReloadPerMappingId): Saga<void> {
-  const annotationId = yield* select((state) => state.annotation.annotationId);
-  const version = yield* select((state) => state.annotation.version);
-  const tracingStoreUrl = yield* select((state) => state.annotation.tracingStore.url);
-  const activeMappingByLayer = yield* select(
-    (store) => store.temporaryConfiguration.activeMappingByLayer,
-  );
-  for (const volumeTracingId of Object.keys(idsToReload)) {
-    if (idsToReload[volumeTracingId].length === 0) {
-      continue;
-    }
-    const activeMapping = activeMappingByLayer[volumeTracingId];
-    // Ask the server to map the segment ids needing reloading. This creates a partial mapping
-    // that only contains these ids.
-    const mappingWithMissingIds = yield* call(
-      getAgglomeratesForSegmentsFromTracingstore,
-      tracingStoreUrl,
-      volumeTracingId,
-      idsToReload[volumeTracingId],
-      annotationId,
-      version,
-    );
-    const mergedMapping = new Map(
-      Array.from((activeMapping.mapping ?? new Map()) as NumberLikeMap).concat(
-        Array.from(mappingWithMissingIds as NumberLikeMap),
-      ),
-    );
-    yield* put(
-      setMappingAction(volumeTracingId, activeMapping.mappingName, activeMapping.mappingType, {
-        mapping: mergedMapping as Mapping,
-      }),
-    );
-  }
-}
-
-// Gathers info mapped info for segment ids from proofreading actions where the mapping is unknown.
-// This happens in case of mesh proofreading actions. To re-apply the user's changes in the rebasing
-// up-to-date mapping info is needed for all segments in all proofreading actions. Thus, the missing info
-// is first loaded and then the save queue update actions are remapped to update their agglomerate id infos
-// to apply them correctly during rebasing. Lastly, the save queue is replaced with the updated save queue entries.
-function* updateSaveQueueEntriesToStateAfterRebase(): Saga<
-  | {
-      success: false;
-      updatedSaveQueue: undefined;
-    }
-  | {
-      success: true;
-      updatedSaveQueue: SaveQueueEntry[];
-    }
-> {
-  const saveQueue = yield* select((state) => state.save.queue);
-  const idsToFetch = yield* call(getAllUnknownSegmentIdsInPendingUpdates, saveQueue);
-  yield* call(addMissingSegmentsToLoadedMappings, idsToFetch);
-  const activeMappingByLayer = yield* select(
-    (store) => store.temporaryConfiguration.activeMappingByLayer,
-  );
-
-  let success = true;
-  const updatedSaveQueue = saveQueue.map((saveQueueEntry) => ({
-    ...saveQueueEntry,
-    actions: saveQueueEntry.actions
-      .map((action) => {
-        switch (action.name) {
-          case "mergeAgglomerate":
-          case "splitAgglomerate": {
-            const { segmentId1, segmentId2, actionTracingId } = action.value;
-            const mappingSyncedWithBackend = activeMappingByLayer[actionTracingId]?.mapping;
-            if (!mappingSyncedWithBackend) {
-              console.error(
-                "Found proofreading action without matching mapping in save queue. This should never happen.",
-                action,
-              );
-              success = false;
-              return null;
-            }
-            if (segmentId1 == null || segmentId2 == null) {
-              console.error(
-                "Found proofreading action without given segmentIds in save queue. This should never happen.",
-                action,
-              );
-              success = false;
-              return null;
-            }
-
-            const adaptToType = getAdaptToTypeFunction(mappingSyncedWithBackend);
-            let upToDateAgglomerateId1 = (mappingSyncedWithBackend as NumberLikeMap).get(
-              adaptToType(segmentId1),
-            );
-            let upToDateAgglomerateId2 = (mappingSyncedWithBackend as NumberLikeMap).get(
-              adaptToType(segmentId2),
-            );
-            if (!upToDateAgglomerateId1 || !upToDateAgglomerateId2) {
-              console.error(
-                "Found proofreading action without loaded agglomerate ids. This should never occur.",
-                action,
-              );
-              success = false;
-              return null;
-            }
-            if (action.name === "mergeAgglomerate") {
-              return {
-                name: action.name,
-                value: {
-                  ...action.value,
-                  agglomerateId1: Number(upToDateAgglomerateId1),
-                  agglomerateId2: Number(upToDateAgglomerateId2),
-                },
-              } satisfies MergeAgglomerateUpdateAction;
-            } else {
-              return {
-                name: action.name,
-                value: {
-                  ...action.value,
-                  agglomerateId: Number(upToDateAgglomerateId1),
-                },
-              } satisfies SplitAgglomerateUpdateAction;
-            }
-          }
-          default:
-            return action;
-        }
-      })
-      .filter((a) => a != null),
-  }));
-  if (success) {
-    yield put(replaceSaveQueueAction(updatedSaveQueue));
-    return { success: true, updatedSaveQueue };
-  }
-  return { success: false, updatedSaveQueue: undefined };
-}
-
 export default [setupSavingToServer, watchForNewerAnnotationVersion];

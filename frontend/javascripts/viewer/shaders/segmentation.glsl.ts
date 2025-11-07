f54a123f82216f4839cecfc6f9e48b0616ec1c86
Philipp Otto <philipp.4096@gmail.com>
2025-11-07 17:58:04 +0100

    also pass the layer's bbox for seg layers

--- frontend/javascripts/viewer/shaders/segmentation.glsl.ts
+++ frontend/javascripts/viewer/shaders/segmentation.glsl.ts
@@ -395,8 +395,8 @@ export const getSegmentId: ShaderModule = {
 
   <% _.each(segmentationLayerNames, function(segmentationName, layerIndex) { %>
     void getSegmentId_<%= segmentationName %>(vec3 worldPositionUVW, out vec4[2] segment_id, out vec4[2] mapped_id) {
-      vec3 transformedCoordUVW = transDim((<%= segmentationName %>_transform * vec4(transDim(worldPositionUVW), 1.0)).xyz);
-      if (isOutsideOfBoundingBox(transformedCoordUVW, bboxMin, bboxMax)) {
+      vec3 layerCoordUVW = transDim((<%= segmentationName %>_transform * vec4(transDim(worldPositionUVW), 1.0)).xyz);
+      if (isOutsideOfBoundingBox(layerCoordUVW, <%= segmentationName %>_bboxMin, <%= segmentationName %>_bboxMax)) {
         // Some GPUs don't null-initialize the variables.
         segment_id[0] = vec4(0.);
         segment_id[1] = vec4(0.);
@@ -410,7 +410,7 @@ export const getSegmentId: ShaderModule = {
           <%= formatNumberAsGLSLFloat(colorLayerNames.length + layerIndex) %>,
           <%= segmentationName %>_data_texture_width,
           <%= formatNumberAsGLSLFloat(textureLayerInfos[segmentationName].packingDegree) %>,
-          transformedCoordUVW,
+          layerCoordUVW,
           vec4(0.0, 0.0, 0.0, 0.0),
           !<%= segmentationName %>_has_transform
         );

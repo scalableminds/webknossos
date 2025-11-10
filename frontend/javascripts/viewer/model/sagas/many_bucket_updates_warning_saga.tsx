d6ab6af9d84f34c9fbaef2eac4b8c324786b8bfd
Michael Büßemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>
2025-11-10 12:37:55 +0100

    fix select imports

--- frontend/javascripts/viewer/model/sagas/many_bucket_updates_warning_saga.tsx
+++ frontend/javascripts/viewer/model/sagas/many_bucket_updates_warning_saga.tsx
@@ -1,8 +1,8 @@
 import { updateNovelUserExperienceInfos } from "admin/rest_api";
 import { Button, Checkbox, type CheckboxChangeEvent, Space } from "antd";
 import Toast from "libs/toast";
-import { select, takeEvery } from "typed-redux-saga";
-import type { Saga } from "viewer/model/sagas/effect-generators";
+import { takeEvery } from "typed-redux-saga";
+import { type Saga, select } from "viewer/model/sagas/effect-generators";
 import type { WebknossosState } from "viewer/store";
 
 const TOO_MANY_BUCKETS_TOAST_KEY = "manyBucketUpdatesWarningToast";

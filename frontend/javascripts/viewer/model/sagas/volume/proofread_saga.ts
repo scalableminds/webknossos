6065eebcd3d1af2ff72c720f7383df1094eecfca
Michael Büßemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>
2025-11-21 19:13:42 +0100

    fix initial proofread action upon while othersMayEdit is active

--- frontend/javascripts/viewer/model/sagas/volume/proofread_saga.ts
+++ frontend/javascripts/viewer/model/sagas/volume/proofread_saga.ts
@@ -342,7 +342,7 @@ export function* createEditableMapping(): Saga<string> {
   yield* put(setMappingNameAction(layerName, volumeTracingId, "HDF5"));
   yield* put(setHasEditableMappingAction(volumeTracingId));
   // Ensure a saved state so that the mapping is locked and editable before doing the first proofreading operation.
-  yield* call([Model, Model.ensureSavedState]);
+  yield* call(syncWithBackend);
   const editableMapping: ServerEditableMapping = {
     baseMappingName: baseMappingName,
     tracingId: volumeTracingId,

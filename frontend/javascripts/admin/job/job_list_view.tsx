525dcdf896c9190d151ef50031873d9f7f026445
Tom Herold <tom.herold@scalableminds.com>
2025-11-25 11:27:28 +0100

    refactor loading to react-query

--- frontend/javascripts/admin/job/job_list_view.tsx
+++ frontend/javascripts/admin/job/job_list_view.tsx
@@ -11,6 +11,7 @@ import {
   QuestionCircleTwoTone,
 } from "@ant-design/icons";
 import { PropTypes } from "@scalableminds/prop-types";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { cancelJob, getJobs, retryJob } from "admin/rest_api";
 import { Input, Modal, Spin, Table, Tooltip, Typography } from "antd";
 import { AsyncLink } from "components/async_clickables";
@@ -18,7 +19,6 @@ import FormattedDate from "components/formatted_date";
 import { confirmAsync } from "dashboard/dataset/helper_components";
 import { formatCreditsString, formatWkLibsNdBBox } from "libs/format_utils";
 import Persistence from "libs/persistence";
-import { useInterval } from "libs/react_helpers";
 import { useWkSelector } from "libs/react_hooks";
 import Toast from "libs/toast";
 import * as Utils from "libs/utils";
@@ -133,29 +133,19 @@ export function JobState({ job }: { job: APIJob }) {
 }
 
 function JobListView() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [jobs, setJobs] = useState<APIJob[]>([]);
+  const queryClient = useQueryClient();
+  const { data: jobs, isLoading } = useQuery({
+    queryKey: ["jobs"],
+    queryFn: getJobs,
+    refetchInterval: refreshInterval,
+  });
   const [searchQuery, setSearchQuery] = useState("");
   const isCurrentUserSuperUser = useWkSelector((state) => state.activeUser?.isSuperUser);
 
   useEffect(() => {
-    fetchData();
     const { searchQuery } = persistence.load();
     setSearchQuery(searchQuery || "");
   }, []);
-
-  async function fetchData() {
-    setIsLoading(true);
-    try {
-      setJobs(await getJobs());
-    } finally {
-      setIsLoading(false);
-    }
-  }
-
-  useInterval(async () => {
-    setJobs(await getJobs());
-  }, refreshInterval);
 
   useEffect(() => {
     persistence.persist({ searchQuery });
@@ -322,7 +312,7 @@ function JobListView() {
             });
 
             if (isDeleteConfirmed) {
-              cancelJob(job.id).then(() => fetchData());
+              cancelJob(job.id).then(() => queryClient.invalidateQueries({ queryKey: ["jobs"] }));
             }
           }}
           icon={<CloseCircleOutlined className="icon-margin-right" />}
@@ -338,7 +328,7 @@ function JobListView() {
             onClick={async () => {
               try {
                 await retryJob(job.id);
-                await fetchData();
+                await queryClient.invalidateQueries({ queryKey: ["jobs"] });
                 Toast.success("Job is being retried");
               } catch (e) {
                 console.error("Could not retry job", e);
@@ -473,7 +463,7 @@ function JobListView() {
       />
       <Spin spinning={isLoading} size="large">
         <Table
-          dataSource={Utils.filterWithSearchQueryAND(jobs, ["datasetName"], searchQuery)}
+          dataSource={Utils.filterWithSearchQueryAND(jobs || [], ["datasetName"], searchQuery)}
           rowKey="id"
           pagination={{
             defaultPageSize: 50,

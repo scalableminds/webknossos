scalableminds/webknossos

commit 1b4f2402e8bc2a3a9e7e1584b1e5dc9b3bb0813c

Add loading state spinner for job and workflow list views (#9095)
I fixed the loading state spinner for the job list view and added one
for the VX workflow list view too. I also took the opportunity to
replace the loading and polling code with standardized react-query
hooks.

### Steps to test:
- it is hard to test since you need many jobs workflows
- Jobs List:
- I just spammed a bunch of tiff exports, Blender animations and AI jobs
(without running the worker) to fill up the page with (pending) jobs
- 


### Issues:
- fixes #9051

------
(Please delete unneeded items, merge only when none are left open)
- [ ] Added changelog entry (create a `$PR_NUMBER.md` file in
`unreleased_changes` or use `./tools/create-changelog-entry.py`)
- [ ] Added migration guide entry if applicable (edit the same file as
for the changelog)
- [ ] Updated [documentation](../blob/master/docs) if applicable
- [ ] Adapted [wk-libs python
client](https://github.com/scalableminds/webknossos-libs/tree/master/webknossos/webknossos/client)
if relevant API parts change
- [ ] Removed dev-only changes like prints and application.conf edits
- [ ] Considered [common edge
cases](../blob/master/.github/common_edge_cases.md)
- [ ] Needs datastore update after deployment

frontend/javascripts/admin/voxelytics/workflow_list_view.tsx
@@ -1,10 +1,10 @@
 import { SyncOutlined } from "@ant-design/icons";
 import { PropTypes } from "@scalableminds/prop-types";
+import { useQuery } from "@tanstack/react-query";
 import { getVoxelyticsWorkflows } from "admin/rest_api";
-import { Button, Input, Progress, Table, Tooltip } from "antd";
+import { Button, Input, Progress, Spin, Table, Tooltip } from "antd";
 import { formatCountToDataAmountUnit, formatDateMedium, formatNumber } from "libs/format_utils";
 import Persistence from "libs/persistence";
-import { usePolling } from "libs/react_hooks";
 import Toast from "libs/toast";
 import * as Utils from "libs/utils";
 import type React from "react";
@@ -59,8 +59,6 @@ type RenderRunInfo = Omit<VoxelyticsWorkflowListingRun, "userFirstName" | "userL
 };
 
 export default function WorkflowListView() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [workflows, setWorkflows] = useState<Array<VoxelyticsWorkflowListing>>([]);
   const [searchQuery, setSearchQuery] = useState("");
 
   function handleSearch(event: React.ChangeEvent<HTMLInputElement>): void {
@@ -70,30 +68,30 @@ export default function WorkflowListView() {
   useEffect(() => {
     const { searchQuery } = persistence.load();
     setSearchQuery(searchQuery || "");
-    loadData();
   }, []);
 
   useEffect(() => {
     persistence.persist({ searchQuery });
   }, [searchQuery]);
 
-  async function loadData() {
-    setIsLoading(true);
-    try {
-      const _workflows = (await getVoxelyticsWorkflows()).map(parseWorkflowInfo);
-      setWorkflows(_workflows);
-    } catch (err) {
-      Toast.error("Could not load workflow list.");
-      console.error(err);
-    } finally {
-      setIsLoading(false);
-    }
-  }
-
-  usePolling(async () => {
-    // initial data fetch is done above, thus only load data here if it is polled repeatedly
-    if (VX_POLLING_INTERVAL != null) loadData();
-  }, VX_POLLING_INTERVAL);
+  const {
+    data: workflows = [],
+    isLoading,
+    refetch,
+    isFetching,
+  } = useQuery({
+    queryKey: ["voxelyticsWorkflows"],
+    queryFn: async () => {
+      try {
+        return (await getVoxelyticsWorkflows()).map(parseWorkflowInfo);
+      } catch (err) {
+        Toast.error("Could not load workflow list.");
+        console.error(err);
+        throw err;
+      }
+    },
+    refetchInterval: VX_POLLING_INTERVAL ?? false,
+  });
 
   const getUserDisplayName = (run: VoxelyticsWorkflowListingRun) => {
     return run.userFirstName != null || run.userLastName != null
@@ -172,8 +170,8 @@ export default function WorkflowListView() {
   return (
     <div className="container voxelytics-view">
       <div className="pull-right">
-        <Button onClick={() => loadData()} style={{ marginRight: 20 }}>
-          <SyncOutlined spin={isLoading} /> Refresh
+        <Button onClick={() => refetch()} style={{ marginRight: 20 }}>
+          <SyncOutlined spin={isFetching} /> Refresh
         </Button>
         <Search
           style={{
@@ -184,96 +182,99 @@ export default function WorkflowListView() {
         />
       </div>
       <h3>Voxelytics Workflows</h3>
-      <Table
-        bordered
-        rowKey={(run: RenderRunInfo) => `${run.id}-${run.workflowHash}`}
-        pagination={{ pageSize: 100 }}
-        columns={[
-          {
-            title: "Workflow",
-            key: "workflow",
-            render: (run: RenderRunInfo) =>
-              run.id === "" ? (
-                <Link to={`/workflows/${run.workflowHash}`}>
-                  {run.workflowName} ({run.workflowHash})
-                </Link>
-              ) : (
-                <Link to={`/workflows/${run.workflowHash}?runId=${encodeURIComponent(run.id)}`}>
-                  {run.name}
-                </Link>
+      <Spin spinning={isLoading} size="large">
+        <Table
+          bordered
+          rowKey={(run: RenderRunInfo) => `${run.id}-${run.workflowHash}`}
+          pagination={{ pageSize: 100 }}
+          columns={[
+            {
+              title: "Workflow",
+              key: "workflow",
+              render: (run: RenderRunInfo) =>
+                run.id === "" ? (
+                  <Link to={`/workflows/${run.workflowHash}`}>
+                    {run.workflowName} ({run.workflowHash})
+                  </Link>
+                ) : (
+                  <Link to={`/workflows/${run.workflowHash}?runId=${encodeURIComponent(run.id)}`}>
+                    {run.name}
+                  </Link>
+                ),
+            },
+            {
+              title: "User",
+              key: "userName",
+              dataIndex: "userDisplayName",
+              filters: uniqueify(renderRuns.map((run) => run.userDisplayName)).map((username) => ({
+                text: username || "",
+                value: username || "",
+              })),
+              onFilter: (value: Key | boolean, run: RenderRunInfo) =>
+                run.userDisplayName?.startsWith(String(value)) || false,
+              filterSearch: true,
+            },
+            {
+              title: "Host",
+              dataIndex: "hostName",
+              key: "host",
+              filters: uniqueify(renderRuns.map((run) => run.hostName)).map((hostname) => ({
+                text: hostname,
+                value: hostname,
+              })),
+              onFilter: (value: Key | boolean, run: RenderRunInfo) =>
+                run.hostName.startsWith(String(value)),
+              filterSearch: true,
+            },
+            {
+              title: "Progress",
+              key: "progress",
+              width: 200,
+              render: renderProgress,
+            },
+            {
+              title: "File Size",
+              key: "fileSize",
+              width: 200,
+              render: (run: RenderRunInfo) => (
+                <Tooltip
+                  overlay={
+                    <>
+                      {formatCountToDataAmountUnit(run.taskCounts.fileSize)} •{" "}
+                      {formatNumber(run.taskCounts.inodeCount)} inodes
+                      <br />
+                      Note: manual changes on disk are not reflected here
+                    </>
+                  }
+                >
+                  {formatCountToDataAmountUnit(run.taskCounts.fileSize)}
+                </Tooltip>
               ),
-          },
-          {
-            title: "User",
-            key: "userName",
-            dataIndex: "userDisplayName",
-            filters: uniqueify(renderRuns.map((run) => run.userDisplayName)).map((username) => ({
-              text: username || "",
-              value: username || "",
-            })),
-            onFilter: (value: Key | boolean, run: RenderRunInfo) =>
-              run.userDisplayName?.startsWith(String(value)) || false,
-            filterSearch: true,
-          },
-          {
-            title: "Host",
-            dataIndex: "hostName",
-            key: "host",
-            filters: uniqueify(renderRuns.map((run) => run.hostName)).map((hostname) => ({
-              text: hostname,
-              value: hostname,
-            })),
-            onFilter: (value: Key | boolean, run: RenderRunInfo) =>
-              run.hostName.startsWith(String(value)),
-            filterSearch: true,
-          },
-          {
-            title: "Progress",
-            key: "progress",
-            width: 200,
-            render: renderProgress,
-          },
-          {
-            title: "File Size",
-            key: "fileSize",
-            width: 200,
-            render: (run: RenderRunInfo) => (
-              <Tooltip
-                overlay={
-                  <>
-                    {formatCountToDataAmountUnit(run.taskCounts.fileSize)} •{" "}
-                    {formatNumber(run.taskCounts.inodeCount)} inodes
-                    <br />
-                    Note: manual changes on disk are not reflected here
-                  </>
-                }
-              >
-                {formatCountToDataAmountUnit(run.taskCounts.fileSize)}
-              </Tooltip>
-            ),
-            sorter: (a: RenderRunInfo, b: RenderRunInfo) =>
-              a.taskCounts.fileSize - b.taskCounts.fileSize,
-          },
-          {
-            title: "Begin",
-            key: "begin",
-            defaultSortOrder: "descend",
-            sorter: (a: RenderRunInfo, b: RenderRunInfo) =>
-              (a.beginTime?.getTime() ?? Number.POSITIVE_INFINITY) -
-              (b.beginTime?.getTime() ?? Number.POSITIVE_INFINITY),
-            render: (run: RenderRunInfo) => run.beginTime && formatDateMedium(run.beginTime),
-          },
-          {
-            title: "End",
-            key: "end",
-            sorter: (a: RenderRunInfo, b: RenderRunInfo) =>
-              (a.endTime?.getTime() ?? Number.POSITIVE_INFINITY) -
-              (b.endTime?.getTime() ?? Number.POSITIVE_INFINITY),
-            render: (run: RenderRunInfo) => run.endTime && formatDateMedium(run.endTime),
-          },
-        ]}
-        dataSource={Utils.filterWithSearchQueryAND(renderRuns, ["workflowName"], searchQuery)}
-      />
+              sorter: (a: RenderRunInfo, b: RenderRunInfo) =>
+                a.taskCounts.fileSize - b.taskCounts.fileSize,
+            },
+            {
+              title: "Begin",
+              key: "begin",
+              defaultSortOrder: "descend",
+              sorter: (a: RenderRunInfo, b: RenderRunInfo) =>
+                (a.beginTime?.getTime() ?? Number.POSITIVE_INFINITY) -
+                (b.beginTime?.getTime() ?? Number.POSITIVE_INFINITY),
+              render: (run: RenderRunInfo) => run.beginTime && formatDateMedium(run.beginTime),
+            },
+            {
+              title: "End",
+              key: "end",
+              sorter: (a: RenderRunInfo, b: RenderRunInfo) =>
+                (a.endTime?.getTime() ?? Number.POSITIVE_INFINITY) -
+                (b.endTime?.getTime() ?? Number.POSITIVE_INFINITY),
+              render: (run: RenderRunInfo) => run.endTime && formatDateMedium(run.endTime),
+            },
+          ]}
+          dataSource={Utils.filterWithSearchQueryAND(renderRuns, ["workflowName"], searchQuery)}
+          locale={{ emptyText: null }}
+        />
+      </Spin>
     </div>
   );
 }

frontend/javascripts/admin/job/job_list_view.tsx
@@ -11,6 +11,7 @@ import {
   QuestionCircleTwoTone,
 } from "@ant-design/icons";
 import { PropTypes } from "@scalableminds/prop-types";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { cancelJob, getJobs, retryJob } from "admin/rest_api";
 import { Input, Modal, Spin, Table, Tooltip, Typography } from "antd";
 import { AsyncLink } from "components/async_clickables";
@@ -18,7 +19,6 @@ import FormattedDate from "components/formatted_date";
 import { confirmAsync } from "dashboard/dataset/helper_components";
 import { formatCreditsString, formatWkLibsNdBBox } from "libs/format_utils";
 import Persistence from "libs/persistence";
-import { useInterval } from "libs/react_helpers";
 import { useWkSelector } from "libs/react_hooks";
 import Toast from "libs/toast";
 import * as Utils from "libs/utils";
@@ -132,25 +132,19 @@ export function JobState({ job }: { job: APIJob }) {
 }
 
 function JobListView() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [jobs, setJobs] = useState<APIJob[]>([]);
+  const queryClient = useQueryClient();
+  const { data: jobs, isLoading } = useQuery({
+    queryKey: ["jobs"],
+    queryFn: getJobs,
+    refetchInterval: refreshInterval,
+  });
   const [searchQuery, setSearchQuery] = useState("");
   const isCurrentUserSuperUser = useWkSelector((state) => state.activeUser?.isSuperUser);
 
   useEffect(() => {
-    fetchData();
     const { searchQuery } = persistence.load();
     setSearchQuery(searchQuery || "");
-    setIsLoading(false);
   }, []);
-
-  async function fetchData() {
-    setJobs(await getJobs());
-  }
-
-  useInterval(async () => {
-    setJobs(await getJobs());
-  }, refreshInterval);
 
   useEffect(() => {
     persistence.persist({ searchQuery });
@@ -316,7 +310,7 @@ function JobListView() {
             });
 
             if (isDeleteConfirmed) {
-              cancelJob(job.id).then(() => fetchData());
+              cancelJob(job.id).then(() => queryClient.invalidateQueries({ queryKey: ["jobs"] }));
             }
           }}
           icon={<CloseCircleOutlined className="icon-margin-right" />}
@@ -331,7 +325,7 @@ function JobListView() {
             onClick={async () => {
               try {
                 await retryJob(job.id);
-                await fetchData();
+                await queryClient.invalidateQueries({ queryKey: ["jobs"] });
                 Toast.success("Job is being retried");
               } catch (e) {
                 console.error("Could not retry job", e);
@@ -466,7 +460,7 @@ function JobListView() {
       />
       <Spin spinning={isLoading} size="large">
         <Table
-          dataSource={Utils.filterWithSearchQueryAND(jobs, ["datasetName"], searchQuery)}
+          dataSource={Utils.filterWithSearchQueryAND(jobs || [], ["datasetName"], searchQuery)}
           rowKey="id"
           pagination={{
             defaultPageSize: 50,

commit fb5cc6b382ee6a6e9a926684d225d01bc92c06c1

Fix VX Workflow list cache errors (#9109)
This PR fixes an issues with the VX Worfklow view. After a while or on
page refresh it would show the following error message:
<img width="1207" height="613" alt="Screenshot 2025-12-01 at 10 56 04"
src="https://github.com/user-attachments/assets/db2cb363-72b4-4401-8687-bc6c3641b55a"
/>

1. The error indicates that the workflow list date was not properly
transformed, especially the string timestamps were not transformed into
JS `Date` objects.
2. Upon closer inspection I noticed that this workflow data is being
displayed (and failing) before the request to WK backend is even
completed (or started). Hence, the data must be restored from cache.
3. In PR #9095, I introduced the use of `react-query` for fetching the
workflow. React-query is configured to cache all requests in the
[`localStorage`](https://vscode.dev/github/scalableminds/webknossos/blob/master/frontend/javascripts/main.tsx#L50-L51).
In general, the cached data is serialized as JSON strings.
4. Is suspect, that the workflow list view data including the JS Date
objects, is serialized as JSON strings when being cached. Upon restoring
the cached JS Date objects are parsed/restored as regular strings not
`Date` objects. 🤦
5. I fixed it by applying the workflow data timestamps to `Date`
transformations in a separate `select` (=transform) step in the
`react-query` runtime that is being executing both after fetching the
data from the server and restoring from the cache. See docs:
https://tanstack.com/query/v5/docs/framework/react/reference/useQuery

```
This option can be used to transform or select a part of the data returned by the query function. It affects the returned data value, but does not affect what gets stored in the query cache.
The select function will only run if data changed, or if the reference to the select function itself changes. To optimize, wrap the function in useCallback.
``` 

### Steps to test:
I have not found a way to manual trigger the above error yet. :-/ 

To test the propose solution:
- Clear your browser `localStorage`
- Open the VX workflow list and make sure that it is populated correctly
- Refresh page to trigger a cache response


### Issues:
- fixes a regression from #9095

frontend/javascripts/admin/voxelytics/workflow_list_view.tsx
@@ -83,13 +83,15 @@ export default function WorkflowListView() {
     queryKey: ["voxelyticsWorkflows"],
     queryFn: async () => {
       try {
-        return (await getVoxelyticsWorkflows()).map(parseWorkflowInfo);
+        return await getVoxelyticsWorkflows();
       } catch (err) {
         Toast.error("Could not load workflow list.");
         console.error(err);
         throw err;
       }
     },
+    // We use the select to transform the data because it is run on cached and fetched data
+    select: (data) => data.map(parseWorkflowInfo),
     refetchInterval: VX_POLLING_INTERVAL ?? false,
   });
 
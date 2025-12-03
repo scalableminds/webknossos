950311134af3216542da0631a0676c2dceea5620
Florian M <florian@scm.io>
2025-12-03 10:27:01 +0100

    fix evolution

--- schema/evolutions/146-attachment-realpaths.sql
+++ schema/evolutions/146-attachment-realpaths.sql
@@ -2,7 +2,11 @@ START TRANSACTION;
 
 do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 145 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
 
+DROP VIEW webknossos.jobs_;
+
 ALTER TABLE webknossos.jobs ADD COLUMN lastRetry TIMESTAMPTZ;
+
+CREATE VIEW webknossos.jobs_ AS SELECT * FROM webknossos.jobs WHERE NOT isDeleted;
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 146;
 
--- schema/evolutions/reversions/146-attachment-realpaths.sql
+++ schema/evolutions/reversions/146-attachment-realpaths.sql
@@ -2,7 +2,11 @@ START TRANSACTION;
 
 do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 146 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
 
+DROP VIEW webknossos.jobs_;
+
 ALTER TABLE webknossos.jobs DROP COLUMN lastRetry;
+
+CREATE VIEW webknossos.jobs_ AS SELECT * FROM webknossos.jobs WHERE NOT isDeleted;
 
 UPDATE webknossos.releaseInformation SET schemaVersion = 145;
 

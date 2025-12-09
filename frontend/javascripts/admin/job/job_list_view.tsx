1e6b65a83d1dd2ba8f5e9f91fff285442108bbf8
Tom Herold <tom.herold@scalableminds.com>
2025-12-09 10:17:18 +0100

    Added a new Analysis navigation submenu (#9091)
    As discussed in #8383, we have several navbar menu entries that should
    be available to all users that are currently hidden in the "Admin"
    section. I moved them into a separate sub menu called "Analysis" (I am
    open for alternative nameing suggestions)
    
    <img width="1651" height="299" alt="Screenshot 2025-11-24 at 16 44 46"
    src="https://github.com/user-attachments/assets/0638aca8-6a0d-467f-9200-e2956678447d"
    />
    
    <img width="1159" height="211" alt="Screenshot 2025-11-24 at 16 44 19"
    src="https://github.com/user-attachments/assets/c79da0f4-9ecf-46ad-83ee-dd41bf6489f0"
    />
    
    Additionally:
    - I updated the AI model list to have a small description and link to
    docs
    - I updated the list views for Jobs and VX workflow to use the same
    basic layout as AI model view (flex box spacing)
    - I updated the AI model listing API to be available for all users
    
    ### URL of deployed dev instance (used for testing):
    - https://___.webknossos.xyz
    
    ### Steps to test:
    - Click through navbar
    
    
    ### Issues:
    - fixes #8383
    - fixes #5701
    
    ------
    (Please delete unneeded items, merge only when none are left open)
    - [x] Added changelog entry (create a `$PR_NUMBER.md` file in
    `unreleased_changes` or use `./tools/create-changelog-entry.py`)
    - [ ] Added migration guide entry if applicable (edit the same file as
    for the changelog)
    - [x] Updated [documentation](../blob/master/docs) if applicable
    - [ ] Adapted [wk-libs python
    client](https://github.com/scalableminds/webknossos-libs/tree/master/webknossos/webknossos/client)
    if relevant API parts change
    - [ ] Removed dev-only changes like prints and application.conf edits
    - [x] Considered [common edge
    cases](../blob/master/.github/common_edge_cases.md)
    - [ ] Needs datastore update after deployment

--- frontend/javascripts/admin/job/job_list_view.tsx
+++ frontend/javascripts/admin/job/job_list_view.tsx
@@ -13,7 +13,7 @@ import {
 import { PropTypes } from "@scalableminds/prop-types";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { cancelJob, getJobs, retryJob } from "admin/rest_api";
-import { Input, Modal, Spin, Table, Tooltip, Typography } from "antd";
+import { Flex, Input, Modal, Spin, Table, Tooltip, Typography } from "antd";
 import { AsyncLink } from "components/async_clickables";
 import FormattedDate from "components/formatted_date";
 import FormattedId from "components/formatted_id";
@@ -451,7 +451,26 @@ function JobListView() {
 
   return (
     <div className="container">
-      <div className="pull-right">
+      <Flex justify="space-between" align="baseline" style={{ marginBottom: 20 }}>
+        <div>
+          <h3>Jobs</h3>
+          <Typography.Paragraph type="secondary">
+            Some actions such as dataset conversions or export as Tiff files require some time for
+            processing in the background.
+            <a
+              href="https://docs.webknossos.org/webknossos/automation/jobs.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Tooltip title="Read more in the documentation">
+                <InfoCircleOutlined className="icon-margin-left" />
+              </Tooltip>
+            </a>
+            <br />
+            WEBKNOSSOS will notify you via email when a background job has finished. Alternatively,
+            reload this page to track the progress.
+          </Typography.Paragraph>
+        </div>
         <Search
           style={{
             width: 200,
@@ -459,30 +478,7 @@ function JobListView() {
           onChange={handleSearch}
           value={searchQuery}
         />
-      </div>
-      <h3>Jobs</h3>
-      <Typography.Paragraph type="secondary">
-        Some actions such as dataset conversions or export as Tiff files require some time for
-        processing in the background.
-        <a
-          href="https://docs.webknossos.org/webknossos/automation/jobs.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Tooltip title="Read more in the documentation">
-            <InfoCircleOutlined style={{ marginLeft: 10 }} />
-          </Tooltip>
-        </a>
-        <br />
-        WEBKNOSSOS will notify you via email when a job has finished or reload this page to track
-        progress.
-      </Typography.Paragraph>
-      <div
-        className="clearfix"
-        style={{
-          margin: "20px 0px",
-        }}
-      />
+      </Flex>
       <Spin spinning={isLoading} size="large">
         <Table
           dataSource={Utils.filterWithSearchQueryAND(

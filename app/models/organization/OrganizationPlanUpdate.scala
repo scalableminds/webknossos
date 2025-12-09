23d6f07c5d1cb9a36dddff2717066817a23a89bc
Florian M <fm3@users.noreply.github.com>
2025-12-09 13:18:43 +0100

    Route + log for organization plan updates (#9129)
    Superusers can now edit organization pricing plans via API.
    
    Admins and superusers can list organization pricing plan updates.
    
    Example: `curl 'http://localhost:9000/api/pricing/updatePlan' -X 'POST'
    --data '{"organizationId":"sample_organization","description":"a
    description!","paidUntil":"2025-12-12T00:00:00Z","includedUsers": null}'
    -H 'X-Auth-Token: secretSampleUserToken' --header "Content-Type:
    application/json"`
    
    Note that for the fields `paidUntil`, `includedUsers` and
    `includedStorageBytes` the value `null` in the json means that the value
    is updated to `null` (which means unlimited). If the key is absent from
    the json, the value is unchanged. Description and pricingPlan can also
    be omitted, meaning unchanged.
    
    in the update list route `GET
    http://localhost:9000/api/pricing/planUpdates`, the same differentiation
    between null and absent applies for the response body.
    
    ### URL of deployed dev instance (used for testing):
    - https://organizationplanupdates.webknossos.xyz
    
    ### Steps to test:
    - send update requests as mentioned above
    - organization values should be updated
    - update list route should return the updates in order of creation
    
    ### Issues:
    - fixes #9120
    
    ------
    - [x] Added changelog entry (create a `$PR_NUMBER.md` file in
    `unreleased_changes` or use `./tools/create-changelog-entry.py`)
    - [x] Added migration guide entry if applicable (edit the same file as
    for the changelog)
    - [x] Removed dev-only changes like prints and application.conf edits
    - [x] Considered [common edge
    cases](../blob/master/.github/common_edge_cases.md)

---
+++ app/models/organization/OrganizationPlanUpdate.scala
@@ -0,0 +1,28 @@
+package models.organization
+
+import com.scalableminds.util.time.Instant
+import com.scalableminds.util.tools.TristateOptionJsonHelper
+import models.team.PricingPlan.PricingPlan
+import play.api.libs.json.{Json, OFormat}
+
+case class OrganizationPlanUpdate(
+    organizationId: String,
+    description: Option[String],
+    pricingPlan: Option[PricingPlan],
+    paidUntil: Option[Option[Instant]] = Some(None), // None means unchanged, Some(None) means set to None
+    includedUsers: Option[Option[Int]] = Some(None), // None means unchanged, Some(None) means set to None
+    includedStorageBytes: Option[Option[Long]] = Some(None), // None means unchanged, Some(None) means set to None
+    created: Instant = Instant.now
+) {
+  lazy val paidUntilChanged: Boolean = paidUntil.isDefined
+  lazy val includedUsersChanged: Boolean = includedUsers.isDefined
+  lazy val includedStorageChanged: Boolean = includedStorageBytes.isDefined
+  lazy val paidUntilFlat: Option[Instant] = paidUntil.flatten
+  lazy val includedUsersFlat: Option[Int] = includedUsers.flatten
+  lazy val includedStorageFlat: Option[Long] = includedStorageBytes.flatten
+}
+
+object OrganizationPlanUpdate extends TristateOptionJsonHelper {
+  implicit val jsonFormat: OFormat[OrganizationPlanUpdate] =
+    Json.configured(tristateOptionParsing).format[OrganizationPlanUpdate]
+}

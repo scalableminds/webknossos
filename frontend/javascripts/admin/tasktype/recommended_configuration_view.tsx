bfbffa2fffb5af7a4b28644ce0c720e239107cc1
Tom Herold <tom.herold@scalableminds.com>
2026-01-21 11:16:44 +0100

    PR feedback

--- frontend/javascripts/admin/tasktype/recommended_configuration_view.tsx
+++ frontend/javascripts/admin/tasktype/recommended_configuration_view.tsx
@@ -125,7 +125,7 @@ export default function RecommendedConfigurationView({
   onChangeEnabled: (arg0: boolean) => void;
 }) {
   const recommendedConfiguration = getDefaultRecommendedConfiguration();
-  const configurationEntries = Object.entries(recommendedConfiguration).map(([key, _value]) => {
+  const configurationEntries = Object.entries(recommendedConfiguration).map(([key]) => {
     // @ts-ignore Typescript doesn't infer that key will be of type keyof RecommendedConfiguration
     const settingsKey: keyof RecommendedConfiguration = key;
     return {

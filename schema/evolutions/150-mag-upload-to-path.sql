35cc6a1a9a31b53f008f4f6a789a5769b714278a
Florian M <florian@scm.io>
2026-01-26 09:45:55 +0100

    fix after merge

--- schema/evolutions/150-mag-upload-to-path.sql
+++
@@ -1,9 +0,0 @@
-START TRANSACTION;
-
-do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 149 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
-
-ALTER TABLE webknossos.dataset_mags ADD COLUMN uploadToPathIsPending BOOLEAN NOT NULL DEFAULT FALSE;
-
-UPDATE webknossos.releaseInformation SET schemaVersion = 150;
-
-COMMIT TRANSACTION;
--- schema/evolutions/151-mag-upload-to-path.sql
+++ schema/evolutions/151-mag-upload-to-path.sql
@@ -2,8 +2,8 @@ START TRANSACTION;
 
 do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 150 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
 
-ALTER TABLE webknossos.dataset_mags DROP COLUMN uploadToPathIsPending;
+ALTER TABLE webknossos.dataset_mags ADD COLUMN uploadToPathIsPending BOOLEAN NOT NULL DEFAULT FALSE;
 
-UPDATE webknossos.releaseInformation SET schemaVersion = 149;
+UPDATE webknossos.releaseInformation SET schemaVersion = 151;
 
 COMMIT TRANSACTION;

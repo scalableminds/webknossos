05f836003292a0484a1b48f181e6dc1679c69111
Florian M <florian@scm.io>
2026-02-05 11:24:27 +0100

    In Start Inference Modal, make seed generator distance threshold optional

--- frontend/javascripts/viewer/view/ai_jobs/run_ai_model/ai_analysis_settings.tsx
+++ frontend/javascripts/viewer/view/ai_jobs/run_ai_model/ai_analysis_settings.tsx
@@ -186,15 +186,10 @@ export const AiAnalysisSettings: React.FC = () => {
                     <Form.Item
                       name="seedGeneratorDistanceThreshold"
                       label="Seed generator distance threshold (nm)"
-                      tooltip="Controls the distance between two objects' centers used as a starting point (seed) for a growing segmentation. It should be set to a positive value in nm, typically 10–30% of the model’s `max_distance` parameter (= diameter/thickness of the object). For larger objects, such as nuclei (~1000 nm), use higher values. For small ones, such as synaptic vesicles (~10 nm), use lower values. If set too low, objects may merge; if too high, they may split or be missed."
-                      rules={[{ required: true, message: "Please enter a positive number" }]}
+                      tooltip="Controls the distance between two objects' centers used as a starting point (seed) for a growing segmentation. If empty, a default based on the selected model is used. It should be set to a positive value in nm, typically 10–30% of the model’s `max_distance` parameter (= diameter/thickness of the object). For larger objects, such as nuclei (~1000 nm), use higher values. For small ones, such as synaptic vesicles (~10 nm), use lower values. If set too low, objects may merge; if too high, they may split or be missed."
+                      rules={[{ required: false, message: "Please enter a positive number" }]}
                     >
-                      <InputNumber
-                        min={0.1}
-                        suffix="nm"
-                        style={{ width: "100%" }}
-                        defaultValue={1000}
-                      />
+                      <InputNumber min={0.1} suffix="nm" style={{ width: "100%" }} />
                     </Form.Item>
                   </Col>
                 )}

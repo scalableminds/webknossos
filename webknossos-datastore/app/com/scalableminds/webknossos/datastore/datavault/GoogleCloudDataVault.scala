scalableminds/webknossos

commit eb9bddb3f47cb625fd3b7e9120fe3a1b33486b9f

Proxy mag and attachment requests to underlying storage (#9247)
New datastore routes
- `GET`
`http://localhost:9000/data/datasets/:datasetId/proxy/layers/:dataLayerName/mags/:mag/*path`
- `GET`
`http://localhost:9000/data/datasets/:datasetId/proxy/layers/:dataLayerName/attachments/:attachmentName/*path`

where *path is forwarded to the underlying (possibly remote) storage,
with credentials.

### Steps to test:
- To be used with wk libs pr
https://github.com/scalableminds/webknossos-libs/pull/1418

### Issues:
- fixes #9224

------
- [x] Added changelog entry (create a `$PR_NUMBER.md` file in
`unreleased_changes` or use `./tools/create-changelog-entry.py`)
- [x] Removed dev-only changes like prints and application.conf edits
- [x] Considered [common edge
cases](../blob/master/.github/common_edge_cases.md)
- [X] Needs datastore update after deployment

---------

Co-authored-by: Norman Rzepka <code@normanrz.com>

webknossos-datastore/app/com/scalableminds/webknossos/datastore/datavault/GoogleCloudDataVault.scala
@@ -36,16 +36,16 @@ class GoogleCloudDataVault(uri: URI, credential: Option[GoogleServiceAccountCred
   // dashes, excluding chars that may be part of the bucket name (e.g. underscore).
   private lazy val bucket: String = uri.getAuthority
 
-  override def readBytesAndEncoding(path: VaultPath, range: RangeSpecifier)(
+  override def readBytesEncodingAndRangeHeader(path: VaultPath, range: ByteRange)(
       implicit ec: ExecutionContext,
-      tc: TokenContext): Fox[(Array[Byte], Encoding.Value)] = {
+      tc: TokenContext): Fox[(Array[Byte], Encoding.Value, Option[String])] = {
 
     val objName = path.toRemoteUriUnsafe.getPath.tail
     val blobId = BlobId.of(bucket, objName)
     for {
       bytes <- try {
         range match {
-          case StartEnd(r) =>
+          case r: StartEndExclusiveByteRange =>
             val blobReader = storage.reader(blobId)
             blobReader.seek(r.start)
             blobReader.limit(r.end)
@@ -55,7 +55,7 @@ class GoogleCloudDataVault(uri: URI, credential: Option[GoogleServiceAccountCred
             bb.position(0)
             bb.get(arr)
             Fox.successful(arr)
-          case SuffixLength(l) =>
+          case SuffixLengthByteRange(l) =>
             val blobReader = storage.reader(blobId)
             blobReader.seek(-l)
             val bb = ByteBuffer.allocateDirect(l)
@@ -64,7 +64,7 @@ class GoogleCloudDataVault(uri: URI, credential: Option[GoogleServiceAccountCred
             bb.position(0)
             bb.get(arr)
             Fox.successful(arr)
-          case Complete() => Fox.successful(storage.readAllBytes(bucket, objName))
+          case CompleteByteRange() => Fox.successful(storage.readAllBytes(bucket, objName))
         }
       } catch {
         case s: StorageException =>
@@ -73,9 +73,16 @@ class GoogleCloudDataVault(uri: URI, credential: Option[GoogleServiceAccountCred
           else Fox.failure(s.getMessage)
         case t: Throwable => Fox.failure(t.getMessage)
       }
-      blobInfo <- tryo(BlobInfo.newBuilder(blobId).setContentType("text/plain").build).toFox
-      encoding <- Encoding.fromRfc7231String(Option(blobInfo.getContentEncoding).getOrElse("")).toFox
-    } yield (bytes, encoding)
+      blobInfo: BlobInfo <- tryo(
+        storage.get(
+          blobId,
+          Storage.BlobGetOption
+            .fields(Storage.BlobField.SIZE, Storage.BlobField.CONTENT_TYPE, Storage.BlobField.CONTENT_ENCODING),
+        )).toFox ?~> "could not get blobInfo"
+      encoding <- Encoding
+        .fromRfc7231String(Option(blobInfo.getContentEncoding).getOrElse(""))
+        .toFox ?~> "could not get encoding"
+    } yield (bytes, encoding, Option(blobInfo.getSize).flatMap(size => range.toContentRangeHeaderWithLength(size)))
   }
 
   override def listDirectory(path: VaultPath, maxItems: Int)(implicit ec: ExecutionContext): Fox[List[VaultPath]] =

app/controllers/DatasetController.scala
@@ -226,8 +226,12 @@ class DatasetController @Inject()(userService: UserService,
         _ <- Fox.fromBool(dataSource.dataLayers.nonEmpty) ?~> "dataset.explore.zeroLayers"
         dataStore <- dataStoreDAO.findOneWithUploadsAllowed
         _ <- datasetService.validatePaths(dataSource.allExplicitPaths, dataStore) ?~> "dataSource.add.pathsNotAllowed"
-        folderIdOpt <- Fox.runOptional(request.body.folderPath)(folderPath =>
-          folderService.getOrCreateFromPathLiteral(folderPath, request.identity._organization)) ?~> "dataset.explore.autoAdd.getFolder.failed"
+        folderIdOpt <- request.body.folderId match {
+          case Some(passedFolderId) => Fox.successful(Some(passedFolderId))
+          case None =>
+            Fox.runOptional(request.body.folderPath)(folderPath =>
+              folderService.getOrCreateFromPathLiteral(folderPath, request.identity._organization)) ?~> "dataset.explore.autoAdd.getFolder.failed"
+        }
         _ <- datasetService.assertValidDatasetName(request.body.datasetName)
         _ <- Fox.serialCombined(dataSource.dataLayers)(layer => datasetService.assertValidLayerNameLax(layer.name))
         newDataset <- datasetService.createAndSetUpDataset(

app/models/dataset/explore/WKExploreRemoteLayerService.scala
@@ -36,6 +36,7 @@ object WKExploreRemoteLayerParameters {
 
 case class ExploreAndAddRemoteDatasetParameters(remoteUri: String,
                                                 datasetName: String,
+                                                folderId: Option[ObjectId],
                                                 folderPath: Option[String],
                                                 dataStoreName: Option[String])
 

conf/messages
@@ -159,6 +159,7 @@ dataLayer.notFound=DataLayer “{0}” not found
 dataLayer.mustBeSegmentation=DataLayer “{0}” is not a segmentation layer
 
 dataLayer.wrongMag=DataLayer “{0}” does not have mag “{1}”
+dataLayer.wrongAttachment=DataLayer “{0}” does not have attachment “{1}”
 dataLayer.invalidMag=Supplied “{0}” is not a valid mag format. Please use “x-y-z”
 
 zarr.invalidChunkCoordinates=Invalid chunk coordinates. Expected dot separated coordinates like c.<additional_axes.>x.y.z

test/backend/DataVaultTestSuite.scala
@@ -6,22 +6,23 @@ import org.scalatestplus.play.PlaySpec
 
 import java.net.URI
 import com.scalableminds.webknossos.datastore.datavault.{
+  ByteRange,
   DataVault,
   Encoding,
   GoogleCloudDataVault,
   HttpsDataVault,
-  RangeSpecifier,
   S3DataVault,
+  StartEndExclusiveByteRange,
+  SuffixLengthByteRange,
   VaultPath
 }
-import com.scalableminds.webknossos.datastore.storage.{GoogleServiceAccountCredential, CredentializedUPath}
+import com.scalableminds.webknossos.datastore.storage.{CredentializedUPath, GoogleServiceAccountCredential}
 import com.scalableminds.util.tools.{Box, Empty, EmptyBox, Failure, Full}
 import com.scalableminds.webknossos.datastore.helpers.UPath
 import play.api.libs.json.JsString
 import play.api.test.WsTestClient
 
 import java.util.UUID
-import scala.collection.immutable.NumericRange
 import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.{global => globalExecutionContext}
 
@@ -33,7 +34,8 @@ class DataVaultTestSuite extends PlaySpec {
 
   "Data vault" when {
     "using Range requests" when {
-      val range: NumericRange[Long] = Range.Long(0, 100, 1)
+      val range: StartEndExclusiveByteRange = ByteRange.startEndExclusive(0, 100)
+      val suffixRange: SuffixLengthByteRange = ByteRange.suffix(100)
       val dataKey = "32_32_40/15360-15424_8384-8448_3520-3584" // when accessed via range request, the response body is 1024 bytes long, otherwise 124.8 KB
 
       "using HTTP Vault" should {
@@ -44,7 +46,7 @@ class DataVaultTestSuite extends PlaySpec {
               new VaultPath(upath, HttpsDataVault.create(CredentializedUPath(upath, None), ws, dummyDataStoreHost))
             val bytes =
               (vaultPath / s"neuroglancer-fafb-data/fafb_v14/fafb_v14_orig/$dataKey")
-                .readBytes(Some(range))(globalExecutionContext, emptyTokenContext)
+                .readBytes(range)(globalExecutionContext, emptyTokenContext)
                 .get(handleFoxJustification)
 
             assert(bytes.length == range.length)
@@ -56,14 +58,27 @@ class DataVaultTestSuite extends PlaySpec {
       "using Google Cloud Storage Vault" should {
         val upath = UPath.fromStringUnsafe("gs://neuroglancer-fafb-data/fafb_v14/fafb_v14_orig")
         val vaultPath = new VaultPath(upath, GoogleCloudDataVault.create(CredentializedUPath(upath, None)))
-        "return correct response" in {
+        "return correct response (start-end range)" in {
 
-          val bytes = (vaultPath / dataKey)
-            .readBytes(Some(range))(globalExecutionContext, emptyTokenContext)
+          val (bytes, encoding, rangeHeader) = (vaultPath / dataKey)
+            .readBytesEncodingAndRangeHeader(range)(globalExecutionContext, emptyTokenContext)
             .get(handleFoxJustification)
 
           assert(bytes.length == range.length)
+          assert(encoding == Encoding.identity)
+          assert(rangeHeader.contains("bytes 0-99/127808"))
           assert(bytes.take(10).sameElements(Array(-1, -40, -1, -32, 0, 16, 74, 70, 73, 70)))
+        }
+
+        "return correct response (suffix-length range)" in {
+          val (bytes, encoding, rangeHeader) = (vaultPath / dataKey)
+            .readBytesEncodingAndRangeHeader(suffixRange)(globalExecutionContext, emptyTokenContext)
+            .get(handleFoxJustification)
+
+          assert(bytes.length == suffixRange.length)
+          assert(encoding == Encoding.identity)
+          assert(rangeHeader.contains("bytes 127708-127807/127808"))
+          assert(bytes.takeRight(10).sameElements(Array(-61, 45, -114, -64, -109, -64, 25, -81, -1, -39)))
         }
 
         "return empty box" when {
@@ -78,7 +93,7 @@ class DataVaultTestSuite extends PlaySpec {
         "return failure" when {
           "requesting invalid range" in {
             val result = (vaultPath / dataKey)
-              .readBytes(Some(Range.Long(-5, -10, 1)))(globalExecutionContext, emptyTokenContext)
+              .readBytes(ByteRange.startEndExclusive(-5, -10))(globalExecutionContext, emptyTokenContext)
               .await(handleFoxJustification)
             assertBoxFailure(result)
           }
@@ -92,7 +107,7 @@ class DataVaultTestSuite extends PlaySpec {
                     Some(GoogleServiceAccountCredential("name", JsString("secret"), Some("user"), Some("org")))))
               )
             val result = (vaultPath / dataKey)
-              .readBytes(Some(Range.Long(-10, 10, 1)))(globalExecutionContext, emptyTokenContext)
+              .readBytes(ByteRange.startEndExclusive(-10, 10))(globalExecutionContext, emptyTokenContext)
               .await(handleFoxJustification)
             assertBoxFailure(result)
           }
@@ -107,7 +122,7 @@ class DataVaultTestSuite extends PlaySpec {
               new VaultPath(upath, S3DataVault.create(CredentializedUPath(upath, None), ws)(globalExecutionContext))
             val bytes =
               (vaultPath / "s0/5/5/5")
-                .readBytes(Some(range))(globalExecutionContext, emptyTokenContext)
+                .readBytes(range)(globalExecutionContext, emptyTokenContext)
                 .get(handleFoxJustification)
             assert(bytes.length == range.length)
             assert(bytes.take(10).sameElements(Array(0, 0, 0, 3, 0, 0, 0, 64, 0, 0)))
@@ -221,9 +236,9 @@ class DataVaultTestSuite extends PlaySpec {
 
     "using vault path" when {
       class MockDataVault extends DataVault {
-        override def readBytesAndEncoding(path: VaultPath, range: RangeSpecifier)(
+        override def readBytesEncodingAndRangeHeader(path: VaultPath, range: ByteRange)(
             implicit ec: ExecutionContext,
-            tc: TokenContext): Fox[(Array[Byte], Encoding.Value)] = ???
+            tc: TokenContext): Fox[(Array[Byte], Encoding.Value, Option[String])] = ???
 
         override def listDirectory(path: VaultPath,
                                    maxItems: Int)(implicit ec: ExecutionContext): Fox[List[VaultPath]] = ???

util/src/main/scala/com/scalableminds/util/tools/Box.scala
@@ -747,9 +747,7 @@ sealed case class Failure(msg: String, exception: Box[Throwable], chain: Box[Fai
   type A = Nothing
 
   override def getOrThrow(justification: => String) =
-    throw new NullPointerException(
-      "An Failure Box was opened.  Failure Message: " + msg +
-        ".  The justification for allowing the getOrThrow was " + justification) {
+    throw new NullPointerException(s"Opened Failure Box (justification: $justification). Details: $this") {
       override def getCause: Throwable = exception getOrElse null
     }
 

webknossos-datastore/app/com/scalableminds/webknossos/datastore/controllers/DataProxyController.scala
@@ -1,0 +1,123 @@
+package com.scalableminds.webknossos.datastore.controllers
+
+import com.google.inject.Inject
+import com.scalableminds.util.geometry.Vec3Int
+import com.scalableminds.util.objectid.ObjectId
+import com.scalableminds.util.tools.{Fox, FoxImplicits}
+import com.scalableminds.webknossos.datastore.dataformats.MagLocator
+import com.scalableminds.webknossos.datastore.datavault.{ByteRange, Encoding}
+import com.scalableminds.webknossos.datastore.datavault.Encoding.Encoding
+import com.scalableminds.webknossos.datastore.helpers.UPath
+import com.scalableminds.webknossos.datastore.models.datasource.{DataLayerAttachments, UsableDataSource}
+import com.scalableminds.webknossos.datastore.services.{DataStoreAccessTokenService, DatasetCache, UserAccessRequest}
+import com.scalableminds.webknossos.datastore.storage.DataVaultService
+import play.api.http.Writeable
+import play.api.i18n.Messages
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ResponseHeader, Result}
+
+import scala.concurrent.ExecutionContext
+
+class DataProxyController @Inject()(accessTokenService: DataStoreAccessTokenService,
+                                    dataVaultService: DataVaultService,
+                                    datasetCache: DatasetCache)(implicit ec: ExecutionContext)
+    extends Controller
+    with FoxImplicits {
+
+  def proxyMag(datasetId: ObjectId, dataLayerName: String, mag: String, path: String): Action[AnyContent] =
+    Action.async { implicit request =>
+      accessTokenService.validateAccessFromTokenContext(UserAccessRequest.readDataset(datasetId)) {
+        for {
+          _ <- validatePath(path)
+          magValidated <- Vec3Int.fromMagLiteral(mag, allowScalar = true).toFox ?~> Messages("dataLayer.invalidMag",
+                                                                                             mag)
+          (dataSource, dataLayer) <- datasetCache.getWithLayer(datasetId, dataLayerName) ?~> Messages(
+            "dataLayer.notFound",
+            dataLayerName) ~> NOT_FOUND
+          magLocator <- dataLayer.mags.find(_.mag == magValidated).toFox ?~> Messages("dataLayer.wrongMag",
+                                                                                      dataLayerName,
+                                                                                      mag) ~> NOT_FOUND
+          magPath <- dataVaultService.vaultPathFor(magLocator, dataSource.id, dataLayerName)
+          requestedPath = magPath / path
+          byteRange <- ByteRange.fromRequest(request)
+          (bytes, encoding, rangeHeader) <- requestedPath.readBytesEncodingAndRangeHeader(byteRange)
+          headers = buildResponseHeaders(encoding, rangeHeader)
+        } yield resultWithStatus(byteRange.successResponseCode, bytes).withHeaders(headers: _*)
+      }
+    }
+
+  private def buildResponseHeaders(encoding: Encoding, rangeHeader: Option[String]): Seq[(String, String)] = {
+    val contentEncoding = encoding match {
+      case Encoding.`identity` => None
+      case _                   => Some((CONTENT_ENCODING, encoding.toString))
+    }
+    val contentRange = rangeHeader.map(value => (CONTENT_RANGE, value))
+    val acceptRanges = Some(ACCEPT_RANGES, "bytes")
+    Seq(contentEncoding, contentRange, acceptRanges).flatten
+  }
+
+  def proxyAttachment(datasetId: ObjectId,
+                      dataLayerName: String,
+                      attachmentName: String,
+                      path: String): Action[AnyContent] = Action.async { implicit request =>
+    accessTokenService.validateAccessFromTokenContext(UserAccessRequest.readDataset(datasetId)) {
+      for {
+        _ <- validatePath(path)
+        (_, dataLayer) <- datasetCache.getWithLayer(datasetId, dataLayerName) ?~> Messages("dataLayer.notFound",
+                                                                                           dataLayerName) ~> NOT_FOUND
+        attachment <- dataLayer.allAttachments.find(_.name == attachmentName).toFox ?~> Messages(
+          "dataLayer.wrongAttachment",
+          dataLayerName,
+          attachmentName) ~> NOT_FOUND
+        attachmentPath <- dataVaultService.vaultPathFor(attachment)
+        requestedPath = attachmentPath / path
+        byteRange <- ByteRange.fromRequest(request)
+        (bytes, encoding, rangeHeader) <- requestedPath.readBytesEncodingAndRangeHeader(byteRange)
+        headers = buildResponseHeaders(encoding, rangeHeader)
+      } yield resultWithStatus(byteRange.successResponseCode, bytes).withHeaders(headers: _*)
+    }
+  }
+
+  def proxyDatasource(datasetId: ObjectId): Action[AnyContent] = Action.async { implicit request =>
+    accessTokenService.validateAccessFromTokenContext(UserAccessRequest.readDataset(datasetId)) {
+      for {
+        dataSource <- datasetCache.getById(datasetId) ?~> "dataSource.notFound" ~> NOT_FOUND
+        dataSourceWithAdaptedPaths = adaptPathsForDataSource(dataSource)
+      } yield Ok(Json.toJson(dataSourceWithAdaptedPaths))
+    }
+  }
+
+  private def adaptPathsForDataSource(dataSource: UsableDataSource) =
+    dataSource.copy(
+      dataLayers = dataSource.dataLayers.map(
+        layer =>
+          layer.mapped(magMapping = mag => adaptPathForMag(mag, layer.name),
+                       attachmentMapping = attachments => adaptPathsForAttachments(attachments, layer.name)))
+    )
+
+  private def adaptPathForMag(mag: MagLocator, layerName: String): MagLocator =
+    mag.copy(
+      path = Some(UPath.fromStringUnsafe(s"./layers/$layerName/mags/${mag.mag.toMagLiteral(allowScalar = true)}")))
+
+  private def adaptPathsForAttachments(attachments: DataLayerAttachments, layerName: String): DataLayerAttachments =
+    attachments.mapped(
+      attachment =>
+        attachment.copy(
+          path = UPath.fromStringUnsafe(s"./layers/$layerName/attachments/${attachment.name}")
+      ))
+
+  private def validatePath(path: String): Fox[Unit] =
+    for {
+      _ <- Fox.fromBool(!path.contains("..")) ?~> "path must not contain “..”"
+      _ <- Fox.fromBool(!path.startsWith("/")) ?~> "path must not start with “/”"
+      _ <- Fox.fromBool(!path.contains("%2e%2e")) ?~> "path must not contain “%2e%2e”"
+      _ <- Fox.fromBool(!path.startsWith("%2f")) ?~> "path must not start with “%2f”"
+    } yield ()
+
+  private def resultWithStatus[C](statusCode: Int, content: C)(implicit writeable: Writeable[C]): Result =
+    Result(
+      ResponseHeader(statusCode),
+      writeable.toEntity(content)
+    )
+
+}

webknossos-datastore/app/com/scalableminds/webknossos/datastore/datareaders/ChunkReader.scala
@@ -2,12 +2,11 @@ package com.scalableminds.webknossos.datastore.datareaders
 
 import com.scalableminds.util.accesscontext.TokenContext
 import com.scalableminds.util.tools.{Fox, FoxImplicits}
-import com.scalableminds.webknossos.datastore.datavault.VaultPath
+import com.scalableminds.webknossos.datastore.datavault.{ByteRange, VaultPath}
 import com.scalableminds.util.tools.{Box, Empty, Failure, Full}
 import com.scalableminds.util.tools.Box.tryo
 import ucar.ma2.{Array => MultiArray}
 
-import scala.collection.immutable.NumericRange
 import scala.concurrent.ExecutionContext
 
 class ChunkReader(header: DatasetHeader) extends FoxImplicits {
@@ -15,10 +14,9 @@ class ChunkReader(header: DatasetHeader) extends FoxImplicits {
   private lazy val chunkTyper = ChunkTyper.createFromHeader(header)
   private lazy val shortcutChunkTyper = new ShortcutChunkTyper(header)
 
-  def read(path: VaultPath,
-           chunkShapeFromMetadata: Array[Int],
-           range: Option[NumericRange[Long]],
-           useSkipTypingShortcut: Boolean)(implicit ec: ExecutionContext, tc: TokenContext): Fox[MultiArray] =
+  def read(path: VaultPath, chunkShapeFromMetadata: Array[Int], range: ByteRange, useSkipTypingShortcut: Boolean)(
+      implicit ec: ExecutionContext,
+      tc: TokenContext): Fox[MultiArray] =
     for {
       chunkBytesAndShapeBox: Box[(Array[Byte], Option[Array[Int]])] <- readChunkBytesAndShape(path, range).shiftBox
       chunkShape: Array[Int] = chunkBytesAndShapeBox.toOption.flatMap(_._2).getOrElse(chunkShapeFromMetadata)
@@ -43,7 +41,7 @@ class ChunkReader(header: DatasetHeader) extends FoxImplicits {
 
   // Returns bytes (optional, Fox.empty may later be replaced with fill value)
   // and chunk shape (optional, only for data formats where each chunk reports its own shape, e.g. N5)
-  protected def readChunkBytesAndShape(path: VaultPath, range: Option[NumericRange[Long]])(
+  protected def readChunkBytesAndShape(path: VaultPath, range: ByteRange)(
       implicit ec: ExecutionContext,
       tc: TokenContext): Fox[(Array[Byte], Option[Array[Int]])] =
     for {

commit 79ffe47cf2dab313117c39b8cb570c72b7de2f69

Avoid double decode in GCS Data Vault (#9282)
For GCS the default when requesting a full object is to automatically
decode the content. In #9247 I started returning the correct encoding
header. This now leads to double decoding. Since the other DataVault
clients don’t do decoding on their own, this PR now adapts the GCS one
to also return the raw response, along with the encoding, to the next
step.

### URL of deployed dev instance (used for testing):
- https://gcsnodoubledecode.webknossos.xyz

### Steps to test:
- Import dataset mentioned at
https://scm.slack.com/archives/C02H5T8Q08P/p1770628265672969?thread_ts=1770410096.440179&cid=C02H5T8Q08P
- Should load ok (if slow)
- Note: an error `Could not get minishard index` in the backend logging
is expected on this dataset. This also happened before the regression in
#9247 (I tested with the old code)

### Issues:
- fixes https://scm.slack.com/archives/C02H5T8Q08P/p1770410096440179

------
- [x] Added changelog entry (create a `$PR_NUMBER.md` file in
`unreleased_changes` or use `./tools/create-changelog-entry.py`)
- [x] Removed dev-only changes like prints and application.conf edits
- [x] Considered [common edge
cases](../blob/master/.github/common_edge_cases.md)
- [x] Needs datastore update after deployment

webknossos-datastore/app/com/scalableminds/webknossos/datastore/datavault/GoogleCloudDataVault.scala
@@ -1,10 +1,11 @@
 package com.scalableminds.webknossos.datastore.datavault
 
 import com.google.auth.oauth2.ServiceAccountCredentials
+import com.google.cloud.storage.Storage.BlobSourceOption
 import com.google.cloud.storage.{BlobId, BlobInfo, Storage, StorageException, StorageOptions}
 import com.scalableminds.util.accesscontext.TokenContext
 import com.scalableminds.util.tools.{Fox, FoxImplicits}
-import com.scalableminds.webknossos.datastore.storage.{GoogleServiceAccountCredential, CredentializedUPath}
+import com.scalableminds.webknossos.datastore.storage.{CredentializedUPath, GoogleServiceAccountCredential}
 import com.scalableminds.util.tools.Box.tryo
 import com.scalableminds.webknossos.datastore.helpers.UPath
 import org.apache.commons.lang3.builder.HashCodeBuilder
@@ -64,7 +65,8 @@ class GoogleCloudDataVault(uri: URI, credential: Option[GoogleServiceAccountCred
             bb.position(0)
             bb.get(arr)
             Fox.successful(arr)
-          case CompleteByteRange() => Fox.successful(storage.readAllBytes(bucket, objName))
+          case CompleteByteRange() =>
+            Fox.successful(storage.readAllBytes(bucket, objName, BlobSourceOption.shouldReturnRawInputStream(true)))
         }
       } catch {
         case s: StorageException =>
scalableminds/webknossos

commit c98fd0c4edae6b21346f695478031e55eb9c9742

Replace Store.dispatch with useDispatch (#9221)
This PR id part of the effort to update imports and undo some cyclic
dependencies. The WK `Store` is imported by a vast number of modules. In
many cases, it is only used to dispatch Redux actions, e.g.
`Store.dispatch`.

This PR updates all calls of `Store.dispatch` with the `useDispatch`
hook in React functional components. In some cases, this meant to to
"move" constant event handler code from outside the React component
inside of it. In these cases, I typically added a `React.useCallback`
hook for stable references. All in all, this should be a very
lightweight refactoring.

### URL of deployed dev instance (used for testing):
- https://___.webknossos.xyz

### Steps to test:
- Ideally CI is enough
- Make sure that Dashboard, Annotation View load fine

### Issues:
- fixes #

------
(Please delete unneeded items, merge only when none are left open)
- [ ] Added changelog entry (create a `$PR_NUMBER.md` file in
`unreleased_changes` or use `./tools/create-changelog-entry.py`)
- [ ] Added migration guide entry if applicable (edit the same file as
for the changelog)
- [ ] Updated [documentation](../blob/master/docs) if applicable
- [ ] Adapted [wk-libs python
client](https://github.com/scalableminds/webknossos-libs/tree/master/webknossos/webknossos/client)
if relevant API parts change
- [ ] Removed dev-only changes like prints and application.conf edits
- [ ] Considered [common edge
cases](../blob/master/.github/common_edge_cases.md)
- [ ] Needs datastore update after deployment

frontend/javascripts/viewer/view/right-border-tabs/segments_tab/segment_list_item.tsx
@@ -281,7 +281,7 @@ function _MeshInfoItem(props: {
           if (!props.visibleSegmentationLayer) {
             return;
           }
-          Store.dispatch(
+          dispatch(
             triggerMeshDownloadAction(
               segment.name ? segment.name : "mesh",
               segment.id,
@@ -292,6 +292,7 @@ function _MeshInfoItem(props: {
       />
     </FastTooltip>
   );
+
   const deleteButton = (
     <FastTooltip title="Remove Mesh">
       <DeleteOutlined
@@ -301,11 +302,12 @@ function _MeshInfoItem(props: {
             return;
           }
 
-          Store.dispatch(removeMeshAction(props.visibleSegmentationLayer.name, segment.id));
+          dispatch(removeMeshAction(props.visibleSegmentationLayer.name, segment.id));
         }}
       />
     </FastTooltip>
   );
+
   const toggleVisibilityCheckbox = (
     <FastTooltip title="Change visibility of mesh">
       <Checkbox
@@ -325,6 +327,7 @@ function _MeshInfoItem(props: {
     </FastTooltip>
   );
   const actionVisibility = isLoading || isHovered ? "visible" : "hidden";
+
   return (
     <div
       style={{
@@ -363,7 +366,11 @@ function _MeshInfoItem(props: {
             marginLeft: 6,
           }}
         >
-          {getRefreshButton(segment, isLoading, props.visibleSegmentationLayer)}
+          <RefreshButton
+            segment={segment}
+            isLoading={isHovered}
+            visibleSegmentationLayer={props.visibleSegmentationLayer}
+          />
           {downloadButton}
           {deleteButton}
         </div>
@@ -687,11 +694,16 @@ function _SegmentListItem({
 
 const SegmentListItem = React.memo<Props>(_SegmentListItem);
 
-function getRefreshButton(
-  segment: Segment,
-  isLoading: boolean,
-  visibleSegmentationLayer: APISegmentationLayer | null | undefined,
-) {
+const RefreshButton = ({
+  segment,
+  isLoading,
+  visibleSegmentationLayer,
+}: {
+  segment: Segment;
+  isLoading: boolean;
+  visibleSegmentationLayer: APISegmentationLayer | null | undefined;
+}) => {
+  const dispatch = useDispatch();
   if (isLoading) {
     return (
       <LoadingOutlined
@@ -701,27 +713,27 @@ function getRefreshButton(
             return;
           }
 
-          Store.dispatch(refreshMeshAction(visibleSegmentationLayer.name, segment.id));
+          dispatch(refreshMeshAction(visibleSegmentationLayer.name, segment.id));
         }}
       />
     );
-  } else {
-    return (
-      <FastTooltip title="Refresh Mesh">
-        <ReloadOutlined
-          key="refresh-button"
-          onClick={() => {
-            if (!visibleSegmentationLayer) {
-              return;
-            }
-
-            Store.dispatch(refreshMeshAction(visibleSegmentationLayer.name, segment.id));
-          }}
-        />
-      </FastTooltip>
-    );
   }
-}
+
+  return (
+    <FastTooltip title="Refresh Mesh">
+      <ReloadOutlined
+        key="refresh-button"
+        onClick={() => {
+          if (!visibleSegmentationLayer) {
+            return;
+          }
+
+          dispatch(refreshMeshAction(visibleSegmentationLayer.name, segment.id));
+        }}
+      />
+    </FastTooltip>
+  );
+};
 
 function getComputeMeshAdHocTooltipInfo(
   isForCenteredSegment: boolean,

frontend/javascripts/admin/account/account_profile_view.tsx
@@ -7,16 +7,18 @@ import { useWkSelector } from "libs/react_hooks";
 
 import { isUserAdmin, isUserTeamManager } from "libs/utils";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { getSystemColorTheme } from "theme";
 import type { APIUserTheme } from "types/api_types";
 import { formatUserName } from "viewer/model/accessors/user_accessor";
 import { setThemeAction } from "viewer/model/actions/ui_actions";
 import { setActiveUserAction } from "viewer/model/actions/user_actions";
-import Store from "viewer/store";
 import { SettingsCard, type SettingsCardProps } from "./helpers/settings_card";
 import { SettingsTitle } from "./helpers/settings_title";
 
 function AccountProfileView() {
+  const dispatch = useDispatch();
+
   const activeUser = useWkSelector((state) => state.activeUser);
   const activeOrganization = useWkSelector((state) => state.activeOrganization);
   const { selectedTheme } = activeUser || { selectedTheme: "auto" };
@@ -35,8 +37,8 @@ function AccountProfileView() {
 
     if (selectedTheme !== newTheme) {
       const newUser = await updateSelectedThemeOfUser(activeUser.id, newTheme);
-      Store.dispatch(setThemeAction(newTheme));
-      Store.dispatch(setActiveUserAction(newUser));
+      dispatch(setThemeAction(newTheme));
+      dispatch(setActiveUserAction(newUser));
     }
   };
 
@@ -68,7 +70,7 @@ function AccountProfileView() {
         <ChangeUsernameView
           user={activeUser}
           onClose={() => setChangeNameViewVisible(false)}
-          setEditedUser={(updatedUser) => Store.dispatch(setActiveUserAction(updatedUser))}
+          setEditedUser={(updatedUser) => dispatch(setActiveUserAction(updatedUser))}
         />
       ) : (
         formatUserName(activeUser, activeUser)

frontend/javascripts/admin/account/account_security_view.tsx
@@ -7,10 +7,10 @@ import messages from "messages";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { logoutUserAction } from "viewer/model/actions/user_actions";
-import Store from "viewer/store";
 import { SettingsTitle } from "./helpers/settings_title";
 const FormItem = Form.Item;
 const { Password } = Input;
+import { useDispatch } from "react-redux";
 import PasskeysView from "../auth/passkeys_view";
 import { SettingsCard, type SettingsCardProps } from "./helpers/settings_card";
 
@@ -19,8 +19,9 @@ const MIN_PASSWORD_LENGTH = 8;
 function AccountSecurityView() {
   const navigate = useNavigate();
   const [form] = Form.useForm();
-  const modal = App.useApp();
-  const confirm = modal.modal.confirm;
+  const { modal } = App.useApp();
+  const dispatch = useDispatch();
+
   const [isResetPasswordVisible, setResetPasswordVisible] = useState(false);
 
   async function onFinish(formValues: Record<string, any>) {
@@ -28,7 +29,7 @@ function AccountSecurityView() {
       await changePassword(formValues);
       Toast.success(messages["auth.reset_pw_confirmation"]);
       await logoutUserEverywhere();
-      Store.dispatch(logoutUserAction());
+      dispatch(logoutUserAction());
       navigate("/auth/login");
     } catch (error) {
       console.error("Password change failed:", error);
@@ -155,7 +156,7 @@ function AccountSecurityView() {
   }
 
   const handleLogoutEverywhere = () => {
-    confirm({
+    modal.confirm({
       title: "Confirm Logout",
       content: <p>Are you sure you want to log out on all devices?</p>,
       onOk: handleLogout,
@@ -192,7 +193,7 @@ function AccountSecurityView() {
   async function handleLogout() {
     logoutUserEverywhere()
       .then(() => {
-        Store.dispatch(logoutUserAction());
+        dispatch(logoutUserAction());
         navigate("/auth/login");
       })
       .catch((error) => {

frontend/javascripts/admin/auth/change_email_view.tsx
@@ -4,8 +4,8 @@ import { Alert, Button, Form, Input, Space } from "antd";
 import { useWkSelector } from "libs/react_hooks";
 import Toast from "libs/toast";
 import { logoutUserAction } from "viewer/model/actions/user_actions";
-import { Store } from "viewer/singletons";
 
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { handleResendVerificationEmail } from "./verify_email_view";
 
@@ -17,8 +17,10 @@ const PASSWORD_FIELD_KEY = "password";
 
 function ChangeEmailView({ onCancel }: { onCancel: () => void }) {
   const [form] = Form.useForm();
-  const activeUser = useWkSelector((state) => state.activeUser);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const activeUser = useWkSelector((state) => state.activeUser);
 
   async function changeEmail(newEmail: string, password: string) {
     const newUser = Object.assign({}, activeUser, {
@@ -37,7 +39,7 @@ function ChangeEmailView({ onCancel }: { onCancel: () => void }) {
         handleResendVerificationEmail();
         Toast.success("Email address changed successfully. You will be logged out.");
         await logoutUserEverywhere();
-        Store.dispatch(logoutUserAction());
+        dispatch(logoutUserAction());
         navigate("/auth/login");
       })
       .catch((error) => {

frontend/javascripts/admin/auth/login_form.tsx
@@ -6,10 +6,10 @@ import LinkButton from "components/link_button";
 import features from "features";
 import { getIsInIframe } from "libs/utils";
 import messages from "messages";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { setActiveOrganizationAction } from "viewer/model/actions/organization_actions";
 import { setActiveUserAction } from "viewer/model/actions/user_actions";
-import Store from "viewer/store";
 
 const FormItem = Form.Item;
 const { Password } = Input;
@@ -27,12 +27,13 @@ const DEFAULT_STYLE = {
 
 function LoginForm({ layout, onLoggedIn, hideFooter, style }: Props) {
   const [form] = Form.useForm();
+  const dispatch = useDispatch();
 
   // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'formValues' implicitly has an 'any' typ... Remove this comment to see the full error message
   const onFinish = async (formValues) => {
     const [user, organization] = await loginUser(formValues);
-    Store.dispatch(setActiveUserAction(user));
-    Store.dispatch(setActiveOrganizationAction(organization));
+    dispatch(setActiveUserAction(user));
+    dispatch(setActiveOrganizationAction(organization));
 
     if (onLoggedIn) {
       onLoggedIn();
@@ -43,8 +44,8 @@ function LoginForm({ layout, onLoggedIn, hideFooter, style }: Props) {
   const webauthnLogin = async () => {
     try {
       const [user, organization] = await doWebAuthnLogin();
-      Store.dispatch(setActiveUserAction(user));
-      Store.dispatch(setActiveOrganizationAction(organization));
+      dispatch(setActiveUserAction(user));
+      dispatch(setActiveOrganizationAction(organization));
       if (onLoggedIn) {
         onLoggedIn();
       }

frontend/javascripts/admin/auth/registration_form_generic.tsx
@@ -6,11 +6,11 @@ import { useFetch } from "libs/react_helpers";
 import Request from "libs/request";
 import messages from "messages";
 import React from "react";
+import { useDispatch } from "react-redux";
 import type { APIOrganization } from "types/api_types";
 import { setActiveOrganizationAction } from "viewer/model/actions/organization_actions";
 import { setHasOrganizationsAction } from "viewer/model/actions/ui_actions";
 import { setActiveUserAction } from "viewer/model/actions/user_actions";
-import Store from "viewer/throttled_store";
 import { TOSCheckFormItem } from "./tos_check_form_item";
 
 const FormItem = Form.Item;
@@ -28,6 +28,7 @@ type Props = {
 
 function RegistrationFormGeneric(props: Props) {
   const [form] = Form.useForm();
+  const dispatch = useDispatch();
 
   const terms = useFetch(getTermsOfService, null, []);
 
@@ -44,7 +45,7 @@ function RegistrationFormGeneric(props: Props) {
         },
       },
     );
-    Store.dispatch(setHasOrganizationsAction(true));
+    dispatch(setHasOrganizationsAction(true));
     const { targetOrganization } = props;
     const autoVerified = targetOrganization != null ? targetOrganization.enableAutoVerify : false;
     const tryAutoLogin = props.tryAutoLogin || props.inviteToken != null || autoVerified;
@@ -54,8 +55,8 @@ function RegistrationFormGeneric(props: Props) {
         email: formValues.email,
         password: formValues.password.password1,
       });
-      Store.dispatch(setActiveUserAction(user));
-      Store.dispatch(setActiveOrganizationAction(organization));
+      dispatch(setActiveUserAction(user));
+      dispatch(setActiveOrganizationAction(organization));
     }
 
     props.onRegistered(tryAutoLogin);

frontend/javascripts/admin/auth/registration_form_wkorg.tsx
@@ -6,9 +6,9 @@ import { useFetch } from "libs/react_helpers";
 import Request from "libs/request";
 import messages from "messages";
 import { memo, useRef } from "react";
+import { useDispatch } from "react-redux";
 import { setActiveOrganizationAction } from "viewer/model/actions/organization_actions";
 import { setActiveUserAction } from "viewer/model/actions/user_actions";
-import Store from "viewer/throttled_store";
 import { TOSCheckFormItem } from "./tos_check_form_item";
 
 const FormItem = Form.Item;
@@ -31,6 +31,7 @@ function generateOrganizationId() {
 }
 
 function RegistrationFormWKOrg(props: Props) {
+  const dispatch = useDispatch();
   const [form] = Form.useForm();
   const organizationId = useRef(generateOrganizationId());
   const terms = useFetch(getTermsOfService, null, []);
@@ -54,8 +55,8 @@ function RegistrationFormWKOrg(props: Props) {
       email: formValues.email,
       password: formValues.password.password1,
     });
-    Store.dispatch(setActiveUserAction(user));
-    Store.dispatch(setActiveOrganizationAction(organization));
+    dispatch(setActiveUserAction(user));
+    dispatch(setActiveOrganizationAction(organization));
     props.onRegistered(true);
   }
 

frontend/javascripts/admin/organization/organization_notifications_view.tsx
@@ -7,9 +7,9 @@ import { Button, Col, Form, Input, Row } from "antd";
 import { useWkSelector } from "libs/react_hooks";
 import Toast from "libs/toast";
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { enforceActiveOrganization } from "viewer/model/accessors/organization_accessors";
 import { setActiveOrganizationAction } from "viewer/model/actions/organization_actions";
-import { Store } from "viewer/singletons";
 
 const FormItem = Form.Item;
 
@@ -19,6 +19,7 @@ type FormValues = {
 };
 
 export function OrganizationNotificationsView() {
+  const dispatch = useDispatch();
   const organization = useWkSelector((state) =>
     enforceActiveOrganization(state.activeOrganization),
   );
@@ -44,7 +45,7 @@ export function OrganizationNotificationsView() {
       organization.name,
       formValues.newUserMailingList,
     );
-    Store.dispatch(setActiveOrganizationAction(updatedOrganization));
+    dispatch(setActiveOrganizationAction(updatedOrganization));
     Toast.success("Notification settings were saved successfully.");
   }
 

commit f00eed116a3974145ab97a6cec983017eb037aba

Fix that meshes were always shown as loading when hovered (#9284)
### Steps to test:
- load a mesh and check loading state 

### Issues:
- fixes regression introduced in
https://github.com/scalableminds/webknossos/commit/c98fd0c4edae6b21346f695478031e55eb9c9742#diff-f8291d3c061733c70d608636e1a1b591b5b4d3a3ed6607ebc97a75703628f8bbR369

------
(Please delete unneeded items, merge only when none are left open)
- [x] Added changelog entry (create a `$PR_NUMBER.md` file in
`unreleased_changes` or use `./tools/create-changelog-entry.py`)

frontend/javascripts/viewer/view/right-border-tabs/segments_tab/segment_list_item.tsx
@@ -366,7 +366,7 @@ function _MeshInfoItem(props: {
         >
           <RefreshButton
             segment={segment}
-            isLoading={isHovered}
+            isLoading={isLoading}
             visibleSegmentationLayer={props.visibleSegmentationLayer}
           />
           {downloadButton}
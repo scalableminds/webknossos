scalableminds/webknossos

commit 35928f058c4adc3dc522f249b4ad163dab767c3b

Fix setting initial teams during publish if no write access via folder/admin (#9306)
This line should have GlobalAccessContext permissions, same as the one
above and below.

We didn’t see this error before, presumably because the requesting users
had permissions in the folder anyway.

### Steps to test:
- Publish dataset as non-orga-admin
- should still work

### Issues:
- fixes https://scm.slack.com/archives/CMBMU5684/p1771332622649169

app/models/dataset/DatasetUploadToPathsService.scala
@@ -84,7 +84,7 @@ class DatasetUploadToPathsService @Inject()(datasetService: DatasetService,
       )
       _ <- datasetDAO.updateFolder(newDatasetId, parameters.folderId.getOrElse(organization._rootFolder))(
         GlobalAccessContext)
-      _ <- datasetService.addInitialTeams(dataset, parameters.initialTeamIds, requestingUser)
+      _ <- datasetService.addInitialTeams(dataset, parameters.initialTeamIds, requestingUser)(GlobalAccessContext)
       _ <- datasetService.addUploader(dataset, requestingUser._id)(GlobalAccessContext)
     } // Note: not returning the one with layersToLink. Those are managed by the server entirely, so the client doesn’t need their paths.
     yield dataSourceWithPaths

commit b01b9637547889f5cbfe2a4e6798959d2d0cf58e

Fix publish regression + Better error message when trying to publish in folder user has no write access in (#9308)
- removes the GlobalAccessContext from addInitialTeams (reverting
regression introduced earlier today in #9306)
- more explicit error message when trying to publish in folder user has
no write access in

### Steps to test:
- As a user with no write access in a folder, try to publish
- should get readable error message
- should be able to publish to folder you do have write access in.

### Issues:
- fixes
https://scm.slack.com/archives/C5AKLAV0B/p1771344557006389?thread_ts=1771344185.149199&cid=C5AKLAV0B

---------

Co-authored-by: MichaelBuessemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>

app/models/dataset/DatasetUploadToPathsService.scala
@@ -29,6 +29,7 @@ import controllers.{
   ReserveDatasetUploadToPathsRequest,
   ReserveMagUploadToPathRequest
 }
+import models.folder.FolderDAO
 import models.organization.OrganizationDAO
 import models.user.User
 import play.api.i18n.MessagesProvider
@@ -46,6 +47,7 @@ class DatasetUploadToPathsService @Inject()(datasetService: DatasetService,
                                             pathDeletionService: PathDeletionService,
                                             datasetLayerAttachmentsDAO: DatasetLayerAttachmentsDAO,
                                             datasetMagsDAO: DatasetMagsDAO,
+                                            folderDAO: FolderDAO,
                                             conf: WkConf)
     extends FoxImplicits
     with DataSourceValidation {
@@ -73,6 +75,8 @@ class DatasetUploadToPathsService @Inject()(datasetService: DatasetService,
       dataSourceWithLayersToLink <- layerToLinkService.addLayersToLinkToDataSource(dataSourceWithPaths,
                                                                                    parameters.layersToLink)
       _ <- assertValidDataSource(dataSourceWithLayersToLink).toFox
+      folderIdWithFallback = parameters.folderId.getOrElse(organization._rootFolder)
+      _ <- folderDAO.assertUpdateAccess(folderIdWithFallback) ?~> "folder.noWriteAccess"
       dataStore <- findReferencedDataStore(parameters.layersToLink)
       dataset <- datasetService.createDataset(
         dataStore,
@@ -84,7 +88,7 @@ class DatasetUploadToPathsService @Inject()(datasetService: DatasetService,
       )
       _ <- datasetDAO.updateFolder(newDatasetId, parameters.folderId.getOrElse(organization._rootFolder))(
         GlobalAccessContext)
-      _ <- datasetService.addInitialTeams(dataset, parameters.initialTeamIds, requestingUser)(GlobalAccessContext)
+      _ <- datasetService.addInitialTeams(dataset, parameters.initialTeamIds, requestingUser) // called with user access context. Should be fine now that the folder is set correctly
       _ <- datasetService.addUploader(dataset, requestingUser._id)(GlobalAccessContext)
     } // Note: not returning the one with layersToLink. Those are managed by the server entirely, so the client doesn’t need their paths.
     yield dataSourceWithPaths
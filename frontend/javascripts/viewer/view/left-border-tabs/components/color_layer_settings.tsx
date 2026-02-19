169ad82690a0a9f6dcb97b481c700a0759a8f88d
Tom Herold <tom.herold@scalableminds.com>
2026-02-19 15:56:29 +0100

    split layer settings view into sub components

---
+++ frontend/javascripts/viewer/view/left-border-tabs/components/color_layer_settings.tsx
@@ -0,0 +1,109 @@
+import classnames from "classnames";
+import { Col, Row } from "antd";
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { rgbToHex } from "libs/utils";
+import { getDefaultLayerViewConfiguration } from "types/schemas/dataset_view_configuration.schema";
+import ButtonComponent from "viewer/view/components/button_component";
+import {
+  ColorSetting,
+  LogSliderSetting,
+  SETTING_LEFT_SPAN,
+  SETTING_MIDDLE_SPAN,
+  SETTING_VALUE_SPAN,
+} from "viewer/view/components/setting_input_views";
+import { layerViewConfigurations, layerViewConfigurationTooltips } from "messages";
+import FastTooltip from "components/fast_tooltip";
+import { updateLayerSettingAction } from "viewer/model/actions/settings_actions";
+import type { DatasetLayerConfiguration } from "viewer/store";
+
+export default function ColorLayerSettings({
+  layerName,
+  layerConfiguration,
+}: {
+  layerName: string;
+  layerConfiguration: DatasetLayerConfiguration;
+}) {
+  const dispatch = useDispatch();
+  const defaultSettings = getDefaultLayerViewConfiguration();
+
+  const onChangeLayer = useCallback(
+    (propertyName: keyof DatasetLayerConfiguration, value: any) => {
+      dispatch(updateLayerSettingAction(layerName, propertyName, value));
+    },
+    [dispatch, layerName],
+  );
+
+  const onChangeGamma = useCallback(
+    (value: number) => onChangeLayer("gammaCorrectionValue", value),
+    [onChangeLayer],
+  );
+
+  const onChangeColor = useCallback((value: any) => onChangeLayer("color", value), [onChangeLayer]);
+
+  const onToggleInvert = useCallback(
+    () => onChangeLayer("isInverted", layerConfiguration ? !layerConfiguration.isInverted : false),
+    [onChangeLayer, layerConfiguration],
+  );
+
+  return (
+    <div>
+      <LogSliderSetting
+        label={
+          <FastTooltip title={layerViewConfigurationTooltips.gammaCorrectionValue}>
+            {layerViewConfigurations.gammaCorrectionValue}
+          </FastTooltip>
+        }
+        min={0.01}
+        max={10}
+        roundToDigit={3}
+        value={layerConfiguration.gammaCorrectionValue}
+        onChange={onChangeGamma}
+        defaultValue={defaultSettings.gammaCorrectionValue}
+      />
+      <Row
+        style={{
+          marginBottom: "var(--ant-margin-sm)",
+          marginTop: 6,
+        }}
+      >
+        <Col span={SETTING_LEFT_SPAN}>
+          <label className="setting-label">Color</label>
+        </Col>
+        <Col span={SETTING_MIDDLE_SPAN}>
+          <ColorSetting
+            value={rgbToHex(layerConfiguration.color)}
+            onChange={onChangeColor}
+            style={{
+              marginLeft: 6,
+            }}
+          />
+        </Col>
+        <Col span={SETTING_VALUE_SPAN}>
+          <ButtonComponent
+            variant="text"
+            color="default"
+            size="small"
+            title="Invert the color of this layer."
+            onClick={onToggleInvert}
+            icon={
+              <i
+                className={classnames("fas", "fa-adjust", {
+                  "flip-horizontally": layerConfiguration.isInverted,
+                })}
+                style={{
+                  margin: 0,
+                  transition: "transform 0.5s ease 0s",
+                  color: layerConfiguration.isInverted ? "var(--ant-color-primary)" : undefined,
+                }}
+              />
+            }
+            style={{
+              marginLeft: 10,
+            }}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+}
---
+++ frontend/javascripts/viewer/view/left-border-tabs/components/segmentation_layer_settings.tsx
@@ -0,0 +1,37 @@
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { useWkSelector } from "libs/react_hooks";
+import { settings } from "messages";
+import { defaultDatasetViewConfiguration } from "types/schemas/dataset_view_configuration.schema";
+import { updateDatasetSettingAction } from "viewer/model/actions/settings_actions";
+import { NumberSliderSetting } from "viewer/view/components/setting_input_views";
+import { HideUnregisteredSegmentsSwitch } from "../hide_unregistered_segments_switch";
+import MappingSettingsView from "../mapping_settings_view";
+
+export default function SegmentationLayerSettings({ layerName }: { layerName: string }) {
+  const dispatch = useDispatch();
+  const segmentationPatternOpacity = useWkSelector(
+    (state) => state.datasetConfiguration.segmentationPatternOpacity,
+  );
+
+  const onChangeOpacity = useCallback(
+    (value: number) => dispatch(updateDatasetSettingAction("segmentationPatternOpacity", value)),
+    [dispatch],
+  );
+
+  return (
+    <div>
+      <NumberSliderSetting
+        label={settings.segmentationPatternOpacity}
+        min={0}
+        max={100}
+        step={1}
+        value={segmentationPatternOpacity}
+        onChange={onChangeOpacity}
+        defaultValue={defaultDatasetViewConfiguration.segmentationPatternOpacity}
+      />
+      <HideUnregisteredSegmentsSwitch layerName={layerName} />
+      <MappingSettingsView layerName={layerName} />
+    </div>
+  );
+}

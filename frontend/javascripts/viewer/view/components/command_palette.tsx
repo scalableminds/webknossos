9a0c956ab5b258628af152495d553380f0d9bde4
Charlie Meister <charlie.meister@student.hpi.de>
2026-03-02 17:31:15 +0100

    improve naming

--- frontend/javascripts/viewer/view/components/command_palette.tsx
+++ frontend/javascripts/viewer/view/components/command_palette.tsx
@@ -222,7 +222,7 @@ export const CommandPalette = ({ label }: { label: string | JSX.Element | null }
     color: commandEntryColor,
   };
 
-  const getAuthorizationCommands = () => {
+  const getCopyTokenOrIDCommands = () => {
     if (activeUser == null) return [];
     return [
       {
@@ -404,7 +404,7 @@ export const CommandPalette = ({ label }: { label: string | JSX.Element | null }
     ...mapMenuActionsToCommands(menuActions),
     ...getTabsAndSettingsMenuItems(),
     ...getSuperUserItems(),
-    ...getAuthorizationCommands(),
+    ...getCopyTokenOrIDCommands(),
   ];
 
   const [commands, setCommands] = useState<CommandWithoutId[]>(allStaticCommands);

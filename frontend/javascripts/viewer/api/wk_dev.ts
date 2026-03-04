64341825ee29f15ca003604fae79c54775e4d47a
Philipp Otto <philipp.4096@gmail.com>
2026-03-04 16:51:30 +0100

    change liveCollab back to false

--- frontend/javascripts/viewer/api/wk_dev.ts
+++ frontend/javascripts/viewer/api/wk_dev.ts
@@ -12,7 +12,7 @@ import type ApiLoader from "./api_loader";
 // for debugging or one off scripts.
 export const WkDevFlags = {
   logActions: false,
-  liveCollab: true, // todop: revert
+  liveCollab: false,
   sam: {
     useLocalMask: true,
   },

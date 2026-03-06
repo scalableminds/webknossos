101a218f358a6601b8f38fd1b08e33fc6f49e9e7
Tom Herold <tom.herold@scalableminds.com>
2026-03-06 09:48:16 +0100

    fix icons colors

--- frontend/javascripts/viewer/view/layouting/floating_mobile_controls.tsx
+++ frontend/javascripts/viewer/view/layouting/floating_mobile_controls.tsx
@@ -1,10 +1,11 @@
 import { CaretDownOutlined, CaretUpOutlined, ExpandAltOutlined } from "@ant-design/icons";
-import { Space, Tooltip } from "antd";
+import { ConfigProvider, Space, Tooltip } from "antd";
 import { ThemedIcon } from "components/themed_icon";
 import { useRepeatedButtonTrigger, useWkSelector } from "libs/react_hooks";
 import type * as React from "react";
 import { useCallback } from "react";
 import { useDispatch } from "react-redux";
+import { getAntdTheme } from "theme";
 import { OrthoViews, OrthoViewsToName } from "viewer/constants";
 import { moveW } from "viewer/controller/combinations/move_handlers";
 import { getMoveOffset, getMoveOffset3d } from "viewer/model/accessors/flycam_accessor";
@@ -45,76 +46,79 @@ export function FloatingMobileControls() {
   const handleContextMenu = (event: React.SyntheticEvent) => {
     event.preventDefault();
   };
+  const lightTheme = getAntdTheme("light");
 
   return (
-    <div
-      className="floating-buttons-bar"
-      style={{ position: "fixed", left: 8, bottom: 28, zIndex: 1000 }}
-      onContextMenu={handleContextMenu}
-    >
-      <Space>
-        <ButtonComponent
-          size="large"
-          type="primary"
-          shape="circle"
-          style={BUTTON_STYLE}
-          onClick={() => layoutEmitter.emit(LayoutEvents.toggleBorder, "left")}
-          icon={
-            <ThemedIcon
-              name="icon-sidebar-hide-left"
-              aria-label="Toggle left sidebar"
-              style={{ filter: "brightness(10)" }}
-            />
-          }
-        />
-        <ButtonComponent
-          size="large"
-          type="primary"
-          shape="circle"
-          style={BUTTON_STYLE}
-          onClick={() => layoutEmitter.emit(LayoutEvents.toggleBorder, "right")}
-          icon={
-            <ThemedIcon
-              name="icon-sidebar-hide-right"
-              aria-label="Toggle right sidebar"
-              style={{ filter: "brightness(10)" }}
-            />
-          }
-        />
-        <ButtonComponent
-          size="large"
-          type="primary"
-          shape="circle"
-          style={BUTTON_STYLE}
-          disabled={activeViewport === OrthoViews.TDView}
-          icon={<CaretUpOutlined />}
-          {...moveForwardProps}
-        />
-        <ButtonComponent
-          size="large"
-          type="primary"
-          shape="circle"
-          style={BUTTON_STYLE}
-          disabled={activeViewport === OrthoViews.TDView}
-          icon={<CaretDownOutlined />}
-          {...moveBackwardProps}
-        />
-        <ButtonComponent
-          size="large"
-          type="primary"
-          shape="circle"
-          style={BUTTON_STYLE}
-          onClick={() => layoutEmitter.emit(LayoutEvents.toggleMaximize)}
-          icon={<ExpandAltOutlined />}
-        />
-        {viewMode === "orthogonal" && (
-          <Tooltip title="The navigation and maximization button refers to the active viewport. A viewport can be activated by tapping on it.">
-            <ButtonComponent size="large" shape="circle" style={BUTTON_STYLE}>
-              {OrthoViewsToName[activeViewport]}
-            </ButtonComponent>
-          </Tooltip>
-        )}
-      </Space>
-    </div>
+    <ConfigProvider theme={lightTheme}>
+      <div
+        className="floating-buttons-bar"
+        style={{ position: "fixed", left: 8, bottom: 28, zIndex: 1000 }}
+        onContextMenu={handleContextMenu}
+      >
+        <Space>
+          <ButtonComponent
+            size="large"
+            type="primary"
+            shape="circle"
+            style={BUTTON_STYLE}
+            onClick={() => layoutEmitter.emit(LayoutEvents.toggleBorder, "left")}
+            icon={
+              <ThemedIcon
+                name="icon-sidebar-hide-left"
+                aria-label="Toggle left sidebar"
+                style={{ filter: "brightness(10)" }}
+              />
+            }
+          />
+          <ButtonComponent
+            size="large"
+            type="primary"
+            shape="circle"
+            style={BUTTON_STYLE}
+            onClick={() => layoutEmitter.emit(LayoutEvents.toggleBorder, "right")}
+            icon={
+              <ThemedIcon
+                name="icon-sidebar-hide-right"
+                aria-label="Toggle right sidebar"
+                style={{ filter: "brightness(10)" }}
+              />
+            }
+          />
+          <ButtonComponent
+            size="large"
+            type="primary"
+            shape="circle"
+            style={BUTTON_STYLE}
+            disabled={activeViewport === OrthoViews.TDView}
+            icon={<CaretUpOutlined />}
+            {...moveForwardProps}
+          />
+          <ButtonComponent
+            size="large"
+            type="primary"
+            shape="circle"
+            style={BUTTON_STYLE}
+            disabled={activeViewport === OrthoViews.TDView}
+            icon={<CaretDownOutlined />}
+            {...moveBackwardProps}
+          />
+          <ButtonComponent
+            size="large"
+            type="primary"
+            shape="circle"
+            style={BUTTON_STYLE}
+            onClick={() => layoutEmitter.emit(LayoutEvents.toggleMaximize)}
+            icon={<ExpandAltOutlined />}
+          />
+          {viewMode === "orthogonal" && (
+            <Tooltip title="The navigation and maximization button refers to the active viewport. A viewport can be activated by tapping on it.">
+              <ButtonComponent size="large" shape="circle" style={BUTTON_STYLE}>
+                {OrthoViewsToName[activeViewport]}
+              </ButtonComponent>
+            </Tooltip>
+          )}
+        </Space>
+      </div>
+    </ConfigProvider>
   );
 }

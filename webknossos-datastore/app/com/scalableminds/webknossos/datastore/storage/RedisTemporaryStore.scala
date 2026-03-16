ea6616c36811fc4afcbca901a9760fc0f62fb3d2
Florian M <florian@scm.io>
2026-03-16 16:15:38 +0100

    WIP Allow uploading mags, attachments to existing datasets

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/storage/RedisTemporaryStore.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/storage/RedisTemporaryStore.scala
@@ -12,119 +12,89 @@ trait RedisTemporaryStore extends LazyLogging {
   protected def address: String
   protected def port: Int
   lazy val authority: String = f"$address:$port"
-  private lazy val r = new RedisClient(address, port)
+  private lazy val r = new RedisClientPool(address, port)
 
   def find(id: String): Fox[Option[String]] =
-    withExceptionHandler {
-      r.get(id)
-    }
+    withExceptionHandler(_.get(id))
 
   def findLong(id: String): Fox[Option[Long]] =
-    withExceptionHandler {
-      r.get(id).map(s => s.toLong)
-    }
+    withExceptionHandler(_.get(id).map(s => s.toLong))
 
   def removeAllConditional(pattern: String): Fox[Unit] =
-    withExceptionHandler {
-      val keysOpt: Option[List[Option[String]]] = r.keys(pattern)
+    withExceptionHandler { client =>
+      val keysOpt: Option[List[Option[String]]] = client.keys(pattern)
       keysOpt.foreach { keys: Seq[Option[String]] =>
         keys.flatMap { key: Option[String] =>
-          key.flatMap(r.del(_))
+          key.flatMap(client.del(_))
         }
       }
     }
 
   def findAllConditional(pattern: String): Fox[Seq[String]] =
-    withExceptionHandler {
-      val keysOpt: Option[List[Option[String]]] = r.keys(pattern)
+    withExceptionHandler { client =>
+      val keysOpt: Option[List[Option[String]]] = client.keys(pattern)
       keysOpt.map { keys: Seq[Option[String]] =>
         keys.flatMap { key: Option[String] =>
-          key.flatMap(r.get(_))
+          key.flatMap(client.get(_))
         }
       }.getOrElse(Seq())
     }
 
   def keys(pattern: String): Fox[List[String]] =
-    withExceptionHandler {
-      r.keys(pattern).map(_.flatten).getOrElse(List())
-    }
+    withExceptionHandler(_.keys(pattern).map(_.flatten).getOrElse(List()))
 
   def insertKey(id: String, expirationOpt: Option[FiniteDuration] = None): Fox[Unit] =
     insert(id, "", expirationOpt)
 
   def insert(id: String, value: String, expirationOpt: Option[FiniteDuration] = None): Fox[Unit] =
-    withExceptionHandler {
+    withExceptionHandler { client =>
       expirationOpt
-        .map(
-          expiration => r.setex(id, expiration.toSeconds, value)
-        )
-        .getOrElse(
-          r.set(id, value)
-        )
+        .map(expiration => client.setex(id, expiration.toSeconds, value))
+        .getOrElse(client.set(id, value))
     }
 
   def insertLong(id: String, value: Long, expirationOpt: Option[FiniteDuration] = None): Fox[Unit] =
-    withExceptionHandler {
+    withExceptionHandler { client =>
       expirationOpt
-        .map(
-          expiration => r.setex(id, expiration.toSeconds, value)
-        )
-        .getOrElse(
-          r.set(id, value)
-        )
+        .map(expiration => client.setex(id, expiration.toSeconds, value))
+        .getOrElse(client.set(id, value))
     }
 
   def contains(id: String): Fox[Boolean] =
-    withExceptionHandler {
-      r.exists(id)
-    }
+    withExceptionHandler(_.exists(id))
 
   def remove(id: String): Fox[Unit] =
-    withExceptionHandler {
-      r.del(id)
-    }
+    withExceptionHandler(_.del(id))
 
   def checkHealth(implicit ec: ExecutionContext): Fox[Unit] =
-    try {
-      val reply = r.ping
+    withExceptionHandler { client =>
+      val reply = client.ping
       if (!reply.contains("PONG")) throw new Exception(reply.getOrElse("No Reply"))
-      Fox.successful(())
-    } catch {
-      case e: Exception =>
-        logger.error(s"Redis health check failed at $address:$port (reply: ${e.getMessage})")
-        Fox.failure(s"Redis health check failed")
+      ()
     }
 
-  def withExceptionHandler[B](f: => B): Fox[B] =
+  def insertIntoSet(id: String, value: String): Fox[Boolean] =
+    withExceptionHandler(_.sadd(id, value).getOrElse(0L) > 0)
+
+  def isContainedInSet(id: String, value: String): Fox[Boolean] =
+    withExceptionHandler(_.sismember(id, value))
+
+  def removeFromSet(id: String, value: String): Fox[Boolean] =
+    withExceptionHandler(_.srem(id, value).getOrElse(0L) > 0)
+
+  def findSet(id: String): Fox[Set[String]] =
+    withExceptionHandler(_.smembers(id).map(_.flatten).getOrElse(Set.empty))
+
+  private def withExceptionHandler[B](f: RedisClient => B): Fox[B] =
     try {
-      r.synchronized {
-        Fox.successful(f)
+      r.withClient { client =>
+        Fox.successful(f(client))
       }
     } catch {
       case e: Exception =>
         val msg = "Redis access exception: " + e.getMessage
         logger.error(msg)
         Fox.failure(msg)
-    }
-
-  def insertIntoSet(id: String, value: String): Fox[Boolean] =
-    withExceptionHandler {
-      r.sadd(id, value).getOrElse(0L) > 0
-    }
-
-  def isContainedInSet(id: String, value: String): Fox[Boolean] =
-    withExceptionHandler {
-      r.sismember(id, value)
-    }
-
-  def removeFromSet(id: String, value: String): Fox[Boolean] =
-    withExceptionHandler {
-      r.srem(id, value).getOrElse(0L) > 0
-    }
-
-  def findSet(id: String): Fox[Set[String]] =
-    withExceptionHandler {
-      r.smembers(id).map(_.flatten).getOrElse(Set.empty)
     }
 
 }

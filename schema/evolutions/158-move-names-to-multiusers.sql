05fe09ddfbadd69a5456d3ef70ea288a6a3a296e
Florian M <fm3@users.noreply.github.com>
2026-03-18 13:12:21 +0100

    Move firstName,lastName to MulitUser (#9379)
    All users belonging to a MultiUser already had to have the same name.
    The logical cleanup is to move these fields to the MultiUser directly.
    
    Also fixed gitignore for type_check/ files, as mentioned in
    https://scm.slack.com/archives/C5AKLAV0B/p1773329610452609
    
    ### URL of deployed dev instance (used for testing):
    - https://multiusername.webknossos.xyz
    
    ### Steps to test:
    - Annotate some, download, user name should be in NML (and abbreviated
    in filename unless explicitly named via UI)
    - Run vx jobs (e.g. via worker), workflow list should show correct user
    name
    - Users should still be able to change their own name
    - User names should still be in annotation list view
    
    ### Issues:
    - fixes #9105
    
    ------
    - [x] Added migration guide entry if applicable (edit the same file as
    for the changelog)
    - [x] Considered [common edge
    cases](../blob/master/.github/common_edge_cases.md)

---
+++ schema/evolutions/158-move-names-to-multiusers.sql
@@ -0,0 +1,38 @@
+START TRANSACTION;
+
+do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 157 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
+
+DROP VIEW webknossos.userInfos;
+DROP VIEW webknossos.multiUsers_;
+DROP VIEW webknossos.users_;
+
+ALTER TABLE webknossos.multiUsers ADD COLUMN firstName TEXT;
+ALTER TABLE webknossos.multiUsers ADD COLUMN lastName TEXT;
+
+UPDATE webknossos.multiUsers m
+SET firstName = u.firstName, lastName = u.lastName
+FROM webknossos.users u
+WHERE u._multiUser = m._id;
+
+ALTER TABLE webknossos.multiUsers ALTER COLUMN firstName SET NOT NULL;
+ALTER TABLE webknossos.multiUsers ALTER COLUMN lastName SET NOT NULL;
+
+ALTER TABLE webknossos.users DROP COLUMN firstName;
+ALTER TABLE webknossos.users DROP COLUMN lastName;
+
+CREATE VIEW webknossos.multiUsers_ AS SELECT * FROM webknossos.multiUsers WHERE NOT isDeleted;
+CREATE VIEW webknossos.users_ AS SELECT * FROM webknossos.users WHERE NOT isDeleted;
+
+CREATE VIEW webknossos.userInfos AS
+SELECT
+u._id AS _user, m.email, m.firstName, m.lastName, o.name AS organization_name,
+u.isDeactivated, u.isDatasetManager, u.isAdmin, m.isSuperUser,
+u._organization, o._id AS organization_id, u.created AS user_created,
+m.created AS multiuser_created, u._multiUser, m._lastLoggedInIdentity, u.lastActivity, m.isEmailVerified
+FROM webknossos.users_ u
+JOIN webknossos.organizations_ o ON u._organization = o._id
+JOIN webknossos.multiUsers_ m on u._multiUser = m._id;
+
+UPDATE webknossos.releaseInformation SET schemaVersion = 158;
+
+COMMIT TRANSACTION;

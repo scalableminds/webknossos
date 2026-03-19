3fa84b909802d149881b45f2be9f327126879c28
Florian M <florian@scm.io>
2026-03-19 09:53:19 +0100

    bump schema version

--- schema/evolutions/159-inference-without-custom-model.sql
+++ schema/evolutions/159-inference-without-custom-model.sql
@@ -1,6 +1,6 @@
 START TRANSACTION;
 
-do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 157 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
+do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 158 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
 
 DROP VIEW webknossos.aiInferences_;
 
@@ -8,6 +8,6 @@ ALTER TABLE webknossos.aiInferences ALTER COLUMN _aiModel DROP NOT NULL;
 
 CREATE VIEW webknossos.aiInferences_ as SELECT * FROM webknossos.aiInferences WHERE NOT isDeleted;
 
-UPDATE webknossos.releaseInformation SET schemaVersion = 158;
+UPDATE webknossos.releaseInformation SET schemaVersion = 159;
 
 COMMIT TRANSACTION;
--- schema/evolutions/reversions/159-inference-without-custom-model.sql
+++ schema/evolutions/reversions/159-inference-without-custom-model.sql
@@ -1,6 +1,6 @@
 START TRANSACTION;
 
-do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 158 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
+do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 159 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
 
 DROP VIEW webknossos.aiInferences_;
 
@@ -10,6 +10,6 @@ ALTER TABLE webknossos.aiInferences ALTER COLUMN _aiModel SET NOT NULL;
 
 CREATE VIEW webknossos.aiInferences_ as SELECT * FROM webknossos.aiInferences WHERE NOT isDeleted;
 
-UPDATE webknossos.releaseInformation SET schemaVersion = 157;
+UPDATE webknossos.releaseInformation SET schemaVersion = 158;
 
 COMMIT TRANSACTION;

8bba63ad0ca7dab32f61327987a02da7a6022742
Charlie Meister <charliemeister@proton.me>
2026-03-23 16:30:50 +0100

    Handle Narrow Toolbar Better (#9351)
    This PR adds more aggressive breakpoints when the window width changes,
    e.g. to hide some menu entries. There was a redesign of the toolbar.
    Next to that, the extrude feature within the quickselect tool was
    removed.
    
    ### URL of deployed dev instance (used for testing):
    -
    https://toolbarredesign.webknossos.xyz/annotations/69af11ef01000030013b0b22#3584,3584,1024,0,1.3
    
    ### Steps to test:
    - Open any annotation
    - use a narrow browser window and have a look at the nav bar. 
    - resize the window and make sure the menu in the navbar changes, as
    well as other breakpoints (e.g. links to admin views)
    - test locally to have a look at the "AI Analysis" button
    - 
    
    ### TODOs:
    - More aggressive breakpoints
        - AI Analysis → AI → Icon Only :white_check_mark:
        - Menu → Hamburger button :white_check_mark:
    Hide Navigation Links including help :heavy_check_mark:
        - Slimmer padding/margin :heavy_check_mark:  (e.g. save button)
    - Move Toolkit as “Prefix” of toolbox :white_check_mark: 
    - Move Quick-Select-AI into settings icon :white_check_mark:
    - make “flight mode” a boolean switch in position input popover
    :white_check_mark:
    - Remove Extrude Feature :white_check_mark: 
    - ask in chat for veto
    https://scm.slack.com/archives/C5AKLAV0B/p1772644658435199
    - Move what’s new into avatar icon (but keep it sticky)
    :white_check_mark: (avatar should be sticky)
    - adjust changing the width ->
    https://github.com/scalableminds/webknossos/issues/5366#issuecomment-3996583191
    :white_check_mark:
    
    ### Issues:
    - contributes to #9263 
    - feedback thread:
    https://scm.slack.com/archives/C5AKLAV0B/p1773081243514139
    
    ------
    (Please delete unneeded items, merge only when none are left open)
    - [x] Added changelog entry (create a `$PR_NUMBER.md` file in
    `unreleased_changes` or use `./tools/create-changelog-entry.py`)
    - [x] Removed dev-only changes like prints and application.conf edits
    - [x] Considered [common edge
    cases](../blob/master/.github/common_edge_cases.md)

--- frontend/javascripts/viewer/view/action_bar/tools/toolbar_view.tsx
+++ frontend/javascripts/viewer/view/action_bar/tools/toolbar_view.tsx
@@ -1,5 +1,4 @@
 import Icon, { InfoCircleOutlined } from "@ant-design/icons";
-import AiAnalysisIcon from "@images/icons/icon-ai-analysis.svg?react";
 import AreaMeasurementIcon from "@images/icons/icon-area-measurement.svg?react";
 import NewBoundingBoxIcon from "@images/icons/icon-bounding-box-new.svg?react";
 import LineMeasurementIcon from "@images/icons/icon-line-measurement.svg?react";
@@ -20,9 +19,8 @@ import {
   VolumeTools,
 } from "viewer/model/accessors/tool_accessor";
 import { addUserBoundingBoxAction } from "viewer/model/actions/annotation_actions";
-import { updateUserSettingAction } from "viewer/model/actions/settings_actions";
 import { setToolAction } from "viewer/model/actions/ui_actions";
-import ButtonComponent, { ToggleButton } from "viewer/view/components/button_component";
+import ButtonComponent from "viewer/view/components/button_component";
 import { ChangeBrushSizePopover } from "./brush_presets";
 import { SkeletonSpecificButtons } from "./skeleton_specific_ui";
 import { ToolIdToComponent } from "./tool_buttons";
@@ -147,24 +145,7 @@ function ToolSpecificSettings({
     showCreateCellButton &&
     (adaptedActiveTool === AnnotationTool.BRUSH ||
       adaptedActiveTool === AnnotationTool.ERASE_BRUSH);
-  const dispatch = useDispatch();
-  const quickSelectConfig = useWkSelector((state) => state.userConfiguration.quickSelect);
-  const isAISelectAvailable = features().segmentAnythingEnabled;
-  const isQuickSelectHeuristic = quickSelectConfig.useHeuristic || !isAISelectAvailable;
-  const quickSelectTooltipText = isAISelectAvailable
-    ? isQuickSelectHeuristic
-      ? "The quick select tool is now working without AI. Activate AI for better results."
-      : "The quick select tool is now working with AI."
-    : "The quick select tool with AI is only available on webknossos.org";
   const areEditableMappingsEnabled = features().editableMappingsEnabled;
-  const toggleQuickSelectStrategy = () => {
-    dispatch(
-      updateUserSettingAction("quickSelect", {
-        ...quickSelectConfig,
-        useHeuristic: !quickSelectConfig.useHeuristic,
-      }),
-    );
-  };
 
   return (
     <>
@@ -189,25 +170,7 @@ function ToolSpecificSettings({
         visible={adaptedActiveTool.hasOverwriteCapabilities}
       />
 
-      {adaptedActiveTool === AnnotationTool.QUICK_SELECT && (
-        <div>
-          <ToggleButton
-            active={!isQuickSelectHeuristic}
-            style={{
-              ...NARROW_BUTTON_STYLE,
-              opacity: isQuickSelectHeuristic ? 0.5 : 1,
-            }}
-            onClick={toggleQuickSelectStrategy}
-            disabled={!isAISelectAvailable}
-            title={quickSelectTooltipText}
-            icon={<Icon component={AiAnalysisIcon} />}
-          >
-            AI
-          </ToggleButton>
-
-          <QuickSelectSettingsPopover />
-        </div>
-      )}
+      {adaptedActiveTool === AnnotationTool.QUICK_SELECT && <QuickSelectSettingsPopover />}
 
       {adaptedActiveTool.hasOverwriteCapabilities ? <VolumeInterpolationButton /> : null}
 

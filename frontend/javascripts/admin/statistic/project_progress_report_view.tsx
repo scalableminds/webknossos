14540a7e28a3e60d5c7f2136fb13bc9eb4bbc297
Tom Herold <tom.herold@scalableminds.com>
2026-03-25 07:56:11 +0100

    code rabbit feedback

--- frontend/javascripts/admin/statistic/project_progress_report_view.tsx
+++ frontend/javascripts/admin/statistic/project_progress_report_view.tsx
@@ -63,6 +63,7 @@ function ProjectProgressReportView() {
             icon={<ReloadOutlined spin={isFetching} />}
             variant="outlined"
             onClick={() => refetch()}
+            disabled={team == null}
           >
             Refresh
           </Button>

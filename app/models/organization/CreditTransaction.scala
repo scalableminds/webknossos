scalableminds/webknossos

commit d9bac7c0baba03fad3aee99dd8bdc43d2d7e3005

Replace remaining slick-style sql queries with SqlTokens (#9405)
- refactoring: no more slick-style sql queries
- the SQLDAO generic implementations of findAll and findOne now use the
access queries, so many more DAOs can use them.
- update access query for ai models

### URL of deployed dev instance (used for testing):
- https://sqlcleanup.webknossos.xyz

### Steps to test:
- Test basic webknossos functionality
- test AI model organization sharing
- update description + name + othersMayEdit of annotations
- transfer annotation to other user
- test editing dataset transformations
- test pausing/unpausing a project
- increase task instances
- test invites, test deleting expired
- test changing user team memberships, experiences
- test revoking user auth token

------
- [x] Considered [common edge
cases](../blob/master/.github/common_edge_cases.md)

app/models/organization/CreditTransaction.scala
@@ -4,15 +4,17 @@ import com.scalableminds.util.accesscontext.DBAccessContext
 import com.scalableminds.util.objectid.ObjectId
 import com.scalableminds.util.time.Instant
 import com.scalableminds.util.tools.Fox
-import com.scalableminds.webknossos.schema.Tables.CreditTransactionsRow
-import com.scalableminds.webknossos.schema.Tables.CreditTransactions
+import com.scalableminds.webknossos.schema.Tables.{
+  CreditTransactions,
+  CreditTransactionsRow,
+  GetResultCreditTransactionsRow
+}
 import models.organization.CreditState.CreditState
 import models.organization.CreditTransactionState.TransactionState
 import slick.dbio.DBIO
 import slick.jdbc.GetResult
 import slick.jdbc.PostgresProfile.api._
 import slick.jdbc.TransactionIsolation.Serializable
-import slick.lifted.Rep
 import telemetry.SlackNotificationService
 import utils.WkConf
 import utils.sql.{SQLDAO, SqlClient, SqlToken}
@@ -40,10 +42,7 @@ class CreditTransactionDAO @Inject()(conf: WkConf,
     extends SQLDAO[CreditTransaction, CreditTransactionsRow, CreditTransactions](sqlClient) {
 
   protected val collection = CreditTransactions
-
-  protected def idColumn(x: CreditTransactions): Rep[String] = x._Id
-
-  override protected def isDeletedColumn(x: CreditTransactions): Rep[Boolean] = x.isDeleted
+  protected def resultConverter = GetResultCreditTransactionsRow
 
   override protected def parse(row: CreditTransactionsRow): Fox[CreditTransaction] =
     for {
@@ -114,22 +113,6 @@ class CreditTransactionDAO @Inject()(conf: WkConf,
   override protected def updateAccessQ(requestingUserId: ObjectId): SqlToken = readAccessQ(requestingUserId)
 
   override protected def anonymousReadAccessQ(sharingToken: Option[String]): SqlToken = q"FALSE"
-
-  override def findAll(implicit ctx: DBAccessContext): Fox[List[CreditTransaction]] =
-    for {
-      accessQuery <- accessQueryFromAccessQ(listAccessQ)
-      r <- run(q"SELECT $columns FROM $existingCollectionName WHERE $accessQuery".as[CreditTransactionsRow])
-      parsed <- parseAll(r)
-    } yield parsed
-
-  def findOne(transactionId: String)(implicit ctx: DBAccessContext): Fox[CreditTransaction] =
-    for {
-      accessQuery <- readAccessQuery
-      r <- run(
-        q"SELECT $columns FROM $existingCollectionName WHERE _id = $transactionId AND $accessQuery"
-          .as[CreditTransactionsRow])
-      parsed <- parseFirst(r, transactionId)
-    } yield parsed
 
   def getMilliCreditBalance(organizationId: String)(implicit ctx: DBAccessContext): Fox[Int] =
     for {

app/controllers/AiModelController.scala
@@ -7,7 +7,7 @@ import models.aimodels.{AiInference, AiInferenceDAO, AiInferenceService, AiModel
 import models.annotation.AnnotationDAO
 import models.dataset.{DataStoreDAO, DatasetDAO, DatasetService, UploadToPathsService}
 import models.job.{JobCommand, JobService}
-import models.user.{User, UserService}
+import models.user.User
 import play.api.libs.json.{Json, OFormat}
 import play.api.mvc.{Action, AnyContent, PlayBodyParsers}
 import play.silhouette.api.Silhouette
@@ -96,7 +96,6 @@ class AiModelController @Inject()(
     aiModelDAO: AiModelDAO,
     aiModelService: AiModelService,
     sil: Silhouette[WkEnv],
-    userService: UserService,
     annotationDAO: AnnotationDAO,
     aiInferenceService: AiInferenceService,
     aiInferenceDAO: AiInferenceDAO,

app/controllers/AnnotationController.scala
@@ -399,8 +399,7 @@ class AnnotationController @Inject()(
       } yield Ok(Json.toJson(othersMayEdit))
     }
 
-  private def duplicateAnnotation(annotation: Annotation, user: User)(implicit ctx: DBAccessContext,
-                                                                      m: MessagesProvider): Fox[Annotation] =
+  private def duplicateAnnotation(annotation: Annotation, user: User)(implicit m: MessagesProvider): Fox[Annotation] =
     for {
       // GlobalAccessContext is allowed here because the user was already allowed to see the annotation
       dataset <- datasetDAO.findOne(annotation._dataset)(GlobalAccessContext) ?~> "dataset.notFoundForAnnotation" ~> NOT_FOUND

app/controllers/AuthenticationController.scala
@@ -927,7 +927,7 @@ class AuthenticationController @Inject()(
                       teamMemberships = teamMemberships
                     ) ?~> "user.creation.failed"
                     _ = analyticsService.track(SignupEvent(user, hadInvite = false))
-                    multiUser <- multiUserDAO.findOne(user._multiUser)
+                    multiUser <- multiUserDAO.findOne(user._multiUser)(GlobalAccessContext)
                     _ <- organizationService
                       .createOrganizationDirectory(organization._id) ?~> "organization.folderCreation.failed"
                     _ <- Fox.runIf(conf.WebKnossos.TermsOfService.enabled)(

app/controllers/CreditTransactionController.scala
@@ -68,7 +68,7 @@ class CreditTransactionController @Inject()(organizationDAO: OrganizationDAO,
     } yield Ok
   }
 
-  def refundCreditTransaction(organizationId: String, transactionId: String): Action[AnyContent] =
+  def refundCreditTransaction(organizationId: String, transactionId: ObjectId): Action[AnyContent] =
     sil.SecuredAction.async { implicit request =>
       for {
         _ <- userService.assertIsSuperUser(request.identity) ?~> "Only super users can manually refund credits"

app/controllers/JobController.scala
@@ -6,13 +6,7 @@ import com.scalableminds.util.accesscontext.GlobalAccessContext
 import com.scalableminds.util.tools.{Fox, JsonHelper}
 import models.dataset.{DataStoreDAO, DatasetDAO, DatasetLayerAdditionalAxesDAO, DatasetService}
 import models.job._
-import models.organization.{
-  CreditTransactionDAO,
-  CreditTransactionService,
-  OrganizationDAO,
-  OrganizationService,
-  PricingPlan
-}
+import models.organization.{CreditTransactionDAO, CreditTransactionService, OrganizationDAO, PricingPlan}
 import models.user.{MultiUserDAO, UserService}
 import play.api.i18n.Messages
 import play.api.libs.json._
@@ -71,7 +65,6 @@ class JobController @Inject()(jobDAO: JobDAO,
                               wkSilhouetteEnvironment: WkSilhouetteEnvironment,
                               slackNotificationService: SlackNotificationService,
                               organizationDAO: OrganizationDAO,
-                              organizationService: OrganizationService,
                               creditTransactionService: CreditTransactionService,
                               creditTransactionDAO: CreditTransactionDAO,
                               dataStoreDAO: DataStoreDAO,
@@ -83,7 +76,7 @@ class JobController @Inject()(jobDAO: JobDAO,
     for {
       _ <- Fox.successful(())
       jobCountsByState <- jobDAO.countByState
-      workers <- workerDAO.findAll
+      workers <- workerDAO.findAll(GlobalAccessContext)
       workersJson = workers.map(workerService.publicWrites)
       jsStatus = Json.obj(
         "workers" -> workersJson,

app/controllers/MaintenanceController.scala
@@ -1,12 +1,12 @@
 package controllers
 
+import com.scalableminds.util.accesscontext.GlobalAccessContext
 import play.silhouette.api.Silhouette
 import com.scalableminds.util.time.Instant
 import com.scalableminds.util.tools.{Fox, FoxImplicits}
 import models.user.UserService
 import play.api.libs.json.{JsObject, Json, OFormat}
 import play.api.mvc.{Action, AnyContent, PlayBodyParsers}
-import slick.lifted.Rep
 import com.scalableminds.util.objectid.ObjectId
 import utils.sql.{SQLDAO, SqlClient}
 
@@ -60,7 +60,7 @@ class MaintenanceController @Inject()(
   def listAll: Action[AnyContent] = sil.SecuredAction.async { implicit request =>
     for {
       _ <- userService.assertIsSuperUser(request.identity) ?~> "notAllowed" ~> FORBIDDEN
-      maintenances <- maintenanceDAO.findAll
+      maintenances <- maintenanceDAO.findAll(GlobalAccessContext)
       js = maintenances.map(maintenanceService.publicWrites)
     } yield Ok(Json.toJson(js))
   }
@@ -119,10 +119,7 @@ class MaintenanceService @Inject()() {
 class MaintenanceDAO @Inject()(sqlClient: SqlClient)(implicit ec: ExecutionContext)
     extends SQLDAO[Maintenance, MaintenancesRow, Maintenances](sqlClient) {
   protected val collection = Maintenances
-
-  protected def idColumn(x: Maintenances): Rep[String] = x._Id
-
-  protected def isDeletedColumn(x: Maintenances): Rep[Boolean] = x.isdeleted
+  protected def resultConverter = GetResultMaintenancesRow
 
   protected def parse(r: MaintenancesRow): Fox[Maintenance] =
     Fox.successful(
@@ -138,12 +135,6 @@ class MaintenanceDAO @Inject()(sqlClient: SqlClient)(implicit ec: ExecutionConte
   def findCurrentAndUpcoming: Fox[List[Maintenance]] =
     for {
       rows <- run(q"SELECT $columns FROM $existingCollectionName WHERE endTime >= ${Instant.now}".as[MaintenancesRow])
-      parsed <- parseAll(rows)
-    } yield parsed
-
-  def findAll: Fox[List[Maintenance]] =
-    for {
-      rows <- run(q"SELECT $columns FROM $existingCollectionName".as[MaintenancesRow])
       parsed <- parseAll(rows)
     } yield parsed
 

app/mail/MailchimpTicker.scala
@@ -31,7 +31,7 @@ class MailchimpTicker @Inject()(val lifecycle: ApplicationLifecycle,
   override protected def tick(): Fox[Unit] = {
     logger.info("Checking if any users need mailchimp tagging...")
     for {
-      multiUsers: List[MultiUser] <- multiUserDAO.findAll
+      multiUsers: List[MultiUser] <- multiUserDAO.findAll(GlobalAccessContext)
       _ = multiUsers.foreach(withErrorLogging(_, tagUserByActivity))
     } yield ()
   }

commit 30992bafb96ab4671f8c440e4fb43ea763458510

Fix listing credit transactions for superusers (#9454)
Regression from #9405 

this findAll does differ from the parent class, because it uses
listAccessQ, not the default readAccessQ, which in this case is
different – it allows superUsers to access all creditTransactions across
orgas. However, the *list* should still access only those of the current
orga.

### Steps to test:
- In multi-orga setup with worker, check that superusers can see credit
transactions of their orga in the admin page.

### Issues:
- fixes https://scm.slack.com/archives/C5AKLAV0B/p1775553642484799

app/models/organization/CreditTransaction.scala
@@ -114,6 +114,13 @@ class CreditTransactionDAO @Inject()(conf: WkConf,
 
   override protected def anonymousReadAccessQ(sharingToken: Option[String]): SqlToken = q"FALSE"
 
+  override def findAll(implicit ctx: DBAccessContext): Fox[List[CreditTransaction]] =
+    for {
+      accessQuery <- accessQueryFromAccessQ(listAccessQ)
+      r <- run(q"SELECT $columns FROM $existingCollectionName WHERE $accessQuery".as[CreditTransactionsRow])
+      parsed <- parseAll(r)
+    } yield parsed
+
   def getMilliCreditBalance(organizationId: String)(implicit ctx: DBAccessContext): Fox[Int] =
     for {
       accessQuery <- readAccessQuery
08535b986eacb9ee02fd30bae1e7f58aafd06e0f
Charlie Meister <charlie.meister@student.hpi.de>
2026-04-22 14:57:24 +0200

    Mount command palette only once

--- frontend/javascripts/router/router.tsx
+++ frontend/javascripts/router/router.tsx
@@ -85,13 +85,10 @@ const AsyncWorkflowListView = loadable<EmptyObject>(
 
 function RootLayout() {
   const isAuthenticated = useWkSelector((state) => state.activeUser != null);
-  const isAdminView = useWkSelector((state) => !state.uiInformation.isInAnnotationView);
 
   return (
     <Layout>
-      {/* TODO (#9483): always show command palette; remove logic from router
-      within tracing view, the command palette is rendered in the status bar. */}
-      {isAuthenticated && isAdminView && <CommandPalette label={null} />}
+      <CommandPalette label={null} />
       <Navbar isAuthenticated={isAuthenticated} />
       <Content>
         <ErrorBoundary>

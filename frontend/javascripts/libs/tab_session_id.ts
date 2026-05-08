bf585c20fca01086848487356988b0f9488c66f0
Philipp Otto <philipp.4096@gmail.com>
2026-05-08 12:22:12 +0200

    Add human-readable prefix to session IDs
    Include browser, OS, and timestamp information in
    session IDs to improve debugging and session tracking.

--- frontend/javascripts/libs/tab_session_id.ts
+++ frontend/javascripts/libs/tab_session_id.ts
@@ -11,10 +11,38 @@ import window from "libs/window";
 
 const SESSION_ID_STORAGE_KEY = "wk_tab_session_id";
 
+function getHumanReadablePrefix(): string {
+  const ua = navigator.userAgent;
+
+  let browser = "Browser";
+  if (/Edg\//.test(ua)) browser = "Edge";
+  else if (/OPR\//.test(ua)) browser = "Opera";
+  else if (/Chrome\//.test(ua)) browser = "Chrome";
+  else if (/Firefox\//.test(ua)) browser = "Firefox";
+  else if (/Safari\//.test(ua)) browser = "Safari";
+
+  let os = "Unknown";
+  if (/Android/.test(ua)) os = "Android";
+  else if (/iPhone|iPad/.test(ua)) os = "iOS";
+  else if (/Windows/.test(ua)) os = "Windows";
+  else if (/Mac/.test(ua)) os = "Mac";
+  else if (/Linux/.test(ua)) os = "Linux";
+
+  const now = new Date();
+  const date = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, "0")}-${String(now.getDate()).padStart(2, "0")}`;
+  const time = `${String(now.getHours()).padStart(2, "0")}:${String(now.getMinutes()).padStart(2, "0")}`;
+
+  return `${browser}_${os}_${date}_${time}`;
+}
+
+function createSessionId(): string {
+  return `${getHumanReadablePrefix()}_${getUid()}`;
+}
+
 function getOrCreateSessionId(): string {
   if (window.opener != null) {
     // This tab inherited a copy of the opener's sessionStorage; discard it.
-    const freshId = getUid();
+    const freshId = createSessionId();
     sessionStorage.setItem(SESSION_ID_STORAGE_KEY, freshId);
     return freshId;
   }
@@ -22,7 +50,7 @@ function getOrCreateSessionId(): string {
   if (stored != null) {
     return stored;
   }
-  const newId = getUid();
+  const newId = createSessionId();
   sessionStorage.setItem(SESSION_ID_STORAGE_KEY, newId);
   return newId;
 }

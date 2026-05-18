d157138cfb9f47a6abfeb1dcd8c665da1256d605
Florian M <florian@scm.io>
2026-05-18 17:22:31 +0200

    extract loading code

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/helpers/NativeBucketScanner.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/helpers/NativeBucketScanner.scala
@@ -1,36 +1,44 @@
 package com.scalableminds.webknossos.datastore.helpers
 
+import com.github.sbt.jni.syntax.NativeLoader
+import com.scalableminds.webknossos.datastore.helpers.NativeBucketScanner.getClass
+
 import java.nio.file.Files
+
+object NativeLoaderUtils {
+  def prepare(libName: String): String = {
+    val lib = System.mapLibraryName(libName)
+    val uname = scala.sys.process.Process("uname -sm").!!.trim.split(" ")
+    val plat = uname(1).toLowerCase + "-" + uname(0).toLowerCase
+    val resourcePath = s"/native/$plat/$lib"
+    val stream = getClass.getResourceAsStream(resourcePath)
+    if (stream == null)
+      throw new UnsatisfiedLinkError(s"Native library $lib ($resourcePath) cannot be found on the classpath.")
+    val tmp = Files.createTempDirectory("jni-")
+    val extracted = tmp.resolve(lib)
+    Files.copy(stream, extracted)
+    extracted.toAbsolutePath.toString
+  }
+}
 
 // Load the native library from this class's classloader (application classloader).
 // Extending NativeLoader directly causes the load to happen from the sbt-jni-core classloader
 // (parent), so JNI cannot resolve native methods declared on NativeBucketScanner (child).
 object NativeBucketScanner {
-  val loaded: Boolean = {
+  val load: Boolean = {
     val libName = "webknossosJni0"
     try
       System.loadLibrary(libName)
     catch {
       case _: UnsatisfiedLinkError =>
-        println("####################### loadLibary failed")
-        val lib = System.mapLibraryName(libName)
-        val uname = scala.sys.process.Process("uname -sm").!!.trim.split(" ")
-        val plat = uname(1).toLowerCase + "-" + uname(0).toLowerCase
-        val resourcePath = s"/native/$plat/$lib"
-        val stream = getClass.getResourceAsStream(resourcePath)
-        if (stream == null)
-          throw new UnsatisfiedLinkError(s"Native library $lib ($resourcePath) cannot be found on the classpath.")
-        val tmp = Files.createTempDirectory("jni-")
-        val extracted = tmp.resolve(lib)
-        Files.copy(stream, extracted)
-        System.load(extracted.toAbsolutePath.toString)
+        System.load(NativeLoaderUtils.prepare(libName))
     }
     true
   }
 }
 
-class NativeBucketScanner() {
-  NativeBucketScanner.loaded // triggers companion object initialization and library load
+class NativeBucketScanner {
+  NativeBucketScanner.load // triggers companion object initialization and library load
   @native def collectSegmentIds(
       bucketBytes: Array[Byte],
       bytesPerElement: Int,

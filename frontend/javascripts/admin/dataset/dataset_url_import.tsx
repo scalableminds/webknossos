20a34a1ec98d2ee50ea7702a1888fff91add4460
Michael Büßemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>
2026-05-22 13:45:26 +0200

    apply review feedback - use dataloader to load existing dataset info

--- frontend/javascripts/admin/dataset/dataset_url_import.tsx
+++ frontend/javascripts/admin/dataset/dataset_url_import.tsx
@@ -1,61 +1,43 @@
 import DatasetAddRemoteView from "admin/dataset/dataset_add_remote_view";
 import { findDatasetByImportUrl, getDatastores } from "admin/rest_api";
-import BrainSpinner from "components/brain_spinner";
-import { useFetch } from "libs/react_helpers";
-import Toast from "libs/toast";
-import { getUrlParamsObject } from "libs/utils";
-import has from "lodash-es/has";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import type { LoaderFunctionArgs } from "react-router-dom";
+import { redirect, useLoaderData, useNavigate } from "react-router-dom";
+import type { APIDataStore } from "types/api_types";
 import { getViewDatasetURL } from "viewer/model/accessors/dataset_accessor";
+
+export async function datasetURLImportLoader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const datasetUri = url.searchParams.get("url");
+
+  if (datasetUri != null) {
+    let existingDataset;
+    try {
+      existingDataset = await findDatasetByImportUrl(datasetUri);
+    } catch (e) {
+      console.error("Check for existing dataset with the same remote URL failed.", e);
+    }
+    if (existingDataset?.dataSource) {
+      throw redirect(getViewDatasetURL(existingDataset));
+    }
+  }
+
+  const datastores = await getDatastores();
+
+  return { datastores, datasetUri };
+}
+
+type LoaderData = {
+  datastores: APIDataStore[];
+  datasetUri: string | null;
+};
 
 export function DatasetURLImport() {
   const navigate = useNavigate();
-  const params = getUrlParamsObject();
-  const datasetUri = has(params, "url") ? params.url : null;
-  // Check if there is already a dataset with the same import url.
-  const { dataset: maybeExistingDS, checkedForExistence } = useFetch(
-    async () => {
-      let dataset = null;
-      let checkedForExistence = false;
-      if (datasetUri == null) return { dataset, checkedForExistence };
-      try {
-        dataset = await findDatasetByImportUrl(datasetUri);
-        checkedForExistence = true;
-      } catch (_e) {
-        checkedForExistence = true;
-        const errorText = "Check for existing dataset with the same remote URL failed.";
-        Toast.error(errorText);
-        console.error(errorText, _e);
-      }
-      return { dataset, checkedForExistence };
-    },
-    { dataset: null, checkedForExistence: false },
-    [datasetUri],
-  );
-  const datastores = useFetch(async () => await getDatastores(), null, []);
+  const { datastores, datasetUri } = useLoaderData() as LoaderData;
+
   const handleDatasetAdded = async (addedDatasetId: string): Promise<void> => {
     navigate(`/datasets/${addedDatasetId}/view`);
   };
-
-  useEffect(() => {
-    if (maybeExistingDS?.dataSource) {
-      navigate(getViewDatasetURL(maybeExistingDS), { replace: true });
-    }
-  }, [maybeExistingDS, navigate]);
-
-  if (
-    (datasetUri !== null && !checkedForExistence) ||
-    maybeExistingDS?.dataSource ||
-    datastores == null
-  ) {
-    // First check whether there is already a dataset with this import url
-    // before rendering DatasetAddRemoteView which potentially auto-imports the dataset
-    // and would thus duplicate it.
-    // In case a duplicate was found, also render a placeholder while navigating to this dataset.
-    // Else if the datastores are still unknown, also render the placeholder.
-    return <BrainSpinner />;
-  }
 
   return (
     <DatasetAddRemoteView

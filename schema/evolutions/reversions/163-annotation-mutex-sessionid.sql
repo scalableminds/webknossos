8bdb440428760dba91458f9c6441cf4bf11061b9
Florian M <florian@scm.io>
2026-05-26 13:11:09 +0200

    Revert "bump schema version"
    This reverts commit 2306907ee03cb707198b9876cbf6566a3e8537ee.

--- schema/evolutions/reversions/163-annotation-mutex-sessionid.sql
+++
@@ -1,9 +0,0 @@
-START TRANSACTION;
-
-do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 163 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
-
-ALTER TABLE webknossos.annotation_mutexes DROP COLUMN sessionId;
-
-UPDATE webknossos.releaseInformation SET schemaVersion = 162;
-
-COMMIT TRANSACTION;

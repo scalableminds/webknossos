3f1ecc065f78e1cf539e71ccc768f20fb95c1c2d
Norman Rzepka <code@normanrz.com>
2026-05-26 17:32:08 +0200

    schema version

--- schema/evolutions/162-upload-mags-attachments.sql
+++ schema/evolutions/162-upload-mags-attachments.sql
@@ -1,6 +1,6 @@
 START TRANSACTION;
 
-do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 160 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
+do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 161 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
 
 ALTER TABLE webknossos.dataset_layer_attachments ADD COLUMN uploadIsPending BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE webknossos.dataset_mags ADD COLUMN uploadIsPending BOOLEAN NOT NULL DEFAULT FALSE;
--- schema/evolutions/reversions/162-upload-mags-attachments.sql
+++ schema/evolutions/reversions/162-upload-mags-attachments.sql
@@ -1,6 +1,6 @@
 START TRANSACTION;
 
-do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 161 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
+do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 162 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
 
 ALTER TABLE webknossos.dataset_layer_attachments DROP COLUMN uploadIsPending;
 ALTER TABLE webknossos.dataset_mags DROP COLUMN uploadIsPending;

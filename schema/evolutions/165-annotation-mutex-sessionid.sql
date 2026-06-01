a675c7bca9d6fffa37fdb122ca6687ba57e3a291
Philipp Otto <philipp.4096@gmail.com>
2026-06-01 09:43:25 +0200

    update migrations

--- schema/evolutions/165-annotation-mutex-sessionid.sql
+++ schema/evolutions/165-annotation-mutex-sessionid.sql
@@ -1,11 +1,11 @@
 START TRANSACTION;
 
-do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 161 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
+do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 164 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
 
 ALTER TABLE webknossos.annotation_mutexes ADD COLUMN sessionId TEXT;
 UPDATE webknossos.annotation_mutexes SET sessionId = _user;
 ALTER TABLE webknossos.annotation_mutexes ALTER COLUMN sessionId SET NOT NULL;
 
-UPDATE webknossos.releaseInformation SET schemaVersion = 162;
+UPDATE webknossos.releaseInformation SET schemaVersion = 165;
 
 COMMIT TRANSACTION;

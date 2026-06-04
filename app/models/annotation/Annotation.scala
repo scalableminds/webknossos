scalableminds/webknossos

commit 0e12fdf1e3a79f88fb00580494ac63cdcaed8489

Remove redundant annotation → team reference (#9610)
The _team reference for annotations only ever made sense for tasks. It
was used to check teamManager access rights for the annotation. This is
done by joining the task and it’s project now (which still has the same
team reference).

This allows cleanly creating annotations even for users with no teams.
(Though they can only see public datasets)

### URL of deployed dev instance (used for testing):
- https://teamscleanup.webknossos.xyz

### Steps to test:
- Create some tasks, set up experience
- As non-admin member user
  - Create annotation
  - Annotate some tasks
- As non-admin team manager
  - try resetting one of the tasks of the member user

### Issues:
- fixes
https://scm.slack.com/archives/C5AKLAV0B/p1780299221170949?thread_ts=1780297035.709579&cid=C5AKLAV0B

------
- [x] Removed dev-only changes like prints and application.conf edits
- [x] Considered [common edge
cases](../blob/master/.github/common_edge_cases.md)

app/models/annotation/Annotation.scala
@@ -33,7 +33,6 @@ case class Annotation(
     _id: ObjectId,
     _dataset: ObjectId,
     _task: Option[ObjectId] = None,
-    _team: ObjectId,
     _user: ObjectId,
     annotationLayers: List[AnnotationLayer],
     description: String = AnnotationDefaults.defaultDescription,
@@ -210,7 +209,6 @@ class AnnotationDAO @Inject()(sqlClient: SqlClient, annotationLayerDAO: Annotati
         ObjectId(r._Id),
         ObjectId(r._Dataset),
         r._Task.map(ObjectId(_)),
-        ObjectId(r._Team),
         ObjectId(r._User),
         annotationLayers,
         r.description,
@@ -267,21 +265,20 @@ class AnnotationDAO @Inject()(sqlClient: SqlClient, annotationLayerDAO: Annotati
         OR (
           ${prefix}visibility = ${AnnotationVisibility.Internal}
           AND (
-            (SELECT _organization FROM webknossos.teams WHERE webknossos.teams._id = ${prefix}_team)
+            (SELECT _organization FROM webknossos.users_ WHERE _id = ${prefix}_user)
             IN (SELECT _organization FROM webknossos.users_ WHERE _id = $requestingUserId)
-          )
-          OR ${prefix}_team IN (SELECT _team FROM webknossos.user_team_roles WHERE _user = $requestingUserId AND isTeamManager)
-          OR ${prefix}_user = $requestingUserId
-          OR (
-            (SELECT _organization FROM webknossos.teams WHERE webknossos.teams._id = ${prefix}_team)
-            IN (SELECT _organization FROM webknossos.users_ where _id = $requestingUserId AND isAdmin)
           )
         )"""
 
   override protected def deleteAccessQ(requestingUserId: ObjectId) =
-    q"""(_team IN (SELECT _team FROM webknossos.user_team_roles WHERE isTeamManager AND _user = $requestingUserId) OR _user = $requestingUserId
-       OR (SELECT _organization FROM webknossos.teams WHERE webknossos.teams._id = _team)
-        IN (SELECT _organization FROM webknossos.users_ WHERE _id = $requestingUserId AND isAdmin))"""
+    q"""(_user = $requestingUserId
+       OR (SELECT _organization FROM webknossos.users_ WHERE _id = _user)
+          IN (SELECT _organization FROM webknossos.users_ WHERE _id = $requestingUserId AND isAdmin)
+       OR (
+         _task IS NOT NULL
+         AND (SELECT p._team FROM webknossos.projects p JOIN webknossos.tasks t ON t._project = p._id WHERE t._id = _task)
+           IN (SELECT _team FROM webknossos.user_team_roles WHERE _user = $requestingUserId AND isTeamManager)
+       ))"""
 
   override protected def updateAccessQ(requestingUserId: ObjectId): SqlToken =
     deleteAccessQ(requestingUserId)
@@ -518,12 +515,6 @@ class AnnotationDAO @Inject()(sqlClient: SqlClient, annotationLayerDAO: Annotati
     } yield parsed
   }
 
-  def countForTeam(teamId: ObjectId): Fox[Int] =
-    for {
-      countList <- run(q"SELECT COUNT(*) FROM $existingCollectionName WHERE _team = $teamId".as[Int])
-      count <- countList.headOption.toFox
-    } yield count
-
   // Does not use access query (because they dont support prefixes). Use only after separate access check!
   def findAllFinishedForProject(projectId: ObjectId): Fox[List[Annotation]] =
     for {
@@ -597,7 +588,7 @@ class AnnotationDAO @Inject()(sqlClient: SqlClient, annotationLayerDAO: Annotati
     for {
       accessQuery <- readAccessQuery
       excludeTeamsQ = if (excludedTeamIds.isEmpty) q"TRUE"
-      else q"(NOT t._id IN ${SqlToken.tupleFromList(excludedTeamIds)})"
+      else q"(NOT p._team IN ${SqlToken.tupleFromList(excludedTeamIds)})"
       countList <- run(q"""SELECT COUNT(*)
                            FROM (
                              SELECT a._id
@@ -609,7 +600,8 @@ class AnnotationDAO @Inject()(sqlClient: SqlClient, annotationLayerDAO: Annotati
                                AND state = ${AnnotationState.Active}
                                AND $accessQuery
                              ) a
-                             JOIN webknossos.teams t ON a._team = t._id
+                             JOIN webknossos.tasks_ t ON a._task = t._id
+                             JOIN webknossos.projects_ p ON t._project = p._id
                              WHERE $excludeTeamsQ
                            ) q
                          """.as[Int])
@@ -651,9 +643,9 @@ class AnnotationDAO @Inject()(sqlClient: SqlClient, annotationLayerDAO: Annotati
 
   def insertOne(a: Annotation): Fox[Unit] = {
     val insertAnnotationQuery = q"""
-        INSERT INTO webknossos.annotations(_id, _dataset, _task, _team, _user, description, visibility,
+        INSERT INTO webknossos.annotations(_id, _dataset, _task, _user, description, visibility,
                                            name, viewConfiguration, state, tags, tracingTime, typ, collaborationMode, created, modified, isDeleted)
-        VALUES(${a._id}, ${a._dataset}, ${a._task}, ${a._team},
+        VALUES(${a._id}, ${a._dataset}, ${a._task},
          ${a._user}, ${a.description}, ${a.visibility}, ${a.name},
          ${a.viewConfiguration},
          ${a.state},
@@ -673,7 +665,6 @@ class AnnotationDAO @Inject()(sqlClient: SqlClient, annotationLayerDAO: Annotati
              UPDATE webknossos.annotations
              SET
                _dataset = ${a._dataset},
-               _team = ${a._team},
                _user = ${a._user},
                description = ${a.description},
                visibility = ${a.visibility},

.gitignore
@@ -25,7 +25,14 @@ RUNNING_PID
 .metals
 metals.sbt
 __pycache__/
+<<<<<<< Updated upstream
+.jvmopts
 
+||||||| constructed merge base
+
+=======
+.jvmopts
+>>>>>>> Stashed changes
 
 # Webknossos
 backup/

app/controllers/AnnotationController.scala
@@ -150,7 +150,8 @@ class AnnotationController @Inject()(
   def reset(typ: String, id: ObjectId): Action[AnyContent] = sil.SecuredAction.async { implicit request =>
     for {
       annotation <- provider.provideAnnotation(typ, id, request.identity) ?~> Msg.Annotation.notFound ~> NOT_FOUND
-      _ <- Fox.assertTrue(userService.isTeamManagerOrAdminOf(request.identity, annotation._team))
+      owner <- userService.findOneCached(annotation._user)(GlobalAccessContext)
+      _ <- Fox.assertTrue(userService.isTeamManagerOrAdminOf(request.identity, owner._organization, annotation._task))
       _ <- annotationService.resetToBase(annotation) ?~> Msg.Annotation.Reset.failed
       updated <- provider.provideAnnotation(typ, id, request.identity)
       json <- annotationService.publicWrites(updated, Some(request.identity))
@@ -160,7 +161,8 @@ class AnnotationController @Inject()(
   def reopen(typ: String, id: ObjectId): Action[AnyContent] = sil.SecuredAction.async { implicit request =>
     def isReopenAllowed(user: User, annotation: Annotation) =
       for {
-        isAdminOrTeamManager <- userService.isTeamManagerOrAdminOf(user, annotation._team)
+        owner <- userService.findOneCached(annotation._user)(GlobalAccessContext)
+        isAdminOrTeamManager <- userService.isTeamManagerOrAdminOf(user, owner._organization, annotation._task)
         _ <- Fox.fromBool(annotation.state == AnnotationState.Finished) ?~> Msg.Annotation.Reopen.notFinished
         _ <- Fox.fromBool(isAdminOrTeamManager || annotation._user == user._id) ?~> Msg.Annotation.Reopen.notAllowed
         _ <- Fox
@@ -221,7 +223,6 @@ class AnnotationController @Inject()(
           ObjectId.dummyId,
           dataset._id,
           None,
-          ObjectId.dummyId,
           ObjectId.dummyId,
           List(
             AnnotationLayer(TracingId.dummy,
@@ -320,7 +321,8 @@ class AnnotationController @Inject()(
 
     for {
       annotation <- provider.provideAnnotation(typ, id, request.identity) ~> NOT_FOUND
-      _ <- Fox.assertTrue(userService.isTeamManagerOrAdminOf(request.identity, annotation._team))
+      owner <- userService.findOneCached(annotation._user)(GlobalAccessContext)
+      _ <- Fox.assertTrue(userService.isTeamManagerOrAdminOf(request.identity, owner._organization, annotation._task))
       result <- tryToCancel(annotation)
     } yield result
   }
@@ -441,13 +443,13 @@ class AnnotationController @Inject()(
         datasetBoundingBox = dataSource.map(_.boundingBox)
       )
       newAnnotationLayers = newAnnotationProto.annotationLayers.map(AnnotationLayer.fromProto)
-      clonedAnnotation <- annotationService.createFrom(user,
-                                                       dataset,
-                                                       newAnnotationLayers,
-                                                       AnnotationType.Explorational,
-                                                       None,
-                                                       annotation.description,
-                                                       newAnnotationId) ?~> Msg.Annotation.createFailed
+      clonedAnnotation = annotationService.createFrom(user,
+                                                      dataset,
+                                                      newAnnotationLayers,
+                                                      AnnotationType.Explorational,
+                                                      None,
+                                                      annotation.description,
+                                                      newAnnotationId)
       _ <- annotationDAO.insertOne(clonedAnnotation)
     } yield clonedAnnotation
 

app/controllers/AnnotationIOController.scala
@@ -141,13 +141,13 @@ class AnnotationIOController @Inject()(
                                                                                       usableDataSource,
                                                                                       dataset._id)
           mergedSkeletonLayers <- mergeAndSaveSkeletonLayers(skeletonTracings, tracingStoreClient)
-          annotation <- annotationService.createFrom(request.identity,
-                                                     dataset,
-                                                     mergedSkeletonLayers ::: mergedVolumeLayers,
-                                                     AnnotationType.Explorational,
-                                                     name,
-                                                     description,
-                                                     newAnnotationId)
+          annotation = annotationService.createFrom(request.identity,
+                                                    dataset,
+                                                    mergedSkeletonLayers ::: mergedVolumeLayers,
+                                                    AnnotationType.Explorational,
+                                                    name,
+                                                    description,
+                                                    newAnnotationId)
           annotationProto = AnnotationProto(
             description = annotation.description,
             version = 0L,

app/controllers/TaskController.scala
@@ -198,12 +198,11 @@ class TaskController @Inject()(taskCreationService: TaskCreationService,
     log() {
       for {
         assignee <- userService.findOneCached(userId)
-        teams <- userService.teamIdsFor(userId)
         task <- taskDAO.findOne(id)
         project <- projectDAO.findOne(task._project)
         _ <- Fox.assertTrue(userService.isTeamManagerOrAdminOf(request.identity, project._team)) ?~> Msg.notAllowed
         _ <- Fox.assertTrue(userService.isTeamManagerOrAdminOf(request.identity, assignee)) ?~> Msg.notAllowed
-        (_, initializingAnnotationId) <- taskDAO.assignOneTo(id, userId, teams) ?~> Msg.Task.unavailable
+        (_, initializingAnnotationId) <- taskDAO.assignOneTo(id, userId) ?~> Msg.Task.unavailable
         insertedAnnotationBox <- annotationService.createAnnotationFor(assignee, id, initializingAnnotationId).shiftBox
         _ <- annotationService.abortInitializedAnnotationOnFailure(initializingAnnotationId, insertedAnnotationBox)
         _ <- insertedAnnotationBox.toFox

app/models/annotation/AnnotationMerger.scala
@@ -29,7 +29,6 @@ class AnnotationMerger @Inject()(datasetDAO: DatasetDAO, tracingStoreService: Tr
       toTemporaryStore = false,
       issuingUser._id,
       annotationB._dataset,
-      annotationB._team,
       AnnotationType.Explorational,
       List(annotationA, annotationB),
       Seq.empty
@@ -40,7 +39,6 @@ class AnnotationMerger @Inject()(datasetDAO: DatasetDAO, tracingStoreService: Tr
       toTemporaryStore: Boolean,
       userId: ObjectId,
       datasetId: ObjectId,
-      teamId: ObjectId,
       typ: AnnotationType,
       annotations: List[Annotation],
       additionalBoundingBoxes: Seq[NamedBoundingBox]
@@ -61,7 +59,6 @@ class AnnotationMerger @Inject()(datasetDAO: DatasetDAO, tracingStoreService: Tr
           newId,
           datasetId,
           None,
-          teamId,
           userId,
           mergedAnnotationLayers,
           typ = typ

app/models/annotation/AnnotationRestrictions.scala
@@ -66,7 +66,10 @@ class AnnotationRestrictionDefaults @Inject()(userService: UserService, annotati
         } else {
           (for {
             user <- userOption.toFox
-            isTeamManagerOrAdminOfTeam <- userService.isTeamManagerOrAdminOf(user, annotation._team)
+            owner <- userService.findOneCached(annotation._user)(GlobalAccessContext)
+            isTeamManagerOrAdminOfTeam <- userService.isTeamManagerOrAdminOf(user,
+                                                                             owner._organization,
+                                                                             annotation._task)
           } yield annotation._user == user._id || isTeamManagerOrAdminOfTeam).orElse(Fox.successful(false))
         }
 
@@ -98,7 +101,8 @@ class AnnotationRestrictionDefaults @Inject()(userService: UserService, annotati
       override def allowFinish(userOption: Option[User]): Fox[Boolean] =
         (for {
           user <- userOption.toFox
-          isTeamManagerOrAdminOfTeam <- userService.isTeamManagerOrAdminOf(user, annotation._team)
+          owner <- userService.findOneCached(annotation._user)(GlobalAccessContext)
+          isTeamManagerOrAdminOfTeam <- userService.isTeamManagerOrAdminOf(user, owner._organization, annotation._task)
         } yield {
           (annotation._user == user._id || isTeamManagerOrAdminOfTeam) && !(annotation.state == Finished) && !annotation.isLockedByOwner
         }).orElse(Fox.successful(false))
@@ -107,7 +111,8 @@ class AnnotationRestrictionDefaults @Inject()(userService: UserService, annotati
       override def allowFinishSoft(userOption: Option[User]): Fox[Boolean] =
         (for {
           user <- userOption.toFox
-          isTeamManagerOrAdminOfTeam <- userService.isTeamManagerOrAdminOf(user, annotation._team)
+          owner <- userService.findOneCached(annotation._user)(GlobalAccessContext)
+          isTeamManagerOrAdminOfTeam <- userService.isTeamManagerOrAdminOf(user, owner._organization, annotation._task)
         } yield {
           (annotation._user == user._id || isTeamManagerOrAdminOfTeam) && !annotation.isLockedByOwner
         }).orElse(Fox.successful(false))

app/models/annotation/AnnotationService.scala
@@ -103,23 +103,6 @@ class AnnotationService @Inject()(
 
   val DefaultAnnotationListLimit = 1000
 
-  private def selectSuitableTeam(user: User, dataset: Dataset): Fox[ObjectId] =
-    (for {
-      userTeamIds <- userService.teamIdsFor(user._id)
-      datasetAllowedTeamIds <- teamService.allowedTeamIdsForDataset(dataset, cumulative = true) ?~> Msg.Dataset.allowedTeamsNotFound
-    } yield {
-      val selectedTeamOpt = datasetAllowedTeamIds.intersect(userTeamIds).headOption
-      selectedTeamOpt match {
-        case Some(selectedTeam) => Fox.successful(selectedTeam)
-        case None =>
-          for {
-            isTeamManagerOrAdminOfOrg <- userService.isTeamManagerOrAdminOfOrg(user, user._organization)
-            _ <- Fox.fromBool(isTeamManagerOrAdminOfOrg || dataset.isPublic || user.isDatasetManager)
-            organizationTeamId <- organizationDAO.findOrganizationTeamId(user._organization)
-          } yield organizationTeamId
-      }
-    }).flatten
-
   private def createVolumeTracing(
       dataSource: UsableDataSource,
       datasetOrganizationId: String,
@@ -307,8 +290,7 @@ class AnnotationService @Inject()(
     val datasetId = dataset._id
     for {
       annotationLayers <- createLayersForExplorational(dataset, newAnnotationId, annotationLayerParameters) ?~> Msg.Annotation.createTracingsFailed
-      teamId <- selectSuitableTeam(user, dataset) ?~> Msg.Annotation.createForbidden
-      annotation = Annotation(newAnnotationId, datasetId, None, teamId, user._id, annotationLayers)
+      annotation = Annotation(newAnnotationId, datasetId, None, user._id, annotationLayers)
       _ <- annotationDAO.insertOne(annotation)
     } yield annotation
   }
@@ -476,7 +458,6 @@ class AnnotationService @Inject()(
       annotationBase = Annotation(ObjectId.generate,
                                   datasetId,
                                   Some(task._id),
-                                  project._team,
                                   userId,
                                   annotationLayers,
                                   description.getOrElse(""),
@@ -497,19 +478,15 @@ class AnnotationService @Inject()(
                  annotationType: AnnotationType,
                  name: Option[String],
                  description: String,
-                 newAnnotationId: ObjectId): Fox[Annotation] =
-    for {
-      teamId <- selectSuitableTeam(user, dataset)
-      annotation = Annotation(newAnnotationId,
-                              dataset._id,
-                              None,
-                              teamId,
-                              user._id,
-                              annotationLayers.toList,
-                              description,
-                              name = name.getOrElse(""),
-                              typ = annotationType)
-    } yield annotation
+                 newAnnotationId: ObjectId): Annotation =
+    Annotation(newAnnotationId,
+               dataset._id,
+               None,
+               user._id,
+               annotationLayers.toList,
+               description,
+               name = name.getOrElse(""),
+               typ = annotationType)
 
   def updateTeamsForSharedAnnotation(annotationId: ObjectId, teams: List[ObjectId])(
       implicit ctx: DBAccessContext): Fox[Unit] =

commit f8867ecc518afedf9da73c0ac6198b2b671b3a3d

Restore annotation read access for admins and owner for visibility=Private (#9642)
fixing regression from #9610 

### URL of deployed dev instance (used for testing):
- https://privateanotations.webknossos.xyz

### Steps to test:
- As non-admin user, set an annotation to visibility private in sharing
modal
- after reload you should still see your own annotation
- as other non-admin user you should not be able to view it
- as admin user of the same orga, you should be able to see it.

### Issues:
- fixes https://scm.slack.com/archives/C02H5T8Q08P/p1780557725510199

------
- [x] Considered [common edge
cases](../blob/master/.github/common_edge_cases.md)

app/models/annotation/Annotation.scala
@@ -268,7 +268,18 @@ class AnnotationDAO @Inject()(sqlClient: SqlClient, annotationLayerDAO: Annotati
             (SELECT _organization FROM webknossos.users_ WHERE _id = ${prefix}_user)
             IN (SELECT _organization FROM webknossos.users_ WHERE _id = $requestingUserId)
           )
-        )"""
+        )
+        OR (
+          ${prefix}visibility = ${AnnotationVisibility.Private}
+          AND (
+            ${prefix}_user = $requestingUserId
+            OR (
+              (SELECT _organization FROM webknossos.users_ WHERE _id = ${prefix}_user)
+              IN (SELECT _organization FROM webknossos.users_ WHERE _id = $requestingUserId AND isAdmin)
+            )
+          )
+        )
+        """
 
   override protected def deleteAccessQ(requestingUserId: ObjectId) =
     q"""(_user = $requestingUserId
eeff763f32dbcae52c72b993599e02ce827f9cf9
Philipp Otto <philipp.4096@gmail.com>
2026-06-24 14:41:47 +0200

    tmp: add debug logging

--- frontend/javascripts/viewer/api/wk_dev.ts
+++ frontend/javascripts/viewer/api/wk_dev.ts
@@ -11,8 +11,8 @@ import type ApiLoader from "./api_loader";
 // Can be accessed via window.webknossos.DEV.flags. Only use this
 // for debugging or one off scripts.
 export const WkDevFlags = {
-  logActions: false,
-  logFullActionObjects: false,
+  logActions: true,
+  logFullActionObjects: true,
   sam: {
     useLocalMask: true,
   },
--- frontend/javascripts/viewer/view/left_border_tabs/mapping_settings_view.tsx
+++ frontend/javascripts/viewer/view/left_border_tabs/mapping_settings_view.tsx
@@ -214,6 +214,8 @@ function MappingSettingsView({ layerName }: Props) {
     mappingType === "JSON" &&
     hideUnmappedIds != null;
 
+  console.log("MappingSettingsView: mappingStatus=", mappingStatus);
+
   return (
     <React.Fragment>
       {!isMergerModeEnabled ? (

11edd304bef2960a7cec86ea6afa9eb52f9e683d
Philipp Otto <philipp.4096@gmail.com>
2026-07-01 08:59:05 +0200

    restore formatting and adapt wording in auto deploy yaml

--- frontend/javascripts/viewer/view/arbitrary_view.ts
+++ frontend/javascripts/viewer/view/arbitrary_view.ts
@@ -191,22 +191,10 @@ class FlightModeView {
       const m = getZoomedMatrix(Store.getState().flycam);
       // biome-ignore format: don't format array
       camera.matrix.set(
-        m[0],
-        m[4],
-        m[8],
-        m[12],
-        m[1],
-        m[5],
-        m[9],
-        m[13],
-        m[2],
-        m[6],
-        m[10],
-        m[14],
-        m[3],
-        m[7],
-        m[11],
-        m[15],
+        m[0], m[4], m[8], m[12],
+        m[1], m[5], m[9], m[13],
+        m[2], m[6], m[10], m[14],
+        m[3], m[7], m[11], m[15],
       );
       camera.matrix.multiply(flipYRotationMatrix);
       camera.matrix.multiply(new Matrix4().makeTranslation(...this.cameraPosition));

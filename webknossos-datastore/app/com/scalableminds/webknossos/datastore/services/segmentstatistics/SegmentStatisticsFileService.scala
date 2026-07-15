61758e69d882b28472df6525904504aab240996b
Florian M <florian@scm.io>
2026-07-15 11:52:18 +0200

    Read attributes from zarr.json

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/segmentstatistics/SegmentStatisticsFileService.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/segmentstatistics/SegmentStatisticsFileService.scala
@@ -1,14 +1,16 @@
 package com.scalableminds.webknossos.datastore.services.segmentstatistics
 
 import com.scalableminds.util.Msg
+import com.scalableminds.util.accesscontext.TokenContext
 import com.scalableminds.util.box.Box
 import com.scalableminds.util.cache.AlfuCache
 import com.scalableminds.util.geometry.Vec3Int
-import com.scalableminds.util.tools.Fox
+import com.scalableminds.util.tools.{Fox, JsonHelper}
 import com.scalableminds.util.tools.Fox.toFox
 import com.scalableminds.webknossos.datastore.models.datasource.{DataLayer, DataSourceId, LayerAttachment}
-import com.scalableminds.webknossos.datastore.storage.AttachmentKey
-import play.api.libs.json.{Json, OFormat}
+import com.scalableminds.webknossos.datastore.services.VoxelyticsZarrArtifactUtils
+import com.scalableminds.webknossos.datastore.storage.{AttachmentKey, DataVaultService}
+import play.api.libs.json.{Json, JsResult, JsValue, OFormat, Reads}
 
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
@@ -22,10 +24,30 @@ object SegmentStatisticsFileInfos {
   implicit val jsonFormat: OFormat[SegmentStatisticsFileInfos] = Json.format[SegmentStatisticsFileInfos]
 }
 
-class SegmentStatisticsFileService @Inject() {
+case class SegmentStatisticsFileAttributes(mag: Option[Vec3Int], mappingName: Option[String])
+
+object SegmentStatisticsFileAttributes extends VoxelyticsZarrArtifactUtils {
+  private val attrKeyMag = "mag"
+  private val attrKeyMappingName = "mapping_name"
+
+  implicit object SegmentStatisticsFileAttributesZarr3GroupHeaderReads extends Reads[SegmentStatisticsFileAttributes] {
+    override def reads(json: JsValue): JsResult[SegmentStatisticsFileAttributes] = {
+      val segmentStatisticsFileAttrs = lookUpArtifactAttributes(json)
+      for {
+        mag <- (segmentStatisticsFileAttrs \ attrKeyMag).validateOpt[Vec3Int]
+        mappingName <- (segmentStatisticsFileAttrs \ attrKeyMappingName).validateOpt[String]
+      } yield SegmentStatisticsFileAttributes(mag, mappingName)
+    }
+  }
+}
+
+class SegmentStatisticsFileService @Inject() (dataVaultService: DataVaultService) {
 
   private val segmentStatisticsFileKeyCache: AlfuCache[(DataSourceId, String), SegmentStatisticsFileKey] =
     AlfuCache() // dataSourceId, layerName → SegmentStatisticsFileKey
+
+  private val attributesCache: AlfuCache[SegmentStatisticsFileKey, SegmentStatisticsFileAttributes] =
+    AlfuCache()
 
   def lookUpSegmentStatisticsFileKey(dataSourceId: DataSourceId, dataLayer: DataLayer)(implicit
       ec: ExecutionContext
@@ -48,15 +70,41 @@ class SegmentStatisticsFileService @Inject() {
       attachment
     )
 
-  def getInfos(dataSourceId: DataSourceId, dataLayer: DataLayer)(implicit
-      ec: ExecutionContext
+  private def readSegmentStatisticsFileAttributesImpl(
+      segmentStatisticsFileKey: SegmentStatisticsFileKey
+  )(using ec: ExecutionContext, tc: TokenContext): Fox[SegmentStatisticsFileAttributes] =
+    for {
+      groupVaultPath <- dataVaultService.vaultPathFor(segmentStatisticsFileKey.attachment)
+      groupHeaderBytes <- (groupVaultPath / SegmentStatisticsFileAttributes.FILENAME_ZARR_JSON)
+        .readBytes() ?~> "Could not read segment statistics file zarr group file"
+      segmentStatisticsFileAttributes <- JsonHelper
+        .parseAs[SegmentStatisticsFileAttributes](groupHeaderBytes)
+        .toFox ?~> "Could not parse segment statistics file attributes from zarr group file."
+    } yield segmentStatisticsFileAttributes
+
+  private def readSegmentStatisticsFileAttributes(
+      segmentStatisticsFileKey: SegmentStatisticsFileKey
+  )(using ec: ExecutionContext, tc: TokenContext): Fox[SegmentStatisticsFileAttributes] =
+    attributesCache.getOrLoad(segmentStatisticsFileKey, key => readSegmentStatisticsFileAttributesImpl(key))
+
+  def getInfos(dataSourceId: DataSourceId, dataLayer: DataLayer)(using
+      ec: ExecutionContext,
+      tc: TokenContext
   ): Fox[SegmentStatisticsFileInfos] = for {
     key <- lookUpSegmentStatisticsFileKey(dataSourceId, dataLayer)
-    // TODO read from file metadata
-  } yield SegmentStatisticsFileInfos(Vec3Int.ones, Seq.empty, None)
+    attributes <- readSegmentStatisticsFileAttributes(key)
+    mag <- attributes.mag
+      .orElse(dataLayer.finestMag)
+      .toFox ?~> "Could not determine mag for segment statistics file, layer has no mags"
+  } yield SegmentStatisticsFileInfos(mag, Seq.empty, attributes.mappingName)
 
-  def clearCache(dataSourceId: DataSourceId, layerNameOpt: Option[String]): Int =
+  def clearCache(dataSourceId: DataSourceId, layerNameOpt: Option[String]): Int = {
+    attributesCache.clear { key =>
+      key.dataSourceId == dataSourceId && layerNameOpt.forall(_ == key.layerName)
+    }
+
     segmentStatisticsFileKeyCache.clear { case (keyDataSourceId, keyLayerName) =>
       dataSourceId == keyDataSourceId && layerNameOpt.forall(_ == keyLayerName)
     }
+  }
 }

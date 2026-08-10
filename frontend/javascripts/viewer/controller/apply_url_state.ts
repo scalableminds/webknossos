ddf351ae91b365bad6dfb1138d356824a22893a1
Philipp Otto <philippotto@users.noreply.github.com>
2026-08-10 12:00:02 +0200

    Add 3D camera state to sharing links (#9868)
    ### Summary
    Adds the camera state of the 3d viewport to the state encoded in sharing
    links. This iteration does NOT encode whether the camera is perspective
    or orthogonal. Two reasons for this:
    1) If we did this, links would "poison" the user config (i.e., a user
    that clicks on such a link will get their user config overwritten for
    the camera type)
    2) We expect most users to use the perspective camera anyway. If they
    deliberately switch to ortho, it's probably for a good reason (so, they
    don't want to get switched to perspective again). The only problematic
    scenario I see is when the "sharer" wants that other users view with an
    ortho camera. This wouldn't be possible yet. However, we can always do a
    follow-up for this.
    
    ### Steps to test:
    - create an annotation
    - load some meshes
    - rotate/zoom/pan in the 3d viewport
    - e.g., create a sharing link (sharing button next to the position
    field)
    - open in a new tab
    - beware that having the same annotation open in two tabs simultaneously
    will cause saving problems. either, ignore saving errors OR close the
    first tab quickly OR use another user for opening the link
    
    ### Issues:
    - fixes #9829
    
    ------
    (Please delete unneeded items, merge only when none are left open)
    - [x] Added changelog entry (create a `$PR_NUMBER.md` file in
    `unreleased_changes` or use `./tools/create-changelog-entry.py`)
    - [ ] Added migration guide entry if applicable (edit the same file as
    for the changelog)
    - [ ] Updated [documentation](../blob/master/docs) if applicable
    - [ ] Adapted [wk-libs python
    client](https://github.com/scalableminds/webknossos-libs/tree/master/webknossos/webknossos/client)
    if relevant API parts change
    - [ ] Removed dev-only changes like prints and application.conf edits
    - [ ] Considered [common edge
    cases](../blob/master/.github/common_edge_cases.md)
    - [ ] Needs datastore update after deployment

--- frontend/javascripts/viewer/controller/apply_url_state.ts
+++ frontend/javascripts/viewer/controller/apply_url_state.ts
@@ -43,6 +43,7 @@ import {
   setActiveNodeAction,
   setShowSkeletonsAction,
 } from "viewer/model/actions/skeletontracing_actions";
+import { setTDCameraWithoutTimeTrackingAction } from "viewer/model/actions/view_mode_actions";
 import Store from "viewer/store";
 
 export function applyState(
@@ -66,6 +67,10 @@ export function applyState(
 
   if (state.rotation != null) {
     Store.dispatch(setRotationAction(state.rotation));
+  }
+
+  if (state.tdCamera != null) {
+    Store.dispatch(setTDCameraWithoutTimeTrackingAction(state.tdCamera));
   }
 
   if (state.stateByLayer != null) {

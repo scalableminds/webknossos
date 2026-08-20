744d83d32e5b3fbe4f43bb43b03cf21ee1415944
Florian M <florian@scm.io>
2026-08-20 12:08:56 +0200

    Evolutions

---
+++ schema/evolutions/180-token-user-id.sql
@@ -0,0 +1,25 @@
+START TRANSACTION;
+
+do $$ begin if (select schemaVersion from webknossos.releaseInformation) <> 179 then raise exception 'Previous schema version mismatch'; end if; end; $$ language plpgsql;
+
+ALTER TABLE webknossos.tokens ADD COLUMN _user TEXT;
+
+-- loginInfo_providerKey already held the user id as a string (loginInfo_providerID was always 'credentials')
+UPDATE webknossos.tokens SET _user = loginInfo_providerKey;
+
+ALTER TABLE webknossos.tokens ADD CONSTRAINT _user_objectId CHECK (_user ~ '^[0-9a-f]{24}$');
+ALTER TABLE webknossos.tokens ALTER COLUMN _user SET NOT NULL;
+
+DROP INDEX webknossos.tokens_logininfo_providerid_logininfo_providerkey_tokentype_idx;
+CREATE INDEX ON webknossos.tokens(_user, tokenType);
+
+DROP VIEW webknossos.tokens_;
+ALTER TABLE webknossos.tokens DROP COLUMN loginInfo_providerID;
+ALTER TABLE webknossos.tokens DROP COLUMN loginInfo_providerKey;
+CREATE VIEW webknossos.tokens_ AS SELECT * FROM webknossos.tokens WHERE NOT isDeleted;
+
+DROP TYPE webknossos.USER_LOGININFO_PROVDERIDS;
+
+UPDATE webknossos.releaseInformation SET schemaVersion = 180;
+
+COMMIT TRANSACTION;

b8908f728f6ec096a8783aa2f0565dcbbe9f0061
Florian M <florian@scm.io>
2026-08-24 10:36:20 +0200

    add comment

--- app/security/LoginInfoAdapter.scala
+++ app/security/LoginInfoAdapter.scala
@@ -4,6 +4,7 @@ import com.scalableminds.util.objectid.ObjectId
 import play.silhouette.api.LoginInfo
 import play.silhouette.impl.providers.CredentialsProvider
 
+// The auth library silhouette expects a LoginInfo object instead of just the user id.
 object LoginInfoAdapter {
   def loginInfoFromUserId(userId: ObjectId): LoginInfo =
     LoginInfo(CredentialsProvider.ID, userId.toString)

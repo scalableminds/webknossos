5fa22623a40b88b1ff28d6b429b1c30369ba483e
Michael Büßemeyer <MichaelBuessemeyer@users.noreply.github.com>
2026-08-31 13:06:33 +0200

    remove warning about selector returning new object although store was in the same state (likely from react strict mode)

--- frontend/javascripts/libs/react_hooks.ts
+++ frontend/javascripts/libs/react_hooks.ts
@@ -368,6 +368,7 @@ export function useApi<TData, TQueryKey extends QueryKey = QueryKey>(
 /**
  * Hook that provides type-safe access to the Webknossos Redux store.
  * @param fn - Selector function that receives the Webknossos state
+ * @param equalityFn — An optional equality function or options object for customizing the behavior of the selector.
  * @returns Selected state value
  */
 export function useWkSelector<T>(fn: (state: WebknossosState) => T, equalityFn?: EqualityFn<T>): T {

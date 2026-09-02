scalableminds/webknossos

commit 70cca506c78df9be09052547edc131344d57fe85

Scala3 follow-up 4: Reduce implicit conversions, add scalafix (#9719)
### Summary
implicit conversions are a common source of pitfalls, so we need to be
particulate about when to use them. This PR removes most of them and
marks the rest with `import scala.language.implicitConversions` as
recommended by the scala compiler warning. Previously, it was enabled
everyhwere via a compiler flag in build.sbt
 - `FoxImplicits`: replaced by importable `toFox` extension in `Fox`
 - `q""` interpolator: replaced by extension on StringContext
- `Box` to `IterableOnce` – `Box` now actually implements the
`IterableOnce` interface!
- `Fox[Result]` to `Future[Result]` – Replaced by extension `fox` on
Action
- This revealed a lot of hidden conversions, which are now cleaned up,
mostly in `AuthenticationController`
- Also added formatting for the test code and fixed compiler warnings
there
- Added scalafix linter to the CI and fixed some files with it. Note
that some rules would require semanticDB. I disabled those rules,
because it slows down compilation significantly. I think the existing
rules are ok for us.

### Steps to test:
- CI should be enough

### Issues:
- contributes to #9606 

---
- [x] Removed dev-only changes like prints and application.conf edits
- [x] Considered [common edge
cases](../blob/master/.github/common_edge_cases.md)
- [x] Needs datastore update after deployment

app/controllers/WKRemoteWorkerController.scala
@@ -4,6 +4,7 @@ import com.scalableminds.util.Msg
 import com.scalableminds.util.accesscontext.{DBAccessContext, GlobalAccessContext}
 import com.scalableminds.util.objectid.ObjectId
 import com.scalableminds.util.tools.Fox
+import com.scalableminds.util.tools.Fox.toFox
 import models.aimodels.AiInferenceDAO
 import models.dataset.DatasetDAO
 import models.job.JobCommand.JobCommand
@@ -31,7 +32,7 @@ class WKRemoteWorkerController @Inject() (
 )(implicit ec: ExecutionContext, bodyParsers: PlayBodyParsers)
     extends Controller {
 
-  def requestJobs(key: String, workerVersion: Option[String]): Action[AnyContent] = Action.async {
+  def requestJobs(key: String, workerVersion: Option[String]): Action[AnyContent] = Action.fox { _ =>
     for {
       worker <- workerDAO.findOneByKey(key) ?~> Msg.Job.workerNotFound
       _ = workerDAO.updateHeartBeat(worker._id)
@@ -88,7 +89,7 @@ class WKRemoteWorkerController @Inject() (
     lowPriorityOrEmpty ++ highPriorityOrEmpty
   }
 
-  def updateJobStatus(key: String, id: ObjectId): Action[JobStatus] = Action.async(validateJson[JobStatus]) {
+  def updateJobStatus(key: String, id: ObjectId): Action[JobStatus] = Action.fox(validateJson[JobStatus]) {
     implicit request =>
       for {
         _ <- workerDAO.findOneByKey(key) ?~> Msg.Job.workerNotFound
@@ -113,7 +114,7 @@ class WKRemoteWorkerController @Inject() (
       } yield Ok
   }
 
-  def attachVoxelyticsWorkflow(key: String, id: ObjectId): Action[String] = Action.async(validateJson[String]) {
+  def attachVoxelyticsWorkflow(key: String, id: ObjectId): Action[String] = Action.fox(validateJson[String]) {
     implicit request =>
       for {
         _ <- workerDAO.findOneByKey(key) ?~> Msg.Job.workerNotFound
@@ -131,7 +132,7 @@ class WKRemoteWorkerController @Inject() (
   }
 
   def attachDatasetToInference(key: String, id: ObjectId): Action[String] =
-    Action.async(validateJson[String]) { implicit request =>
+    Action.fox(validateJson[String]) { implicit request =>
       implicit val ctx: DBAccessContext = GlobalAccessContext
       for {
         _ <- workerDAO.findOneByKey(key) ?~> Msg.Job.workerNotFound

app/models/job/JobService.scala
@@ -7,7 +7,8 @@ import com.scalableminds.webknossos.datastore.models.VoxelSize
 import models.dataset.Dataset
 import com.scalableminds.util.mvc.Formatter
 import com.scalableminds.util.objectid.ObjectId
-import com.scalableminds.util.tools.{Fox, FoxImplicits}
+import com.scalableminds.util.tools.Fox
+import com.scalableminds.util.tools.Fox.toFox
 import com.typesafe.scalalogging.LazyLogging
 import mail.{DefaultMails, MailchimpClient, MailchimpTag, Send}
 import models.analytics.{AnalyticsService, FailedJobEvent, RunJobEvent}
@@ -42,8 +43,7 @@ class JobService @Inject() (
     wkSilhouetteEnvironment: WkSilhouetteEnvironment,
     slackNotificationService: SlackNotificationService
 )(implicit ec: ExecutionContext)
-    extends FoxImplicits
-    with LazyLogging
+    extends LazyLogging
     with Formatter {
 
   private val ONE_GIGAVOXEL = math.pow(10, 9)

app/security/UserAwareRequestLogging.scala
@@ -2,17 +2,16 @@ package security
 
 import play.silhouette.api.actions.{SecuredRequest, UserAwareRequest}
 import com.scalableminds.util.requestlogging.AbstractRequestLogging
+import com.scalableminds.util.tools.Fox
 import play.api.mvc.{Request, Result}
-
-import scala.concurrent.{ExecutionContext, Future}
 
 trait UserAwareRequestLogging extends AbstractRequestLogging {
 
   case class RequesterIdOpt(id: Option[String]) // forcing implicit conversion
 
   def log(notifier: Option[String => Unit] = None)(
-      block: => Future[Result]
-  )(implicit request: Request[?], requesterIdOpt: RequesterIdOpt, ec: ExecutionContext): Future[Result] =
+      block: => Fox[Result]
+  )(implicit request: Request[?], requesterIdOpt: RequesterIdOpt): Fox[Result] =
     for {
       result: Result <- block
       _ = logRequestFormatted(request, result, notifier, requesterIdOpt.id)

util/src/main/scala/com/scalableminds/util/Msg.scala
@@ -624,7 +624,7 @@ object Msg {
   object Project {
     def notFound(id: ObjectId): String = s"Project “$id” could not be found or accessed."
     def notFound(name: String): String = s"Project with name “$name” could not be found or accessed."
-    val deleteSuccess: String = s"Project was successfully deleted."
+    val deleteSuccess: String = "Project was successfully deleted."
     def nameTaken(name: String): String =
       s"A project named “$name” already exists. Please choose a different name."
     def nameTooShort: String = "Project name must be at least three characters long."
@@ -700,7 +700,7 @@ object Msg {
       "You are not authorized to view this resource. Please log in."
     val passwordsDontMatch: String = "The two passwords do not match."
     val isDeactivated: String =
-      s"Your account has not been activated by an admin yet. Please contact your organization’s admin for help."
+      "Your account has not been activated by an admin yet. Please contact your organization’s admin for help."
     val noUserWithThisEmail: String = "There is no user registered with this email."
     val invalidCredentials: String = "Incorrect email or password. Please try again."
     val createFailed: String = "Could not create user."
@@ -719,7 +719,7 @@ object Msg {
     val invalidLastName: String = "Please check your last name for any special characters."
     object Token {
       val deleted: String = "Token was deleted."
-      val invalid: String = "The supplied token is invalid."
+      val invalid: String = "The supplied token is invalid or expired."
     }
     object Configuration {
       val updateSuccess: String = "Your configuration was successfully updated."

util/src/main/scala/com/scalableminds/util/mvc/ExtendedController.scala
@@ -3,7 +3,7 @@ package com.scalableminds.util.mvc
 import com.google.protobuf.CodedInputStream
 import com.scalableminds.util.Msg
 import com.scalableminds.util.accesscontext.TokenContext
-import com.scalableminds.util.tools.{Fox, FoxImplicits}
+import com.scalableminds.util.tools.Fox
 import com.typesafe.scalalogging.LazyLogging
 import com.scalableminds.util.tools._
 import com.scalableminds.util.tools.Box.tryo
@@ -19,18 +19,28 @@ import play.filters.csp.CSPConfig
 import java.io.FileInputStream
 import scala.concurrent.{ExecutionContext, Future}
 
-trait BoxToResultHelpers extends Formatter with RemoteOriginHelpers with HeaderNames {
+trait FoxToResultHelpers extends Formatter with RemoteOriginHelpers with HeaderNames {
 
+  // Override this in your controller to add the CORS headers to the results of its actions
+  protected def allowRemoteOrigin: Boolean = false
+
+  // Override this in your controller make this the default error code for Failures
   protected def defaultErrorCode: Int = BAD_REQUEST
 
-  def asResult[T <: Result](b: Box[T]): Result = {
+  extension [R[_], B](ab: ActionBuilder[R, B])
+    def fox(block: R[B] => Fox[Result])(using ec: ExecutionContext): Action[B] =
+      ab.async(req => block(req).futureBox.map(asResult))
+    def fox[A](bodyParser: BodyParser[A])(block: R[A] => Fox[Result])(using ec: ExecutionContext): Action[A] =
+      ab.async(bodyParser)(req => block(req).futureBox.map(asResult))
+
+  private def asResult[T <: Result](b: Box[T]): Result = {
     val result = b match {
       case Full(result) =>
         result
       case ParamFailure(msg, _, chain, statusCode: Int) =>
         new JsonResult(statusCode)(msg, formatChainOpt(chain))
       case ParamFailure(_, _, _, msgs: JsArray) =>
-        new JsonResult(defaultErrorCode)(jsonMessages(msgs))
+        new JsonResult(defaultErrorCode)(Json.obj("messages" -> msgs))
       case Failure(msg, _, chain) =>
         new JsonResult(defaultErrorCode)(msg, formatChainOpt(chain))
       case Empty =>
@@ -45,21 +55,16 @@ trait BoxToResultHelpers extends Formatter with RemoteOriginHelpers with HeaderN
       case _           => None
     }
 
-  private def jsonMessages(msgs: JsArray): JsObject =
-    Json.obj("messages" -> msgs)
-
-  // Override this in your controller to add the CORS headers to the results of its actions
-  def allowRemoteOrigin: Boolean = false
-
   private def allowRemoteOriginIfSelected(result: Result): Result =
     if (allowRemoteOrigin) {
       addRemoteOriginHeaders(result)
     } else result
 
-  def addNoCacheHeaderFallback(result: Result): Result =
+  protected def addNoCacheHeaderFallback(result: Result): Result =
     if (result.header.headers.contains(CACHE_CONTROL)) {
       result
     } else result.withHeaders(CACHE_CONTROL -> "no-cache")
+
 }
 
 trait RemoteOriginHelpers {
@@ -83,31 +88,6 @@ trait CspHeaders extends HeaderNames {
       action: Action[AnyContent]
   )(implicit request: Request[AnyContent], ec: ExecutionContext): Future[Result] =
     action.apply(request).map(addCspHeader)
-}
-
-trait ResultImplicits extends BoxToResultHelpers {
-
-  implicit def fox2FutureResult[T <: Result](b: Fox[T])(implicit ec: ExecutionContext): Future[Result] =
-    b.futureBox.map(asResult)
-
-  implicit def futureBox2Result[T <: Result](b: Box[Future[T]])(implicit ec: ExecutionContext): Future[Result] =
-    b match {
-      case Full(f) =>
-        f.map(value => asResult(Full(value)))
-      case Empty =>
-        Future.successful(asResult(Empty))
-      case f: Failure =>
-        Future.successful(asResult(f))
-    }
-
-  implicit def boxFuture2Result[T <: Result](f: Future[Box[T]])(implicit ec: ExecutionContext): Future[Result] =
-    f.map { b =>
-      asResult(b)
-    }
-
-  implicit def box2Result[T <: Result](b: Box[T]): Result =
-    asResult(b)
-
 }
 
 class JsonResult(status: Int)
@@ -192,8 +172,8 @@ trait JsonResults extends JsonResultAttributes {
 }
 
 trait JsonResultAttributes {
-  val jsonSuccess = "success"
-  val jsonError = "error"
+  protected val jsonSuccess = "success"
+  protected val jsonError = "error"
 }
 
 trait ValidationHelpers {
@@ -228,8 +208,7 @@ trait RequestTokenHelper {
 
 trait ExtendedController
     extends JsonResults
-    with FoxImplicits
-    with ResultImplicits
+    with FoxToResultHelpers
     with Status
     with InjectedController
     with MimeTypes

util/src/main/scala/com/scalableminds/util/requestlogging/RequestLogging.scala
@@ -2,12 +2,12 @@ package com.scalableminds.util.requestlogging
 
 import com.scalableminds.util.mvc.Formatter
 import com.scalableminds.util.time.Instant
+import com.scalableminds.util.tools.Fox
 import com.typesafe.scalalogging.LazyLogging
 import play.api.http.{HttpEntity, Status}
 import play.api.mvc.{Request, Result}
 
-import scala.concurrent.duration._
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.*
 
 trait AbstractRequestLogging extends LazyLogging with Formatter {
 
@@ -32,8 +32,8 @@ trait AbstractRequestLogging extends LazyLogging with Formatter {
     }
 
   def logTime(notifier: String => Unit, durationThreshold: FiniteDuration = 2 minutes)(
-      block: => Future[Result]
-  )(implicit request: Request[?], ec: ExecutionContext): Future[Result] = {
+      block: => Fox[Result]
+  )(implicit request: Request[?]): Fox[Result] = {
     def logTimeFormatted(executionTime: FiniteDuration, request: Request[?], result: Result): Unit = {
       val debugString =
         s"Request `${request.method}` `${request.uri}` took ${formatDuration(executionTime)} and was${
@@ -59,7 +59,7 @@ trait RequestLogging extends AbstractRequestLogging {
 
   def log(
       notifier: Option[String => Unit] = None
-  )(block: => Future[Result])(implicit request: Request[?], ec: ExecutionContext): Future[Result] =
+  )(block: => Fox[Result])(implicit request: Request[?]): Fox[Result] =
     for {
       result: Result <- block
       _ = logRequestFormatted(request, result, notifier)

commit d2ea212b84b430bd43029c251840f18690222a36

Fix server-side logging of failed requests (#9959)
### Summary
- While investigating a failed request I noticed that the server-side
logging of failed requests no longer fired. This is a regression from
#9719 where the handling of failed requests changed.
- This PR fixes that, while also adding this logging for another
request.
 - Make sure to review while hiding whitespace changes
- redact GET params “key” and “token” from URIs for logging, avoiding
leaked secrets

### Steps to test:
(I tested locally)
- provoke error locally for a route that has the log(){} wrapper, should
see “Answering 400 for…” in backend logs

------
- [x] Removed dev-only changes like prints and application.conf edits
- [x] Considered [common edge
cases](../blob/master/.github/common_edge_cases.md)
- [x] Needs datastore update after deployment

---------

Co-authored-by: MichaelBuessemeyer <39529669+MichaelBuessemeyer@users.noreply.github.com>

app/controllers/WKRemoteWorkerController.scala
@@ -91,28 +91,30 @@ class WKRemoteWorkerController @Inject() (
 
   def updateJobStatus(key: String, id: ObjectId): Action[JobStatus] = Action.fox(validateJson[JobStatus]) {
     implicit request =>
-      for {
-        _ <- workerDAO.findOneByKey(key) ?~> Msg.Job.workerNotFound
-        jobBeforeChange <- jobDAO.findOne(id)(using GlobalAccessContext)
-        _ <- jobDAO.updateStatus(id, request.body) ?~> Msg.Job.updateStatusFailed
-        jobAfterChange <- jobDAO.findOne(id)(using GlobalAccessContext) ?~> Msg.Job.notFound
-        _ = jobService.trackStatusChange(jobBeforeChange, jobAfterChange)
-        _ <- jobService.cleanUpIfFailed(jobAfterChange) ?~> Msg.Job.cleanupFailed
-        _ = jobService.cleanUpUploadFilesIfNeeded(jobBeforeChange, jobAfterChange)
-        _ <- Fox.runIf(request.body.state == JobState.SUCCESS) {
-          creditTransactionService.completeTransactionOfJob(jobAfterChange._id)(using
-            GlobalAccessContext
-          ) ?~> Msg.Job.Credits.failed
-        }
-        _ <- Fox.runIf(
-          jobBeforeChange.state != request.body.state && (request.body.state == JobState.FAILURE || request.body.state == JobState.CANCELLED)
-        ) {
-          creditTransactionService.refundTransactionForJob(
-            jobBeforeChange._id,
-            isCancelled = request.body.state == JobState.CANCELLED
-          )(using GlobalAccessContext) ?~> Msg.Job.Credits.refundFailed
-        }
-      } yield Ok
+      log() {
+        for {
+          _ <- workerDAO.findOneByKey(key) ?~> Msg.Job.workerNotFound
+          jobBeforeChange <- jobDAO.findOne(id)(using GlobalAccessContext)
+          _ <- jobDAO.updateStatus(id, request.body) ?~> Msg.Job.updateStatusFailed
+          jobAfterChange <- jobDAO.findOne(id)(using GlobalAccessContext) ?~> Msg.Job.notFound
+          _ = jobService.trackStatusChange(jobBeforeChange, jobAfterChange)
+          _ <- jobService.cleanUpIfFailed(jobAfterChange) ?~> Msg.Job.cleanupFailed
+          _ = jobService.cleanUpUploadFilesIfNeeded(jobBeforeChange, jobAfterChange)
+          _ <- Fox.runIf(request.body.state == JobState.SUCCESS) {
+            creditTransactionService.completeTransactionOfJob(jobAfterChange._id)(using
+              GlobalAccessContext
+            ) ?~> Msg.Job.Credits.failed
+          }
+          _ <- Fox.runIf(
+            jobBeforeChange.state != request.body.state && (request.body.state == JobState.FAILURE || request.body.state == JobState.CANCELLED)
+          ) {
+            creditTransactionService.refundTransactionForJob(
+              jobBeforeChange._id,
+              isCancelled = request.body.state == JobState.CANCELLED
+            )(using GlobalAccessContext) ?~> Msg.Job.Credits.refundFailed
+          }
+        } yield Ok
+      }
   }
 
   def attachVoxelyticsWorkflow(key: String, id: ObjectId): Action[String] = Action.fox(validateJson[String]) {

app/models/job/JobService.scala
@@ -191,8 +191,8 @@ class JobService @Inject() (
         organizationId <- commandArgs.get("organization_id").map(_.as[String]).toFox
         dataset <- datasetDAO.findOneByDirectoryNameAndOrganization(datasetDirectoryName, organizationId)(using
           GlobalAccessContext
-        )
-        _ <- datasetDAO.deleteDataset(dataset._id)
+        ) ?~> Msg.Dataset.notFound(datasetDirectoryName)
+        _ <- datasetDAO.deleteDataset(dataset._id) ?~> Msg.Dataset.deleteFromDbFailed
       } yield ()
     } else Fox.successful(())
 

app/security/UserAwareRequestLogging.scala
@@ -5,17 +5,23 @@ import com.scalableminds.util.requestlogging.AbstractRequestLogging
 import com.scalableminds.util.tools.Fox
 import play.api.mvc.{Request, Result}
 
+import scala.concurrent.ExecutionContext
+
 trait UserAwareRequestLogging extends AbstractRequestLogging {
 
   case class RequesterIdOpt(id: Option[String]) // forcing implicit conversion
 
   def log(notifier: Option[String => Unit] = None)(
       block: => Fox[Result]
-  )(implicit request: Request[?], requesterIdOpt: RequesterIdOpt): Fox[Result] =
-    for {
-      result: Result <- block
+  )(implicit
+      request: Request[?],
+      ec: ExecutionContext,
+      requesterIdOpt: RequesterIdOpt = RequesterIdOpt(None)
+  ): Fox[Result] =
+    Fox.fromFuture(for {
+      result: Result <- block.futureBox.map(boxToResult)
       _ = logRequestFormatted(request, result, notifier, requesterIdOpt.id)
-    } yield result
+    } yield result)
 
   implicit def userAwareRequestToRequesterIdOpt(implicit request: UserAwareRequest[WkEnv, ?]): RequesterIdOpt =
     RequesterIdOpt(request.identity.map(_._id.toString))

util/src/main/scala/com/scalableminds/util/Msg.scala
@@ -434,7 +434,8 @@ object Msg {
     val allowedTeamsNotFound: String = "Could not find allowed teams for dataset."
     val voxelSizeFailedToFetch: String = "Could not fetch voxel size for annotation."
     val additionalCoordinatesDiffer: String = "Additional coordinates differ in merged units."
-    val findByImportURLFailed = "Failed to look up whether a dataset with the import url already exists."
+    val findByImportURLFailed = "Could not look up whether a dataset with the import url already exists."
+    val deleteFromDbFailed = "Could not delete dataset from the database."
     object Compose {
       val failed: String = "Could not compose dataset."
       val addAttachmentFailed: String = "Could not add attachment to composed dataset."

util/src/main/scala/com/scalableminds/util/mvc/ExtendedController.scala
@@ -29,11 +29,11 @@ trait FoxToResultHelpers extends Formatter with RemoteOriginHelpers with HeaderN
 
   extension [R[_], B](ab: ActionBuilder[R, B])
     def fox(block: R[B] => Fox[Result])(using ec: ExecutionContext): Action[B] =
-      ab.async(req => block(req).futureBox.map(asResult))
+      ab.async(req => block(req).futureBox.map(boxToResult))
     def fox[A](bodyParser: BodyParser[A])(block: R[A] => Fox[Result])(using ec: ExecutionContext): Action[A] =
-      ab.async(bodyParser)(req => block(req).futureBox.map(asResult))
+      ab.async(bodyParser)(req => block(req).futureBox.map(boxToResult))
 
-  private def asResult[T <: Result](b: Box[T]): Result = {
+  protected def boxToResult[T <: Result](b: Box[T]): Result = {
     val result = b match {
       case Full(result) =>
         result

util/src/main/scala/com/scalableminds/util/requestlogging/RequestLogging.scala
@@ -1,15 +1,16 @@
 package com.scalableminds.util.requestlogging
 
-import com.scalableminds.util.mvc.Formatter
+import com.scalableminds.util.mvc.{Formatter, FoxToResultHelpers}
 import com.scalableminds.util.time.Instant
 import com.scalableminds.util.tools.Fox
 import com.typesafe.scalalogging.LazyLogging
 import play.api.http.{HttpEntity, Status}
 import play.api.mvc.{Request, Result}
 
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.*
 
-trait AbstractRequestLogging extends LazyLogging with Formatter {
+trait AbstractRequestLogging extends LazyLogging with Formatter with FoxToResultHelpers {
 
   def logRequestFormatted(
       request: Request[?],
@@ -20,9 +21,16 @@ trait AbstractRequestLogging extends LazyLogging with Formatter {
     if (!Status.isSuccessful(result.header.status)) {
       val userIdMsg = requesterId.map(id => s" for user $id").getOrElse("")
       val resultMsg = s": `${resultBody(result)}`"
-      val msg = s"Answering ${result.header.status} at ${request.uri}$userIdMsg$resultMsg"
+      val msg = s"Answering ${result.header.status} at ${redactUri(request.uri)}$userIdMsg$resultMsg"
       logger.warn(msg)
       notifier.foreach(_(msg))
+    }
+
+  private val redactedUriParams = Seq("key", "token")
+
+  private def redactUri(uri: String): String =
+    redactedUriParams.foldLeft(uri) { (uriAcc, param) =>
+      uriAcc.replaceAll(s"([?&]$param=)[^&]*", "$1xxx")
     }
 
   private def resultBody(result: Result): String =
@@ -33,10 +41,10 @@ trait AbstractRequestLogging extends LazyLogging with Formatter {
 
   def logTime(notifier: String => Unit, durationThreshold: FiniteDuration = 2 minutes)(
       block: => Fox[Result]
-  )(implicit request: Request[?]): Fox[Result] = {
+  )(implicit request: Request[?], ec: ExecutionContext): Fox[Result] = {
     def logTimeFormatted(executionTime: FiniteDuration, request: Request[?], result: Result): Unit = {
       val debugString =
-        s"Request `${request.method}` `${request.uri}` took ${formatDuration(executionTime)} and was${
+        s"Request `${request.method}` `${redactUri(request.uri)}` took ${formatDuration(executionTime)} and was${
             if (result.header.status != 200) " not "
             else " "
           }successful"
@@ -45,11 +53,11 @@ trait AbstractRequestLogging extends LazyLogging with Formatter {
     }
 
     val start = Instant.now
-    for {
-      result: Result <- block
+    Fox.fromFuture(for {
+      result: Result <- block.futureBox.map(boxToResult)
       executionTime = Instant.since(start)
       _ = if (executionTime > durationThreshold) logTimeFormatted(executionTime, request, result)
-    } yield result
+    } yield result)
   }
 
 }
@@ -59,10 +67,10 @@ trait RequestLogging extends AbstractRequestLogging {
 
   def log(
       notifier: Option[String => Unit] = None
-  )(block: => Fox[Result])(implicit request: Request[?]): Fox[Result] =
-    for {
-      result: Result <- block
+  )(block: => Fox[Result])(implicit request: Request[?], ec: ExecutionContext): Fox[Result] =
+    Fox.fromFuture(for {
+      result: Result <- block.futureBox.map(boxToResult)
       _ = logRequestFormatted(request, result, notifier)
-    } yield result
+    } yield result)
 
 }
b82bedb3af2266d5b436629f96ff73e013d3efc1
Florian M <florian@scm.io>
2026-09-03 10:58:44 +0200

    bump schema version

--- schema/schema.sql
+++ schema/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE webknossos.releaseInformation (
   schemaVersion BIGINT NOT NULL
 );
 
-INSERT INTO webknossos.releaseInformation(schemaVersion) values(181);
+INSERT INTO webknossos.releaseInformation(schemaVersion) values(182);
 COMMIT TRANSACTION;
 
 

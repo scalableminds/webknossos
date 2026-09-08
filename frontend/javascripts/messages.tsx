1904f97665a4058a33ffe312472ae0b672d15907
Philipp Otto <philipp.4096@gmail.com>
2026-09-08 20:34:20 +0200

    dont link to settings page for not-imported datasets (but still allow to open settings page via URL for pro users)

--- frontend/javascripts/messages.tsx
+++ frontend/javascripts/messages.tsx
@@ -356,9 +356,9 @@ instead. Only enable this option if you understand its effect. All layers will n
     "The explored data has a different voxel size from the datasource that was already loaded. The explored voxel size was:",
   "dataset.segmentationlayer_not_existing": "This annotation has no segmentation layer.",
   "dataset.invalid_datasource_json":
-    "The datasource-properties.json on disk is invalid. Please review all properties below to use the dataset. You can always go back and change the values later.",
+    "The datasource-properties.json on disk is invalid. Please contact an administrator to fix this dataset.",
   "dataset.missing_datasource_json":
-    "A datasource-properties.json file was not found. Please review all properties below to use the dataset. You can always go back and change the values later.",
+    "A datasource-properties.json file was not found. Please contact an administrator to fix this dataset.",
   "dataset.import_complete":
     "A valid datasource-properties.json file was found. The dataset is imported and ready to use. You may still change the properties below.",
   "dataset.confirm_signup":

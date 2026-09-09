1e4633383fb4faa30b1ebff98f8f43158baa9ec6
Florian M <florian@scm.io>
2026-09-09 13:21:50 +0200

    cleanup

--- webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/TSThreadPoolHealthService.scala
+++ webknossos-tracingstore/app/com/scalableminds/webknossos/tracingstore/TSThreadPoolHealthService.scala
@@ -7,9 +7,6 @@ import play.api.inject.ApplicationLifecycle
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
-// Registers periodic thread-pool health logging for the plain (unqualified) default ActorSystem, i.e. the one
-// backing controller/request execution contexts, so this also runs in a standalone tracingstore deployment
-// (which never loads app/Startup.scala). Dedupes with the main app's and the datastore's registration.
 class TSThreadPoolHealthService @Inject() (actorSystem: ActorSystem, lifecycle: ApplicationLifecycle)(implicit
     ec: ExecutionContext
 ) {

57f64b8c470792221eea22f1d26dae6a9e0ec5a5
Philipp Otto <philipp.4096@gmail.com>
2026-09-11 16:07:28 +0200

    remove obsolete import

--- frontend/javascripts/dashboard/advanced_dataset/dataset_action_view.tsx
+++ frontend/javascripts/dashboard/advanced_dataset/dataset_action_view.tsx
@@ -22,7 +22,7 @@ import messages from "messages";
 import type * as React from "react";
 import { useState } from "react";
 import { Link } from "react-router";
-import type { APIDataset, APIDatasetCompact } from "types/api_types";
+import type { APIDatasetCompact } from "types/api_types";
 import { getReadableURLPart, getViewDatasetURL } from "viewer/model/accessors/dataset_accessor";
 import { getNoActionsAvailableMenu } from "viewer/view/context_menu/helpers";
 
